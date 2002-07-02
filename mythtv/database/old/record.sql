bb1cf3f8d9b5ceb1e9b08880a7971e1417d293de
Isaac Richards <irichards@mythtv.org>
2002-07-02 20:38:38 +0000

    Adding some table to keep track of recordings.
    git-svn-id: http://svn.mythtv.org/svn/trunk@81 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/database/old/record.sql
+++ mythtv/database/old/record.sql
@@ -1,25 +1,4 @@
-CREATE DATABASE mythconverg;
-GRANT ALL ON mythconverg.* TO mythtv@localhost IDENTIFIED BY "mythtv";
 USE mythconverg;
-CREATE TABLE channel
-(
-    channum INT UNSIGNED NOT NULL PRIMARY KEY,
-    callsign VARCHAR(20) NULL,
-    name VARCHAR(20) NULL,
-    icon VARCHAR(255) NULL
-);
-CREATE TABLE program
-(
-    channum INT UNSIGNED NOT NULL,
-    starttime TIMESTAMP NOT NULL,
-    endtime TIMESTAMP NOT NULL,
-    title VARCHAR(128) NULL,
-    subtitle VARCHAR(128) NULL,
-    description TEXT NULL,
-    category VARCHAR(64) NULL,
-    PRIMARY KEY (channum, starttime),
-    INDEX (endtime)
-);
 CREATE TABLE singlerecord
 (
     channum INT UNSIGNED NOT NULL,
@@ -56,4 +35,4 @@ CREATE TABLE recorded
     PRIMARY KEY (channum, starttime),
     INDEX (endtime)
 );
-     
+
