69f2b1639f024b042258a27dc2e00143d88f3624
Isaac Richards <irichards@mythtv.org>
2002-07-15 00:51:18 +0000

    Adding the database changes. Add .cvsignore files to shut up cvs diff.
    git-svn-id: http://svn.mythtv.org/svn/trunk@111 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/database/mc.sql
+++ mythtv/database/mc.sql
@@ -61,4 +61,30 @@ CREATE TABLE settings
     data TEXT NULL,
     PRIMARY KEY(value)
 );
-     
+CREATE TABLE conflictresolutionoverride
+(
+    channum INT UNSIGNED NOT NULL,
+    starttime TIMESTAMP NOT NULL,
+    endtime TIMESTAMP NOT NULL,
+    PRIMARY KEY (channum, starttime),
+    INDEX (endtime)
+);
+CREATE TABLE conflictresolutionsingle
+(
+    preferchannum INT UNSIGNED NOT NULL,
+    preferstarttime TIMESTAMP NOT NULL,
+    preferendtime TIMESTAMP NOT NULL,
+    dislikechannum INT UNSIGNED NOT NULL,
+    dislikestarttime TIMESTAMP NOT NULL,
+    dislikeendtime TIMESTAMP NOT NULL,
+    PRIMARY KEY (preferchannum, preferstarttime),
+    INDEX (preferendtime)
+);
+CREATE TABLE conflictresolutionany
+(
+    prefertitle VARCHAR(128) NOT NULL,
+    disliketitle VARCHAR(128) NOT NULL,
+    INDEX (prefertitle),
+    INDEX (disliketitle)
+);
+ 
