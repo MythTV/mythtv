c8480e5883078603c33e8906bc9d7ec5b18e5783
Thor Sigvaldason <tsigvaldason@mythtv.org>
2002-09-21 22:06:17 +0000

    Made mythlcd compile against libmyth instead of just local mythlcd.cpp
    git-svn-id: http://svn.mythtv.org/svn/trunk@226 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythlcd/main.cpp
+++ mythtv/programs/mythlcd/main.cpp
@@ -6,9 +6,26 @@
 #include <stream.h>
 #include <qapplication.h>
 #include "mythlcd.h"
+#include "settings.h"
+
+//
+//	Temp hack as can't link with libmyth unless this exists
+//
+
+Settings *globalsettings;
+char installprefix[] = "/usr/local";
+
 
 int main(int argc, char **argv)
 {
+	//
+	//	Temp hack
+	//
+	
+	globalsettings = new Settings;
+    globalsettings->LoadSettingsFiles("theme.txt", installprefix);
+    globalsettings->LoadSettingsFiles("mysql.txt", installprefix);
+	
 
 	QApplication a(argc, argv);
 	MythLCD mythLCD;
