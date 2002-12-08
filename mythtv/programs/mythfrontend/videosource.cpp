d2d10c36a26fbbecdf7f49cfe5b647f6f909b417
Matt Zimmerman <mzimmerman@mythtv.org>
2002-12-08 23:27:26 +0000

    Getting there
    git-svn-id: http://svn.mythtv.org/svn/trunk@489 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythfrontend/videosource.cpp
+++ mythtv/programs/mythfrontend/videosource.cpp
@@ -4,24 +4,108 @@
 #include <qlayout.h>
 #include <iostream>
 
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/videodev.h>
+
 QString VSSetting::whereClause(void) {
-  return QString("sourceid = %1").arg(parent.getSourceID());
+    return QString("sourceid = %1").arg(parent.getSourceID());
 }
 
 QString VSSetting::setClause(void) {
-  return QString("sourceid = %1, %2 = '%3'")
-    .arg(parent.getSourceID())
-    .arg(getColumn())
-    .arg(getValue());
+    return QString("sourceid = %1, %2 = '%3'")
+        .arg(parent.getSourceID())
+        .arg(getColumn())
+        .arg(getValue());
 }
 
-QString CaptureCard::CCSetting::whereClause(void) {
-  return QString("cardid = %1").arg(parent.getCardID());
+QString CCSetting::whereClause(void) {
+    return QString("cardid = %1").arg(parent.getCardID());
 }
 
-QString CaptureCard::CCSetting::setClause(void) {
-  return QString("cardid = %1, %2 = '%3'")
-    .arg(parent.getCardID())
-    .arg(getColumn())
-    .arg(getValue());
+QString CCSetting::setClause(void) {
+    return QString("cardid = %1, %2 = '%3'")
+        .arg(parent.getCardID())
+        .arg(getColumn())
+        .arg(getValue());
+}
+
+QString CISetting::whereClause(void) {
+    return QString("cardinputid = %1").arg(parent.getInputID());
+}
+
+QString CISetting::setClause(void) {
+    return QString("cardinputid = %1, %2 = '%3'")
+        .arg(parent.getInputID())
+        .arg(getColumn())
+        .arg(getValue());
+}
+
+void VideoSource::fillSelections(QSqlDatabase* db,
+                                 StringSelectSetting* setting) {
+    QString query = QString("SELECT sourceid, name FROM videosource");
+    QSqlQuery result = db->exec(query);
+
+    if (result.isActive() && result.numRowsAffected() > 0)
+        while (result.next())
+            setting->addSelection(result.value(1).toString(),
+                                  result.value(0).toString());
+}
+
+void CaptureCard::fillSelections(QSqlDatabase* db,
+                                 StringSelectSetting* setting) {
+    QString query = QString("SELECT cardid, videodevice FROM capturecard");
+    QSqlQuery result = db->exec(query);
+
+    if (result.isActive() && result.numRowsAffected() > 0)
+        while (result.next())
+            setting->addSelection(result.value(1).toString(),
+                                  result.value(0).toString());
+}
+
+void CardInput::fillSelections(QSqlDatabase* db) {
+    selector->fillSelections(db);
+    sourceid->fillSelections(db);
+}
+
+int CISetting::getInputID(void) const {
+    return parent.getInputID();
+}
+
+int CCSetting::getCardID(void) const {
+    return parent.getCardID();
+}
+
+void VideoInputSelector::fillSelections(QSqlDatabase* db) {
+    cardid->fillSelections(db);
+}
+
+
+void VideoInputSelector::probeInputs(const QString& videoDevice, QString cardID) {
+    int videofd = open(videoDevice.ascii(), O_RDWR);
+    if (videofd <= 0) {
+        cout << "Couldn't open " << videoDevice << " to probe its inputs.\n";
+        return;
+    }
+
+    InputName* name = new InputName(parent);
+    addTrigger(cardID, name);
+
+    struct video_capability vidcap;
+    memset(&vidcap, 0, sizeof(vidcap));
+    // xxx should check return value
+    ioctl(videofd, VIDIOCGCAP, &vidcap);
+    
+    for (int i = 0; i < vidcap.channels; i++) {
+        struct video_channel test;
+        memset(&test, 0, sizeof(test));
+        test.channel = i;
+        // xxx should check return value
+        ioctl(videofd, VIDIOCGCHAN, &test);
+        name->addSelection(test.name);
+    }
 }
--- mythtv/programs/mythfrontend/videosource.h
+++ mythtv/programs/mythfrontend/videosource.h
@@ -32,6 +32,8 @@ public:
         
     int getSourceID(void) const { return id->intValue(); };
 
+    static void fillSelections(QSqlDatabase* db, StringSelectSetting* setting);
+
 private:
     class ID: virtual public IntegerSetting,
               public AutoIncrementStorage {
@@ -57,16 +59,85 @@ private:
     ID* id;
 };
 
+class CaptureCard;
+class CCSetting: virtual public Setting, public SimpleDBStorage {
+protected:
+    CCSetting(const CaptureCard& _parent, QString name):
+        SimpleDBStorage("capturecard", name),
+        parent(_parent) {
+        setName(name);
+    };
+
+    int getCardID(void) const;
+
+protected:
+    virtual QString setClause(void);
+    virtual QString whereClause(void);
+private:
+    const CaptureCard& parent;
+};
+
+class VideoDevice: public PathSetting, public CCSetting {
+public:
+    VideoDevice(const CaptureCard& parent):
+        PathSetting(true),
+        CCSetting(parent, "videodevice") {
+        setLabel("Video device");
+        addSelection("/dev/video");
+        addSelection("/dev/video0");
+        addSelection("/dev/video1");
+        addSelection("/dev/v4l/video0");
+        addSelection("/dev/v4l/video1");
+    };
+};
+
+class AudioDevice: public PathSetting, public CCSetting {
+public:
+    AudioDevice(const CaptureCard& parent):
+        PathSetting(true),
+        CCSetting(parent, "audiodevice") {
+        setLabel("Audio device");
+        addSelection("/dev/dsp");
+        addSelection("/dev/dsp1");
+        addSelection("/dev/sound/dsp");
+    };
+};
+
+class CardType: public ComboBoxSetting, public CCSetting {
+public:
+    CardType(const CaptureCard& parent):
+        CCSetting(parent, "cardtype") {
+        setLabel("Card type");
+        addSelection("V4L");
+    };
+};
+
+class AudioRateLimit: public SpinBoxSetting, public CCSetting {
+public:
+    AudioRateLimit(const CaptureCard& parent):
+        SpinBoxSetting(0, 48000, 100),
+        CCSetting(parent, "audioratelimit") {
+        setLabel("Audio sampling rate limit");
+        setValue(0);
+    };
+};
+
 class CaptureCard: public VerticalConfigurationGroup {
 public:
     CaptureCard() {
+        setLabel("Capture card");
         // must be first
         addChild(id = new ID());
+        addChild(new VideoDevice(*this));
+        addChild(new AudioDevice(*this));
+        addChild(new AudioRateLimit(*this));
     };
 
     int getCardID(void) const {
         return id->intValue();
     };
+
+    static void fillSelections(QSqlDatabase* db, StringSelectSetting* setting);
 
 private:
     class ID: virtual public IntegerSetting,
@@ -75,6 +146,7 @@ private:
         ID():
             AutoIncrementStorage("capturecard", "cardid") {
             setVisible(false);
+            setName("ID");
         };
         virtual QWidget* configWidget(QWidget* parent, const char* widgetName = 0) {
             (void)parent; (void)widgetName;
@@ -82,49 +154,125 @@ private:
         };
     };
 
-    class CCSetting: virtual public Setting, public SimpleDBStorage {
-    protected:
-        CCSetting(const CaptureCard& _parent, QString name):
-            SimpleDBStorage("capturecard", name),
-            parent(_parent) {
-            setName(name);
-        };
 
-        int getCardID(void) const { return parent.getCardID(); };
+private:
+    ID* id;
+};
 
-    protected:
-        virtual QString setClause(void);
-        virtual QString whereClause(void);
-    private:
-        const CaptureCard& parent;
+class CardInput;
+class CISetting: virtual public Setting, public SimpleDBStorage {
+protected:
+    CISetting(const CardInput& _parent, QString name):
+        SimpleDBStorage("cardinput", name),
+        parent(_parent) {
+        setName(name);
     };
 
-    class VideoDevice: public PathSetting, public CCSetting {
-    public:
-        VideoDevice(const CaptureCard& parent):
-            PathSetting(true),
-            CCSetting(parent, "VideoDevice") {
-            addSelection("/dev/video");
-            addSelection("/dev/video0");
-            addSelection("/dev/video1");
-            addSelection("/dev/v4l/video0");
-            addSelection("/dev/v4l/video1");
-        };
+    int getInputID(void) const;
+
+    void fillSelections(QSqlDatabase* db);
+
+protected:
+    virtual QString setClause(void);
+    virtual QString whereClause(void);
+private:
+    const CardInput& parent;
+};
+
+class CardID: public ComboBoxSetting, public CISetting {
+public:
+    CardID(const CardInput& parent):
+        CISetting(parent, "cardid") {
+        setLabel("Capture device");
     };
 
-    class AudioDevice: public PathSetting, public CCSetting {
+    void fillSelections(QSqlDatabase* db) {
+        CaptureCard::fillSelections(db, this);
+    };
+};
+
+
+class VideoInputSelector: public TriggeredConfigurationGroup,
+                          public VerticalConfigurationGroup {
+    Q_OBJECT
+public:
+    VideoInputSelector(const CardInput& _parent):
+        parent(_parent) {
+        setLabel("Input");
+        addChild(cardid = new CardID(parent));
+        setTrigger(cardid);
+
+        connect(cardid, SIGNAL(selectionAdded(const QString&,
+                                              QString)),
+                this, SLOT(probeInputs(const QString&,
+                                       QString)));
+    };
+
+    void fillSelections(QSqlDatabase* db);
+
+protected slots:
+    void probeInputs(const QString& videoDevice, QString cardID);
+
+private:
+    CardID* cardid;
+    const CardInput& parent;
+};
+
+class SourceID: public ComboBoxSetting, public CISetting {
+public:
+    SourceID(const CardInput& parent):
+        CISetting(parent, "sourceid") {
+        setLabel("Video source");
+    };
+
+    void fillSelections(QSqlDatabase* db) {
+        VideoSource::fillSelections(db, this);
+    };
+};
+
+class InputName: public ComboBoxSetting, public CISetting {
+public:
+    InputName(const CardInput& parent):
+        CISetting(parent, "inputname") {
+    };
+};
+
+class CardInput: public VerticalConfigurationGroup {
+public:
+    CardInput() {
+        setLabel("Capture card input");
+        addChild(id = new ID());
+        addChild(selector = new VideoInputSelector(*this));
+        addChild(sourceid = new SourceID(*this));
+    };
+
+    void fillSelections(QSqlDatabase* db);
+
+    int getInputID(void) const { return id->intValue(); };
+
+private:
+    class ID: virtual public IntegerSetting,
+              public AutoIncrementStorage {
     public:
-        AudioDevice(const CaptureCard& parent):
-            PathSetting(true),
-            CCSetting(parent, "AudioDevice") {
-            addSelection("/dev/dsp");
-            addSelection("/dev/dsp1");
-            addSelection("/dev/sound/dsp");
+        ID():
+            AutoIncrementStorage("cardinput", "cardid") {
+            setVisible(false);
+            setName("ID");
+        };
+        virtual QWidget* configWidget(QWidget* parent, const char* widgetName = 0) {
+            (void)parent; (void)widgetName;
+            return NULL;
         };
     };
 
 private:
     ID* id;
+    VideoInputSelector* selector;
+    SourceID* sourceid;
+    QSqlDatabase* db;
+};
+
+class VideoSourceDialog: public ConfigurationWizard {
 };
 
 #endif
