71ab427c51e359b5046437f62c3b762d64937a1e
Thor Sigvaldason <tsigvaldason@mythtv.org>
2003-02-16 01:34:47 +0000

    Add settings values for LCD host and port number Eventually these will be configurable from setup
    git-svn-id: http://svn.mythtv.org/svn/trunk@967 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/database/cvs.sql
+++ mythtv/database/cvs.sql
@@ -4,4 +4,5 @@ ALTER TABLE recorded ADD COLUMN bookmark VARCHAR(128) NULL;
 ALTER TABLE recorded ADD COLUMN editing INT UNSIGNED NOT NULL DEFAULT 0;
 ALTER TABLE recorded ADD COLUMN cutlist TEXT NULL;
 ALTER TABLE capturecard ADD COLUMN vbidevice VARCHAR(255);
-
+REPLACE INTO settings (value, data) VALUES ("LCDHost","localhost");
+REPLACE INTO settings (value, data) VALUES ("LCDPort","13666");
