f255370c94b26e61514a60b58734642c4c458b1d
John Danner <jdanner@mythtv.org>
2003-04-29 18:23:03 +0000

    Updated library version.
    git-svn-id: http://svn.mythtv.org/svn/trunk@1398 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/mythcontext.h
+++ mythtv/libs/libmyth/mythcontext.h
@@ -44,7 +44,7 @@ class MythEvent : public QCustomEvent
     QString extradata;
 };
 
-#define MYTH_BINARY_VERSION "0.9.04292003-1"
+#define MYTH_BINARY_VERSION "0.9.04292003-2"
 
 class MythContext : public QObject
 {
