ab627302a90482b47d141c56bb2a4dd315df2130
Isaac Richards <irichards@mythtv.org>
2003-06-30 23:58:22 +0000

    Stefan Frank's mythmusic i18n patch.
    git-svn-id: http://svn.mythtv.org/svn/trunk@1781 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ mythplugins/mythmusic/i18n/mythmusic_de.ts
@@ -0,0 +1,196 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>DatabaseBox</name>
+    <message>
+        <source>Select music to be played:</source>
+        <translation type="unfinished">Bitte Musik auswählen:</translation>
+    </message>
+    <message>
+        <source>Active Play Queue</source>
+        <translation type="unfinished">Aktuelle Spielliste</translation>
+    </message>
+    <message>
+        <source>All My Playlists</source>
+        <translation type="unfinished">Alle meine Spiellisten</translation>
+    </message>
+    <message>
+        <source>All My Music</source>
+        <translation type="unfinished">Meine Musik</translation>
+    </message>
+    <message>
+        <source>Move to Active Play Queue</source>
+        <translation type="unfinished">In aktuelle Spielliste verschieben</translation>
+    </message>
+    <message>
+        <source>Delete This Playlist</source>
+        <translation type="unfinished">Diese Spielliste löschen</translation>
+    </message>
+    <message>
+        <source>Rename This Playlist</source>
+        <translation type="unfinished">Diese Spielliste umbenennen</translation>
+    </message>
+    <message>
+        <source>Copy To New Playlist</source>
+        <translation type="unfinished">In neue Abspielliste kopieren</translation>
+    </message>
+    <message>
+        <source>Clear the Active Play Queue</source>
+        <translation type="unfinished">Lösche die aktuelle Warteliste</translation>
+    </message>
+    <message>
+        <source>Save Back to Playlist Tree</source>
+        <translation type="unfinished">Im Spiellisten-Baum speichern</translation>
+    </message>
+</context>
+<context>
+    <name>PlaybackBox</name>
+    <message>
+        <source>Edit Playlist</source>
+        <translation type="unfinished">Spielliste ändern</translation>
+    </message>
+    <message>
+        <source>Visualize</source>
+        <translation type="unfinished">Musik sehen</translation>
+    </message>
+    <message>
+        <source>3 Edit Playlist</source>
+        <translation type="unfinished">3 Liste ändern</translation>
+    </message>
+    <message>
+        <source>4 Visualize</source>
+        <translation type="unfinished">4 Musik sehen</translation>
+    </message>
+    <message>
+        <source>1 Shuffle: Smart</source>
+        <translation type="unfinished">1 Mix: Smart</translation>
+    </message>
+    <message>
+        <source>Shuffle: Smart</source>
+        <translation type="unfinished">&gt;Mix: Smart</translation>
+    </message>
+    <message>
+        <source>1 Shuffle: Rand</source>
+        <translation type="unfinished">1 Mix: Zufall</translation>
+    </message>
+    <message>
+        <source>Shuffle: Rand</source>
+        <translation type="unfinished">Mix: Zufall</translation>
+    </message>
+    <message>
+        <source>1 Shuffle: None</source>
+        <translation type="unfinished">1 Mix: Nein</translation>
+    </message>
+    <message>
+        <source>Shuffle: None</source>
+        <translation type="unfinished">Mix: Nein</translation>
+    </message>
+    <message>
+        <source>2 Repeat: All</source>
+        <translation type="unfinished">2 Wdh: Alle</translation>
+    </message>
+    <message>
+        <source>Repeat: All</source>
+        <translation type="unfinished">Wdh: Alle</translation>
+    </message>
+    <message>
+        <source>2 Repeat: Track</source>
+        <translation type="unfinished">2 Wdh: Lied</translation>
+    </message>
+    <message>
+        <source>Repeat: Track</source>
+        <translation type="unfinished">Wdh: Lied</translation>
+    </message>
+    <message>
+        <source>2 Repeat: None</source>
+        <translation type="unfinished">2 Wdh: Nein</translation>
+    </message>
+    <message>
+        <source>Repeat: None</source>
+        <translation type="unfinished">Wdh: Nein</translation>
+    </message>
+    <message>
+        <source>Playing stream.</source>
+        <translation type="unfinished">Spiele Stream.</translation>
+    </message>
+    <message>
+        <source>Buffering stream.</source>
+        <translation type="unfinished">Puffere Stream.</translation>
+    </message>
+    <message>
+        <source>Stream paused.</source>
+        <translation type="unfinished">Stream angehalten.</translation>
+    </message>
+    <message>
+        <source>Output error.</source>
+        <translation type="unfinished">&gt;Ausgabefehler.</translation>
+    </message>
+    <message>
+        <source>Stream stopped.</source>
+        <translation type="unfinished">Stream gestoppt.</translation>
+    </message>
+    <message>
+        <source>Finished playing stream.</source>
+        <translation type="unfinished">Stream beendet.</translation>
+    </message>
+    <message>
+        <source>Decoder error.</source>
+        <translation type="unfinished">Dekodierfehler.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Searching for music files</source>
+        <translation type="unfinished">Suche nach Musikdateien</translation>
+    </message>
+    <message>
+        <source>Updating music database</source>
+        <translation type="unfinished">Aktualisiere Musikdatenbank</translation>
+    </message>
+</context>
+<context>
+    <name>Ripper</name>
+    <message>
+        <source>Please select a quality level and check the album information below:</source>
+        <translation type="unfinished">Bitte wählen Sie eine Qualitätsstufe und prüfen Sie die Albumdaten:</translation>
+    </message>
+    <message>
+        <source>Low</source>
+        <translation type="unfinished">Niedrig</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation type="unfinished">Mittel</translation>
+    </message>
+    <message>
+        <source>High</source>
+        <translation type="unfinished">Hoch</translation>
+    </message>
+    <message>
+        <source>Perfect</source>
+        <translation type="unfinished">Perfekt</translation>
+    </message>
+    <message>
+        <source>Artist: </source>
+        <translation type="unfinished">Interpret:</translation>
+    </message>
+    <message>
+        <source>Album: </source>
+        <translation type="unfinished">Album:</translation>
+    </message>
+    <message>
+        <source>Import this CD</source>
+        <translation type="unfinished">CD importieren</translation>
+    </message>
+    <message>
+        <source>Importing CD:
+</source>
+        <translation type="unfinished">Importiere CD:</translation>
+    </message>
+    <message>
+        <source>Copying from CD:
+</source>
+        <translation type="unfinished">Kopiere von CD:</translation>
+    </message>
+</context>
+</TS>
--- mythplugins/mythmusic/i18n/translate.pro
+++ mythplugins/mythmusic/i18n/translate.pro
@@ -4,4 +4,4 @@ SOURCES += ../mythmusic/cdrip.cpp
 SOURCES += ../mythmusic/main.cpp
 
 TRANSLATIONS  = mythmusic_it.ts mythmusic_es.ts mythmusic_ca.ts
-TRANSLATIONS += mythmusic_nl.ts
+TRANSLATIONS += mythmusic_nl.ts mythmusic_de.ts
