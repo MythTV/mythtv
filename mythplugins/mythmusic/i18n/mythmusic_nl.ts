ebe3343e5f2d59f858e8336620af963b70d9760b
Isaac Richards <irichards@mythtv.org>
2003-07-12 02:14:39 +0000

    Dutch translation update.
    git-svn-id: http://svn.mythtv.org/svn/trunk@1824 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythmusic/i18n/mythmusic_nl.ts
+++ mythplugins/mythmusic/i18n/mythmusic_nl.ts
@@ -3,194 +3,196 @@
     <name>DatabaseBox</name>
     <message>
         <source>Select music to be played:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies muziek om af te spelen:</translation>
     </message>
     <message>
         <source>Copy To New Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieer naar Nieuwe Speellijst</translation>
     </message>
     <message>
         <source>Clear the Active Play Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Ledig Actieve Speellijst</translation>
     </message>
     <message>
         <source>Save Back to Playlist Tree</source>
-        <translation type="unfinished"></translation>
+        <translation>Sla op in Speellijst</translation>
     </message>
     <message>
         <source>Move to Active Play Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Verplaats naar Actieve Speellijst</translation>
     </message>
     <message>
         <source>Delete This Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Verwijder deze Speellijst</translation>
     </message>
     <message>
         <source>Rename This Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Hernoem deze Speellijst</translation>
     </message>
     <message>
         <source>Active Play Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Actieve Speellijst</translation>
     </message>
     <message>
         <source>All My Playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle Speellijsten</translation>
     </message>
     <message>
         <source>All My Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle muziek</translation>
     </message>
 </context>
 <context>
     <name>PlaybackBox</name>
     <message>
         <source>1 Shuffle: None</source>
-        <translation type="unfinished"></translation>
+        <translation>1 Mengen: Geen</translation>
     </message>
     <message>
         <source>Shuffle: None</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengen: Geen</translation>
     </message>
     <message>
         <source>2 Repeat: All</source>
-        <translation type="unfinished"></translation>
+        <translation>2 Herhalen: Alles</translation>
     </message>
     <message>
         <source>Repeat: All</source>
-        <translation type="unfinished"></translation>
+        <translation>Herhalen: Alles</translation>
     </message>
     <message>
         <source>2 Repeat: Track</source>
-        <translation type="unfinished"></translation>
+        <translation>2 Herhalen: Nummer</translation>
     </message>
     <message>
         <source>Repeat: Track</source>
-        <translation type="unfinished"></translation>
+        <translation>Herhalen: Nummer</translation>
     </message>
     <message>
         <source>2 Repeat: None</source>
-        <translation type="unfinished"></translation>
+        <translation>2 Herhalen: Geen</translation>
     </message>
     <message>
         <source>Repeat: None</source>
-        <translation type="unfinished"></translation>
+        <translation>Herhalen: Geen</translation>
     </message>
     <message>
         <source>Playing stream.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig afspelen stream.</translation>
     </message>
     <message>
         <source>Buffering stream.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig bufferen stream.</translation>
     </message>
     <message>
         <source>Stream paused.</source>
-        <translation type="unfinished"></translation>
+        <translation>Stream gepauseerd.</translation>
     </message>
     <message>
         <source>Output error.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uitvoer fout.</translation>
     </message>
     <message>
         <source>Stream stopped.</source>
-        <translation type="unfinished"></translation>
+        <translation>Stream gestopt.</translation>
     </message>
     <message>
         <source>Finished playing stream.</source>
-        <translation type="unfinished"></translation>
+        <translation>Stream afgespeeld.</translation>
     </message>
     <message>
         <source>Decoder error.</source>
-        <translation type="unfinished"></translation>
+        <translation>Decodeer fout.</translation>
     </message>
     <message>
         <source>Edit Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Wijzig Speellijst</translation>
     </message>
     <message>
         <source>Visualize</source>
-        <translation type="unfinished"></translation>
+        <translation>Weergave</translation>
     </message>
     <message>
         <source>3 Edit Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>3 Wijzig Speellijst</translation>
     </message>
     <message>
         <source>4 Visualize</source>
-        <translation type="unfinished"></translation>
+        <translation>4 Weergave</translation>
     </message>
     <message>
         <source>1 Shuffle: Smart</source>
-        <translation type="unfinished"></translation>
+        <translation>1 Mengen: Slim</translation>
     </message>
     <message>
         <source>Shuffle: Smart</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengen: Slim</translation>
     </message>
     <message>
         <source>1 Shuffle: Rand</source>
-        <translation type="unfinished"></translation>
+        <translation>1 Mengen: Random</translation>
     </message>
     <message>
         <source>Shuffle: Rand</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengen: Random</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Searching for music files</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig zoeken naar muziekbestanden</translation>
     </message>
     <message>
         <source>Updating music database</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig bijwerken databank</translation>
     </message>
 </context>
 <context>
     <name>Ripper</name>
     <message>
         <source>Please select a quality level and check the album information below:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies een kwaliteitsniveau en controleer de informatie hieronder:</translation>
     </message>
     <message>
         <source>Low</source>
-        <translation type="unfinished"></translation>
+        <translation>Laag</translation>
     </message>
     <message>
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Gemiddeld</translation>
     </message>
     <message>
         <source>High</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoog</translation>
     </message>
     <message>
         <source>Perfect</source>
-        <translation type="unfinished"></translation>
+        <translation>Perfect</translation>
     </message>
     <message>
         <source>Artist: </source>
-        <translation type="unfinished"></translation>
+        <translation>Artiest:</translation>
     </message>
     <message>
         <source>Album: </source>
-        <translation type="unfinished"></translation>
+        <translation>Album:</translation>
     </message>
     <message>
         <source>Import this CD</source>
-        <translation type="unfinished"></translation>
+        <translation>Importeer deze CD</translation>
     </message>
     <message>
         <source>Importing CD:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig importeren CD:
+</translation>
     </message>
     <message>
         <source>Copying from CD:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig copieren van CD:
+</translation>
     </message>
 </context>
 </TS>
