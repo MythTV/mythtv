e2d8e30cfbe6114403507bd89daf7aae3047ac89
Matt Zimmerman <mzimmerman@mythtv.org>
2003-09-12 03:27:19 +0000

    Give an example of a shorter sleep
    git-svn-id: http://svn.mythtv.org/svn/trunk@2230 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/contrib/change-channel-lirc.sh
+++ mythtv/contrib/change-channel-lirc.sh
@@ -5,4 +5,6 @@ REMOTE_NAME=gi-motorola-dct2000
 for digit in $(echo $1 | sed -e 's/./& /g'); do 
     irsend SEND_ONCE $REMOTE_NAME $digit
     sleep 1
+    # If things work OK with sleep 1, try this for faster channel changes:
+    # sleep 0.3
 done
