4761e23ba498d8c0b995cdcedb12103c643131cc
Matt Zimmerman <mzimmerman@mythtv.org>
2004-01-02 04:31:57 +0000

    Have the "Unable to create <lockfile>" error message (the one that nobody reads) actually show the error
    git-svn-id: http://svn.mythtv.org/svn/trunk@2805 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythbackend/main.cpp
+++ mythtv/programs/mythbackend/main.cpp
@@ -477,11 +477,11 @@ int main(int argc, char **argv)
        nfsfd = open(lockfile_location.ascii(), O_WRONLY|O_CREAT|O_APPEND, 0664);
        if (nfsfd < 0) 
        {
+           perror("open lockfile"); 
            cerr << "Unable to create \'" << lockfile_location << "\'!\n"
                 << "Be sure that \'" << gContext->GetSetting("RecordFilePrefix")
                 << "\' exists and that both \nthe directory and that "
                 << "file are writeble by this user.\n";
-          perror("open lockfile"); 
           return -1;
        }
        close(nfsfd);
