10371e4cf6f7ab33d787a7192ef675bc47b71b67
Thor Sigvaldason <tsigvaldason@mythtv.org>
2004-01-15 08:56:43 +0000

    Forgot the MythMusic plugin. Oh, and if it doesn't build go into mfdlib, make install, and then go back to a regular make. Need to fix that.
    git-svn-id: http://svn.mythtv.org/svn/trunk@2903 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ mfd/mfd/plugins/mmusic/main.cpp
@@ -0,0 +1,61 @@
+/*
+	main.cpp
+
+	(c) 2003 Thor Sigvaldason and Isaac Richards
+	Part of the mythTV project
+	
+    entry point for MythMusic Watcher
+
+*/
+
+#include "mmusic.h"
+#include "../../mfd.h"
+
+MMusicWatcher *mmusic_watcher = NULL;
+
+extern "C" {
+bool         mfdplugin_init(MFD*, int);
+bool         mfdplugin_run();
+bool         mfdplugin_stop();
+bool         mfdplugin_can_unload();
+}
+
+
+bool mfdplugin_init(MFD *owner, int identifier)
+{
+    mmusic_watcher = new MMusicWatcher(owner, identifier);
+    return true;
+}
+
+bool mfdplugin_run()
+{
+    if(mmusic_watcher)
+    {
+        mmusic_watcher->start();
+        return true;
+    }
+    return false;
+}
+
+bool mfdplugin_stop()
+{
+    if(mmusic_watcher)
+    {
+        mmusic_watcher->stop();
+    }
+    return true;
+}
+
+bool mfdplugin_can_unload()
+{
+    if(mmusic_watcher)
+    {
+        if(!mmusic_watcher->running())
+        {
+            delete mmusic_watcher;
+            mmusic_watcher = NULL;
+        }
+        return false;
+    }
+    return true;
+}
---
+++ mfd/mfd/plugins/mmusic/mmusic.h
@@ -0,0 +1,61 @@
+#ifndef MMUSIC_H_
+#define MMUSIC_H_
+/*
+	mmusic.h
+
+	(c) 2003 Thor Sigvaldason and Isaac Richards
+	Part of the mythTV project
+	
+	stay on top of mythmusic data
+
+*/
+
+#include <qdatetime.h>
+#include <qmutex.h>
+#include <qsqldatabase.h>
+
+#include "mfd_plugin.h"
+#include "../../mdserver.h"
+
+enum MusicFileLocation
+{
+    MFL_on_file_system = 0,
+    MFL_in_myth_database
+};
+    
+typedef QMap <QString, MusicFileLocation> MusicLoadedMap;
+
+
+
+class MMusicWatcher: public MFDServicePlugin
+{
+
+  public:
+
+    MMusicWatcher(MFD *owner, int identity);
+    ~MMusicWatcher();
+
+    void    run();
+    bool    sweepMetadata();
+    void    buildFileList(QString &directory, MusicLoadedMap &music_files);
+    bool    checkNewMusicFile(const QString &filename);
+
+  private:
+  
+    QTime           metadata_sweep_time;
+    bool            first_time;
+    bool            force_sweep;
+    QMutex          force_sweep_mutex;
+    QSqlDatabase    *db;
+
+    QMutex              metadata_mutex;
+    QIntDict<Metadata>  *current_metadata;
+    QIntDict<Metadata>  *new_metadata;
+    QIntDict<Playlist>  *current_playlists;
+    MetadataServer      *metadata_server;
+
+};
+
+
+
+#endif  // mmusic_h_
