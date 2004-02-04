0ac5e6af3d9b3223189074f628f2c1d34f0cfcc6
Isaac Richards <irichards@mythtv.org>
2004-02-04 01:23:37 +0000

    More Dutch translation updates.
    git-svn-id: http://svn.mythtv.org/svn/trunk@3088 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythdvd/i18n/mythdvd_nl.ts
+++ mythplugins/mythdvd/i18n/mythdvd_nl.ts
@@ -3,51 +3,51 @@
     <name>DVDRipBox</name>
     <message>
         <source>Your connection to the Myth Transcoding Daemon has gone away. This is not a good thing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Probleem: Uw verbinding met het Transcode Programma is weg. </translation>
     </message>
     <message>
         <source>Cannot connect to your Myth Transcoding Daemon. You can try hitting any number key to start it. If you still see this message, then something is really wrong.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan geen verbinding krijgen met het Transcode Programma. Probeer het op te starten door op een nr. toets te drukken. Als dit bericht blijft staan is er iets ernstig fout.</translation>
     </message>
     <message>
         <source>Attempting to connect to your mtd said host not found. This is unrecoverably bad. </source>
-        <translation type="unfinished"></translation>
+        <translation>Kan geen verbinding maken met het Transcode Programma</translation>
     </message>
     <message>
         <source>Socket communication errors. This is unrecoverably bad. </source>
-        <translation type="unfinished"></translation>
+        <translation>Netwerk error.</translation>
     </message>
     <message>
         <source>Something is wrong, but I don&apos;t know what.</source>
-        <translation type="unfinished"></translation>
+        <translation>Onbekende fout.</translation>
     </message>
     <message>
         <source>Job %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Taak %1 van %2</translation>
     </message>
     <message>
         <source>No jobs and nothing else to do. You could hit 0 to rip a DVD.</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen taken. Druk 0 om een DVD te &quot;Rippen&quot;.</translation>
     </message>
     <message>
         <source>No Jobs. Checking and/or waiting for DVD.</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen taken. Bezig met controleren/wachten op DVD.</translation>
     </message>
     <message>
         <source>No jobs and nothing else to do. You could hit 0 to rip a disc if you like.</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen taken. Druk 0 om een CD te &quot;Rippen&quot;.</translation>
     </message>
     <message>
         <source>Cancelling ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig met anuleren...</translation>
     </message>
     <message>
         <source>0 New Rip</source>
-        <translation type="unfinished"></translation>
+        <translation>0 Nieuwe Rip</translation>
     </message>
     <message>
         <source>9 Cancel Job</source>
-        <translation type="unfinished"></translation>
+        <translation>9 Anuleer taak</translation>
     </message>
 </context>
 <context>
@@ -56,81 +56,92 @@
         <source>
 
 You have no VCD Player command defined.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+        
+Geen VCD afpeel commando gedefinieerd.
+</translation>
     </message>
     <message>
         <source>OK, I&apos;ll go run Setup</source>
-        <translation type="unfinished"></translation>
+        <translation>Ik ga naar het Instelling scherm.</translation>
     </message>
     <message>
         <source>
 
 You have no VCD Device defined.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+Er is geen VCD lezer ingesteld.</translation>
     </message>
     <message>
         <source>
 
 You have no DVD Player command defined.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+Geen DVD afpeel commando gedefinieerd.
+</translation>
     </message>
     <message>
         <source>
 
 You have no DVD Device defined.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+Er is geen VCD lezer ingesteld.
+</translation>
     </message>
     <message>
         <source>Location of VCD device</source>
-        <translation type="unfinished"></translation>
+        <translation>Path naar VCD lezer</translation>
     </message>
     <message>
         <source>This device must exist, and the user running MythDVD needs to have read permission on the device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze lezer moet bestaan en de juister permissies moeten ingesteld worden.</translation>
     </message>
     <message>
         <source>Location of DVD device</source>
-        <translation type="unfinished"></translation>
+        <translation>Path naar DVD lezer</translation>
     </message>
     <message>
         <source>General Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Algemene Instellingen</translation>
     </message>
     <message>
         <source>DVD Player Command</source>
-        <translation type="unfinished"></translation>
+        <translation>DVD afspeel commando</translation>
     </message>
     <message>
         <source>This can be any command to launch a DVD player (e.g. MPlayer, ogle, etc.). If present, %d will be substituted for the DVD device (e.g. /dev/dvd).</source>
-        <translation type="unfinished"></translation>
+        <translation>Dit kan elke DVD speler zijn (bvb. MPlayer, ogle, xine). %d wordt vervangen door het hierboven ingestelde DVD lezer.</translation>
     </message>
     <message>
         <source>VCD Player Command</source>
-        <translation type="unfinished"></translation>
+        <translation>VCD Afspeel commando</translation>
     </message>
     <message>
         <source>This can be any command to launch a VCD player (e.g. MPlayer, xine, etc.). If present, %d will be substituted for the VCD device (e.g. /dev/cdrom).</source>
-        <translation type="unfinished"></translation>
+        <translation>Dit kan elke VCD speler zijn (bvb. MPlayer, xine). %d wordt vervangen door het hierboven ingestelde VCD lezer.</translation>
     </message>
     <message>
         <source>DVD Player Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>DVD afspeel Instellingen</translation>
     </message>
     <message>
         <source>VCD Player Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>VCD afspeel Instellingen</translation>
     </message>
     <message>
         <source>Directory to hold temporary files</source>
-        <translation type="unfinished"></translation>
+        <translation>Map voor tijdelijke bestanden.</translation>
     </message>
     <message>
         <source>This directory must exist, and the user running MythDVD needs to have write permission to the directory.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze map moet bestaan en de juister permissies moeten ingesteld worden.</translation>
     </message>
     <message>
         <source>Title Playing Command</source>
-        <translation type="unfinished"></translation>
+        <translation>Afspeelcommando</translation>
     </message>
     <message>
         <source>This is a command used to preview a given title on a DVD. If present %t will be set to the title, %d for device, %a for audio track, %c for audio channels.</source>
@@ -210,47 +221,47 @@ You have no DVD Device defined.</source>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Onbekend</translation>
     </message>
     <message>
         <source>Waiting For Access to DVD</source>
-        <translation type="unfinished"></translation>
+        <translation>Wacht op DVD</translation>
     </message>
     <message>
         <source>Ripping to file ~</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig met copieren ~</translation>
     </message>
     <message>
         <source>Perfect DVD Rip of %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Perfecte DVD Rip van %1</translation>
     </message>
     <message>
         <source>Transcode of %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Transcode van %1</translation>
     </message>
     <message>
         <source>Waiting for Permission to Start Transcoding</source>
-        <translation type="unfinished"></translation>
+        <translation>Wacht op toelating op te beginnen met Transcode</translation>
     </message>
     <message>
         <source>Transcoding Pass 1 of 2 ~</source>
-        <translation type="unfinished"></translation>
+        <translation>Transcode Pass 1 van 2</translation>
     </message>
     <message>
         <source>Transcoding Pass 2 of 2 ~</source>
-        <translation type="unfinished"></translation>
+        <translation>Transcode Pass 2 van 2</translation>
     </message>
     <message>
         <source>Transcoding ~</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezig met Transcode ~</translation>
     </message>
     <message>
         <source>Transcode is thinking </source>
-        <translation type="unfinished"></translation>
+        <translation>Transcode denkt na </translation>
     </message>
     <message>
         <source>Simultaneous Transcode Jobs</source>
-        <translation type="unfinished"></translation>
+        <translation>Gelijktijdige Transcode Taken</translation>
     </message>
     <message>
         <source>This determines the number of simultaneous transcode jobs. If set at 1 (the default), there will only be one active job at a time.</source>
@@ -265,39 +276,39 @@ You have no DVD Device defined.</source>
     </message>
     <message>
         <source>Job 1 of 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Taak 1 van 1</translation>
     </message>
     <message>
         <source>Length:</source>
-        <translation type="unfinished"></translation>
+        <translation>Lengte:</translation>
     </message>
     <message>
         <source>Select:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies:</translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Naam:</translation>
     </message>
     <message>
         <source>Quality:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kwaliteit:</translation>
     </message>
     <message>
         <source>Audio Track:</source>
-        <translation type="unfinished"></translation>
+        <translation>Geluidsspoor:</translation>
     </message>
     <message>
         <source>AC3 Audio:</source>
-        <translation type="unfinished"></translation>
+        <translation>AC3 Geluid:</translation>
     </message>
     <message>
         <source>Subtitles:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ondertitels:</translation>
     </message>
     <message>
         <source>View:</source>
-        <translation type="unfinished"></translation>
+        <translation>Preview:</translation>
     </message>
     <message>
         <source>0:00:00</source>
@@ -305,18 +316,18 @@ You have no DVD Device defined.</source>
     </message>
     <message>
         <source>Title 1 of 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel 1 van 1</translation>
     </message>
 </context>
 <context>
     <name>TitleDialog</name>
     <message>
         <source>Unkown</source>
-        <translation type="unfinished"></translation>
+        <translation>Onbekend</translation>
     </message>
     <message>
         <source>%1 - Title %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 - Titel %2</translation>
     </message>
     <message>
         <source>Perfect</source>
@@ -324,19 +335,19 @@ You have no DVD Device defined.</source>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen</translation>
     </message>
     <message>
         <source>Title %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel %1 van %2</translation>
     </message>
     <message>
         <source>0 Process Selected Title</source>
-        <translation type="unfinished"></translation>
+        <translation>0 Verwerk de geselecteerde Titel</translation>
     </message>
     <message>
         <source>0 Process Selected Titles</source>
-        <translation type="unfinished"></translation>
+        <translation>0 Verwerk de geselecteerde Titels</translation>
     </message>
 </context>
 </TS>
