c98fbe16d18eb7f2d1f96ded657511a7ef3cccf1
Matt Zimmerman <mzimmerman@mythtv.org>
2004-03-12 23:22:29 +0000

    Allow the same script to be used for changing channels and sending lirc commands
    git-svn-id: http://svn.mythtv.org/svn/trunk@3399 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/contrib/change-channel-lirc.sh
+++ mythtv/contrib/change-channel-lirc.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 REMOTE_NAME=gi-motorola-dct2000
+cmd="$1"
 
-for digit in $(echo $1 | sed -e 's/./& /g'); do 
-    irsend SEND_ONCE $REMOTE_NAME $digit
-    sleep 1
-    # If things work OK with sleep 1, try this for faster channel changes:
-    # sleep 0.3
-done
+case $cmd in
+    [0-9]*)
+    for digit in $(echo $1 | sed -e 's/./& /g'); do 
+        irsend SEND_ONCE $REMOTE_NAME $digit
+        sleep 1
+        # If things work OK with sleep 1, try this for faster channel changes:
+        # sleep 0.3
+    done
+    ;;
+
+    *)
+        irsend SEND_ONCE $REMOTE_NAME $cmd
+        ;;
+esac
