cb75f0b13f5c1bf83acfd1218f723b2721b0570b
Thor Sigvaldason <tsigvaldason@mythtv.org>
2004-05-11 00:57:22 +0000

    File I forgot to add before last commit.
    git-svn-id: http://svn.mythtv.org/svn/trunk@3632 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ mfd/mfd/plugins/daapclient/server_types.h
@@ -0,0 +1,20 @@
+#ifndef SERVER_TYPES_H_
+#define SERVER_TYPES_H_
+/*
+	server_types.h
+
+	(c) 2003, 2004 Thor Sigvaldason and Isaac Richards
+	Part of the mythTV project
+	
+	enum for server types
+
+*/
+
+enum DaapServerType {
+    DAAP_SERVER_UNKNOWN = 0,
+    DAAP_SERVER_ITUNES4X,
+    DAAP_SERVER_ITUNES45,
+    DAAP_SERVER_MYTH
+};
+    
+#endif
