f467678093e697e5a9a176269594c20af8842c6f
Chris Petersen <cpetersen@mythtv.org>
2004-10-04 00:39:10 +0000

    add a makefile, deprecate install.sh
    git-svn-id: http://svn.mythtv.org/svn/trunk@4495 7dbf422c-18fa-0310-86e9-fd20926502f2

--- nuvexport/install.sh
+++ nuvexport/install.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# First, install nuvexport itself
-    install -Dv -o root -g root -m 0755 nuvexport /usr/local/bin/nuvexport
-    install -Dv -o root -g root -m 0755 nuvinfo   /usr/local/bin/nuvinfo
-    install -Dv -o root -g root -m 0755 mpeg2cut  /usr/local/bin/mpeg2cut
+echo "Use \"make install\" instead"
+exit 0
 
-# Next, create the nuvexport shared directory
-    mkdir -pvm 0755 /usr/local/share/nuvexport
-
-# Finally, install the other files
-    for dir in 'export' 'mythtv' 'nuv_export'; do
-        mkdir -pvm 0755 /usr/local/share/nuvexport/"$dir"
-        install -v -o root -g root -m 0755 "$dir"/*pm /usr/local/share/nuvexport/"$dir"
-    done
