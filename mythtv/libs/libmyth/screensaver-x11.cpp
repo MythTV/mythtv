695d27a3853dc6301a8299aaa104a7bb059b8b13
Isaac Richards <irichards@mythtv.org>
2004-12-24 23:41:56 +0000

    Andrew Dennison's 'screensaver DPMS support' patch.
    git-svn-id: http://svn.mythtv.org/svn/trunk@4986 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/screensaver-x11.cpp
+++ mythtv/libs/libmyth/screensaver-x11.cpp
@@ -1,6 +1,13 @@
+#define QT_CLEAN_NAMESPACE // no qt 1.x compatability, INT32 conflicts with X
 #include "screensaver-x11.h"
 
 #include <X11/Xlib.h>
+
+extern "C" {
+#include <X11/extensions/dpms.h>
+}
+
+#include "mythcontext.h"
 
 class ScreenSaverX11Private 
 {
@@ -11,6 +18,7 @@ class ScreenSaverX11Private
         int interval;
         int preferblank;
         int allowexposure;
+        bool dpmsdisabled;
     } state;
 
     friend class ScreenSaverX11;
@@ -39,6 +47,22 @@ void ScreenSaverX11::Disable(void)
 
     XResetScreenSaver(qt_xdisplay());
     XSetScreenSaver(qt_xdisplay(), 0, 0, 0, 0);
+
+    int nothing;
+
+    if (DPMSQueryExtension(qt_xdisplay(), &nothing, &nothing))
+    {
+        BOOL on;
+        CARD16 power_level;
+
+        DPMSInfo(qt_xdisplay(), &power_level, &on);
+        if (on)
+        {
+            d->state.dpmsdisabled = true;
+            DPMSDisable(qt_xdisplay());       // monitor powersave off
+            VERBOSE(VB_GENERAL, "Disable DPMS");
+        }
+    }
 }
 
 void ScreenSaverX11::Restore(void) 
@@ -49,6 +73,17 @@ void ScreenSaverX11::Restore(void)
                     d->state.preferblank, 
                     d->state.allowexposure);
     d->state.saved = false;
+
+    if (d->state.dpmsdisabled)
+    {
+        int nothing;
+        d->state.dpmsdisabled = false;
+        if (DPMSQueryExtension(qt_xdisplay(), &nothing, &nothing))
+        {
+            DPMSEnable(qt_xdisplay());
+            VERBOSE(VB_GENERAL, "Enable DPMS");
+        }
+    }
 }
 
 void ScreenSaverX11::Reset(void) 
