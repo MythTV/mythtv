40d1b83f3ab6942b1ab37bec49384a04b1d77ef8
J. Donovan Stanley <jstanley@mythtv.org>
2005-02-01 02:33:41 +0000

    Patch from Andy Poling to clear up the delay in creatign preview PNGs caused by my ringbuffer bad file handle work around
    git-svn-id: http://svn.mythtv.org/svn/trunk@5281 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/RingBuffer.cpp
+++ mythtv/libs/libmythtv/RingBuffer.cpp
@@ -354,6 +354,9 @@ RingBuffer::RingBuffer(const QString &lfilename, bool write, bool usereadahead)
     normalfile = true;
     filename = (QString)lfilename;
 
+    if (filename.right(4) != ".nuv")
+        openAttempts = 1;
+
     if (write)
     {
         tfw = new ThreadedFileWriter(filename.ascii(),
