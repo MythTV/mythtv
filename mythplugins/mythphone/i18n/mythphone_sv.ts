a436f98dd5e77e35c98c6b073e51c531e5840b10
Oscar Carlsson <ocarlsson@mythtv.org>
2005-02-07 00:32:35 +0000

    Swedish translation from Daniel
    git-svn-id: http://svn.mythtv.org/svn/trunk@5443 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythphone/i18n/mythphone_sv.ts
+++ mythplugins/mythphone/i18n/mythphone_sv.ts
@@ -3,71 +3,71 @@
     <name>PhoneUIBox</name>
     <message>
         <source>Add someone to your Directory </source>
-        <translation type="unfinished"></translation>
+        <translation>Lägg till någon till din katalog</translation>
     </message>
     <message>
         <source>Edit this Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Redigera denna post</translation>
     </message>
     <message>
         <source>Remove from Speed Dials</source>
-        <translation type="unfinished"></translation>
+        <translation>Ta bort från kortnummer</translation>
     </message>
     <message>
         <source>Save this in the Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Spara detta i katalogen</translation>
     </message>
     <message>
         <source>Clear the Call History</source>
-        <translation type="unfinished"></translation>
+        <translation>Rensa samtalshistoriken</translation>
     </message>
     <message>
         <source>Make this a Speeddial</source>
-        <translation type="unfinished"></translation>
+        <translation>Gör detta till ett kortnummer</translation>
     </message>
     <message>
         <source>Delete this Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Radera denna post</translation>
     </message>
     <message>
         <source>Delete this Voicemail</source>
-        <translation type="unfinished"></translation>
+        <translation>Radera detta röstmeddelande</translation>
     </message>
     <message>
         <source>Delete all Voicemails</source>
-        <translation type="unfinished"></translation>
+        <translation>Radera alla röstmeddelanden</translation>
     </message>
     <message>
         <source>Place Videocall Now</source>
-        <translation type="unfinished"></translation>
+        <translation>Starta videosamtal nu</translation>
     </message>
     <message>
         <source>Place Voice-Only Call Now</source>
-        <translation type="unfinished"></translation>
+        <translation>Starta röstsamtal nu</translation>
     </message>
     <message>
         <source>Switch from digits to URL input</source>
-        <translation type="unfinished"></translation>
+        <translation>Växla från siffror till URL-inmatning</translation>
     </message>
     <message>
         <source>Switch from URL to Digits input</source>
-        <translation type="unfinished"></translation>
+        <translation>Växla från URL till sifferinmatning</translation>
     </message>
     <message>
         <source>Send IM</source>
-        <translation type="unfinished"></translation>
+        <translation>Skicka IM</translation>
     </message>
     <message>
         <source>Save Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Spara ändringar</translation>
     </message>
     <message>
         <source>ADD</source>
-        <translation type="unfinished"></translation>
+        <translation>Lägg till</translation>
     </message>
     <message>
         <source>ADD DIRECTORY</source>
-        <translation type="unfinished"></translation>
+        <translation>Lägg till katalog</translation>
     </message>
 </context>
 <context>
@@ -76,255 +76,257 @@
         <source>
 
 A directory entry already exists with this URL.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+En katalogpost finns redan med denna URL.</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <source>Webcam device</source>
-        <translation type="unfinished"></translation>
+        <translation>Webbkameraenhet</translation>
     </message>
     <message>
         <source>Select the device path your webcam is using. If you have a capture card it may be using /dev/video so try dev/video1</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj din webbkameras enhetssökväg, t.ex. /dev/video1</translation>
     </message>
     <message>
         <source>Transmit Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Sändningsupplösning</translation>
     </message>
     <message>
         <source>176x144</source>
-        <translation type="unfinished"></translation>
+        <translation>176x144</translation>
     </message>
     <message>
         <source>128x96</source>
-        <translation type="unfinished"></translation>
+        <translation>128x96</translation>
     </message>
     <message>
         <source>704x576</source>
-        <translation type="unfinished"></translation>
+        <translation>704x576</translation>
     </message>
     <message>
         <source>352x288</source>
-        <translation type="unfinished"></translation>
+        <translation>352x288</translation>
     </message>
     <message>
         <source>Size of video window to transmit; higher resolutions require more bandwidth.</source>
-        <translation type="unfinished"></translation>
+        <translation>Storlek för videofönster att sända; högre upplösning kräver mer bandbredd.</translation>
     </message>
     <message>
         <source>Capture Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Upptagningsupplösning</translation>
     </message>
     <message>
         <source>320x240</source>
-        <translation type="unfinished"></translation>
+        <translation>320x240</translation>
     </message>
     <message>
         <source>160x120</source>
-        <translation type="unfinished"></translation>
+        <translation>160x120</translation>
     </message>
     <message>
         <source>640x480</source>
-        <translation type="unfinished"></translation>
+        <translation>640x480</translation>
     </message>
     <message>
         <source>Size of video source from your webcam. Choose a value compatible with your webcam hardware. Choose higher values to digitally pan/zoom before transmission.</source>
-        <translation type="unfinished"></translation>
+        <translation>Storlek för videokällan för din webbkamera. Välj ett värde din webbkamerahårdvara stödjer eller högre för att zooma digitalt före överföring.</translation>
     </message>
     <message>
         <source>Transmit Frames/Second</source>
-        <translation type="unfinished"></translation>
+        <translation>Sänd bilder/sek</translation>
     </message>
     <message>
         <source>Time to Answer</source>
-        <translation type="unfinished"></translation>
+        <translation>Tid till svar</translation>
     </message>
     <message>
         <source>10</source>
-        <translation type="unfinished"></translation>
+        <translation>10</translation>
     </message>
     <message>
         <source>The time in seconds a call rings before being automatically answered and diverted to a VXML script.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tid i sekunder ett samtal ringer innan det besvaras automatiskt och dirigeras om till ett VXML-skript.</translation>
     </message>
     <message>
         <source>Default VXML URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Förvald VXML-URL</translation>
     </message>
     <message>
         <source>http://127.0.0.1/vxml/index.vxml</source>
-        <translation type="unfinished"></translation>
+        <translation>http://127.0.0.1/vxml/index.vxml</translation>
     </message>
     <message>
         <source>The URL to retrieve a VXML script which can be used to prompt for leaving a voicemail etc. Leave blank if you have no HTTP server and a default Voicemail script will be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>URL där ett VXML-skript hämtas och används som prompt för röstbrevlåda etc. Lämna tomt om du inte har en HTTP-server så används ett standard-röstbrevlådeskript.</translation>
     </message>
     <message>
         <source>Default Voicemail Prompt</source>
-        <translation type="unfinished"></translation>
+        <translation>Förvald röstbrevlåde-prompt</translation>
     </message>
     <message>
         <source>I am not at home, please leave a message after the tone</source>
-        <translation type="unfinished"></translation>
+        <translation>Jag är inte hemma, lämna ett meddelande efter tonen tack</translation>
     </message>
     <message>
         <source>Either a text message to be read by the TTS engine or the filename of a .wav file to be played to callers. Only used where the above setting is blank.</source>
-        <translation type="unfinished"></translation>
+        <translation>Antingen ett textmeddelande som TTS-motorn ska läsa upp eller ett filnamn för en .wav-fil att spela upp för den som ringer. Används bara när inställningen ovan är blank.</translation>
     </message>
     <message>
         <source>Text to Speech Voice</source>
-        <translation type="unfinished"></translation>
+        <translation>Text-till-tal-röst</translation>
     </message>
     <message>
         <source>Choose a voice to use from the Text To Speech library. </source>
-        <translation type="unfinished"></translation>
+        <translation>Välj en röst att använda från text-till-tal-biblioteket.</translation>
     </message>
     <message>
         <source>Allows you to register with services such as Free World Dialup; or with applications like Asterisk. Restart mythfrontend if you change this.</source>
-        <translation type="unfinished"></translation>
+        <translation>Låter dig registrera dig med tjänster som Free World Dialup eller applikationer som Asterisk. Starta om mythfrontend om du ändrar detta.</translation>
     </message>
     <message>
         <source>Name of the Proxy, such as fwd.pulver.com for Free World Dialup.</source>
-        <translation type="unfinished"></translation>
+        <translation>Namn på proxyn, som exempelvis fwd.pulver.com för Free World Dialup.</translation>
     </message>
     <message>
         <source>SIP Local Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Lokal SIP-port</translation>
     </message>
     <message>
         <source>5060</source>
-        <translation type="unfinished"></translation>
+        <translation>5060</translation>
     </message>
     <message>
         <source>The port on this machine to use. You may need to make these different for each Mythfrontend and setup your firewall to let this port through.</source>
-        <translation type="unfinished"></translation>
+        <translation>Porten att använda på denna maskin. Du kan behöva använda olika portar för varje mythfrontend och öppna motsvarande portar på din brandvägg.</translation>
     </message>
     <message>
         <source>Microphone device</source>
-        <translation type="unfinished"></translation>
+        <translation>Mikrofonenhet</translation>
     </message>
     <message>
         <source>Select the device path for your microphone. Currently this CANNOT be the same device as used for audio output.</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj din mikrofonenhet. För närvarande kan detta INTE vara samma enhet som används för ljudutmatning.</translation>
     </message>
     <message>
         <source>My Display Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Mitt visade namn</translation>
     </message>
     <message>
         <source>My common name to display when I call other people. </source>
-        <translation type="unfinished"></translation>
+        <translation>Namn att visa vid samtal med andra.</translation>
     </message>
     <message>
         <source>Codecs Supported</source>
-        <translation type="unfinished"></translation>
+        <translation>Stödda kodare</translation>
     </message>
     <message>
         <source>The list of codecs to use, in the preferred order separated by semicolon. Supported codecs are G.711u, G.711a and GSM.</source>
-        <translation type="unfinished"></translation>
+        <translation>Listan med kodare som stöds, i den ordning de föredras, separerade med semikolon. Stödda codec är G.711u, G.711a och GSM.</translation>
     </message>
     <message>
         <source>SIP Network Interface</source>
-        <translation type="unfinished"></translation>
+        <translation>SIP nätverksinterface</translation>
     </message>
     <message>
         <source>eth0</source>
-        <translation type="unfinished"></translation>
+        <translation>eth0</translation>
     </message>
     <message>
         <source>Enter the name of the network to bind to e.g. eth0</source>
-        <translation type="unfinished"></translation>
+        <translation>Ange namnet på nätverket att binda till dvs eth0</translation>
     </message>
     <message>
         <source>NAT Traversal Method</source>
-        <translation type="unfinished"></translation>
+        <translation>NAT-traverseringsmetod</translation>
     </message>
     <message>
         <source>NAT IP Address</source>
-        <translation type="unfinished"></translation>
+        <translation>NAT IP-adress</translation>
     </message>
     <message>
         <source>http://checkip.dyndns.org</source>
-        <translation type="unfinished"></translation>
+        <translation>http://checkip.dyndns.org</translation>
     </message>
     <message>
         <source>If you selected MANUAL above, then enter your Public IP Address here. If you selected WEB Server above then enter your web server URL here.</source>
-        <translation type="unfinished"></translation>
+        <translation>Om du valde MANUAL ovan, ange en publik IP-adress här. Om du valde Web Server ange webbservers URL här.</translation>
     </message>
     <message>
         <source>Audio RTP Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Ljud-RTP-port</translation>
     </message>
     <message>
         <source>Enter the port to use for Audio RTP; an even number between 1024 and 32767. If you have a firewall you should enable UDP through the firewall on this port.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ange porten för ljud-RTP; ett jämnt nummer mellan 1024 och 32767. Ställ in din brandvägg på att tillåta UDP på denna port.</translation>
     </message>
     <message>
         <source>Video RTP Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Video-RTP-port</translation>
     </message>
     <message>
         <source>Enter the port to use for Video RTP; an even number between 1024 and 32767. If you have a firewall you should enable UDP through the firewall on this port.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ange porten för video-RTP; ett jämnt nummer mellan 1024 och 32767. Ställ in din brandvägg på att tillåta UDP på denna port.</translation>
     </message>
     <message>
         <source>Auto-Answer</source>
-        <translation type="unfinished"></translation>
+        <translation>Autosvar</translation>
     </message>
     <message>
         <source>When the MythFrontend is in the MythPhone plugin, setting this automatically answers all incoming calls. There is no security yet.</source>
-        <translation type="unfinished"></translation>
+        <translation>Svarar automatiskt inkommande samtal när mythfrontend är i MythPhone-insticksmodulen. Det finns ingen säkerhet än.</translation>
     </message>
     <message>
         <source>SIP Proxy Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>SIP-proxyinställningar</translation>
     </message>
     <message>
         <source>SIP Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>SIP-inställningar</translation>
     </message>
     <message>
         <source>VXML Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>VXML-inställningar</translation>
     </message>
     <message>
         <source>WEBCAM Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Webbkamerainställningar</translation>
     </message>
     <message>
         <source>Number of webcam frames/sec to transmit, from 1 to 30. Higher numbers create better results but use more bandwidth.</source>
-        <translation type="unfinished"></translation>
+        <translation>Antal bildrutor/sek att sända, från 1 till 30. Högre nummer ger bättre resultat men använder mer bandbredd.</translation>
     </message>
     <message>
         <source>Login to a SIP Server</source>
-        <translation type="unfinished"></translation>
+        <translation>Logga in till en SIP-server</translation>
     </message>
     <message>
         <source>SIP Server DNS Name</source>
-        <translation type="unfinished"></translation>
+        <translation>SIP-serverns DNS-namn</translation>
     </message>
     <message>
         <source>Sign-in Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Inloggningsnamn</translation>
     </message>
     <message>
         <source>Your username for authentication with the SIP Server. For FWD this is your FWD number.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ditt användarnamn för authentisiering med SIP-servern. För FWD är detta ditt FWD-nummer.</translation>
     </message>
     <message>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Lösenord</translation>
     </message>
     <message>
         <source>Your password for authentication with the SIP Server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ditt lösenord för authentisiering med SIP-servern.</translation>
     </message>
     <message>
         <source>Method to use for NAT traversal; needs a Frontend restart after changing. Choose NONE if you have a public IP address, choose MANUAL if your ISP always gives you the same public address and manually enter this address below. Choose Web Server if you have a dynamic NAT address and enter a web address like http://checkip.dyndns.org below. </source>
-        <translation type="unfinished"></translation>
+        <translation>Metod för NAT-traversering; ändring kräver omstart av klienten. Välj NONE om du har en publik IP-adress, välj MANUAL om din ISP alltid ger dig samma publika adress och ange din adress nedan. Välj Web Server om du har en dynamisk NAT-adress och ange en webbadress som http://checkip.dyndns.org.</translation>
     </message>
 </context>
 </TS>
