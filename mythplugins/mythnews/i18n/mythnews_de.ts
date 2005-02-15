d1492bbd660dd789936619a51af004cdbba411bd
Oscar Carlsson <ocarlsson@mythtv.org>
2005-02-15 08:11:01 +0000

    German update from Stefan Dutch update from Steven
    git-svn-id: http://svn.mythtv.org/svn/trunk@5614 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythnews/i18n/mythnews_de.ts
+++ mythplugins/mythnews/i18n/mythnews_de.ts
@@ -13,41 +13,41 @@
 <context>
     <name>MythNewsConfig</name>
     <message>
-        <source>Select News Feeds</source>
-        <translation>Auswählen der News-Feeds</translation>
+        <source>Press MENU to set the update frequency.</source>
+        <translation>Drücken Sie &apos;Menü&apos; um die Aktualisierungsfrequenz zu ändern.</translation>
     </message>
     <message>
-        <source>Set Update Frequency</source>
-        <translation>Aktualisierungsfrequenz</translation>
+        <source>Set update frequency by using the up/down arrows.
+The minimum allowed value is 30 Minutes.</source>
+        <translation>Mit den Pfeil-Auf/Ab Tasten die Aktualisierungsfrequenz auswählen.
+Das Minimum sind 30 Minuten.</translation>
     </message>
     <message>
-        <source>Set Update Frequency in Minutes
-Minimum allowed value is 30 Minutes</source>
-        <translation>Aktualisierungsfrequenz setzen
-Minimal erlaubter Wert ist 30 Minuten</translation>
+        <source>Press MENU to return to feed selection.</source>
+        <translation>Mit &apos;Menü&apos; gelangen Sie zurück zur Feed-Auswahl.</translation>
     </message>
 </context>
 <context>
     <name>NewsSite</name>
     <message>
         <source>Failed to retrieve news</source>
-        <translation>Fehler beim Abrufen der News</translation>
+        <translation>Fehler beim Abrufen der Nachrichten.</translation>
     </message>
     <message>
         <source>Retrieve Failed. </source>
-        <translation>Empfang gescheitert.</translation>
+        <translation>Empfang gescheitert. </translation>
     </message>
     <message>
         <source>No Cached News</source>
-        <translation>Keine zwischengespeicherten News</translation>
+        <translation>Keine zwischengespeicherten Nachrichten.</translation>
     </message>
     <message>
         <source>Failed to read downloaded file</source>
-        <translation>Fehler beim Lesen der geladenen Datei</translation>
+        <translation>Fehler beim Lesen der geladenen Datei.</translation>
     </message>
     <message>
         <source>Showing Cached News</source>
-        <translation>Zeige zwischengespeicherte News</translation>
+        <translation>Zeige zwischengespeicherte Nachrichten.</translation>
     </message>
 </context>
 <context>
--- mythplugins/mythnews/i18n/mythnews_nl.ts
+++ mythplugins/mythnews/i18n/mythnews_nl.ts
@@ -3,57 +3,58 @@
     <name>MythNews</name>
     <message>
         <source>Updated</source>
-        <translation type="unfinished"></translation>
+        <translation>Ververst</translation>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Onbekend</translation>
     </message>
 </context>
 <context>
     <name>MythNewsConfig</name>
     <message>
         <source>Select News Feeds</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecter News Feeds</translation>
     </message>
     <message>
         <source>Set Update Frequency</source>
-        <translation type="unfinished"></translation>
+        <translation>Stel De Verversfrekwentie In</translation>
     </message>
     <message>
         <source>Set Update Frequency in Minutes
 Minimum allowed value is 30 Minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>Verversfrekwentie in Minuten
+Minimum is 30 Minuten</translation>
     </message>
 </context>
 <context>
     <name>NewsSite</name>
     <message>
         <source>Failed to retrieve news</source>
-        <translation type="unfinished"></translation>
+        <translation>Kon Berichten Niet Inlezen</translation>
     </message>
     <message>
         <source>Retrieve Failed. </source>
-        <translation type="unfinished"></translation>
+        <translation>Inlezen Mislukt. </translation>
     </message>
     <message>
         <source>No Cached News</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen Bewaarde Nieuwsitems</translation>
     </message>
     <message>
         <source>Failed to read downloaded file</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ingelzen Bestand niet lezen</translation>
     </message>
     <message>
         <source>Showing Cached News</source>
-        <translation type="unfinished"></translation>
+        <translation>Ik Toon de Bewaarde Nieuwsitems</translation>
     </message>
 </context>
 <context>
     <name>ThemeUI</name>
     <message>
         <source>Minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>Minuten</translation>
     </message>
 </context>
 </TS>
