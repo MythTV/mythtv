9f35329756539f66614e3450aef7e3a97f3b1f5b
Isaac Richards <irichards@mythtv.org>
2005-03-23 05:12:31 +0000

    - Make nes screenshots check filename-based paths first, from Jason Gabriele
    git-svn-id: http://svn.mythtv.org/svn/trunk@5813 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythgame/mythgame/nesrominfo.cpp
+++ mythplugins/mythgame/mythgame/nesrominfo.cpp
@@ -8,10 +8,20 @@
 
 bool NesRomInfo::FindImage(QString type, QString* result)
 {
-    bool retval = false;
-
     if (type != "screenshot")
         return false;
+
+    QString screenLocation = gContext->GetSetting("NesScreensLocation");
+
+    // First, before looking at the DB, just try and see if a screenshot matches the rom name
+    *result = screenLocation + "/" + Romname() + ".gif";
+    if (QFile::exists(*result)) 
+        return true;
+
+    // Now just try to match the game name
+    *result = screenLocation + "/" + Gamename() + ".gif";
+    if (QFile::exists(*result)) 
+        return true;
     
     // Try to match the GoodNES name against the title table in order to get an image
     // index that hopefully matches up with the game.
@@ -28,28 +38,16 @@ bool NesRomInfo::FindImage(QString type, QString* result)
         // Take first entry since that will be the most relevant match.
         query.first();
         QString current = query.value(0).toString();
-        *result = gContext->GetSetting("NesScreensLocation") + "/" + current + ".gif";
-        retval = QFile::exists(*result);
-        if (!retval)
-        {
-            // Look for file with the same name as the Game
-            current = query.value(1).toString();
-            *result = gContext->GetSetting("NesScreensLocation") + "/" + current + ".gif";
-            retval = QFile::exists(*result);
-        }
-        else if (!retval)
-        {
-            // Now just try to match the game name
-            *result = gContext->GetSetting("NesScreensLocation") + "/" + Gamename() + ".gif";
-            retval = QFile::exists(*result);
-        }
-        else if (!retval)
-        {
-            // Now just try to match the game name
-            *result = gContext->GetSetting("NesScreensLocation") + "/" + Romname() + ".gif";
-            retval = QFile::exists(*result);
-        }
+        *result = screenLocation + "/" + current + ".gif";
+        if (QFile::exists(*result)) 
+            return true;
+
+        // Look for file with the same name as the Game
+        current = query.value(1).toString();
+        *result = screenLocation + "/" + current + ".gif";
+        if (QFile::exists(*result)) 
+            return true;
     }
 
-    return retval;
+    return false;
 }
