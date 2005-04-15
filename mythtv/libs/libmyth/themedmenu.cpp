35e8cff5ae249de166f44ff76c8520b4a9ddc611
J. Donovan Stanley <jstanley@mythtv.org>
2005-04-15 15:22:03 +0000

    Added a buttoncenter element for the background that allows themes to turn off the automatic centering of buttons along the Y axis.  With <buttonspread>no</buttonspread> and <buttoncenter>no</buttoncenter> buttons always start at the top left of the button area, and have no gaps between them
    git-svn-id: http://svn.mythtv.org/svn/trunk@6085 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/themedmenu.cpp
+++ mythtv/libs/libmyth/themedmenu.cpp
@@ -163,7 +163,7 @@ class ThemedMenuPrivate
 
     QRect logoRect;
     QPixmap *logo;
-
+    
     QImage *buttonnormal;
     QImage *buttonactive;
 
@@ -192,6 +192,7 @@ class ThemedMenuPrivate
     int exitModifier;
 
     bool spreadbuttons;
+    bool buttoncenter;
 
     QMap<QString, QPixmap> titleIcons;
     QPixmap *curTitle;
@@ -201,6 +202,7 @@ class ThemedMenuPrivate
     bool drawTitle;
 
     QPixmap backgroundPixmap;
+    QPixmap* buttonBackground;
 
     bool ignorekeys;
 
@@ -243,6 +245,7 @@ ThemedMenuPrivate::ThemedMenuPrivate(ThemedMenu *lparent, float lwmult,
     buttonactive = NULL;
     uparrow = NULL;
     downarrow = NULL;
+    buttonBackground = NULL;
 
     ignorekeys = false;
 }
@@ -259,7 +262,9 @@ ThemedMenuPrivate::~ThemedMenuPrivate()
         delete uparrow;
     if (downarrow)
         delete downarrow;
-
+    if (buttonBackground)
+        delete buttonBackground;
+        
     QMap<QString, ButtonIcon>::Iterator it;
     for (it = allButtonIcons.begin(); it != allButtonIcons.end(); ++it)
     {
@@ -292,7 +297,7 @@ void ThemedMenuPrivate::parseBackground(const QString &dir,
     QString path;
 
     bool hasarea = false;
-
+    buttoncenter = true;
     spreadbuttons = true;
     maxColumns = 20;        // Arbitrary number
     visiblerowlimit = 6;    // the old default
@@ -323,12 +328,26 @@ void ThemedMenuPrivate::parseBackground(const QString &dir,
 
                 buttonArea = tmpArea;
                 hasarea = true;
+                
+                if (info.hasAttribute( "background" ))
+                {
+                    cerr << "Loading "  << info.attribute( "background" ) << endl;
+                    QString bPath = dir + info.attribute( "background" );
+                    buttonBackground = gContext->LoadScalePixmap(bPath);
+                }
+                
             }
             else if (info.tagName() == "buttonspread")
             {
                 QString val = getFirstText(info);
                 if (val == "no")
                     spreadbuttons = false;
+            }
+            else if (info.tagName() == "buttoncenter")
+            {
+                QString val = getFirstText(info);
+                if (val == "no")
+                    buttoncenter = false;
             }
             else if (info.tagName() == "balancerows")
             {
@@ -1732,7 +1751,10 @@ void ThemedMenuPrivate::positionButtons(bool resetpos)
     if (!spreadbuttons)
     {
         yspacing = 0;
-        ystart = (buttonArea.height() - buttonnormal->height() * rows) / 2;
+        if (buttoncenter)
+        {
+            ystart = (buttonArea.height() - buttonnormal->height() * rows) / 2;
+        }
     }
 
     int row = 1;
@@ -1860,6 +1882,13 @@ void ThemedMenuPrivate::drawInactiveButtons(void)
 
     paintLogo(&tmp);
     paintTitle(&tmp);
+    
+    
+    if (buttonBackground)
+    {
+        tmp.drawPixmap(buttonArea.topLeft(), *buttonBackground);
+         
+    }
 
     ThemedButton *store = activebutton;
     activebutton = NULL;
