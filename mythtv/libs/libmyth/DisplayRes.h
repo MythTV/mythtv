009866985294846eca5c2bd9c741c6fd6a854320
Daniel Kristjansson <dkristjansson@mythtv.org>
2005-04-22 03:49:47 +0000

    Adds doxygen documentation to DisplayRes.h.
    git-svn-id: http://svn.mythtv.org/svn/trunk@6157 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/DisplayRes.h
+++ mythtv/libs/libmyth/DisplayRes.h
@@ -17,39 +17,100 @@ using namespace std;
  *
  *  The SwitchToVideoMode routine takes care of the actual
  *  work involved in changing the display mode. There are
- *  currently XRandR and OSX Carbon implementation so this
- *  works for X11 and OSX.
+ *  currently XRandR and OS X Carbon implementation so this
+ *  works for X (Linux/BSD/UNIX) and Mac OS X.
  */
 
 typedef enum { GUI = 0, VIDEO = 1, CUSTOM_GUI = 2, CUSTOM_VIDEO = 3 } tmode;
 
 class DisplayRes {
   public:
-    /// Factory method that returns DisplayRes singleton
+    /** \brief Factory method that returns a DisplayRes singleton */
     static DisplayRes *GetDisplayRes(void);
 
-    /// Initialize is called automatically on creation, but 
-    /// should be called again if the configuration is changed.
+    /** \brief Initialize DisplayRes, normally called automatically.
+     *
+     *   While Initialize is called automatically by GetDisplayRes(void)
+     *   it should be called again if the database settings for DisplayRes
+     *   have changed.
+     */
     bool Initialize(void);
 
-    // These return true if mode is changed
+    /** \defgroup vid_mode_switch Resolution/Refresh Rate Changing methods.
+     *   These return true if mode is changed
+     * @{
+     */
+
+    /** \fn SwitchToVideo(int iwidth, int iheight, short irate = 0)
+     *  \brief Switches to the resolution and refresh rate defined in the
+     *         database for the specified video resolution and frame rate.
+     */
     bool SwitchToVideo(int iwidth, int iheight, short irate = 0);
+    /** \brief Switches to the GUI resolution specified.
+     *
+     *   If which_gui is GUI then this switches to the resolution
+     *   and refresh rate set in the database for the GUI. If 
+     *   which_gui is set to CUSTOM_GUI then we switch to the
+     *   resolution and refresh rate specified in the last
+     *   call to SwitchToCustomGUI(int, int, short).
+     *
+     *  \param which_gui either regular GUI or CUSTOM_GUI
+     *  \sa SwitchToCustomGUI(int, int, short)
+     */
     bool SwitchToGUI(tmode which_gui=GUI);
+    /** \brief Switches to the custom GUI resolution specified.
+     *
+     *   This switches to the specified resolution, and refresh
+     *   rate if specified. It also makes saves this resolution
+     *   as the CUSTOM_GUI resolution, so that it can be 
+     *   recalled with SwitchToGUI(CUSTOM_GUI) in later 
+     *   DisplayRes calls.
+     *  \sa SwitchToGUI(tmode)
+     */
     bool SwitchToCustomGUI(int width, int height, short rate = 0);
+    /** @} */
 
-    // These return the current display attributes
-    int GetWidth(void)       const { return last.Width(); }
-    int GetHeight(void)      const { return last.Height(); }
-    int GetPhysicalWidth(void)    const { return last.Width_mm(); }
-    int GetPhysicalHeight(void)   const { return last.Height_mm(); }
-    int GetRefreshRate(void) const { return last.RefreshRate(); }
+
+    /** \defgroup vid_mode_current Queries of current video mode
+     * @{
+     */
+
+    /** \brief Returns current screen width in pixels. */
+    int GetWidth(void)          const { return last.Width(); }
+
+    /** \brief Returns current screen width in pixels. */
+    int GetHeight(void)         const { return last.Height(); }
+
+    /** \brief Returns current screen width in millimeters. */
+    int GetPhysicalWidth(void)  const { return last.Width_mm(); }
+
+    /** \brief Returns current screen height in millimeters. */
+    int GetPhysicalHeight(void) const { return last.Height_mm(); }
+
+    /** \brief Returns current screen refresh rate. */
+    int GetRefreshRate(void)    const { return last.RefreshRate(); }
+    /** \brief Returns current screen aspect ratio.
+     *
+     *  If there is an aspect overide in the database that aspect
+     *  ratio is returned instead of the actual screen aspect ratio.
+     */
     double GetAspectRatio(void) const { return last.AspectRatio(); }
+    /** @} */
 
-    // These are global for all video modes
+
+    /** \defgroup vid_mode_global These methods are global for all video modes
+     * @{
+     */
+
+    /// \brief Returns maximum width in pixels supported by display.
     int GetMaxWidth(void)    const { return max_width; }
+    /// \brief Returns maximum height in pixels supported by display.
     int GetMaxHeight(void)   const { return max_height; }
+    /// \brief Returns all video modes supported by the display.
     virtual const vector<DisplayResScreen>& GetVideoModes() const = 0;
+    /// \brief Returns refresh rates available at a specific screen resolution.
     const vector<short> GetRefreshRates(int width, int height) const;
+    /** @} */
 
     // These are for backward compatability
     static DisplayRes *getDisplayRes(void);
@@ -71,8 +132,8 @@ class DisplayRes {
     int Height_mm(void)   const { return GetPhysicalHeight(); }
     int vidWidth(void)    const { return vwidth; }
     int vidHeight(void)    const { return vheight; }
-
   protected:
+    /// \brief DisplayRes is an abstract class, instanciate with GetDisplayRes(void)
     DisplayRes(void) : max_width(0), max_height(0) {;}
     virtual ~DisplayRes(void) {;}
     
@@ -87,7 +148,7 @@ class DisplayRes {
     DisplayResScreen mode[4]; // GUI, default video, custom GUI, custom video
     DisplayResScreen last;    // mirror of mode[current_mode]
 
-    // maps input video parameters to output video modes
+    /// maps input video parameters to output video modes
     DisplayResMap in_size_to_output_mode;
 
     int max_width, max_height;
@@ -95,7 +156,14 @@ class DisplayRes {
     static DisplayRes *instance;
 };
 
-// Helper function
-const vector<DisplayResScreen> GetVideoModes();
+/** \fn GetVideoModes(void)
+ *  \relates DisplayRes
+ *  \brief Returns all video modes available.
+ * 
+ *   This is a conveniance class that instanciates a DisplayRes
+ *   class if needed, and returns a copy of vector returned by
+ *   GetVideoModes(void).
+ */
+const vector<DisplayResScreen> GetVideoModes(void);
 
 #endif
