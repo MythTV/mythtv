f714866fc44cd3921b03c3da78272222069bb1d8
J. Donovan Stanley <jstanley@mythtv.org>
2005-05-11 14:35:58 +0000

    Minor change to the drawing order for the containers so that the (very small) "view" container can be overlaid on the (much larger) "program_info" container.
    git-svn-id: http://svn.mythtv.org/svn/trunk@6366 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/proglist.cpp
+++ mythtv/libs/libmythtv/proglist.cpp
@@ -300,13 +300,14 @@ void ProgLister::paintEvent(QPaintEvent *e)
     QRect r = e->rect();
     QPainter p(this);
  
-    if (updateAll || r.intersects(viewRect))
-        updateView(&p);
+    
     if (updateAll || r.intersects(listRect))
         updateList(&p);
     if (updateAll || r.intersects(infoRect))
         updateInfo(&p);
-
+    if (updateAll || r.intersects(viewRect))
+        updateView(&p);
+    
     updateAll = false;
 }
 
