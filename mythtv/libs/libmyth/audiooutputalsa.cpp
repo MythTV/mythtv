e798eced8b6461e69c2c1a58c81d3d9ff0cf1ffd
Chris Pinkham <cpinkham@mythtv.org>
2005-06-03 03:29:58 +0000

    Patch by Rich Goodin to add missing colon in code that allows setting mixer device to "ALSA:default".
    git-svn-id: http://svn.mythtv.org/svn/trunk@6561 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/audiooutputalsa.cpp
+++ mythtv/libs/libmyth/audiooutputalsa.cpp
@@ -551,7 +551,7 @@ void AudioOutputALSA::SetupMixer(void)
     int err;
 
     QString alsadevice = gContext->GetSetting("MixerDevice", "default");
-    QString device = alsadevice.remove(QString("ALSA"));
+    QString device = alsadevice.remove(QString("ALSA:"));
 
     if (mixer_handle != NULL)
         CloseMixer();
