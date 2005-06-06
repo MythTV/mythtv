059a9d052b709fdf421a1c722c12f8535aa86da9
Daniel Kristjansson <dkristjansson@mythtv.org>
2005-06-06 05:18:49 +0000

    Cleans up formatting in PESPacket a bit and adds a new method, dvbParseableData(), which produces a unsigned char pointer to data that can be passed to SIParser::ParseTable().
    git-svn-id: http://svn.mythtv.org/svn/trunk@6593 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/mpeg/pespacket.h
+++ mythtv/libs/libmythtv/mpeg/pespacket.h
@@ -15,29 +15,41 @@ extern "C" {
 #include "../libavformat/mpegts.h"
 }
 
-inline unsigned int pes_length(const unsigned char* pesbuf) {
-    return (pesbuf[2] & 0x0f) << 8 | pesbuf[3];
-}
+inline unsigned int pes_length(const unsigned char* pesbuf)
+    { return (pesbuf[2] & 0x0f) << 8 | pesbuf[3]; }
 
-class PESPacket {
-    // only handles single TS packet PES packets, for tables basically
-    void InitPESPacket(TSPacket& tspacket) {
-        if (tspacket.PayloadStart()) {
-            _pesdata = tspacket.data() + tspacket.AFCOffset() + tspacket.StartOfFieldPointer();
-        } else {
+/** \class PESPacket
+ *  \brief Allows us to transform TS packets to PES packets, which
+ *         are used to hold PSIP tables as well as multimedia streams.
+ *  \sa PSIPTable, TSPacket
+ */
+class PESPacket
+{
+    /// Only handles single TS packet PES packets, for PMT/PAT tables basically
+    void InitPESPacket(TSPacket& tspacket)
+    {
+        if (tspacket.PayloadStart())
+        {
+            _pesdata = tspacket.data() + tspacket.AFCOffset() + 
+                tspacket.StartOfFieldPointer();
+        }
+        else
+        {
             cerr<<"Started PESPacket, but !payloadStart()"<<endl;
             _pesdata = tspacket.data() + tspacket.AFCOffset();
         }
 
-        _badPacket=true;
-        if ((_pesdata - tspacket.data())<(188-3-4)) {
-            _badPacket = CalcCRC()!=CRC();
+        _badPacket = true;
+        if ((_pesdata - tspacket.data()) < (188-3-4))
+        {
+            _badPacket = !VerifyCRC();
         }
     }
 
   protected:
     // does not create it's own data
-    PESPacket(const TSPacket* tspacket, bool) : _pesdata(0), _fullbuffer(0), _allocSize(0)
+    PESPacket(const TSPacket* tspacket, bool)
+        : _pesdata(0), _fullbuffer(0), _allocSize(0)
     {
         InitPESPacket(const_cast<TSPacket&>(*tspacket));
         _fullbuffer = const_cast<unsigned char*>(tspacket->data());
@@ -81,13 +93,11 @@ class PESPacket {
         _pesdata = 0;
     }
 
-    static const PESPacket View(const TSPacket& tspacket) {
-        return PESPacket(&tspacket, false);
-    }
+    static const PESPacket View(const TSPacket& tspacket)
+        { return PESPacket(&tspacket, false); }
 
-    static PESPacket View(TSPacket& tspacket) {
-        return PESPacket(&tspacket, false);
-    }
+    static PESPacket View(TSPacket& tspacket)
+        { return PESPacket(&tspacket, false); }
 
     bool IsClone() const { return bool(_allocSize); }
 
@@ -97,45 +107,53 @@ class PESPacket {
     bool IsGood() const { return !_badPacket; }
 
     const TSHeader* tsheader() const 
-    {
-        return reinterpret_cast<const TSHeader*>(_fullbuffer);
-    }
-
-    TSHeader* tsheader() { return reinterpret_cast<TSHeader*>(_fullbuffer); }
+        { return reinterpret_cast<const TSHeader*>(_fullbuffer); }
+    TSHeader* tsheader()
+        { return reinterpret_cast<TSHeader*>(_fullbuffer); }
 
     unsigned int StartCodePrefix() const { return _pesdata[0]; }
     unsigned int StreamID() const { return _pesdata[1]; }
-    unsigned int Length() const {
-        return (pesdata()[2] & 0x0f) << 8 | pesdata()[3];
-    }
+    unsigned int Length() const
+        { return (pesdata()[2] & 0x0f) << 8 | pesdata()[3]; }
 
     const unsigned char* pesdata() const { return _pesdata; }
     unsigned char* pesdata() { return _pesdata; }
 
+    const unsigned char* dvbParseableData() const { return _pesdata+1; }
+    /// \brief You can call SIParser::ParseTable(data, size, pid)
+    ///        where data = pes.dvbParseableData(), size = pes.length()+12,
+    ///        and pid = pes.tspacket()->PID().
+    unsigned char* dvbParseableData() { return _pesdata+1; }
+
     void SetStartCodePrefix(unsigned int val) { _pesdata[0]=val&0xff; }
     void SetStreamID(unsigned int id) { _pesdata[1]=id; }
-    void SetLength(unsigned int len) {
+    void SetLength(unsigned int len)
+    {
         pesdata()[2] = (pesdata()[2] & 0xf0) | ((len>>8) & 0x0f);
         pesdata()[3] = len & 0xff;
     }
 
-    unsigned int CRC() const {
+    unsigned int CRC() const 
+    {
         unsigned int offset = Length();
         return ((_pesdata[offset+0]<<24) |
                 (_pesdata[offset+1]<<16) |
                 (_pesdata[offset+2]<<8) |
                 (_pesdata[offset+3]));
     }
-    unsigned int CalcCRC() const {
-        return mpegts_crc32(_pesdata+1, Length()-1);
-    }
-    void SetCRC(unsigned int crc) {
+
+    unsigned int CalcCRC() const
+        { return mpegts_crc32(dvbParseableData(), Length()-1); }
+
+    void SetCRC(unsigned int crc)
+    {
         _pesdata[Length()+0] = (crc & 0xff000000) >> 24;
         _pesdata[Length()+1] = (crc & 0x00ff0000) >> 16;
         _pesdata[Length()+2] = (crc & 0x0000ff00) >> 8;
         _pesdata[Length()+3] = (crc & 0x000000ff);
     }
-    bool VerifyCRC() const { return CalcCRC()==CRC(); }
+
+    bool VerifyCRC() const { return CalcCRC() == CRC(); }
 
     /*
     // for testing
