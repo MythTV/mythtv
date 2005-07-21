MythTV/mythtv

commit 73d3fa9466fd08feed56009a74003422ce52e69d

The "if this won't put you over your email inbox quota for the day, then nothing will" commit log message....
* This is a large Commercial Detection re-organization patch by Lucas Meijer
  with a little cleanup and minor modifications by me (Chris Pinkham).
  The comments here are mine so don't fault him for my wordiness or typos. :)

  Even though the patch is large, no big new Commercial Detection
  functionality is being added, this is mainly a re-org and cleanup of code.
  The main purposes are to move the Commmercial Flagger code out of libmythtv
  into mythtv/programs/mythcommflag and to make it easier to add and test
  new commercial detection methods.  The commercial detection results from
  this code should be the same as from prior code, no enhancement or
  degredation should be experienced.

  - Added files/classes:
    - ClassicCommDetector.(cpp|h).  This is the main guts of the current
      (aka original) commercial detection code previously located in
      mythtv/libs/libmythtv/commercial_skip.(cpp|h).
    - CommDetectorBase.(cpp|h).  This is a base commercial detector
      class which is now inherited by ClassicCommDetector.
    - CommDirectorFactory.(cpp|h).  This is a wrapper for the
      ClassicCommDetector and any new detectors that are added/tested.
      mythcommflag now instantiates a commercial detector via the
      CommDirectorFactory.
    - CustomEventRelayer.h.  A simple class for relaying QCustomEvent events
      to a function pointer.
    - SlotRelayer.h.  A simple class for relaying a Qt slot to a function
      pointer.

  - Removed files/classes:
    - commercial_skip.(cpp|h) containing CommDetect class.  This
      code and functionality has been moved to ClassicCommDetector.(cpp|h).

  - Minor feature additions:
    - mythcommflag now sends out a COMMFLAG_START event when it starts
      flagging a recording.
    - players send out a COMMFLAG_REQUEST event when the user starts playing
      a recording.

mythtv/libs/libmythtv/tv_play.cpp
@@ -26,7 +26,6 @@ using namespace std;
 #include "NuppelVideoPlayer.h"
 #include "programinfo.h"
 #include "udpnotify.h"
-#include "commercial_skip.h"
 #include "vsync.h"
 #include "lcddevice.h"
 #include "jobqueue.h"
@@ -179,7 +178,7 @@ TV::TV(void)
       stickykeys(0), ff_rew_repos(1.00), ff_rew_reverse(false),
       smartForward(false),
       // Configuration varibles from DB just before playback
-      autoCommercialSkip(false), tryUnflaggedSkip(false),
+      autoCommercialSkip(false),
       osd_display_time(0),
       // State variables
       internalState(kState_None), nextState(kState_None), changeState(false),
@@ -760,6 +759,11 @@ void TV::HandleStateChange(void)
             changed = true;
     
             StartPlayerAndRecorder(true, false);
+
+            QString message = "COMMFLAG_REQUEST ";
+            message += playbackinfo->chanid + " " +
+                       playbackinfo->startts.toString(Qt::ISODate);
+            RemoteSendMessage(message);
         }
         else
         {
@@ -914,11 +918,6 @@ void TV::SetupPlayer(void)
 
     autoCommercialSkip = gContext->GetNumSetting("AutoCommercialSkip", 0);
     nvp->SetAutoCommercialSkip(autoCommercialSkip);
-    nvp->SetCommercialSkipMethod(gContext->GetNumSetting("CommercialSkipMethod",
-                                                         COMM_DETECT_BLANKS));
-
-    tryUnflaggedSkip = gContext->GetNumSetting("TryUnflaggedSkip", 0);
-    nvp->SetTryUnflaggedSkip(tryUnflaggedSkip);
 
     osd_display_time = gContext->GetNumSetting("OSDDisplayTime");
 
@@ -3657,14 +3656,30 @@ void TV::customEvent(QCustomEvent *e)
             wantsToQuit = true;
             exitPlayer = true;
         }
-        else if (message.left(15) == "COMMFLAG_UPDATE")
+        else if (message.left(14) == "COMMFLAG_START")
         {
+            message = message.simplifyWhiteSpace();
             QStringList tokens = QStringList::split(" ", message);
             QString evchanid = tokens[1];
             QDateTime evstartts = QDateTime::fromString(tokens[2], Qt::ISODate);
 
             if ((playbackinfo->chanid == evchanid) &&
-                (playbackinfo->recstartts == evstartts))
+                (playbackinfo->startts == evstartts))
+            {
+                QString msg = "COMMFLAG_REQUEST ";
+                msg += tokens[1] + " " + tokens[2];
+                RemoteSendMessage(msg);
+            }
+        }
+        else if (message.left(15) == "COMMFLAG_UPDATE")
+        {
+            message = message.simplifyWhiteSpace();
+            QStringList tokens = QStringList::split(" ", message);
+            QString evchanid = tokens[1];
+            QDateTime evstartts = QDateTime::fromString(tokens[2], Qt::ISODate);
+
+            if ((playbackinfo->chanid == evchanid) &&
+                (playbackinfo->startts == evstartts))
             {
                 QMap<long long, int> newMap;
                 QStringList mark;

mythtv/libs/libmyth/exitcodes.h
@@ -113,7 +113,6 @@
 // Commercial flagging should return number of breaks,
 // or one of these exit codes
 #define COMMFLAG_EXIT_NO_ERROR_WITH_NO_BREAKS     GENERIC_EXIT_OK
-#define COMMFLAG_EXIT_TEST_MODE                   GENERIC_EXIT_OK
 
 #define COMMFLAG_EXIT_NO_MYTHCONTEXT              GENERIC_EXIT_NO_MYTHCONTEXT
 #define COMMFLAG_EXIT_DB_ERROR                    GENERIC_EXIT_DB_ERROR

mythtv/libs/libmythtv/NuppelVideoPlayer.h
@@ -47,8 +47,6 @@ class NuppelVideoPlayer
     NuppelVideoPlayer(ProgramInfo *info = NULL);
    ~NuppelVideoPlayer();
 
-    friend class CommDetect;
-
     void SetParentWidget(QWidget *widget) { parentWidget = widget; }
 
     void SetAsPIP(void) { disableaudio = using_null_videoout = true; }
@@ -60,11 +58,10 @@ class NuppelVideoPlayer
 
     void SetExactSeeks(bool exact) { exactseeks = exact; }
     void SetAutoCommercialSkip(int autoskip);
-    void SetTryUnflaggedSkip(bool tryskip) { tryunflaggedskip = tryskip; };
     void SetCommercialSkipMethod(int method) { commercialskipmethod = method; }
     void SetCommBreakMap(QMap<long long, int> &newMap);
 
-    int OpenFile(bool skipDsp = false, bool initCommFlag = true);
+    int OpenFile(bool skipDsp = false);
     void StartPlaying(void);
     void StopPlaying(void) { killplayer = true; decoder_thread_alive = false; }
 
@@ -208,7 +205,9 @@ class NuppelVideoPlayer
     long long CalcMaxFFTime(long long ff);
 
     bool IsErrored() { return errored; }
-
+    bool InitVideo(void);
+    VideoFrame* GetRawVideoFrame(long long frameNumber = -1);
+    
     long long GetAudioTimecodeOffset() { return audio_timecode_offset; }
     long long AdjustAudioTimecodeOffset(long long v) { 
         audio_timecode_offset += v;
@@ -226,8 +225,6 @@ class NuppelVideoPlayer
     VideoOutputType forceVideoOutput;
 
  private:
-    bool InitVideo(void);
-
     void InitFilters(void);
 
     bool GetVideoPause(void);
@@ -249,12 +246,11 @@ class NuppelVideoPlayer
 
     int GetStatusbarPos(void);
 
-    bool FrameIsBlank(VideoFrame *frame);
-    bool LastFrameIsBlank(void);
     int SkipTooCloseToEnd(int frames);
     void SkipCommercialsByBlanks(void);
     bool DoSkipCommercials(int direction);
     void AutoCommercialSkip(void);
+    bool FrameIsInMap(long long frameNumber, QMap<long long, int> &breakMap);
 
     void JumpToFrame(long long frame);
     void JumpToNetFrame(long long net) { JumpToFrame(framesPlayed + net); }
@@ -278,7 +274,6 @@ class NuppelVideoPlayer
     bool IsInDelete(long long testframe);
     void SaveCutList(void);
     void LoadCutList(void);
-    void LoadBlankList(void);
     void LoadCommBreakList(void);
     void DisableEdit(void);
     void SetDeleteIter(void);
@@ -361,8 +356,6 @@ class NuppelVideoPlayer
     long long rewindtime, fftime;
     RemoteEncoder *nvr_enc;
 
-    class CommDetect *commDetect;
-
     int totalLength;
     long long totalFrames;
 
@@ -413,13 +406,11 @@ class NuppelVideoPlayer
     long long bookmarkseek;
     bool previewFromBookmark;
     
-    int consecutive_blanks;
     int skipcommercials;
     int autocommercialskip;
     int commercialskipmethod;
     int commrewindamount;
     int commnotifyamount;
-    bool tryunflaggedskip;
     int lastCommSkipDirection;
     long long lastCommSkipStart;
     time_t lastCommSkipTime;

mythtv/libs/libmythtv/NuppelVideoRecorder.cpp
@@ -20,7 +20,6 @@ using namespace std;
 
 #include "mythcontext.h"
 #include "NuppelVideoRecorder.h"
-#include "commercial_skip.h"
 #include "vbitext/cc.h"
 #include "channelbase.h"
 #include "filtermanager.h"
@@ -82,7 +81,6 @@ NuppelVideoRecorder::NuppelVideoRecorder(ChannelBase *channel)
     strm = NULL;   
     mp3buf = NULL;
 
-    commDetect = NULL;
     transcoding = false;
 
     act_video_encode = 0;
@@ -175,8 +173,6 @@ NuppelVideoRecorder::~NuppelVideoRecorder(void)
         lame_close(gf);  
     if (strm)
         delete [] strm;
-    if (commDetect)
-        delete commDetect;
     if (fd >= 0)
         close(fd);
     if (seektable)
@@ -951,9 +947,6 @@ void NuppelVideoRecorder::StartRecording(void)
     else
         useavcodec = true;
 
-    if (!hardware_encode)
-        blank_frames.clear();
-
     if (useavcodec)
         useavcodec = SetupAVCodec();
 
@@ -980,25 +973,6 @@ void NuppelVideoRecorder::StartRecording(void)
         VERBOSE(VB_IMPORTANT, "NVR: Error, couldn't spawn children");
         errored = true;
         return;
-    }
-
-    if ((!pip_mode) && (curRecording) && (curRecording->chancommfree == 0))
-    {
-        int commDetectMethod = gContext->GetNumSetting("CommercialSkipMethod",
-                                                       COMM_DETECT_BLANKS);
-
-        if (commDetectMethod & COMM_DETECT_BLANKS)
-        {
-            if (commDetect)
-                commDetect->Init(w_out, h_out, video_frame_rate,
-                                 commDetectMethod);
-            else
-                commDetect = new CommDetect(w_out, h_out, video_frame_rate,
-                                            commDetectMethod);
-
-            commDetect->SetAggressiveDetection(
-                            gContext->GetNumSetting("AggressiveCommDetect", 1));
-        }
     }
 
     // save the start time
@@ -3553,24 +3527,6 @@ void NuppelVideoRecorder::WriteVideo(VideoFrame *frame, bool skipsync,
     frameofgop++;
     framesWritten++;
 
-    if ((!hardware_encode) && (commDetect) && (!pip_mode))
-    {
-        commDetect->ProcessFrame(frame, (fnum-startnum)>>1 );
-        if (commDetect->FrameIsBlank())
-        {
-            commDetect->GetBlankFrameMap(blank_frames);
-
-            if (curRecording && 
-                ((blank_frames.size() % 15 ) == 0))
-            {
-                curRecording->SetBlankFrameList(blank_frames,
-                        prev_bframe_save_pos, (fnum-startnum)>>1 );
-
-                prev_bframe_save_pos = ((fnum-startnum)>>1) + 1;
-            }
-        }
-    }
-   
     // now we reset the last frame number so that we can find out
     // how many frames we didn't get next time
     lf = fnum;
@@ -3727,10 +3683,4 @@ void NuppelVideoRecorder::WriteText(unsigned char *buf, int len, int timecode,
         WriteFrameheader(&frameheader);
         ringBuffer->Write(buf, len);
     }
-}
-
-void NuppelVideoRecorder::GetBlankFrameMap(QMap<long long, int> &blank_frame_map)
-{
-    if (commDetect)
-        commDetect->GetBlankFrameMap(blank_frame_map);
 }

mythtv/libs/libmythtv/NuppelVideoRecorder.h
@@ -19,7 +19,6 @@
 
 #include "recorderbase.h"
 
-#include "commercial_skip.h"
 #include "minilzo.h"
 #include "format.h"
 
@@ -76,7 +75,6 @@ class NuppelVideoRecorder : public RecorderBase
     void SetTranscoding(bool value) { transcoding = value; };
 
     long long GetKeyframePosition(long long desired);
-    void GetBlankFrameMap(QMap<long long, int> &blank_frame_map);
 
     // reencode stuff
     void StreamAllocate(void);
@@ -155,15 +153,12 @@ class NuppelVideoRecorder : public RecorderBase
     int usebttv;
     float video_aspect;
 
-    CommDetect *commDetect;
     bool transcoding;
 
     int mp3quality;
     char *mp3buf;
     int mp3buf_size;
     lame_global_flags *gf;
-
-    QMap<long long, int> blank_frames;
 
     RTjpeg *rtjc;
 

mythtv/libs/libmythtv/commercial_skip.h
@@ -1,248 +1,0 @@
-#ifndef __COMMERCIAL_SKIP_H__
-#define __COMMERCIAL_SKIP_H__
-
-#include <qmap.h>
-
-#include "NuppelVideoPlayer.h"
-#include "mythcontext.h"
-#include "remoteencoder.h"
-
-enum commMapValues {
-    MARK_START = 0,
-    MARK_END,
-    MARK_PRESENT
-};
-
-// This is used as a bitmask.
-enum SkipTypes {
-    COMM_DETECT_OFF         = 0x00,
-    COMM_DETECT_BLANKS      = 0x01,
-    COMM_DETECT_SCENE       = 0x02,
-    COMM_DETECT_BLANK_SCENE = 0x03,
-    COMM_DETECT_LOGO        = 0x04,
-    COMM_DETECT_ALL         = 0xFF
-};
-
-enum frameMaskValues {
-    COMM_FRAME_BLANK         = 0x0001,
-    COMM_FRAME_SCENE_CHANGE  = 0x0002,
-    COMM_FRAME_LOGO_PRESENT  = 0x0004,
-    COMM_FRAME_ASPECT_CHANGE = 0x0008,
-    COMM_FRAME_RATING_SYMBOL = 0x0010
-};
-
-enum frameAspects {
-    COMM_ASPECT_NORMAL = 0,
-    COMM_ASPECT_WIDE
-};
-
-enum frameFormats {
-    COMM_FORMAT_NORMAL = 0,
-    COMM_FORMAT_LETTERBOX,
-    COMM_FORMAT_PILLARBOX,
-    COMM_FORMAT_MAX
-};
-
-#define DEFAULT_LOGO_SECONDS_NEEDED  240
-#define DEFAULT_LOGO_SAMPLE_SPACING  2
-
-extern "C" {
-#include "frame.h"
-}
-
-class CommDetect : public QObject
-{
-    Q_OBJECT
-  public:
-    CommDetect(int w, int h, double frame_rate, int method);
-    ~CommDetect(void);
-
-    void customEvent(QCustomEvent *e);
-
-    void Init(int w, int h, double frame_rate, int method);
-    void SetVideoParams(float aspect);
-    void SetCommDetectMethod(int method) { commDetectMethod = method; };
-    void SetWatchingRecording(bool watching, RemoteEncoder *rec,
-                              NuppelVideoPlayer *nvp);
-
-    void ProcessFrame(VideoFrame *frame, long long frame_number = -1);
-    void SetVerboseDebugging(bool beVerbose = true)
-                             { verboseDebugging = beVerbose; };
-
-    bool FrameIsBlank(void);
-    bool SceneHasChanged(void);
-
-    void SetAggressiveDetection(bool onOff)  { aggressiveDetection = onOff; }
-
-    void ClearAllMaps(void);
-    void GetCommBreakMap(QMap<long long, int> &marks);
-    void SetBlankFrameMap(QMap<long long, int> &blanks);
-    void GetBlankFrameMap(QMap<long long, int> &blanks,
-                          long long start_frame = -1);
-    void GetBlankCommMap(QMap<long long, int> &comms);
-    void GetBlankCommBreakMap(QMap<long long, int> &breaks);
-    void GetSceneChangeMap(QMap<long long, int> &scenes,
-                           long long start_frame = -1);
-    void GetLogoCommBreakMap(QMap<long long, int> &map);
-
-    void BuildMasterCommList(void);
-    void BuildAllMethodsCommList(void);
-    void BuildBlankFrameCommList(void);
-    void BuildSceneChangeCommList(void);
-    void BuildLogoCommList(void);
-
-    void MergeBlankCommList(void);
-    void DeleteCommAtFrame(QMap<long long, int> &commMap, long long frame);
-
-    static bool FrameIsInBreakMap(long long f, QMap<long long, int> &breakMap,
-                                  long long maxFrame);
-
-    bool FrameIsInBreakMap(long long f, QMap<long long, int> &breakMap);
-
-    void DumpMap(QMap<long long, int> &map);
-    void DumpLogo(bool fromCurrentFrame = false);
-
-    void SearchForLogo(NuppelVideoPlayer *nvp, bool fullSpeed);
-    void SetLogoMaskArea();
-    void GetLogoMask(unsigned char *mask);
-    void SetLogoMask(unsigned char *mask);
-
-    int GetLogoSecondsNeeded(void) { return commDetectLogoSecondsNeeded; };
-
-  private:
-    typedef struct edgemaskentry {
-        int isedge;
-        int horiz;
-        int vert;
-        int rdiag;
-        int ldiag;
-    } EdgeMaskEntry;
-
-    typedef struct frameinfo {
-        int minBrightness;
-        int maxBrightness;
-        int avgBrightness;
-        int sceneChangePercent;
-        int aspect;
-        int format;
-        int flagMask;
-    } FrameInfoEntry;
-
-    typedef struct frameblock {
-        long start;
-        long end;
-        long frames;
-        double length;
-        int bfCount;
-        int logoCount;
-        int ratingCount;
-        int scCount;
-        double scRate;
-        int formatMatch;
-        int aspectMatch;
-        int score;
-    } FrameBlock;
-
-    bool CheckStationLogo(void);
-    bool CheckEdgeLogo(void);
-    bool CheckRatingSymbol(void);
-    bool CheckFrameIsInCommMap(long long frameNumber, QMap<long long, int>);
-
-    void CondenseMarkMap(QMap<long long, int>&map, int spacing, int length);
-    void ConvertShowMapToCommMap(QMap<long long, int>&map);
-
-    void CleanupFrameInfo(void);
-    void UpdateFrameBlock(FrameBlock *fbp, FrameInfoEntry finfo, int format,
-                          int aspect);
-    void DetectEdges(VideoFrame *frame, EdgeMaskEntry *edges, int edgeDiff);
-
-    bool aggressiveDetection;
-
-    int commDetectBorder;
-    int commDetectBlankFrameMaxDiff;
-    int commDetectDarkBrightness;
-    int commDetectDimBrightness;
-    int commDetectBoxBrightness;
-    int commDetectDimAverage;
-    int commDetectMaxCommBreakLength;
-    int commDetectMinCommBreakLength;
-    int commDetectMinShowLength;
-    int commDetectMaxCommLength;
-    int commDetectLogoSamplesNeeded;
-    int commDetectLogoSampleSpacing;
-    int commDetectLogoSecondsNeeded;
-    bool commDetectBlankCanHaveLogo;
-    double commDetectLogoGoodEdgeThreshold;
-    double commDetectLogoBadEdgeThreshold;
-
-    int commDetectMethod;
-    bool verboseDebugging;
-
-    long long curFrameNumber;
-
-    int width;
-    int height;
-    int horizSpacing;
-    int vertSpacing;
-    double fps;
-    double fpm;
-    bool blankFramesOnly;
-    int blankFrameCount;
-    int currentAspect;
-
-    long long framesProcessed;
-
-    int totalMinBrightness;
-
-    bool detectBlankFrames;
-    bool detectSceneChanges;
-    bool detectStationLogo;
-
-    bool skipAllBlanks;
-
-    NuppelVideoPlayer *m_parent;
-    bool watchingRecording;
-    RemoteEncoder *recorder;
-
-    EdgeMaskEntry *edgeMask;
-
-    unsigned char *logoMaxValues;
-    unsigned char *logoMinValues;
-    unsigned char *logoFrame;
-    unsigned char *logoMask;
-    unsigned char *logoCheckMask;
-    unsigned char *tmpBuf;
-    bool logoInfoAvailable;
-    int logoEdgeDiff;
-    int logoFrameCount;
-    int logoMinX;
-    int logoMaxX;
-    int logoMinY;
-    int logoMaxY;
-
-    unsigned char *framePtr;
-
-    QMap<long long, FrameInfoEntry> frameInfo;
-    QMap<long long, int> blankFrameMap;
-    QMap<long long, int> blankCommMap;
-    QMap<long long, int> blankCommBreakMap;
-    QMap<long long, int> sceneMap;
-    QMap<long long, int> sceneCommBreakMap;
-    QMap<long long, int> commBreakMap;
-    QMap<long long, int> logoCommBreakMap;
-
-    bool frameIsBlank;
-    bool sceneHasChanged;
-    bool stationLogoPresent;
-
-    bool lastFrameWasBlank;
-    bool lastFrameWasSceneChange;
-    bool decoderFoundAspectChanges;
-
-    int histogram[256];
-    int lastHistogram[256];
-};
-
-#endif
-
-/* vim: set expandtab tabstop=4 shiftwidth=4: */

mythtv/libs/libmythtv/dtvrecorder.cpp
@@ -69,11 +69,6 @@ long long DTVRecorder::GetKeyframePosition(long long desired)
     return ret;
 }
 
-void DTVRecorder::GetBlankFrameMap(QMap<long long, int> &blank_frame_map)
-{
-    (void)blank_frame_map;
-}
-
 void DTVRecorder::Reset()
 {
     _error = false;

commit 5ebbea019cf8eb8e25d89cc47308c855786dd5f4

Fix segfault introduced in r6583, reported properly by Niels den Otter.
Also possibly fixes #139 and #129, but the people reporting those tickets haven't bothered to get useful backtraces yet, so I'm not 100% sure.


git-svn-id: http://svn.mythtv.org/svn/trunk@6890 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/tv_play.cpp
@@ -3806,7 +3806,7 @@ void TV::customEvent(QCustomEvent *e)
                 AskAllowRecording(me->ExtraDataList(), timeuntil);
             }
         }
-        else if (message.left(11) == "QUIT_LIVETV")
+        else if (recorder && message.left(11) == "QUIT_LIVETV")
         {
             message = message.simplifyWhiteSpace();
             QStringList tokens = QStringList::split(" ", message);
@@ -3819,7 +3819,7 @@ void TV::customEvent(QCustomEvent *e)
                 exitPlayer = true;
             }
         }
-        else if (message.left(12) == "EXIT_TO_MENU")
+        else if (nvp && message.left(12) == "EXIT_TO_MENU")
         {
             int exitprompt = gContext->GetNumSetting("PlaybackExitPrompt");
             if (exitprompt == 1 || exitprompt == 2)
@@ -3837,7 +3837,7 @@ void TV::customEvent(QCustomEvent *e)
                 UpdateOSDSignal(signalList);
             }
         }
-        else if (message.left(14) == "COMMFLAG_START")
+        else if (playbackinfo && message.left(14) == "COMMFLAG_START")
         {
             message = message.simplifyWhiteSpace();
             QStringList tokens = QStringList::split(" ", message);
@@ -3852,7 +3852,7 @@ void TV::customEvent(QCustomEvent *e)
                 RemoteSendMessage(msg);
             }
         }
-        else if (message.left(15) == "COMMFLAG_UPDATE")
+        else if (playbackinfo && message.left(15) == "COMMFLAG_UPDATE")
         {
             message = message.simplifyWhiteSpace();
             QStringList tokens = QStringList::split(" ", message);