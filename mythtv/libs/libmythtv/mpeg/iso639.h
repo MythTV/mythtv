246967bac224c2b19b83fcba972ebe3846d69f45
Daniel Kristjansson <dkristjansson@mythtv.org>
2005-08-28 10:33:48 +0000

    Prep for DVB/ATSC language selection
    Ref #238.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@7154 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/mpeg/iso639.h
+++ mythtv/libs/libmythtv/mpeg/iso639.h
@@ -1,7 +1,40 @@
+// -*- Mode: c++ -*-
+
+#include <qmap.h>
 #include <qstring.h>
 
-QString iso639_Alpha3_toName(const unsigned char *iso639_2);
-QString iso639_Alpha2_toName(const unsigned char *iso639_1);
+extern QMap<int, QString> iso639_key_to_english_name;
+extern QMap<int, int>     iso639_str2_to_str3;
 
+/// Converts a 2 or 3 character iso639 string to a language name in English.
 QString iso639_toName(const unsigned char *iso639);
 
+static inline QString iso639_key_to_str3(int code)
+{
+    char str[4];
+    str[0] = (code>>16) & 0xFF;
+    str[1] = (code>>8)  & 0xFF;
+    str[2] = code & 0xFF;
+    str[3] = 0;
+    return QString(str);
+}
+
+static inline int iso639_str3_to_key(const unsigned char *iso639_2)
+{
+    return (iso639_2[0]<<16)|(iso639_2[1]<<8)|iso639_2[2];
+}
+
+static inline int iso639_str3_to_key(const char *iso639_2)
+{
+    return iso639_str3_to_key((const unsigned char*)iso639_2);
+}
+
+static inline int iso639_str2_to_key(const unsigned char *iso639_1)
+{
+    return (iso639_1[1]<<8)|iso639_1[1];
+}
+
+static inline int iso639_str2_to_key(const char *iso639_1)
+{
+    return iso639_str2_to_key((const unsigned char*)iso639_1);
+}
