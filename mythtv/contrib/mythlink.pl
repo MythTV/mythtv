4346e2f1c9c091f238c61c5e7cd053c02efaea98
Chris Petersen <cpetersen@mythtv.org>
2005-09-13 04:42:48 +0000

    apply patch to close #278
    git-svn-id: http://svn.mythtv.org/svn/trunk@7242 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/contrib/mythlink.pl
+++ mythtv/contrib/mythlink.pl
@@ -26,14 +26,43 @@
     our ($db_host, $db_user, $db_name, $db_pass, $video_dir);
     our ($hostname, $dbh, $sh);
 
+# Default filename format
+    $dformat = '%T - %Y-%m-%d, %g-%i %A - %S';
+# Default separator character
+    $dseparator_char = '-';
+# Default replacement character
+    $dreplacement_char = '-';
+# Default space character
+    $dspace_char = ' ';
+
+# Provide default values for GetOptions
+    $format = $dformat;
+    $separator_char = $dseparator_char;
+    $replacement_char = $dreplacement_char;
+    $space_char = $dspace_char;
+
 # Load the destination directory, if one was specified
-    GetOptions('dest|destination|path=s' => \$dest,
-               'format=s'                => \$format,
-               'usage|help|h'            => \$usage,
+    GetOptions('dest|destination|path=s'    => \$dest,
+               'format=s'                   => \$format,
+               'sepchar|separator-char=s'   => \$separator_char,
+               'repchar|replacement-char=s' => \$replacement_char,
+               'space|space-char=s'         => \$space_char,
+               'usage|help|h'               => \$usage,
               );
 
-# Default filename format
-    $format ||= '%T - %Y-%m-%d, %g-%i %A - %S';
+# Check the separator, replacement, and space characters for illegal characters
+    if ($separator_char =~ /(?:[\/\\:*?<>|"])/) {
+      print "Illegal separator character specified.  Using default.\n";
+      $separator_char = $dseparator_char;
+    }
+    if ($replacement_char =~ /(?:[\/\\:*?<>|"])/) {
+      print "Illegal replacement character specified.  Using default.\n";
+      $replacement_char = $dreplacement_char;
+    }
+    if ($space_char =~ /(?:[\/\\:*?<>|"])/) {
+      print "Illegal space character specified.  Using default.\n";
+      $space_char = $dspace_char;
+    }
 
 # Print usage
     if ($usage) {
@@ -51,11 +80,14 @@ options:
 
 --format
 
-    default:  $format
+    default:  $dformat
 
     \%T = title    (aka show name)
     \%S = subtitle (aka episode name)
     \%R = description
+    \%C = category (as reported by grabber)
+    \%U = recording group
+    \%p = separator character
     \%y = year, 2 digits
     \%Y = year, 4 digits
     \%n = month
@@ -71,8 +103,37 @@ options:
     \%a = am/pm
     \%A = AM/PM
 
-    * Illegal filename characters will be replaced with dashes.
+    * For end time, prepend an "e" to the appropriate time/date format code
+      above; i.e. "\%eG" gives the 24-hour hour for the end time.
+
+    * For original airdate, prepend an "o" to the year, month, or day format
+      codes above; i.e. "\%oY" gives the year in which the episode was first
+      aired.
+
     * A suffix of .mpg or .nuv will be added where appropriate.
+
+--separator-char
+
+    The character used to separate sections of the link name.  Specifying the
+    separator character allows trailing separators to be removed from the link
+    name and multiple separators caused by missing data to be consolidated.
+    Indicate the separator character in the format string using either a
+    literal character or the '%p' specifier.
+
+    default:  '$dseparator_char'
+
+--replacement-char
+
+    Characters in the link name which are not legal on some filesystems will
+    be replaced with the given character
+
+    default:  '$dreplacement_char'
+
+--space-char
+
+    Use the specified character instead of space in the link name
+
+    default:  '$dspace_char'
 
 --help
 
@@ -165,7 +226,7 @@ EOF
     }
 
 # Prepare a database query
-    $sh = $dbh->prepare('SELECT title, subtitle, description FROM recorded WHERE chanid=? AND starttime=? AND endtime=?');
+    $sh = $dbh->prepare('SELECT title, subtitle, description, recgroup, category, originalairdate FROM recorded WHERE chanid=? AND starttime=? AND endtime=?');
 
 # Create symlinks for the files on this machine
     foreach my $file (<$video_dir/*.nuv>) {
@@ -182,8 +243,9 @@ EOF
     # Query the desired info about this recording
         $sh->execute($channel, "$syear$smonth$sday$shour$sminute$ssecond", "$eyear$emonth$eday$ehour$eminute$esecond")
             or die "Could not execute ($q):  $!\n\n";
-        my ($title, $subtitle, $description) = $sh->fetchrow_array;
+        my ($title, $subtitle, $description, $recgroup, $category, $oad) = $sh->fetchrow_array;
     # Format some fields we may be parsing below
+        # Start time
         my $meridian = ($shour > 12) ? 'AM' : 'PM';
         my $hour = ($shour > 12) ? $shour - 12 : $shour;
         if ($hour < 10) {
@@ -192,11 +254,28 @@ EOF
         elsif ($hour < 1) {
             $hour = 12;
         }
+        # End time
+        my $emeridian = ($ehour > 12) ? 'AM' : 'PM';
+        my $ethour = ($ehour > 12) ? $ehour - 12 : $ehour;
+        if ($ethour < 10) {
+            $ethour = "0$ethour";
+        }
+        elsif ($ethour < 1) {
+            $ethour = 12;
+        }
+        # Original airdate
+        # Handle NULL values for original airdate.
+        $oad ||= '0000-00-00';
+        my ($oyear, $omonth, $oday) = split(/\-/, $oad, 3);
     # Build a list of name format options
         my %fields;
         ($fields{'T'} = ($title       or '')) =~ s/%/%%/g;
         ($fields{'S'} = ($subtitle    or '')) =~ s/%/%%/g;
         ($fields{'R'} = ($description or '')) =~ s/%/%%/g;
+        ($fields{'C'} = ($category    or '')) =~ s/%/%%/g;
+        ($fields{'U'} = ($recgroup    or '')) =~ s/%/%%/g;
+        $fields{'p'} = $separator_char;     # separator character
+        # Start time
         $fields{'y'} = substr($syear, 2);   # year, 2 digits
         $fields{'Y'} = $syear;              # year, 4 digits
         $fields{'n'} = int($smonth);        # month
@@ -211,18 +290,40 @@ EOF
         $fields{'s'} = $ssecond;            # seconds
         $fields{'a'} = lc($meridian);       # am/pm
         $fields{'A'} = $meridian;           # AM/PM
+        # End time
+        $fields{'ey'} = substr($eyear, 2);  # year, 2 digits
+        $fields{'eY'} = $eyear;             # year, 4 digits
+        $fields{'en'} = int($emonth);       # month
+        $fields{'em'} = $emonth;            # month, leading zero
+        $fields{'ej'} = int($eday);         # day of month
+        $fields{'ed'} = $eday;              # day of month, leading zero
+        $fields{'eg'} = int($ethour);       # 12-hour hour
+        $fields{'eG'} = int($ehour);        # 24-hour hour
+        $fields{'eh'} = $ethour;            # 12-hour hour, with leading zero
+        $fields{'eH'} = $ehour;             # 24-hour hour, with leading zero
+        $fields{'ei'} = $eminute;           # minutes
+        $fields{'es'} = $esecond;           # seconds
+        $fields{'ea'} = lc($emeridian);     # am/pm
+        $fields{'eA'} = $emeridian;         # AM/PM
+        # Original Airdate
+        $fields{'oy'} = substr($oyear, 2);  # year, 2 digits
+        $fields{'oY'} = $oyear;             # year, 4 digits
+        $fields{'on'} = int($omonth);       # month
+        $fields{'om'} = $omonth;            # month, leading zero
+        $fields{'oj'} = int($oday);         # day of month
+        $fields{'od'} = $oday;              # day of month, leading zero
     # Make the substitution
         my $keys = join('', sort keys %fields);
         my $name = $format;
-        $name =~ s/(?<!%)(?:%([$keys]))/$fields{$1}/g;
+        $name =~ s/(?<!%)(?:%([eo]?)([$keys]))/$fields{$1.$2}/g;
         $name =~ s/%%/%/g;
     # Some basic cleanup for illegal (windows) filename characters, etc.
-        $name =~ tr/\ \t\r\n/ /s;
+        $name =~ s/(?:[\/\\\:\*\?\<\>\|$replacement_char])+/$replacement_char/sg;
         $name =~ tr/"/'/s;
-        $name =~ s/(?:[\-\/\\:*?<>|]+\s*)+(?=[^\d\s])/- /sg;
-        $name =~ tr/\/\\:*?<>|/-/;
-        $name =~ s/^[\-\ ]+//s;
-        $name =~ s/[\-\ ]+$//s;
+        $name =~ s/[\s$space_char]+/$space_char/sg;
+        $name =~ s/($space_char*)(?:$separator_char+($space_char*))+/$1$separator_char$2/sg;
+        $name =~ s/^$space_char+//s;
+        $name =~ s/$space_char*$separator_char+$space_char*$//s;
     # Get a shell-safe version of the filename (yes, I know it's not needed in this case, but I'm anal about such things)
         my $safe_file = $file;
         $safe_file =~ s/'/'\\''/sg;
