85458d1bba288addd867e3414cce8a09343de843
Daniel Kristjansson <dkristjansson@mythtv.org>
2005-10-28 16:16:18 +0000

    Documentation update
    Adds a description of the QObject deletion problem
    Adds a warning about threadsafety and Qt container objects
    Changes the version of the documentation from 0.18 to 0.19-prerelease
    Updates the bug db link to point to trac
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@7627 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/docs/doxygen-architecture-document.cpp
+++ mythtv/docs/doxygen-architecture-document.cpp
@@ -9,24 +9,41 @@ into the code for developers to get their heads around it.
 This is intended for both those new to %MythTV and experienced 
 with it to get familiar with different aspects of the code base.
 
-Before we get started, if you are just looking for the code 
-formatting standards, see the unofficial mythtv.info page 
+If you are just looking for the code formatting standards, 
+see the unofficial mythtv.info page 
 <a href="http://www.mythtv.info/moin.cgi/CodingStandards">
-Coding Standards</a>. If you are looking for the 
-<a href="http://svn.mythtv.org/trac/wiki/TicketHowTo">Bug tracker</a>,
-it can be found on the official pages. And you should subscribe to
+coding standards</a>. If you are looking for the 
+<a href="http://svn.mythtv.org/trac/wiki/TicketHowTo">bug tracker</a>,
+it can be found on the official pages. 
+If you haven't already, you should subscribe to
 the <a href="http://www.mythtv.org/mailman/listinfo/mythtv-dev/">
-developer mailing list</a>, if you haven't already.
-And, of course, if you just stumbled onto the developer pages
-by accident, maybe you want to go to the 
+developer mailing list</a>
+
+If you just stumbled onto the developer pages
+by accident, maybe you want to go to the official
 <a href="http://www.mythtv.org/modules.php?name=MythInstall">%MythTV
-Installation page</a>.
+Installation page</a>. There is also a good unofficial 
+<a href="http://wilsonet.com/mythtv/fcmyth.php">Fedora %MythTV installation</a> page,
+and a 
+<a href="http://dev.gentoo.org/~cardoe/mythtv/">Gentoo %MythTV installation</a> page.
+
+If you are new to Qt programming it is essential that you keep in mind
+that almost all Qt objects are not thread-safe, including QString.
+Almost all Qt container objects, including QString, make shallow copies
+on assignment, the two copies of the object must only be used in one
+thread unless you use a lock on the object. You can use the 
+<a href="http://doc.trolltech.com/3.1/qdeepcopy.html">QDeepCopy</a>
+template on most Qt containers to make a copy you can use in another
+thread.
+
+There are also special dangers when 
+\ref qobject_dangers "using QObject" outside the Qt event thread.
 
 \section libs Libraries
 
 %MythTV is divided up into eight libraries:
 <dl>
-  <dt>libmyth                <dd>Core Myth library.
+  <dt>libmyth                <dd>%MythTV Plugin library.
       The \ref database_subsystem "database",
       \ref audio_subsystem "audio",
       \ref lcd_subsystem "LCD",
@@ -129,7 +146,7 @@ MythContext, MSqlDatabase, DBStorage, MDBManager, SimpleDBStorage.
 And one function UpgradeTVDatabaseSchema() located in dbcheck.cpp.
 
 MythTV's Configurable widgets also do save and restore their values
-from the database automagically when used in MythTV's window classes.
+from the database automagically when used in %MythTV's window classes.
  */
 
 /** \defgroup audio_subsystem       Audio Subsystem
@@ -246,5 +263,119 @@ state changing commands should not be issued.
 
  */
 
+/** \defgroup qobject_dangers QObject is dangerous for your health
 
+QObject derived classes can be quite useful, they can send and receive
+signals, get keyboard events, translate strings into another language
+and use QTimers.
 
+But they also can't be \ref qobject_delete "deleted" easily, they 
+are not \ref qobject_threadsafe "thread-safe", can't participate
+as equals in multiple \ref qobject_inheritence "inheritence", can
+not be used at all in virtual \ref qobject_inheritence "inheritence".
+
+\section qobject_delete Deleting QObjects
+
+The problem with deleting QObjects has to do with signals, and events.
+These can arrive from any thread, and often arrive from the Qt event
+thread even if you have not explicitly requested them.
+
+If you have not explicitly connected any signals or slots, and the 
+only events you might get are dispatched with qApp->postEvent(),
+not qApp->sendEvent(), then it is safe to delete the QObject in the
+Qt event thread. If your QObject is a GUI object you've passed 
+given to Qt for it to delete all is good. If your object is only 
+deleted in response to a posted event or a signal from the Qt event
+thread (say from a QTimer()) then you are OK as well.
+
+If your class may be deleted outside a Qt thread, but it does not
+explicitly connect any signals or slots, and the only events it
+might get are dispatched with qApp->postEvent(), then you can 
+safely use deleteLater() to queue your object up for deletion
+in the event thread.
+
+MythTV is a very much a multithreaded program so you may need to have
+a QObject that may get events from another thread if you send signals
+but do not get any signals and or events from outside the Qt event
+thread, then you can use deleteLater() or delete depending on whether
+you are deleteing the QObject in the event thread, see above. But
+if you are not in the Qt event thread then you need to call disconnect()
+in the thread that is sending the signals, before calling deleteLater().
+This prevents your object from sending events after you consider it 
+deleted (and begin to delete other QObjects that may still be getting
+signals from your object.)
+
+What about if you are getting events via qApp->sendEvent() or a signal
+from another thread? In this case things get complicated, so we highly
+discourage this in %MythTV and prefer that you use callbacks if at all
+possible. But in say you need to do this, then you need to disconnect
+all the signals being sent to your QObject. But disconnect() itself
+is not atomic. That is you may still get a signal after you disconnect
+it. What you need to is disconnect the signal from your slot in the 
+thread that is sending the signal. This prevents the signal from
+being emitted while you are disconnecting. Doing this gracefully is
+left as an excersize for the reader.
+
+Ok, so the above is not entirely complete, for instance you could wrap
+every signal emit in an instance lock and reimplement a disconnect
+that uses that instance lock. But if you've figured this or another
+solution out already all you need to know is that almost all Qt
+objects are reenterant but not thread-safe, and that QObjects recieve
+events and signals from the Qt event thread and from other threads
+if you use qApp->sendEvent() or send signals to it from another thread.
+
+\section qobject_threadsafe QObject thread-safety
+
+The only thread-safe classes in Qt are QThread and the mutex classes.
+when you call any non-static QObject method you must have a lock on
+it or make sure only thread ever calls that function.
+
+QObject itself is pretty safe because when you assign one QObject to
+another you make a copy. Qt's container classes, including QString
+do not make a copy but rather pass a reference counted pointer to
+the data when you assign one to another. This reference counter is
+not protected by a mutex so you can not use regular assignment when
+passing a QString or other Qt container from one thread to another.
+
+In order to use these classes safely you must use 
+<a href="http://doc.trolltech.com/3.1/qdeepcopy.html">QDeepCopy</a>
+when passing them from one thread to another. This is thankfully quite
+easy to use.
+
+\code
+  QString original = "hello world";
+  QString unsafe = original;
+  QString safe = QDeepCopy<QString>(original);
+\endcode
+
+In this case safe and original can be used by two seperate
+threads, while unsafe can only be used in the same thread 
+as originalStr.
+
+The QDeepCopy template will work on the most popular Qt containers,
+for the ones it doesn't support you will have to copy manually.
+
+\section qobject_inheritence QObject inheritence
+
+You can not inherit more than one QObject derived class.
+This is because of how signals and slots are implemented. What
+happens is that each slot is transformed into an integer that
+is used to find the right function pointer quickly when a signal
+is sent. But if you have more than one QObject in the inheritence
+tree the integers will alias so that signals intended for the
+second slot in parent class B might get sent to the second slot
+in parent class A instead. Badness ensues.
+
+For the similar reason you can not inherit a QObject derived
+class virtually, in this case the badness is a little more
+complicated. The result is the same however, signals arrive
+at the wrong slot. Usually, what happens is that the developer
+tries to inherit two QObject derived classes but gets linking
+errors, so they make one or more of the classes virtual, or
+inherit a QObject non-virtually but then also inherit one
+or more QObject derived classes virtually. The linker doesn't
+complain, but strange unexplainable behaviour ensues when
+the application is run and signals start showing up on the
+wrong slots.
+
+*/
