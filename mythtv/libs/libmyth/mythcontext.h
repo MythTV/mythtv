056fa146d3d69182de585fbabdafb361a398a0cb
Isaac Richards <irichards@mythtv.org>
2005-11-19 05:03:19 +0000

    Bump the library version, ABI was changed in [7916].
    git-svn-id: http://svn.mythtv.org/svn/trunk@7924 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/mythcontext.h
+++ mythtv/libs/libmyth/mythcontext.h
@@ -206,7 +206,7 @@ class MythPrivRequest
 
 /// Update this whenever the plug-in API changes.
 /// Including changes in the libmythtv class methods used by plug-ins.
-#define MYTH_BINARY_VERSION "0.19.20051109-1"
+#define MYTH_BINARY_VERSION "0.19.20051119-1"
 
 /** \brief Increment this whenever the MythTV network protocol changes.
  *
