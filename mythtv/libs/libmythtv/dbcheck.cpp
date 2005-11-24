db6300399d02fc1f3ae2176b98eadd4cfd0c12d6
Bruce Markey <bmarkey@mythtv.org>
2005-11-24 00:47:55 +0000

    Add generic column to recordedprogram.
    git-svn-id: http://svn.mythtv.org/svn/trunk@8029 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/dbcheck.cpp
+++ mythtv/libs/libmythtv/dbcheck.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #include "mythdbcon.h"
 
 /// This is the DB schema version expected by the running MythTV instance.
-const QString currentDatabaseVersion = "1109";
+const QString currentDatabaseVersion = "1110";
 
 static bool UpdateDBVersionNumber(const QString &newnumber);
 static bool performActualUpdate(const QString updates[], QString version,
@@ -2371,6 +2371,17 @@ QString("ALTER TABLE videosource ADD COLUMN freqtable VARCHAR(16) NOT NULL DEFAU
 };
 
         if (!performActualUpdate(updates, "1109", dbver))
+            return false;
+    }
+
+    if (dbver == "1109")
+    {
+        const QString updates[] = {
+"ALTER TABLE recordedprogram ADD COLUMN generic TINYINT(1) DEFAULT 0;",
+""
+};
+
+        if (!performActualUpdate(updates, "1110", dbver))
             return false;
     }
 
