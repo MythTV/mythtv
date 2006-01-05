3230e087f17dd47427e2d1e7296e31fbeaa29ce6
Chris Pinkham <cpinkham@mythtv.org>
2006-01-05 05:24:50 +0000

    Allow mc.sql to be processed more than once without error by changing the command to only create the mythconverg database if it does not exist already. Closes #938 by applying part of the patch.  Not committing the hints/comments included in the patch since the method for allowing remote access to the database is covered in the MythTV HOWTO and other places.
    git-svn-id: http://svn.mythtv.org/svn/trunk@8506 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/database/mc.sql
+++ mythtv/database/mc.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE mythconverg;
+CREATE DATABASE if not exists mythconverg;
 GRANT ALL ON mythconverg.* TO mythtv@localhost IDENTIFIED BY "mythtv";
 FLUSH PRIVILEGES;
 GRANT CREATE TEMPORARY TABLES ON mythconverg.* TO mythtv@localhost IDENTIFIED BY "mythtv";
