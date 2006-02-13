516b89309bb7c83b418eff8ecfd382fb96818852
Chris Pinkham <cpinkham@mythtv.org>
2006-02-13 04:30:15 +0000

    Switch a few places, where we open a new RingBuffer, over to using ProgramInfo::GetPlaybackURL() so the file can be read locally if available rather than having to be streamed from the backend.  This brings a few more places into consistency.  I think that NuppelVideoPlayer::JumpToProgram() and NuppelVideoPlayer::SwitchToProgram() could also possibly be switched over in order to allow LiveTV recordings to be read locally as well, but I am not changing those places with this commit.
    git-svn-id: http://svn.mythtv.org/svn/trunk@8935 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -249,7 +249,7 @@ TV::TV(void)
       browsechannum(""), browsechanid(""), browsestarttime(""),
       // Program Info for currently playing video
       recorderPlaybackInfo(NULL),
-      playbackinfo(NULL), inputFilename(""), playbackLen(0),
+      playbackinfo(NULL), playbackLen(0),
       lastProgram(NULL), jumpToProgram(false),
       // Video Players
       nvp(NULL), pipnvp(NULL), activenvp(NULL),
@@ -661,8 +661,6 @@ int TV::Playback(ProgramInfo *rcinfo)
     if (internalState != kState_None)
         return 0;
 
-    inputFilename = rcinfo->pathname;
-
     playbackLen = rcinfo->CalculateLength();
     playbackinfo = new ProgramInfo(*rcinfo);
 
@@ -846,9 +844,10 @@ void TV::HandleStateChange(void)
         }
         else
         {
-            tvchain->SetProgram(playbackinfo);
+            QString playbackURL = playbackinfo->GetPlaybackURL();
 
-            prbuffer = new RingBuffer(playbackinfo->pathname, false);
+            tvchain->SetProgram(playbackinfo);
+            prbuffer = new RingBuffer(playbackURL, false);
             prbuffer->SetLiveMode(tvchain);
         }
 
@@ -897,7 +896,9 @@ void TV::HandleStateChange(void)
     else if (TRANSITION(kState_None, kState_WatchingPreRecorded) ||
              TRANSITION(kState_None, kState_WatchingRecording))
     {
-        prbuffer = new RingBuffer(inputFilename, false);
+        QString playbackURL = playbackinfo->GetPlaybackURL();
+
+        prbuffer = new RingBuffer(playbackURL, false);
         if (prbuffer->IsOpen())
         {
             gContext->DisableScreensaver();
@@ -2812,8 +2813,10 @@ void TV::TogglePIPView(void)
         }
         else
         {
+            QString playbackURL = playbackinfo->GetPlaybackURL();
+
             piptvchain->SetProgram(playbackinfo);
-            piprbuffer = new RingBuffer(playbackinfo->pathname, false);
+            piprbuffer = new RingBuffer(playbackURL, false);
             piprbuffer->SetLiveMode(piptvchain);
         }
 
@@ -3478,8 +3481,10 @@ void TV::SwitchCards(uint chanid, QString channum)
         }
         else
         {
+            QString playbackURL = playbackinfo->GetPlaybackURL();
+
             tvchain->SetProgram(playbackinfo);
-            prbuffer = new RingBuffer(playbackinfo->pathname, false);
+            prbuffer = new RingBuffer(playbackURL, false);
             prbuffer->SetLiveMode(tvchain);
         }
 
