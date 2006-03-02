b48d3a10af36c7de525dbf87915342b8a1c55332
David Engel <dengel@mythtv.org>
2006-03-02 19:44:17 +0000

    Additional change that should have been part of r9243.
    git-svn-id: http://svn.mythtv.org/svn/trunk@9244 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythbackend/mainserver.cpp
+++ mythtv/programs/mythbackend/mainserver.cpp
@@ -1928,11 +1928,8 @@ void MainServer::HandleGetPendingRecordings(PlaybackSock *pbs, QString table, in
     if (m_sched) {
         if (table == "") m_sched->getAllPending(strList);
         else {
-            // We need a different connection from the scheduler proper
-            // DDCon exists, although it's designed for other purposes.
-            MSqlQueryInfo dbconn = MSqlQuery::DDCon();
-            Scheduler *sched = new Scheduler(false, encoderList,
-                                             table, &dbconn, m_sched);
+            Scheduler *sched = new Scheduler(false, encoderList, 
+                                             table, m_sched);
             sched->FillRecordListFromDB(recordid);
             sched->getAllPending(strList);
             delete sched;
