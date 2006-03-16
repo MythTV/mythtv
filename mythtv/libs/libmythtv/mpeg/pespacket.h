01fb94b578eb91e39d5e76c9a438377b9a425a45
Isaac Richards <irichards@mythtv.org>
2006-03-16 19:10:54 +0000

    Fix build for DVB.
    git-svn-id: http://svn.mythtv.org/svn/trunk@9380 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/mpeg/pespacket.h
+++ mythtv/libs/libmythtv/mpeg/pespacket.h
@@ -12,7 +12,7 @@
 extern "C" {
 #include "../libavcodec/avcodec.h"
 #include "../libavformat/avformat.h"
-#include "../libavformat/mpegts.h"
+#include "../libavutil/crc.h"
 }
 
 unsigned char *pes_alloc(uint size);
@@ -232,7 +232,7 @@ class PESPacket
     {
         if (Length() < 1)
             return 0xffffffff;
-        return mpegts_crc32(_pesdata, Length() - 1);
+        return av_crc(av_crc04C11DB7, (uint32_t)-1, _pesdata, Length() - 1);
     }
 
     void SetCRC(uint crc);
