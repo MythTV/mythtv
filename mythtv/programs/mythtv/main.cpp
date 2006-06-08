6a7d583926bb7344ceceb6ac2d3d013f6175c36a
Nigel Pearson <npearson@mythtv.org>
2006-06-08 23:17:16 +0000

    Local definition of gContext clashes with the one in libmyth. Mac OS X doesn't define an AudioOutputDevice, so the check is unwanted.
    git-svn-id: http://svn.mythtv.org/svn/trunk@10170 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythtv/main.cpp
+++ mythtv/programs/mythtv/main.cpp
@@ -16,8 +16,6 @@
 #include <iostream>
 using namespace std;
 
-MythContext *gContext;
-
 static void *run_priv_thread(void *data)
 {
     (void)data;
@@ -183,6 +181,9 @@ int main(int argc, char *argv[])
     
     gContext->LoadQtConfig();
 
+#if defined(Q_OS_MACX)
+    // Mac OS X doesn't define the AudioOutputDevice setting
+#else
     QString auddevice = gContext->GetSetting("AudioOutputDevice");
     if (auddevice == "" || auddevice == QString::null)
     {
@@ -190,6 +191,7 @@ int main(int argc, char *argv[])
                 "to run 'mythfrontend', not 'mythtv'.");
         return TV_EXIT_NO_AUDIO;
     }
+#endif
 
     MythMainWindow *mainWindow = GetMythMainWindow();
     mainWindow->Init();
