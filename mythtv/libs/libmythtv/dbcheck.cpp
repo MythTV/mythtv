398974f293b5bbcd170ea23dd728e722641512ca
Bruce Markey <bmarkey@mythtv.org>
2006-06-29 02:17:53 +0000

    Add new columns to recordedprogram .
    git-svn-id: http://svn.mythtv.org/svn/trunk@10347 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/dbcheck.cpp
+++ mythtv/libs/libmythtv/dbcheck.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #include "mythdbcon.h"
 
 /// This is the DB schema version expected by the running MythTV instance.
-const QString currentDatabaseVersion = "1147";
+const QString currentDatabaseVersion = "1148";
 
 static bool UpdateDBVersionNumber(const QString &newnumber);
 static bool performActualUpdate(const QString updates[], QString version,
@@ -2324,6 +2324,18 @@ static bool doUpgradeTVDatabaseSchema(void)
 };
 
         if (!performActualUpdate(updates, "1147", dbver))
+            return false;
+    }
+
+    if (dbver == "1147")
+    {
+        const QString updates[] = {
+"ALTER TABLE recordedprogram ADD first TINYINT(1) NOT NULL DEFAULT 0;",
+"ALTER TABLE recordedprogram ADD last  TINYINT(1) NOT NULL DEFAULT 0;",
+""
+};
+
+        if (!performActualUpdate(updates, "1148", dbver))
             return false;
     }
 
