0e88adf8e49d22c0d34ac65328d97601fdc98f03
Nigel Pearson <npearson@mythtv.org>
2006-08-17 06:07:51 +0000

    Intel OS X compile fixes from recent ffmpeg changes, and a similar change to x86 code in a filter.
    git-svn-id: http://svn.mythtv.org/svn/trunk@10798 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/filters/ivtc/pullup.c
+++ mythtv/filters/ivtc/pullup.c
@@ -21,7 +21,7 @@ static int diff_y_mmx(unsigned char *a, unsigned char *b, int s)
 		"pxor %%mm4, %%mm4 \n\t"
 		"pxor %%mm7, %%mm7 \n\t"
 		
-		".balign 16 \n\t"
+		ASMALIGN(4)
 		"1: \n\t"
 		
 		"movq (%%esi), %%mm0 \n\t"
@@ -70,7 +70,7 @@ static int licomb_y_mmx(unsigned char *a, unsigned char *b, int s)
 		"pxor %%mm7, %%mm7 \n\t"
 		"subl %%eax, %%edi \n\t"
 		
-		".balign 16 \n\t"
+		ASMALIGN(4)
 		"2: \n\t"
 
 		"movq (%%esi), %%mm0 \n\t"
@@ -159,7 +159,7 @@ static int var_y_mmx(unsigned char *a, unsigned char *b, int s)
 		"pxor %%mm4, %%mm4 \n\t"
 		"pxor %%mm7, %%mm7 \n\t"
 		
-		".balign 16 \n\t"
+		ASMALIGN(4)
 		"1: \n\t"
 		
 		"movq (%%esi), %%mm0 \n\t"
