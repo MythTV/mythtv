9f23e9091d0995356207d8541252fa4fcf2801e0
Daniel Kristjansson <dkristjansson@mythtv.org>
2006-10-02 17:52:21 +0000

    Refs #2171. Fix "crop" filter so it works with padded YUV strides.
    This only leaves "convert", the "force" filter works as is.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@11384 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/filters/crop/filter_crop.c
+++ mythtv/filters/crop/filter_crop.c
@@ -29,222 +29,279 @@ static const char FILTER_NAME[] = "crop";
 
 typedef struct ThisFilter
 {
-  VideoFilter vf;
+        VideoFilter vf;
 
-  int Chroma_plane_size;
-  int Luma_plane_size;
- 
-  int width;
-  int height;
+        int yp1, yp2, xp1, xp2;
 
-  int cropY1, cropY2;
-  int cropC1, cropC2, cropC3;
-  int xcrop1, xcrop2;
-  int xcropYint, xcropCint, xcropend;
-  TF_STRUCT;
+        TF_STRUCT;
 
 } ThisFilter;
 
 int crop(VideoFilter *f, VideoFrame *frame)
 {
-  ThisFilter *tf = (ThisFilter *)f;
-  uint64_t *buf=(uint64_t *)frame->buf;
-  int x,y;
-  const uint64_t Y_black=0x1010101010101010LL;
-  const uint64_t UV_black=0x8080808080808080LL;
-  TF_VARS;
-  
-  TF_START;
-  for(y = 0; y < tf->cropY1; y += 2) {// Y Luma
-    buf[y] = Y_black;
-    buf[y + 1] = Y_black;
-  }
+    ThisFilter *tf = (ThisFilter*) f;
+    uint64_t *ybuf = (uint64_t*) (frame->buf + frame->offsets[0]);
+    uint64_t *ubuf = (uint64_t*) (frame->buf + frame->offsets[1]);
+    uint64_t *vbuf = (uint64_t*) (frame->buf + frame->offsets[2]);
+    const uint64_t Y_black  = 0x1010101010101010LL; // 8 bytes
+    const uint64_t UV_black = 0x8080808080808080LL; // 8 bytes
+    int x, y, sz, t1, t2;
 
-  for(y = tf->cropY2; y < tf->Luma_plane_size; y += 2) {
-    buf[y] = Y_black;
-    buf[y + 1] = Y_black;
-  }
-  
-  for(y = tf->Luma_plane_size; y < tf->cropC1; y++) {// Y Chroma
-    buf[y] = UV_black;
-    buf[y + tf->Chroma_plane_size] = UV_black;
-  }
+    TF_VARS;
 
-  for(y = tf->cropC2; y < tf->cropC3; y++) {
-    buf[y] = UV_black;
-    buf[y + tf->Chroma_plane_size] = UV_black;
-  }
- 
-  for(y = tf->cropY1; y < tf->cropY2; y += tf->xcropYint) { // X Luma
-    for(x = 0; x <  tf->xcrop1; x++) {
-      buf[y + (x << 1)] = Y_black;
-      buf[y + (x << 1) + 1] = Y_black;
-    }
-    for(x = tf->xcrop2; x < tf->xcropend; x++) {
-      buf[y + (x << 1)] = Y_black;
-      buf[y + (x << 1) + 1] = Y_black;
-    }
-  }
+    TF_START;
 
-  for(y = tf->cropC1; y < tf->cropC2; y+= tf->xcropCint) { // X Chroma
-    for(x = 0; x <  tf->xcrop1; x++) {
-      buf[y + x] = UV_black;
-      buf[y + x + tf->Chroma_plane_size] = UV_black;
+    if (frame->pitches[1] != frame->pitches[2])
+        return -1;
+
+    // Luma top
+    sz = (frame->pitches[0] * frame->height) >> 3; // div 8 bytes
+    for (y = 0; (y < tf->yp1 * frame->pitches[0] << 1) && (y < sz); y += 2)
+    {
+        ybuf[y + 0] = Y_black;
+        ybuf[y + 1] = Y_black;
     }
-    for(x = tf->xcrop2; x < tf->xcropend; x++) {
-      buf[y + x] = UV_black;
-      buf[y + x + tf->Chroma_plane_size] = UV_black;
+
+    // Luma bottom
+    for (y = ((frame->height >> 4) - tf->yp2) * frame->pitches[0] << 1;
+         y < sz; y += 2)
+    {
+        ybuf[y + 0] = Y_black;
+        ybuf[y + 1] = Y_black;
     }
-  }
- 
-  TF_END(tf, "Crop: ");
-  return 0;
+
+    // Chroma top
+    sz = (frame->pitches[1] * (frame->height >> 1)) >> 3; // div 8 bytes
+    for (y = 0; (y < tf->yp1 * frame->pitches[1]) && (y < sz); y++)
+    {
+        ubuf[y] = UV_black;
+        vbuf[y] = UV_black;
+    }
+
+    // Chroma bottom
+    for (y = ((frame->height >> 4) - tf->yp2) * frame->pitches[1]; y < sz; y++)
+    {
+        ubuf[y] = UV_black;
+        vbuf[y] = UV_black;
+    }
+
+    // Luma left and right
+    sz = (frame->pitches[0] * frame->height) >> 3; // div 8 bytes
+    t1 = frame->pitches[0] << 1;
+    t2 = frame->pitches[0] >> 3;
+    for (y = tf->yp1 * t1;
+         (y < ((frame->height >> 4) - tf->yp2) * t1) && (y < sz); y += t2)
+    {
+        for (x = 0; (x < (tf->xp1 << 1)) && (x < t1); x += 2)
+        {
+            ybuf[y + x + 0] = Y_black;
+            ybuf[y + x + 1] = Y_black;
+        }
+
+        for (x = t2 - (tf->xp2 << 1); (x < t2) && (x < t1); x += 2)
+        {
+            ybuf[y + x + 0] = Y_black;
+            ybuf[y + x + 1] = Y_black;
+        }
+    }
+
+    // Chroma left and right
+    sz = (frame->pitches[1] * (frame->height >> 1)) >> 3; // div 8 bytes
+    t1 = (frame->pitches[1] * ((frame->height >> 4) - tf->yp2) << 2) >> 2;
+    t2 = frame->pitches[1] >> 3;
+    for (y = (frame->pitches[1] * tf->yp1) >> 1; (y < t1) && (y < sz); y += t2)
+    {
+        for (x = 0; x < tf->xp1; x++)
+        {
+            ubuf[y + x] = UV_black;
+            vbuf[y + x] = UV_black;
+        }
+
+        for (x = t2 - tf->xp2; x < t2; x++)
+        {
+            ubuf[y + x] = UV_black;
+            vbuf[y + x] = UV_black;
+        }
+    }
+
+    TF_END(tf, "Crop: ");
+    return 0;
 }
 
 #ifdef MMX
 int cropMMX(VideoFilter *f, VideoFrame *frame)
 {
-  ThisFilter *tf = (ThisFilter *)f;  
-  uint64_t *buf=(uint64_t *)frame->buf;
-  int y,x; 
-  const uint64_t Y_black=0x1010101010101010LL;
-  const uint64_t UV_black=0x8080808080808080LL;
-  TF_VARS;
+    ThisFilter *tf = (ThisFilter*) f;  
+    uint64_t *ybuf = (uint64_t*) (frame->buf + frame->offsets[0]);
+    uint64_t *ubuf = (uint64_t*) (frame->buf + frame->offsets[1]);
+    uint64_t *vbuf = (uint64_t*) (frame->buf + frame->offsets[2]);
+    const uint64_t Y_black  = 0x1010101010101010LL;
+    const uint64_t UV_black = 0x8080808080808080LL;
+    int x, y, sz, t1, t2;
 
-  TF_START;
+    TF_VARS;
 
-  asm volatile("movq (%1),%%mm0    \n\t"	       
-	       "movq (%0),%%mm1    \n\t"
-	       : : "r" (&UV_black), "r"(&Y_black));
+    TF_START;
+
+    if (frame->pitches[1] != frame->pitches[2])
+        return -1;
+
+    asm volatile("emms\n\t");
+
+    asm volatile("movq (%1),%%mm0    \n\t"	       
+                 "movq (%0),%%mm1    \n\t"
+                 : : "r" (&UV_black), "r"(&Y_black));
   
-  for(y = 0; y < tf->cropY1; y += 2) { // Y Luma
-    asm volatile("movq %%mm0, (%0)    \n\t"
-		 "movq %%mm0, 8(%0)    \n\t"
-		 : : "r" (buf + y));
-  }
+    // Luma top
+    sz = (frame->pitches[0] * frame->height) >> 3; // div 8 bytes
+    for (y = 0; (y < tf->yp1 * frame->pitches[0] << 1) && (y < sz); y += 2)
+    {
+        asm volatile("movq %%mm0, (%0)     \n\t"
+                     "movq %%mm0, 8(%0)    \n\t"
+                     : : "r" (ybuf + y));
+    }
 
-  for(y = tf->cropY2; y < tf->Luma_plane_size; y += 2) {
-    asm volatile("movq %%mm0, (%0)    \n\t"
-		 "movq %%mm0, 8(%0)    \n\t"
-		 : : "r" (buf + y));
-  }
+    // Luma bottom
+    for (y = ((frame->height >> 4) - tf->yp2) * frame->pitches[0] << 1;
+         y < sz; y += 2)
+    {
+        asm volatile("movq %%mm0, (%0)     \n\t"
+                     "movq %%mm0, 8(%0)    \n\t"
+                     : : "r" (ybuf + y));
+    }
 
-  for(y = tf->Luma_plane_size; y < tf->cropC1; y++) // Y Chroma
-    asm volatile("movq %%mm1, (%0)    \n\t"
-		 "movq %%mm1, (%1)    \n\t"
-		 : : "r" (buf + y), "r" (buf + y + tf->Chroma_plane_size));
+    // Chroma top
+    sz = (frame->pitches[1] * (frame->height >> 1)) >> 3; // div 8 bytes
+    for (y = 0; (y < tf->yp1 * frame->pitches[1]) && (y < sz); y++)
+    {
+        asm volatile("movq %%mm1, (%0)    \n\t"
+                     "movq %%mm1, (%1)    \n\t"
+                     : : "r" (ubuf + y), "r" (vbuf + y));
+    }
 
-  for(y = tf->cropC2; y < tf->cropC3; y++)
-    asm volatile("movq %%mm1, (%0)    \n\t"
-		 "movq %%mm1, (%1)    \n\t"
-		 : : "r" (buf + y), "r" (buf + y + tf->Chroma_plane_size));
+    // Chroma bottom
+    for (y = ((frame->height >> 4) - tf->yp2) * frame->pitches[1]; y < sz; y++)
+    {
+        asm volatile("movq %%mm1, (%0)    \n\t"
+                     "movq %%mm1, (%1)    \n\t"
+                     : : "r" (ubuf + y), "r" (vbuf + y));
+    }
  
-  for(y = tf->cropY1; y < tf->cropY2; y += tf->xcropYint) { // X Luma
-    for(x = 0; x < tf->xcrop1; x++)
-      asm volatile("movq %%mm0, (%0)    \n\t"
-		   "movq %%mm0, 8(%0)    \n\t"
-		   : : "r" (buf + y + (x << 1)));
-    for(x = tf->xcrop2; x < tf->xcropend; x++)
-      asm volatile("movq %%mm0, (%0)    \n\t"
-		   "movq %%mm0, 8(%0)    \n\t"
-		   : : "r" (buf + y + (x << 1)));
-  }
+    // Luma left and right
+    sz = (frame->pitches[0] * frame->height) >> 3; // div 8 bytes
+    t1 = frame->pitches[0] << 1;
+    t2 = frame->pitches[0] >> 3;
+    for (y = tf->yp1 * t1;
+         (y < ((frame->height >> 4) - tf->yp2) * t1) && (y < sz); y += t2)
+    {
+        for (x = 0; (x < (tf->xp1 << 1)) && (x < t1); x += 2)
+        {
+            asm volatile("movq %%mm0, (%0)     \n\t"
+                         "movq %%mm0, 8(%0)    \n\t"
+                         : : "r" (ybuf + y + x));
+        }
 
-  for(y = tf->cropC1; y < tf->cropC2; y += tf->xcropCint) { // X Chroma
-    for(x=0; x < tf->xcrop1; x++)
-      asm volatile("movq %%mm1, (%0)    \n\t"
-		   "movq %%mm1, (%1)    \n\t"
-		   : : "r" (buf + y + x), "r" (buf + y + x + tf->Chroma_plane_size));
-    for(x = tf->xcrop2; x < tf->xcropend; x++)
-      asm volatile("movq %%mm1, (%0)    \n\t"
-		   "movq %%mm1, (%1)    \n\t"
-		   : : "r" (buf + y + x), "r" (buf + y + x + tf->Chroma_plane_size));
-  }
+        for (x = t2 - (tf->xp2 << 1); (x < t2) && (x < t1); x += 2)
+        {
+            asm volatile("movq %%mm0, (%0)     \n\t"
+                         "movq %%mm0, 8(%0)    \n\t"
+                         : : "r" (ybuf + y + x));
+        }
+    }
 
-  asm volatile("emms\n\t");
+    // Chroma left and right
+    sz = (frame->pitches[1] * (frame->height >> 1)) >> 3; // div 8 bytes
+    t1 = (frame->pitches[1] * ((frame->height >> 4) - tf->yp2) << 2) >> 2;
+    t2 = frame->pitches[1] >> 3;
+    for (y = (frame->pitches[1] * tf->yp1) >> 1; (y < t1) && (y < sz); y += t2)
+    {
+        for (x = 0; x < tf->xp1; x++)
+        {
+            asm volatile("movq %%mm1, (%0)    \n\t"
+                         "movq %%mm1, (%1)    \n\t"
+                         : : "r" (ubuf + y + x), "r" (vbuf + y + x));
+        }
 
-  TF_END(tf, "Crop: ");
-  return 0;
+        for (x = t2 - tf->xp2; x < t2; x++)
+        {
+            asm volatile("movq %%mm1, (%0)    \n\t"
+                         "movq %%mm1, (%1)    \n\t"
+                         : : "r" (ubuf + y + x), "r" (vbuf + y + x));
+        }
+    }
+
+    asm volatile("emms\n\t");
+
+    TF_END(tf, "CropMMX: ");
+    return 0;
 }
 #endif /* MMX */
 
 VideoFilter *new_filter(VideoFrameType inpixfmt, VideoFrameType outpixfmt, 
                         int *width, int *height, char *options)
 {
-  unsigned int Param1, Param2, Param3, Param4;
-  ThisFilter *filter;
+    ThisFilter *filter;
 
-  int yp1,yp2,xp1,xp2;
+    (void) width;
+    (void) height;
 
-  if (inpixfmt != FMT_YV12 || outpixfmt != FMT_YV12)
+    if (inpixfmt != FMT_YV12 || outpixfmt != FMT_YV12)
     {
-      fprintf(stderr,"crop: attempt to initialize with unsupported format\n");
-      return NULL;
+        fprintf(stderr,
+                "crop: Attempt to initialize with unsupported format\n");
+
+        return NULL;
     }
 
-  filter = malloc(sizeof(ThisFilter));
-
-  if (filter == NULL)
+    filter = malloc(sizeof(ThisFilter));
+    if (filter == NULL)
     {
-      fprintf(stderr,"Couldn't allocate memory for filter\n");
-      return NULL;
+        fprintf(stderr, "crop: Couldn't allocate memory for filter\n");
+        return NULL;
     }
 
-  filter->Luma_plane_size = (*width * *height) / 8;
-  filter->Chroma_plane_size = (*height * *width / 4) / 8;
-  filter->cropC3 = filter->Luma_plane_size + filter->Chroma_plane_size;
-  filter->xcropend = *width / 16;
-  filter->xcropYint = *width / 8;
-  filter->xcropCint = *width / 16;
+    filter->yp1 = filter->yp2 = filter->xp1 = filter->xp2 = 1;
 
-  if (options && (sscanf(options, "%u:%u:%u:%u", &Param1, &Param2, &Param3, &Param4) == 4)) { 
-    yp1 = (uint8_t) Param1;
-    yp2 = (uint8_t) Param3;
-    xp1 = (uint8_t) Param2;
-    xp2 = (uint8_t) Param4;
-  }
-  else {
-    yp1 = 1;
-    yp2 = 1;
-    xp1 = 1;
-    xp2 = 1;
-  }
+    if (options)
+    {
+        unsigned int param1, param2, param3, param4;
+        if (sscanf(options, "%u:%u:%u:%u",
+                   &param1, &param2, &param3, &param4) == 4)
+        {
+            filter->yp1 = param1;
+            filter->yp2 = param3;
+            filter->xp1 = param2;
+            filter->xp2 = param4;
+        }
+    }
 
-  // linear addresses of 8-byte-block planes
-  filter->cropY1 = yp1 * *width * 2;
-  filter->cropY2 = ((*height/16) - yp2) * *width * 2;
-  filter->cropC1 = filter->Luma_plane_size+(*width * (yp1 * 4) / 8);
-  filter->cropC2 = filter->Luma_plane_size + ((*width * (*height/16 - yp2) * 4) / 8);
-  filter->xcrop1 = xp1;
-  filter->xcrop2 = (*width/16) - xp2;
-  
+    filter->vf.cleanup = NULL;
+    filter->vf.filter  = &crop;
+
 #ifdef MMX
-  if ( mm_support() & MM_MMX)
-    filter->vf.filter = &cropMMX;
-  else
+    if (mm_support() & MM_MMX)
+        filter->vf.filter = &cropMMX;
 #endif
-    filter->vf.filter = &crop;
 
-  filter->vf.cleanup = NULL;
-  TF_INIT(filter);
+    TF_INIT(filter);
 
-  return (VideoFilter *)filter;
+    return (VideoFilter*) filter;
 }
 
 static FmtConv FmtList[] = 
-  {
+{
     { FMT_YV12, FMT_YV12 },
     FMT_NULL
-  };
+};
 
 FilterInfo filter_table[] = 
-  {
+{
     {
-      symbol:     "new_filter",
-      name:       "crop",
-      descript:   "crops picture by macroblock intervals",
-      formats:    FmtList,
-      libname:    NULL
+        symbol:     "new_filter",
+        name:       "crop",
+        descript:   "crops picture by macroblock intervals",
+        formats:    FmtList,
+        libname:    NULL
     },
     FILT_NULL
-  };
+};
