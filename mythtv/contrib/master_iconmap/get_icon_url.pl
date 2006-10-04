91492e7559c3d284384a28b1d6c997d4146dcf97
Chris Petersen <cpetersen@mythtv.org>
2006-10-04 07:39:38 +0000

    some new channels from myself and kormoc, and split the 'get' stuff out to its own slightly more optimized script in prep for loading things into a web page
    git-svn-id: http://svn.mythtv.org/svn/trunk@11420 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ mythtv/contrib/master_iconmap/get_icon_url.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl -w
+#
+# Look up the logo icon URL for a specified callsign.
+#
+# @url       $URL$
+# @date      $Date$
+# @version   $Revision$
+# @author    $Author$
+# @license   GPL
+#
+
+# Data file locations
+    my $callsigntonetwork = 'data/callsigntonetwork.txt';
+    my $networktourl      = 'data/networktourl.txt';
+    my $baseurl           = 'data/baseurl.txt';
+
+# Includes
+    use Getopt::Long;
+
+# Variables
+    my $lookup; # Requested callsign to look up
+
+# Load the cli options
+    GetOptions('callsign|lookup=s' => \$lookup,
+               'help|h|usage'      => sub { print_help(); }
+              );
+
+# Nothing requested
+    print_help() unless ($lookup =~ /\w/);
+
+# All callsigns are stored in upper case
+    $lookup = uc($lookup);
+
+# Variables we will use
+    my $ignore  = '';
+    my $network = '';
+    my $url     = '';
+
+# First, we find the callsign
+    open(DATA, $callsigntonetwork) or die "Can't open $callsigntonetwork:  $!\n";
+    while (<DATA>) {
+        next unless (/^$lookup\s/);
+        chomp;
+        ($ignore, $network) = split(/\s+/, $_, 2);
+        last;
+    }
+    close DATA;
+
+# Leave early?
+    if (!$network) {
+        print "Unknown callsign:  $lookup\n";
+        exit;
+    }
+
+# On to the network
+    open(DATA, $networktourl) or die "Can't open $networktourl:  $!\n";
+    while (<DATA>) {
+        next unless (/^$network\s/);
+        chomp;
+        ($ignore, $url) = split(/\s+/, $_, 2);
+        last;
+    }
+    close DATA;
+
+# Leave early?
+    if (!$url) {
+        print "Unknown network:  $network\n";
+        exit;
+    }
+
+# Only load the stub list if we have to
+    if ($url =~ /\[.+?\]/) {
+        my %urls;
+        open(DATA, $baseurl) or die "Can't open $baseurl:  $!\n";
+        while (<DATA>) {
+            chomp;
+            next if (/^\s*#/ || /^\s*$/);
+            my ($stub, $url) = split(/\s+/, $_, 2);
+            $urls{$stub} = $url;
+        }
+        close DATA;
+    # Replace any URL stubs with the actual values
+        $url =~ s/\[(.+?)\]/$urls{$1}/sg;
+    }
+
+# Print the result (if there was one)
+    print "$url\n";
+
+# Done
+    exit;
+
+# Function
+    sub print_help {
+        print <<EOF;
+usage:  ./get_icon_url.pl [options]
+
+    --callsign=<callsign>
+
+        Look up a specific callsign and return the URL for its icon.
+
+    --help
+    --usage
+
+        Print this help message.
+
+EOF
+        exit;
+    }
+
