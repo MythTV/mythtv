ff0a79c548cc4647627b59135d2487e193e26b08
Daniel Kristjansson <dkristjansson@mythtv.org>
2007-01-12 00:35:36 +0000

    Fixes #2889. Fix from Michael Tiller & Michael Niedermayer for a division by zero in ffmpeg.
    Michael Tiller submitted this to ffmpeg and Michael Niedermayer has applied a version of this there (it is textually slightly different due to other changes in ffmpeg.)
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@12484 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libavutil/rational.c
+++ mythtv/libs/libavutil/rational.c
@@ -36,8 +36,10 @@ int av_reduce(int *dst_nom, int *dst_den, int64_t nom, int64_t den, int64_t max)
     int sign= (nom<0) ^ (den<0);
     int64_t gcd= ff_gcd(ABS(nom), ABS(den));
 
-    nom = ABS(nom)/gcd;
-    den = ABS(den)/gcd;
+    if(gcd){
+        nom = ABS(nom)/gcd;
+        den = ABS(den)/gcd;
+    }
     if(nom<=max && den<=max){
         a1= (AVRational){nom, den};
         den=0;
@@ -56,7 +58,7 @@ int av_reduce(int *dst_nom, int *dst_den, int64_t nom, int64_t den, int64_t max)
         nom= den;
         den= next_den;
     }
-    assert(ff_gcd(a1.num, a1.den) == 1);
+    assert(ff_gcd(a1.num, a1.den) <= 1U);
 
     *dst_nom = sign ? -a1.num : a1.num;
     *dst_den = a1.den;
