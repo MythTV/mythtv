2e9d8ce63a5725e4cf2cda2292e3802f5bca929f
Chris Petersen <cpetersen@mythtv.org>
2007-01-28 22:04:04 +0000

    forgot to update the current version to match my changes
    git-svn-id: http://svn.mythtv.org/svn/trunk@12668 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/dbcheck.cpp
+++ mythtv/libs/libmythtv/dbcheck.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #include "mythdbcon.h"
 
 /// This is the DB schema version expected by the running MythTV instance.
-const QString currentDatabaseVersion = "1177";
+const QString currentDatabaseVersion = "1178";
 
 static bool UpdateDBVersionNumber(const QString &newnumber);
 static bool performActualUpdate(const QString updates[], QString version,
