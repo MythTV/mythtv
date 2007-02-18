d7878bc2c167dc7f76bbb54b700d47d319d9a7de
Jarod Wilson <jwilson@mythtv.org>
2007-02-18 06:22:17 +0000

    Add support for new flac API introduced in 1.1.3. Rather insane to have a huge API change from 1.1.2 to 1.1.3, but whatever... At least the flac folks wrote some decent docs on porting to the new API... Builds cleanly against both flac 1.1.2 and 1.1.4, and basic sanity checks show it appears to be doing the right thing.
    Zero-effort back-port of [12810], which was actually
    more of a forward-port, since this code was originally
    built and tested on 0.20-fixes (these files hadn't
    changed in years). Relates #2957.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-20-fixes@12811 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythmusic/mythmusic/flacencoder.cpp
+++ mythplugins/mythmusic/mythmusic/flacencoder.cpp
@@ -9,7 +9,15 @@ using namespace std;
 #include "flacencoder.h"
 #include "metaioflacvorbiscomment.h"
 
-#include <FLAC/file_encoder.h>
+#include <FLAC/export.h>
+#if !defined(NEWFLAC)
+  /* FLAC 1.0.4 to 1.1.2 */
+  #include <FLAC/file_encoder.h>
+#else
+  /* FLAC 1.1.3 and up */
+  #include <FLAC/stream_encoder.h>
+#endif
+
 #include <FLAC/assert.h>
 #include <mythtv/mythcontext.h>
 
@@ -19,42 +27,47 @@ FlacEncoder::FlacEncoder(const QString &outfile, int qualitylevel,
 {
     sampleindex = 0;
 
-    int blocksize = 4608;
-    bool do_exhaustive_model_search = false;
-    bool do_escape_coding = false;
+    bool streamable_subset = true;
     bool do_mid_side = true;
     bool loose_mid_side = false;
-    int qlp_coeff_precision = 0;
-    int min_residual_partition_order = 3, max_residual_partition_order = 3;
-    int rice_parameter_search_dist = 0;
+    int bits_per_sample = 16;
+    int sample_rate = 44100;
+    int blocksize = 4608;
     int max_lpc_order = 8;
+    int qlp_coeff_precision = 0;
+    bool qlp_coeff_prec_search = false;
+    bool do_escape_coding = false;
+    bool do_exhaustive_model_search = false;
+    int min_residual_partition_order = 3;
+    int max_residual_partition_order = 3;
+    int rice_parameter_search_dist = 0;
 
-    encoder = FLAC__file_encoder_new();
+    encoder = encoder_new();
+    encoder_setup(encoder, streamable_subset, \
+                  do_mid_side, loose_mid_side, \
+                  NUM_CHANNELS, bits_per_sample, \
+                  sample_rate, blocksize, \
+                  max_lpc_order, qlp_coeff_precision, \
+                  qlp_coeff_prec_search, do_escape_coding, \
+                  do_exhaustive_model_search, \
+                  min_residual_partition_order, \
+                  max_residual_partition_order, \
+                  rice_parameter_search_dist);
 
-    FLAC__file_encoder_set_streamable_subset(encoder, true);
-    FLAC__file_encoder_set_do_mid_side_stereo(encoder, do_mid_side);
-    FLAC__file_encoder_set_loose_mid_side_stereo(encoder, loose_mid_side);
-    FLAC__file_encoder_set_channels(encoder, NUM_CHANNELS);
-    FLAC__file_encoder_set_bits_per_sample(encoder, 16);
-    FLAC__file_encoder_set_sample_rate(encoder, 44100);
-    FLAC__file_encoder_set_blocksize(encoder, blocksize);
-    FLAC__file_encoder_set_max_lpc_order(encoder, max_lpc_order);
-    FLAC__file_encoder_set_qlp_coeff_precision(encoder, qlp_coeff_precision);
-    FLAC__file_encoder_set_do_qlp_coeff_prec_search(encoder, false);
-    FLAC__file_encoder_set_do_escape_coding(encoder, do_escape_coding);
-    FLAC__file_encoder_set_do_exhaustive_model_search(encoder, 
-                                                    do_exhaustive_model_search);
-    FLAC__file_encoder_set_min_residual_partition_order(encoder, 
-                                                  min_residual_partition_order);
-    FLAC__file_encoder_set_max_residual_partition_order(encoder, 
-                                                  max_residual_partition_order);
-    FLAC__file_encoder_set_rice_parameter_search_dist(encoder, 
-                                                    rice_parameter_search_dist);
-
+#if !defined(NEWFLAC)
+    /* FLAC 1.0.4 to 1.1.2 */
     FLAC__file_encoder_set_filename(encoder, outfile.local8Bit());
 
     int ret = FLAC__file_encoder_init(encoder);
     if (ret != FLAC__FILE_ENCODER_OK)
+#else
+    /* FLAC 1.1.3 and up */
+    int ret = FLAC__stream_encoder_init_file(encoder,
+                           outfile.local8Bit(),
+                           NULL,
+                           NULL);
+    if(ret != FLAC__STREAM_ENCODER_INIT_STATUS_OK)
+#endif
     {
         VERBOSE(VB_GENERAL, QString("Error initializing FLAC encoder."
                                     " Got return code: %1").arg(ret));
@@ -70,8 +83,8 @@ FlacEncoder::~FlacEncoder()
 
     if (encoder)
     {
-        FLAC__file_encoder_finish(encoder);
-        FLAC__file_encoder_delete(encoder);
+        encoder_finish(encoder);
+        encoder_delete(encoder);
     }
 
     if (metadata)
@@ -102,9 +115,8 @@ int FlacEncoder::addSamples(int16_t *bytes, unsigned int length)
 
         if(sampleindex == MAX_SAMPLES || (length == 0 && sampleindex > 0) ) 
         {
-            if (!FLAC__file_encoder_process(encoder,
-                                            (const FLAC__int32 * const *) input,
-                                            sampleindex))
+            if (!encoder_process(encoder, (const FLAC__int32 * const *) input,
+                                 sampleindex))
             {
                 VERBOSE(VB_GENERAL, QString("Failed to write flac data."
                                             " Aborting."));
--- mythplugins/mythmusic/mythmusic/flacencoder.h
+++ mythplugins/mythmusic/mythmusic/flacencoder.h
@@ -4,7 +4,74 @@
 #include <qstring.h>
 
 #define HAVE_INTTYPES_H
-#include <FLAC/file_encoder.h>
+
+#include <FLAC/export.h>
+#if !defined(FLAC_API_VERSION_CURRENT) || FLAC_API_VERSION_CURRENT <= 7
+  /* FLAC 1.0.4 to 1.1.2 */
+  #include <FLAC/file_encoder.h>
+  #define encoder_new() FLAC__file_encoder_new()
+  #define encoder_setup(enc, streamable_subset, do_mid_side_stereo, \
+            loose_mid_side_stereo, channels, bits_per_sample, \
+            sample_rate, blocksize, max_lpc_order, \
+            qlp_coeff_precision, do_qlp_coeff_prec_search, \
+            do_escape_coding, do_exhaustive_model_search, \
+            min_residual_partition_order, max_residual_partition_order, \
+            rice_parameter_search_dist) \
+            { \
+              FLAC__file_encoder_set_streamable_subset(enc, streamable_subset) \
+              FLAC__file_encoder_set_do_mid_side_stereo(enc, do_mid_side_stereo) \
+              FLAC__file_encoder_set_loose_mid_side_stereo(enc, loose_mid_side_stereo) \
+              FLAC__file_encoder_set_channels(enc, channels) \
+              FLAC__file_encoder_set_bits_per_sample(enc, bits_per_sample) \
+              FLAC__file_encoder_set_sample_rate(enc, sample_rate) \
+              FLAC__file_encoder_set_blocksize(enc, blocksize) \
+              FLAC__file_encoder_set_max_lpc_order(enc, max_lpc_order) \
+              FLAC__file_encoder_set_qlp_coeff_precision(enc, qlp_coeff_precision) \
+              FLAC__file_encoder_set_do_qlp_coeff_prec_search(enc, do_qlp_coeff_prec_search) \
+              FLAC__file_encoder_set_do_escape_coding(enc, do_escape_coding) \
+              FLAC__file_encoder_set_do_exhaustive_model_search(enc, do_exhaustive_model_search) \
+              FLAC__file_encoder_set_min_residual_partition_order(enc, min_residual_partition_order) \
+              FLAC__file_encoder_set_max_residual_partition_order(enc, max_residual_partition_order) \
+              FLAC__file_encoder_set_rice_parameter_search_dist(enc, rice_parameter_search_dist) \
+            }
+  #define encoder_finish(enc) FLAC__file_encoder_finish(enc)
+  #define encoder_delete(enc) FLAC__file_encoder_delete(enc)
+  #define encoder_process(enc, data, index) FLAC__file_encoder_process(enc, data, index)
+  #define FLAC_ENCODER FLAC__FileEncoder
+#else 
+  /* FLAC 1.1.3 and up */
+  #define NEWFLAC
+  #include <FLAC/stream_encoder.h>
+  #define encoder_new() FLAC__stream_encoder_new()
+  #define encoder_setup(enc, streamable_subset, do_mid_side_stereo, \
+            loose_mid_side_stereo, channels, bits_per_sample, \
+            sample_rate, blocksize, max_lpc_order, \
+            qlp_coeff_precision, do_qlp_coeff_prec_search, \
+            do_escape_coding, do_exhaustive_model_search, \
+            min_residual_partition_order, max_residual_partition_order, \
+            rice_parameter_search_dist) \
+            { \
+              FLAC__stream_encoder_set_streamable_subset(enc, streamable_subset); \
+              FLAC__stream_encoder_set_do_mid_side_stereo(enc, do_mid_side_stereo); \
+              FLAC__stream_encoder_set_loose_mid_side_stereo(enc, loose_mid_side_stereo); \
+              FLAC__stream_encoder_set_channels(enc, channels); \
+              FLAC__stream_encoder_set_bits_per_sample(enc, bits_per_sample); \
+              FLAC__stream_encoder_set_sample_rate(enc, sample_rate); \
+              FLAC__stream_encoder_set_blocksize(enc, blocksize); \
+              FLAC__stream_encoder_set_max_lpc_order(enc, max_lpc_order); \
+              FLAC__stream_encoder_set_qlp_coeff_precision(enc, qlp_coeff_precision); \
+              FLAC__stream_encoder_set_do_qlp_coeff_prec_search(enc, do_qlp_coeff_prec_search); \
+              FLAC__stream_encoder_set_do_escape_coding(enc, do_escape_coding); \
+              FLAC__stream_encoder_set_do_exhaustive_model_search(enc, do_exhaustive_model_search); \
+              FLAC__stream_encoder_set_min_residual_partition_order(enc, min_residual_partition_order); \
+              FLAC__stream_encoder_set_max_residual_partition_order(enc, max_residual_partition_order); \
+              FLAC__stream_encoder_set_rice_parameter_search_dist(enc, rice_parameter_search_dist); \
+            }
+  #define encoder_finish(enc) FLAC__stream_encoder_finish(enc)
+  #define encoder_delete(enc) FLAC__stream_encoder_delete(enc)
+  #define encoder_process(enc, data, index) FLAC__stream_encoder_process(enc, data, index)
+  #define FLAC_ENCODER FLAC__StreamEncoder
+#endif
 
 #include "encoder.h"
 
@@ -21,7 +88,7 @@ class FlacEncoder : public Encoder
     int addSamples(int16_t *bytes, unsigned int len);
 
   private:
-    FLAC__FileEncoder *encoder;
+    FLAC_ENCODER *encoder;
     unsigned int sampleindex;
     FLAC__int32 inputin[NUM_CHANNELS][MAX_SAMPLES];
     FLAC__int32 *input[NUM_CHANNELS];
