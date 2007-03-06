4ff37aae2ff345506617d8617527e394bfd1f6a1
Chris Pinkham <cpinkham@mythtv.org>
2007-03-06 04:08:07 +0000

    Carry over [12944] from trunk.
    Don't allow decrementing inSearch below 0.  Previously this could happen
    if UseArrowAccels was turned off.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-20-fixes@12945 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/progfind.cpp
+++ mythtv/libs/libmythtv/progfind.cpp
@@ -516,11 +516,9 @@ void ProgFinder::update_timeout()
 
 void ProgFinder::cursorLeft()
 {
-    inSearch--;
-    if (inSearch == -1 && arrowAccel)
-        escape();
-    else
+    if (inSearch > 0)
     {
+        inSearch--;
         if (inSearch == 0)
             showSearchList();
         else if (inSearch == 1)
@@ -529,6 +527,9 @@ void ProgFinder::cursorLeft()
             clearShowData();
         }
     }
+    else if (inSearch == 0 && arrowAccel)
+        escape();
+
     update(infoRect);
     update(listRect);
 }
