0d0f668eafcd8fee3e54eeda9b5e1f0ae94de01c
Stuart Morgan <smorgan@mythtv.org>
2007-04-16 21:50:49 +0000

    Refs #3131
    Backports the mythmainwindow.cpp part of [13260] to -fixes.
    
    Removes unnecessary escaping of backslashes and double quotes in MythMainWindow::RegisterKey and MythMainWindow::RegisterJump
    
    The DB update isn't included because it's not practical to backport schema changes.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-20-fixes@13261 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythui/mythmainwindow.cpp
+++ mythtv/libs/libmythui/mythmainwindow.cpp
@@ -814,8 +814,6 @@ void MythMainWindow::RegisterKey(const QString &context, const QString &action,
         else
         {
             QString inskey = keybind;
-            inskey.replace('\\', "\\\\");
-            inskey.replace('\"', "\\\"");
 
             query.prepare("INSERT INTO keybindings (context, action, "
                           "description, keylist, hostname) VALUES "
@@ -936,8 +934,6 @@ void MythMainWindow::RegisterJump(const QString &destination,
         else
         {
             QString inskey = keybind;
-            inskey.replace('\\', "\\\\");
-            inskey.replace('\"', "\\\"");
 
             query.prepare("INSERT INTO jumppoints (destination, description, "
                           "keylist, hostname) VALUES ( :DEST, :DESC, :KEYLIST, "
