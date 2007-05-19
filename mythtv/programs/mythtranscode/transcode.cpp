0998045aa5b020ab8530d39aa375895db024d2e5
Chris Pinkham <cpinkham@mythtv.org>
2007-05-19 06:21:05 +0000

    Reset frame.buf when necessary in the transcoder.  This should fix an issue with segfaults when the dimensions of the input video change over the course of the video.
    Also reuse the calculated frame size instead of recalculating when allocating
    the frame buffer.
    
    Patch by Bryan Mayland.
    
    Closes #3464.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@13467 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythtranscode/transcode.cpp
+++ mythtv/programs/mythtranscode/transcode.cpp
@@ -664,7 +664,7 @@ int Transcode::TranscodeFile(char *inputname, char *outputname,
     VideoOutput *videoOutput = nvp->getVideoOutput();
     bool is_key = 0;
     bool first_loop = true;
-    unsigned char *newFrame = new unsigned char[newWidth * newHeight * 3 / 2];
+    unsigned char *newFrame = new unsigned char[frame.size];
 
     frame.buf = newFrame;
     AVPicture imageIn, imageOut;
@@ -853,6 +853,7 @@ int Transcode::TranscodeFile(char *inputname, char *outputname,
                 }
                 else
                 {
+                    frame.buf = newFrame;
                     avpicture_fill(&imageIn, lastDecode->buf, PIX_FMT_YUV420P,
                                    video_width, video_height);
                     avpicture_fill(&imageOut, frame.buf, PIX_FMT_YUV420P,
@@ -905,6 +906,7 @@ int Transcode::TranscodeFile(char *inputname, char *outputname,
             }
             else
             {
+                frame.buf = newFrame;
                 avpicture_fill(&imageIn, lastDecode->buf, PIX_FMT_YUV420P,
                                video_width, video_height);
                 avpicture_fill(&imageOut, frame.buf, PIX_FMT_YUV420P,
