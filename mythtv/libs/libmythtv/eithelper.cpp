bc3715eae8d308ba47e9ba9b3b57a80e5f69bffe
Janne Grunau <jgrunau@mythtv.org>
2007-05-20 09:31:03 +0000

    Closes #3457. Carries [13469] over from trunk. Pro7/Sat.1 declares finally their EIT text encoding
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-20-fixes@13470 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/eithelper.cpp
+++ mythtv/libs/libmythtv/eithelper.cpp
@@ -535,11 +535,6 @@ static void init_fixup(QMap<uint64_t,uint> &fix)
     fix[ 4096 << 16] = EITFixUp::kFixAUStar;
     fix[ 4096 << 16] = EITFixUp::kFixAUStar;
 
-    fix[ 3074LL << 32 | 8468 << 16] = EITFixUp::kEFixPro7Sat; // DVB-T Hamburg
-    fix[ 3075LL << 32 | 8468 << 16] = EITFixUp::kEFixPro7Sat; // DVB-T Bremen
-    fix[ 8705LL << 32 | 8468 << 16] = EITFixUp::kEFixPro7Sat; // DVB-T Hessen
-    fix[13057LL << 32 | 8468 << 16] = EITFixUp::kEFixPro7Sat; // DVB-T Munich
-
     // DVB-C germany: Kabel Deutschland encoding fixes
     fix[   112LL << 32 | 61441U << 16] = EITFixUp::kEFixPro7Sat;
     fix[ 10000LL << 32 | 61441U << 16] = EITFixUp::kEFixPro7Sat;
