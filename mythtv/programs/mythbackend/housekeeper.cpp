18521715a24f9d26b42f43a6773430e92691cb12
Chris Pinkham <cpinkham@mythtv.org>
2007-05-28 05:15:49 +0000

    Allow the user to set a mythfilldatabase run period where the max hour is less than the min hour, for instance if they want the run period to be from 8PM till 4AM.
    This is loosely based on a patch by WP van Paassen in #3310.  I shortened
    it up a bit, and removed a part unecessary with current SVN head.
    While I was in the housekeeper, I also converted a few things like
    now.toString(QString("d")).toInt() to a much cleaner now.date().day().
    
    Closes #3310.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@13524 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythbackend/housekeeper.cpp
+++ mythtv/programs/mythbackend/housekeeper.cpp
@@ -50,7 +50,7 @@ bool HouseKeeper::wantToRun(const QString &dbTag, int period, int minhour,
 {
     bool runOK = false;
     unsigned int oneday = 60 * 60 * 24;
-    unsigned int longEnough = 0;
+    int longEnough = 0;
 
     if (period)
         longEnough = ((period * oneday) - oneday/2);
@@ -65,11 +65,6 @@ bool HouseKeeper::wantToRun(const QString &dbTag, int period, int minhour,
         minhour = 0;
     if (maxhour > 23)
         maxhour = 23;
-    if (minhour > maxhour)
-    {
-        VERBOSE(VB_GENERAL,
-            "Housekeeping thread will not run: supplied time range is empty");
-    }
 
     MSqlQuery result(MSqlQuery::InitCon());
     if (result.isConnected())
@@ -82,14 +77,16 @@ bool HouseKeeper::wantToRun(const QString &dbTag, int period, int minhour,
             result.next();
             lastrun = result.value(0).toDateTime();
 
-            if ((now.toTime_t() - lastrun.toTime_t()) > longEnough &&
-                (now.toString(QString("d")).toInt() !=
-                     lastrun.toString(QString("d")).toInt()))
+            if ((lastrun.secsTo(now) > longEnough) &&
+                (now.date().day() != lastrun.date().day()))
             {
-                int hour = now.toString(QString("h")).toInt();
-                if ((hour >= minhour) && (hour <= maxhour))
+                int hour = now.time().hour();
+
+                if (((minhour > maxhour) &&
+                     ((hour <= maxhour) || (hour >= minhour))) ||
+                    ((hour >= minhour) && (hour <= maxhour)))
                 {
-                    int minute = now.toString(QString("m")).toInt();
+                    int minute = now.time().minute();
                     if ((hour == maxhour && minute > 30) ||
                         ((random()%(((maxhour-hour)*12+(60-minute)/5 - 6) + 1)) == 0))
                         runOK = true;
