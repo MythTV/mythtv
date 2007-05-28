ef017db37b842417d1a47131df34fbe4e1defd2b
Chris Pinkham <cpinkham@mythtv.org>
2007-05-28 04:38:24 +0000

    Revert the libavcodec/mpegaudiodec.c patch I made in [13511] in favor of the 1-line fix that the ffmpeg guys made in their SVN tree after I contacted them. This way we don't have to merge in the change later when we resync again.
    I'm leaving in an if statement to squelch most of the "incorrect frame size"
    messages since these will occur very very frequently when playing an old .nuv
    file that was transcoded with mythtranscode before I committed [13510].  We
    can remove this if statement at a later time if we want to or maintain it in
    our libavcodec tree for a while if we want so people's logs don't fill up with
    these warnings.
    
    References #2972.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@13523 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libavcodec/mpegaudiodec.c
+++ mythtv/libs/libavcodec/mpegaudiodec.c
@@ -2550,7 +2550,6 @@ static int decode_frame(AVCodecContext * avctx,
     MPADecodeContext *s = avctx->priv_data;
     uint32_t header;
     int out_size;
-    int result;
     OUT_INT *out_samples = data;
 
 retry:
@@ -2593,20 +2592,23 @@ retry:
         av_log(avctx, AV_LOG_ERROR, "incomplete frame\n");
         return -1;
     }else if(s->frame_size < buf_size){
+        // FIXME, This 'if' statement can be taken out at some point in the
+        // future.  For now, it prevents people's logs filling up with these
+        // errors when they play .nuv files created by the old mythtranscode
+        if ((buf_size % s->frame_size) != 0)
         av_log(avctx, AV_LOG_ERROR, "incorrect frame size\n");
+        buf_size= s->frame_size;
     }
 
-    out_size = mp_decode_frame(s, out_samples, buf, s->frame_size);
+    out_size = mp_decode_frame(s, out_samples, buf, buf_size);
     if(out_size>=0){
         *data_size = out_size;
         avctx->sample_rate = s->sample_rate;
         //FIXME maybe move the other codec info stuff from above here too
     }else
         av_log(avctx, AV_LOG_DEBUG, "Error while decoding MPEG audio frame.\n"); //FIXME return -1 / but also return the number of bytes consumed
-
-    result = s->frame_size;
     s->frame_size = 0;
-    return result;
+    return buf_size;
 }
 
 static void flush(AVCodecContext *avctx){
