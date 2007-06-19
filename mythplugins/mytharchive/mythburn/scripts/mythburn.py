19375f220136f648af28bbfaca86679cc0c57316
Paul Harrison <pharrison@mythtv.org>
2007-06-19 12:11:59 +0000

    Apply patch to fix multipass encoding in MythArchive.
    Refs #3626.
    
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-20-fixes@13737 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mytharchive/mythburn/scripts/mythburn.py
+++ mythplugins/mytharchive/mythburn/scripts/mythburn.py
@@ -1366,9 +1366,9 @@ def encodeVideoToMPEG2(source, destvideofile, video, audio1, audio2, aspectratio
                        "Command was %s" % command)
 
     else:
-        passLog = os.path.join(getTempPath(), 'pass.log')
+        passLog = os.path.join(getTempPath(), 'pass')
 
-        pass1 = string.replace(command, "%pass","1")
+        pass1 = string.replace(command, "%passno","1")
         pass1 = string.replace(pass1, "%passlogfile", passLog)
         write("Pass 1 - " + pass1)
         result = runCommand(pass1)
@@ -1380,7 +1380,7 @@ def encodeVideoToMPEG2(source, destvideofile, video, audio1, audio2, aspectratio
         if os.path.exists(destvideofile):
             os.remove(destvideofile)
 
-        pass2 = string.replace(command, "%pass","2")
+        pass2 = string.replace(command, "%passno","2")
         pass2 = string.replace(pass2, "%passlogfile", passLog)
         write("Pass 2 - " + pass2)
         result = runCommand(pass2)
