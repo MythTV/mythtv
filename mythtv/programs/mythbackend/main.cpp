20f998b6174af174c7f6f74d37294c4397b499db
Stuart Morgan <smorgan@mythtv.org>
2007-07-04 14:33:19 +0000

    Change wording of the --version output.
    git-svn-id: http://svn.mythtv.org/svn/trunk@13819 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythbackend/main.cpp
+++ mythtv/programs/mythbackend/main.cpp
@@ -403,7 +403,7 @@ int main(int argc, char **argv)
             extern const char *myth_source_path;
             cout << "Library API version     : " << MYTH_BINARY_VERSION << endl;
             cout << "Source code version     : " << myth_source_version << endl;
-            cout << "Source code path        : " << myth_source_path << endl;
+            cout << "SVN branch              : " << myth_source_path << endl;
             cout << "Network Protocol Version: " << MYTH_PROTO_VERSION << endl;
 #ifdef MYTH_BUILD_CONFIG
             cout << "Options compiled in:" <<endl;
