b47c404ccafa4c1f6189697919cce47e4e7611f2
Daniel Kristjansson <dkristjansson@mythtv.org>
2007-07-19 22:38:30 +0000

    Refs #3710. Fixes error handling in ffmpeg file.
    I've just sent a version of this patch to ffmpeg. Until this get's fixed upstream I'm applying this patch here. If we fail in alloc_picture() after successfully calling s->avctx->get_buffer we call s->avctx->release_buffer so that we don't leak buffers like crazy.
    
    The underlying problem is that ffmpeg isn't picking up on the resolution change, this will be fixed separately.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@14016 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libavcodec/mpegvideo.c
+++ mythtv/libs/libavcodec/mpegvideo.c
@@ -379,30 +379,35 @@ static int alloc_picture(MpegEncContext *s, Picture *pic, int shared){
     const int b8_array_size= s->b8_stride*s->mb_height*2;
     const int b4_array_size= s->b4_stride*s->mb_height*4;
     int i;
+    int r= -1;
 
     if(shared){
         assert(pic->data[0]);
         assert(pic->type == 0 || pic->type == FF_BUFFER_TYPE_SHARED);
         pic->type= FF_BUFFER_TYPE_SHARED;
     }else{
-        int r;
-
         assert(!pic->data[0]);
 
         r= s->avctx->get_buffer(s->avctx, (AVFrame*)pic);
 
         if(r<0 || !pic->age || !pic->type || !pic->data[0]){
             av_log(s->avctx, AV_LOG_ERROR, "get_buffer() failed (%d %d %d %p)\n", r, pic->age, pic->type, pic->data[0]);
+            if(r>=0)
+                s->avctx->release_buffer(s->avctx, (AVFrame*)pic);
             return -1;
         }
 
         if(s->linesize && (s->linesize != pic->linesize[0] || s->uvlinesize != pic->linesize[1])){
             av_log(s->avctx, AV_LOG_ERROR, "get_buffer() failed (stride changed)\n");
+            if(r>=0)
+                s->avctx->release_buffer(s->avctx, (AVFrame*)pic);
             return -1;
         }
 
         if(pic->linesize[1] != pic->linesize[2]){
             av_log(s->avctx, AV_LOG_ERROR, "get_buffer() failed (uv stride mismatch)\n");
+            if(r>=0)
+                s->avctx->release_buffer(s->avctx, (AVFrame*)pic);
             return -1;
         }
 
@@ -451,6 +456,8 @@ static int alloc_picture(MpegEncContext *s, Picture *pic, int shared){
 
     return 0;
 fail: //for the CHECKED_ALLOCZ macro
+    if(r>=0)
+        s->avctx->release_buffer(s->avctx, (AVFrame*)pic);
     return -1;
 }
 
