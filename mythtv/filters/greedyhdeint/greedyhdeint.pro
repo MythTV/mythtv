64e0ae45da92ed6001ec4123926a30e91abceeee
Nigel Pearson <npearson@mythtv.org>
2007-12-05 01:04:15 +0000

    Minor mac OS X compile fixes
    git-svn-id: http://svn.mythtv.org/svn/trunk@15056 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/filters/greedyhdeint/greedyhdeint.pro
+++ mythtv/filters/greedyhdeint/greedyhdeint.pro
@@ -5,3 +5,5 @@ INCLUDEPATH += ../../libs/libmythtv ../../libs/libavcodec ../..
 
 # Input
 SOURCES += filter_greedyhdeint.c color.c
+
+macx : QMAKE_LFLAGS += -read_only_relocs warning
--- mythtv/filters/greedyhdeint/mangle.h
+++ mythtv/filters/greedyhdeint/mangle.h
@@ -10,7 +10,7 @@
 /* Feel free to add more to the list, eg. a.out IMO */
 /* Use rip-relative addressing if compiling PIC code on x86-64. */
 #if defined(__CYGWIN__) || defined(__MINGW32__) || defined(__OS2__) || \
-   (defined(__OpenBSD__) && !defined(__ELF__))
+    defined(CONFIG_DARWIN) || (defined(__OpenBSD__) && !defined(__ELF__))
 #if defined(ARCH_X86_64) && defined(PIC)
 #define MANGLE(a) "_" #a"(%%rip)"
 #else
