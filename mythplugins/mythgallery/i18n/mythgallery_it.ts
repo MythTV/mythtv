013d15fc328665ec879892602371b45838d52c43
Nigel Pearson <npearson@mythtv.org>
2008-01-29 09:03:41 +0000

    Updated translation from Guido Grazioli. Closes #3714
    git-svn-id: http://svn.mythtv.org/svn/trunk@15660 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythgallery/i18n/mythgallery_it.ts
+++ mythplugins/mythgallery/i18n/mythgallery_it.ts
@@ -3,59 +3,59 @@
     <name>IconView</name>
     <message>
         <source>Import pictures?</source>
-        <translation type="unfinished"></translation>
+        <translation>Importa le foto?</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>No</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Si</translation>
     </message>
     <message>
         <source>Nothing found to import</source>
-        <translation type="unfinished"></translation>
+        <translation>Niente da importare</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Errore</translation>
     </message>
     <message>
         <source>Sorry: OpenGL support not available</source>
-        <translation type="unfinished"></translation>
+        <translation>Supporto OpenGL non disponibile</translation>
     </message>
     <message>
         <source>SlideShow</source>
-        <translation type="unfinished"></translation>
+        <translation>Slideshow</translation>
     </message>
     <message>
         <source>Rotate CW</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ruota DX</translation>
     </message>
     <message>
         <source>Rotate CCW</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ruota SX</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Importa</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Opzioni</translation>
     </message>
     <message>
         <source>Random</source>
-        <translation type="unfinished"></translation>
+        <translation>Casuale</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Elimina</translation>
     </message>
     <message>
         <source>MythGallery Directory &apos;%1&apos; does not exist or is unreadable.</source>
@@ -71,194 +71,194 @@
     </message>
     <message>
         <source>Meta Data...</source>
-        <translation type="unfinished"></translation>
+        <translation>Metadati...</translation>
     </message>
     <message>
         <source>Marking...</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleziona...</translation>
     </message>
     <message>
         <source>File...</source>
-        <translation type="unfinished"></translation>
+        <translation>File...</translation>
     </message>
     <message>
         <source>Return</source>
-        <translation type="unfinished"></translation>
+        <translation>Chiudi</translation>
     </message>
     <message>
         <source>Clear Marked</source>
-        <translation type="unfinished"></translation>
+        <translation>Nessuno</translation>
     </message>
     <message>
         <source>Select All</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutti</translation>
     </message>
     <message>
         <source>Show Devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Dispositivi</translation>
     </message>
     <message>
         <source>Copy here</source>
-        <translation type="unfinished"></translation>
+        <translation>Copia qui</translation>
     </message>
     <message>
         <source>Move here</source>
-        <translation type="unfinished"></translation>
+        <translation>Sposta qui</translation>
     </message>
     <message>
         <source>Create Dir</source>
-        <translation type="unfinished"></translation>
+        <translation>Crea cartella</translation>
     </message>
     <message>
         <source>Delete Current File or Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Elimina la foto o la cartella corrente</translation>
     </message>
     <message>
         <source>Deleting 1 folder, including any subfolders and files.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Eliminazione di 1 cartella, incluse tutte le sottocartelle e files.</translation>
     </message>
     <message>
         <source>Deleting 1 image.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Eliminazione di 1 immagine.</translation>
     </message>
     <message>
         <source>Delete Marked Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Elimina selezionati</translation>
     </message>
     <message>
         <source>Deleting %1 images and folders, including any subfolders and files.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Eliminazione di %1 immagini e cartelle, incluse le sottocartelle.</translation>
     </message>
     <message>
         <source>New Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuova cartella</translation>
     </message>
     <message>
         <source>Create New Folder</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Crea nuova cartella</translation>
     </message>
     <message>
         <source>Moving marked images...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Spostamento in corso...</translation>
     </message>
     <message>
         <source>Copying marked images...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Copia in corso...</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Directory that holds images</source>
-        <translation type="unfinished"></translation>
+        <translation>Cartella contenente le immagini</translation>
     </message>
     <message>
         <source>This directory must exist and MythGallery needs to have read permission.</source>
-        <translation type="unfinished"></translation>
+        <translation>La cartella deve esistere e MythGallery deve avere i permessi di lettura su di essa.</translation>
     </message>
     <message>
         <source>Paths to import images from</source>
-        <translation type="unfinished"></translation>
+        <translation>Percorsi per importazione</translation>
     </message>
     <message>
         <source>Type of transition</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo di transizione</translation>
     </message>
     <message>
         <source>Type of background</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo di sfondo</translation>
     </message>
     <message>
         <source>This is the number of seconds to display each picture.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il numero di secondi per cui mostrare una immagine.</translation>
     </message>
     <message>
         <source>This is a colon separated list of paths. If the path in the list is a directory, its contents will be copied. If it is an executable, it will be run.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lista di percorsi separati da virgola. Se il percorso è una cartella, ne vengono copiati i contenuti. Se è un comando eseguibile, verrà eseguito.</translation>
     </message>
     <message>
         <source>This is the type of transition used between pictures in slideshow mode.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il tipo di transizione usato tra le immagini in modalità slideshow.</translation>
     </message>
     <message>
         <source>This is the type of background for each picture in single view mode.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il tipo di sfondo usato per ogni immagine in modalità visualizzazione singola.</translation>
     </message>
     <message>
         <source>Slideshow Delay</source>
-        <translation type="unfinished"></translation>
+        <translation>Ritardo Slideshow</translation>
     </message>
     <message>
         <source>Use OpenGL transitions</source>
-        <translation type="unfinished"></translation>
+        <translation>Usa transizioni OpenGL</translation>
     </message>
     <message>
         <source>Check this to enable OpenGL based slideshow transitions</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Abilita le transizioni OpenGL in modalità slideshow.</translation>
     </message>
     <message>
         <source>Type of OpenGL transition</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo di transizione OpenGL</translation>
     </message>
     <message>
         <source>This is the type of OpenGL transition used between pictures in slideshow mode.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tipo di transizione OpenGL usato tra le immagini in modalità slideshow.</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Ok</translation>
     </message>
     <message>
         <source>Command run to display movie files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Comando eseguito per i filmati</translation>
     </message>
     <message>
         <source>This command is executed whenever a movie file is selected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Setta il comando da eseguire quando è selezionato un filmato.</translation>
     </message>
     <message>
         <source>Store thumbnails in image directory</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Salva le miniature nella cartella immagini</translation>
     </message>
     <message>
         <source>If set, thumbnails are stored in &apos;.thumbcache&apos; directories within the above directory. If cleared, they are stored in your home directory.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Se selezionato, le anteprime saranno salvate in sottocartelle &apos;.thumbcache&apos; delle cartelle che contengono le immagini. Altrimenti saranno salvate nella home directory.</translation>
     </message>
     <message>
         <source>Recurse into directories</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Sfoglia le sottocartelle</translation>
     </message>
     <message>
         <source>Check this to let the slideshow recurse into sub-directories.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Quando selezionato, la modalità slideshow mostrerà la immagini contenute nelle sottocartelle.</translation>
     </message>
     <message>
         <source>Overlay caption</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Didascalia</translation>
     </message>
     <message>
         <source>This is the number of seconds to show a caption on top of a full size picture.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Il numero di secondi per cui mostrare una didascalia in sovraimpressione a una immagine a pieno schermo.</translation>
     </message>
     <message>
         <source>Sort order when browsing</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ordinamento anteprime</translation>
     </message>
     <message>
         <source>This is the sort order for the displayed picture thumbnails.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">La modalità di ordinamento delle anteprime delle immagini.</translation>
     </message>
     <message>
         <source>Duration of OpenGL Transition (milliseconds)</source>
-        <translation type="unfinished"></translation>
+        <translation>Durata delle transizioni OpenGL (millisecondi)</translation>
     </message>
     <message>
         <source>MythGallery Settings (Slideshow)</source>
-        <translation type="unfinished"></translation>
+        <translation>Opzioni galleria foto (Slideshow)</translation>
     </message>
     <message>
         <source>MythGallery Settings (General)</source>
-        <translation type="unfinished"></translation>
+        <translation>Opzioni galleria foto (Generale)</translation>
     </message>
 </context>
 </TS>
