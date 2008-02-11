d260734d829de7ced89d851205762c6ba334ddca
Jarod Wilson <jwilson@mythtv.org>
2008-02-11 05:31:57 +0000

    Build fails for me on OS X w/o this include
    git-svn-id: http://svn.mythtv.org/svn/trunk@15904 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythfreesurround/freesurround.h
+++ mythtv/libs/libmythfreesurround/freesurround.h
@@ -19,6 +19,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef FREESURROUND_H
 #define FREESURROUND_H
 
+#include <sys/types.h>
+
 class FreeSurround
 {
 public:
