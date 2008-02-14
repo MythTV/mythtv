f244267f6dd3f83ec19d62866d99433cd5f49274
Nigel Pearson <npearson@mythtv.org>
2008-02-14 01:41:46 +0000

    MinGW compile fixes. See #1104, [16001] and [16005]
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-21-fixes@16008 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/audiooutputdigitalencoder.cpp
+++ mythtv/libs/libmyth/audiooutputdigitalencoder.cpp
@@ -304,7 +304,7 @@ static int encode_frame(
 
     //uint32_t x = *(uint32_t*)(data+8);
     // in place swab
-    swab((const unsigned char*)(data + 8), data + 8, enc_len);
+    swab((const char *)data + 8, (char *)data + 8, enc_len);
     //VERBOSE(VB_AUDIO|VB_TIMESTAMP, 
     //        QString("DigitalEncoder::Encode swab test %1 %2")
     //        .arg(x,0,16).arg(*(uint32_t*)(data+8),0,16));
