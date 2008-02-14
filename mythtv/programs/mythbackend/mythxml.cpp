a9fb2865f2b13f3f9cdd064a7f0764bedc5091e5
Stuart Morgan <smorgan@mythtv.org>
2008-02-14 19:57:06 +0000

    Merge [16020] and [16021] from trunk.
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-21-fixes@16023 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythbackend/mythxml.cpp
+++ mythtv/programs/mythbackend/mythxml.cpp
@@ -1032,7 +1032,7 @@ void MythXML::GetPreviewImage( HTTPRequest *pRequest )
 
     if ( pInfo->hostname != gContext->GetHostName())
     {
-        // We only handle requests for local resources   
+        // We only handle requests for local resources
 
         delete pInfo;
 
@@ -1075,10 +1075,14 @@ void MythXML::GetPreviewImage( HTTPRequest *pRequest )
         {
             pRequest->m_eResponseType   = ResponseTypeFile;
             pRequest->m_nResponseStatus = 404;
+            previewgen->deleteLater();
+            delete pInfo;
             return;
         }
         previewgen->deleteLater();
     }
+
+    delete pInfo;
 
     pRequest->m_eResponseType   = ResponseTypeFile;
     pRequest->m_nResponseStatus = 200;
@@ -1094,7 +1098,10 @@ void MythXML::GetPreviewImage( HTTPRequest *pRequest )
         fAspect = (float)(pImage->width()) / pImage->height();
 
     if (fAspect == 0)
+    {
+        delete pImage;
         return;
+    }
 
     if ( nWidth == 0 )
         nWidth = (int)rint(nHeight * fAspect);
@@ -1114,8 +1121,13 @@ void MythXML::GetPreviewImage( HTTPRequest *pRequest )
     // check to see if scaled preview image is already created.
     // ----------------------------------------------------------------------
 
+    
+
     if (QFile::exists( pRequest->m_sFileName ))
+    {
+        delete pImage;
         return;
+    }
 
     QImage img = pImage->smoothScale( nWidth, nHeight);
 
