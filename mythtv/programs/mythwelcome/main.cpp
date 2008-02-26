cae5c6a0592a74980b84906bd58dca81aff38978
Paul Harrison <pharrison@mythtv.org>
2008-02-26 22:44:00 +0000

    Fix MythWelcome not de-activating the screensaver on key/remote presses. Unfortunately this does mean the return of the pesky warning dialogs if MythWelcome starts up before mythbackend is ready to accept connections.
    Lesser of two evils I think.
    
    Fixes #4771.
    
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-21-fixes@16279 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythwelcome/main.cpp
+++ mythtv/programs/mythwelcome/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char **argv)
 
     gContext = NULL;
     gContext = new MythContext(MYTH_BINARY_VERSION);
-    if (!gContext->Init(false)) 
+    if (!gContext->Init()) 
     {
         VERBOSE(VB_IMPORTANT, "mythwelcome: Could not initialize myth context. "
                         "Exiting.");
