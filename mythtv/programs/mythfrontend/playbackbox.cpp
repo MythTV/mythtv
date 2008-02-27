d088dde007df7fd03f0f36c0c7d68941bca627e7
Chris Pinkham <cpinkham@mythtv.org>
2008-02-27 05:12:15 +0000

    Bugfix from Yeechang Lee to the PageTop/Middle/Bottom code in PlaybackBox.
    This commit still doesn't add default keybindings for these since we have
    been trying not to add new defaults for optional items such as this because
    of the large number of existing actions and jumppoints that already exist.
    We prefer to leave optional actions like this up to the user to bind rather
    than risk potential conflict with an existing binding which the user may have
    configured.
    
    Closes #4451.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-21-fixes@16286 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythfrontend/playbackbox.cpp
+++ mythtv/programs/mythfrontend/playbackbox.cpp
@@ -1686,6 +1686,7 @@ void PlaybackBox::pageTop()
         paintSkipUpdate = false;
         update(drawListBounds);
         update(drawInfoBounds);
+        update(blackholeBounds);
     }
 }
 
@@ -1702,7 +1703,7 @@ void PlaybackBox::pageMiddle()
     }
     else 
     {
-        int progCount = progLists[titleList[titleIndex]].count();
+        int progCount = progLists[titleList[titleIndex].lower()].count();
 
         if (progCount > 0)
         {
@@ -1711,6 +1712,7 @@ void PlaybackBox::pageMiddle()
             paintSkipUpdate = false;
             update(drawListBounds);
             update(drawInfoBounds);
+            update(blackholeBounds);
         }
     }
 }
@@ -1721,11 +1723,12 @@ void PlaybackBox::pageBottom()
         pageTop();
     else 
     {
-        progIndex = progLists[titleList[titleIndex]].count() - 1;
+        progIndex = progLists[titleList[titleIndex].lower()].count() - 1;
 
         paintSkipUpdate = false;
         update(drawListBounds);
         update(drawInfoBounds);
+        update(blackholeBounds);
     }
 }
 
