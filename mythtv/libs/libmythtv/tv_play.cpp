668c94296907adfad9cd592eca3d5232b0d40552
Chris Pinkham <cpinkham@mythtv.org>
2008-03-22 03:28:48 +0000

    Fix a regression caused by [15805] which caused playback to continue on when using the FINDER keybinding to bring up the Program Finder during playback of a recording.  This patch causes playback to be paused when going into the finder if we are not paused already.
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-21-fixes@16743 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -5880,6 +5880,8 @@ void TV::doEditSchedule(int editType)
             break;
         }
         case kScheduleProgramFinder:
+            if (!paused)
+                DoPause(false);
             RunProgramFind(false, false);
             break;
         case kScheduledRecording:
