769e89ae71f2ba39e10b1d30448da96c51a0e674
Nigel Pearson <npearson@mythtv.org>
2008-09-20 00:47:06 +0000

    Whitespace changes to shrink future changes.
    git-svn-id: http://svn.mythtv.org/svn/trunk@18338 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/backendselect.h
+++ mythtv/libs/libmyth/backendselect.h
@@ -14,62 +14,60 @@ class DatabaseParams;
 
 class ListBoxDevice : public Q3ListBoxText
 {
-    public:
+  public:
 
-        ListBoxDevice(Q3ListBox *list, const QString &name, DeviceLocation *dev)
-            : Q3ListBoxText(list, name)
-        {
-            if ((m_dev = dev) != NULL)
-                m_dev->AddRef();
-        }
+    ListBoxDevice(Q3ListBox *list, const QString &name, DeviceLocation *dev)
+        : Q3ListBoxText(list, name)
+    {
+        if ((m_dev = dev) != NULL)
+            m_dev->AddRef();
+    }
 
-        virtual ~ListBoxDevice()
-        {
-            if (m_dev != NULL)
-                m_dev->Release();
-        }
+    virtual ~ListBoxDevice()
+    {
+        if (m_dev != NULL)
+            m_dev->Release();
+    }
 
-        DeviceLocation *m_dev;
+    DeviceLocation *m_dev;
 };
 
-typedef QMap< QString, ListBoxDevice *> ItemMap;
+typedef QMap <QString, ListBoxDevice *> ItemMap;
 
 
 class BackendSelect : public MythDialog 
 {
     Q_OBJECT
 
-    public:
-                 BackendSelect(MythMainWindow *parent, DatabaseParams *params);
-        virtual ~BackendSelect();
+  public:
+             BackendSelect(MythMainWindow *parent, DatabaseParams *params);
+    virtual ~BackendSelect();
 
-        void     customEvent(QEvent *e);
+    void     customEvent(QEvent *e);
 
-        QString  m_PIN;
-        QString  m_USN;
+    QString  m_PIN;
+    QString  m_USN;
 
 
-    public slots:
-
-        void Accept    (void);   ///< Linked to the OK button
-        void Manual    (void);   ///< Linked to 'Configure Manually' button
+  public slots:
+    void Accept    (void);   ///< Linked to the OK button
+    void Manual    (void);   ///< Linked to 'Configure Manually' button
 #ifdef SEARCH_BUTTON
-        void Search    (void);
+    void Search    (void);
 #endif
 
 
-    protected:
+  protected:
+    void AddItem    (DeviceLocation *dev);
+    bool Connect    (DeviceLocation *dev);
+    void CreateUI   (void);
+    void FillListBox(void);
+    void RemoveItem (QString URN);
 
-        void AddItem    (DeviceLocation *dev);
-        bool Connect    (DeviceLocation *dev);
-        void CreateUI   (void);
-        void FillListBox(void);
-        void RemoveItem (QString URN);
-
-        DatabaseParams *m_DBparams;
-        ItemMap         m_devices;
-        MythMainWindow *m_parent;
-        MythListBox    *m_backends;
+    DatabaseParams *m_DBparams;
+    ItemMap         m_devices;
+    MythMainWindow *m_parent;
+    MythListBox    *m_backends;
 };
 
 #endif
