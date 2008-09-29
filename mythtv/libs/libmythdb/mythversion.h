80932c06ec0e625caf65e7a8003adbdb94599f24
Nigel Pearson <npearson@mythtv.org>
2008-09-29 11:47:20 +0000

    Changeset [18478] slightly changed the method for MythThemedDialog
    git-svn-id: http://svn.mythtv.org/svn/trunk@18482 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythdb/mythversion.h
+++ mythtv/libs/libmythdb/mythversion.h
@@ -8,7 +8,7 @@
 /// Update this whenever the plug-in API changes.
 /// Including changes in the libmyth, libmythtv and libmythui class methods
 /// used by plug-ins.
-#define MYTH_BINARY_VERSION "0.22.20080928-2"
+#define MYTH_BINARY_VERSION "0.22.20080929-1"
 
 /** \brief Increment this whenever the MythTV network protocol changes.
  *
