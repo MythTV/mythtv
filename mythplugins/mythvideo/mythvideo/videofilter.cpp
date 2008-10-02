8e44c099e9924ecd7cf0bc06a69ef3ba69bdbde7
Anduin Withers <awithers@mythtv.org>
2008-10-02 16:46:13 +0000

    References #5756
    Fixes kRuntimeFilterUnknown brokenness, ignores Metadata field/representation
    mismatch.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@18534 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythvideo/mythvideo/videofilter.cpp
+++ mythplugins/mythvideo/mythvideo/videofilter.cpp
@@ -289,7 +289,7 @@ bool VideoFilterSettings::matches_filter(const Metadata &mdata) const
     {
         if (runtime == kRuntimeFilterUnknown)
         {
-            matches = mdata.Length() < 0;
+            matches = mdata.Length() == 0;
         }
         else
         {
