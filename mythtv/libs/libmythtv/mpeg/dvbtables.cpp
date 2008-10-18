185e4360139c0c06cd4e8209b158a76aa25e52a9
Janne Grunau <jgrunau@mythtv.org>
2008-10-18 21:58:37 +0000

    DVB EIT uses UTC, QDateTime in Qt4 needs explicitly set to UTC, fixes EIT times after libmythtv Qt4 port
    git-svn-id: http://svn.mythtv.org/svn/trunk@18763 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/mpeg/dvbtables.cpp
+++ mythtv/libs/libmythtv/mpeg/dvbtables.cpp
@@ -190,6 +190,7 @@ QDateTime dvbdate2qt(const unsigned char *buf)
     if (mjd >= 40587)
     {
         QDateTime result;
+        result.setTimeSpec(Qt::UTC);
  	// Modified Julian date as number of days since 17th November 1858.
  	// 1st Jan 1970 was date 40587.
  	uint secsSince1970 = (mjd - 40587)   * 86400;
@@ -220,7 +221,7 @@ QDateTime dvbdate2qt(const unsigned char *buf)
     QTime time(byteBCD2int(buf[2]), byteBCD2int(buf[3]),
                byteBCD2int(buf[4]));
 
-    return QDateTime(date, time);
+    return QDateTime(date, time, Qt::UTC);
 }
 
 /** \fn dvbdate2unix(const unsigned char*)
