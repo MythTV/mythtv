5196799693beae05228cb50323b70ff7934d87c1
Isaac Richards <irichards@mythtv.org>
2009-01-21 00:39:57 +0000

    Move some stuff out of the mythvirtualkeyboard header to speed compilation elsewhere.
    git-svn-id: http://svn.mythtv.org/svn/trunk@19769 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythui/mythvirtualkeyboard.h
+++ mythtv/libs/libmythui/mythvirtualkeyboard.h
@@ -15,65 +15,12 @@ enum PopupPosition
     VK_POSCENTERDIALOG
 };
 
-typedef struct KeyDefinition
+struct KeyDefinition
 {
     QString name;
     QString type;
     QString normal, shift, alt, altshift;
     QString up, down, left, right;
-};
-
-const int numcomps = 95;
-
-const QString comps[numcomps][3] = {
-        {"!", "!", (QChar)0xa1},    {"c", "/", (QChar)0xa2},
-        {"l", "-", (QChar)0xa3},    {"o", "x", (QChar)0xa4},
-        {"y", "-", (QChar)0xa5},    {"|", "|", (QChar)0xa6},
-        {"s", "o", (QChar)0xa7},    {"\"", "\"", (QChar)0xa8},
-        {"c", "o", (QChar)0xa9},    {"-", "a", (QChar)0xaa},
-        {"<", "<", (QChar)0xab},    {"-", "|", (QChar)0xac},
-        {"-", "-", (QChar)0xad},    {"r", "o", (QChar)0xae},
-        {"^", "-", (QChar)0xaf},    {"^", "0", (QChar)0xb0},
-        {"+", "-", (QChar)0xb1},    {"^", "2", (QChar)0xb2},
-        {"^", "3", (QChar)0xb3},    {"/", "/", (QChar)0xb4},
-        {"/", "u", (QChar)0xb5},    {"P", "!", (QChar)0xb6},
-        {"^", ".", (QChar)0xb7},    {",", ",", (QChar)0xb8},
-        {"^", "1", (QChar)0xb9},    {"_", "o", (QChar)0xba},
-        {">", ">", (QChar)0xbb},    {"1", "4", (QChar)0xbc},
-        {"1", "2", (QChar)0xbd},    {"3", "4", (QChar)0xbe},
-        {"?", "?", (QChar)0xbf},    {"A", "`", (QChar)0xc0},
-        {"A", "'", (QChar)0xc1},    {"A", "^", (QChar)0xc2},
-        {"A", "~", (QChar)0xc3},    {"A", "\"", (QChar)0xc4},
-        {"A", "*", (QChar)0xc5},    {"A", "E", (QChar)0xc6},
-        {"C", ",", (QChar)0xc7},    {"E", "`", (QChar)0xc8},
-        {"E", "'", (QChar)0xc9},    {"E", "^", (QChar)0xca},
-        {"E", "\"", (QChar)0xcb},   {"I", "`", (QChar)0xcc},
-        {"I", "'", (QChar)0xcd},    {"I", "^", (QChar)0xce},
-        {"I", "\"", (QChar)0xcf},   {"D", "-", (QChar)0xd0},
-        {"N", "~", (QChar)0xd1},    {"O", "`", (QChar)0xd2},
-        {"O", "'", (QChar)0xd3},    {"O", "^", (QChar)0xd4},
-        {"O", "~", (QChar)0xd5},    {"O", "\"", (QChar)0xd6},
-        {"x", "x", (QChar)0xd7},    {"O", "/", (QChar)0xd8},
-        {"U", "`", (QChar)0xd9},    {"U", "'", (QChar)0xda},
-        {"U", "^", (QChar)0xdb},    {"U", "\"", (QChar)0xdc},
-        {"Y", "'", (QChar)0xdd},    {"T", "H", (QChar)0xde},
-        {"s", "s", (QChar)0xdf},    {"a", "`", (QChar)0xe0},
-        {"a", "'", (QChar)0xe1},    {"a", "^", (QChar)0xe2},
-        {"a", "~", (QChar)0xe3},    {"a", "\"", (QChar)0xe4},
-        {"a", "*", (QChar)0xe5},    {"a", "e", (QChar)0xe6},
-        {"c", ",", (QChar)0xe7},    {"e", "`", (QChar)0xe8},
-        {"e", "'", (QChar)0xe9},    {"e", "^", (QChar)0xea},
-        {"e", "\"", (QChar)0xeb},   {"i", "`", (QChar)0xec},
-        {"i", "'", (QChar)0xed},    {"i", "^", (QChar)0xee},
-        {"i", "\"", (QChar)0xef},   {"d", "-", (QChar)0xf0},
-        {"n", "~", (QChar)0xf1},    {"o", "`", (QChar)0xf2},
-        {"o", "'", (QChar)0xf3},    {"o", "^", (QChar)0xf4},
-        {"o", "~", (QChar)0xf5},    {"o", "\"", (QChar)0xf6},
-        {"-", ":", (QChar)0xf7},    {"o", "/", (QChar)0xf8},
-        {"u", "`", (QChar)0xf9},    {"u", "'", (QChar)0xfa},
-        {"u", "^", (QChar)0xfb},    {"u", "\"", (QChar)0xfc},
-        {"y", "'", (QChar)0xfd},    {"t", "h", (QChar)0xfe},
-        {"y", "\"", (QChar)0xff}
 };
 
 class MPUBLIC MythUIVirtualKeyboard : public MythScreenType
