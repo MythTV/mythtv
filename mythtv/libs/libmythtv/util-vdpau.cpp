MythTV/mythtv

commit 1368186f5f4180a311aa5d1a731d80a5ef6be931

Small VDPAU PiP fixes. Delete the PiP Layer when we have no more PiPs and ensure we do not try to use an OpenGL PiP if VDPAU PiP fails.
git-svn-id: http://svn.mythtv.org/svn/trunk@19828 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/util-vdpau.cpp
@@ -2015,6 +2015,12 @@ void VDPAUContext::DeinitPIP(NuppelVideoPlayer *pipplayer)
 
     pips.remove(pipplayer);
     VERBOSE(VB_PLAYBACK, LOC + "Removed 1 PIP");
+
+    if (pips.size() < 1)
+    {
+        DeinitPIPLayer();
+        VERBOSE(VB_PLAYBACK, LOC + "Removed PIP Layer");
+    }
 }
 
 bool VDPAUContext::InitPIP(NuppelVideoPlayer *pipplayer,

mythtv/libs/libmythtv/videoout_xv.cpp
@@ -3701,9 +3701,9 @@ void VideoOutputXv::ShowPIP(VideoFrame        *frame,
     QRect dvr = GetTotalVisibleRect();
  
 #ifdef USING_VDPAU
-    if (vdpau && vdpau_use_pip && VideoOutputSubType() == XVideoVDPAU)
+    if (vdpau && VideoOutputSubType() == XVideoVDPAU)
     {
-        if (vdpau->InitPIPLayer(dvr.size()))
+        if (vdpau_use_pip && vdpau->InitPIPLayer(dvr.size()))
             vdpau_use_pip = vdpau->ShowPIP(pipplayer, pipimage,
                                            position, pipActive);
         pipplayer->ReleaseCurrentFrame(pipimage);

commit 33ca7b799449c45b76606c41ee85635096eb22b3

Fix a regression in the VDPAU PiP code introduced with r19828. If we had one PiP, the pip layer was being deleted when the pip itself was re-initialised.
git-svn-id: http://svn.mythtv.org/svn/trunk@19920 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/util-vdpau.cpp
@@ -2028,7 +2028,7 @@ bool VDPAUContext::InitPIPLayer(QSize screen_size)
     return ok;
 }
 
-void VDPAUContext::DeinitPIP(NuppelVideoPlayer *pipplayer)
+void VDPAUContext::DeinitPIP(NuppelVideoPlayer *pipplayer, bool check_layer)
 {
     if (!pips.contains(pipplayer))
         return;
@@ -2042,7 +2042,7 @@ void VDPAUContext::DeinitPIP(NuppelVideoPlayer *pipplayer)
     pips.remove(pipplayer);
     VERBOSE(VB_PLAYBACK, LOC + "Removed 1 PIP");
 
-    if (pips.size() < 1)
+    if (pips.size() < 1 && check_layer)
     {
         DeinitPIPLayer();
         VERBOSE(VB_PLAYBACK, LOC + "Removed PIP Layer");
@@ -2129,7 +2129,7 @@ bool VDPAUContext::ShowPIP(NuppelVideoPlayer *pipplayer,
     if (pips.contains(pipplayer) &&
         pips[pipplayer].videoSize != vid_size)
     {
-        DeinitPIP(pipplayer);
+        DeinitPIP(pipplayer, false);
     }
 
     if (!pips.contains(pipplayer))

mythtv/libs/libmythtv/util-vdpau.h
@@ -68,7 +68,7 @@ class VDPAUContext
     bool ShowPIP(NuppelVideoPlayer *pipplayer,
                  VideoFrame * frame, QRect position,
                  bool pip_is_active);
-    void DeinitPIP(NuppelVideoPlayer *pipplayer);
+    void DeinitPIP(NuppelVideoPlayer *pipplayer, bool check_layer = true);
 
   private:
     bool InitProcs(Display *disp, int screen);