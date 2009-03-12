c5505554dc51400be2e2e97fb1a58bc32671d5c6
Anduin Withers <awithers@mythtv.org>
2009-03-12 00:26:11 +0000

    Closes #6364
    Fixes issues with TMDB I know about and can reproduce, guess fix multiple
    results bug.
    
    Made it look better, to me.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@20185 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythvideo/mythvideo/scripts/tmdb.pl
+++ mythplugins/mythvideo/mythvideo/scripts/tmdb.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
 
-#
 # This perl script is intended to perform movie data lookups based on
 # the themoviedb.org website
 #
@@ -12,7 +11,13 @@
 # v0.1
 # - Initial script
 
+#TODO: use TMDB IDs and fallback to assuming imdb ID rather than the
+# current way.
+
+use strict;
+
 use File::Basename;
+use URI::Escape;
 use lib dirname($0);
 
 use MythTV::MythVideoCommon;
@@ -23,105 +28,139 @@ use vars qw($opt_h $opt_r $opt_d $opt_i $opt_v $opt_D $opt_M $opt_P $opt_B);
 use Getopt::Std;
 use Data::Dumper;
 use LWP::Simple;
+#use XML::Simple qw(:strict);
 use XML::Simple;
 
-$title = "themoviedb Query";
-$version = "v0.1.0";
-$author = "William Stewart, Stuart Morgan";
+my $title = "themoviedb Query";
+my $version = "v0.1.1";
+my $author = "William Stewart, Stuart Morgan";
 push(@MythTV::MythVideoCommon::URL_get_extras, ($title, $version));
 
 my @countries = qw(USA UK Canada Japan);
 
-my $base_url       = "http://api.themoviedb.org/2.0/";
-my $posterimdb_url = "http://api.themoviedb.org/cover.php?imdb=";
+my $base_url       = "http://api.themoviedb.org/2.0";
+#my $posterimdb_url = "http://api.themoviedb.org/cover.php?imdb=";
 
 # themoviedb.org api key given by Travis Bell for Mythtv
-my $api_key        = "c27cb71cff5bd76e1a7a009380562c62";
+my $API_KEY        = "c27cb71cff5bd76e1a7a009380562c62";
 
 binmode(STDOUT, ":utf8");
 
 # display usage
 sub usage {
-print "usage: $0 -hdrviMPD [parameters]\n";
-print "       -h           help\n";
-print "       -d           debug\n";
-print "       -r           dump raw query result data only\n";
-print "       -v           display version\n";
-print "       -i           display info\n";
-print "\n";
-print "       -M <query>    get movie list\n";
-print "       -P <movieid>  get movie poster\n";
-print "       -B <movieid>  get movie backdrop\n";
-print "       -D <movieid>  get movie data\n";
-exit(-1);
+    print "usage: $0 -hdrviMPD [parameters]\n";
+    print "       -h           help\n";
+    print "       -d           debug\n";
+    print "       -r           dump raw query result data only\n";
+    print "       -v           display version\n";
+    print "       -i           display info\n";
+    print "\n";
+    print "       -M <query>    get movie list\n";
+    print "       -P <movieid>  get movie poster\n";
+    print "       -B <movieid>  get movie backdrop\n";
+    print "       -D <movieid>  get movie data\n";
+    exit(-1);
 }
 
 # display 1-line of info that describes the version of the program
 sub version {
-print "$title ($version) by $author\n"
+    print "$title ($version) by $author\n"
 }
 
 # display 1-line of info that can describe the type of query used
 sub info {
-print "Performs queries using the themoviedb.org website.\n";
+    print "Performs queries using the themoviedb.org website.\n";
 }
 
 # display detailed help
 sub help {
-version();
-info();
-usage();
+    version();
+    info();
+    usage();
+}
+
+sub TMDBAPIRequest($$) {
+    sub encode_args {
+        my ($paramshashref) = @_;
+        my @ret;
+        while (my ($key, $value) = each(%{$paramshashref})) {
+            push(@ret, uri_escape($key) . "=" . uri_escape($value));
+        }
+
+        return join('&', @ret);
+    }
+
+    my ($relurl, $queryargs_hashref) = @_;
+
+    if (!defined $queryargs_hashref) {
+        $queryargs_hashref = {}
+    }
+
+    my $request = join('/', $base_url, $relurl);
+
+    ${$queryargs_hashref}{'api_key'} = $API_KEY;
+    $request = $request . "?" . encode_args($queryargs_hashref);
+
+    if (defined $opt_d) { printf("# request: '%s'\n", $request); }
+
+    my ($rc, $response) = myth_url_get($request);
+
+    if (defined $opt_r) { printf(STDERR "%s", $response); }
+
+    return ($rc, $response);
 }
 
 # get Movie Data
 sub getMovieData {
-my ($movieid)=@_; # grab movieid parameter
-if (defined $opt_d) { printf("# looking for movie id: '%s'\n", $movieid);}
+    my ($movieid) = @_; # grab movieid parameter
+    if (defined $opt_d) { printf("# looking for movie id: '%s'\n", $movieid); }
 
-# get the search results page via Movie.imdbLookup
-my $request = $base_url . "Movie.imdbLookup?imdb_id=tt" . $movieid . "&api_key=" . $api_key;
-if (defined $opt_d) { printf("# request: '%s'\n", $request); }
-my ($rc, $response) = myth_url_get($request);
-if (defined $opt_r) { printf("%s", $response); }
+    # get the search results page via Movie.imdbLookup
+    my ($rc, $response) =
+        TMDBAPIRequest('Movie.imdbLookup', {'imdb_id' => "tt$movieid"});
 
-my $xs = new XML::Simple(suppressempty => '');
-my $xml = $xs->XMLin($response);
+    my $xs = new XML::Simple(SuppressEmpty => '', ForceArray => ['movie'],
+        KeyAttr => []);
+    my $xml = $xs->XMLin($response);
 
-if ($xml->{"opensearch:totalResults"} > 0) {
-        #
-        # now get the movie data via Movie.getInfo, Movie.imdbLookup does not provide us all the data
-        #
-        my $tmdbid = $xml->{moviematches}->{movie}->{id};
-        $request = $base_url . "Movie.getInfo?id=" . $tmdbid . "&api_key=" . $api_key;
-        if (defined $opt_d) { printf("# request: '%s'\n", $request); }
-        my ($rc, $response) = myth_url_get($request);
-        if (defined $opt_r) { printf("%s", $response); }
+    if ($xml->{"opensearch:totalResults"} > 0) {
+        # now get the movie data via Movie.getInfo, Movie.imdbLookup does not
+        # provide us all the data
+        my $tmdbid = $xml->{moviematches}->{movie}->[0]->{id};
+        my ($rc, $response) =
+            TMDBAPIRequest('Movie.getInfo', {'id' => $tmdbid});
 
-        $xml = $xs->XMLin($response, KeyAttr => ['key', 'id'], forcearray => ['category','production_countries']);
+        $xml = $xs->XMLin($response,
+            ForceArray => ['category', 'production_countries'],
+            KeyAttr => ['key', 'id']);
 
-        my $title       = $xml->{moviematches}->{movie}->{title};
-        my $releasedate = $xml->{moviematches}->{movie}->{release};
+        my $movie = \$xml->{moviematches}->{movie};
+        my $title       = $$movie->{title};
+        my $releasedate = $$movie->{release};
         my $year        = substr($releasedate, 0, 4);
-        my $plot        = $xml->{moviematches}->{movie}->{short_overview};
-        my $userrating  = $xml->{moviematches}->{movie}->{rating};
-        my $runtime     = $xml->{moviematches}->{movie}->{runtime};
-        my $budget      = $xml->{moviematches}->{movie}->{budget};
-        my $revenue     = $xml->{moviematches}->{movie}->{revenue};
-        my $trailer     = $xml->{moviematches}->{movie}->{trailer}->{content};
-        my $homepage    = $xml->{moviematches}->{movie}->{homepage};
+        my $plot        = $$movie->{short_overview};
+        my $userrating  = $$movie->{rating};
+        my $runtime     = $$movie->{runtime};
+        my $budget      = $$movie->{budget};
+        my $revenue     = $$movie->{revenue};
+        my $trailer     = $$movie->{trailer}->{content};
+        my $homepage    = $$movie->{homepage};
 
         # Country
-        my @countrylist = @{$xml->{moviematches}->{movie}->{production_countries}};
+        my @countrylist =
+            @{$xml->{moviematches}->{movie}->{production_countries}};
         my $country;
-        if (@countrylist > 0) {
-                $country = $countrylist[0]->{name};
+        if (@countrylist > 0 && ref $countrylist[0] eq 'HASH' &&
+                exists $countrylist[0]->{name}) {
+            $country = $countrylist[0]->{name};
         }
 
         # Genre
         my $genres;
         my @lgenres;
         if (exists $xml->{moviematches}->{movie}->{categories}) {
-            my @catlist = @{$xml->{moviematches}->{movie}->{categories}->{category}};
+            my @catlist =
+                @{$xml->{moviematches}->{movie}->{categories}->{category}};
             if (@catlist > 0) {
                 my $j = 0;
                 for (my $i = 0; $i < @catlist; $i++)
@@ -164,7 +203,8 @@ if ($xml->{"opensearch:totalResults"} > 0) {
         my $director = join(',', @ldirector);
         my $writer = join(',', @lwriter);
 
-        # output fields (these field names must match what MythVideo is looking for)
+        # output fields (these field names must match what MythVideo is looking
+        # for)
         print "Title:$title\n";
         if ($year) {print "Year:$year\n";}
         if ($releasedate) {print "ReleaseDate:$releasedate\n";}
@@ -186,75 +226,55 @@ if ($xml->{"opensearch:totalResults"} > 0) {
 
 # dump Movie Poster
 sub getMoviePoster {
-    my ($movieid)=@_; # grab movieid parameter
-    if (defined $opt_d) { printf("# looking for poster for movie id: '%s'\n", $movieid);}
+    my ($movieid) = @_; # grab movieid parameter
+    if (defined $opt_d) {
+        printf("# looking for poster for movie id: '%s'\n", $movieid);
+    }
 
     # get the search results  page
-    my $request = $base_url . "Movie.imdbLookup?imdb_id=tt" . $movieid . "&api_key=" . $api_key;
-    if (defined $opt_d) { printf("# request: '%s'\n", $request); }
-    my ($rc, $response) = myth_url_get($request);
-    if (defined $opt_r) { printf("%s", $response); }
+    my ($rc, $response) =
+        TMDBAPIRequest('Movie.imdbLookup', {'imdb_id' => "tt$movieid"});
 
-    my $xml = XMLin($response, forcearray => [ 'poster', 'backdrop' ]);
+    my $xml = XMLin($response, ForceArray => ['movie', 'poster', 'backdrop'],
+        KeyAttr => {poster => 'size'});
 
     if ($xml->{"opensearch:totalResults"} > 0) {
-        my $poster = $xml->{moviematches}->{movie}->{poster}->[0]->{content};
-
-        if ($poster) {
-        # check for a mid sized poster
-        foreach my $p (@{$xml->{moviematches}->{movie}->{poster}})
+        if (exists $xml->{moviematches}->{movie}->[0]->{poster}->{mid})
         {
-           if ($p->{size} eq "mid")
-           {
-               $poster = $p->{content};
-               last;
-           }
+            print($xml->{moviematches}->{movie}->[0]->{poster}->
+                {mid}->{content} . "\n");
         }
-
-        if ($poster) {
-            print "$poster\n";
-        }
-        }
-#        else {
-#            my $backdrop = $xml->{moviematches}->{movie}->{backdrop}->[0]->{content};
-#
-#            if ($backdrop)
-#            {
-#                print "$backdrop\n";
-#            }
-#        }
     }
 }
 
 # dump Movie Backdrop
 sub getMovieBackdrop {
-    my ($movieid)=@_; # grab movieid parameter
-    if (defined $opt_d) { printf("# looking for backdrop for movie id: '%s'\n", $movieid);}
+    my ($movieid) = @_; # grab movieid parameter
+    if (defined $opt_d) {
+        printf("# looking for backdrop for movie id: '%s'\n", $movieid);
+    }
 
     # get the search results page via Movie.imdbLookup
-    my $request = $base_url . "Movie.imdbLookup?imdb_id=tt" . $movieid . "&api_key=" . $api_key;
-    if (defined $opt_d) { printf("# request: '%s'\n", $request); }
-    my ($rc, $response) = myth_url_get($request);
-    if (defined $opt_r) { printf("%s", $response); }
+    my ($rc, $response) =
+        TMDBAPIRequest('Movie.imdbLookup', {'imdb_id' => "tt$movieid"});
 
-    my $xs = new XML::Simple(suppressempty => '');
+    my $xs = new XML::Simple(SuppressEmpty => '', ForceArray => [],
+        KeyAttr => []);
     my $xml = $xs->XMLin($response);
 
     if ($xml->{"opensearch:totalResults"} > 0) {
-        #
-        # now get the movie data via Movie.getInfo, Movie.imdbLookup does not provide us all the data
-        #
+        # now get the movie data via Movie.getInfo, Movie.imdbLookup does not
+        # provide us all the data
         my $tmdbid = $xml->{moviematches}->{movie}->{id};
-        $request = $base_url . "Movie.getInfo?id=" . $tmdbid . "&api_key=" . $api_key;
-        if (defined $opt_d) { printf("# request: '%s'\n", $request); }
-        my ($rc, $response) = myth_url_get($request);
-        if (defined $opt_r) { printf("%s", $response); }
 
-        $xml = XMLin($response, KeyAttr => ['key', 'id']);
+        my ($rc, $response) =
+            TMDBAPIRequest('Movie.getInfo', {'id' => $tmdbid});
+
+        $xml = XMLin($response, ForceArray=> [], KeyAttr => ['key', 'id']);
 
         foreach my $backdrop (@{$xml->{moviematches}->{movie}->{backdrop}})
         {
-         # print "$backdrop->{content}\n";
+            # print "$backdrop->{content}\n";
 
             if ($backdrop->{size} eq "original")
             {
@@ -266,68 +286,57 @@ sub getMovieBackdrop {
 
 # dump Movie list:  1 entry per line, each line as 'movieid:Movie Title'
 sub getMovieList {
-my ($filename)=@_; # grab parameters
+    my ($filename)=@_; # grab parameters
 
-#
-# Convert filename into a query string
-# (use same rules that Metadata::guesTitle does)
-my $query = cleanTitleQuery($filename);
+    # Convert filename into a query string
+    # (use same rules that Metadata::guesTitle does)
+    my $query = cleanTitleQuery($filename);
 
-if (defined $opt_d) {
-    printf("# query: '%s'\n", $query);
-}
+    if (defined $opt_d) {
+        printf("# query: '%s'\n", $query);
+    }
 
-# get the search results  page
-my $request = $base_url . "Movie.search?title=" . $query . "&api_key=" . $api_key;
-if (defined $opt_d) { printf("# request: '%s'\n", $request); }
-my ($rc, $response) = myth_url_get($request);
-if (defined $opt_r) {
-    print $response;
-    exit(0);
-}
+    # get the search results  page
+    my ($rc, $response) =
+        TMDBAPIRequest('Movie.search', {'title' => $query});
 
-my $xs = new XML::Simple(suppressempty => '');
-my $xml = $xs->XMLin($response, forcearray => [ 'movie' ]);
+    my $xs = new XML::Simple(SuppressEmpty => '', ForceArray => ['movie'],
+        KeyAttr => []);
+    my $xml = $xs->XMLin($response);
 
-if ($xml->{"opensearch:totalResults"} > 0) {
-    my @movies;
-    my $j = 0;
+    if ($xml->{"opensearch:totalResults"} > 0) {
+        my @movies;
 
-    foreach my $key (keys(%{$xml->{moviematches}->{movie}}))
-    {
-        my $type = $xml->{moviematches}->{movie}->{$key}->{type};
-        if ($type ne "movie")
+        foreach my $movie (@{$xml->{moviematches}->{movie}})
         {
-            next;
-        }
+            if ($movie->{type} ne 'movie') {
+                next;
+            }
 
-        my $movienum  = $xml->{moviematches}->{movie}->{$key}->{imdb};
-        my $moviename = $xml->{moviematches}->{movie}->{$key}->{title};
-        my $release   = $xml->{moviematches}->{movie}->{$key}->{release};
-        my $movieyear = 0;
+            my $movienum  = $movie->{imdb};
+            my $moviename = $movie->{title};
+            my $release   = $movie->{release};
+            my $movieyear = 0;
 
-        if ($release) {
-            $movieyear = substr($release, 0, 4);
-        }
-
-        if ($movienum) {
             if ($release) {
-                $movies[$j++]= substr($movienum,2) . ":" . $moviename . " (". $movieyear . ")";
+                $movieyear = substr($release, 0, 4);
             }
-            else {
-                $movies[$j++]= substr($movienum,2) . ":" . $moviename;
+
+            if ($movienum) {
+                $movienum =~ s/^tt//;
+                my $entry = $movienum . ":" . $moviename;
+                if ($release) {
+                    $entry = $entry . " (". $movieyear . ")";
+                }
+                push(@movies, $entry);
             }
         }
-    }
-    # display array of values
 
-    for $movie (@movies) { print "$movie\n"; }
+        for my $movie (@movies) { print "$movie\n"; }
     }
 }
 
-#
 # Main Program
-#
 
 # parse command line arguments
 getopts('ohrdivDMPB');
@@ -337,29 +346,26 @@ if (defined $opt_v) { version(); exit 1; }
 if (defined $opt_i) { info(); exit 1; }
 
 # print out usage if needed
-if (defined $opt_h || $#ARGV<0) { help(); }
+if (defined $opt_h || $#ARGV < 0) {
+    help();
+}
 
 if (defined $opt_D) {
-# take movieid from cmdline arg
-$movieid = shift || die "Usage : $0 -D <movieid>\n";
-getMovieData($movieid);
+    # take movieid from cmdline arg
+    my $movieid = shift || die "Usage : $0 -D <movieid>\n";
+    getMovieData($movieid);
+} elsif (defined $opt_P) {
+    # take movieid from cmdline arg
+    my $movieid = shift || die "Usage : $0 -P <movieid>\n";
+    getMoviePoster($movieid);
+} elsif (defined $opt_B) {
+    # take movieid from cmdline arg
+    my $movieid = shift || die "Usage : $0 -P <movieid>\n";
+    getMovieBackdrop($movieid);
+} elsif (defined $opt_M) {
+    # take query from cmdline arg
+    my $query = shift || die "Usage : $0 -M <query>\n";
+    getMovieList($query);
 }
 
-elsif (defined $opt_P) {
-# take movieid from cmdline arg
-$movieid = shift || die "Usage : $0 -P <movieid>\n";
-getMoviePoster($movieid);
-}
-
-elsif (defined $opt_B) {
-# take movieid from cmdline arg
-$movieid = shift || die "Usage : $0 -P <movieid>\n";
-getMovieBackdrop($movieid);
-}
-
-elsif (defined $opt_M) {
-# take query from cmdline arg
-$query = shift || die "Usage : $0 -M <query>\n";
-getMovieList($query);
-}
-# vim: set expandtab ts=3 sw=3 :
+# vim: set expandtab ts=4 sw=4 :
