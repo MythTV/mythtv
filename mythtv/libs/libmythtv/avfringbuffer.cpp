fa38e665d0f90795baa87d1fea70975f1c88de4b
Chris Pinkham <cpinkham@mythtv.org>
2009-04-08 01:41:37 +0000

    Initialize (URLContext*)->priv_data in AVF_Open() in case one of the other AVF_* functions gets called before we can allocate a AVFRingBuffer and assign it.  This can happen if we use libavformat/aviobuf.c's url_fopen() which calls avio.c's url_open() which calls url_open_protocol(). The URLContext isn't allocated until url_open_protocol() gets called, but url_open_protocol immediately tries to do a url_seek() (aka AVF_Seek()) which depend(s|ed) on priv_data already being pointed to an AVFRingBuffer.
    The bug didn't affect current usage, but showed up as a segfault when
    writing files using libavformat via our RingBuffer class.
    
    References #2972.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@20321 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/avfringbuffer.cpp
+++ mythtv/libs/libmythtv/avfringbuffer.cpp
@@ -2,15 +2,19 @@
 
 int AVF_Open(URLContext *h, const char *filename, int flags)
 {
-    (void)h;
     (void)filename;
     (void)flags;
+
+    h->priv_data = NULL;
     return 0;
 }   
     
 int AVF_Read(URLContext *h, uint8_t *buf, int buf_size)
 {
     AVFRingBuffer *avfr = (AVFRingBuffer *)h->priv_data;
+
+    if (!avfr)
+        return 0;
 
     return avfr->GetRingBuffer()->Read(buf, buf_size);
 }   
@@ -19,12 +23,18 @@ int AVF_Write(URLContext *h, uint8_t *buf, int buf_size)
 {
     AVFRingBuffer *avfr = (AVFRingBuffer *)h->priv_data;
   
+    if (!avfr)
+        return 0;
+
     return avfr->GetRingBuffer()->Write(buf, buf_size);
 }
 
 offset_t AVF_Seek(URLContext *h, offset_t offset, int whence)
 {
     AVFRingBuffer *avfr = (AVFRingBuffer *)h->priv_data;
+
+    if (!avfr)
+        return 0;
 
     if (whence == AVSEEK_SIZE)
         return avfr->GetRingBuffer()->GetRealFileSize(); 
@@ -53,20 +63,26 @@ URLProtocol AVF_RingBuffer_Protocol = {
 
 int AVF_Write_Packet(void *opaque, uint8_t *buf, int buf_size)
 {
-    URLContext *h = (URLContext *)opaque;
-    return url_write(h, buf, buf_size);
+    if (!opaque)
+        return 0;
+
+    return url_write((URLContext *)opaque, buf, buf_size);
 }
 
 int AVF_Read_Packet(void *opaque, uint8_t *buf, int buf_size)
 {
-    URLContext *h = (URLContext *)opaque;
-    return url_read(h, buf, buf_size);
+    if (!opaque)
+        return 0;
+
+    return url_read((URLContext *)opaque, buf, buf_size);
 }
 
 offset_t AVF_Seek_Packet(void *opaque, int64_t offset, int whence)
 {
-    URLContext *h = (URLContext *)opaque;
-    return url_seek(h, offset, whence);
+    if (!opaque)
+        return 0;
+
+    return url_seek((URLContext *)opaque, offset, whence);
 }
 
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
