883d98685b41a6bd7c233c499a0d5208bfdbb758
Daniel Kristjansson <dkristjansson@mythtv.org>
2009-04-20 03:33:44 +0000

    Backported visibility fixes from trunk.
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-21-fixes@20436 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/audiopulseutil.cpp
+++ mythtv/libs/libmyth/audiopulseutil.cpp
@@ -19,6 +19,7 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "audiopulseutil.h"
 #include "util.h" // for IsPulseAudioRunning()
 #include "exitcodes.h"
 #include "mythcontext.h"
