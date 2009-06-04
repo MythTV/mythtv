MythTV/mythtv

commit 2352d919ea3676b63f953730a9bf8b34f81d1004

Initial VDPAU patch.  Don't expect this to work, yet. Major contributions from Mark Kendall.
Enable with --enable-vdpau, do a distclean first.

Setup a display profile with:
  decoder = nvidia vdpau acceleration
  video + osd renderers: vdpau

What works:
- deinterlacing (select "Advanced (2x, Hw)" for best results)
- OSD/PiP.
- Color controls (slightly broken)
- Other stuff

What doesn't:
- mpeg2 looks bad-ish (blocky), possibly a driver issue.
- Lots and lots of other stuff.



git-svn-id: http://svn.mythtv.org/svn/trunk@19165 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -38,6 +38,13 @@ extern "C" {
 }
 #endif // USING_XVMC
 
+#ifdef USING_VDPAU
+#include "videoout_xv.h"
+extern "C" {
+#include "libavcodec/vdpau_render.h"
+}
+#endif // USING_VDPAU
+
 extern "C" {
 #include "../libavutil/avutil.h"
 #include "../libavcodec/ac3_parser.h"
@@ -75,6 +82,11 @@ void release_avf_buffer_xvmc(struct AVCodecContext *c, AVFrame *pic);
 void render_slice_xvmc(struct AVCodecContext *s, const AVFrame *src,
                        int offset[4], int y, int type, int height);
 void decode_cc_dvd(struct AVCodecContext *c, const uint8_t *buf, int buf_size);
+
+int get_avf_buffer_vdpau(struct AVCodecContext *c, AVFrame *pic);
+void release_avf_buffer_vdpau(struct AVCodecContext *c, AVFrame *pic);
+void render_slice_vdpau(struct AVCodecContext *s, const AVFrame *src,
+                        int offset[4], int y, int type, int height);
 
 static void myth_av_log(void *ptr, int level, const char* fmt, va_list vl)
 {
@@ -1135,6 +1147,17 @@ void AvFormatDecoder::InitVideoCodec(AVStream *stream, AVCodecContext *enc,
         enc->draw_horiz_band  = NULL;
         directrendering      |= selectedStream;
     }
+    else if (codec && (codec->id == CODEC_ID_MPEGVIDEO_VDPAU ||
+                       codec->id == CODEC_ID_H264_VDPAU ||
+                       codec->id == CODEC_ID_VC1_VDPAU ||
+                       codec->id == CODEC_ID_WMV3_VDPAU))
+    {
+        enc->get_buffer      = get_avf_buffer_vdpau;
+        enc->release_buffer  = release_avf_buffer_vdpau;
+        enc->draw_horiz_band = render_slice_vdpau;
+        enc->slice_flags     = SLICE_FLAG_CODED_ORDER | SLICE_FLAG_ALLOW_FIELD;
+        directrendering     |= selectedStream;
+    }
     else if (codec && codec->capabilities & CODEC_CAP_DR1)
     {
         enc->flags          |= CODEC_FLAG_EMU_EDGE;
@@ -1166,7 +1189,7 @@ void AvFormatDecoder::InitVideoCodec(AVStream *stream, AVCodecContext *enc,
     }
 }
 
-#if defined(USING_XVMC) || defined(USING_DVDV)
+#if defined(USING_XVMC) || defined(USING_DVDV) || defined(USING_VDPAU)
 static int mpeg_version(int codec_id)
 {
     switch (codec_id)
@@ -1177,13 +1200,23 @@ static int mpeg_version(int codec_id)
         case CODEC_ID_MPEG2VIDEO_XVMC:
         case CODEC_ID_MPEG2VIDEO_XVMC_VLD:
         case CODEC_ID_MPEG2VIDEO_DVDV:
+        case CODEC_ID_MPEGVIDEO_VDPAU:
             return 2;
         case CODEC_ID_H263:
             return 3;
         case CODEC_ID_MPEG4:
             return 4;
         case CODEC_ID_H264:
+        case CODEC_ID_H264_VDPAU:
             return 5;
+        case CODEC_ID_VC1:
+        case CODEC_ID_VC1_VDPAU:
+            return 6;
+        case CODEC_ID_WMV3:
+        case CODEC_ID_WMV3_VDPAU:
+            return 7;
+        default:
+            break;
     }
     return 0;
 }
@@ -1518,24 +1551,9 @@ int AvFormatDecoder::ScanStreams(bool novideo)
                 uint height = max(enc->height, 16);
                 QString dec = "ffmpeg";
                 uint thread_count = 1;
-                if (!is_db_ignored)
-                {
-                    VideoDisplayProfile vdp;
-                    vdp.SetInput(QSize(width, height));
-                    dec = vdp.GetDecoder();
-                    thread_count = vdp.GetMaxCPUs();
-                }
-                VERBOSE(VB_PLAYBACK, QString("Using %1 CPUs for decoding")
-                        .arg(ENABLE_THREADS ? thread_count : 1));
-
-                if (ENABLE_THREADS && thread_count > 1)
-                {
-                    avcodec_thread_init(enc, thread_count);
-                    enc->thread_count = thread_count;
-                }
 
                 bool handled = false;
-#ifdef USING_XVMC
+#if defined(USING_VDPAU) || defined(USING_XVMC)
                 if (!using_null_videoout && mpeg_version(enc->codec_id))
                 {
                     // HACK -- begin
@@ -1569,9 +1587,9 @@ int AvFormatDecoder::ScanStreams(bool novideo)
                         /* xvmc pix fmt */ xvmc_pixel_format(enc->pix_fmt),
                         /* test surface */ kCodec_NORMAL_END > video_codec_id,
                         /* force_xv     */ force_xv);
-                    bool vcd, idct, mc;
+                    bool vcd, idct, mc, vdpau;
                     enc->codec_id = (CodecID)
-                        myth2av_codecid(mcid, vcd, idct, mc);
+                        myth2av_codecid(mcid, vcd, idct, mc, vdpau);
 
                     if (ringBuffer && ringBuffer->isDVD() && 
                         (mcid == video_codec_id) &&
@@ -1606,6 +1624,26 @@ int AvFormatDecoder::ScanStreams(bool novideo)
                     handled = true;
                 }
 #endif // USING_XVMC || USING_DVDV
+
+                if (!is_db_ignored)
+                {
+                    VideoDisplayProfile vdp;
+                    vdp.SetInput(QSize(width, height));
+                    dec = vdp.GetDecoder();
+                    thread_count = vdp.GetMaxCPUs();
+                }
+
+                if (video_codec_id > kCodec_NORMAL_END)
+                    thread_count = 1;
+
+                VERBOSE(VB_PLAYBACK, QString("Using %1 CPUs for decoding")
+                        .arg(ENABLE_THREADS ? thread_count : 1));
+
+                if (ENABLE_THREADS && thread_count > 1)
+                {
+                    avcodec_thread_init(enc, thread_count);
+                    enc->thread_count = thread_count;
+                }
 
                 if (!handled)
                 {
@@ -2113,6 +2151,76 @@ void release_avf_buffer_xvmc(struct AVCodecContext *c, AVFrame *pic)
 
 void render_slice_xvmc(struct AVCodecContext *s, const AVFrame *src,
                        int offset[4], int y, int type, int height)
+{
+    if (!src)
+        return;
+
+    (void)offset;
+    (void)type;
+
+    if (s && src && s->opaque && src->opaque)
+    {
+        AvFormatDecoder *nd = (AvFormatDecoder *)(s->opaque);
+
+        int width = s->width;
+
+        VideoFrame *frame = (VideoFrame *)src->opaque;
+        nd->GetNVP()->DrawSlice(frame, 0, y, width, height);
+    }
+    else
+    {
+        VERBOSE(VB_IMPORTANT, LOC +
+                "render_slice_xvmc called with bad avctx or src");
+    }
+}
+
+int get_avf_buffer_vdpau(struct AVCodecContext *c, AVFrame *pic)
+{
+    AvFormatDecoder *nd = (AvFormatDecoder *)(c->opaque);
+    VideoFrame *frame = nd->GetNVP()->GetNextVideoFrame(false);
+
+    pic->data[0] = frame->priv[0];
+    pic->data[1] = frame->priv[1];
+    pic->data[2] = frame->buf;
+
+    pic->linesize[0] = 0;
+    pic->linesize[1] = 0;
+    pic->linesize[2] = 0;
+
+    pic->opaque = frame;
+    pic->type = FF_BUFFER_TYPE_USER;
+
+    pic->age = 256 * 256 * 256 * 64;
+
+    frame->pix_fmt = c->pix_fmt;
+
+#ifdef USING_VDPAU
+    vdpau_render_state_t *render = (vdpau_render_state_t *)frame->buf;
+    render->state |= MP_VDPAU_STATE_USED_FOR_REFERENCE;
+#endif
+
+    return 0;
+}
+
+void release_avf_buffer_vdpau(struct AVCodecContext *c, AVFrame *pic)
+{
+    assert(pic->type == FF_BUFFER_TYPE_USER);
+
+#ifdef USING_VDPAU
+    vdpau_render_state_t *render = (vdpau_render_state_t *)pic->data[2];
+    render->state &= ~MP_VDPAU_STATE_USED_FOR_REFERENCE;
+#endif
+
+    AvFormatDecoder *nd = (AvFormatDecoder *)(c->opaque);
+    if (nd && nd->GetNVP() && nd->GetNVP()->getVideoOutput())
+        nd->GetNVP()->getVideoOutput()->DeLimboFrame((VideoFrame*)pic->opaque);
+
+    for (uint i = 0; i < 4; i++)
+        pic->data[i] = NULL;
+}
+
+void render_slice_vdpau(struct AVCodecContext *s, const AVFrame *src,
+                        int offset[4], int y, int type, int height)
 {
     if (!src)
         return;
@@ -3277,12 +3385,14 @@ bool AvFormatDecoder::GetFrame(int onlyvideo)
             if (context->codec_id == CODEC_ID_MPEG1VIDEO ||
                 context->codec_id == CODEC_ID_MPEG2VIDEO ||
                 context->codec_id == CODEC_ID_MPEG2VIDEO_XVMC ||
-                context->codec_id == CODEC_ID_MPEG2VIDEO_XVMC_VLD)
+                context->codec_id == CODEC_ID_MPEG2VIDEO_XVMC_VLD ||
+                context->codec_id == CODEC_ID_MPEGVIDEO_VDPAU)
             {
                 if (!ringBuffer->isDVD())
                     MpegPreProcessPkt(curstream, pkt);
             }
-            else if (context->codec_id == CODEC_ID_H264)
+            else if (context->codec_id == CODEC_ID_H264 ||
+                     context->codec_id == CODEC_ID_H264_VDPAU)
             {
                 H264PreProcessPkt(curstream, pkt);
             }
@@ -3961,6 +4071,10 @@ QString AvFormatDecoder::GetCodecDecoderName(void) const
     if ((video_codec_id > kCodec_STD_XVMC_END) &&
         (video_codec_id < kCodec_VLD_END))
         return "xvmc-vld";
+
+    if ((video_codec_id > kCodec_DVDV_END) &&
+        (video_codec_id < kCodec_VDPAU_END))
+        return "vdpau";
 
     return "ffmpeg";
 }

mythtv/configure
@@ -193,6 +193,7 @@ BLOCK_QUOTE
   echo "  --enable-xvmc-pro        enable XvMC for the Unichrome Pro chipset"
   echo "                           (NOT Unichrome chipset)"
   echo "  --enable-xvmc-opengl     enable nVidia XvMC OpenGL texture method"
+  echo "  --enable-vdpau           enable NVidia VDPAU hardware acceleration."
   echo "  --xvmc-lib=LIB           XvMC library override (for crosscompiling)"
   #echo "  --disable-xvmc-vld       disable XvMC-VLD (VIA Epia MPEG accel.)"
   echo "  --enable-opengl-video    enable OpenGL based video display "
@@ -1000,6 +1001,7 @@ HAVE_LIST="
     termios_h
     threads
     winsock2_h
+    vdpau
     yasm
 "
 
@@ -1056,6 +1058,7 @@ USING_LIST='
     darwin_da
     dvdv
     opengl
+    vdpau
 '
 
 CMDLINE_SELECT="
@@ -1072,6 +1075,7 @@ CMDLINE_SELECT="
     shared
     static
     stripping
+    vdpau
 "
 
 PATHS_LIST='
@@ -1121,13 +1125,17 @@ dxa_decoder_deps="zlib"
 eac3_decoder_deps="gpl"
 flashsv_decoder_deps="zlib"
 flashsv_encoder_deps="zlib"
+h264_vdpau_decoder_deps="vdpau"
 mlp_decoder_deps="mlp_parser"
 mpeg_dvdv_decoder_deps="dvdv"
+mpeg_vdpau_decoder_deps="vdpau"
 mpeg_xvmc_decoder_deps="xvmc"
 mpeg_xvmc_vld_decoder_deps="xvmc_vld"
 png_decoder_deps="zlib"
 png_encoder_deps="zlib"
 snow_decoder_deps="disabled_in_mythtv"
+vc1_vdpau_decoder_deps="vdpau"
+wmv3_vdpau_decoder_deps="vdpau"
 zmbv_decoder_deps="zlib"
 zmbv_encoder_deps="zlib"
 
@@ -1213,6 +1221,7 @@ opengl_deps_any="windows x11"
 opengl_video_deps="opengl xv"
 opengl_vsync_deps="opengl"
 v4l_deps="backend"
+vdpau_deps="x11 vdpau_vdpau_h"
 xrandr_deps="x11"
 xv_deps="x11"
 xvmc_deps="xv X11_extensions_XvMClib_h"
@@ -3017,6 +3026,8 @@ fi
 
 enabled xvmc_pro && enable xvmc_vld
 
+check_header vdpau/vdpau.h
+enabled vdpau && has_library libvdpau || disable vdpau
 
 # Can only do Mac accel on Mac platform
 enabled dvdv && test $target_os = darwin || disable dvdv
@@ -3315,6 +3326,7 @@ if enabled x11 ; then
 if test "$VENDOR_XVMC_LIBS" != "" ; then
   echo "XvMC libs                 $VENDOR_XVMC_LIBS"
 fi
+  echo "VDPAU support             ${vdpau-no}"
 fi
   echo "OpenGL video              ${opengl_video-no}"
   if test x"$target_os" = x"darwin" ; then

mythtv/libs/libavcodec/allcodecs.c
@@ -97,6 +97,7 @@ void avcodec_register_all(void)
     REGISTER_DECODER (H263I, h263i);
     REGISTER_ENCODER (H263P, h263p);
     REGISTER_DECODER (H264, h264);
+    REGISTER_DECODER (H264_VDPAU, h264_vdpau);
     REGISTER_ENCDEC  (HUFFYUV, huffyuv);
     REGISTER_DECODER (IDCIN, idcin);
     REGISTER_DECODER (INDEO2, indeo2);
@@ -113,6 +114,7 @@ void avcodec_register_all(void)
     REGISTER_DECODER (MMVIDEO, mmvideo);
     REGISTER_DECODER (MOTIONPIXELS, motionpixels);
     REGISTER_DECODER (MPEG_DVDV, mpeg_dvdv);
+    REGISTER_DECODER (MPEG_VDPAU, mpeg_vdpau);
     REGISTER_DECODER (MPEG_XVMC, mpeg_xvmc);
     REGISTER_DECODER (MPEG_XVMC_VLD, mpeg_xvmc_vld);
     REGISTER_ENCDEC  (MPEG1VIDEO, mpeg1video);
@@ -163,6 +165,7 @@ void avcodec_register_all(void)
     REGISTER_DECODER (ULTI, ulti);
     REGISTER_DECODER (VB, vb);
     REGISTER_DECODER (VC1, vc1);
+    REGISTER_DECODER (VC1_VDPAU, vc1_vdpau);
     REGISTER_DECODER (VCR1, vcr1);
     REGISTER_DECODER (VMDVIDEO, vmdvideo);
     REGISTER_DECODER (VMNC, vmnc);
@@ -175,6 +178,7 @@ void avcodec_register_all(void)
     REGISTER_ENCDEC  (WMV1, wmv1);
     REGISTER_ENCDEC  (WMV2, wmv2);
     REGISTER_DECODER (WMV3, wmv3);
+    REGISTER_DECODER (WMV3_VDPAU, wmv3_vdpau);
     REGISTER_DECODER (WNV1, wnv1);
     REGISTER_DECODER (XAN_WC3, xan_wc3);
     REGISTER_DECODER (XL, xl);

mythtv/libs/libavcodec/avcodec.h
@@ -191,6 +191,10 @@ enum CodecID {
     CODEC_ID_CMV,
     CODEC_ID_MOTIONPIXELS,
     CODEC_ID_TGV,
+    CODEC_ID_MPEGVIDEO_VDPAU,
+    CODEC_ID_H264_VDPAU,
+    CODEC_ID_VC1_VDPAU,
+    CODEC_ID_WMV3_VDPAU,
 
     /* various PCM "codecs" */
     CODEC_ID_PCM_S16LE= 0x10000,
@@ -514,6 +518,8 @@ typedef struct RcOverride{
  * This can be used to prevent truncation of the last audio samples.
  */
 #define CODEC_CAP_SMALL_LAST_FRAME 0x0040
+/* Codec can export data for HW decoding (VDPAU). */
+#define CODEC_CAP_HWACCEL_VDPAU    0x0080
 
 //The following defines may change, don't expect compatibility if you use them.
 #define MB_TYPE_INTRA4x4   0x0001
@@ -1761,6 +1767,13 @@ typedef struct AVCodecContext {
     int xvmc_acceleration;
 
     /**
+     * VDPAU Acceleration
+     * - encoding: forbidden
+     * - decoding: set by decoder
+     */
+    int vdpau_acceleration;
+
+    /**
      * macroblock decision mode
      * - encoding: Set by user.
      * - decoding: unused
@@ -2965,7 +2978,7 @@ typedef struct AVCodecParserContext {
 } AVCodecParserContext;
 
 typedef struct AVCodecParser {
-    int codec_ids[5]; /* several codec IDs are permitted */
+    int codec_ids[6]; /* several codec IDs are permitted */
     int priv_data_size;
     int (*parser_init)(AVCodecParserContext *s);
     int (*parser_parse)(AVCodecParserContext *s,

mythtv/libs/libavcodec/h263dec.c
@@ -92,6 +92,8 @@ av_cold int ff_h263_decode_init(AVCodecContext *avctx)
         break;
     case CODEC_ID_VC1:
     case CODEC_ID_WMV3:
+    case CODEC_ID_VC1_VDPAU:
+    case CODEC_ID_WMV3_VDPAU:
         s->h263_msmpeg4 = 1;
         s->h263_pred = 1;
         s->msmpeg4_version=6;

mythtv/libs/libavcodec/h264.c
@@ -72,6 +72,10 @@ static VLC run7_vlc;
 static VLC_TYPE run7_vlc_table[96][2];
 static const int run7_vlc_table_size = 96;
 
+extern int VDPAU_h264_set_reference_frames(H264Context *h);
+extern int VDPAU_h264_picture_complete(H264Context *h, const uint8_t *buf, int buf_size);
+extern void VDPAU_h264_set_reference_frames_count(H264Context *h);
+
 static void svq3_luma_dc_dequant_idct_c(DCTELEM *block, int qp);
 static void svq3_add_idct_c(uint8_t *dst, DCTELEM *block, int stride, int qp, int dc);
 static void filter_mb( H264Context *h, int mb_x, int mb_y, uint8_t *img_y, uint8_t *img_cb, uint8_t *img_cr, unsigned int linesize, unsigned int uvlinesize);
@@ -100,6 +104,16 @@ static const int left_block_options[4][8]={
     {0,0,1,1,7,10,7,10},
     {0,2,0,2,7,10,7,10}
 };
+
+static const enum PixelFormat pixfmt_vdpau_h264_baseline_420[] = {
+                                           PIX_FMT_VDPAU_H264_BASELINE,
+                                           PIX_FMT_NONE};
+static const enum PixelFormat pixfmt_vdpau_h264_main_420[] = {
+                                           PIX_FMT_VDPAU_H264_MAIN,
+                                           PIX_FMT_NONE};
+static const enum PixelFormat pixfmt_vdpau_h264_high_420[] = {
+                                           PIX_FMT_VDPAU_H264_HIGH,
+                                           PIX_FMT_NONE};
 
 static void fill_caches(H264Context *h, int mb_type, int for_deblock){
     MpegEncContext * const s = &h->s;
@@ -2198,10 +2212,8 @@ static av_cold int decode_init(AVCodecContext *avctx){
     s->quarter_sample = 1;
     s->low_delay= 1;
 
-    if(avctx->codec_id == CODEC_ID_SVQ3)
-        avctx->pix_fmt= PIX_FMT_YUVJ420P;
-    else
-        avctx->pix_fmt= PIX_FMT_YUV420P;
+    // Set in decode_postinit() once initial parsing is complete
+    avctx->pix_fmt = PIX_FMT_NONE;
 
     decode_init_vlc();
 
@@ -2219,9 +2231,42 @@ static av_cold int decode_init(AVCodecContext *avctx){
     return 0;
 }
 
+static int decode_postinit(H264Context *h, SPS *sps){
+    AVCodecContext * const avctx= h->s.avctx;
+
+    if (avctx->pix_fmt != PIX_FMT_NONE){
+        return 0;
+    }
+
+    if (avctx->vdpau_acceleration) {
+        if(h->s.chroma_format >= 2) {
+            return -2;
+        }
+        if (sps->profile_idc == 66) {
+            avctx->pix_fmt = avctx->get_format(avctx, pixfmt_vdpau_h264_baseline_420);
+        } else if (sps->profile_idc == 77) {
+            avctx->pix_fmt = avctx->get_format(avctx, pixfmt_vdpau_h264_main_420);
+        } else if (sps->profile_idc == 100) {
+            avctx->pix_fmt = avctx->get_format(avctx, pixfmt_vdpau_h264_high_420);
+        } else {
+            return -2;
+        }
+    } else if (avctx->codec_id == CODEC_ID_SVQ3) {
+        avctx->pix_fmt= PIX_FMT_YUVJ420P;
+    } else {
+        avctx->pix_fmt= PIX_FMT_YUV420P;
+    }
+
+    return 0;
+}
+
 static int frame_start(H264Context *h){
     MpegEncContext * const s = &h->s;
     int i;
+
+#ifdef HAVE_VDPAU
+    VDPAU_h264_set_reference_frames_count(h);
+#endif
 
     if(MPV_frame_start(s, s->avctx) < 0)
         return -1;
@@ -7102,6 +7147,10 @@ static inline int decode_seq_parameter_set(H264Context *h){
                ((const char*[]){"Gray","420","422","444"})[sps->chroma_format_idc]
                );
     }
+
+    if (decode_postinit(h, sps) < 0)
+        return -1;
+
     return 0;
 }
 
@@ -7234,7 +7283,9 @@ static void execute_decode_slices(H264Context *h, int context_count){
     H264Context *hx;
     int i;
 
-    if(context_count == 1) {
+    if(avctx->vdpau_acceleration) {
+        return;
+    } else if(context_count == 1) {
         decode_slice(avctx, h);
     } else {
         for(i = 1; i < context_count; i++) {
@@ -7558,11 +7609,24 @@ static int decode_frame(AVCodecContext *avctx,
 
         h->prev_frame_num_offset= h->frame_num_offset;
         h->prev_frame_num= h->frame_num;
+
+#ifdef HAVE_VDPAU
+        if (avctx->vdpau_acceleration) {
+            VDPAU_h264_set_reference_frames(h);
+        }
+#endif
+
         if(!s->dropable) {
             h->prev_poc_msb= h->poc_msb;
             h->prev_poc_lsb= h->poc_lsb;
             execute_ref_pic_marking(h, h->mmco, h->mmco_index);
         }
+
+#ifdef HAVE_VDPAU
+        if (avctx->vdpau_acceleration) {
+            VDPAU_h264_picture_complete(h, buf, buf_size);
+        }
+#endif
 
         /*
          * FIXME: Error handling code does not seem to support interlaced
@@ -7576,8 +7640,11 @@ static int decode_frame(AVCodecContext *avctx,
          * past end by one (callers fault) and resync_mb_y != 0
          * causes problems for the first MB line, too.
          */
-        if (!FIELD_PICTURE)
-            ff_er_frame_end(s);
+#ifdef HAVE_VDPAU
+        if (!avctx->vdpau_acceleration)
+#endif
+            if (!FIELD_PICTURE)
+                ff_er_frame_end(s);
 
         MPV_frame_end(s);
 
@@ -7889,5 +7956,36 @@ AVCodec h264_decoder = {
     .flush= flush_dpb,
     .long_name = NULL_IF_CONFIG_SMALL("H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10"),
 };
+
+#ifdef HAVE_VDPAU
+static av_cold int h264_vdpau_decode_init(AVCodecContext *avctx){
+    if( avctx->thread_count > 1)
+        return -1;
+    if( !(avctx->slice_flags & SLICE_FLAG_CODED_ORDER) )
+        return -1;
+    if( !(avctx->slice_flags & SLICE_FLAG_ALLOW_FIELD) ){
+        dprintf(avctx, "h264.c: VDPAU decoder does not set SLICE_FLAG_ALLOW_FIELD\n");
+    }
+    decode_init(avctx);
+
+    avctx->vdpau_acceleration = 1;
+
+    return 0;
+}
+
+AVCodec h264_vdpau_decoder = {
+    "h264_vdpau",
+    CODEC_TYPE_VIDEO,
+    CODEC_ID_H264_VDPAU,
+    sizeof(H264Context),
+    h264_vdpau_decode_init,
+    NULL,
+    decode_end,
+    decode_frame,
+    CODEC_CAP_DR1 | CODEC_CAP_DELAY | CODEC_CAP_HWACCEL_VDPAU,
+    .flush= flush_dpb,
+    .long_name = NULL_IF_CONFIG_SMALL("H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10 (VDPAU acceleration)"),
+};
+#endif
 
 #include "svq3.c"

mythtv/libs/libavcodec/h264_parser.c
@@ -139,7 +139,9 @@ static int h264_split(AVCodecContext *avctx,
 
 
 AVCodecParser h264_parser = {
-    { CODEC_ID_H264 },
+    { CODEC_ID_H264,
+      CODEC_ID_H264_VDPAU
+     },
     sizeof(H264Context),
     NULL,
     h264_parse,

mythtv/libs/libavcodec/imgconvert.c
@@ -266,6 +266,33 @@ static const PixFmtInfo pix_fmt_info[PIX_FMT_NB] = {
     [PIX_FMT_XVMC_MPEG2_IDCT] = {
         .name = "xvmcidct",
     },
+    [PIX_FMT_VDPAU_MPEG1] = {
+        .name = "vdpau_mpeg1",
+    },
+    [PIX_FMT_VDPAU_MPEG2_SIMPLE] = {
+        .name = "vdpau_mpeg2_simple",
+    },
+    [PIX_FMT_VDPAU_MPEG2_MAIN] = {
+        .name = "vdpau_mpeg2_main",
+    },
+    [PIX_FMT_VDPAU_H264_BASELINE] = {
+        .name = "vdpau_h264_baseline",
+    },
+    [PIX_FMT_VDPAU_H264_MAIN] = {
+        .name = "vdpau_h264_main",
+    },
+    [PIX_FMT_VDPAU_H264_HIGH] = {
+        .name = "vdpau_h264_high",
+    },
+    [PIX_FMT_VDPAU_VC1_SIMPLE] = {
+        .name = "vdpau_vc1_simple",
+    },
+    [PIX_FMT_VDPAU_VC1_MAIN] = {
+        .name = "vdpau_vc1_main",
+    },
+    [PIX_FMT_VDPAU_VC1_ADVANCED] = {
+        .name = "vdpau_vc1_advanced",
+    },
     [PIX_FMT_UYYVYY411] = {
         .name = "uyyvyy411",
         .nb_channels = 1,

commit 5a0640a8549b936d703909df1b296a2127a0d7f2

Fix a small XvMC regression introduced in r19165.
git-svn-id: http://svn.mythtv.org/svn/trunk@20669 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -1559,6 +1559,14 @@ int AvFormatDecoder::ScanStreams(bool novideo)
                 QString dec = "ffmpeg";
                 uint thread_count = 1;
 
+                if (!is_db_ignored)
+                {
+                    VideoDisplayProfile vdp;
+                    vdp.SetInput(QSize(width, height));
+                    dec = vdp.GetDecoder();
+                    thread_count = vdp.GetMaxCPUs();
+                }
+
                 bool handled = false;
 #if defined(USING_VDPAU) || defined(USING_XVMC)
                 if (!using_null_videoout && mpeg_version(enc->codec_id))
@@ -1631,14 +1639,6 @@ int AvFormatDecoder::ScanStreams(bool novideo)
                     handled = true;
                 }
 #endif // USING_XVMC || USING_DVDV
-
-                if (!is_db_ignored)
-                {
-                    VideoDisplayProfile vdp;
-                    vdp.SetInput(QSize(width, height));
-                    dec = vdp.GetDecoder();
-                    thread_count = vdp.GetMaxCPUs();
-                }
 
                 if (video_codec_id > kCodec_NORMAL_END)
                     thread_count = 1;