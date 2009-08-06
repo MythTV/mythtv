2613e1ac078a1a1bbf9aa9620201c1a6fbd2fd7c
Stuart Morgan <smorgan@mythtv.org>
2009-08-06 17:20:04 +0000

    Fix compilation error.
    git-svn-id: http://svn.mythtv.org/svn/trunk@21142 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythmusic/mythmusic/cdrip.cpp
+++ mythplugins/mythmusic/mythmusic/cdrip.cpp
@@ -501,7 +501,7 @@ bool Ripper::Create(void)
             SLOT(compilationChanged(bool)));
     connect(m_searchGenreButton, SIGNAL(Clicked()), SLOT(searchGenre()));
     connect(m_genreEdit, SIGNAL(valueChanged()), SLOT(genreChanged()));
-    m_yearEdit->SetFilter(FilterAlpha|FilterSymbols|FilterPunct);
+    m_yearEdit->SetFilter((InputFilter)(FilterAlpha | FilterSymbols | FilterPunct));
     connect(m_yearEdit, SIGNAL(valueChanged()), SLOT(yearChanged()));
     connect(m_artistEdit, SIGNAL(valueChanged()), SLOT(artistChanged()));
     connect(m_searchArtistButton, SIGNAL(Clicked()), SLOT(searchArtist()));
