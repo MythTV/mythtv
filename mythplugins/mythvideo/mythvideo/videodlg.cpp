491168013b830906ec50dc7d1cb814cd72c430ef
Robert McNamara <rmcnamara@mythtv.org>
2009-08-29 06:19:20 +0000

    Temporarily disable the SG image hunt-- works great when the files exist, or exist nowhere else, but the remotefile fallback behavior was assigning the wrong images.  Will re-enable when the fallback issue is figured out.
    git-svn-id: http://svn.mythtv.org/svn/trunk@21583 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythvideo/mythvideo/videodlg.cpp
+++ mythplugins/mythvideo/mythvideo/videodlg.cpp
@@ -1548,21 +1548,21 @@ namespace
                 for (QStringList::const_iterator i = sfn.begin();
                         i != sfn.end(); ++i)
                 {
-                    if (!host.isEmpty())
-                    {
-                        QString url = GenRemoteFileURL(sgroup, host, *i);
-                        RemoteFile *rf = new RemoteFile(url);
-                        if (rf->GetFileSize() > 0)
-                        {
-                            image = *i;
-                            return true;
-                        }
-                        else
-                        {
-                            delete rf;
-                            rf = NULL;
-                        }
-                    }
+//                    if (!host.isEmpty())
+//                    {
+//                        QString url = GenRemoteFileURL(sgroup, host, *i);
+//                        RemoteFile *rf = new RemoteFile(url);
+//                        if (rf->GetFileSize() > 0)
+//                        {
+//                            image = *i;
+//                            return true;
+//                        }
+//                        else
+//                        {
+//                            delete rf;
+//                            rf = NULL;
+//                        }
+//                    }
                 }
             }
         }
