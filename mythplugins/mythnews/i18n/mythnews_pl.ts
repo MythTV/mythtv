862cb2a340c537dc36cab8de9871a22521f4bd85
Robert McNamara <rmcnamara@mythtv.org>
2009-09-12 05:50:29 +0000

    Refs #6936.  Creates an unfinished polish translation for MythNews.  Wagabunda, did you intend to mark these strings as finished before the release?  I will leave the ticket open in case you would like to change the few translations still in the unfinished state.
    git-svn-id: http://svn.mythtv.org/svn/trunk@21802 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ mythplugins/mythnews/i18n/mythnews_pl.ts
@@ -0,0 +1,90 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MythNews</name>
+    <message>
+        <source>Updated</source>
+        <translation>Zaktualizowano</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Nieznany</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <source>Edit News Site</source>
+        <translation>Edytuj stronę wiadomości</translation>
+    </message>
+    <message>
+        <source>Add News Site</source>
+        <translation>Dodaj stronę wiadomości</translation>
+    </message>
+    <message>
+        <source>Delete News Site</source>
+        <translation>Usuń stronę wiadomości</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opcje</translation>
+    </message>
+    <message>
+        <source>You haven&apos;t configured MythNews to use any sites.</source>
+        <translation>W MythNews nie skonfigurowano żadnej strony wiadomości.</translation>
+    </message>
+    <message>
+        <source>No browser command set! MythNews needs MythBrowser to be installed.</source>
+        <translation>Nie podano polecenia przeglądarki! Należy zainstalować MythBrowser.</translation>
+    </message>
+</context>
+<context>
+    <name>MythNewsEditor</name>
+    <message>
+        <source>Edit Site Details</source>
+        <translation type="unfinished">Edytuj szczegóły strony</translation>
+    </message>
+    <message>
+        <source>Enter Site Details</source>
+        <translation type="unfinished">Wprowadź szczegóły strony</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation type="unfinished">Nazwa:</translation>
+    </message>
+    <message>
+        <source>URL:</source>
+        <translation type="unfinished">URL:</translation>
+    </message>
+    <message>
+        <source>Icon:</source>
+        <translation type="unfinished">Ikona:</translation>
+    </message>
+    <message>
+        <source>Podcast:</source>
+        <translation type="unfinished">Podkast:</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation type="unfinished">Ok</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished">Anuluj</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Downloading media...</source>
+        <translation>Pobieranie zawartości...</translation>
+    </message>
+</context>
+<context>
+    <name>ThemeUI</name>
+    <message>
+        <source>Minutes</source>
+        <translation>Minuty</translation>
+    </message>
+</context>
+</TS>
--- mythplugins/mythnews/i18n/translate.pro
+++ mythplugins/mythnews/i18n/translate.pro
@@ -1,6 +1,6 @@
 SOURCES += ../mythnews/mythnewsconfig.cpp
 SOURCES += ../mythnews/mythnews.cpp
-SOURCES += ../mythnews/newsengine.cpp
+SOURCES += ../mythnews/mythnewseditor.cpp
 SOURCES += ./themestrings.h
 
 TRANSLATIONS  = mythnews_it.ts mythnews_es.ts mythnews_ca.ts
@@ -8,4 +8,4 @@ TRANSLATIONS += mythnews_nl.ts mythnews_de.ts mythnews_da.ts
 TRANSLATIONS += mythnews_pt.ts mythnews_sv.ts mythnews_fr.ts
 TRANSLATIONS += mythnews_ja.ts mythnews_sl.ts mythnews_et.ts
 TRANSLATIONS += mythnews_fi.ts mythnews_nb.ts mythnews_ru.ts
-TRANSLATIONS += mythnews_cs.ts
+TRANSLATIONS += mythnews_cs.ts mythnews_pl.ts
