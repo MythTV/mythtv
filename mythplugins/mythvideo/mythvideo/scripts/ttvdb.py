44f04874a30def8c00373c8d213ea914b593b073
Robert McNamara <rmcnamara@mythtv.org>
2009-09-24 22:37:36 +0000

    Remove URL replace from TVDB to work with their currently-broken mirror.
    git-svn-id: http://svn.mythtv.org/svn/trunk@22026 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythvideo/mythvideo/scripts/ttvdb.py
+++ mythplugins/mythvideo/mythvideo/scripts/ttvdb.py
@@ -307,7 +307,7 @@ in the same directory as ttvdb.py. They should have been included with the distr
 
 # Global variables
 http_find="http://www.thetvdb.com"
-http_replace="http://images.thetvdb.com" #.nyud.net:8080"
+http_replace="http://www.thetvdb.com" #Keep replace code "just in case"
 
 logfile="/home/doug/joblog/tvdb.log"
 
