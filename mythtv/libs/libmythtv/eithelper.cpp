3893de0159c12b51774844a6d3c4ad9ea027f7d1
Janne Grunau <jgrunau@mythtv.org>
2009-09-25 14:39:43 +0000

    Fixes EIT encoding of a single portuguese DVB-T transport. Closes #7117
    git-svn-id: http://svn.mythtv.org/svn/trunk@22037 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/eithelper.cpp
+++ mythtv/libs/libmythtv/eithelper.cpp
@@ -870,6 +870,9 @@ static void init_fixup(QMap<uint64_t,uint> &fix)
     // DVB-C Welho Finland
     fix[ 42249U << 16] = EITFixUp::kFixFI;
     fix[ 15U << 16] = EITFixUp::kFixFI;
+
+    // DVB-T TDT, Portugal (RTP 1, RTP 2, SIC, TVI)
+    fix[ 1101LL << 32 | 8904U << 16 ] = EITFixUp::kEFixForceISO8859_15;
 }
 
 static int calc_eit_utc_offset(void)
