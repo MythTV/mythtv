26bdf37e4140bda3e7d769f8b031fdb742f83685
Daniel Kristjansson <dkristjansson@mythtv.org>
2009-10-01 21:39:06 +0000

    Fixes #7014. clear pointers after free to avoid possible deref of free'd pointer in lirc code.
    git-svn-id: http://svn.mythtv.org/svn/trunk@22167 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythui/lirc_client.c
+++ mythtv/libs/libmythui/lirc_client.c
@@ -932,8 +932,8 @@ int lirc_readconfig(const struct lirc_state *state,
 		goto lirc_readconfig_compat;
 	}
 	
-	if(sha_bang!=NULL) free(sha_bang);
-	free(filename);
+	if(sha_bang!=NULL) { free(sha_bang); sha_bang = NULL; }
+	free(filename); filename = NULL;
 	
 	sockfd=socket(AF_UNIX,SOCK_STREAM,0);
 	if(sockfd==-1)
