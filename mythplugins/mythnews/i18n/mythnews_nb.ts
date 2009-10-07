11fb7900cd078077b5718a555e379e8e8975f61a
Robert McNamara <rmcnamara@mythtv.org>
2009-10-07 20:46:06 +0000

    Closes #7266.  Norwegian translation of MythNews.
    git-svn-id: http://svn.mythtv.org/svn/trunk@22301 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythnews/i18n/mythnews_nb.ts
+++ mythplugins/mythnews/i18n/mythnews_nb.ts
@@ -5,19 +5,19 @@
     <name>MythControls</name>
     <message>
         <source>RSS News feed reader</source>
-        <translation type="unfinished"></translation>
+        <translation>RSS Nyhetsfeed leser</translation>
     </message>
     <message>
         <source>Update news items</source>
-        <translation type="unfinished"></translation>
+        <translation>Oppdater nyhetselementer</translation>
     </message>
     <message>
         <source>Force update news items</source>
-        <translation type="unfinished"></translation>
+        <translation>Tving oppdatering av nyhetselementer</translation>
     </message>
     <message>
         <source>Cancel news item updating</source>
-        <translation type="unfinished"></translation>
+        <translation>Avbryt oppdatering av nyhetselementer</translation>
     </message>
 </context>
 <context>
@@ -60,7 +60,7 @@
     </message>
     <message>
         <source>%1 of %2 (%3 percent)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 av %2 (%3 prosent)</translation>
     </message>
 </context>
 <context>
@@ -102,27 +102,27 @@
     <name>NewsSite</name>
     <message>
         <source>Retrieve Failed. </source>
-        <translation type="unfinished">Klarte ikke hente. </translation>
+        <translation>Klarte ikke å hente. </translation>
     </message>
     <message>
         <source>Failed to retrieve news</source>
-        <translation type="unfinished">Klarte ikke hente nyheter</translation>
+        <translation>Klarte ikke å hente nyheter</translation>
     </message>
     <message>
         <source>No Cached News.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen nyheter i hurtiglager.</translation>
     </message>
     <message>
         <source>Failed to read downloaded file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke lese nedlastet fil.</translation>
     </message>
     <message>
         <source>Showing Cached News.</source>
-        <translation type="unfinished"></translation>
+        <translation>Viser nyheter fra hurtiglager.</translation>
     </message>
     <message>
         <source>XML-file is not valid RSS-feed</source>
-        <translation type="unfinished"></translation>
+        <translation>XML-filen er ikke en gyldig RSS feed</translation>
     </message>
 </context>
 <context>
@@ -136,11 +136,11 @@
     <name>ThemeUI</name>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished">Avbryt</translation>
+        <translation>Avbryt</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation type="unfinished">OK</translation>
+        <translation>OK</translation>
     </message>
 </context>
 </TS>
