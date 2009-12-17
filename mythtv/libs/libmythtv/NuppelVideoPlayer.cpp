e6e2ce00d8971b82b6f63651c0ffd96e077c49c2
Stuart Morgan <smorgan@mythtv.org>
2009-12-17 15:53:54 +0000

    Revert [22584] and [22585] until I can work on a fix for non-square pixel video resolutions.
    git-svn-id: http://svn.mythtv.org/svn/trunk@22975 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/NuppelVideoPlayer.cpp
+++ mythtv/libs/libmythtv/NuppelVideoPlayer.cpp
@@ -7082,8 +7082,14 @@ void NuppelVideoPlayer::DisplayAVSubtitles(void)
                     }
                 }
 
-                float hmult = osd->GetSubtitleBounds().width() / (float) video_disp_dim.width();
-                float vmult = osd->GetSubtitleBounds().height() / (float) video_disp_dim.height();
+                // scale the subtitle images which are scaled and positioned for
+                // a 720x576 video resolution to fit the current OSD resolution
+                float vsize = 576.0;
+                if (player_ctx->buffer->isDVD())
+                    vsize = (float) video_disp_dim.height();
+
+                float hmult = osd->GetSubtitleBounds().width() / 720.0;
+                float vmult = osd->GetSubtitleBounds().height() / vsize;
 
                 rect->x = (int)(rect->x * hmult);
                 rect->y = (int)(rect->y * vmult);
