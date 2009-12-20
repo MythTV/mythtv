80e66370230057dcf9c5c16416e0f69300ca5556
Paul Harrison <pharrison@mythtv.org>
2009-12-20 11:43:05 +0000

    Fix MythWeather's location searches following changes at the BBC's Weather Forecast site. Patch from Joe Ripley. Fixes #7758.
    git-svn-id: http://svn.mythtv.org/svn/trunk@22988 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythweather/mythweather/scripts/uk_bbc/BBCLocation.pm
+++ mythplugins/mythweather/mythweather/scripts/uk_bbc/BBCLocation.pm
@@ -12,19 +12,17 @@ my $resultcount = -1;
 
 sub Search {
 
-    my $base_url       = 'http://news.bbc.co.uk/weather/forecast/';
-    my $world_base_url = $base_url . '?lowgraphics=true&search=';
-    my $local_base_url = $base_url . '?lowgraphics=true&type=county_state&search=';
+    my $base_url       = 'http://news.bbc.co.uk/weather/util/search/Search.xhtml?';
+    my $world_base_url = $base_url . 'lowgraphics=true&region=world&search=';
+    my $local_base_url = $base_url . 'lowgraphics=true&region=uk&search=';
 
     my $search_string = shift;
 
     my $world_response = get $world_base_url . $search_string;
-    die unless defined $world_response;
     my $local_response = get $local_base_url . $search_string;
-    die unless defined $local_response;
 
-    &parseResults($world_response);
-    &parseResults($local_response);
+    &parseResults($world_response) if defined($world_response);
+    &parseResults($local_response) if defined($local_response);
 
     if ( ($resultcount > 0 ) && ($#searchresults < 0) ) {
         foreach my $url (@resulturl) {
@@ -40,8 +38,6 @@ sub Search {
 sub parseResults {
     my $response = shift;
     my $isresults = 0;
-    my $havename = 0;
-    my $haveid = 0;
     my $resultline = "";
 
     foreach (split("\n", $response)) {
@@ -59,14 +55,14 @@ sub parseResults {
             $resultcount = $1 if (/<strong>There \w{2,3} (\d*) forecasts? matching/);
 
             # Collect result URLs
-            if (/<a id=\"result_\d*\" href \=\"?.*search\=.*/) {
+            if (/<a id=\"result_\d*\" .* href \=\"?.*search\=.*/) {
                 $url = $_;
                 $url =~ s/.*href \=\"(.*)\".*/$1/s;
                 push (@resulturl, $url);
             }
 
             # Collect location IDs and location names
-            elsif (/<a id=\"result_\d*\" href \=\"\/weather\/forecast\//) {
+            elsif (/<a id=\"result_\d*\" .* href \=\"\/weather\/forecast\//) {
                 $locid = $_;
                 $locid =~ s/.*\/weather\/forecast\/(\d{0,5})\?.*/$1/s;
         
@@ -74,13 +70,10 @@ sub parseResults {
                 $locname =~ s/.*<a id=\"result_\d*\".*>(.*)<\/a>.*/$1/s;
 
                 $resultline = $locid . "::" . $locname;
-                push (@searchresults, $resultline);
+                if (! grep(/^$locid/, @searchresults)) {
+                    push (@searchresults, $resultline);
+                }
             }
-        }
-
-        if ($havename && $haveid) {
-            push (@searchresults, $resultline);
-            last;
         }
     }
 
