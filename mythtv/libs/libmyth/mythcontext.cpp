52811a70e429a95cec20d76ad149c460e810fcb8
Daniel Kristjansson <dkristjansson@mythtv.org>
2010-01-01 16:47:40 +0000

    Refs #7603. Backports [23052]. This fixes the immediate problem bam experienced, but doesn't backport the related fixes which are too invasive for backport.
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-22-fixes@23053 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/mythcontext.cpp
+++ mythtv/libs/libmyth/mythcontext.cpp
@@ -1251,7 +1251,7 @@ bool MythContextPrivate::WaitForWOL(int timeout_in_ms)
     int timeout_remaining = timeout_in_ms;
     while (WOLInProgress && (timeout_remaining > 0))
     {
-        VERBOSE(VB_GENERAL, LOC + "Wake-On-Lan in progress, waiting...");
+        VERBOSE(VB_GENERAL, LOC + "Wake-On-LAN in progress, waiting...");
 
         int max_wait = min(1000, timeout_remaining);
         WOLInProgressWaitCondition.wait(
@@ -1646,6 +1646,7 @@ MythSocket *MythContext::ConnectCommandSocket(
         }
         else if (!WOLcmd.isEmpty() && (cnt < maxConnTry))
         {
+            if (!we_attempted_wol)
             {
                 QMutexLocker locker(&d->WOLInProgressLock);
                 if (d->WOLInProgress)
