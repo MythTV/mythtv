e7b512bf3af3738b5f9895a6f7a285326d7f5912
Mike Dean <mdean@mythtv.org>
2010-01-14 02:33:26 +0000

    Remove the "Show Group Summary" setting widget.  The setting has been disabled s ince [19603] (the playbackbox port to MythUI).
    In the settings rework thread, removal of the Show Group Summary setting was proposed since it's no longer used.  We got 2 comments expressing a desire to keep the group summary itself, but no one made any case for allowing the group summary to be disabled.  Even though the setting itself is not actually used (and hasn't been for a year), the current behavior is the behavior both responders wanted:  MythTV always shows information about the selected group when in the groups list.  Therefore, this change removes the no-longer-used setting widget.
    
    Thanks to Tim Mackinnon for noticing the old widget and reminding us to remove it.  Refs #7905.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@23128 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythfrontend/globalsettings.cpp
+++ mythtv/programs/mythfrontend/globalsettings.cpp
@@ -425,17 +425,6 @@ static HostCheckBox *PBBStartInTitle()
     return gc;
 }
 
-static HostCheckBox *PBBShowGroupSummary()
-{
-    HostCheckBox *gc = new HostCheckBox("ShowGroupInfo");
-    gc->setLabel(QObject::tr("Show group summary"));
-    gc->setValue(false);
-    gc->setHelpText(QObject::tr("While selecting a group, show a group "
-                    "summary instead of showing info about the first episode "
-                    "in that group."));
-    return gc;
-}
-
 static HostCheckBox *SmartForward()
 {
     HostCheckBox *gc = new HostCheckBox("SmartForward");
@@ -4358,7 +4347,6 @@ PlaybackSettings::PlaybackSettings()
     pbox->addChild(PlaybackPreview());
     pbox->addChild(HWAccelPlaybackPreview());
     pbox->addChild(PBBStartInTitle());
-    pbox->addChild(PBBShowGroupSummary());
     addChild(pbox);
 
     VerticalConfigurationGroup* pbox2 = new VerticalConfigurationGroup(false);
