ef21fed33a8143bf2438a5ae5fc311a940b7496a
Stuart Morgan <smorgan@mythtv.org>
2010-01-15 20:08:22 +0000

    Set a max length of 4 on the year field in mythmusic.
    git-svn-id: http://svn.mythtv.org/svn/trunk@23146 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythmusic/mythmusic/cdrip.cpp
+++ mythplugins/mythmusic/mythmusic/cdrip.cpp
@@ -502,6 +502,7 @@ bool Ripper::Create(void)
     connect(m_searchGenreButton, SIGNAL(Clicked()), SLOT(searchGenre()));
     connect(m_genreEdit, SIGNAL(valueChanged()), SLOT(genreChanged()));
     m_yearEdit->SetFilter((InputFilter)(FilterAlpha | FilterSymbols | FilterPunct));
+    m_yearEdit->SetMaxLength(4);
     connect(m_yearEdit, SIGNAL(valueChanged()), SLOT(yearChanged()));
     connect(m_artistEdit, SIGNAL(valueChanged()), SLOT(artistChanged()));
     connect(m_searchArtistButton, SIGNAL(Clicked()), SLOT(searchArtist()));
