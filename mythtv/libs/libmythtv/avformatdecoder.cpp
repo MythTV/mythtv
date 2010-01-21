b55dcc1d2f5bb12ba646faeed787b1a0691e9bab
Mark Kendall <mkendall@mythtv.org>
2010-01-21 09:39:43 +0000

    Ignore unsupported DTS HD Audio frames. Patch provided by Taylor Ralph. Closes #7409.
    git-svn-id: http://svn.mythtv.org/svn/trunk@23232 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/avformatdecoder.cpp
+++ mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -3808,6 +3808,7 @@ bool AvFormatDecoder::GetFrame(DecodeType decodetype)
         while (!have_err && len > 0)
         {
             int ret = 0;
+            bool dts = false;
             switch (ctype)
             {
                 case CODEC_TYPE_AUDIO:
@@ -3924,7 +3925,7 @@ bool AvFormatDecoder::GetFrame(DecodeType decodetype)
                     if (audioOut.do_passthru)
                     {
                         data_size = pkt->size;
-                        bool dts = CODEC_ID_DTS == curstream->codec->codec_id;
+                        dts = CODEC_ID_DTS == curstream->codec->codec_id;
                         ret = encode_frame(dts, ptr, len,
                                            audioSamples, data_size);
                         s = (char *)audioSamples;
@@ -4631,6 +4632,8 @@ static int encode_frame(bool dts, unsigned char *data, int len,
     if (dts)
     {
         enc_len = dts_syncinfo(data, &flags, &sample_rate, &bit_rate);
+        if (enc_len < 0)
+            return enc_len;
         int rate, sfreq, nblks;
         dts_decode_header(data, &rate, &nblks, &sfreq);
         nr_samples = nblks * 32;
@@ -4740,14 +4743,34 @@ static int dts_syncinfo(uint8_t *indata_ptr, int */*flags*/,
     return fsize;
 }
 
+// defines from libavcodec/dca.h
+#define DCA_MARKER_RAW_BE 0x7FFE8001
+#define DCA_MARKER_RAW_LE 0xFE7F0180
+#define DCA_MARKER_14B_BE 0x1FFFE800
+#define DCA_MARKER_14B_LE 0xFF1F00E8
+#define DCA_HD_MARKER     0x64582025
+
 static int dts_decode_header(uint8_t *indata_ptr, int *rate,
                              int *nblks, int *sfreq)
 {
     uint id = ((indata_ptr[0] << 24) | (indata_ptr[1] << 16) |
                (indata_ptr[2] << 8)  | (indata_ptr[3]));
 
-    if (id != 0x7ffe8001)
-        return -1;
+    switch (id)
+    {
+        case DCA_MARKER_RAW_BE:
+            break;
+        case DCA_MARKER_RAW_LE:
+        case DCA_MARKER_14B_BE:
+        case DCA_MARKER_14B_LE:
+        case DCA_HD_MARKER:
+            VERBOSE(VB_AUDIO+VB_EXTRA, LOC + "Unsupported DTS frame");
+            return -1;
+            break;
+        default:
+            VERBOSE(VB_IMPORTANT, LOC_ERR + "Unknown DTS frame");
+            return -1;
+    }
 
     int ftype = indata_ptr[4] >> 7;
 
