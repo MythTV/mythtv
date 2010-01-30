becea980639aa3232449fbdf4c580c8ec05db405
Raymond Wagner <rwagner@mythtv.org>
2010-01-30 01:25:12 +0000

    Bump internal schema version following [23364].
    git-svn-id: http://svn.mythtv.org/svn/trunk@23365 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/bindings/python/MythTV/MythStatic.py
+++ mythtv/bindings/python/MythTV/MythStatic.py
@@ -4,7 +4,7 @@
 Contains any static and global variables for MythTV Python Bindings
 """
 
-SCHEMA_VERSION = 1253
+SCHEMA_VERSION = 1254
 MVSCHEMA_VERSION = 1032
 NVSCHEMA_VERSION = 1004
 PROTO_VERSION = 56
