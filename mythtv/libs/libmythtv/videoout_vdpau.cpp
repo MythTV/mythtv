MythTV/mythtv

commit 06928e53219130251b958ff20e7373fc99890914

Enable support for MPEG4 hardware decoding with VDPAU. Refs r23525.
git-svn-id: http://svn.mythtv.org/svn/trunk@23530 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/videoout_vdpau.cpp
@@ -560,6 +560,9 @@ void VideoOutputVDPAU::DrawSlice(VideoFrame *frame, int x, int y, int w, int h)
             case PIX_FMT_VDPAU_MPEG2:
                 vdp_decoder_profile = VDP_DECODER_PROFILE_MPEG2_MAIN;
                 break;
+            case PIX_FMT_VDPAU_MPEG4:
+                vdp_decoder_profile = VDP_DECODER_PROFILE_MPEG4_PART2_ASP;
+                break;
             case PIX_FMT_VDPAU_H264:
                 vdp_decoder_profile = VDP_DECODER_PROFILE_H264_HIGH;
                 break;
@@ -863,6 +866,7 @@ MythCodecID VideoOutputVDPAU::GetBestSupportedCodec(
 
     MythCodecID test_cid = (MythCodecID)(kCodec_MPEG1_VDPAU + (stream_type-1));
     use_cpu |= !codec_is_vdpau_hw(test_cid);
+    use_cpu |= !MythRenderVDPAU::IsMPEG4Available();
     if ((dec != "vdpau") || getenv("NO_VDPAU") || use_cpu)
         return (MythCodecID)(kCodec_MPEG1 + (stream_type-1));
 

mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -1280,6 +1280,7 @@ static bool IS_VDPAU_PIX_FMT(enum PixelFormat fmt)
         fmt == PIX_FMT_VDPAU_H264  ||
         fmt == PIX_FMT_VDPAU_MPEG1 ||
         fmt == PIX_FMT_VDPAU_MPEG2 ||
+        fmt == PIX_FMT_VDPAU_MPEG4 ||
         fmt == PIX_FMT_VDPAU_WMV3  ||
         fmt == PIX_FMT_VDPAU_VC1;
 }

mythtv/libs/libmythtv/mythcodecid.cpp
@@ -212,7 +212,7 @@ int myth2av_codecid(MythCodecID codec_id,
             VERBOSE(VB_IMPORTANT, "Error: VDPAU H.263 not supported by ffmpeg");
             break;
         case kCodec_MPEG4_VDPAU:
-            VERBOSE(VB_IMPORTANT, "Error: VDPAU MPEG4 not supported by ffmpeg");
+            ret = CODEC_ID_MPEG4;
             break;
 
         case kCodec_H264_VDPAU:

mythtv/libs/libmythtv/mythcodecid.h
@@ -92,8 +92,7 @@ typedef enum
 #define codec_is_vdpau(id)    (id > kCodec_VDPAU_BEGIN) &&\
                               (id < kCodec_VDPAU_END)
 #define codec_is_vdpau_hw(id) (codec_is_vdpau(id) &&\
-                              (id != kCodec_H263_VDPAU) &&\
-                              (id != kCodec_MPEG4_VDPAU))
+                              (id != kCodec_H263_VDPAU))
 
 QString get_encoding_type(MythCodecID codecid);
 QString get_decoder_name(MythCodecID codec_id, bool libmpeg2);

mythtv/libs/libmythui/mythrender_vdpau.cpp
@@ -2030,6 +2030,7 @@ void MythRenderVDPAU::ResetVideoSurfaces(void)
                 it.value().m_render.info.h264.referenceFrames[i].surface =
                     old_surfaces[ref];
         }
+
         // VC1
         fwd  = it.value().m_render.info.vc1.forward_reference;
         back = it.value().m_render.info.vc1.backward_reference;
@@ -2037,6 +2038,14 @@ void MythRenderVDPAU::ResetVideoSurfaces(void)
             it.value().m_render.info.vc1.forward_reference = old_surfaces[fwd];
         if (back != VDP_INVALID_HANDLE && old_surfaces.contains(back))
             it.value().m_render.info.vc1.backward_reference = old_surfaces[back];
+
+        // MPEG4
+        fwd  = it.value().m_render.info.mpeg4.forward_reference;
+        back = it.value().m_render.info.mpeg4.backward_reference;
+        if (fwd != VDP_INVALID_HANDLE && old_surfaces.contains(fwd))
+            it.value().m_render.info.mpeg4.forward_reference = old_surfaces[fwd];
+        if (back != VDP_INVALID_HANDLE && old_surfaces.contains(back))
+            it.value().m_render.info.mpeg4.backward_reference = old_surfaces[back];
     }
 
     if (ok)

commit ff9413d8ef93fa172dbf90243da2a20a70c53451

Fix VDPAU hardware decoding for 'older' gpus that was broken in r23530.
git-svn-id: http://svn.mythtv.org/svn/trunk@23532 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/videoout_vdpau.cpp
@@ -866,7 +866,8 @@ MythCodecID VideoOutputVDPAU::GetBestSupportedCodec(
 
     MythCodecID test_cid = (MythCodecID)(kCodec_MPEG1_VDPAU + (stream_type-1));
     use_cpu |= !codec_is_vdpau_hw(test_cid);
-    use_cpu |= !MythRenderVDPAU::IsMPEG4Available();
+    if (test_cid == kCodec_MPEG4_VDPAU)
+        use_cpu |= !MythRenderVDPAU::IsMPEG4Available();
     if ((dec != "vdpau") || getenv("NO_VDPAU") || use_cpu)
         return (MythCodecID)(kCodec_MPEG1 + (stream_type-1));
 