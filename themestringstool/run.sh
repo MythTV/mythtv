039ffce451800078e27539e431aa57e51e99490d
Daniel Kristjansson <dkristjansson@mythtv.org>
2010-03-03 01:42:26 +0000

    Refs #8124. Exclude mythweather grabber strings from i18n. Ideally these would be translated into at least Spanish and French but I we'd probably need code changes for that first.
    git-svn-id: http://svn.mythtv.org/svn/trunk@23652 7dbf422c-18fa-0310-86e9-fd20926502f2

--- themestringstool/run.sh
+++ themestringstool/run.sh
@@ -13,11 +13,16 @@ pushd ..
     chmod a+x mythplugins
 popd > /dev/null
 
+# exclude mythweather us_nws strings
+chmod a-r ../mythplugins/mythweather/mythweather/scripts/us_nws/maps.xml
+
 for I in `ls ../mythplugins --file-type | grep "/$" | grep -v cleanup | grep -v mythweb` ; do
     pushd ../mythplugins/$I
     $TS . `pwd`/i18n > /dev/null
     popd > /dev/null
 done
+
+chmod a+r ../mythplugins/mythweather/mythweather/scripts/us_nws/maps.xml
 
 pushd .. > /dev/null
     svn st
