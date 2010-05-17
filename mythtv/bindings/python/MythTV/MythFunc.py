3566d3b311527d8a6b47f0fefbaa8212246a7a84
Raymond Wagner <rwagner@mythtv.org>
2010-05-17 06:39:17 +0000

    Backport from [24709].
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-23-fixes@24710 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/bindings/python/MythTV/MythFunc.py
+++ mythtv/bindings/python/MythTV/MythFunc.py
@@ -820,7 +820,7 @@ class MythDB( MythDBBase ):
 
     def getGuideData(self, chanid, date):
         return self.searchGuide(chanid=chanid, 
-                                custom=('DATE(starttime)=%s',date))
+                                custom=(('DATE(starttime)=%s',date),))
 
     def getSetting(self, value, hostname=None):
         if not hostname:
