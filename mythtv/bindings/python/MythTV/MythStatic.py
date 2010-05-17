90a96c27d056a23ce7a379ca30ad8c73ea6cb53c
Mike Dean <mdean@mythtv.org>
2010-05-17 22:07:10 +0000

    Update binary version and inform Python bindings of the DB schema change in [24722].
    Will likely require a make clean.  Plugins must be rebuilt.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@24725 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/bindings/python/MythTV/MythStatic.py
+++ mythtv/bindings/python/MythTV/MythStatic.py
@@ -5,7 +5,7 @@ Contains any static and global variables for MythTV Python Bindings
 """
 
 OWN_VERSION = (0,23,0,10)
-SCHEMA_VERSION = 1255
+SCHEMA_VERSION = 1256
 MVSCHEMA_VERSION = 1035
 NVSCHEMA_VERSION = 1007
 PROTO_VERSION = 57
