ed29dcb7bc177e332b55e448c105d117341ee481
Mark Kendall <mkendall@mythtv.org>
2010-07-07 16:02:13 +0000

    Add chapter handling support to MythBDPlayer.
    Some 'upstream' changes are still required in NuppelVideoPlayer before
    it will work.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@25283 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/mythbdplayer.cpp
+++ mythtv/libs/libmythtv/mythbdplayer.cpp
@@ -1,3 +1,4 @@
+#include "BDRingBuffer.h"
 #include "mythbdplayer.h"
 
 #define LOC     QString("BDPlayer: ")
@@ -5,4 +6,50 @@
 
 MythBDPlayer::MythBDPlayer(bool muted) : NuppelVideoPlayer(muted)
 {
+}
+
+int MythBDPlayer::GetNumChapters(void)
+{
+    if (player_ctx->buffer->BD() && player_ctx->buffer->BD()->IsOpen())
+        return player_ctx->buffer->BD()->GetNumChapters();
+    return 0;
+}
+
+int MythBDPlayer::GetCurrentChapter(void)
+{
+    uint total = GetNumChapters();
+    if (!total)
+        return 0;
+
+    for (int i = (total - 1); i > -1 ; i--)
+    {
+        uint64_t frame = player_ctx->buffer->BD()->GetChapterStartFrame(i);
+        if (framesPlayed >= frame)
+        {
+            VERBOSE(VB_PLAYBACK, LOC +
+                    QString("GetCurrentChapter(selected chapter %1 framenum %2)")
+                            .arg(i + 1).arg(frame));
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+int64_t MythBDPlayer::GetChapter(int chapter)
+{
+    uint total = GetNumChapters();
+    if (!total)
+        return -1;
+
+    return (int64_t)player_ctx->buffer->BD()->GetChapterStartFrame(chapter-1);
+}
+
+void MythBDPlayer::GetChapterTimes(QList<long long> &times)
+{
+    uint total = GetNumChapters();
+    if (!total)
+        return;
+
+    for (uint i = 0; i < total; i++)
+        times.push_back(player_ctx->buffer->BD()->GetChapterStartTime(i));
 }
--- mythtv/libs/libmythtv/mythbdplayer.h
+++ mythtv/libs/libmythtv/mythbdplayer.h
@@ -7,6 +7,10 @@ class MythBDPlayer : public NuppelVideoPlayer
 {
   public:
     MythBDPlayer(bool muted = false);
+    virtual int     GetNumChapters(void);
+    virtual int     GetCurrentChapter(void);
+    virtual void    GetChapterTimes(QList<long long> &times);
+    virtual int64_t GetChapter(int chapter);
 };
 
 #endif // MYTHBDPLAYER_H
