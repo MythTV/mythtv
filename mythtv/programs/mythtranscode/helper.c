4056fb1308e05002c61c9ec463449c76241682b3
Janne Grunau <jgrunau@mythtv.org>
2010-07-28 21:36:22 +0000

    mythtranscode: simplify copy_quant_matrix
    git-svn-id: http://svn.mythtv.org/svn/trunk@25460 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythtranscode/helper.c
+++ mythtv/programs/mythtranscode/helper.c
@@ -1,20 +1,14 @@
 #include "config.h"
 #include <inttypes.h>
-#include <stdlib.h> /* defines NULL */
-#include <string.h> /* memcmp */
 
-#include "mpeg2.h"
-#include "attributes.h"
 #include "mpeg2_internal.h"
 
-extern uint8_t mpeg2_scan_norm[64] ATTR_ALIGN(16);
+extern uint16_t inv_zigzag_direct16[64];
+
 void copy_quant_matrix(mpeg2dec_t *dec, uint16_t *dest)
 {
   int i;
-  uint8_t reverse[64];
   for(i=0; i< 64; i++)
-    reverse[mpeg2_scan_norm[i]]=i;
-  for(i=0; i< 64; i++)
-    dest[reverse[i]]=dec->quantizer_matrix[0][i];
+    dest[inv_zigzag_direct16[i]-1] = dec->quantizer_matrix[0][i];
 }
 
