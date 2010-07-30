MythTV/mythtv

commit 0ddb01b6440f7c402af1eda2d18ea7db4fcd438a

Fix type handling for joinInt, for dealing with split integers over myth protocol. Fix issue with data becoming out-of-sync, if the backend responds to a command after the bindings have timed out.
Add handling for timeouts in FileTransfer.
Add FILE_FAILED_SEEK error code for use in FileTransfer.
Add logging to MSearch class.


git-svn-id: http://svn.mythtv.org/svn/trunk@25438 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/bindings/python/MythTV/connections.py
@@ -385,6 +385,8 @@ class BEConnection( object ):
 
         # send command string
         if data is not None:
+            # flush socket
+            self.backendCommand(deadline=0.)
             self.socket.sendheader(data.encode('utf-8'))
 
         # lock socket access
@@ -415,7 +417,8 @@ class BEConnection( object ):
                         # no sent data, no further response expected
                         return u''
                 else:
-                    return event
+                    if data is not None:
+                        return event
 
                 if t >= deadline:
                     break
@@ -433,7 +436,7 @@ class BEConnection( object ):
     def eventloop(self):
         self.threadrunning = True
         while (len(self._regevents) > 0) and self.connected:
-            self.backendCommand(deadline=0.001)
+            self.backendCommand(deadline=0.)
             sleep(0.1)
         self.threadrunning = False
 

mythtv/bindings/python/MythTV/mythproto.py
@@ -327,10 +327,12 @@ class FileTransfer( BEEvent ):
         def recv(self, count):
             return self.socket.dlrecv(count)
 
+    def __str__(self):
+        return 'myth://%s@%s/%s' % (self.sgroup, self.host, self.filename)
+
     def __repr__(self):
-        return "<open file 'myth://%s@%s/%s', mode '%s' at %s>" % \
-                          (self.sgroup, self.host, self.filename, \
-                                self.mode, hex(id(self)))
+        return "<open file '%s', mode '%s' at %s>" % \
+                          (str(self), self.mode, hex(id(self)))
 
     def __init__(self, host, filename, sgroup, mode, db=None):
         self.filename = filename
@@ -398,6 +400,12 @@ class FileTransfer( BEEvent ):
                                 [str(self._sockno),
                                  'REQUEST_BLOCK',
                                  str(ct)]))
+
+            if res == '':
+                # complete failure, hard reset position and retry
+                self._count = 0
+                self.seek(self._pos)
+                continue
 
             if int(res) == ct:
                 if (self._count >= 5) and (self._tsize < self._tmax):
@@ -487,6 +495,9 @@ class FileTransfer( BEEvent ):
                          str(whence),
                          str(curhigh),str(curlow)])\
                  ).split(BACKEND_SEP)
+        if res[0] == '-1':
+            raise MythFileError(MythError.FILE_FAILED_SEEK, \
+                                    str(self), offset, whence)
         self._pos = self.joinInt(*res)
 
 class RecordFileTransfer( FileTransfer ):
@@ -748,7 +759,7 @@ class Program( DictData, RECSTATUS, CMPRecord ):
                      0,      0,      4,
                      4,      3,      3,
                      3,      3,      3,
-                     3,      1,      3,
+                     4,      1,      3,
                      3,      0,      3,
                      3,      0,      0,
                      0,      0]

mythtv/bindings/python/MythTV/static.py
@@ -4,7 +4,7 @@
 Contains any static and global variables for MythTV Python Bindings
 """
 
-OWN_VERSION = (0,23,0,17)
+OWN_VERSION = (0,23,0,18)
 SCHEMA_VERSION = 1261
 MVSCHEMA_VERSION = 1036
 NVSCHEMA_VERSION = 1007
@@ -176,6 +176,7 @@ class ERRCODES( object ):
     FILE_ERROR          = 200
     FILE_FAILED_READ    = 201
     FILE_FAILED_WRITE   = 202
+    FILE_FAILED_SEEK    = 203
 
 class MythSchema( object ):
     _schema_value = 'DBSchemaVer'

mythtv/bindings/python/MythTV/altdict.py
@@ -90,6 +90,8 @@ class DictData( OrdDict ):
     Modified OrdDict, with a pre-defined item order. Allows processing of
         raw input data.
     """
+    _field_order = None
+    _field_type = None
     def __setattr__(self, name, value):
         if name in self._localvars:
             self.__dict__[name] = value

mythtv/bindings/python/MythTV/exceptions.py
@@ -12,6 +12,7 @@ class MythError( Exception, ERRCODES ):
     Error string will be available as obj.args[0].  Additional attributes
         may be available depending on the error code.
     """
+    ecode = None
 
     def __init__(self, *args):
         if args[0] == self.SYSTEM:
@@ -22,7 +23,7 @@ class MythError( Exception, ERRCODES ):
             self.ename = 'SOCKET'
             self.ecode, (self.sockcode, self.sockerr) = args
             self.args = ("Socket Error: %s" %self.sockerr,)
-        else:
+        elif self.ecode is not None:
             self.ename = 'GENERIC'
             self.ecode = self.GENERIC
             self.args = args
@@ -84,9 +85,7 @@ class MythDBError( MythError ):
             else:
                 self.sqlcode, self.sqlerr = sqlerr
                 self.args = ("Schema update failure %d: %s" % sqlerr,)
-        else:
-            MythError.__init__(self, *args)
-        self.message = str(self.args[0])
+        MythError.__init__(self, *args)
 
 class MythBEError( MythError ): 
     """
@@ -119,9 +118,7 @@ class MythBEError( MythError ):
             self.ename = 'PROTO_PROGRAMINFO'
             self.ecode = args[0]
             self.args = ("Received invalid field count for program info",)
-        else:
-            MythError.__init__(self, *args)
-        self.message = str(self.args[0])
+        MythError.__init__(self, *args)
 
 class MythFEError( MythError ):
     """
@@ -143,9 +140,7 @@ class MythFEError( MythError ):
             self.ecode, self.frontend, self.port = args
             self.args = ('Open socket at %s:%d not recognized as mythfrontend'\
                                             % (self.frontend, self.port),)
-        else:
-            MythError.__init__(self, *args)
-        self.message = str(self.args[0])
+        MythError.__init__(self, *args)
 
 class MythFileError( MythError ):
     """
@@ -169,8 +164,12 @@ class MythFileError( MythError ):
         elif args[0] == self.FILE_FAILED_WRITE:
             self.ename = 'FILE_FAILED_WRITE'
             self.ecode, self.file, self.reason = args
-            self.args = ("Error writing to %s, %s" % (self.file, self.reason),)
-        else:
-            MythError.__init__(self, *args)
-        self.message = str(self.args[0])
+            self.args = ("Error writing to %s, %s" % \
+                    (self.file, self.reason),)
+        elif args[0] == self.FILE_FAILED_SEEK:
+            self.ename = 'FILE_FAILED_SEEK'
+            self.ecode, self.file, self.offset, self.whence = args
+            self.args = ("Error seeking %s to %d,%d" % \
+                    (self.file, self.offset, self.whence),)
+        MythError.__init__(self, *args)
 

mythtv/bindings/python/MythTV/msearch.py
@@ -2,6 +2,7 @@
 """Provides tools for UPNP searches"""
 
 from exceptions import MythError
+from logging import MythLog
 
 from time import time
 import socket
@@ -11,6 +12,7 @@ class MSearch( object ):
     Opens a socket for performing UPNP searches.
     """
     def __init__(self):
+        self.log = MythLog('Python M-Search')
         port = 1900
         addr = '239.255.255.250'
         self.dest = (addr, port)
@@ -38,6 +40,7 @@ class MSearch( object ):
                 content-length,   request,   date,   usn,    location,
                 cache-control,    server,    ext,    st
         """
+        self.log(MythLog.UPNP, 'running UPnP search')
         sock = self.sock
         sreq = '\r\n'.join(['M-SEARCH * HTTP/1.1',
                             'HOST: %s:%s' % self.dest,
@@ -69,6 +72,7 @@ class MSearch( object ):
                 if sdict['st'] not in filter:
                     continue
 
+            self.log(MythLog.UPNP, sdict['st'], sdict['location'])
             yield sdict
 
     def searchMythBE(self, timeout=5.0):

mythtv/bindings/python/MythTV/utility.py
@@ -238,12 +238,12 @@ class SplitInt( object ):
     @staticmethod
     def joinInt(high,low):
         """obj.joinInt(high, low) -> 64-bit int, from two signed ints"""
-        return (high + (low<0))*2**32 + low
+        return (int(high) + (int(low)<0))*2**32 + int(low)
 
     @staticmethod
-    def splitInt(integer):
+    def splitInt(dint):
         """obj.joinInt(64-bit int) -> (high, low)"""
-        return integer/(2**32),integer%2**32 - (integer%2**32 > 2**31)*2**32
+        return dint/(2**32),dint%2**32 - (dint%2**32 > 2**31)*2**32
 
 class CMPVideo( object ):
     """

commit 99d48573cdde2727bbfc89b7a77943f4324f62f7

Add 'gethostname()' method to DBCache class to handle alternate hostnames defined in config.xml. Fix issue with event handler caused by r25438.
git-svn-id: http://svn.mythtv.org/svn/trunk@25490 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/bindings/python/MythTV/connections.py
@@ -258,7 +258,8 @@ class BEConnection( object ):
                     res[key] = True
             return res
 
-    def __init__(self, backend, port, opts=None, deadline=10.0):
+    def __init__(self, backend, port, localname=None, \
+                            opts=None, deadline=10.0):
         """
         BEConnection(backend, type, db=None) -> backend socket connection
 
@@ -276,8 +277,11 @@ class BEConnection( object ):
         self.host = backend
         self.port = port
         self.hostname = None
-        self.localname = socket.gethostname()
         self.deadline = deadline
+
+        self.localname = localname
+        if self.localname is None:
+            self.localname = socket.gethostname()
 
         self.opts = opts
         if self.opts is None:
@@ -436,7 +440,7 @@ class BEConnection( object ):
     def eventloop(self):
         self.threadrunning = True
         while (len(self._regevents) > 0) and self.connected:
-            self.backendCommand(deadline=0.)
+            self.backendCommand(deadline=0.001)
             sleep(0.1)
         self.threadrunning = False
 

mythtv/bindings/python/MythTV/database.py
@@ -907,12 +907,11 @@ class DBCache( MythSchema ):
                 # push data to new settings file
                 self._writeXML(dbconn)
 
-        if 'DBPort' not in dbconn:
-            dbconn['DBPort'] = 3306
-        else:
-            dbconn['DBPort'] = int(dbconn['DBPort'])
+        dbconn['DBPort'] = int(dbconn['DBPort'])
         if dbconn['DBPort'] == 0:
             dbconn['DBPort'] = 3306
+        if dbconn['LocalHostName'] is None:
+            dbconn['LocalHostName'] = gethostname()
 
         self.dbconn = dbconn
         self.ident = "sql://%s@%s:%d/" % \
@@ -939,7 +938,7 @@ class DBCache( MythSchema ):
 
     def _readXML(self, path):
         dbconn = { 'DBHostName':None,  'DBName':None, 'DBUserName':None,
-                   'DBPassword':None,  'DBPort':0}
+                   'DBPassword':None,  'DBPort':0,    'LocalHostName':None}
         try:
             config = etree.parse(path).getroot()
             for child in config.find('UPnP').find('MythFrontend').\
@@ -1011,6 +1010,9 @@ class DBCache( MythSchema ):
             self.db = None
             raise MythDBError(MythError.DB_SCHEMAMISMATCH, value, sver, local)
 
+    def gethostname(self):
+        return self.dbconn['LocalHostName']
+
     def getStorageGroup(self, groupname=None, hostname=None):
         """
         obj.getStorageGroup(groupname=None, hostname=None)
@@ -1069,7 +1071,7 @@ class StorageGroup( DBData ):
 
     def _evalwheredat(self, wheredat=None):
         DBData._evalwheredat(self, wheredat)
-        if (self.hostname == gethostname()) or \
+        if (self.hostname == self._db.gethostname()) or \
               os.access(self.dirname.encode('utf-8'), os.F_OK):
             self.local = True
         else:

mythtv/bindings/python/MythTV/mythproto.py
@@ -109,7 +109,8 @@ class BECache( SplitInt ):
             self.be.reconnect()
         else:
             # no existing connection, create new
-            self.be = BEConnection(self.host, self.port, self.opts)
+            self.be = BEConnection(self.host, self.port, \
+                                    self.db.gethostname(), self.opts)
             self.be.registeruser(self._uuid, self.opts)
             self._shared[self._ident] = self.be
 
@@ -293,11 +294,11 @@ class FileTransfer( BEEvent ):
     logmodule = 'Python FileTransfer'
 
     class BETransConn( BEConnection ):
-        def __init__(self, host, port, filename, sgroup, mode):
+        def __init__(self, host, port, localname, filename, sgroup, mode):
             self.filename = filename
             self.sgroup = sgroup
             self.mode = mode
-            BEConnection.__init__(self, host, port)
+            BEConnection.__init__(self, host, port, localname)
 
         def announce(self):
             if self.mode == 'r':
@@ -342,8 +343,8 @@ class FileTransfer( BEEvent ):
         # open control socket
         BEEvent.__init__(self, host, True, db=db)
         # open transfer socket
-        self.ftsock = self.BETransConn(self.host, self.port, self.filename,
-                                       self.sgroup, self.mode)
+        self.ftsock = self.BETransConn(self.host, self.port, \
+                    self.be.localname, self.filename, self.sgroup, self.mode)
         self.open = True
 
         self._sockno = self.ftsock._sockno

mythtv/bindings/python/MythTV/static.py
@@ -4,7 +4,7 @@
 Contains any static and global variables for MythTV Python Bindings
 """
 
-OWN_VERSION = (0,23,0,19)
+OWN_VERSION = (0,23,0,20)
 SCHEMA_VERSION = 1262
 MVSCHEMA_VERSION = 1036
 NVSCHEMA_VERSION = 1007

mythtv/bindings/python/MythTV/system.py
@@ -10,7 +10,6 @@ from altdict import DictData, OrdDict
 from database import DBCache
 
 from subprocess import Popen
-from socket import gethostname
 import xml.etree.cElementTree as etree
 
 class System( DBCache ):
@@ -29,7 +28,7 @@ class System( DBCache ):
         if path is not None:
             self.path = path
         elif setting is not None:
-            host = gethostname()
+            host = self.gethostname()
             self.path = self.settings[host][setting]
             if self.path is None:
                 raise MythDBError(MythError.DB_SETTING, setting, host)