edc41f84b18c324941ecdf51a4848ccb9fcc0962
Rob Smith <rsmith@mythtv.org>
2010-07-31 04:00:00 +0000

    Re [25498], this allows shows upto a day in the past to be considered in the scheduler query. This should prevent any issues introducted. Thanks to David Engel for the catch.
    git-svn-id: http://svn.mythtv.org/svn/trunk@25517 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythbackend/scheduler.cpp
+++ mythtv/programs/mythbackend/scheduler.cpp
@@ -3294,7 +3294,7 @@ void Scheduler::AddNewRecords(void)
 "      recduplicate = (recorded.endtime IS NOT NULL), "
 "      findduplicate = (oldfind.findid IS NOT NULL), "
 "      oldrecstatus = oldrecorded.recstatus "
-" WHERE program.endtime >= NOW() "
+" WHERE program.endtime >= NOW() - INTERVAL 1 DAY "
 );
     rmquery.replace("RECTABLE", schedTmpRecord);
 
@@ -3340,7 +3340,7 @@ void Scheduler::AddNewRecords(void)
         "ON ( oldrecstatus.station   = c.callsign  AND "
         "     oldrecstatus.starttime = p.starttime AND "
         "     oldrecstatus.title     = p.title ) "
-        "WHERE p.endtime >= NOW() "
+        "WHERE p.endtime >= NOW() - INTERVAL 1 DAY "
         "ORDER BY RECTABLE.recordid DESC ");
     query.replace("RECTABLE", schedTmpRecord);
 
