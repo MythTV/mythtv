8a58bf40afcaa1b9482c3d463dfd25ea15f3eea9
Nigel Pearson <npearson@mythtv.org>
2010-07-31 10:04:05 +0000

    Re-enable building mythtranscode on OS X. May help on Windows too? (untested)
    git-svn-id: http://svn.mythtv.org/svn/trunk@25521 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythtranscode/helper.c
+++ mythtv/programs/mythtranscode/helper.c
@@ -1,8 +1,8 @@
 #include "config.h"
 #include <inttypes.h>
 
-#include "mpeg2.h"
-#include "attributes.h"
+#include "mpeg2.h"          // for mpeg2_decoder_t, mpeg2_fbuf_t, et c
+#include "attributes.h"     // for ATTR_ALIGN()
 #include "mpeg2_internal.h"
 
 extern uint16_t inv_zigzag_direct16[64];
