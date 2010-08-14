52e4197e24e6678c035e2d51de65b40e35aef68b
Mark Kendall <mkendall@mythtv.org>
2010-08-14 09:28:02 +0000

    Add VDPAU as a UI painter option in the Appearance settings.
    The VDPAU painter is fully functional and using it allows for improved
    memory management when using VDPAU for video acceleration.
    
    See r25644. Refs#7570
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@25645 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/programs/mythfrontend/globalsettings.cpp
+++ mythtv/programs/mythfrontend/globalsettings.cpp
@@ -2847,7 +2847,12 @@ static HostComboBox *ThemePainter()
     gc->addSelection(QObject::tr("Direct3D"), "d3d9");
 #endif
     gc->addSelection(QObject::tr("Qt"), "qt");
+#ifdef USING_OPENGL
     gc->addSelection(QObject::tr("OpenGL"), "opengl");
+#endif
+#ifdef USING_VDPAU
+    gc->addSelection("VDPAU", "vdpau");
+#endif
     gc->setHelpText(QObject::tr("This selects what MythTV uses to draw. If "
                     "you have decent hardware, select OpenGL."));
     return gc;
