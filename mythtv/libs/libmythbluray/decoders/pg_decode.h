065679a42cbf82d8e9815d10280a51d71a167f9a
Robert McNamara <rmcnamara@mythtv.org>
2010-08-24 15:10:52 +0000

    libmythbluray: Add missing endif.  Wasn't hurting us, but it pays to be correct.
    git-svn-id: http://svn.mythtv.org/svn/trunk@25832 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythbluray/decoders/pg_decode.h
+++ mythtv/libs/libmythbluray/decoders/pg_decode.h
@@ -57,4 +57,8 @@ BD_PRIVATE void pg_free_window(BD_PG_WINDOW **p);
 BD_PRIVATE void pg_free_palette(BD_PG_PALETTE **p);
 BD_PRIVATE void pg_free_object(BD_PG_OBJECT **p);
 
+#ifdef __cplusplus
+};
+#endif
+
 #endif // _PG_DECODE_H_
