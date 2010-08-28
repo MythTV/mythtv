a67a601f342459d80e12402867407095af88b53f
Gavin Hurlbut <ghurlbut@mythtv.org>
2010-08-28 19:12:50 +0000

    Tweak version.sh to use the MYTH_BINARY_VERSION right out of mythversion.sh rather than using sed to extract it.  Let the preprocessor do the work.
    Also, force version.sh to be run *before* building the libs subdir as after
    running configure, there were build failures in libmythdb as version.sh was
    somehow being run incorrectly.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@25910 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/version.sh
+++ mythtv/version.sh
@@ -33,15 +33,11 @@ case "${SOURCE_VERSION}" in
      ;;
 esac
 
-# Get a string like "0.21.20071125-1"
-BINARY_VERSION=$(grep MYTH_BINARY_VERSION \
-    "${SVNTREEDIR}/libs/libmythdb/mythversion.h" \
-    | sed -e 's/.*MYTH_BINARY_VERSION //')
-
 echo "#include \"mythexp.h\"" > .vers.new
+echo "#include \"mythversion.h\"" > .vers.new
 echo "const MPUBLIC char *myth_source_version = \"${SOURCE_VERSION}\";" >> .vers.new
 echo "const MPUBLIC char *myth_source_path = \"${SVNREPOPATH}\";" >> .vers.new
-echo "const MPUBLIC char *myth_binary_version = ${BINARY_VERSION};" >> .vers.new
+echo "const MPUBLIC char *myth_binary_version = MYTH_BINARY_VERSION;" >> .vers.new
 
 # check if the version strings are changed and update version.pro if necessary
 diff .vers.new version.cpp > .vers.diff 2>&1
