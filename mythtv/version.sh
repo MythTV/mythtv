6d8fe98204e4020317cd52df9e7d0044539fff6d
Janne Grunau <jgrunau@mythtv.org>
2010-08-29 22:22:54 +0000

    make version.sh output friendlier for non-svn checkouts
    The not expanded $$URL$$ is not really friendly in shell scripts
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@25974 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/version.sh
+++ mythtv/version.sh
@@ -19,9 +19,7 @@ else
 fi
 
 SVNTREEDIR=$1
-SVNREPOPATH=$(echo "$$URL$$" | sed -e 's,.*/svn/,,' \
-                            -e 's,/mythtv/version\.sh.*,,' \
-                            -e 's,/version\.sh.*,,')
+SVNREPOPATH="exported"
 
 SOURCE_VERSION=$(svnversion ${SVNTREEDIR} 2>/dev/null || echo Unknown)
 
@@ -30,18 +28,25 @@ case "${SOURCE_VERSION}" in
         if test -e $SVNTREEDIR/VERSION ; then
             . $SVNTREEDIR/VERSION
         fi
-     ;;
+    ;;
+    *)
+    SVNREPOPATH=$(echo "$$URL$$" | sed -e 's,.*/svn/,,' \
+                                       -e 's,/mythtv/version\.sh.*,,' \
+                                       -e 's,/version\.sh.*,,')
+    ;;
 esac
 
-echo "#include \"mythexp.h\"" > .vers.new
-echo "#include \"mythversion.h\"" >> .vers.new
-echo "const MPUBLIC char *myth_source_version = \"${SOURCE_VERSION}\";" >> .vers.new
-echo "const MPUBLIC char *myth_source_path = \"${SVNREPOPATH}\";" >> .vers.new
-echo "const MPUBLIC char *myth_binary_version = MYTH_BINARY_VERSION;" >> .vers.new
+cat > .vers.new <<EOF
+#include "mythexp.h"
+#include "mythversion.h"
+
+const MPUBLIC char *myth_source_version = "${SOURCE_VERSION}";
+const MPUBLIC char *myth_source_path = "${SVNREPOPATH}";
+const MPUBLIC char *myth_binary_version = MYTH_BINARY_VERSION;
+EOF
 
 # check if the version strings are changed and update version.pro if necessary
-diff .vers.new version.cpp > .vers.diff 2>&1
-if test -s .vers.diff ; then
+if ! cmp -s .vers.new version.cpp; then
    mv -f .vers.new version.cpp
 fi
-rm -f .vers.new .vers.diff
+rm -f .vers.new
