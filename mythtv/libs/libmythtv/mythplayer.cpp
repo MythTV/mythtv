MythTV/mythtv

commit ca705a223f60d3d8392c00ee8e88812d2e8c7354

Add a new cross platform display class, MythDisplay.
This consolidates various chunks of X11, OS X and windows code into one
wrapper class to provide information about the current display. The
OpenGL codebase is now entirely platform independant and OS X builds
should work with either Carbon or Cocoa based Qt libraries.

I may revisit MythXDisplay, the existing X11 specific class, in the
future but for the time being we just call it from MythDisplay.


git-svn-id: http://svn.mythtv.org/svn/trunk@25457 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythui/DisplayResOSX.cpp
@@ -6,10 +6,8 @@
 #import <Carbon/Carbon.h>
 #import <IOKit/graphics/IOGraphicsLib.h> // for IODisplayCreateInfoDictionary()
 
+#include "mythdisplay.h"
 #include "util-osx.h"
-
-CGDirectDisplayID mythtv_display();
-
 
 DisplayResOSX::DisplayResOSX(void)
 {
@@ -23,61 +21,21 @@ DisplayResOSX::~DisplayResOSX(void)
 bool DisplayResOSX::GetDisplayInfo(int &w_pix, int &h_pix, int &w_mm,
                                    int &h_mm, double &rate, double &par) const
 {
-    CGDirectDisplayID d = mythtv_display();
-
-    io_connect_t port = CGDisplayIOServicePort(d);
-    if (port == MACH_PORT_NULL )
-        return false;
-
-    CFDictionaryRef disp_dict = IODisplayCreateInfoDictionary(port, 0);
-    CFDictionaryRef mode_dict = CGDisplayCurrentMode(d);
-    if (!disp_dict || !mode_dict)
-        return false;
-
-    w_mm   = get_int_CF(disp_dict, CFSTR(kDisplayHorizontalImageSize));
-    h_mm   = get_int_CF(disp_dict, CFSTR(kDisplayVerticalImageSize));
-    w_pix  = get_int_CF(mode_dict, kCGDisplayWidth);
-    h_pix  = get_int_CF(mode_dict, kCGDisplayHeight);
-    rate   = (double) get_int_CF(mode_dict, kCGDisplayRefreshRate);
+    DisplayInfo info = MythDisplay::GetDisplayInfo();
+    w_mm   = info.res.width();
+    h_mm   = info.res.height();
+    w_pix  = info.size.width();
+    h_pix  = info.size.height();
+    rate   = info.rate;
     par    = 1.0;
-
-    //CFRelease(dict); // this release causes a segfault
-    
     return true;
-}
-
-CGDirectDisplayID mythtv_display()
-{
-    CGDirectDisplayID d = NULL;
-    
-    // Find the display containing the MythTV main window
-    Rect windowBounds;
-    if (!GetWindowBounds(FrontNonFloatingWindow(),
-                         kWindowContentRgn,
-                         &windowBounds))
-    {
-        CGPoint pt;
-        pt.x = windowBounds.left;
-        pt.y = windowBounds.top;
-        
-        CGDisplayCount ct;
-        if (CGGetDisplaysWithPoint(pt, 1, &d, &ct))
-        {
-            d = NULL;   // window is offscreen?
-        }
-    }
-    if (!d)
-    {
-        d = CGMainDisplayID();
-    }
-    return d;
 }
 
 bool DisplayResOSX::SwitchToVideoMode(int width, int height, double refreshrate)
 {
-    CGDirectDisplayID d = mythtv_display();
+    CGDirectDisplayID d = GetOSXDisplay(MythDisplay::GetWindowID());
     CFDictionaryRef dispMode = NULL;
-    int match = 0;
+    boolean_t match = 0;
 
     // find mode that matches the desired size
     if (refreshrate)
@@ -111,7 +69,7 @@ const DisplayResVector& DisplayResOSX::GetVideoModes() const
     if (m_video_modes.size())
         return m_video_modes;
 
-    CGDirectDisplayID d = mythtv_display();
+    CGDirectDisplayID d = GetOSXDisplay(MythDisplay::GetWindowID());
     CFArrayRef displayModes = CGDisplayAvailableModes(d);
     if (NULL == displayModes)
         return m_video_modes;

mythtv/libs/libmythui/DisplayResX.cpp
@@ -6,6 +6,7 @@
 
 #include "mythverbose.h"
 #include "mythdb.h"
+#include "mythdisplay.h"
 #include "mythxdisplay.h"
 #include "util-nvctrl.h"
 
@@ -28,29 +29,16 @@ DisplayResX::~DisplayResX(void)
 bool DisplayResX::GetDisplayInfo(int &w_pix, int &h_pix, int &w_mm,
                                  int &h_mm, double &rate, double &par) const
 {
-    bool success = false;
-    MythXDisplay *d = OpenMythXDisplay();
-    if (!d)
-        return success;
-
-    QSize mm  = d->GetDisplayDimensions();
-    QSize pix = d->GetDisplaySize();
-    double  rr = 1000000.0 / d->GetRefreshRate();
-
-    if (mm.width() > 0 && mm.height() > 0 &&
-        pix.width() > 0 && pix.height() > 0 && rr > 0)
-    {
-        rate = rr;
-        w_mm = mm.width();
-        h_mm = mm.height();
-        w_pix = pix.width();
-        h_pix = pix.height();
-        par = d->GetPixelAspectRatio();
-        success = true;
-    }
-
-    delete d;
-    return success;
+    DisplayInfo info = MythDisplay::GetDisplayInfo();
+    w_mm   = info.res.width();
+    h_mm   = info.res.height();
+    w_pix  = info.size.width();
+    h_pix  = info.size.height();
+    rate   = info.rate;
+    par    = 1.0;
+    if (w_mm > 0 && h_mm > 0 && w_pix > 0 && h_pix > 0)
+        par = ((double)w_mm  / (double)w_pix) / ((double)h_mm / (double)h_pix);
+    return true;
 }
 
 bool DisplayResX::SwitchToVideoMode(int width, int height, double desired_rate)

mythtv/libs/libmythui/mythdisplay.cpp
@@ -1,0 +1,138 @@
+#include "mythdisplay.h"
+#include "mythmainwindow.h"
+
+#if defined(Q_WS_MAC) 
+#import "util-osx.h"
+#elif defined(Q_WS_WIN)
+#include <windows.h>
+#elif defined(Q_WS_X11)
+#include "mythxdisplay.h"
+#elif USING_DIRECTFB
+#include <linux/fb.h>
+extern "C" {
+#include <directfb.h>
+#include <directfb_version.h>
+}
+#endif
+
+WId MythDisplay::GetWindowID(void)
+{
+    WId win = 0;
+    QWidget *main_widget = (QWidget*)MythMainWindow::getMainWindow();
+    if (main_widget)
+        win = main_widget->winId();
+    return win;
+}
+
+DisplayInfo MythDisplay::GetDisplayInfo(void)
+{
+    DisplayInfo ret;
+
+#if defined(Q_WS_MAC)
+    CGDirectDisplayID disp = GetOSXDisplay(GetWindowID());
+    if (!disp)
+        return ret;
+
+    CFDictionaryRef ref = CGDisplayCurrentMode(disp);
+    if (ref)
+    {
+        int rate = get_float_CF(ref, kCGDisplayRefreshRate);
+        // N.B. A rate of zero typically indicates the internal macbook
+        // lcd display which does not have a rate in the traditional sense
+        if (rate > 20 && rate < 200)
+            ret.rate = 1000000 / rate;
+    }
+
+    CGSize size_in_mm = CGDisplayScreenSize(disp);
+    ret.res = QSize((uint) size_in_mm.width, (uint) size_in_mm.height);
+
+    uint width  = (uint)CGDisplayPixelsWide(disp);
+    uint height = (uint)CGDisplayPixelsHigh(disp);
+    ret.size   = QSize(width, height);
+
+#elif defined(Q_WS_WIN)
+    HDC hdc = GetDC(GetWindowID());
+    if (hdc)
+    {
+        int rate = GetDeviceCaps(hdc, VREFRESH);
+        if (rate > 20 && rate < 200)
+        {
+            // see http://support.microsoft.com/kb/2006076
+            switch (rate)
+            {
+                case 23:  ret.rate = 41708; break; // 23.976Hz
+                case 29:  ret.rate = 33367; break; // 29.970Hz
+                case 47:  ret.rate = 20854; break; // 47.952Hz
+                case 59:  ret.rate = 16683; break; // 59.940Hz
+                case 71:  ret.rate = 13903; break; // 71.928Hz
+                case 119: ret.rate = 8342;  break; // 119.880Hz
+                default:  ret.rate = 1000000 / rate;
+            }
+        }
+        int width  = GetDeviceCaps(hdc, HORZSIZE);
+        int height = GetDeviceCaps(hdc, VERTSIZE);
+        ret.size   = QSize((uint)width, (uint)height);
+        width      = GetDeviceCaps(hdc, HORZRES);
+        height     = GetDeviceCaps(hdc, VERTRES);
+        ret.res    = QSize((uint)width, (uint)height);
+    }
+
+#elif defined (Q_WS_X11)
+    MythXDisplay *disp = OpenMythXDisplay();
+    if (!disp)
+        return ret;
+    
+    ret.rate = disp->GetRefreshRate();
+    ret.res  = disp->GetDisplaySize();
+    ret.size = disp->GetDisplayDimensions();
+    delete disp;
+#elif USING_DIRECTFB
+    int fh, v;
+    struct fb_var_screeninfo si;
+    double drate;
+    double hrate;
+    double vrate;
+    long htotal;
+    long vtotal;
+    const char *fb_dev_name = NULL;
+    if (!(fb_dev_name = getenv("FRAMEBUFFER")))
+        fb_dev_name = "/dev/fb0";
+
+    fh = open(fb_dev_name, O_RDONLY);
+    if (-1 == fh)
+        return ret;
+
+    if (ioctl(fh, FBIOGET_VSCREENINFO, &si))
+    {
+        close(fh);
+        return ret;
+    }
+
+    htotal = si.left_margin + si.xres + si.right_margin + si.hsync_len;
+    vtotal = si.upper_margin + si.yres + si.lower_margin + si.vsync_len;
+
+    switch (si.vmode & FB_VMODE_MASK)
+    {
+        case FB_VMODE_INTERLACED:
+            break;
+        case FB_VMODE_DOUBLE:
+            vtotal <<= 2;
+            break;
+        default:
+            vtotal <<= 1;
+    }
+
+    drate = 1E12 / si.pixclock;
+    hrate = drate / htotal;
+    vrate = hrate / vtotal * 2;
+
+    v = (int)(1E3 / vrate + 0.5);
+    /* h = hrate / 1E3; */
+    ret.rate = v;
+    close(fh);
+#endif
+    return ret;
+}
+
+
+

mythtv/libs/libmythui/mythdisplay.h
@@ -1,0 +1,26 @@
+#ifndef MYTHDISPLAY_H
+#define MYTHDISPLAY_H
+
+#include <QWindowsStyle>
+#include <QWidget>
+#include <QSize>
+
+class DisplayInfo
+{
+  public:
+    DisplayInfo(void)  : size(QSize(0,0)), res(QSize(0,0)), rate(-1) { }
+    DisplayInfo(int r) : size(QSize(0,0)), res(QSize(0,0)), rate(r)  { }
+
+    QSize size;
+    QSize res;
+    int   rate;
+};
+
+class MythDisplay
+{
+  public:
+    static DisplayInfo GetDisplayInfo(void);
+    static WId GetWindowID(void);
+};
+
+#endif // MYTHDISPLAY_H

mythtv/libs/libmythui/mythxdisplay.cpp
@@ -224,23 +224,6 @@ QSize MythXDisplay::GetDisplayDimensions(void)
     return QSize(displayWidthMM, displayHeightMM);
 }
 
-double MythXDisplay::GetPixelAspectRatio()
-{
-    double pixelAspect = 1.0;
-    QSize dim = GetDisplayDimensions();
-    QSize sz  = GetDisplaySize();
-
-    if ((dim.width() > 0) && (dim.height() > 0) &&
-        (sz.width()  > 0) && (sz.height()  > 0))
-    {
-        pixelAspect =
-            ((double)dim.width()  / (double)sz.width()) /
-            ((double)dim.height() / (double)sz.height());
-    }
-
-    return pixelAspect;
-}
-
 int MythXDisplay::GetRefreshRate(void)
 {
     XF86VidModeModeLine mode_line;

mythtv/libs/libmythui/mythxdisplay.h
@@ -34,7 +34,6 @@ class MPUBLIC MythXDisplay
     int      GetNumberXineramaScreens(void);
     QSize    GetDisplaySize(void);
     QSize    GetDisplayDimensions(void);
-    double   GetPixelAspectRatio(void);
     int      GetRefreshRate(void);
     void     Sync(bool flush = false);
     void     StartLog(void);

mythtv/libs/libmythtv/NuppelVideoPlayer.cpp
@@ -1555,7 +1555,7 @@ void NuppelVideoPlayer::InitAVSync(void)
 
     repeat_delay = 0;
 
-    refreshrate = videoOutput ? videoOutput->GetDisplayInfo().rate : 0;
+    refreshrate = MythDisplay::GetDisplayInfo().rate;
     if (refreshrate <= 0)
         refreshrate = frame_interval;
 
@@ -1955,9 +1955,7 @@ void NuppelVideoPlayer::VideoStart(void)
 
     float temp_speed = (play_speed == 0.0) ? audio.GetStretchFactor() : play_speed;
     uint fr_int = (int)(1000000.0 / video_frame_rate / temp_speed);
-    uint rf_int = 0;
-    if (videoOutput)
-        rf_int = videoOutput->GetDisplayInfo().rate;
+    uint rf_int = MythDisplay::GetDisplayInfo().rate;
 
     // Default to Interlaced playback to allocate the deinterlacer structures
     // Enable autodetection of interlaced/progressive from video stream

mythtv/libs/libmythtv/videoout_d3d.cpp
@@ -189,37 +189,6 @@ bool VideoOutputD3D::SetupContext()
     return true;
 }
 
-DisplayInfo VideoOutputD3D::GetDisplayInfo(void)
-{
-    DisplayInfo ret;
-    HDC hdc = GetDC(m_hWnd);
-    if (hdc)
-    {
-        int rate = GetDeviceCaps(hdc, VREFRESH);
-        if (rate > 20 && rate < 200)
-        {
-            // see http://support.microsoft.com/kb/2006076
-            switch (rate)
-            {
-                case 23:  ret.rate = 41708; break; // 23.976Hz
-                case 29:  ret.rate = 33367; break; // 29.970Hz
-                case 47:  ret.rate = 20854; break; // 47.952Hz
-                case 59:  ret.rate = 16683; break; // 59.940Hz
-                case 71:  ret.rate = 13903; break; // 71.928Hz
-                case 119: ret.rate = 8342;  break; // 119.880Hz
-                default:  ret.rate = 1000000 / rate;
-            }
-        }
-        int width  = GetDeviceCaps(hdc, HORZSIZE);
-        int height = GetDeviceCaps(hdc, VERTSIZE);
-        ret.size   = QSize((uint)width, (uint)height);
-        width      = GetDeviceCaps(hdc, HORZRES);
-        height     = GetDeviceCaps(hdc, VERTRES);
-        ret.res    = QSize((uint)width, (uint)height);
-    }
-    return ret;
-}
-
 bool VideoOutputD3D::Init(int width, int height, float aspect,
                           WId winid, int winx, int winy, int winw,
                           int winh, WId embedid)

commit 40cb32ab0660e497c71cc59c605ea401cecb6a5e

Fix the refresh rate reported for the Desktop video mode.
This was broken in r25457. I've also taken the opportunity to move the
rate variable to a float for increased accuracy.


git-svn-id: http://svn.mythtv.org/svn/trunk@25983 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/mythplayer.cpp
@@ -1596,7 +1596,7 @@ void MythPlayer::InitAVSync(void)
 
     repeat_delay = 0;
 
-    refreshrate = MythDisplay::GetDisplayInfo().rate;
+    refreshrate = (int)MythDisplay::GetDisplayInfo().rate;
     if (refreshrate <= 0)
         refreshrate = frame_interval;
 
@@ -1994,7 +1994,7 @@ void MythPlayer::VideoStart(void)
 
     float temp_speed = (play_speed == 0.0) ? audio.GetStretchFactor() : play_speed;
     uint fr_int = (int)(1000000.0 / video_frame_rate / temp_speed);
-    uint rf_int = MythDisplay::GetDisplayInfo().rate;
+    uint rf_int = (int)MythDisplay::GetDisplayInfo().rate;
 
     // Default to Interlaced playback to allocate the deinterlacer structures
     // Enable autodetection of interlaced/progressive from video stream

mythtv/libs/libmythui/DisplayResOSX.cpp
@@ -26,7 +26,7 @@ bool DisplayResOSX::GetDisplayInfo(int &w_pix, int &h_pix, int &w_mm,
     h_mm   = info.res.height();
     w_pix  = info.size.width();
     h_pix  = info.size.height();
-    rate   = info.rate;
+    rate   = 1000000.0f / info.rate;
     par    = 1.0;
     return true;
 }

mythtv/libs/libmythui/DisplayResX.cpp
@@ -34,7 +34,7 @@ bool DisplayResX::GetDisplayInfo(int &w_pix, int &h_pix, int &w_mm,
     h_mm   = info.res.height();
     w_pix  = info.size.width();
     h_pix  = info.size.height();
-    rate   = info.rate;
+    rate   = 1000000.0f / info.rate;
     par    = 1.0;
     if (w_mm > 0 && h_mm > 0 && w_pix > 0 && h_pix > 0)
         par = ((double)w_mm  / (double)w_pix) / ((double)h_mm / (double)h_pix);

mythtv/libs/libmythui/mythdisplay.cpp
@@ -36,11 +36,11 @@ DisplayInfo MythDisplay::GetDisplayInfo(void)
     CFDictionaryRef ref = CGDisplayCurrentMode(disp);
     if (ref)
     {
-        int rate = get_float_CF(ref, kCGDisplayRefreshRate);
+        float rate = get_float_CF(ref, kCGDisplayRefreshRate);
         // N.B. A rate of zero typically indicates the internal macbook
         // lcd display which does not have a rate in the traditional sense
         if (rate > 20 && rate < 200)
-            ret.rate = 1000000 / rate;
+            ret.rate = 1000000.0f / rate;
     }
 
     CGSize size_in_mm = CGDisplayScreenSize(disp);
@@ -66,7 +66,7 @@ DisplayInfo MythDisplay::GetDisplayInfo(void)
                 case 59:  ret.rate = 16683; break; // 59.940Hz
                 case 71:  ret.rate = 13903; break; // 71.928Hz
                 case 119: ret.rate = 8342;  break; // 119.880Hz
-                default:  ret.rate = 1000000 / rate;
+                default:  ret.rate = 1000000.0f / (float)rate;
             }
         }
         int width  = GetDeviceCaps(hdc, HORZSIZE);

mythtv/libs/libmythui/mythdisplay.h
@@ -15,7 +15,7 @@ class DisplayInfo
 
     QSize size;
     QSize res;
-    int   rate;
+    float rate;
 };
 
 class MPUBLIC MythDisplay

mythtv/libs/libmythui/mythxdisplay.cpp
@@ -108,9 +108,9 @@ QSize MythXGetDisplayDimensions(void)
     return ret;
 }
 
-int MythXGetRefreshRate(void)
+float MythXGetRefreshRate(void)
 {
-    int ret = -1;
+    float ret = -1;
     MythXDisplay *d = OpenMythXDisplay();
     if (d)
     {
@@ -224,7 +224,7 @@ QSize MythXDisplay::GetDisplayDimensions(void)
     return QSize(displayWidthMM, displayHeightMM);
 }
 
-int MythXDisplay::GetRefreshRate(void)
+float MythXDisplay::GetRefreshRate(void)
 {
     XF86VidModeModeLine mode_line;
     int dot_clock;
@@ -267,7 +267,7 @@ int MythXDisplay::GetRefreshRate(void)
 
     rate = 1000000.0 / rate;
 
-    return (int)rate;
+    return rate;
 }
 
 void MythXDisplay::Sync(bool flush)

mythtv/libs/libmythui/mythxdisplay.h
@@ -34,7 +34,7 @@ class MPUBLIC MythXDisplay
     int      GetNumberXineramaScreens(void);
     QSize    GetDisplaySize(void);
     QSize    GetDisplayDimensions(void);
-    int      GetRefreshRate(void);
+    float    GetRefreshRate(void);
     void     Sync(bool flush = false);
     void     StartLog(void);
     bool     StopLog(void);
@@ -74,7 +74,7 @@ MPUBLIC void          LockMythXDisplays(bool lock);
 MPUBLIC MythXDisplay *GetMythXDisplay(Display*);
 MPUBLIC MythXDisplay *OpenMythXDisplay(void);
 MPUBLIC QSize         MythXGetDisplayDimensions(void);
-MPUBLIC int           MythXGetRefreshRate(void);
+MPUBLIC float         MythXGetRefreshRate(void);
 #define XLOCK(dpy, arg) { dpy->Lock(); arg; dpy->Unlock(); }
 #endif // USING_X11
 