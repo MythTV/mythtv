MythTV/mythtv

commit 42590aa0535b895a9746edd880e1b8f7647f3630

Make the actual process of changing the channel part of the signal monitor.  Refs #6719
For LiveTV this allows feedback to be given to the user, instead of leaving
them in limbo until the channel change is complete.  Also allows for
situations where channel changes can take a very long time (e.g. using IR
blaster).

This does require the channel change process to be moved into it's own
thread, and is therefore a somewhat major change.


git-svn-id: http://svn.mythtv.org/svn/trunk@25543 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/tv_play.cpp
@@ -7137,6 +7137,7 @@ void TV::UpdateOSDSignal(const PlayerContext *ctx, const QStringList &strlist)
     float snr  = 0.0f;
     uint  ber  = 0xffffffff;
     int   pos  = -1;
+    int   tuned = -1;
     QString pat(""), pmt(""), mgt(""), vct(""), nit(""), sdt(""), crypt("");
     QString err = QString::null, msg = QString::null;
     for (it = slist.begin(); it != slist.end(); ++it)
@@ -7163,6 +7164,8 @@ void TV::UpdateOSDSignal(const PlayerContext *ctx, const QStringList &strlist)
             ber = it->GetValue();
         else if ("pos" == it->GetShortName())
             pos = it->GetValue();
+        else if ("tuned" == it->GetShortName())
+            tuned = it->GetValue();
         else if ("seen_pat" == it->GetShortName())
             pat = it->IsGood() ? "a" : "_";
         else if ("matching_pat" == it->GetShortName())
@@ -7196,6 +7199,7 @@ void TV::UpdateOSDSignal(const PlayerContext *ctx, const QStringList &strlist)
         infoMap["signal"] = QString::number(sig); // use normalized value
 
     bool    allGood = SignalMonitorValue::AllGood(slist);
+    char    tuneCode;
     QString slock   = ("1" == infoMap["slock"]) ? "L" : "l";
     QString lockMsg = (slock=="L") ? tr("Partial Lock") : tr("No Lock");
     QString sigMsg  = allGood ? tr("Lock") : lockMsg;
@@ -7208,9 +7212,18 @@ void TV::UpdateOSDSignal(const PlayerContext *ctx, const QStringList &strlist)
     if ((pos >= 0) && (pos < 100))
         sigDesc += " | " + tr("Rotor %1\%").arg(pos,2);
 
-    sigDesc = sigDesc + QString(" | (%1%2%3%4%5%6%7%8) %9")
-        .arg(slock).arg(pat).arg(pmt).arg(mgt).arg(vct)
-        .arg(nit).arg(sdt).arg(crypt).arg(sigMsg);
+    if (tuned == 1)
+        tuneCode = 't';
+    else if (tuned == 2)
+        tuneCode = 'F';
+    else if (tuned == 3)
+        tuneCode = 'T';
+    else
+        tuneCode = '_';
+
+    sigDesc = sigDesc + QString(" | (%1%2%3%4%5%6%7%8%9) %10")
+              .arg(tuneCode).arg(slock).arg(pat).arg(pmt).arg(mgt).arg(vct)
+              .arg(nit).arg(sdt).arg(crypt).arg(sigMsg);
 
     if (!err.isEmpty())
         sigDesc = err;

mythtv/libs/libmythtv/analogsignalmonitor.cpp
@@ -33,6 +33,9 @@ void AnalogSignalMonitor::UpdateValues(void)
     if (videofd < 0)
         return;
 
+    if (!IsChannelTuned())
+        return;
+
     bool isLocked = false;
     if (usingv4l2)
     {

mythtv/libs/libmythtv/channelbase.cpp
@@ -34,16 +34,95 @@ using namespace std;
 #define LOC_WARN QString("ChannelBase(%1) Warning: ").arg(GetCardID())
 #define LOC_ERR QString("ChannelBase(%1) Error: ").arg(GetCardID())
 
+/*
+ * Run the channel change thread, and report the status when done
+ */
+void ChannelThread::run(void)
+{
+    VERBOSE(VB_CHANNEL, "ChannelThread::run");
+    bool result = tuner->SetChannelByString(channel);
+    tuner->setStatus(result ?
+                     ChannelBase::changeSuccess : ChannelBase::changeFailed);
+}
+
 ChannelBase::ChannelBase(TVRec *parent)
     :
     m_pParent(parent), m_curchannelname(""),
-    m_currentInputID(-1), m_commfree(false), m_cardid(0)
+    m_currentInputID(-1), m_commfree(false), m_cardid(0),
+    m_abort_change(false)
 {
+    m_tuneStatus = changeUnknown;
+    m_tuneThread.tuner = this;
 }
 
 ChannelBase::~ChannelBase(void)
 {
     ClearInputMap();
+    TeardownAll();
+}
+
+void ChannelBase::TeardownAll(void)
+{
+    if (m_tuneThread.isRunning())
+    {
+        m_thread_lock.lock();
+        m_abort_change = true;
+        m_tuneCond.wakeAll();
+        m_thread_lock.unlock();
+        m_tuneThread.wait();
+    }
+}
+
+void ChannelBase::SelectChannel(const QString & chan)
+{
+    VERBOSE(VB_CHANNEL, LOC + "SelectChannel " + chan);
+    TeardownAll();
+
+    m_thread_lock.lock();
+    m_abort_change = false;
+    m_tuneStatus = changePending;
+    m_thread_lock.unlock();
+
+    m_curchannelname = m_tuneThread.channel = chan;
+    m_tuneThread.start();
+}
+
+/*
+ * Returns true of the channel change thread should abort
+ */
+bool ChannelBase::Aborted(void)
+{
+    bool       result;
+
+    m_thread_lock.lock();
+    result = m_abort_change;
+    m_thread_lock.unlock();
+
+    return result;
+}
+
+ChannelBase::Status ChannelBase::GetStatus(void)
+{
+    Status status;
+
+    m_thread_lock.lock();
+    status = m_tuneStatus;
+    m_thread_lock.unlock();
+
+    return status;
+}
+
+ChannelBase::Status ChannelBase::Wait(void)
+{
+    m_tuneThread.wait();
+    return m_tuneStatus;
+}
+
+void ChannelBase::setStatus(ChannelBase::Status status)
+{
+    m_thread_lock.lock();
+    m_tuneStatus = status;
+    m_thread_lock.unlock();
 }
 
 bool ChannelBase::Init(QString &inputname, QString &startchannel, bool setchan)
@@ -53,9 +132,12 @@ bool ChannelBase::Init(QString &inputname, QString &startchannel, bool setchan)
     if (!setchan)
         ok = inputname.isEmpty() ? false : IsTunable(inputname, startchannel);
     else if (inputname.isEmpty())
-        ok = SetChannelByString(startchannel);
+    {
+        SelectChannel(startchannel);
+        ok = Wait();
+    }
     else
-        ok = SwitchToInput(inputname, startchannel);
+        ok = SelectInput(inputname, startchannel, false);
 
     if (ok)
         return true;
@@ -124,12 +206,10 @@ bool ChannelBase::Init(QString &inputname, QString &startchannel, bool setchan)
             if (chanid && cit != channels.end())
             {
                 if (!setchan)
-                {
                     ok = IsTunable(*it, (mplexid_restriction) ?
                                    (*cit).channum : startchannel);
-                }
                 else
-                    ok = SwitchToInput(*it, (*cit).channum);
+                    ok = SelectInput(*it, (*cit).channum, false);
 
                 if (ok)
                 {
@@ -329,6 +409,7 @@ int ChannelBase::GetInputByName(const QString &input) const
     return -1;
 }
 
+#if 0 // Not used?
 bool ChannelBase::SwitchToInput(const QString &inputname)
 {
     int input = GetInputByName(inputname);
@@ -340,25 +421,30 @@ bool ChannelBase::SwitchToInput(const QString &inputname)
                                       "%1 on card\n").arg(inputname));
     return false;
 }
+#endif
 
-bool ChannelBase::SwitchToInput(const QString &inputname, const QString &chan)
+bool ChannelBase::SelectInput(const QString &inputname, const QString &chan,
+                              bool use_sm)
 {
     int input = GetInputByName(inputname);
 
-    bool ok = false;
     if (input >= 0)
     {
-        ok = SwitchToInput(input, false);
-        if (ok)
-            ok = SetChannelByString(chan);
+        if (!SwitchToInput(input, false))
+            return false;
+        if (use_sm)
+            SelectChannel(chan);
+        else
+            return SetChannelByString(chan);
     }
     else
     {
         VERBOSE(VB_IMPORTANT,
                 QString("ChannelBase: Could not find input: %1 on card when "
                         "setting channel %2\n").arg(inputname).arg(chan));
+        return false;
     }
-    return ok;
+    return true;
 }
 
 bool ChannelBase::SwitchToInput(int newInputNum, bool setstarting)
@@ -374,7 +460,7 @@ bool ChannelBase::SwitchToInput(int newInputNum, bool setstarting)
     // input switching code would go here
 
     if (setstarting)
-        return SetChannelByString((*it)->startChanNum);
+        SelectChannel((*it)->startChanNum);
 
     return true;
 }
@@ -485,8 +571,7 @@ static bool is_input_busy(
     return is_busy;
 }
 
-bool ChannelBase::IsInputAvailable(
-    int inputid, uint &mplexid_restriction) const
+bool ChannelBase::IsInputAvailable(int inputid, uint &mplexid_restriction) const
 {
     if (inputid < 0)
         return false;
@@ -651,26 +736,38 @@ bool ChannelBase::ChangeExternalChannel(const QString &channum)
     }
     else
     {   // child contains the pid of the new process
-        int status = 0, pid = 0;
+        QMutex      lock;
+        int         status = 0, pid = 0;
+
         VERBOSE(VB_CHANNEL, "Waiting for External Tuning program to exit");
 
         bool timed_out = false;
         uint timeout = 30; // how long to wait in seconds
         time_t start_time = time(0);
-        while (-1 != pid && !timed_out)
+        while (-1 != pid && !timed_out && !Aborted())
         {
-            sleep(1);
+            lock.lock();
+            m_tuneCond.wait(&lock, 500);  // sleep up to 0.5 seconds
             pid = waitpid(child, &status, WUNTRACED|WNOHANG);
             VERBOSE(VB_IMPORTANT, QString("ret_pid(%1) child(%2) status(0x%3)")
                     .arg(pid).arg(child).arg(status,0,16));
             if (pid==child)
+            {
+                lock.unlock();
                 break;
+            }
             else if (time(0) > (time_t)(start_time + timeout))
                 timed_out = true;
+            lock.unlock();
         }
-        if (timed_out)
+
+        if (timed_out || Aborted())
         {
-            VERBOSE(VB_IMPORTANT, "External Tuning program timed out, killing");
+            if (Aborted())
+                VERBOSE(VB_IMPORTANT, "Aborting External Tuning program");
+            else
+                VERBOSE(VB_IMPORTANT, "External Tuning program timed out, "
+                        "killing");
             kill(child, SIGTERM);
             usleep(500);
             kill(child, SIGKILL);
@@ -752,6 +849,7 @@ int ChannelBase::GetChanID() const
         return -1;
 
     query.next();
+
     return query.value(0).toInt();
 }
 

mythtv/libs/libmythtv/channelbase.h
@@ -5,6 +5,9 @@
 
 // Qt headers
 #include <QStringList>
+#include <qwaitcondition.h>
+#include <qmutex.h>
+#include <qthread.h>
 
 // MythTV headers
 #include "channelutil.h"
@@ -12,6 +15,19 @@
 #include "tv.h"
 
 class TVRec;
+class ChannelBase;
+
+/*
+ * Thread to run tunning process in
+ */
+class ChannelThread : public QThread
+{
+  public:
+    virtual void run(void);
+
+    QString      channel;
+    ChannelBase *tuner;
+};
 
 /** \class ChannelBase
  *  \brief Abstract class providing a generic interface to tuning hardware.
@@ -23,19 +39,28 @@ class TVRec;
 
 class ChannelBase
 {
- public:
+    friend class ChannelThread;
+
+  public:
+    enum Status { changeUnknown = 'U', changePending = 'P',
+                  changeFailed = 'F', changeSuccess = 'S' };
+
     ChannelBase(TVRec *parent);
-    virtual ~ChannelBase();
+    virtual ~ChannelBase(void);
 
     virtual bool Init(QString &inputname, QString &startchannel, bool setchan);
     virtual bool IsTunable(const QString &input, const QString &channum) const;
+
+    virtual void SelectChannel(const QString & chan);
+
+    Status GetStatus(void);
+    Status Wait(void);
 
     // Methods that must be implemented.
     /// \brief Opens the channel changing hardware for use.
     virtual bool Open(void) = 0;
     /// \brief Closes the channel changing hardware to use.
     virtual void Close(void) = 0;
-    virtual bool SetChannelByString(const QString &chan) = 0;
     /// \brief Reports whether channel is already open
     virtual bool IsOpen(void) const = 0;
 
@@ -82,8 +107,9 @@ class ChannelBase
                           const QString &newChanNum);
 
     // Input toggling convenience methods
-    virtual bool SwitchToInput(const QString &input);
-    virtual bool SwitchToInput(const QString &input, const QString &chan);
+//    virtual bool SwitchToInput(const QString &input); // not used?
+    virtual bool SelectInput(const QString &input, const QString &chan,
+                             bool use_sm);
 
     virtual bool InitializeInputs(void);
 
@@ -108,6 +134,8 @@ class ChannelBase
 
     virtual int GetCardID(void) const;
   protected:
+    virtual bool SetChannelByString(const QString &chan) = 0;
+
     /// \brief Switches to another input on hardware,
     ///        and sets the channel is setstarting is true.
     virtual bool SwitchToInput(int inputNum, bool setstarting);
@@ -119,6 +147,10 @@ class ChannelBase
     static void StoreDefaultInput(uint cardid, const QString &input);
     void ClearInputMap(void);
 
+    bool Aborted();
+    void setStatus(Status status);
+    void TeardownAll(void);
+
     TVRec   *m_pParent;
     QString  m_curchannelname;
     int      m_currentInputID;
@@ -126,6 +158,14 @@ class ChannelBase
     uint     m_cardid;
     InputMap m_inputs;
     DBChanList m_allchannels; ///< channels across all inputs
+
+    QWaitCondition  m_tuneCond;
+
+  private:
+    mutable  ChannelThread   m_tuneThread;
+    Status   m_tuneStatus;
+    QMutex   m_thread_lock;
+    bool     m_abort_change;
 };
 
 #endif

mythtv/libs/libmythtv/channelchangemonitor.cpp
@@ -1,0 +1,37 @@
+// -*- Mode: c++ -*-
+
+#include <cerrno>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include "mythcontext.h"
+#include "channelchangemonitor.h"
+#include "v4lchannel.h"
+
+#define LOC QString("ChannelChangeM: ").arg(channel->GetDevice())
+#define LOC_ERR QString("ChannelChangeM, Error: ").arg(channel->GetDevice())
+
+ChannelChangeMonitor::ChannelChangeMonitor(
+    int db_cardnum, V4LChannel *_channel, uint64_t _flags) :
+    SignalMonitor(db_cardnum, _channel, _flags)
+{
+}
+
+void ChannelChangeMonitor::UpdateValues(void)
+{
+    if (!running || exit)
+        return;
+
+    if (!IsChannelTuned())
+        return;
+
+    {
+        QMutexLocker locker(&statusLock);
+        signalLock.SetValue(true);
+        signalStrength.SetValue(100);
+    }
+
+    EmitStatus();
+    SendMessageAllGood();
+}
+

mythtv/libs/libmythtv/channelchangemonitor.h
@@ -1,0 +1,22 @@
+// -*- Mode: c++ -*-
+// Copyright (c) 2005, Daniel Thor Kristjansson
+
+#ifndef _CHANNEL_CHANGE_MONITOR_H_
+#define _CHANNEL_CHANGE_MONITOR_H_
+
+// MythTV headers
+#include "signalmonitor.h"
+
+class V4LChannel;
+
+class ChannelChangeMonitor : public SignalMonitor
+{
+  public:
+    ChannelChangeMonitor(
+        int db_cardnum, V4LChannel *_channel,
+        uint64_t _flags = kSigMon_WaitForSig);
+
+    virtual void UpdateValues(void);
+};
+
+#endif // _CHANNEL_CHANGE_MONITOR_H_

mythtv/libs/libmythtv/channelscan/channelscan_sm.cpp
@@ -1627,7 +1627,7 @@ void ChannelScanSM::ScanTransport(const transport_scan_items_it_t transport)
     }
 
     // Start signal monitor for this channel
-    signalMonitor->Start();
+    signalMonitor->Start(false);
 
     timer.start();
     waitingForTables = (item.tuning.sistandard != "analog");

mythtv/libs/libmythtv/channelscan/channelscan_sm.h
@@ -73,6 +73,7 @@ class AnalogSignalHandler : public SignalMonitorListener
   public slots:
     virtual inline void AllGood(void);
     virtual void StatusSignalLock(const SignalMonitorValue&) { }
+    virtual void StatusChannelTuned(const SignalMonitorValue&) { }
     virtual void StatusSignalStrength(const SignalMonitorValue&) { }
 
   private:

commit 29edc090439578c2d9276a783208f17e2e135015

Fix the channel change timeout popup.
I suspect this was broken by r25543 (Signal monitor changes) and if so
there are sections of code that need to be stripped out as the behaviour
now appears to be triggered by events from the signal monitor and not by
the timer directly.

Closes #8906.

As an aside, the message or the themes probably need to be adjusted as
presentation is generally a mess when using XVideo.


git-svn-id: http://svn.mythtv.org/svn/trunk@26327 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/tv_play.cpp
@@ -8662,6 +8662,7 @@ void TV::customEvent(QEvent *e)
             if (tc && !signalList.empty())
             {
                 UpdateOSDSignal(ctx, signalList);
+                UpdateOSDTimeoutMessage(ctx);
             }
         }
         ReturnPlayerLock(mctx);