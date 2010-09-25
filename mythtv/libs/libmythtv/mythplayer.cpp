MythTV/mythtv

commit c6ba7c8e103a19af5f3026dc8baae84342b5589e

Add a visual indication that we are seeking and still alive when seeking in files without a position map.
Seeking can be slow in certain files and formats and this just re-
have a number of long files where a seek can take over 3 minutes.

I've deliberately recycled an existing string, which shouldn't break the
current translations. If it does, slap me.


git-svn-id: http://svn.mythtv.org/svn/trunk@26487 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/mythplayer.cpp
@@ -3322,8 +3322,25 @@ void MythPlayer::WaitForSeek(uint64_t frame, bool override_seeks,
     decoderSeek = frame;
     decoderSeekLock.unlock();
 
+    int count = 0;
+    bool need_clear = false;
     while (decoderSeek >= 0)
+    {
         usleep(1000);
+
+        // provide some on screen feedback if seeking is slow
+        count++;
+        if (!(count % 150) && !hasFullPositionMap)
+        {
+            int num = (count / 150) % 4;
+            SetOSDMessage(QObject::tr("Searching") + QString().fill('.', num),
+                          kOSDTimeout_Short);
+            DisplayPauseFrame();
+            need_clear = true;
+        }
+    }
+    if (need_clear && osd)
+        osd->HideWindow("osd_message");
     GetDecoder()->setExactSeeks(after);
 }
 

commit 534e53eac24443461cd6f40c81b85eee123a89cc

Fix crash when seeking to a bookmark introduced in r26487.
The video classes aren't fully initialised when the initial seek
happens, so ensure we just return. Really needs to be fixed for 0.25 as
the potentially long wait on start up in some videos is easily mistaken
for a hang.


git-svn-id: http://svn.mythtv.org/svn/trunk@26513 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/mythplayer.cpp
@@ -1831,6 +1831,9 @@ void MythPlayer::AVSync(bool limit_delay)
 
 void MythPlayer::DisplayPauseFrame(void)
 {
+    if (!videoOutput || ! videosync)
+        return;
+
     if (videoOutput->IsErrored())
     {
         SetErrored(QObject::tr("Serious error detected in Video Output"));