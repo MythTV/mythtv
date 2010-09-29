MythTV/mythtv

commit 3af3489357da268916c05f00b202f27c92508545

Add the ability to playback remote DVD and Blu-ray directory images and DVD ISO files via MythTV's streaming protocol.  The full implementation is an interim solution, parts of it will be changed in the future. Post 0.24, we'll probably be moving the libraries into the new 'external' subdir.  The desire is to coordinate a standard 'plugable' layout where can plug in our RingBuffer wrapper routines without having to modify the upstream library in any way.  the Blu-ray library already has partial support for this, but it will need extending to fully allow us to use the library for remote streaming without making any of our own changes to the code.
URI Examples (substitute in your favorite Storage Group and subdirectory):

Blu-ray image (BDMV subdirectory is under a 'Demos/x264_bluray_demo'
  directory in Storage Group)

  mythavtest myth://MyDiscImages@192.168.1.100/Demos/x264_bluray_demo

DVD image (VIDEO_TS subdirectory is under the 'Hot_Dogs_II' directory)

  mythavtest myth://MyDiscImages@192.168.1.100/Hot_Dogs_II

DVD ISO (ISO is in a 'Documentaries' sudirectory of the Storage Group

  mythavtest myth://MyISOs@192.168.1.100/Documentaries/Hot_Dogs_II.iso

ISO playback is by far the fastest, the libs do a lot of seeking around
which exposes some inefficiencies in our RingBuffer.  These are being
looked at, so performance should improve.  Directory image playback
does take longer to startup than ISO playback, but once playback
has started, they should both perform about the same since the RingBuffer
has a built-in read-ahead buffer.

Encrypted DVDs are not supported for remote streaming.  In other words,
you can't currently point a Backend's Storage Group at /dev/dvd or a
directory where you have a DVD mounted and expect to be able to watch
it on a remote frontend.


NOTE: This does modify the binary API, AND the protocol version, so make clean,
upgrade everything, etc..



git-svn-id: http://svn.mythtv.org/svn/trunk@25858 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/DVDRingBuffer.cpp
@@ -1306,7 +1306,8 @@ void DVDRingBufferPriv::SetDVDSpeed(void)
  */
 void DVDRingBufferPriv::SetDVDSpeed(int speed)
 {
-    MediaMonitor::SetCDSpeed(m_dvdFilename.toLocal8Bit().constData(), speed);
+    if (m_dvdFilename.startsWith("/"))
+        MediaMonitor::SetCDSpeed(m_dvdFilename.toLocal8Bit().constData(), speed);
 }
 
 /**\brief returns seconds left in the title

mythtv/bindings/perl/MythTV.pm
@@ -106,7 +106,7 @@ package MythTV;
 # Note: as of July 21, 2010, this is actually a string, to account for proto
 # versions of the form "58a".  This will get used if protocol versions are 
 # changed on a fixes branch ongoing.
-    our $PROTO_VERSION = "58";
+    our $PROTO_VERSION = "59";
 
 # NUMPROGRAMLINES is defined in mythtv/libs/libmythtv/programinfo.h and is
 # the number of items in a ProgramInfo QStringList group used by

mythtv/bindings/python/MythTV/static.py
@@ -9,7 +9,7 @@ SCHEMA_VERSION = 1263
 MVSCHEMA_VERSION = 1036
 NVSCHEMA_VERSION = 1007
 MUSICSCHEMA_VERSION = 1017
-PROTO_VERSION = '58'
+PROTO_VERSION = '59'
 BACKEND_SEP = '[]:[]'
 
 class MARKUP( object ):

mythtv/libs/libmyth/programinfo.cpp
@@ -1865,13 +1865,13 @@ void ProgramInfo::SetPathname(const QString &pn) const
     ProgramInfoType pit = kProgramInfoTypeVideoFile;
     if (chanid)
         pit = kProgramInfoTypeRecording;
-    else if (pathname.toLower().startsWith("dvd:"))
+    else if (myth_FileIsDVD(pathname))
         pit = kProgramInfoTypeVideoDVD;
     else if (pathname.toLower().startsWith("http:"))
         pit = kProgramInfoTypeVideoStreamingHTML;
     else if (pathname.toLower().startsWith("rtsp:"))
         pit = kProgramInfoTypeVideoStreamingRTSP;
-    else if (pathname.toLower().startsWith("bd:"))
+    else if (myth_FileIsBD(pathname))
         pit = kProgramInfoTypeVideoBD;
     const_cast<ProgramInfo*>(this)->SetProgramInfoType(pit);
 }

mythtv/libs/libmyth/util.cpp
@@ -47,6 +47,7 @@ using namespace std;
 #include "msocketdevice.h"
 #include "mythsocket.h"
 #include "mythcoreutil.h"
+#include "remotefile.h"
 
 #include "mythconfig.h" // for CONFIG_DARWIN
 
@@ -1304,6 +1305,40 @@ void myth_yield(void)
 #else
     usleep(5000);
 #endif
+}
+
+bool myth_FileIsDVD(const QString &filename)
+{
+    if ((filename.toLower().startsWith("dvd:")) ||
+        (filename.toLower().endsWith(".iso")) ||
+        (filename.toLower().endsWith(".img")) ||
+        (QDir(filename + "/VIDEO_TS").exists()))
+        return true;
+
+    if (filename.toLower().startsWith("myth://"))
+    {
+        QString tmpFile = filename + "/VIDEO_TS";
+        if (RemoteFile::Exists(tmpFile))
+            return true;
+    }
+
+    return false;
+}
+
+bool myth_FileIsBD(const QString &filename)
+{
+    if ((filename.toLower().startsWith("bd:")) ||
+        (QDir(filename + "/BDMV").exists()))
+        return true;
+
+    if (filename.toLower().startsWith("myth://"))
+    {
+        QString tmpFile = filename + "/BDMV";
+        if (RemoteFile::Exists(tmpFile))
+            return true;
+    }
+
+    return false;
 }
 
 /* vim: set expandtab tabstop=4 shiftwidth=4: */

mythtv/libs/libmyth/util.h
@@ -82,6 +82,9 @@ MPUBLIC bool IsPulseAudioRunning(void);
 MPUBLIC bool myth_nice(int val);
 MPUBLIC void myth_yield(void);
 
+MPUBLIC bool myth_FileIsDVD(const QString &filename);
+MPUBLIC bool myth_FileIsBD(const QString &filename);
+
 // CPU Tick timing function
 #ifdef MMX
 #ifdef _WIN32

mythtv/libs/libmythbluray/bluray.c
@@ -46,6 +46,9 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
+#include <sys/types.h>
+
+#include "mythiowrapper.h"
 
 typedef int     (*fptr_int)();
 typedef int32_t (*fptr_int32)();
@@ -207,6 +210,7 @@ static void _close_m2ts(BD_STREAM *st)
 static int _open_m2ts(BLURAY *bd, BD_STREAM *st)
 {
     char *f_name;
+    struct stat buf;
 
     _close_m2ts(st);
 
@@ -217,8 +221,17 @@ static int _open_m2ts(BLURAY *bd, BD_STREAM *st)
     st->clip_block_pos = (st->clip_pos / 6144) * 6144;
 
     if ((st->fp = file_open(f_name, "rb"))) {
-        file_seek(st->fp, 0, SEEK_END);
-        if ((st->clip_size = file_tell(st->fp))) {
+// Original libbluray code
+//        file_seek(st->fp, 0, SEEK_END);
+//        if ((st->clip_size = file_tell(st->fp))) {
+// New 'stat' and modified 'if' to minimize RingBuffer seeking
+// Optimize here for now until we can optimize in the RingBuffer itself
+        if (mythfile_stat(f_name, &buf) == 0)
+            st->clip_size = buf.st_size;
+        else
+            st->clip_size = 0;
+
+        if (st->clip_size) {
             file_seek(st->fp, st->clip_block_pos, SEEK_SET);
             st->int_buf_off = 6144;
             X_FREE(f_name);

mythtv/libs/libmythbluray/file/dir_mythiowrapper.c
@@ -1,0 +1,56 @@
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "file.h"
+#include "util/macro.h"
+#include "util/logging.h"
+
+#include "mythiowrapper.h"
+
+static void dir_close_mythiowrapper(BD_DIR_H *dir)
+{
+    if (dir) {
+        mythdir_closedir((int)dir->internal);
+
+        DEBUG(DBG_DIR, "Closed mythdir dir (%p)\n", dir);
+
+        X_FREE(dir);
+    }
+}
+
+static int dir_read_mythiowrapper(BD_DIR_H *dir, BD_DIRENT *entry)
+{
+    char *filename = mythdir_readdir((int)dir->internal);
+    if (filename)
+    {
+        strncpy(entry->d_name, filename, 256);
+        free(filename);
+        return 0;
+    }
+
+    return 1;
+}
+
+BD_DIR_H *dir_open_mythiowrapper(const char* dirname)
+{
+    BD_DIR_H *dir = malloc(sizeof(BD_DIR_H));
+
+    DEBUG(DBG_DIR, "Opening mythdir dir %s... (%p)\n", dirname, dir);
+    dir->close = dir_close_mythiowrapper;
+    dir->read = dir_read_mythiowrapper;
+
+    int dirID = 0;
+    if ((dirID = mythdir_opendir(dirname))) {
+        dir->internal = (void *)dirID;
+
+        return dir;
+    }
+
+    DEBUG(DBG_DIR, "Error opening dir! (%p)\n", dir);
+
+    X_FREE(dir);
+
+    return NULL;
+}
+

commit a001a7b084912cb92ae549198f6a672ffba70f2b

Add some additional DVD bookmark debugging and remove some entirely redundant DVD code. Refs #8817.
(Note: DVD bookmark functionality was broken in r25858. MythIOWrapper
doesn't seem to return the serial number or title of the disk.)


git-svn-id: http://svn.mythtv.org/svn/trunk@26569 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/DVDRingBuffer.cpp
@@ -54,7 +54,6 @@ DVDRingBufferPriv::DVDRingBufferPriv()
       m_menupktpts(0), m_curAudioTrack(0),
       m_curSubtitleTrack(0),
       m_autoselectsubtitle(true),
-      m_jumptotitle(true),
       m_seekpos(0), m_seekwhence(0),
       m_dvdname(NULL), m_serialnumber(NULL),
       m_seeking(false), m_seektime(0),
@@ -228,7 +227,6 @@ bool DVDRingBufferPriv::OpenFile(const QString &filename)
         return true;
     }
 }
-
 
 void DVDRingBufferPriv::StartFromBeginning(void)
 {

mythtv/libs/libmythtv/DVDRingBuffer.h
@@ -67,7 +67,6 @@ class MPUBLIC DVDRingBufferPriv
     bool DecodeSubtitles(AVSubtitle * sub, int * gotSubtitles,
                          const uint8_t * buf, int buf_size);
     bool GetNameAndSerialNum(QString& _name, QString& _serialnum);
-    bool JumpToTitle(void) { return m_jumptotitle; }
     double GetFrameRate(void);
     bool StartOfTitle(void) { return (m_part == 0); }
     bool EndOfTitle(void)   { return    ((!m_titleParts) ||
@@ -107,7 +106,6 @@ class MPUBLIC DVDRingBufferPriv
     void  SetTrack(uint type, int trackNo);
     int   GetTrack(uint type);
     uint8_t GetNumAudioChannels(int id);
-    void JumpToTitle(bool change) { m_jumptotitle = change; }
     void SetDVDSpeed(void);
     void SetDVDSpeed(int speed);
     bool SwitchAngle(uint angle);
@@ -163,7 +161,6 @@ class MPUBLIC DVDRingBufferPriv
     int            m_curAudioTrack;
     int8_t         m_curSubtitleTrack;
     bool           m_autoselectsubtitle;
-    bool           m_jumptotitle;
     long long      m_seekpos;
     int            m_seekwhence;
     const char    *m_dvdname;

mythtv/libs/libmythtv/mythdvdplayer.cpp
@@ -26,14 +26,6 @@ void MythDVDPlayer::ReleaseNextVideoFrame(VideoFrame *buffer,
                         !player_ctx->buffer->InDVDMenuOrStillFrame());
 }
 
-int MythDVDPlayer::OpenFile(uint retries, bool allow_libmpeg2)
-{
-    MythPlayer::OpenFile(retries, allow_libmpeg2);
-    if (player_ctx->buffer->isDVD())
-        player_ctx->buffer->DVD()->JumpToTitle(true);
-    return IsErrored() ? -1 : 0;
-}
-
 void MythDVDPlayer::DisableCaptions(uint mode, bool osd_msg)
 {
     if ((kDisplayAVSubtitle & mode) && player_ctx->buffer->isDVD())
@@ -238,8 +230,6 @@ uint64_t MythDVDPlayer::GetBookmark(void) const
     QString name;
     QString serialid;
     long long frames = 0;
-    bool delbookmark, jumptotitle;
-    delbookmark = jumptotitle = player_ctx->buffer->DVD()->JumpToTitle();
     player_ctx->LockPlayingInfo(__FILE__, __LINE__);
     if (player_ctx->playingInfo)
     {
@@ -249,23 +239,20 @@ uint64_t MythDVDPlayer::GetBookmark(void) const
             return 0;
         }
         dvdbookmark = player_ctx->playingInfo->QueryDVDBookmark(serialid,
-                                                                !delbookmark);
+                                                                false);
         if (!dvdbookmark.empty())
         {
             QStringList::Iterator it = dvdbookmark.begin();
             int title = (*it).toInt();
             frames = (long long)((*++it).toLongLong() & 0xffffffffLL);
-            if (jumptotitle)
-            {
-                player_ctx->buffer->DVD()->PlayTitleAndPart(title, 1);
-                int audiotrack    = (*++it).toInt();
-                int subtitletrack = (*++it).toInt();
-                player_ctx->buffer->DVD()->SetTrack(
-                    kTrackTypeAudio, audiotrack);
-                player_ctx->buffer->DVD()->SetTrack(
-                    kTrackTypeSubtitle, subtitletrack);
-                player_ctx->buffer->DVD()->JumpToTitle(false);
-            }
+            player_ctx->buffer->DVD()->PlayTitleAndPart(title, 1);
+            int audiotrack    = (*++it).toInt();
+            int subtitletrack = (*++it).toInt();
+            player_ctx->buffer->DVD()->SetTrack(kTrackTypeAudio, audiotrack);
+            player_ctx->buffer->DVD()->SetTrack(kTrackTypeSubtitle, subtitletrack);
+            VERBOSE(VB_PLAYBACK, LOC +
+                QString("Get Bookmark: title %1 audiotrack %2 subtrack %3 frame %4")
+                .arg(title).arg(audiotrack).arg(subtitletrack).arg(frames));
         }
     }
     player_ctx->UnlockPlayingInfo(__FILE__, __LINE__);
@@ -429,6 +416,7 @@ void MythDVDPlayer::SetDVDBookmark(uint64_t frame)
 {
     if (!player_ctx->buffer->isDVD())
         return;
+
     uint64_t framenum = frame;
     QStringList fields;
     QString name;
@@ -438,7 +426,12 @@ void MythDVDPlayer::SetDVDBookmark(uint64_t frame)
     int audiotrack = -1;
     int subtitletrack = -1;
     if (!player_ctx->buffer->DVD()->GetNameAndSerialNum(name, serialid))
+    {
+        VERBOSE(VB_IMPORTANT, LOC +
+                QString("DVD has no name and serial number. "
+                        "Cannot set bookmark."));
         return;
+    }
 
     if (!player_ctx->buffer->InDVDMenuOrStillFrame() &&
         player_ctx->buffer->DVD()->GetTotalTimeOfTitle() > 120 && frame > 0)
@@ -464,6 +457,9 @@ void MythDVDPlayer::SetDVDBookmark(uint64_t frame)
         fields += QString("%1").arg(subtitletrack);
         fields += QString("%1").arg(framenum);
         player_ctx->playingInfo->SaveDVDBookmark(fields);
+        VERBOSE(VB_PLAYBACK, LOC +
+            QString("Set Bookmark: title %1 audiotrack %2 subtrack %3 frame %4")
+            .arg(title).arg(audiotrack).arg(subtitletrack).arg(framenum));
     }
     player_ctx->UnlockPlayingInfo(__FILE__, __LINE__);
 }

mythtv/libs/libmythtv/mythdvdplayer.h
@@ -9,7 +9,6 @@ class MythDVDPlayer : public MythPlayer
 {
   public:
     MythDVDPlayer(bool muted = false);
-    virtual int  OpenFile(uint retries = 4, bool allow_libmpeg2 = true);
     virtual void ReleaseNextVideoFrame(VideoFrame *buffer, int64_t timecode,
                                        bool wrap = true);
     virtual void DisableCaptions(uint mode, bool osd_msg=true);

mythtv/libs/libmythtv/tv_play.cpp
@@ -4447,9 +4447,6 @@ bool TV::ActivePostQHandleAction(PlayerContext *ctx,
     {
         if (!islivetv || !CommitQueuedInput(ctx))
         {
-            if (isDVD && ctx->buffer && ctx->buffer->DVD())
-                ctx->buffer->DVD()->JumpToTitle(false);
-
             ctx->LockDeletePlayer(__FILE__, __LINE__);
             if (ctx->player)
             {