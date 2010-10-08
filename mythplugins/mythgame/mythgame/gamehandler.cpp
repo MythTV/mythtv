a9430245877479f50b85db9452673f1925b196b9
Robert McNamara <rmcnamara@mythtv.org>
2010-10-08 14:48:00 +0000

    Change the MythUI Location in MythGame to reflect the currently running game.  Potentially useful for users of the telnet interface and non-invasive enough that it's fine to add at this point.  Patch from James Barrett.  Fixes #9073.
    git-svn-id: http://svn.mythtv.org/svn/trunk@26709 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythplugins/mythgame/mythgame/gamehandler.cpp
+++ mythplugins/mythgame/mythgame/gamehandler.cpp
@@ -12,6 +12,7 @@
 #include <mythdialogs.h>
 #include <util.h>
 #include <mythdb.h>
+#include <mythuihelper.h>
 
 #define LOC_ERR QString("MythGame:GAMEHANDLER Error: ")
 #define LOC QString("MythGame:GAMEHANDLER: ")
@@ -902,6 +903,7 @@ void GameHandler::Launchgame(RomInfo *romdata, QString systemname)
            .arg(handler->SystemName())
            .arg(exec));
 
+    GetMythUI()->AddCurrentLocation(QString("MythGame %1 ( %2 )").arg(handler->SystemName()).arg(exec));
 
     QStringList cmdlist = exec.split(";");
     if (cmdlist.count() > 0)
@@ -918,6 +920,8 @@ void GameHandler::Launchgame(RomInfo *romdata, QString systemname)
         VERBOSE(VB_GENERAL, LOC + QString("Executing : %1").arg(exec));
         myth_system(exec, kMSProcessEvents);
     }
+
+    GetMythUI()->RemoveCurrentLocation();
 
     (void)d.cd(savedir);
 }
