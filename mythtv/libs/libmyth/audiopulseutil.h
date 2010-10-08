ecfa4ee225a9b363ad0c9e07d72828c72f7ce0f5
Mark Kendall <mkendall@mythtv.org>
2010-10-08 03:54:17 +0000

    Remove the old PulseAudio suspend code. Refs #9031
    git-svn-id: http://svn.mythtv.org/svn/trunk@26704 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmyth/audiopulseutil.h
+++
@@ -1,6 +0,0 @@
-#include "mythexp.h"
-
-MPUBLIC bool pulseaudio_suspend(void);
-MPUBLIC bool pulseaudio_unsuspend(void);
-MPUBLIC int  pulseaudio_handle_startup(void);
-MPUBLIC int  pulseaudio_handle_teardown(void);
