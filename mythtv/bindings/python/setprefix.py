6c011b4c257b9947abf099c2d2ea46ecea6a8438
Raymond Wagner <rwagner@mythtv.org>
2010-10-10 22:34:11 +0000

    Alter PREFIX handling to edit temporary build file, rather than original from repository.
    git-svn-id: http://svn.mythtv.org/svn/trunk@26743 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/bindings/python/setprefix.py
+++ mythtv/bindings/python/setprefix.py
@@ -1,13 +1,17 @@
 #!python
 
 import sys
+import os
 
 if len(sys.argv) < 2:
     raise Exception('function must be called with prefix argument')
 prefix = sys.argv[1]
 
 buff = []
-with open('MythTV/static.py') as fi:
+path = 'build/lib/MythTV/static.py'
+if not os.access(path, os.F_OK):
+    raise Exception("Cannot access '%s' to update." % path)
+with open(path) as fi:
     for line in fi:
         if 'INSTALL_PREFIX' in line:
             line = "INSTALL_PREFIX = '%s'\n" % prefix
@@ -16,5 +20,5 @@ with open('MythTV/static.py') as fi:
 #print ''.join(buff)
 #sys.exit(0)
 
-with open('MythTV/static.py','w') as fo:
+with open(path,'w') as fo:
     fo.write(''.join(buff))
