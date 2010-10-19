1f9ca6030bfc41e271cf4f0e76b4986f7dcf5d12
Mark Kendall <mkendall@mythtv.org>
2010-10-19 12:40:59 +0000

    Add missing NULL pointer checks in VideoOutputVDPAU.
    git-svn-id: http://svn.mythtv.org/svn/trunk@26896 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/videoout_vdpau.cpp
+++ mythtv/libs/libmythtv/videoout_vdpau.cpp
@@ -471,13 +471,13 @@ void VideoOutputVDPAU::PrepareFrame(VideoFrame *frame, FrameScanType scan,
     VdpVideoMixerPictureStructure field =
         VDP_VIDEO_MIXER_PICTURE_STRUCTURE_FRAME;
 
-    if (scan == kScan_Interlaced && m_deinterlacing)
+    if (scan == kScan_Interlaced && m_deinterlacing && frame)
     {
         field = frame->top_field_first ?
                 VDP_VIDEO_MIXER_PICTURE_STRUCTURE_TOP_FIELD :
                 VDP_VIDEO_MIXER_PICTURE_STRUCTURE_BOTTOM_FIELD;
     }
-    else if (scan == kScan_Intr2ndField && m_deinterlacing)
+    else if (scan == kScan_Intr2ndField && m_deinterlacing && frame)
     {
         field = frame->top_field_first ?
                 VDP_VIDEO_MIXER_PICTURE_STRUCTURE_BOTTOM_FIELD :
