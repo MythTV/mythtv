MythTV/mythtv

commit 21c7ea022ba6b07cd3304fad3b3084c815f84fbd

Fix unexpected MythPlayer exits by clearing the buffering state.
The pre-buffering check in MythPlayer::PreBufferEnoughFrames implicitly
assumes that it is called sequentially during playback and hence any
buffering state is cleared. If playback is paused, or an alternative
playback route used (e.g. DVD playback), then the buffering state may
not be cleared, the timer continues to roll and playback promptly exits
when normal playback is resumed.


git-svn-id: http://svn.mythtv.org/svn/trunk@26887 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/mythdvdplayer.cpp
@@ -208,6 +208,9 @@ bool MythDVDPlayer::VideoLoop(void)
 
 void MythDVDPlayer::DisplayLastFrame(void)
 {
+    // clear the buffering state
+    buffering = false;
+
     videoOutput->StartDisplayingFrame();
     VideoFrame *frame = videoOutput->GetLastShownFrame();
     frame->timecode = audio.GetAudioTime();

mythtv/libs/libmythtv/mythplayer.cpp
@@ -1845,6 +1845,9 @@ void MythPlayer::DisplayPauseFrame(void)
         return;
     }
 
+    // clear the buffering state
+    buffering = false;
+
     if (needNewPauseFrame)
     {
         if (videoOutput->ValidVideoFrames())
@@ -1922,6 +1925,9 @@ void MythPlayer::DisplayNormalFrame(bool check_prebuffer)
 {
     if (allpaused || (check_prebuffer && !PrebufferEnoughFrames()))
         return;
+
+    // clear the buffering state
+    buffering = false;
 
     videoOutput->StartDisplayingFrame();
     VideoFrame *frame = videoOutput->GetLastShownFrame();

commit 624f5ddf5f079a791601d3094438480dd3ec08d6

Backport r26908 from trunk to release-0-24-fixes.
Fix a DVD audio regression introduced in r26887.

We need to unpause the audio when exiting the buffering state.


git-svn-id: http://svn.mythtv.org/svn/branches/release-0-24-fixes@26929 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmythtv/mythdvdplayer.cpp
@@ -214,7 +214,7 @@ bool MythDVDPlayer::VideoLoop(void)
 void MythDVDPlayer::DisplayLastFrame(void)
 {
     // clear the buffering state
-    buffering = false;
+    SetBuffering(false);
 
     DisplayDVDButton();
 

mythtv/libs/libmythtv/mythplayer.cpp
@@ -200,7 +200,7 @@ MythPlayer::MythPlayer(bool muted)
       m_scan_tracker(0),            m_scan_initialized(false),
       keyframedist(30),             noVideoTracks(false),
       // Prebuffering
-      buffering(false), buffering_start(),
+      buffering(false),
       // General Caption/Teletext/Subtitle support
       textDisplayMode(kDisplayNone),
       prevTextDisplayMode(kDisplayNone),
@@ -1864,7 +1864,7 @@ void MythPlayer::DisplayPauseFrame(void)
     }
 
     // clear the buffering state
-    buffering = false;
+    SetBuffering(false);
 
     RefreshPauseFrame();
 
@@ -1877,24 +1877,33 @@ void MythPlayer::DisplayPauseFrame(void)
     videosync->Start();
 }
 
+void MythPlayer::SetBuffering(bool new_buffering, bool pause_audio)
+{
+    if (!buffering && new_buffering)
+    {
+        VERBOSE(VB_PLAYBACK, LOC + "Waiting for video buffers...");
+        buffering = true;
+        audio.Pause(pause_audio);
+        buffering_start = QTime::currentTime();
+    }
+    else if (buffering && !new_buffering)
+    {
+        buffering = false;
+        audio.Pause(false);
+    }
+}
+
 bool MythPlayer::PrebufferEnoughFrames(bool pause_audio, int min_buffers)
 {
     if (!videoOutput)
         return false;
 
-    bool was_buffering = buffering;
     if (!(min_buffers ? (videoOutput->ValidVideoFrames() >= min_buffers) :
                         (videoOutput->hasHWAcceleration() ?
                             videoOutput->EnoughPrebufferedFrames() :
                             videoOutput->EnoughDecodedFrames())))
     {
-        if (!buffering)
-        {
-            VERBOSE(VB_PLAYBACK, LOC + "Waiting for video buffers...");
-            buffering = true;
-            audio.Pause(pause_audio);
-            buffering_start = QTime::currentTime();
-        }
+        SetBuffering(true, pause_audio);
         usleep(frame_interval >> 3);
         int waited_for = buffering_start.msecsTo(QTime::currentTime());
         if ((waited_for & 100) == 100)
@@ -1922,9 +1931,8 @@ bool MythPlayer::PrebufferEnoughFrames(bool pause_audio, int min_buffers)
             videosync->Start();
         return false;
     }
-    buffering = false;
-    if (was_buffering)
-        audio.Pause(false);
+
+    SetBuffering(false);
     return true;
 }
 
@@ -1934,7 +1942,7 @@ void MythPlayer::DisplayNormalFrame(bool check_prebuffer)
         return;
 
     // clear the buffering state
-    buffering = false;
+    SetBuffering(false);
 
     videoOutput->StartDisplayingFrame();
     VideoFrame *frame = videoOutput->GetLastShownFrame();

mythtv/libs/libmythtv/mythplayer.h
@@ -373,6 +373,9 @@ class MPUBLIC MythPlayer
     void JumpChapter(int chapter);
 
     // Playback
+    virtual bool PrebufferEnoughFrames(bool pause_audio = true,
+                                       int  min_buffers = 0);
+    void         SetBuffering(bool new_buffering, bool pause_audio = false);
     void         RefreshPauseFrame(void);
     virtual void DisplayPauseFrame(void);
     virtual void DisplayNormalFrame(bool check_prebuffer = true);
@@ -478,9 +481,6 @@ class MPUBLIC MythPlayer
     bool DecodeFrame(struct rtframeheader *frameheader,
                      unsigned char *strm, unsigned char *outbuf);
 
-    virtual bool PrebufferEnoughFrames(bool pause_audio = true,
-                                       int  min_buffers = 0);
-    void CheckPrebuffering(void);
     virtual bool DecoderGetFrameFFREW(void);
     virtual bool DecoderGetFrameREW(void);
     bool         DecoderGetFrame(DecodeType, bool unsafe = false);