eaa8f0dd129ee1bc6b7be2ba76fb98460bf57ec3
Janne Grunau <jgrunau@mythtv.org>
2010-11-08 16:12:39 +0000

    Revert [25805], the fix for our workaround created playback problems
    Fixes #8927, Reopens #3001
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@27132 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/external/FFmpeg/libavcodec/mpeg4videodec.c
+++ mythtv/external/FFmpeg/libavcodec/mpeg4videodec.c
@@ -116,6 +116,8 @@ static inline int mpeg4_is_resync(MpegEncContext *s){
         return 0;
     }
 
+// disabling the while loop since it causes playback issues. see #3001
+#if 0
     while(v<=0xFF){
         if(s->pict_type==FF_B_TYPE || (v>>(8-s->pict_type)!=1) || s->partitioned_frame)
             break;
@@ -123,6 +125,7 @@ static inline int mpeg4_is_resync(MpegEncContext *s){
         bits_count+= 8+s->pict_type;
         v= show_bits(&s->gb, 16);
     }
+#endif
 
     if(bits_count + 8 >= s->gb.size_in_bits){
         v>>=8;
