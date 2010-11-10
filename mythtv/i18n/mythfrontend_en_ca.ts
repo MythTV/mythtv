0486bc14a63f690ed49f8c0180f8acfe5478cc70
Nicolas Riendeau <nriendeau@mythtv.org>
2010-11-10 07:56:34 +0000

    Backport r27153 from Trunk. Fix small typos in Canadian English translations
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-24-fixes@27154 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/i18n/mythfrontend_en_ca.ts
+++ mythtv/i18n/mythfrontend_en_ca.ts
@@ -19270,7 +19270,8 @@ Finally, you can test, immediately activate, store, or cancel your rule.</transl
     <message>
         <location filename="../themes/themestrings.h" line="496"/>
         <source>Zone Minder</source>
-        <translation>Zone Minder</translation>
+        <translatorcomment>Remove redundant space</translatorcomment>
+        <translation>ZoneMinder</translation>
     </message>
     <message>
         <location filename="../themes/themestrings.h" line="32"/>
