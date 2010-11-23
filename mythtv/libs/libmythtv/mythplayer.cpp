ee9af286f44cedacb718c91cc6093cc671eca8bf
Taylor Ralph <tralph@mythtv.org>
2010-11-23 21:19:26 +0000

    Several changes to fix the current position and duration of the playback OSD  * Start using the currently displayed video timecode for the OSD position instead of frame number and total frames which is inaccurate.  * Use FFmpeg duration for pre-recorded and video playback length. LiveTV is already correct since it uses the actual recording times but in-progress recordings still uses frames written and fps which is obviously wrong for variable framerate and repeat frame material. The problem with in-progress duration should be able to be fixed by using recording times like livetv does. An update to mythplayer and the recorder class will be necessary.  * Refactor and clean-up some timestamp/timecode handling.
    Refs #8631.
    
    Fixes #9109.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@27326 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/mythplayer.cpp
+++ mythtv/libs/libmythtv/mythplayer.cpp
@@ -191,6 +191,7 @@ MythPlayer::MythPlayer(bool muted)
       // Playback misc.
       videobuf_retries(0),          framesPlayed(0),
       totalFrames(0),               totalLength(0),
+      totalDuration(0),
       rewindtime(0),
       // Input Video Attributes
       video_disp_dim(0,0), video_dim(0,0),
@@ -240,6 +241,7 @@ MythPlayer::MythPlayer(bool muted)
       avsync_adjustment(0),         avsync_avg(0),
       refreshrate(0),
       lastsync(false),              repeat_delay(0),
+      disp_timecode(0),
       // Time Code stuff
       prevtc(0),                    prevrp(0),
       // LiveTVChain stuff
@@ -895,6 +897,11 @@ void MythPlayer::SetFileLength(int total, int frames)
 {
     totalLength = total;
     totalFrames = frames;
+}
+
+void MythPlayer::SetDuration(int duration)
+{
+    totalDuration = duration;
 }
 
 void MythPlayer::OpenDummy(void)
@@ -1674,8 +1681,9 @@ void MythPlayer::AVSync(VideoFrame *buffer, bool limit_delay)
 
     if (buffer)
     {
-        repeat_pict = buffer->repeat_pict;
-        timecode    = buffer->timecode;
+        repeat_pict   = buffer->repeat_pict;
+        timecode      = buffer->timecode;
+        disp_timecode = buffer->disp_timecode;
     }
 
     float diverge = 0.0f;
@@ -4267,8 +4275,8 @@ void MythPlayer::calcSliderPos(osdInfo &info, bool paddedFields)
     info.values.insert("progbefore", 0);
     info.values.insert("progafter",  0);
 
-    int playbackLen = totalLength;
-
+    int playbackLen = (totalDuration > 0) ? totalDuration : totalLength;
+       
     if (livetv && player_ctx->tvchain)
     {
         info.values["progbefore"] = (int)player_ctx->tvchain->HasPrev();
@@ -4285,7 +4293,7 @@ void MythPlayer::calcSliderPos(osdInfo &info, bool paddedFields)
         islive = true;
     }
 
-    float secsplayed = ((float)framesPlayed / video_frame_rate);
+    float secsplayed = (float)(disp_timecode / 1000.f);
     calcSliderPosPriv(info, paddedFields, playbackLen, secsplayed, islive);
 }
 
