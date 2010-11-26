69423fbd1759601f86414a8852b4b91a3cae83c8
Robert McNamara <rmcnamara@mythtv.org>
2010-11-26 04:41:36 +0000

    Blu-ray Playback:  Theoretically, bd_seek_time will function just fine on HDMV navigation titles (and seeking wouldn't work without it, soooo better figure it out).
    git-svn-id: http://svn.mythtv.org/svn/trunk@27350 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/BDRingBuffer.cpp
+++ mythtv/libs/libmythtv/BDRingBuffer.cpp
@@ -84,8 +84,8 @@ uint64_t BDRingBufferPriv::Seek(uint64_t pos)
 {
     VERBOSE(VB_PLAYBACK|VB_EXTRA, LOC + QString("Seeking to %1.")
                 .arg(pos));
-    if (!m_is_hdmv_navigation)
-        bd_seek_time(bdnav, pos);
+
+    bd_seek_time(bdnav, pos);
 
     return GetReadPosition();
 }
