5cd75dbea7bf07b1d243a062de63d85b7e927de6
Jean-Yves Avenard <jyavenard@mythtv.org>
2010-12-07 11:13:51 +1100

    Fix logic error that forced to have audio advanced checked to get 7.1 LPCM

--- mythtv/libs/libmyth/audiooutputsettings.cpp
+++ mythtv/libs/libmyth/audiooutputsettings.cpp
@@ -308,7 +308,7 @@ AudioOutputSettings* AudioOutputSettings::GetUsers(bool newcopy)
         gCoreContext->GetNumSetting("DTSPassThru", false);
     bool bLPCM = aosettings->m_LPCM &&
         (aosettings->m_passthrough == -1 ||
-         (bAdv && !gCoreContext->GetNumSetting("StereoPCM", false)));
+         !(bAdv && gCoreContext->GetNumSetting("StereoPCM", false)));
 
     if (max_channels > 2 && !bLPCM)
         max_channels = 2;
