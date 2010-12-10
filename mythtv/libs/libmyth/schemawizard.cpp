6fe23306dfb5f84ee82d2ab362f9fd3fbd62bc3f
Jean-Yves Avenard <jyavenard@mythtv.org>
2010-12-10 20:48:54 +1100

    I can't say what this change is for officially :)

--- mythtv/libs/libmyth/schemawizard.cpp
+++ mythtv/libs/libmyth/schemawizard.cpp
@@ -413,12 +413,12 @@ SchemaUpgradeWizard::PromptForUpgrade(const char *name,
             return MYTH_SCHEMA_ERROR;
         }
 
+        if (m_expertMode)
+            return returnValue;  // Experts don't like to repeat themselves :-)
+
         returnValue = GuiPrompt(message, upgradable, m_expertMode);
         if (returnValue == MYTH_SCHEMA_EXIT)
             return MYTH_SCHEMA_EXIT;
-
-        if (m_expertMode)
-            return returnValue;  // Experts don't like to repeat themselves :-)
 
         // The annoying extra confirmation:
         if (backupStatus == kDB_Backup_Completed)
