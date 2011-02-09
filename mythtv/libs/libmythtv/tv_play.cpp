54a300f80fe1c56ec31e5680fb82f8568c886d12
Mark Kendall <mkendall@mythtv.org>
2011-02-09 19:20:04 +0800

    LiveTV: Construct the TV object immediately.
    This was a hangover from the old playback threading model which was just
    slowing down startup.
    
    This also fixes the issue of LiveTV hanging when 'Start TV in Program
    Guide' is enabled, though I will follow this up with additional
    improvements in that area.
    (cherry picked from commit 48bf9de47c21480b6a03ce0848a25fc6a140c203)

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -1346,7 +1346,7 @@ bool TV::LiveTV(bool showDialogs, bool startInGuide)
         RequestNextRecorder(actx, showDialogs))
     {
         actx->SetInitialTVState(true);
-        ScheduleStateChange(actx);
+        HandleStateChange(actx, actx);
         switchToRec = NULL;
 
         // Start Idle Timer
