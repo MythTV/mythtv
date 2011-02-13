ef9cabd6f44b5ad3ba208e2d3ffaeeb48f2d9208
Jean-Yves Avenard <jyavenard@mythtv.org>
2011-02-13 19:11:52 +1100

    Add ability to play audio-only files with default player. Set -O AudioOnlyPlayback=1

--- mythtv/libs/libmythtv/mythplayer.cpp
+++ mythtv/libs/libmythtv/mythplayer.cpp
@@ -966,7 +966,8 @@ int MythPlayer::OpenFile(uint retries, bool allow_libmpeg2)
     decoder->setWatchingRecording(watchingrecording);
     decoder->setTranscoding(transcoding);
     CheckExtraAudioDecode();
-    noVideoTracks = !decoder->GetTrackCount(kTrackTypeVideo);
+    if (gCoreContext->GetNumSetting("AudioOnlyPlayback", false))
+        noVideoTracks = !decoder->GetTrackCount(kTrackTypeVideo);
 
 
     decoderEof = false;
@@ -2073,7 +2074,7 @@ void MythPlayer::VideoStart(void)
 
 bool MythPlayer::VideoLoop(void)
 {
-    if (videoPaused || isDummy /*|| noVideoTracks*/)
+    if (videoPaused || isDummy || noVideoTracks)
     {
         usleep(frame_interval);
         DisplayPauseFrame();
@@ -2529,7 +2530,7 @@ void MythPlayer::EventLoop(void)
 
     // Disable rewind if we are too close to the beginning of the buffer
     if (CalcRWTime(-ffrew_skip) > 0 &&
-       (/*!noVideoTracks && */(framesPlayed <= keyframedist)))
+       (!noVideoTracks && (framesPlayed <= keyframedist)))
     {
         VERBOSE(VB_PLAYBACK, LOC + "Near start, stopping rewind.");
         float stretch = (ffrew_skip > 0) ? 1.0f : audio.GetStretchFactor();
@@ -2807,8 +2808,8 @@ void MythPlayer::DecoderLoop(bool pause)
 
         DecodeType dt = (audio.HasAudioOut() && normal_speed) ?
             kDecodeAV : kDecodeVideo;
-        //if (noVideoTracks && audio.HasAudioOut())
-        //    dt = kDecodeAudio;
+        if (noVideoTracks && audio.HasAudioOut())
+            dt = kDecodeAudio;
         DecoderGetFrame(dt);
         decodeOneFrame = false;
     }
