4ae3c0d092d441cb9289454623e514d9c13a6d95
Mark Kendall <mkendall@mythtv.org>
2011-02-14 20:45:59 +0800

    TV: Hide and disable the main UI at the last moment.
    This consolidates some duplicated code in TV::HandleStateChange to
    ensure that the various actions required to hide and disable the main ui
    (and screensaver) are the last significant actions before we enter the
    main playback loop.

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -1881,13 +1881,6 @@ void TV::HandleStateChange(PlayerContext *mctx, PlayerContext *ctx)
 
         VERBOSE(VB_IMPORTANT, "We have a RingBuffer");
 
-        if (GetMythMainWindow() && !weDisabledGUI)
-        {
-            weDisabledGUI = true;
-            GetMythMainWindow()->PushDrawDisabled();
-            DrawUnusedRects();
-        }
-
         if (ctx->playingInfo && StartRecorder(ctx,-1))
         {
             ok = StartPlayer(mctx, ctx, desiredNextState);
@@ -1903,7 +1896,6 @@ void TV::HandleStateChange(PlayerContext *mctx, PlayerContext *ctx)
         }
         else if (!ctx->IsPIP())
         {
-            GetMythUI()->DisableScreensaver();
             if (!lastLockSeenTime.isValid() ||
                 (lastLockSeenTime < timerOffTime))
             {
@@ -1951,15 +1943,6 @@ void TV::HandleStateChange(PlayerContext *mctx, PlayerContext *ctx)
 
         if (ctx->buffer && ctx->buffer->IsOpen())
         {
-            GetMythUI()->DisableScreensaver();
-
-            if (GetMythMainWindow() && !weDisabledGUI)
-            {
-                weDisabledGUI = true;
-                GetMythMainWindow()->PushDrawDisabled();
-                DrawUnusedRects();
-            }
-
             if (desiredNextState == kState_WatchingRecording)
             {
                 ctx->LockPlayingInfo(__FILE__, __LINE__);
@@ -2111,6 +2094,8 @@ void TV::HandleStateChange(PlayerContext *mctx, PlayerContext *ctx)
              TRANSITION(kState_None, kState_WatchingRecording) ||
              TRANSITION(kState_None, kState_WatchingLiveTV))
     {
+        if (!ctx->IsPIP())
+            GetMythUI()->DisableScreensaver();
         MythMainWindow *mainWindow = GetMythMainWindow();
         mainWindow->setBaseSize(player_bounds.size());
         mainWindow->setMinimumSize(
@@ -2120,6 +2105,12 @@ void TV::HandleStateChange(PlayerContext *mctx, PlayerContext *ctx)
             QSize(QWIDGETSIZE_MAX, QWIDGETSIZE_MAX));
         mainWindow->setGeometry(player_bounds);
         GetMythMainWindow()->GetPaintWindow()->hide();
+        if (!weDisabledGUI)
+        {
+            weDisabledGUI = true;
+            GetMythMainWindow()->PushDrawDisabled();
+        }
+        DrawUnusedRects();
     }
 
     VERBOSE(VB_PLAYBACK, LOC +
