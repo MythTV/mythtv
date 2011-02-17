80d9234a505598942214978cc58145320aba0aa2
Stuart Morgan <smorgan@mythtv.org>
2011-02-17 12:51:20 +0000

    Add an 'auto-run' setting to mythgallery so that behaviour can be disabled

--- mythplugins/mythgallery/mythgallery/main.cpp
+++ mythplugins/mythgallery/mythgallery/main.cpp
@@ -59,6 +59,9 @@ static void runGallery(void)
 
 static void handleMedia(MythMediaDevice *dev)
 {
+    if (! gCoreContext->GetNumSetting("GalleryAutoLoad", 0))
+        return;
+
     if (dev && dev->isUsable())
     {
         GetMythMainWindow()->JumpTo("Main Menu");
