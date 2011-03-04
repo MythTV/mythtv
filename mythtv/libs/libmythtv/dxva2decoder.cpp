2efc2d3f8d9b2515372b9241a6b784c816724d6a
mark-kendall <mkendall@mythtv.org>
2011-03-04 14:00:42 +0800

    Windows: Fix a crash in DXVA2Decoder when codec not supported.
    Ensure our decoder surfaces are set to NULL and fix a crash when expected codec support is not available.
    
    This avoids the crash but video playback will fail as we can't easily fallback to a different (ie. unaccelerated) codec - we need a full codec support check but it's not obvious how that can be done easily.

--- mythtv/libs/libmythtv/dxva2decoder.cpp
+++ mythtv/libs/libmythtv/dxva2decoder.cpp
@@ -136,6 +136,8 @@ DXVA2Decoder::DXVA2Decoder(uint num_bufs, MythCodecID codec_id,
     m_context.cfg = &m_config;
     m_context.surface_count = num_bufs;
     m_context.surface = new IDirect3DSurface9*[num_bufs];
+    for (int i = 0; i < num_bufs; i++)
+        m_context.surface[i] = NULL;
 }
 
 DXVA2Decoder::~DXVA2Decoder(void)
