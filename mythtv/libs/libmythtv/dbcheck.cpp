024054c73e8b0a9b35861c341b7badf544edef74
Daniel Kristjansson <danielk@cuymedia.net>
2011-03-12 10:22:29 -0500

    Adds OCUR and ASI recording profiles.
    This will have no effect on those not switching back and forth between the mythtv-rec2 and master branches, but will make it much safer for people to try out the mythtv-rec2 branch as manual db updates will no longer need to be made.

--- mythtv/libs/libmythtv/dbcheck.cpp
+++ mythtv/libs/libmythtv/dbcheck.cpp
@@ -21,7 +21,7 @@ using namespace std;
    mythtv/bindings/perl/MythTV.pm
 */
 /// This is the DB schema version expected by the running MythTV instance.
-const QString currentDatabaseVersion = "1269";
+const QString currentDatabaseVersion = "1270";
 
 static bool UpdateDBVersionNumber(const QString &newnumber, QString &dbver);
 static bool performActualUpdate(
@@ -5580,6 +5580,30 @@ NULL
 NULL
 };
         if (!performActualUpdate(updates, "1269", dbver))
+            return false;
+    }
+
+
+    if (dbver == "1269")
+    {
+        const char *updates[] = {
+"DELETE FROM profilegroups WHERE id >= 15;",
+"DELETE FROM recordingprofiles WHERE profilegroup >= 15;",
+"INSERT INTO profilegroups SET id = '15', name = 'ASI Recorder (DVEO)',"
+" cardtype = 'ASI', is_default = 1;",
+"INSERT INTO recordingprofiles SET name = \"Default\", profilegroup = 15;",
+"INSERT INTO recordingprofiles SET name = \"Live TV\", profilegroup = 15;",
+"INSERT INTO recordingprofiles SET name = \"High Quality\", profilegroup = 15;",
+"INSERT INTO recordingprofiles SET name = \"Low Quality\", profilegroup = 15;",
+"INSERT INTO profilegroups SET id = '16', name = 'OCUR Recorder (CableLabs)',"
+" cardtype = 'OCUR', is_default = 1;",
+"INSERT INTO recordingprofiles SET name = \"Default\", profilegroup = 16;",
+"INSERT INTO recordingprofiles SET name = \"Live TV\", profilegroup = 16;",
+"INSERT INTO recordingprofiles SET name = \"High Quality\", profilegroup = 16;",
+"INSERT INTO recordingprofiles SET name = \"Low Quality\", profilegroup = 16;",
+NULL
+};
+        if (!performActualUpdate(updates, "1270", dbver))
             return false;
     }
 
