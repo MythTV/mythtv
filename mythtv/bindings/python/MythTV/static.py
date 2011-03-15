15aea8373966813c39255227b89e61f22b0fed92
Michael T. Dean <mdean@mythtv.org>
2011-03-15 14:48:01 -0400

    Fix bindings schema version.
    Updates schema version in Perl and Python bindings after
    024054c73e8b0a9b35861c341b7badf544edef74 .

--- mythtv/bindings/python/MythTV/static.py
+++ mythtv/bindings/python/MythTV/static.py
@@ -5,7 +5,7 @@ Contains any static and global variables for MythTV Python Bindings
 """
 
 OWN_VERSION = (0,25,-1,1)
-SCHEMA_VERSION = 1269
+SCHEMA_VERSION = 1270
 NVSCHEMA_VERSION = 1007
 MUSICSCHEMA_VERSION = 1018
 PROTO_VERSION = '65'
