MythTV/mythtv

commit 3ad6c81aa83491b7e6d2d54c15daf16894a72299

libmythui: Fix fullscreen windowing with Compiz (and Gnome?)
This is based on the patch from Lawrence Rust in ticket #9589 which
notes that Compiz requires the window to be visible before we set its
state.

Testing across various Ubuntu desktops (with and without Compiz and
composite enabled) and on OSX and windows suggests that it is safe just
to show the window earlier in the initialisation process and avoid the
ifdef.

mythtv/libs/libmythui/mythmainwindow.cpp
@@ -915,6 +915,7 @@ void MythMainWindow::Init(void)
 #endif
 
     setWindowFlags(flags);
+    Show();
 
     if (d->does_fill_screen && !GetMythUI()->IsGeometryOverridden())
     {
@@ -929,7 +930,6 @@ void MythMainWindow::Init(void)
     setFixedSize(QSize(d->screenwidth, d->screenheight));
 
     GetMythUI()->ThemeWidget(this);
-    Show();
 
     if (!GetMythDB()->GetNumSetting("HideMouseCursor", 0))
         setMouseTracking(true); // Required for mouse cursor auto-hide

commit 4322464f6bd4f5a2c26ab7bb85d0cb8cee8113e2

libmythui: Fix window positioning for multi-screen setups.
3ad6c81aa83491b7e6d2 (fullscreen window fix for Compiz) broke window
positioning when using multi-screen setups. Another round of testing
later and it looks like we just need to initialise a few vars before the
Show(). No obvious regressions...

mythtv/libs/libmythui/mythmainwindow.cpp
@@ -915,6 +915,10 @@ void MythMainWindow::Init(void)
 #endif
 
     setWindowFlags(flags);
+
+    d->screenRect = QRect(d->xbase, d->ybase, d->screenwidth, d->screenheight);
+    d->uiScreenRect = QRect(0, 0, d->screenwidth, d->screenheight);
+
     Show();
 
     if (d->does_fill_screen && !GetMythUI()->IsGeometryOverridden())
@@ -922,9 +926,6 @@ void MythMainWindow::Init(void)
         VERBOSE(VB_GENERAL, "Using Full Screen Window");
         setWindowState(Qt::WindowFullScreen);
     }
-
-    d->screenRect = QRect(d->xbase, d->ybase, d->screenwidth, d->screenheight);
-    d->uiScreenRect = QRect(0, 0, d->screenwidth, d->screenheight);
 
     setGeometry(d->xbase, d->ybase, d->screenwidth, d->screenheight);
     setFixedSize(QSize(d->screenwidth, d->screenheight));