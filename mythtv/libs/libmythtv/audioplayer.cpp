3bcecd0d6bfc7d590af61811a57bc537d6587c76
Jean-Yves Avenard <jyavenard@mythtv.org>
2011-03-22 13:05:19 +1100

    MythPlayer: fix possible leak.
    If SetAudioOutput was called when an audio object had already been created; the existing object wouldn't have been destroyed

--- mythtv/libs/libmythtv/audioplayer.cpp
+++ mythtv/libs/libmythtv/audioplayer.cpp
@@ -152,7 +152,7 @@ QString AudioPlayer::ReinitAudio(void)
         }
         m_no_audio_out = true;
     }
-    else if (m_no_audio_out)
+    else if (m_no_audio_out && m_audioOutput)
     {
         VERBOSE(VB_IMPORTANT, LOC + "Enabling Audio");
         m_no_audio_out = false;
@@ -200,8 +200,9 @@ void AudioPlayer::PauseAudioUntilBuffered()
 
 void AudioPlayer::SetAudioOutput(AudioOutput *ao)
 {
+    // delete current audio class if any
+    DeleteOutput();
     m_lock.lock();
-    RemoveVisuals();
     m_audioOutput = ao;
     AddVisuals();
     m_lock.unlock();
