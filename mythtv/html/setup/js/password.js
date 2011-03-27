2b9edb9b5a3b4ded5d56232aae2a5bd37bd725b3
Robert McNamara <rmcnamara@mythtv.org>
2011-03-27 14:03:42 -0700

    HTML Setup: Require confirmation of the new password when changing.
    Use the method employed almost everywhere to insist that the user enter the new password twice.  If they don't match, don't change the password.
    
    My first javascript, hopefully I employed our project coding style, whatever that may be for js.

--- mythtv/html/setup/js/password.js
+++ mythtv/html/setup/js/password.js
@@ -3,18 +3,24 @@ function changePassword() {
     var result = false;
     var oldPassword = $("#oldPassword").val();
     var newPassword = $("#newPassword").val();
+    var newPasswordConfirm = $("#newPasswordConfirm").val();
 
-    $.post("/Myth/ChangePassword",
-        { UserName: "admin",
-          OldPassword: oldPassword,
-          NewPassword: newPassword },
-        function(data) {
-            if (data.bool == "true")
-                alert("Password successfully changed.");
-            else
+    if (newPassword != newPasswordConfirm) {
+        alert("New passwords do not match.");
+    }
+    else {
+        $.post("/Myth/ChangePassword",
+            { UserName: "admin",
+              OldPassword: oldPassword,
+              NewPassword: newPassword },
+            function(data) {
+                if (data.bool == "true")
+                    alert("Password successfully changed.");
+                else
+                    alert("Error changing password, check backend logs for detailed information.");
+            }, "json").error(function(data) {
                 alert("Error changing password, check backend logs for detailed information.");
-        }, "json").error(function(data) {
-            alert("Error changing password, check backend logs for detailed information.");
-        });
+            });
+    }
 }
 
