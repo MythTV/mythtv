29e03ce18d7d4c010ba00c5fa62f1746029e50d6
Stuart Morgan <smorgan@mythtv.org>
2011-05-11 19:47:15 +0100

    Move the time offset debugging messages to VB_CHANNEL+VB_EXTRA.
    These are printed once a second on some systems resulting in
    unnecessarily huge logs for anyone trying to debug with VB_CHANNEL.

--- mythtv/libs/libmythtv/dtvsignalmonitor.cpp
+++ mythtv/libs/libmythtv/dtvsignalmonitor.cpp
@@ -370,7 +370,7 @@ void DTVSignalMonitor::HandlePMT(uint, const ProgramMapTable *pmt)
 
 void DTVSignalMonitor::HandleSTT(const SystemTimeTable*)
 {
-    VERBOSE(VB_CHANNEL, LOC + "Time Offset: "<<GetStreamData()->TimeOffset());
+    VERBOSE(VB_CHANNEL+VB_EXTRA, LOC + "Time Offset: "<<GetStreamData()->TimeOffset());
 }
 
 void DTVSignalMonitor::HandleMGT(const MasterGuideTable* mgt)
@@ -439,7 +439,7 @@ void DTVSignalMonitor::HandleCVCT(uint, const CableVirtualChannelTable* cvct)
 
 void DTVSignalMonitor::HandleTDT(const TimeDateTable*)
 {
-    VERBOSE(VB_CHANNEL, LOC + "Time Offset: "<<GetStreamData()->TimeOffset());
+    VERBOSE(VB_CHANNEL+VB_EXTRA, LOC + "Time Offset: "<<GetStreamData()->TimeOffset());
 }
 
 void DTVSignalMonitor::HandleNIT(const NetworkInformationTable *nit)
