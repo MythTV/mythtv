dfcf4cc307fa9a4192db115e58a29886d30a27da
Gavin Hurlbut <ghurlbut@mythtv.org>
2011-05-16 11:49:14 -0700

    Remove the stupid plugin-related warning
    Why Qt doesn't take care of this for us in the macro, I dunno, but this shuts
    it up.

--- mythplugins/mythgallery/dcrawplugin/dcrawplugin.cpp
+++ mythplugins/mythgallery/dcrawplugin/dcrawplugin.cpp
@@ -38,5 +38,8 @@ QImageIOHandler *DcrawPlugin::create(QIODevice *device, const QByteArray &format
     return handler;
 }
 
+// This shouldn't be necessary, but it shuts up the dang compiler warning.
+QObject* qt_plugin_instance_dcrawplugin();
+
 Q_EXPORT_PLUGIN2(dcrawplugin, DcrawPlugin)
 
