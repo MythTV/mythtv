4d772e0be9cd5daff23009752d2ce24c9495595e
Mark Kendall <mkendall@mythtv.org>
2011-05-31 09:01:30 +0800

    BDRingBuffer: Suppress some compiler warnings for uint comparisons.

--- mythtv/libs/libmythtv/bdringbuffer.cpp
+++ mythtv/libs/libmythtv/bdringbuffer.cpp
@@ -1003,7 +1003,7 @@ void BDRingBuffer::HandleBDEvent(BD_EVENT &ev)
 
 bool BDRingBuffer::IsInStillFrame(void) const
 {
-    return m_stillTime >= 0 && m_stillMode != BLURAY_STILL_NONE;
+    return m_stillTime > 0 && m_stillMode != BLURAY_STILL_NONE;
 }
 
 void BDRingBuffer::WaitForPlayer(void)
@@ -1069,10 +1069,8 @@ void BDRingBuffer::SubmitOverlay(const bd_overlay_s * const overlay)
     if (!overlay)
         return;
 
-    if ((overlay->w <= 0) || (overlay->w > 1920) ||
-        (overlay->x <  0) || (overlay->x > 1920) ||
-        (overlay->h <= 0) || (overlay->h > 1080) ||
-        (overlay->y <  0) || (overlay->y > 1080))
+    if ((overlay->w < 1) || (overlay->w > 1920) || (overlay->x > 1920) ||
+        (overlay->h < 1) || (overlay->h > 1080) || (overlay->y > 1080))
     {
         VERBOSE(VB_IMPORTANT, LOC_ERR +
             QString("Invalid overlay size: %1x%2+%3+%4")
