6142dd22444ca9266f725e14db3d8d2ea0acf5de
Mark Kendall <mkendall@mythtv.org>
2011-06-05 23:08:59 +0800

    Whitespace/coding standards cleanup for Jitterometer.

--- mythtv/libs/libmythtv/jitterometer.cpp
+++ mythtv/libs/libmythtv/jitterometer.cpp
@@ -7,8 +7,8 @@
 
 #include "jitterometer.h"
 
-Jitterometer::Jitterometer(const char *nname, int ncycles) :
-    count(0), num_cycles(ncycles), starttime_valid(0)
+Jitterometer::Jitterometer(const char *nname, int ncycles)
+  : count(0), num_cycles(ncycles), starttime_valid(0)
 {
     times = (unsigned*) malloc(ncycles * sizeof(unsigned));
     memset(&starttime, 0, sizeof(struct timeval));
@@ -21,76 +21,65 @@ Jitterometer::Jitterometer(const char *nname, int ncycles) :
 
 Jitterometer::~Jitterometer()
 {
-  free(times);
-  free(name);
+    free(times);
+    free(name);
 }
 
 bool Jitterometer::RecordCycleTime()
 {
-  bool ret = RecordEndTime();
-  RecordStartTime();
-
-  return ret;
+    bool ret = RecordEndTime();
+    RecordStartTime();
+    return ret;
 }
 
 bool Jitterometer::RecordEndTime()
 {
-  struct timeval timenow;
+    struct timeval timenow;
+    gettimeofday(&timenow, NULL);
 
-  gettimeofday(&timenow, NULL);
-
-  if (starttime_valid)
+    if (starttime_valid)
     {
-      times[count] =
-        (timenow.tv_sec  - starttime.tv_sec ) * 1000000 +
-        (timenow.tv_usec - starttime.tv_usec) ;
-
-      //printf("recorded timediff '%d'\n", times[count]);
-
-      count++;
+        times[count] = (timenow.tv_sec  - starttime.tv_sec ) * 1000000 +
+                       (timenow.tv_usec - starttime.tv_usec) ;
+        count++;
     }
 
-  starttime_valid = 0;
+    starttime_valid = 0;
 
-  if (count==num_cycles)
+    if (count == num_cycles)
     {
-      /* compute and display stuff, reset count to -1  */
+        /* compute and display stuff, reset count to -1  */
+        double mean = 0, sum_of_squared_deviations=0;
+        double standard_deviation;
+        double fps = 0, tottime = 0;
+        int i;
 
-      double mean = 0, sum_of_squared_deviations=0;
-      double standard_deviation;
-      double fps = 0, tottime = 0;
-      int i;
+        /* compute the mean */
+        for(i = 0; i < num_cycles; i++)
+            mean += times[i];
 
-      /* compute the mean */
-      for(i=0; i<num_cycles; i++)
-        {
-          mean += times[i];
-        }
-      tottime = mean;
-      mean /= num_cycles;
+        tottime = mean;
+        mean /= num_cycles;
 
-      fps = num_cycles / tottime * 1000000;
+        fps = num_cycles / tottime * 1000000;
 
-      /* compute the sum of the squares of each deviation from the mean */
-      for(i=0; i<num_cycles;i++)
-        {
-          sum_of_squared_deviations += (mean - times[i]) * (mean - times[i]);
-        }
+        /* compute the sum of the squares of each deviation from the mean */
+        for(i = 0; i < num_cycles; i++)
+            sum_of_squared_deviations += (mean - times[i]) * (mean - times[i]);
 
-      /* compute standard deviation */
-      standard_deviation = sqrt(sum_of_squared_deviations / (num_cycles - 1));
+        /* compute standard deviation */
+        standard_deviation = sqrt(sum_of_squared_deviations / (num_cycles - 1));
 
-      printf("'%s' mean = '%.2f', std. dev. = '%.2f', fps = '%.2f'\n", name, mean, standard_deviation, fps);
+        printf("'%s' mean = '%.2f', std. dev. = '%.2f', fps = '%.2f'\n", name, mean, standard_deviation, fps);
 
-      count = 0;
-
-      return true;
+        count = 0;
+        return true;
     }
     return false;
 }
 
 void Jitterometer::RecordStartTime()
 {
-  gettimeofday(&starttime, NULL);
-  starttime_valid = 1;
+    gettimeofday(&starttime, NULL);
+    starttime_valid = 1;
 }
--- mythtv/libs/libmythtv/jitterometer.h
+++ mythtv/libs/libmythtv/jitterometer.h
@@ -1,60 +1,52 @@
 #ifndef JITTEROMETER_H
 #define JITTEROMETER_H
 
+/* Jitterometer usage. There are 2 ways to use this:
+------------------------------------------------------------------
+
+ 1.  Every 100 iterations of the for loop, RecordCycleTime() will
+     print a report about the mean time to execute the loop, and
+     the jitter in the recorded times.
+
+       my_jmeter = new Jitterometer("forloop", 100);
+       for ( ) {
+         ... some stuff ...
+         my_jmeter->RecordCycleTime();
+       }
+
+-------------------------------------------------------------------
+
+2.  Every 42 times Weird_Operation() is run, RecordEndTime() will
+    print a report about the mean time to do a Weird_Operation(), and
+    the jitter in the recorded times.
+
+      beer = new Jitterometer("weird operation", 42);
+      for( ) {
+         ...
+         beer->RecordStartTime();
+         Weird_Operation();
+         beer->RecordEndTime();
+         ...
+      }
+*/
 
 class Jitterometer
 {
- public:
-  Jitterometer(const char *name, int num_cycles);
-  ~Jitterometer();
+  public:
+    Jitterometer(const char *name, int num_cycles);
+   ~Jitterometer();
 
-
-  /* Jitterometer usage. There are 2 ways to use this:
-
-   ------------------------------------------------------------------
-
-   1.  Every 100 iterations of the for loop, RecordCycleTime() will
-       print a report about the mean time to execute the loop, and
-       the jitter in the recorded times.
- 
-         my_jmeter = new Jitterometer("forloop", 100);
-         for ( ) {
-
-           ... some stuff ...
-
-           my_jmeter->RecordCycleTime();
-         }
-
-  -------------------------------------------------------------------
-
-  2.  Every 42 times Weird_Operation() is run, RecordEndTime() will
-      print a report about the mean time to do a Weird_Operation(), and
-      the jitter in the recorded times.
-
-        beer = new Jitterometer("weird operation", 42);
-        for( ) {
-           ...
-           beer->RecordStartTime();
-           Weird_Operation();
-           beer->RecordEndTime();
-           ...
-        }
-  */
-
-  bool RecordCycleTime();
-
-  void RecordStartTime();
-  bool RecordEndTime();
+    bool RecordCycleTime();
+    void RecordStartTime();
+    bool RecordEndTime();
 
  private:
-  int count;
-  int num_cycles;
-
-  struct timeval starttime;
-  int starttime_valid;
-  unsigned *times; // array of cycle lengths, in uS
-
-  char *name;
+    int count;
+    int num_cycles;
+    struct timeval starttime;
+    int starttime_valid;
+    unsigned *times; // array of cycle lengths, in uS
+    char *name;
 };
 
 #endif // JITTEROMETER_H
