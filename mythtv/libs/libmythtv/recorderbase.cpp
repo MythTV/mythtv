6b30c8a91d9f2f147f1961af712a766fad30bce6
Daniel Kristjansson <danielk@cuymedia.net>
2011-06-17 17:28:28 -0400

    Remove SetOption for 'dvb_on_demand', it isn't used by the recorder.

--- mythtv/libs/libmythtv/recorderbase.cpp
+++ mythtv/libs/libmythtv/recorderbase.cpp
@@ -503,7 +503,6 @@ RecorderBase *RecorderBase::CreateRecorder(
         recorder = new DVBRecorder(
             tvrec, dynamic_cast<DVBChannel*>(channel));
         recorder->SetOption("wait_for_seqstart", genOpt.wait_for_seqstart);
-        recorder->SetOption("dvb_on_demand",     dvbOpt.dvb_on_demand);
 #endif // USING_DVB
     }
     else if (genOpt.cardtype == "FREEBOX")
