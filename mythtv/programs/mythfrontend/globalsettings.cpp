cb5cfeafcb1f348650972372c9fc67da04b9e4e6
Mark Kendall <mkendall@mythtv.org>
2011-06-27 08:39:23 +0800

    TV Playback: Make the default video zoom 'Off'.
    Using auto detect as the default is potentially confusing for new users
    as they may not expect that behaviour, it can be triggerred by false
    positives and is not consistently applied (e.g. VDPAU).

--- mythtv/programs/mythfrontend/globalsettings.cpp
+++ mythtv/programs/mythfrontend/globalsettings.cpp
@@ -1811,12 +1811,12 @@ static HostComboBox *AdjustFill()
 {
     HostComboBox *gc = new HostComboBox("AdjustFill");
     gc->setLabel(QObject::tr("Zoom"));
+    for (int m = kAdjustFill_Off; m < kAdjustFill_END; ++m)
+        gc->addSelection(toString((AdjustFillMode)m), QString::number(m));
     gc->addSelection(toString(kAdjustFill_AutoDetect_DefaultOff),
                      QString::number(kAdjustFill_AutoDetect_DefaultOff));
     gc->addSelection(toString(kAdjustFill_AutoDetect_DefaultHalf),
                      QString::number(kAdjustFill_AutoDetect_DefaultHalf));
-    for (int m = kAdjustFill_Off; m < kAdjustFill_END; ++m)
-        gc->addSelection(toString((AdjustFillMode)m), QString::number(m));
     gc->setHelpText(QObject::tr(
                         "When enabled, these will apply a predefined "
                         "zoom to all video playback in MythTV."));
