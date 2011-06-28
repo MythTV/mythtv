MythTV/mythtv

commit 45cfaa7a8cf7cdca2d7c7cd8d815c08d24a5cfa0

Run the image last modified check in the background for non-local images.
When loading an image, we normally check the last modified date to
determine if we can use the cached image or if we need to load a new
copy.  For non-local images (myth://, http://, etc.), this check can
be expensive, so perform all non-local last modified checks in the
background in the image loading thread.  This should speed up the UI
drawing when there are non-local images on screen.

mythtv/libs/libmythui/mythuiimage.cpp
@@ -602,10 +602,10 @@ bool MythUIImage::Load(bool allowLoadInBackground, bool forceStat)
         ImageCacheMode cacheMode2 = (!forceStat) ? kCacheNormal :
             (ImageCacheMode) ((int)kCacheNormal | (int)kCacheForceStat);
 
-
         if ((allowLoadInBackground) &&
-            (!GetMythUI()->LoadCacheImage(filename, imagelabel,
-                                          GetPainter(), cacheMode)) &&
+            ((!filename.startsWith("/")) ||
+             (!GetMythUI()->LoadCacheImage(filename, imagelabel,
+                                           GetPainter(), cacheMode))) &&
             (!getenv("DISABLETHREADEDMYTHUIIMAGE")))
         {
             VERBOSE(VB_GUI|VB_FILE|VB_EXTRA, LOC + QString(

commit fa0c90d402f70d85051a30500328e90d5d3b6254

Fix a regresssion in 45cfaa7a8 related to local image loading.
45cfaa7a8 made itso that all images not starting with a '/'
were loaded in the background without first checking the last
modified time in the foreground first.  This somehow caused an
issue with local relative filenames.  This bugfix changes the
logic a little to check for remote filenames instead of checking
for a '/'.

There still may be a race condition or locking issue which
actually caused the problem seen after 45cfaa7a8, but this
patch fixes the code so that the intent of 45cfaa7a8 is accomplished.

mythtv/libs/libmythui/mythuiimage.cpp
@@ -580,6 +580,15 @@ bool MythUIImage::Load(bool allowLoadInBackground, bool forceStat)
             h = bForceSize.height();
     }
 
+    bool bPreferLoadInBackground =
+        ((filename.startsWith("myth://")) ||
+         (filename.startsWith("http://")) ||
+         (filename.startsWith("https://")) ||
+         (filename.startsWith("ftp://")));
+
+    if (getenv("DISABLETHREADEDMYTHUIIMAGE"))
+        allowLoadInBackground = false;
+
     QString imagelabel;
 
     int j = 0;
@@ -602,10 +611,9 @@ bool MythUIImage::Load(bool allowLoadInBackground, bool forceStat)
             (ImageCacheMode) ((int)kCacheNormal | (int)kCacheForceStat);
 
         if ((allowLoadInBackground) &&
-            ((!filename.startsWith("/")) ||
+            ((bPreferLoadInBackground) ||
              (!GetMythUI()->LoadCacheImage(filename, imagelabel,
-                                           GetPainter(), cacheMode))) &&
-            (!getenv("DISABLETHREADEDMYTHUIIMAGE")))
+                                           GetPainter(), cacheMode))))
         {
             VERBOSE(VB_GUI|VB_FILE|VB_EXTRA, LOC + QString(
                         "Load(), spawning thread to load '%1'").arg(filename));