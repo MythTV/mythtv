ef3922514267e53c774ecaeda52d2aa071a1ecf2
Stuart Morgan <smorgan@mythtv.org>
2011-06-28 20:34:45 +0100

    Suspend recording list updates whilst playback is in progress as this causes issues for some people. This is likely a temporary fix while I work on something better. (cherry picked from commit 1bbc9a43eb63f1569f8b510c4588a439019b49dc)

--- mythtv/programs/mythfrontend/playbackbox.cpp
+++ mythtv/programs/mythfrontend/playbackbox.cpp
@@ -1219,7 +1219,8 @@ void PlaybackBox::updateRecList(MythUIButtonListItem *sel_item)
 
     updateGroupInfo(groupname, grouplabel);
 
-    if ((m_currentGroup == groupname) && !m_needUpdate)
+    if (((m_currentGroup == groupname) && !m_needUpdate) ||
+        m_playingSomething)
         return;
 
     m_needUpdate = false;
@@ -2351,6 +2352,9 @@ bool PlaybackBox::Play(
         if (pginfo)
             UpdateUIListItem(pginfo, true);
     }
+
+    if (m_needUpdate)
+        ScheduleUpdateUIList();
 
     return playCompleted;
 }
@@ -3852,8 +3856,13 @@ void PlaybackBox::customEvent(QEvent *event)
         }
         else if (message == "UPDATE_UI_LIST")
         {
-            UpdateUILists();
-            m_helper.ForceFreeSpaceUpdate();
+            if (m_playingSomething)
+                m_needUpdate = true;
+            else
+            {
+                UpdateUILists();
+                m_helper.ForceFreeSpaceUpdate();
+            }
         }
         else if (message == "UPDATE_USAGE_UI")
         {
