5251f2877f8105a4bbc699e9df9992c382b16da3
Raymond Wagner <rwagner@mythtv.org>
2011-07-15 13:36:38 -0400

    Fix index rebuilds for video content.  Remove some additional unused variables.

--- mythtv/programs/mythcommflag/main.cpp
+++ mythtv/programs/mythcommflag/main.cpp
@@ -1066,15 +1066,9 @@ int main(int argc, char *argv[])
     bool isVideo = false;
     int result = GENERIC_EXIT_OK;
 
-    QString filename;
-    QString outputfilename = QString::null;
-
-    QDateTime starttime;
-    QString allStart = "19700101000000";
-    QString allEnd   = QDateTime::currentDateTime().toString("yyyyMMddhhmmss");
+//    QString allStart = "19700101000000";
+//    QString allEnd   = QDateTime::currentDateTime().toString("yyyyMMddhhmmss");
     int jobType = JOB_NONE;
-    QDir fullfile;
-    QString newCutList = QString::null;
 
     if (!cmdline.Parse(argc, argv))
     {
@@ -1186,7 +1180,7 @@ int main(int argc, char *argv[])
     else if (cmdline.toBool("video"))
     {
         // build skiplist for video file
-        return RebuildSeekTable(cmdline.toString(filename), -1);
+        return RebuildSeekTable(cmdline.toString("video"), -1);
     }
     else if (cmdline.toBool("file"))
     {
