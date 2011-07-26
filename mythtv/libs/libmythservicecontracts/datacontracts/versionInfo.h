7176c9e84c641ffc475060f4f686bd4c0360515b
Raymond Wagner <rwagner@mythtv.org>
2011-07-26 18:13:12 -0400

    Add missing header from e37bf6c00d99611fd7

---
+++ mythtv/libs/libmythservicecontracts/datacontracts/versionInfo.h
@@ -0,0 +1,95 @@
+//////////////////////////////////////////////////////////////////////////////
+// Program Name: versionInfo.h
+// Created     : Jul. 26, 2011
+//
+// Copyright (c) 2011 Raymond Wagner <rwagner@mythtv.org>
+//                                          
+// This library is free software; you can redistribute it and/or 
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or at your option any later version of the LGPL.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef VERSIONINFO_H_
+#define VERSIONINFO_H_
+
+#include <QString>
+
+#include "serviceexp.h" 
+#include "datacontracthelper.h"
+
+namespace DTC
+{
+
+class SERVICE_PUBLIC VersionInfo : public QObject
+{
+    Q_OBJECT
+
+    Q_CLASSINFO( "version"    , "1.0" );
+    Q_CLASSINFO( "defaultProp", "Command" );
+
+    Q_PROPERTY( QString  Version      READ Version   WRITE setVersion   )
+    Q_PROPERTY( QString  Branch       READ Branch    WRITE setBranch    )
+    Q_PROPERTY( QString  Protocol     READ Protocol  WRITE setProtocol  )
+    Q_PROPERTY( QString  Binary       READ Binary    WRITE setBinary    )
+    Q_PROPERTY( QString  Schema       READ Schema    WRITE setSchema    )
+
+    PROPERTYIMP( QString,  Version   )
+    PROPERTYIMP( QString,  Branch    )
+    PROPERTYIMP( QString,  Protocol  )
+    PROPERTYIMP( QString,  Binary    )
+    PROPERTYIMP( QString,  Schema    )
+
+    public:
+
+        static void InitializeCustomTypes()
+        {
+            qRegisterMetaType< VersionInfo   >();
+            qRegisterMetaType< VersionInfo*  >();
+        }
+
+    public:
+
+        VersionInfo(QObject *parent = 0)
+            : QObject   ( parent ),
+              m_Version ( ""     ),
+              m_Branch  ( ""     ),
+              m_Protocol( ""     ),
+              m_Binary  ( ""     ),
+              m_Schema  ( ""     )
+        {
+        }
+        
+        VersionInfo( const VersionInfo &src ) 
+        {
+            Copy( src );
+        }
+
+        void Copy( const VersionInfo &src )
+        {
+            m_Version  = src.m_Version;
+            m_Branch   = src.m_Branch;
+            m_Protocol = src.m_Protocol;
+            m_Binary   = src.m_Binary;
+            m_Schema   = src.m_Schema;
+        }
+};
+
+typedef VersionInfo* VersionInfoPtr;
+
+} // namespace DTC
+
+Q_DECLARE_METATYPE( DTC::VersionInfo  )
+Q_DECLARE_METATYPE( DTC::VersionInfo* )
+
+
+#endif
