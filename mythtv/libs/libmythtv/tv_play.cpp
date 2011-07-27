5211a49c72be5703b9c3f5c72ef1b9141fca1bfd
Lawrence Rust <lvr@softsystem.co.uk>
2011-07-27 22:56:59 +0100

    TV: Restore OSD tuning info during TV startup.
    Fixes #9933.
    
    Signed-off-by: Paul Harrison <pharrison@mythtv.org>

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -8402,7 +8402,7 @@ void TV::customEvent(QEvent *e)
 
         PlayerContext *mctx = GetPlayerReadLock(0, __FILE__, __LINE__);
         OSD *osd = GetOSDLock(mctx);
-        if (osd && osd->IsWindowVisible("program_info"))
+        if (osd && !osd->IsWindowVisible(OSD_WIN_INTERACT))
         {
             for (uint i = 0; mctx && (i < player.size()); i++)
             {
