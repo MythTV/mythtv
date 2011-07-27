MythTV/mythtv

commit 0affac2f96c9030e185bb919c2920450712344fe

libmyth: Fix/enable visualisations for non mythmusic sources.

mythtv/libs/libmyth/audio/audiooutputbase.cpp
@@ -1292,14 +1292,17 @@ bool AudioOutputBase::AddData(void *in_buffer, int in_len,
     QMutexLocker lock(&audio_buflock);
 
     // Mythmusic doesn't give us timestamps
-    if (timecode < 0)
+    if (hasVisual())
     {
-        // Send original samples to mythmusic visualisation
-        timecode = (frames_buffered * 1000) / source_samplerate;
-        frames_buffered += frames;
+        if (timecode < 0)
+        {
+            // Send original samples to mythmusic visualisation
+            timecode = (frames_buffered * 1000) / source_samplerate;
+            frames_buffered += frames;
+            music = true;
+        }
         dispatchVisual((uchar *)in_buffer, len, timecode, source_channels,
                        output_settings->FormatToBits(format));
-        music = true;
     }
 
     // Calculate amount of free space required in ringbuffer

mythtv/libs/libmyth/output.h
@@ -102,6 +102,7 @@ public:
     OutputListeners();
     virtual ~OutputListeners();
 
+    bool hasVisual(void) { return visuals.size(); }
     void addVisual(MythTV::Visual *);
     void removeVisual(MythTV::Visual *);
 

commit dc4e31667e53cbc41227afd3e0d176ca1a80bf82

Recalculate the audio time stamp even when the audiooutput has no visual attached.
This fixes a bug introduced in https://github.com/MythTV/mythtv/commit/0affac2f96c903
were MythMusic wasn't updating the time on the playback screen after returning to it.

mythtv/libs/libmyth/audio/audiooutputbase.cpp
@@ -1277,15 +1277,16 @@ bool AudioOutputBase::AddData(void *in_buffer, int in_len,
               .arg(needs_upmix));
 
     // Mythmusic doesn't give us timestamps
+    if (timecode < 0)
+    {
+        // Send original samples to mythmusic visualisation
+        timecode = (frames_buffered * 1000) / source_samplerate;
+        frames_buffered += frames;
+        music = true;
+    }
+
     if (hasVisual())
     {
-        if (timecode < 0)
-        {
-            // Send original samples to mythmusic visualisation
-            timecode = (frames_buffered * 1000) / source_samplerate;
-            frames_buffered += frames;
-            music = true;
-        }
         dispatchVisual((uchar *)in_buffer, len, timecode, source_channels,
                        output_settings->FormatToBits(format));
     }