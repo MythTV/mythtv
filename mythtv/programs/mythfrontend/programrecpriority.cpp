2334dd1168bfd61c18b47bfd0fb91bd34b4bab91
Stuart Morgan <smorgan@mythtv.org>
2011-08-01 16:17:33 +0100

    Storagegroup is in the ProgramInfo text map, so we don't need a special textarea for it.

--- mythtv/programs/mythfrontend/programrecpriority.cpp
+++ mythtv/programs/mythfrontend/programrecpriority.cpp
@@ -13,7 +13,6 @@ using namespace std;
 // libmythtv headers
 #include "recordingrule.h"
 #include "scheduledrecording.h"
-#include "storagegroup.h"
 
 // libmythbase
 #include "mythdb.h"
@@ -390,7 +389,6 @@ ProgramRecPriority::ProgramRecPriority(MythScreenStack *parent,
                      m_programList(NULL), m_schedInfoText(NULL),
                      m_rectypePriorityText(NULL), m_recPriorityText(NULL),
                      m_recPriorityBText(NULL), m_finalPriorityText(NULL),
-                     m_storageGroupText(NULL),
                      m_lastRecordedText(NULL), m_lastRecordedDateText(NULL),
                      m_lastRecordedTimeText(NULL), m_channameText(NULL),
                      m_channumText(NULL), m_callsignText(NULL),
@@ -422,7 +420,6 @@ bool ProgramRecPriority::Create()
     m_recPriorityText = dynamic_cast<MythUIText *> (GetChild("recpriority"));
     m_recPriorityBText = dynamic_cast<MythUIText *> (GetChild("recpriorityB"));
     m_finalPriorityText = dynamic_cast<MythUIText *> (GetChild("finalpriority"));
-    m_storageGroupText = dynamic_cast<MythUIText *> (GetChild("storagegroup"));
     m_lastRecordedText = dynamic_cast<MythUIText *> (GetChild("lastrecorded"));
     m_lastRecordedDateText = dynamic_cast<MythUIText *> (GetChild("lastrecordeddate"));
     m_lastRecordedTimeText = dynamic_cast<MythUIText *> (GetChild("lastrecordedtime"));
@@ -1584,16 +1581,6 @@ void ProgramRecPriority::updateInfo(MythUIButtonListItem *item)
 
     if (m_finalPriorityText)
         m_finalPriorityText->SetText(QString::number(finalRecPriority));
-
-    if (m_storageGroupText)
-    {
-        QString storagegroup = pgRecInfo->storagegroup;
-        if (storagegroup == "Default")
-                storagegroup = tr("Default");
-        else if (StorageGroup::kSpecialGroups.contains(storagegroup))
-            storagegroup = tr(storagegroup.toLatin1().constData());
-        m_storageGroupText->SetText(storagegroup);
-    }
 
     if (m_lastRecordedText)
     {
