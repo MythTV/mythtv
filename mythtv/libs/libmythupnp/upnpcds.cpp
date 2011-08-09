6fc96707e185a79be777d8522658a399b87fa1ce
Gavin Hurlbut <ghurlbut@mythtv.org>
2011-08-08 21:16:16 -0700

    Change using std::max to using namespace std to match other code

--- mythtv/libs/libmythupnp/upnpcds.cpp
+++ mythtv/libs/libmythupnp/upnpcds.cpp
@@ -23,7 +23,7 @@
 
 #include <cmath>
 #include <algorithm>
-using std::max;
+using namespace std;
 
 #include "upnp.h"
 #include "upnpcds.h"
@@ -896,7 +896,7 @@ UPnpCDSExtensionResults *
 
             short nStart = max(pRequest->m_nStartingIndex, short(0));
             short nEnd   = min(nRootCount,
-                               short( nStart + pRequest->m_nRequestedCount));
+                               short(nStart + pRequest->m_nRequestedCount));
 
             if (nStart < nRootCount)
             {
