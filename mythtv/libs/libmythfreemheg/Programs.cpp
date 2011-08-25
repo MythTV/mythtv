f9e1569497cd0cfbd5317b9991db290125042ac8
Gavin Hurlbut <ghurlbut@mythtv.org>
2011-08-24 23:58:17 -0700

    Convert a random() to qrand()
    Now that we have qsrand() running in each mthread's prolog, we should be safe
    to use qrand().  This instance is blocking windows compiling as there is no
    "random()" in mingw.

--- mythtv/libs/libmythfreemheg/Programs.cpp
+++ mythtv/libs/libmythfreemheg/Programs.cpp
@@ -19,6 +19,8 @@
 
 */
 
+#include <QtGlobal>
+
 #include "Programs.h"
 #include "Ingredients.h"
 #include "Root.h"
@@ -365,7 +367,7 @@ void MHResidentProgram::CallProgram(bool fIsFork, const MHObjectRef &success, co
             {
                 int nLimit = GetInt(args.GetAt(0), engine);
                 MHParameter *pResInt = args.GetAt(1);
-                int r = random() % nLimit + 1;
+                int r = qrand() % nLimit + 1;
                 engine->FindObject(
                     *(pResInt->GetReference()))->SetVariableValue(r);
                 SetSuccessFlag(success, true, engine);
