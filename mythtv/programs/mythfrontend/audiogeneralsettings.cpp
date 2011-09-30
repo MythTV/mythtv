1d6041ec3a268b163157507ebd38787e9ff8c4a2
Jean-Yves Avenard <jyavenard@mythtv.org>
2011-09-30 13:26:43 +1000

    Prevent leak when testing stereo mode only in speaker-test

--- mythtv/programs/mythfrontend/audiogeneralsettings.cpp
+++ mythtv/programs/mythfrontend/audiogeneralsettings.cpp
@@ -579,7 +579,7 @@ void AudioTestThread::run()
 {
     m_interrupted = false;
     int smptelayout[7][8] = { 
-        { 0, 0 , 1 },                   //stereo
+        { 0, 1, 1 },                    //stereo
         { },                            //not used
         { },                            //not used
         { },                            //not used
@@ -726,11 +726,6 @@ AudioTest::AudioTest(QString main, QString passthrough,
     m_frontright->setLabel(QObject::tr("Front Right"));
     connect(m_frontright,
             SIGNAL(pressed(QString)), this, SLOT(toggle(QString)));
-    m_center = new TransButtonSetting("1");
-    m_center->setLabel(QObject::tr("Center"));
-    connect(m_center,
-            SIGNAL(pressed(QString)), this, SLOT(toggle(QString)));
-
     frontgroup->addChild(m_frontleft);
 
     switch(m_channels)
@@ -763,6 +758,10 @@ AudioTest::AudioTest(QString main, QString passthrough,
             connect(m_lfe,
                     SIGNAL(pressed(QString)), this, SLOT(toggle(QString)));
 
+            m_center = new TransButtonSetting("1");
+            m_center->setLabel(QObject::tr("Center"));
+            connect(m_center,
+                    SIGNAL(pressed(QString)), this, SLOT(toggle(QString)));
             frontgroup->addChild(m_center);
             middlegroup->addChild(m_surroundleft);
             middlegroup->addChild(m_lfe);
