58df3cd5d488081b294348dfc3440fdb765e9e4d
Robert Kulagowski <rkulagowski@mythtv.org>
2011-10-13 11:44:58 -0500

    More spelling.

--- mythtv/themes/themestrings.h
+++ mythtv/themes/themestrings.h
@@ -879,7 +879,7 @@ void strings_null() {
     ThemeUI::tr("TintedGlass is a transparent theme that is a little old school MythTV yet takes advantage of new MythUI features.");
     ThemeUI::tr("a nice transparent theme to use your own background if you want. just copy your favorite picture (be sure that its resolution is 1920x1200) to /usr/share/mythtv/themes/TransBlue/background.jpg");
     ThemeUI::tr("A UI and OSD theme with focus for fanart, banners and cover display.");
-    ThemeUI::tr("This theme breaks away from many conventions. It uses squirrly fonts!");
+    ThemeUI::tr("This theme breaks away from many conventions. It uses squirrely fonts!");
     ThemeUI::tr("blootube-ng is a theme based on the original blootube-wide by Justin Hornsby (justin.hornsby@gmail.com) which uses very large graphics associated with every menu entry. All changes where made to bring this theme forward to MythTV 0.24.");
     ThemeUI::tr("Blue-abstract is a modern looking theme meant for widescreen HD displays. The blue background and the consistent layout makes it an attractive and elegant looking theme.");
 }
