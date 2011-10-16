MythTV/mythtv

commit fc0761d3bda39d4c68db0e22d045d1394002bc97

Remove asserts in MPEG descriptor constructors. Replace with IsValid() method that can be called after construction.

mythtv/libs/libmythtv/mpeg/atscdescriptors.h
@@ -3,7 +3,6 @@
 #ifndef _ATSC_DESCRIPTORS_H_
 #define _ATSC_DESCRIPTORS_H_
 
-#include <cassert>
 #include <vector>
 using namespace std;
 
@@ -19,7 +18,7 @@ typedef QMap<int, const unsigned char*> IntToBuf;
 class MultipleStringStructure
 {
   public:
-    MultipleStringStructure(const unsigned char* data) : _data(data)
+    MultipleStringStructure(const unsigned char *data) : _data(data)
     {
         Parse();
     }
@@ -64,29 +63,24 @@ class MultipleStringStructure
     QString toString() const;
 
   private:
-    static QString Uncompressed(const unsigned char* buf, int len, int mode);
+    static QString Uncompressed(const unsigned char *buf, int len, int mode);
     static uint Index(int i, int j) { return (i<<8)|(j&0xff); }
-    const unsigned char* Offset(int i, int j) const
+    const unsigned char *Offset(int i, int j) const
         { return _ptrs[Index(i,j)]; }
 
   private:
-    const unsigned char* _data;
+    const unsigned char *_data;
     mutable IntToBuf _ptrs;
 };
 
 class CaptionServiceDescriptor : public MPEGDescriptor
 {
   public:
-    CaptionServiceDescriptor(const unsigned char* data) :
-        MPEGDescriptor(data)
-    {
+    CaptionServiceDescriptor(const unsigned char *data, int len = 300) :
+        MPEGDescriptor(data, len, DescriptorID::caption_service) { }
     //       Name             bits  loc  expected value
     // descriptor_tag           8   0.0       0x86
-        assert(DescriptorID::caption_service == DescriptorTag());
-        Parse();
     // descriptor_length        8   1.0
-    }
-
     // reserved                 3   2.0       0x07
     // number_of_services       5   2.3
     uint ServicesCount() const { return _data[2]&0x1f; }
@@ -121,31 +115,26 @@ class CaptionServiceDescriptor : public MPEGDescriptor
         { return bool(((Offset(i,-1)[4])>>6) & 1); }
     //   reserved              14  4.2      0x3fff
     //}                            6.0
-    void Parse(void) const;
+    bool Parse(void);
     QString toString() const;
 
   private:
     int Index(int i, int j) const { return (i<<8) | (j & 0xff); }
-    const unsigned char* Offset(int i, int j) const
+    const unsigned char *Offset(int i, int j) const
         { return _ptrs[Index(i,j)]; }
 
   private:
-    mutable IntToBuf _ptrs;
+    IntToBuf _ptrs;
 };
 
 class ContentAdvisoryDescriptor : public MPEGDescriptor
 {
-    mutable IntToBuf _ptrs;
   public:
-    ContentAdvisoryDescriptor(const unsigned char* data) :
-        MPEGDescriptor(data)
-    {
+    ContentAdvisoryDescriptor(const unsigned char *data, int len = 300) :
+        MPEGDescriptor(data, len, DescriptorID::content_advisory) { }
     //       Name             bits  loc  expected value
     // descriptor_tag           8   0.0       0x87
-        assert(DescriptorID::content_advisory == DescriptorTag());
-        Parse();
     // descriptor_length        8   1.0
-    }
     // reserved                 2   2.0       0x03
     // rating_region_count      6   2.2
     uint RatingRegionCount(void) const { return _data[2] & 0x3f; }
@@ -171,55 +160,60 @@ class ContentAdvisoryDescriptor : public MPEGDescriptor
     //   rating_desc_text         x+2+(rated_dimensions*2)+1.0
     const MultipleStringStructure RatingDescription(uint i) const
     {
-        const unsigned char* data = Offset(i,-1) + 3 + (RatedDimensions(i)<<1);
+        const unsigned char *data = Offset(i,-1) + 3 + (RatedDimensions(i)<<1);
         return MultipleStringStructure(data);
     }
     // }
 
-    void Parse(void) const;
+    bool Parse(void);
     QString toString() const;
   protected:
     int Index(int i, int j) const { return (i<<8)|(j&0xff); }
-    const unsigned char* Offset(int i, int j) const {
-        return _ptrs[Index(i,j)];
+    const unsigned char *Offset(int i, int j) const
+    {
+        IntToBuf::const_iterator it = _ptrs.find(Index(i,j));
+        return (it != _ptrs.end()) ? *it : NULL;
     }
+    IntToBuf _ptrs;
 };
 
-class ComponentNameDescriptor : public MPEGDescriptor {
+class ComponentNameDescriptor : public MPEGDescriptor
+{
   public:
-    ComponentNameDescriptor(const unsigned char* data) :
-        MPEGDescriptor(data) {
-        assert(0xA3==DescriptorTag());
+    ComponentNameDescriptor(const unsigned char *data, int len = 300) :
+        MPEGDescriptor(data, len, DescriptorID::component_name)
+    {
     }
-    const MultipleStringStructure ComponentNameStrings() const {
+    const MultipleStringStructure ComponentNameStrings() const
+    {
         return MultipleStringStructure(_data+2);
     }
-    QString toString() const {
-        return QString("Component Name Descriptor  %1").
-            arg(ComponentNameStrings().toString());
+    QString toString() const
+    {
+        return QString("Component Name Descriptor %1")
+            .arg(ComponentNameStrings().toString());
     }
 };
 
 
 // a_52a.pdf p120, Table A2
-class AudioStreamDescriptor : public MPEGDescriptor {
+class AudioStreamDescriptor : public MPEGDescriptor
+{
   public:
-    AudioStreamDescriptor(const unsigned char* data) :
-        MPEGDescriptor(data) {
-// descriptor_tag   The value for the AC-3 descriptor tag is 0x81.
-        assert(DescriptorID::ac3_audio_stream == DescriptorTag());
-    }
+    AudioStreamDescriptor(const unsigned char *data, int len = 300) :
+        MPEGDescriptor(data, len, DescriptorID::ac3_audio_stream) { }
+    // descriptor_tag                        8   0.0   0x81
     // sample_rate_code                      3   2.0
-    uint SampleRateCode() const { return (_data[2]>>5)&7; }
-    QString SampleRateCodeString() const;
+    uint SampleRateCode(void) const { return (_data[2]>>5)&7; }
+    QString SampleRateCodeString(void) const;
     // bsid                                  5   2.3
-    uint bsid() const { return _data[2]&0x1f; }
+    uint bsid(void) const { return _data[2]&0x1f; }
     // bit_rate_code                         6   3.0
-    uint BitRateCode() const { return (_data[3]>>2)&0x3f; }
-    QString BitRateCodeString() const;
+    uint BitRateCode(void) const { return (_data[3]>>2)&0x3f; }
+    QString BitRateCodeString(void) const;
     // surround_mode                         2   3.6
-    uint SurroundMode() const { return _data[3]&3; }
-    QString SurroundModeString() const;
+    uint SurroundMode(void) const { return _data[3]&3; }
+    QString SurroundModeString(void) const;
     /*
       000 Major ?
       001 Major ?
@@ -227,37 +221,40 @@ class AudioStreamDescriptor : public MPEGDescriptor {
       111 Karaoke Mode if acmod >= 0x2. a_52a.pdf p130
     */
     // bsmod                                 3   4.0
-    uint BasicServiceMode() const { return (_data[4]>>5)&7; }
+    uint BasicServiceMode(void) const { return (_data[4]>>5)&7; }
     // num_channels                          4   4.3
-    uint Channels() const { return (_data[4]>>1)&0xf; }
-    QString ChannelsString() const;
+    uint Channels(void) const { return (_data[4]>>1)&0xf; }
+    QString ChannelsString(void) const;
 
     // full service that can be presented alone to listener?
     // full_svc                              1   4.7
-    bool FullService() const { return bool((_data[4])&1); }
+    bool FullService(void) const { return bool((_data[4])&1); }
 
     // langcod                               8   5.0
     // ignore for language specification
-    uint LanguageCode() const { return _data[5]; }
+    uint LanguageCode(void) const { return _data[5]; }
     // if(num_channels==0) /* 1+1 mode */
     //   langcod2                            8   6.0
     // ignore for language specification
-    uint LanguageCode2() const { return _data[6]; }
+    uint LanguageCode2(void) const { return _data[6]; }
 
     // if(bsmod<2) {
     //  mainid                               3   7.0/6.0
-    uint MainID() const {
+    uint MainID(void) const
+    {
         return _data[(Channels()==0)?7:6]>>5;
     }
     //  reserved                             5   7.3/6.3
-    //uint reserved() const { return _data[7]&0x1f; }
+    //uint reserved(void) const { return _data[7]&0x1f; }
     // } else asvcflags                      8   7.0/6.0
-    uint AServiceFlags() const {
+    uint AServiceFlags(void) const
+    {
         return _data[(Channels()==0)?7:6];
     }
 
     // textlen                               7   8.0/7.0
-    uint TextLength() const {
+    uint TextLength(void) const
+    {
         return _data[(Channels()==0)?8:7]>>1;
     }
 
@@ -265,16 +262,19 @@ class AudioStreamDescriptor : public MPEGDescriptor {
        using the ISO Latin-1 alphabet (ISO 8859-1). If this bit is a 0,
        the text is encoded with 2-byte unicode characters. */
     // text_code 1 bslbf
-    bool IsTextLatin1() const {
+    bool IsTextLatin1(void) const
+    {
         return bool(_data[(Channels()==0)?8:7]&1);
     }
     // for(i=0; i<M; i++) {
     //        text[i] 8 bslbf
     // }
-    QString Text() const { // TODO
+    QString Text(void) const
+    { // TODO
 #if 0
-        char* tmp = new char[TextLength()+2];
-        if (IsTextLatin1()) {
+        char *tmp = new char[TextLength()+2];
+        if (IsTextLatin1())
+        {
             memcpy(tmp, &_data[(Channels()==0)?9:8], TextLength());
             tmp[TextLength()]=0;
             for (uint i=0; i<TextLength(); i++)
@@ -282,9 +282,11 @@ class AudioStreamDescriptor : public MPEGDescriptor {
             QString str(tmp);
             delete[] tmp;
             return str;
-        } else {
+        }
+        else
+        {
             QString str; int len = TextLength();
-            const unsigned char* buf = (&_data[(Channels()==0)?9:8]);
+            const unsigned char *buf = (&_data[(Channels()==0)?9:8]);
             const unsigned short* ustr =
                 reinterpret_cast<const unsigned short*>(buf);
             for (int j=0; j<(len>>1); j++)
@@ -305,12 +307,11 @@ class AudioStreamDescriptor : public MPEGDescriptor {
  *   This is something like an ISBN for %TV shows.
  *   See also: a_57a.pdf page6 Tables 6.1, 6.2, and 7.1
  */
-class ContentIdentifierDescriptor : public MPEGDescriptor {
-    ContentIdentifierDescriptor(const unsigned char* data) :
-        MPEGDescriptor(data) {
+class ContentIdentifierDescriptor : public MPEGDescriptor
+{
+    ContentIdentifierDescriptor(const unsigned char *data, int len = 300) :
+        MPEGDescriptor(data, len, DescriptorID::atsc_content_identifier) { }
     // descriptor_tag                        8   0.0  0xB6
-        assert(0xB6==DescriptorTag());
-    }
     // descriptor_length                     8   1.0
     // content_ID_structure
     //   ID_system                           8   2.0
@@ -319,8 +320,6 @@ class ContentIdentifierDescriptor : public MPEGDescriptor {
     //        0x02-0xFF ATSC Reserved
     //   ID_length                           8   3.0
     //   content_identifier                  v   4.0
-
-    QString toString() const { return QString("ContentIdentifierDescriptor(stub)"); }
 };
 
 /**
@@ -333,10 +332,10 @@ class ContentIdentifierDescriptor : public MPEGDescriptor {
 class ExtendedChannelNameDescriptor : public MPEGDescriptor
 {
   public:
-    ExtendedChannelNameDescriptor(const unsigned char *data);
+    ExtendedChannelNameDescriptor(const unsigned char *data, int len = 300) :
+        MPEGDescriptor(data, len, DescriptorID::extended_channel_name) { }
     MultipleStringStructure LongChannelName(void) const;
     QString LongChannelNameString(void) const;
-
     QString toString() const;
 };
 

mythtv/libs/libmythtv/mpeg/mpegdescriptors.cpp
@@ -18,8 +18,13 @@ desc_list_t MPEGDescriptor::Parse(
     while (off < len)
     {
         tmp.push_back(data+off);
-        MPEGDescriptor desc(data+off);
-        off += desc.DescriptorLength() + 2;
+        MPEGDescriptor desc(data+off, len-off);
+        if (!desc.IsValid())
+        {
+            tmp.pop_back();
+            break;
+        }
+        off += desc.size();
     }
     return tmp;
 }
@@ -33,8 +38,14 @@ desc_list_t MPEGDescriptor::ParseAndExclude(
     {
         if ((data+off)[0] != excluded_descid)
             tmp.push_back(data+off);
-        MPEGDescriptor desc(data+off);
-        off += desc.DescriptorLength() + 2;
+        MPEGDescriptor desc(data+off, len-off);
+        if (!desc.IsValid())
+        {
+            if ((data+off)[0] != excluded_descid)
+                tmp.pop_back();
+            break;
+        }
+        off += desc.size();
     }
     return tmp;
 }
@@ -48,7 +59,14 @@ desc_list_t MPEGDescriptor::ParseOnlyInclude(
     {
         if ((data+off)[0] == excluded_descid)
             tmp.push_back(data+off);
-        MPEGDescriptor desc(data+off);
+        MPEGDescriptor desc(data+off, len-off);
+        if (!desc.IsValid())
+        {
+            if ((data+off)[0] == excluded_descid)
+                tmp.pop_back();
+            break;
+        }
+        off += desc.size();
         off += desc.DescriptorLength() + 2;
     }
     return tmp;
@@ -350,59 +368,81 @@ QString MPEGDescriptor::toString() const
     QString str;
 
     if (DescriptorID::registration == DescriptorTag())
-        str = RegistrationDescriptor(_data).toString();
+        str = RegistrationDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::iso_639_language == DescriptorTag())
-        str = ISO639LanguageDescriptor(_data).toString();
+        str = ISO639LanguageDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::avc_video == DescriptorTag())
-        str = AVCVideoDescriptor(_data).toString();
+        str = AVCVideoDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::ac3_audio_stream == DescriptorTag())
-        str = AudioStreamDescriptor(_data).toString();
+        str = AudioStreamDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::caption_service == DescriptorTag())
-        str = CaptionServiceDescriptor(_data).toString();
+        str = CaptionServiceDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::extended_channel_name == DescriptorTag())
-        str = ExtendedChannelNameDescriptor(_data).toString();
+        str = ExtendedChannelNameDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::component_name == DescriptorTag())
-        str = ComponentNameDescriptor(_data).toString();
+        str = ComponentNameDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::conditional_access == DescriptorTag())
-        str = ConditionalAccessDescriptor(_data).toString();
+        str = ConditionalAccessDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::network_name == DescriptorTag())
-        str = NetworkNameDescriptor(_data).toString();
+        str = NetworkNameDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::linkage == DescriptorTag())
-        str = LinkageDescriptor(_data).toString();
+        str = LinkageDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::adaptation_field_data == DescriptorTag())
-        str = AdaptationFieldDataDescriptor(_data).toString();
+        str = AdaptationFieldDataDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::ancillary_data == DescriptorTag())
-        str = AncillaryDataDescriptor(_data).toString();
+        str = AncillaryDataDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::cable_delivery_system == DescriptorTag())
-        str = CableDeliverySystemDescriptor(_data).toString();
+        str = CableDeliverySystemDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::satellite_delivery_system == DescriptorTag())
-        str = SatelliteDeliverySystemDescriptor(_data).toString();
+        str = SatelliteDeliverySystemDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::terrestrial_delivery_system == DescriptorTag())
-        str = TerrestrialDeliverySystemDescriptor(_data).toString();
+        str = TerrestrialDeliverySystemDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::frequency_list == DescriptorTag())
-        str = FrequencyListDescriptor(_data).toString();
+        str = FrequencyListDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::service == DescriptorTag())
-        str = ServiceDescriptor(_data).toString();
+        str = ServiceDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::stream_identifier == DescriptorTag())
-        str = StreamIdentifierDescriptor(_data).toString();
+        str = StreamIdentifierDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::default_authority == DescriptorTag())
-        str = DefaultAuthorityDescriptor(_data).toString();
+        str = DefaultAuthorityDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::bouquet_name == DescriptorTag())
-        str = BouquetNameDescriptor(_data).toString();
+        str = BouquetNameDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::country_availability == DescriptorTag())
-        str = CountryAvailabilityDescriptor(_data).toString();
+        str = CountryAvailabilityDescriptor(
+            _data, DescriptorLength()+2).toString();
     else if (DescriptorID::service_list == DescriptorTag())
-        str = ServiceListDescriptor(_data).toString();
+        str = ServiceListDescriptor(
+            _data, DescriptorLength()+2).toString();
     /// POSSIBLY UNSAFE ! -- begin
     else if (PrivateDescriptorID::dvb_uk_channel_list == DescriptorTag())
-        str = UKChannelListDescriptor(_data).toString();
+        str = UKChannelListDescriptor(_data, DescriptorLength()+2).toString();
     /// POSSIBLY UNSAFE ! -- end
     else
     {
-        str.append(QString("%1 Descriptor (0x%2)")
-                   .arg(DescriptorTagString())
-                   .arg(int(DescriptorTag()), 0, 16));
-        str.append(QString(" length(%1)").arg(int(DescriptorLength())));
+        str = QString("%1 Descriptor (0x%2) length(%3)")
+            .arg(DescriptorTagString())
+            .arg(DescriptorTag(),2,16,QChar('0'))
+            .arg(DescriptorLength());
         //for (uint i=0; i<DescriptorLength(); i++)
         //    str.append(QString(" 0x%1").arg(int(_data[i+2]), 0, 16));
     }

commit 376cd540a1a7dd6354933dd277c93f6447619773

Fixes #10100. Fix regression caused by [fc0761d3b].
Cut-n-paste error + a thinko. The cut-n-paste caused it to pass my testing,
despite the Probe() bogosity.

mythtv/libs/libmythtv/mpeg/atscdescriptors.h
@@ -77,7 +77,11 @@ class CaptionServiceDescriptor : public MPEGDescriptor
 {
   public:
     CaptionServiceDescriptor(const unsigned char *data, int len = 300) :
-        MPEGDescriptor(data, len, DescriptorID::caption_service) { }
+        MPEGDescriptor(data, len, DescriptorID::caption_service)
+    {
+        if (_data && !Parse())
+            _data = NULL;
+    }
     //       Name             bits  loc  expected value
     // descriptor_tag           8   0.0       0x86
     // descriptor_length        8   1.0
@@ -131,7 +135,11 @@ class ContentAdvisoryDescriptor : public MPEGDescriptor
 {
   public:
     ContentAdvisoryDescriptor(const unsigned char *data, int len = 300) :
-        MPEGDescriptor(data, len, DescriptorID::content_advisory) { }
+        MPEGDescriptor(data, len, DescriptorID::content_advisory)
+    {
+        if (_data && !Parse())
+            _data = NULL;
+    }
     //       Name             bits  loc  expected value
     // descriptor_tag           8   0.0       0x87
     // descriptor_length        8   1.0

mythtv/libs/libmythtv/mpeg/mpegdescriptors.cpp
@@ -51,6 +51,8 @@ desc_list_t MPEGDescriptor::ParseAndExclude(
     return tmp;
 }
 
+#include "mythlogging.h"
+
 desc_list_t MPEGDescriptor::ParseOnlyInclude(
     const unsigned char *data, uint len, int excluded_descid)
 {
@@ -59,7 +61,16 @@ desc_list_t MPEGDescriptor::ParseOnlyInclude(
     while (off < len)
     {
         if ((data+off)[0] == excluded_descid)
+        {
+            LOG(VB_GENERAL, LOG_INFO, QString("Including Descriptor %1")
+                .arg(int((data+off)[0]),0,16));
             tmp.push_back(data+off);
+        }
+        else
+        {
+            LOG(VB_GENERAL, LOG_INFO, QString("Excluding Descriptor %1")
+                .arg(int((data+off)[0]),0,16));
+        }
         MPEGDescriptor desc(data+off, len-off);
         if (!desc.IsValid())
         {
@@ -68,7 +79,6 @@ desc_list_t MPEGDescriptor::ParseOnlyInclude(
             break;
         }
         off += desc.size();
-        off += desc.DescriptorLength() + 2;
     }
     return tmp;
 }

mythtv/libs/libmythtv/mpeg/mpegdescriptors.h
@@ -201,8 +201,6 @@ class MPEGDescriptor
     {
         if ((len < 2) || (int(DescriptorLength()) + 2) > len)
             _data = NULL;
-        else if (!Parse())
-            _data = NULL;
     }
     MPEGDescriptor(const unsigned char *data,
                    int len, uint tag) : _data(data)
@@ -210,8 +208,6 @@ class MPEGDescriptor
         if ((len < 2) || (int(DescriptorLength()) + 2) > len)
             _data = NULL;
         else if (DescriptorTag() != tag)
-            _data = NULL;
-        else if (!Parse())
             _data = NULL;
     }
     MPEGDescriptor(const unsigned char *data,
@@ -222,8 +218,6 @@ class MPEGDescriptor
         else if (DescriptorTag() != tag)
             _data = NULL;
         else if (DescriptorLength() != req_desc_len)
-            _data = NULL;
-        else if (!Parse())
             _data = NULL;
     }
     virtual ~MPEGDescriptor() {}
@@ -253,8 +247,6 @@ class MPEGDescriptor
         const desc_list_t &parsed, uint desc_tag, QMap<uint,uint> &langPref);
 
   protected:
-    virtual bool Parse(void) { return true; }
-
     const unsigned char *_data;
 };
 

mythtv/libs/libmythtv/mpeg/premieredescriptors.h
@@ -24,6 +24,8 @@ class PremiereContentTransmissionDescriptor : public MPEGDescriptor
         MPEGDescriptor(
             data, len, PrivateDescriptorID::premiere_content_transmission)
     {
+        if (_data && !Parse())
+            _data = NULL;
     }
 
     // descriptor_tag           8   0.0

mythtv/libs/libmythtv/mpeg/splicedescriptors.cpp
@@ -80,7 +80,6 @@ desc_list_t SpliceDescriptor::ParseOnlyInclude(
             break;
         }
         off += desc.size();
-        off += desc.DescriptorLength() + 2;
     }
     return tmp;
 }

mythtv/libs/libmythtv/mpeg/splicedescriptors.h
@@ -48,8 +48,6 @@ class SpliceDescriptor
     {
         if ((len < 2) || (int(DescriptorLength()) + 2) > len)
             _data = NULL;
-        else if (!Parse())
-            _data = NULL;
     }
     SpliceDescriptor(const unsigned char *data,
                      int len, uint tag) : _data(data)
@@ -57,8 +55,6 @@ class SpliceDescriptor
         if ((len < 2) || (int(DescriptorLength()) + 2) > len)
             _data = NULL;
         else if (DescriptorTag() != tag)
-            _data = NULL;
-        else if (!Parse())
             _data = NULL;
     }
     virtual ~SpliceDescriptor(void) {}
@@ -163,7 +159,11 @@ class SegmentationDescriptor : SpliceDescriptor
 {
   public:
     SegmentationDescriptor(const unsigned char *data, int len = 300) :
-        SpliceDescriptor(data, len, SpliceDescriptorID::segmentation) { }
+        SpliceDescriptor(data, len, SpliceDescriptorID::segmentation)
+    {
+        if (_data && !Parse())
+            _data = NULL;
+    }
 
     //       Name             bits  loc  expected value
     // splice_descriptor_tag    8   0.0  0x01