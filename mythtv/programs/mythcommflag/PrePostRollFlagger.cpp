39c61832fb8e9e17fecf478426e67a223f6bd008
T.M.Speight <T.M.Speight.90@cantab.net>
2011-10-25 11:35:21 -0400

    Correct copy/paste error in postroll commercial detection
    Corrects an incorrect variable use that was missed when copying a
    similar block of code added in 9f5ee51db82.
    
    Fixes #10120
    Refs #6889
    
    Signed-off-by: Raymond Wagner <rwagner@mythtv.org>

--- mythtv/programs/mythcommflag/PrePostRollFlagger.cpp
+++ mythtv/programs/mythcommflag/PrePostRollFlagger.cpp
@@ -406,10 +406,11 @@ void PrePostRollFlagger::GetCommercialBreakList(frm_dir_map_t &marks)
             end = closestAfterPre;
         else
             end = closestBeforePre;
-    }else if(closestBeforePre)
+    }
+    else if(closestBeforePre)
         end = closestBeforePre;
     else if(closestAfterPre)
-        end  = closestAfterPre;
+        end = closestAfterPre;
     else
         end = preRoll;
 
@@ -431,7 +432,7 @@ void PrePostRollFlagger::GetCommercialBreakList(frm_dir_map_t &marks)
     else if(closestBeforePost)
         start = closestBeforePost;
     else if(closestAfterPost)
-        start  = closestAfterPre;
+        start = closestAfterPost;
     else if(postRoll)
         start = myTotalFrames - postRoll;
 
