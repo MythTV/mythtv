baf11034ec7e5555e7d27f6c5c4cdc6b7848279b
Gavin Hurlbut <ghurlbut@mythtv.org>
2011-10-30 16:49:04 -0700

    Move ReferenceCount debugging to new verbose level
    Removed the conditional compile and replaced with the new VB_REFCOUNT to allow
    the selective enabling and disabling of the log messages at runtime.

--- mythtv/libs/libmythbase/referencecounter.cpp
+++ mythtv/libs/libmythbase/referencecounter.cpp
@@ -5,8 +5,6 @@
 #include "referencecounter.h"
 #include "mythlogging.h"
 
-#define DEBUG_REFERENCE_COUNTING 0
-
 ReferenceCounter::ReferenceCounter(void) :
     m_refCount(1)
 {
@@ -16,24 +14,20 @@ void ReferenceCounter::UpRef(void)
 {
     QMutexLocker mlock(&m_refLock);
     m_refCount++;
-#if DEBUG_REFERENCE_COUNTING
-    LOG(VB_GENERAL, LOG_DEBUG, QString("%1(0x%2)::UpRef() -> %3")
+    LOG(VB_REFCOUNT, LOG_DEBUG, QString("%1(0x%2)::UpRef() -> %3")
                     .arg(metaObject()->className())
                     .arg((uint64_t)this,0,16)
                     .arg(QString::number(m_refCount)));
-#endif
 }
 
 bool ReferenceCounter::DownRef(void)
 {
     m_refLock.lock();
     m_refCount--;
-#if DEBUG_REFERENCE_COUNTING
-    LOG(VB_GENERAL, LOG_DEBUG, QString("%1(0x%2)::DownRef() -> %3")
+    LOG(VB_REFCOUNT, LOG_DEBUG, QString("%1(0x%2)::DownRef() -> %3")
                     .arg(metaObject()->className())
                     .arg((uint64_t)this,0,16)
                     .arg(QString::number(m_refCount)));
-#endif
 
     if (m_refCount == 0)
     {
