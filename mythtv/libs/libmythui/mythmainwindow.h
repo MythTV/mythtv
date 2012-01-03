b3bb76db6626b35981d2b499fdbc9483b4f1c8b2
Stuart Morgan <smorgan@mythtv.org>
2012-01-03 20:10:42 +0000

    Revert "Idle timer"
    This reverts commit 8ad07f7f7a280ee82e9147807a8aa6de114ffd33.
    
    I forgot that this was in my tree, it wasn't meant to be committed.

--- mythtv/libs/libmythui/mythmainwindow.h
+++ mythtv/libs/libmythui/mythmainwindow.h
@@ -125,14 +125,10 @@ class MUI_PUBLIC MythMainWindow : public QWidget
     uint PopDrawDisabled(void);
     void SetEffectsEnabled(bool enable);
     void draw(void);
-    
-    void ResetIdleTimer(void);
-    void PauseIdleTimer(bool pause);
 
   public slots:
     void mouseTimeout();
     void HideMouseTimeout();
-    void IdleTimeout();
 
   protected slots:
     void animate();
@@ -164,9 +160,6 @@ class MUI_PUBLIC MythMainWindow : public QWidget
     void LockInputDevices(bool locked);
 
     void ShowMouseCursor(bool show);
-    
-    void EnterStandby();
-    void ExitStandby();
 
     MythMainWindowPrivate *d;
 };
