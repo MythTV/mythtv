MythTV/mythtv

commit 86409e8e0ae5ddbbda0b2e4c3fd86a2938f2adaa

Add album track count to Metadata object

mythplugins/mythmusic/mythmusic/metadata.cpp
@@ -69,6 +69,7 @@ Metadata& Metadata::operator=(const Metadata &rhs)
     m_genre = rhs.m_genre;
     m_year = rhs.m_year;
     m_tracknum = rhs.m_tracknum;
+    m_trackCount = rhs.m_trackCount;
     m_length = rhs.m_length;
     m_rating = rhs.m_rating;
     m_lastplay = rhs.m_lastplay;
@@ -190,7 +191,8 @@ bool Metadata::isInDatabase()
     "music_albums.album_name, music_songs.name, music_genres.genre, "
     "music_songs.year, music_songs.track, music_songs.length, "
     "music_songs.song_id, music_songs.rating, music_songs.numplays, "
-    "music_songs.lastplay, music_albums.compilation, music_songs.format "
+    "music_songs.lastplay, music_albums.compilation, music_songs.format, "
+    "music_songs.track_count "
     "FROM music_songs "
     "LEFT JOIN music_directories "
     "ON music_songs.directory_id=music_directories.directory_id "
@@ -222,6 +224,7 @@ bool Metadata::isInDatabase()
         m_lastplay = query.value(11).toDateTime();
         m_compilation = (query.value(12).toInt() > 0);
         m_format = query.value(13).toString();
+        m_trackCount = query.value(14).toInt();
 
         retval = true;
     }
@@ -476,6 +479,7 @@ void Metadata::dumpToDatabase()
     query.bindValue(":ALBUMID", m_albumid);
     query.bindValue(":COMPILATION", m_compilation);
     query.bindValue(":YEAR", m_year);
+    query.bindValue(":TRACKCOUNT", m_trackCount);
 
     if (!query.exec() || !query.isActive())
     {
@@ -643,6 +647,8 @@ void Metadata::setField(const QString &field, const QString &data)
         m_year = data.toInt();
     else if (field == "tracknum")
         m_tracknum = data.toInt();
+    else if (field == "trackcount")
+        m_trackCount = data.toInt();
     else if (field == "length")
         m_length = data.toInt();
     else if (field == "compilation")
@@ -682,6 +688,7 @@ void Metadata::toMap(MetadataMap &metadataMap, const QString &prefix)
     metadataMap[prefix + "title"] = m_title;
     metadataMap[prefix + "formattitle"] = FormatTitle();
     metadataMap[prefix + "tracknum"] = (m_tracknum > 0 ? QString("%1").arg(m_tracknum) : "");
+    metadataMap[prefix + "albumtracks"] = (m_trackCount > 0 ? QString("%1").arg(m_trackCount) : "");
     metadataMap[prefix + "genre"] = m_genre;
     metadataMap[prefix + "year"] = (m_year > 0 ? QString("%1").arg(m_year) : "");
 

mythplugins/mythmusic/mythmusic/metadata.h
@@ -95,6 +95,7 @@ class Metadata
                    m_format(lformat),
                    m_year(lyear),
                    m_tracknum(ltracknum),
+                   m_trackCount(0),
                    m_length(llength),
                    m_rating(lrating),
                    m_directoryid(-1),
@@ -171,6 +172,9 @@ class Metadata
 
     int Track() const { return m_tracknum; }
     void setTrack(int ltrack) { m_tracknum = ltrack; }
+
+    int GetAlbumTracks() const { return m_trackCount; }
+    void setAlbumTracks(int labumtracks) { m_trackCount = labumtracks; }
 
     int Length() const { return m_length; }
     void setLength(int llength) { m_length = llength; }
@@ -255,6 +259,7 @@ class Metadata
     QString m_format;
     int m_year;
     int m_tracknum;
+    int m_trackCount;
     int m_length;
     int m_rating;
     int m_directoryid;
@@ -341,7 +346,7 @@ class AllMusic
     bool        doneLoading() {return m_done_loading;}
     bool        cleanOutThreads();
 
-    MetadataPtrList *getAllMetadata(void) { return &m_all_music; } 
+    MetadataPtrList *getAllMetadata(void) { return &m_all_music; }
 
     bool isValidID(int an_id);
 

commit bfe8fbe55ac998bd9243adc4f44b9326841751d6

Actually fix the queries broken in 86409e8e, sorry need more coffee

mythplugins/mythmusic/mythmusic/metadata.cpp
@@ -414,13 +414,13 @@ void Metadata::dumpToDatabase()
                    " artist_id, album_id,  name,         genre_id,"
                    " year,      track,     length,       filename,"
                    " rating,    format,    date_entered, date_modified,"
-                   " numplays) "
+                   " numplays,  track_count) "
                    "VALUES ( "
                    " :DIRECTORY, "
                    " :ARTIST,   :ALBUM,    :TITLE,       :GENRE,"
                    " :YEAR,     :TRACKNUM, :LENGTH,      :FILENAME,"
                    " :RATING,   :FORMAT,   :DATE_ADD,    :DATE_MOD,"
-                   " :PLAYCOUNT );";
+                   " :PLAYCOUNT,:TRACKCOUNT );";
     }
     else
     {
@@ -438,6 +438,7 @@ void Metadata::dumpToDatabase()
                    ", format = :FORMAT"
                    ", date_modified = :DATE_MOD "
                    ", numplays = :PLAYCOUNT "
+                   ", track_count = :TRACKCOUNT "
                    "WHERE song_id= :ID ;";
     }
 