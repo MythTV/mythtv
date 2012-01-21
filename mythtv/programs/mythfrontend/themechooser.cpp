MythTV/mythtv

commit 8bf5157fdb844dc72e65bfc419e343b6764b58ba

This patch adds IPv6 support to mythbackend and mythfrontend. I've been running this patch since XMas with no problems at all.
To use IPv6 you just have to run mythtv-setup and configure your backend IPv6 addresses.
This assumes  you have IPv6 configured internally on your lan of course.

The changes are overall fairly small.  One of the biggest things needed
was to encapsulate IPv6 addresses in the URL with [] (otherwise parsing
out a port could be ugly:) )

So for example instead of

   myth://group@fd14:6bf7:21fb::24/file.mpg

 It would be
   myth://group@[fd14:6bf7:21fb::24]/file.mpg

   or if specifying the port

   myth://group@[fd14:6bf7:21fb::24]:6543/file.mpg

To remove all of the duplicated code where we build
the URL I've added a GenMythURL() function to gContext and it will generate
the path and do the right thing when it comes to[]'s , ports  and storage
groups in the URL.

So for example gContext->GenMythURL("fd14:6bf7:21fb::24","6543","file.mpg","Videos")
Happily generates myth://Videos@[fd14:6bf7:21fb::24]:6543/file.mpg

Only the PHP bindings have been updated (for use with mythweb) so far and I have no had
a chance to test this under Windows or OSX as I do not have access to either of those.

This patch does however change MythSocket to no longer assume that a socket will be IPv4.
It will attempt to detect which it should use however based on the IP it is trying to connect to

Bug reports are welcome.

mythtv/programs/mythfrontend/themechooser.cpp
@@ -878,8 +878,10 @@ ThemeUpdateChecker::ThemeUpdateChecker() :
         m_mythVersion.replace(QRegExp("\\.[0-9]{8,}.*"), "");
     }
 
-    m_infoPackage = QString("myth://Temp@%1/remotethemes/themes.zip")
-                            .arg(gCoreContext->GetSetting("MasterServerIP"));
+    m_infoPackage = gCoreContext->GenMythURL(gCoreContext->GetSetting("MasterServerIP"),
+                                             0,
+                                             "remotethemes/themes.zip",
+                                             "Temp");
 
     gCoreContext->SaveSetting("ThemeUpdateStatus", QString());
                                  
@@ -908,9 +910,11 @@ void ThemeUpdateChecker::checkForUpdate(void)
     if (RemoteFile::Exists(m_infoPackage))
     {
         QString remoteThemeDir =
-            QString("myth://Temp@%1/remotethemes/%2/%3")
-                    .arg(gCoreContext->GetSetting("MasterServerIP"))
-                    .arg(m_mythVersion).arg(GetMythUI()->GetThemeName());
+            gCoreContext->GenMythURL(gCoreContext->GetSetting("MasterServerIP"),
+                                     0,
+                                     QString("%1/%2").arg(m_mythVersion).arg(GetMythUI()->GetThemeName()),
+                                     "Temp");
+
         QString infoXML = remoteThemeDir;
         infoXML.append("/themeinfo.xml");
 

mythtv/bindings/php/MythBackend.php
@@ -50,6 +50,12 @@ class MythBackend {
     function __construct($host, $port = null) {
         $this->host         = $host;
         $this->ip           = _or(setting('BackendServerIP', $this->host), $host);
+
+        // If the IP contains a ':' It's likely an IPv6 address so enclose it in '[]'
+        if (strpos($this->ip,":") > 0) {
+            $this->ip = "[" + $this->ip + "]";
+        }
+
         $this->port         = _or($port, _or(setting('BackendServerPort', $this->host), 6543));
         $this->port_http    = _or(setting('BackendStatusPort', $this->host), _or(setting('BackendStatusPort'), 6544));
     }

mythtv/libs/libmyth/programinfo.cpp
@@ -2032,19 +2032,19 @@ QString ProgramInfo::GetPlaybackURL(
         (gCoreContext->GetNumSetting("MasterBackendOverride", 0)) &&
         (RemoteCheckFile(this, false)))
     {
-        tmpURL = QString("myth://") +
-        gCoreContext->GetSetting("MasterServerIP") + ':' +
-        gCoreContext->GetSetting("MasterServerPort") + '/' + basename;
+        tmpURL = gCoreContext->GenMythURL(gCoreContext->GetSetting("MasterServerIP"),
+                                          gCoreContext->GetSetting("MasterServerPort").toInt(),
+                                          basename);
+
         VERBOSE(VB_FILE, LOC +
                 QString("GetPlaybackURL: Found @ '%1'").arg(tmpURL));
         return tmpURL;
         }
 
     // Fallback to streaming from the backend the recording was created on
-    tmpURL = QString("myth://") +
-        gCoreContext->GetSettingOnHost("BackendServerIP", hostname) + ':' +
-        gCoreContext->GetSettingOnHost("BackendServerPort", hostname) + '/' +
-        basename;
+    tmpURL = gCoreContext->GenMythURL(gCoreContext->GetSettingOnHost("BackendServerIP", hostname),
+                                      gCoreContext->GetSettingOnHost("BackendServerPort", hostname).toInt(),
+                                      basename);
 
     VERBOSE(VB_FILE, LOC + QString("GetPlaybackURL: Using default of: '%1'")
             .arg(tmpURL));

mythtv/libs/libmythbase/msocketdevice.cpp
@@ -173,21 +173,23 @@ MSocketDevice::MSocketDevice( int socket, Type type )
     The \a type argument must be either MSocketDevice::Stream for a
     reliable, connection-oriented TCP socket, or \c
     MSocketDevice::Datagram for an unreliable UDP socket.
-
-    The socket is created as an IPv4 socket.
+    The socket protocol type is defaulting to unknown leaving it to 
+    connect() to determine if an IPv6 or IPv4 type is required.
 
     \sa blocking() protocol()
 */
 MSocketDevice::MSocketDevice( Type type )
     : fd( -1 ), t( type ), p( 0 ), pp( 0 ), e( NoError ),
-      d(new MSocketDevicePrivate(IPv4))
+      d(new MSocketDevicePrivate(Unknown))
+
+    //  d(new MSocketDevicePrivate(IPv4))
 {
 #if defined(MSOCKETDEVICE_DEBUG)
     qDebug( "MSocketDevice: Created MSocketDevice object %p, type %d",
 	    this, type );
 #endif
     init();
-    setSocket( createNewSocket(), type );
+    //setSocket( createNewSocket(), type );
 }
 
 /*!
@@ -274,6 +276,11 @@ MSocketDevice::Protocol MSocketDevice::protocol() const
     if ( d->protocol == Unknown )
 	d->protocol = getProtocol();
     return d->protocol;
+}
+
+void MSocketDevice::setProtocol( Protocol protocol )
+{
+    d->protocol = protocol;
 }
 
 /*!

mythtv/libs/libmythbase/msocketdevice.h
@@ -66,6 +66,8 @@ public:
     Type	 type() const;
     Protocol	 protocol() const;
 
+    void setProtocol( Protocol protocol );
+
     int		 socket() const;
     virtual void setSocket( int socket, Type type );
 
@@ -133,6 +135,7 @@ public:
     Error	 error() const;
 
     inline bool isSequential() const { return true; }
+    int      createNewSocket();
 
 protected:
     void setError( Error err );
@@ -160,7 +163,6 @@ private:
 #endif
 
     static void  init();
-    int		 createNewSocket();
     Protocol	 getProtocol() const;
 
 private:	// Disabled copy constructor and operator=

mythtv/libs/libmythbase/msocketdevice_unix.cpp
@@ -391,7 +391,26 @@ void MSocketDevice::setOption( Option opt, int v )
 bool MSocketDevice::connect( const QHostAddress &addr, quint16 port )
 {
     if ( !isValid() )
-	return false;
+    {
+#if !defined(QT_NO_IPV6)
+        if ( addr.protocol() == QAbstractSocket::IPv6Protocol ) {
+            setProtocol(IPv6);
+            VERBOSE(VB_SOCKET, "MSocketDevice::connect: setting Protocol to IPv6");
+        }
+        else   
+#endif
+        if ( addr.protocol() == QAbstractSocket::IPv4Protocol ) {
+            setProtocol(IPv4);
+            VERBOSE(VB_SOCKET, "MSocketDevice::connect: setting Protocol to IPv4");
+        }
+
+        VERBOSE(VB_SOCKET, "MSocketDevice::connect: attempting to create new socket");
+        setSocket( createNewSocket(), t);
+    
+       // If still not valid, give up.
+       if ( !isValid() )
+           return false;
+    }
 
     pa = addr;
     pp = port;

mythtv/libs/libmythbase/msocketdevice_win.cpp
@@ -405,7 +405,26 @@ void MSocketDevice::setOption( Option opt, int v )
 bool MSocketDevice::connect( const QHostAddress &addr, quint16 port )
 {
     if ( !isValid() )
-	return false;
+    {
+#if !defined(QT_NO_IPV6)
+        if ( addr.protocol() == QAbstractSocket::IPv6Protocol ) {
+            setProtocol(IPv6);
+            VERBOSE(VB_SOCKET, "MSocketDevice::connect: setting Protocol to IPv6");
+        }
+        else
+#endif  
+        if ( addr.protocol() == QAbstractSocket::IPv4Protocol ) {
+            setProtocol(IPv4);
+            VERBOSE(VB_SOCKET, "MSocketDevice::connect: setting Protocol to IPv4");
+        }
+
+        VERBOSE(VB_SOCKET, "MSocketDevice::connect: attempting to create new socket");
+        setSocket( createNewSocket(), t);
+
+       // If still not valid, give up.
+       if ( !isValid() )
+       return false;
+    }
 
     pa = addr;
     pp = port;

mythtv/libs/libmythbase/mythcorecontext.cpp
@@ -555,19 +555,59 @@ bool MythCoreContext::IsFrontendOnly(void)
     return !backendOnLocalhost;
 }
 
+QString MythCoreContext::GenMythURL(QString host, QString port, QString path, QString storageGroup)
+{
+    return GenMythURL(host,port.toInt(),path,storageGroup);
+}
+
+QString MythCoreContext::GenMythURL(QString host, int port, QString path, QString storageGroup) 
+{
+    QString ret;
+
+    QString m_storageGroup;
+    QString m_host;
+    QString m_port;
+
+    QHostAddress addr(host);
+
+    if (!storageGroup.isEmpty()) 
+        m_storageGroup = storageGroup + "@";
+
+    m_host = host;
+
+#if !defined(QT_NO_IPV6)
+    // Basically if it appears to be an IPv6 IP surround the IP with [] otherwise don't bother
+    if (( addr.protocol() == QAbstractSocket::IPv6Protocol ) || (host.contains(":"))) 
+        m_host = "[" + host + "]";
+#endif
+
+    if (port > 0) 
+        m_port = QString(":%1").arg(port);
+    else
+        m_port = "";
+
+    QString seperator = "/";
+    if (path.startsWith("/"))
+        seperator = "";
+
+    ret = QString("myth://%1%2%3%4%5").arg(m_storageGroup).arg(m_host).arg(m_port).arg(seperator).arg(path);
+
+    //VERBOSE(VB_GENERAL, LOC + QString("GenMythURL returning %1").arg(ret));
+
+    return ret;
+}
+
+
 QString MythCoreContext::GetMasterHostPrefix(QString storageGroup)
 {
     QString ret;
 
     if (IsMasterHost())
     {
-        if (storageGroup.isEmpty())
-            return QString("myth://%1:%2/").arg(GetSetting("MasterServerIP"))
-                           .arg(GetNumSetting("MasterServerPort", 6543));
-        else
-            return QString("myth://%1@%2:%3/").arg(storageGroup)
-                           .arg(GetSetting("MasterServerIP"))
-                           .arg(GetNumSetting("MasterServerPort", 6543));
+        return GenMythURL(GetSetting("MasterServerIP"),
+                          GetNumSetting("MasterServerPort", 6543),
+                          "",
+                          storageGroup);
     }
 
     QMutexLocker locker(&d->m_sockLock);
@@ -579,15 +619,11 @@ QString MythCoreContext::GetMasterHostPrefix(QString storageGroup)
 
     if (d->m_serverSock)
     {
-        if (storageGroup.isEmpty())
-            ret = QString("myth://%1:%2/")
-                         .arg(d->m_serverSock->peerAddress().toString())
-                         .arg(d->m_serverSock->peerPort());
-        else
-            ret = QString("myth://%1@%2:%3/")
-                         .arg(storageGroup)
-                         .arg(d->m_serverSock->peerAddress().toString())
-                         .arg(d->m_serverSock->peerPort());
+
+         ret = GenMythURL(d->m_serverSock->peerAddress().toString(),
+                          d->m_serverSock->peerPort(),
+                          "",
+                          storageGroup);
     }
 
     return ret;

commit 790f73c6bd5697f6de33532c76ed5c4e7840df1e

Fix a regression in the ThemeChooser update notification code.
The 'new version of your theme available for download' notification
was broken by commit 8bf5157f due to a missing subdirectory name.
This fixes that regression.

mythtv/programs/mythfrontend/themechooser.cpp
@@ -916,7 +916,9 @@ void ThemeUpdateChecker::checkForUpdate(void)
         QString remoteThemeDir =
             gCoreContext->GenMythURL(gCoreContext->GetSetting("MasterServerIP"),
                                      0,
-                                     QString("%1/%2").arg(m_mythVersion).arg(GetMythUI()->GetThemeName()),
+                                     QString("remotethemes/%1/%2")
+                                             .arg(m_mythVersion)
+                                             .arg(GetMythUI()->GetThemeName()),
                                      "Temp");
 
         QString infoXML = remoteThemeDir;