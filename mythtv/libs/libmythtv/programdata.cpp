28508cb0c686236e8749367a405582ab9abbbc47
Daniel Kristjansson <danielk@cuymedia.net>
2012-02-01 12:55:47 -0500

    Eliminate redundant code. cppcheck.

--- mythtv/libs/libmythtv/programdata.cpp
+++ mythtv/libs/libmythtv/programdata.cpp
@@ -1022,9 +1022,6 @@ void ProgramData::FixProgramList(QList<ProgInfo*> &fixlist)
             else if (!(*cur)->description.isEmpty() &&
                      (*it)->description.isEmpty())
                 tokeep = cur, todelete = it;
-            else if (!(*it)->description.isEmpty() &&
-                     (*cur)->description.isEmpty())
-                tokeep = it, todelete = cur;
             else
                 tokeep = it, todelete = cur;
 
