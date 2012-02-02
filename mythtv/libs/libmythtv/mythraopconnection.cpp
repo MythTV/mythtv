c465cb5827bb36e2bdc67692e962dd5913482f2b
Mark Kendall <mkendall@mythtv.org>
2012-02-02 08:42:06 +0000

    RAOP: Remove the private key.
    Untested.

--- mythtv/libs/libmythtv/mythraopconnection.cpp
+++ mythtv/libs/libmythtv/mythraopconnection.cpp
@@ -8,6 +8,7 @@
 
 #include "mythlogging.h"
 #include "mythcorecontext.h"
+#include "mythdirs.h"
 
 #include <netinet/in.h> // for ntohs
 #include "audiooutput.h"
@@ -698,31 +699,6 @@ void MythRAOPConnection::FinishResponse(QTextStream *stream, QTcpSocket *socket,
          .arg(socket->flush()));
 }
 
-static const QByteArray kPrivateKey =
-"-----BEGIN RSA PRIVATE KEY-----\n"
-"MIIEpQIBAAKCAQEA59dE8qLieItsH1WgjrcFRKj6eUWqi+bGLOX1HL3U3GhC/j0Qg90u3sG/1CUt\n"
-"wC5vOYvfDmFI6oSFXi5ELabWJmT2dKHzBJKa3k9ok+8t9ucRqMd6DZHJ2YCCLlDRKSKv6kDqnw4U\n"
-"wPdpOMXziC/AMj3Z/lUVX1G7WSHCAWKf1zNS1eLvqr+boEjXuBOitnZ/bDzPHrTOZz0Dew0uowxf\n"
-"/+sG+NCK3eQJVxqcaJ/vEHKIVd2M+5qL71yJQ+87X6oV3eaYvt3zWZYD6z5vYTcrtij2VZ9Zmni/\n"
-"UAaHqn9JdsBWLUEpVviYnhimNVvYFZeCXg/IdTQ+x4IRdiXNv5hEewIDAQABAoIBAQDl8Axy9XfW\n"
-"BLmkzkEiqoSwF0PsmVrPzH9KsnwLGH+QZlvjWd8SWYGN7u1507HvhF5N3drJoVU3O14nDY4TFQAa\n"
-"LlJ9VM35AApXaLyY1ERrN7u9ALKd2LUwYhM7Km539O4yUFYikE2nIPscEsA5ltpxOgUGCY7b7ez5\n"
-"NtD6nL1ZKauw7aNXmVAvmJTcuPxWmoktF3gDJKK2wxZuNGcJE0uFQEG4Z3BrWP7yoNuSK3dii2jm\n"
-"lpPHr0O/KnPQtzI3eguhe0TwUem/eYSdyzMyVx/YpwkzwtYL3sR5k0o9rKQLtvLzfAqdBxBurciz\n"
-"aaA/L0HIgAmOit1GJA2saMxTVPNhAoGBAPfgv1oeZxgxmotiCcMXFEQEWflzhWYTsXrhUIuz5jFu\n"
-"a39GLS99ZEErhLdrwj8rDDViRVJ5skOp9zFvlYAHs0xh92ji1E7V/ysnKBfsMrPkk5KSKPrnjndM\n"
-"oPdevWnVkgJ5jxFuNgxkOLMuG9i53B4yMvDTCRiIPMQ++N2iLDaRAoGBAO9v//mU8eVkQaoANf0Z\n"
-"oMjW8CN4xwWA2cSEIHkd9AfFkftuv8oyLDCG3ZAf0vrhrrtkrfa7ef+AUb69DNggq4mHQAYBp7L+\n"
-"k5DKzJrKuO0r+R0YbY9pZD1+/g9dVt91d6LQNepUE/yY2PP5CNoFmjedpLHMOPFdVgqDzDFxU8hL\n"
-"AoGBANDrr7xAJbqBjHVwIzQ4To9pb4BNeqDndk5Qe7fT3+/H1njGaC0/rXE0Qb7q5ySgnsCb3DvA\n"
-"cJyRM9SJ7OKlGt0FMSdJD5KG0XPIpAVNwgpXXH5MDJg09KHeh0kXo+QA6viFBi21y340NonnEfdf\n"
-"54PX4ZGS/Xac1UK+pLkBB+zRAoGAf0AY3H3qKS2lMEI4bzEFoHeK3G895pDaK3TFBVmD7fV0Zhov\n"
-"17fegFPMwOII8MisYm9ZfT2Z0s5Ro3s5rkt+nvLAdfC/PYPKzTLalpGSwomSNYJcB9HNMlmhkGzc\n"
-"1JnLYT4iyUyx6pcZBmCd8bD0iwY/FzcgNDaUmbX9+XDvRA0CgYEAkE7pIPlE71qvfJQgoA9em0gI\n"
-"LAuE4Pu13aKiJnfft7hIjbK+5kyb3TysZvoyDnb3HOKvInK7vXbKuU4ISgxB2bB3HcYzQMGsz1qJ\n"
-"2gG0N5hvJpzwwhbhXqFKA4zaaSrw622wDniAK5MlIE0tIAKKP4yxNGjoD2QYjhBGuhvkWKY=\n"
-"-----END RSA PRIVATE KEY-----";
-
 RSA* MythRAOPConnection::LoadKey(void)
 {
     static QMutex lock;
@@ -731,9 +707,19 @@ RSA* MythRAOPConnection::LoadKey(void)
     if (g_rsa)
         return g_rsa;
 
-    BIO *bio = BIO_new_mem_buf((void*)kPrivateKey.data(), -1);
-    g_rsa = PEM_read_bio_RSAPrivateKey(bio, NULL, NULL, NULL);
-    BIO_free(bio);
+    QString sName( "/RAOPKey.rsa" );
+    FILE * file = fopen(GetConfDir().toUtf8() + sName.toUtf8(), "rb");
+
+    if ( !file )
+    {
+        LOG(VB_GENERAL, LOG_ERR, LOC + "Failed to read key from " + GetConfDir().toUtf8() + sName.toUtf8());
+        g_rsa = NULL;
+        return NULL;
+    }
+
+    g_rsa = PEM_read_RSAPrivateKey(file, NULL, NULL, NULL);
+    fclose(file);
+
     if (g_rsa)
     {
         LOG(VB_GENERAL, LOG_DEBUG, LOC +
