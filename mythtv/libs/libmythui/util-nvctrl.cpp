3c929e8e36194f6cb3aef3b03ecbc7abc9439f9b
Stuart Auchterlonie <stuarta@squashedfrog.net>
2012-02-03 18:10:25 +0000

    Remove a few more warnings. part 2

--- mythtv/libs/libmythui/util-nvctrl.cpp
+++ mythtv/libs/libmythui/util-nvctrl.cpp
@@ -20,12 +20,14 @@
 #include "DisplayResX.h"
 #endif
 
+#ifdef USING_XRANDR
 static unsigned int display_device_mask(char *str);
 static void parse_mode_string(char *modeString, char **modeName, int *mask);
 static char *find_modeline(char *modeString, char *pModeLines,
                            int ModeLineLen);
 static int extract_id_string(char *str);
 static int modeline_is_interlaced(char *modeLine);
+#endif
 
 #define LOC QString("NVCtrl: ")
 
