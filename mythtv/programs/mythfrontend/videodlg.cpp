af8b290a59eefd7652d1c80ac069ca09d3d7febf
Stuart Morgan <smorgan@mythtv.org>
2012-02-08 14:49:39 +0000

    Fix inverted logic in previous commit.

--- mythtv/programs/mythfrontend/videodlg.cpp
+++ mythtv/programs/mythfrontend/videodlg.cpp
@@ -1043,7 +1043,7 @@ void VideoDialog::Load()
 {
     reloadData();
     // We only want to prompt once, on startup, hence we this is done in Load()
-    if (m_d->m_rootNode->getInt() != kNoFilesFound)
+    if (m_d->m_rootNode->getInt() == kNoFilesFound)
         PromptToScan();
 }
 
@@ -1060,7 +1060,7 @@ void VideoDialog::refreshData()
             ParentalLevelToState(m_d->m_parentalLevel.GetLevel()));
 
     if (m_novideoText)
-        m_novideoText->SetVisible(m_d->m_rootNode->getInt() != kNoFilesFound);
+        m_novideoText->SetVisible(m_d->m_rootNode->getInt() == kNoFilesFound);
 }
 
 void VideoDialog::reloadAllData(bool dbChanged)
