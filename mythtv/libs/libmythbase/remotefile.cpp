MythTV/mythtv

commit adca89e6fa276b778b61eeafaefa405b2c10a1ec

Range check string list from network before we access the elements in the list (crash fix).

mythtv/libs/libmythbase/remotefile.cpp
@@ -281,7 +281,6 @@ bool RemoteFile::Exists(const QString &url)
 
 bool RemoteFile::Exists(const QString &url, struct stat *fileinfo)
 {
-    bool result      = false;
     QUrl qurl(url);
     QString filename = qurl.path();
     QString sgroup   = qurl.userName();
@@ -300,32 +299,31 @@ bool RemoteFile::Exists(const QString &url, struct stat *fileinfo)
     if (!sgroup.isEmpty())
         strlist << sgroup;
 
-    gCoreContext->SendReceiveStringList(strlist);
-
-    if (strlist[0] == "1")
+    bool result = false;
+    if ((strlist.size() >= 1) && strlist[0] == "1")
     {
-        result = true;
-        if (fileinfo)
+        if ((strlist.size() >= 15) && fileinfo)
         {
-            int pos = 2;
-            fileinfo->st_dev       = strlist[pos++].toLongLong();
-            fileinfo->st_ino       = strlist[pos++].toLongLong();
-            fileinfo->st_mode      = strlist[pos++].toLongLong();
-            fileinfo->st_nlink     = strlist[pos++].toLongLong();
-            fileinfo->st_uid       = strlist[pos++].toLongLong();
-            fileinfo->st_gid       = strlist[pos++].toLongLong();
-            fileinfo->st_rdev      = strlist[pos++].toLongLong();
-            fileinfo->st_size      = strlist[pos++].toLongLong();
-#ifdef USING_MINGW
-            pos++; // st_blksize
-            pos++; // st_blocks
-#else
-            fileinfo->st_blksize   = strlist[pos++].toLongLong();
-            fileinfo->st_blocks    = strlist[pos++].toLongLong();
+            fileinfo->st_dev       = strlist[2].toLongLong();
+            fileinfo->st_ino       = strlist[3].toLongLong();
+            fileinfo->st_mode      = strlist[4].toLongLong();
+            fileinfo->st_nlink     = strlist[5].toLongLong();
+            fileinfo->st_uid       = strlist[6].toLongLong();
+            fileinfo->st_gid       = strlist[7].toLongLong();
+            fileinfo->st_rdev      = strlist[8].toLongLong();
+            fileinfo->st_size      = strlist[9].toLongLong();
+#ifndef USING_MINGW
+            fileinfo->st_blksize   = strlist[10].toLongLong();
+            fileinfo->st_blocks    = strlist[11].toLongLong();
 #endif
-            fileinfo->st_atime     = strlist[pos++].toLongLong();
-            fileinfo->st_mtime     = strlist[pos++].toLongLong();
-            fileinfo->st_ctime     = strlist[pos++].toLongLong();
+            fileinfo->st_atime     = strlist[12].toLongLong();
+            fileinfo->st_mtime     = strlist[13].toLongLong();
+            fileinfo->st_ctime     = strlist[14].toLongLong();
+            result = true;
+        }
+        else if (!fileinfo)
+        {
+            result = true;
         }
     }
 

commit 4b8bf380c60411d287e25fa49ba1f8dca0fbcef3

Fix regression caused by [adca89e6f]; breakage of remote storage group playback.

mythtv/libs/libmythbase/remotefile.cpp
@@ -299,6 +299,8 @@ bool RemoteFile::Exists(const QString &url, struct stat *fileinfo)
     if (!sgroup.isEmpty())
         strlist << sgroup;
 
+    gCoreContext->SendReceiveStringList(strlist);
+
     bool result = false;
     if ((strlist.size() >= 1) && strlist[0] == "1")
     {