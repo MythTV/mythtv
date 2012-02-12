6f6e34d204574fa32797b60ca90130e912a39bfd
Raymond Wagner <rwagner@mythtv.org>
2012-02-12 02:17:25 -0500

    Add ServerPool class.
    This adds a new ServerPool class to libmythbase, with the purpose of
    emulating the interfaces of both QTcpServer and QUdpSocket as is
    currently required by the various interfaces MythTV supports. This
    class replaces the previous use of QHostAddress::Any and
    QHostAddress::AnyIPv6 to perform blanket binds, with a pool of
    sockets each bound to individual addresses.
    
    The following signals are available for use:
      newConnection(QTcpSocket *)
      newDatagram(QByteArray, QHostAddress, quint16 port)
    
    Additionally, newTcpConnection(int) can be subclassed and overridden to
    produce alternate socket types.

--- mythtv/libs/libmythbase/libmythbase.pro
+++ mythtv/libs/libmythbase/libmythbase.pro
@@ -23,7 +23,7 @@ HEADERS += unzip.h unzip_p.h zipentry_p.h iso639.h iso3166.h mythmedia.h
 HEADERS += util.h mythhdd.h mythcdrom.h autodeletedeque.h dbutil.h
 HEADERS += mythhttppool.h mythhttphandler.h mythdeque.h mythlogging.h
 HEADERS += mythbaseutil.h referencecounter.h version.h mythcommandlineparser.h
-HEADERS += mythscheduler.h filesysteminfo.h hardwareprofile.h
+HEADERS += mythscheduler.h filesysteminfo.h hardwareprofile.h serverpool.h
 
 SOURCES += mthread.cpp mthreadpool.cpp
 SOURCES += mythsocket.cpp mythsocketthread.cpp msocketdevice.cpp
@@ -37,7 +37,7 @@ SOURCES += unzip.cpp iso639.cpp iso3166.cpp mythmedia.cpp util.cpp
 SOURCES += mythhdd.cpp mythcdrom.cpp dbutil.cpp
 SOURCES += mythhttppool.cpp mythhttphandler.cpp logging.cpp
 SOURCES += referencecounter.cpp mythcommandlineparser.cpp
-SOURCES += filesysteminfo.cpp hardwareprofile.cpp
+SOURCES += filesysteminfo.cpp hardwareprofile.cpp serverpool.cpp
 
 win32:SOURCES += msocketdevice_win.cpp
 unix {
@@ -63,7 +63,7 @@ inc.files += mythcoreutil.h mythlocale.h mythdownloadmanager.h
 inc.files += mythtranslation.h iso639.h iso3166.h mythmedia.h util.h
 inc.files += mythcdrom.h autodeletedeque.h dbutil.h mythhttppool.h mythdeque.h
 inc.files += referencecounter.h mythcommandlineparser.h mthread.h mthreadpool.h
-inc.files += filesysteminfo.h hardwareprofile.h bonjourregister.h
+inc.files += filesysteminfo.h hardwareprofile.h bonjourregister.h serverpool.h
 
 # Allow both #include <blah.h> and #include <libmythbase/blah.h>
 inc2.path  = $${PREFIX}/include/mythtv/libmythbase
---
+++ mythtv/libs/libmythbase/serverpool.h
@@ -0,0 +1,89 @@
+#ifndef SERVERPOOL_H_
+#define SERVERPOOL_H_
+
+#include <QList>
+#include <QHostAddress>
+#include <QNetworkProxy>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QUdpSocket>
+#include <QStringList>
+
+#include "mythbaseexp.h"
+
+/** \class ServerPool
+ *  \brief Manages a collection of sockets listening on different ports.
+ *
+ *  This class allows a TCP or UDP server to listen on a list of addresses
+ *  rather than limited to a single or all addresses. This is done by opening
+ *  a separate server for each defined QHostAddress, and signalling
+ *  collectively for any new connections.
+ *
+ *  This can be subclassed with new 'newTcpConnection()' and 'newConnection()'
+ *  methods to allow signalling for alternate socket types.
+ */
+
+class PrivTcpServer : public QTcpServer
+{
+    Q_OBJECT
+  public:
+    PrivTcpServer(QObject *parent = 0);
+   ~PrivTcpServer() {};
+
+  signals:
+    void newConnection(int socket);
+
+  protected:
+    void incomingConnection(int socket);
+};
+
+class MBASE_PUBLIC ServerPool : public QObject
+{
+    Q_OBJECT
+
+  public:
+    ServerPool(QObject *parent=0);
+   ~ServerPool(void);
+
+    bool listen(QList<QHostAddress> addrs, quint16 port, bool requireall=true);
+    bool listen(QStringList addrs, quint16 port, bool requireall=true);
+    bool listen(quint16 port, bool requireall=true);
+
+    bool bind(QList<QHostAddress> addrs, quint16 port, bool requireall=true);
+    bool bind(QStringList addrs, quint16 port, bool requireall=true);
+    bool bind(quint16 port, bool requireall=true);
+
+    qint64 writeDatagram(const char * data, qint64 size,
+                         const QHostAddress &addr, quint16 port);
+    qint64 writeDatagram(const QByteArray &datagram,
+                         const QHostAddress &addr, quint16 port);
+
+    bool isListening(void)                      { return m_listening;       }
+    int  maxPendingConnections(void)            { return m_maxPendingConn;  }
+    void setMaxPendingConnections(int n)        { m_maxPendingConn = n;     }
+    quint16 serverPort(void)                    { return m_port;            }
+
+    QNetworkProxy proxy(void)                   { return m_proxy;           }
+    void setProxy(const QNetworkProxy &proxy)   { m_proxy = proxy;          }
+
+    void close(void);
+
+  signals:
+    void newConnection(QTcpSocket *);
+    void newDatagram(QByteArray, QHostAddress, quint16);
+
+  protected slots:
+    virtual void newUdpDatagram(void);
+    virtual void newTcpConnection(int socket);
+
+  private:
+    bool            m_listening;
+    int             m_maxPendingConn;
+    quint16         m_port;
+    QNetworkProxy   m_proxy;
+
+    QList<PrivTcpServer*>   m_tcpServers;
+    QList<QUdpSocket*>      m_udpSockets;
+};
+
+#endif
