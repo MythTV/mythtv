c1aadbb0398d985fee121aa7fcb31e9ccf74f066
David Blain <dblain@mythtv.org>
2012-02-13 15:47:55 -0500

    Changed Licensing to GPL v2 or later.

--- mythtv/libs/libmythupnp/upnpcdsobjects.h
+++ mythtv/libs/libmythupnp/upnpcdsobjects.h
@@ -4,20 +4,9 @@
 //
 // Purpose     : uPnp Content Directory Service Object Definitions
 //
-// Copyright (c) 2005 David Blain <mythtv@theblains.net>
+// Copyright (c) 2005 David Blain <dblain@mythtv.org>
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or at your option any later version of the LGPL.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+// Licensed under the GPL v2 or later, see COPYING for details                    
 //
 //////////////////////////////////////////////////////////////////////////////
 
