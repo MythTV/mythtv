37157bfc19d9616f177600f699c1f87802f598e7
Stuart Morgan <smorgan@mythtv.org>
2012-02-17 17:40:48 +0000

    Add new 'startyear' and 'endyear' strings to the available recording metadata.
    Historically we've never made the year part of recording start/end strings but lots of people have recordings dating back over a year so we need to allow themers to display this information. The simplest fix without breaking existing themes is to expose these new strings that the themer can use in a template with the existing time/date strings.

--- mythtv/libs/libmyth/programinfo.cpp
+++ mythtv/libs/libmyth/programinfo.cpp
@@ -1407,9 +1407,11 @@ void ProgramInfo::ToMap(InfoMap &progMap,
         progMap["starttime"] = MythDateTimeToString(startts, kTime);
         progMap["startdate"] = MythDateTimeToString(startts, kDateFull | kSimplify);
         progMap["shortstartdate"] = MythDateTimeToString(startts, kDateShort);
+        progMap["startyear"] = startts.toString("yyyy");
         progMap["endtime"] = MythDateTimeToString(endts, kTime);
         progMap["enddate"] = MythDateTimeToString(endts, kDateFull | kSimplify);
         progMap["shortenddate"] = MythDateTimeToString(endts, kDateShort);
+        progMap["endyear"] = endts.toString("yyyy");
         progMap["recstarttime"] = MythDateTimeToString(recstartts, kTime);
         progMap["recstartdate"] = MythDateTimeToString(recstartts, kDateShort);
         progMap["recendtime"] = MythDateTimeToString(recendts, kTime);
