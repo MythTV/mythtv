MythTV/mythtv

commit 9b7eae2fda2e15d20bc846dcef756c05f09d6ac0

MythUI: Add a scroll bar widget.
Add a scroll bar widget and use it in MythUIButtonList.

mythtv/libs/libmythui/mythuibuttonlist.cpp
@@ -12,6 +12,7 @@
 
 // mythui headers
 #include "mythmainwindow.h"
+#include "mythuiscrollbar.h"
 #include "mythuistatetype.h"
 #include "lcddevice.h"
 #include "mythuibutton.h"
@@ -24,17 +25,19 @@ MythUIButtonList::MythUIButtonList(MythUIType *parent, const QString &name)
     : MythUIType(parent, name)
 {
     m_showArrow = true;
+    m_showScrollBar = true;
 
     Const();
 }
 
 MythUIButtonList::MythUIButtonList(MythUIType *parent, const QString &name,
                                    const QRect &area, bool showArrow,
-                                   bool showScrollArrows)
+                                   bool showScrollArrows, bool showScrollBar)
     : MythUIType(parent, name)
 {
     m_Area      = area;
     m_showArrow = showArrow;
+    m_showScrollBar = showScrollBar;
 
     Const();
 }
@@ -79,6 +82,7 @@ void MythUIButtonList::Const(void)
     m_searchStartsWith = false;
 
     m_upArrow = m_downArrow = NULL;
+    m_scrollBar = NULL;
 
     m_buttontemplate = NULL;
 
@@ -1372,8 +1376,6 @@ void MythUIButtonList::SetPositionArrowStates()
     if (m_clearing)
         return;
 
-    m_needsUpdate = false;
-
     // set topitem, top position
     SanitizePosition();
     m_ButtonToItem.clear();
@@ -1599,7 +1601,11 @@ int MythUIButtonList::GetCount() const
 uint MythUIButtonList::GetVisibleCount()
 {
     if (m_needsUpdate)
+    {
         SetPositionArrowStates();
+        SetScrollBarPosition();
+        m_needsUpdate = false;
+    }
 
     return m_itemsVisible;
 }
@@ -2164,12 +2170,16 @@ void MythUIButtonList::Init()
 
     m_upArrow = dynamic_cast<MythUIStateType *>(GetChild("upscrollarrow"));
     m_downArrow = dynamic_cast<MythUIStateType *>(GetChild("downscrollarrow"));
+    m_scrollBar = dynamic_cast<MythUIScrollBar *>(GetChild("scrollbar"));
 
     if (m_upArrow)
         m_upArrow->SetVisible(true);
 
     if (m_downArrow)
         m_downArrow->SetVisible(true);
+
+    if (m_scrollBar)
+        m_scrollBar->SetVisible(m_showScrollBar);
 
     m_contentsRect.CalculateArea(m_Area);
 
@@ -2588,6 +2598,8 @@ bool MythUIButtonList::ParseElement(
     }
     else if (element.tagName() == "showarrow")
         m_showArrow = parseBool(element);
+    else if (element.tagName() == "showscrollbar")
+        m_showScrollBar = parseBool(element);
     else if (element.tagName() == "spacing")
     {
         m_itemHorizSpacing = NormX(getFirstText(element).toInt());
@@ -2638,7 +2650,11 @@ bool MythUIButtonList::ParseElement(
 void MythUIButtonList::DrawSelf(MythPainter *, int, int, int, QRect)
 {
     if (m_needsUpdate)
+    {
         SetPositionArrowStates();
+        SetScrollBarPosition();
+        m_needsUpdate = false;
+    }
 }
 
 /**
@@ -2675,6 +2691,7 @@ void MythUIButtonList::CopyFrom(MythUIType *base)
 
     m_active = lb->m_active;
     m_showArrow = lb->m_showArrow;
+    m_showScrollBar = lb->m_showScrollBar;
 
     m_drawFromBottom = lb->m_drawFromBottom;
 
@@ -2691,6 +2708,7 @@ void MythUIButtonList::CopyFrom(MythUIType *base)
 
     m_upArrow = dynamic_cast<MythUIStateType *>(GetChild("upscrollarrow"));
     m_downArrow = dynamic_cast<MythUIStateType *>(GetChild("downscrollarrow"));
+    m_scrollBar = dynamic_cast<MythUIScrollBar *>(GetChild("scrollbar"));
 
     for (int i = 0; i < (int)m_itemsVisible; i++)
     {
@@ -3553,4 +3571,17 @@ void SearchButtonListDialog::prevClicked(void)
 
     if (m_searchState)
         m_searchState->DisplayState(found ? "found" : "notfound");
+}
+
+void MythUIButtonList::SetScrollBarPosition()
+{
+    if (m_clearing || !m_scrollBar || !m_showScrollBar)
+        return;
+
+    int maximum = m_itemCount <= static_cast<int>(m_itemsVisible) ?
+        0 : m_itemCount;
+    m_scrollBar->SetMaximum(maximum);
+    m_scrollBar->SetPageStep(m_itemsVisible);
+    m_scrollBar->SetSliderPosition(m_selPosition);
+    m_scrollBar->MoveToTop();
 }

mythtv/libs/libmythui/libmythui.pro
@@ -31,7 +31,7 @@ HEADERS += mythvirtualkeyboard.h mythuishape.h mythuiguidegrid.h
 HEADERS += mythrender_base.h mythfontmanager.h mythuieditbar.h
 HEADERS += mythdisplay.h mythuivideo.h mythudplistener.h
 HEADERS += mythuiexp.h mythuisimpletext.h mythuistatetracker.h
-HEADERS += mythuianimation.h
+HEADERS += mythuianimation.h mythuiscrollbar.h
 
 SOURCES  = mythmainwindow.cpp mythpainter.cpp mythimage.cpp mythrect.cpp
 SOURCES += myththemebase.cpp  mythpainter_qimage.cpp mythpainter_yuva.cpp
@@ -50,8 +50,7 @@ SOURCES += mythvirtualkeyboard.cpp mythuishape.cpp mythuiguidegrid.cpp
 SOURCES += mythfontmanager.cpp mythuieditbar.cpp
 SOURCES += mythdisplay.cpp mythuivideo.cpp mythudplistener.cpp
 SOURCES += mythuisimpletext.cpp mythuistatetracker.cpp
-SOURCES += mythuianimation.cpp
-
+SOURCES += mythuianimation.cpp mythuiscrollbar.cpp
 
 inc.path = $${PREFIX}/include/mythtv/libmythui/
 
@@ -68,8 +67,7 @@ inc.files += x11colors.h mythgenerictree.h mythuibuttontree.h
 inc.files += mythvirtualkeyboard.h mythuishape.h mythuiguidegrid.h
 inc.files += mythuieditbar.h mythuifilebrowser.h mythuivideo.h
 inc.files += mythuiexp.h mythuisimpletext.h mythuiactions.h
-inc.files += mythuistatetracker.h
-inc.files += mythuianimation.h
+inc.files += mythuistatetracker.h mythuianimation.h mythuiscrollbar.h
 
 INSTALLS += inc
 

mythtv/libs/libmythui/mythuibuttonlist.h
@@ -13,6 +13,7 @@
 #include "mythscreentype.h"
 
 class MythUIButtonList;
+class MythUIScrollBar;
 class MythUIStateType;
 
 struct TextProperties {
@@ -121,7 +122,8 @@ class MUI_PUBLIC MythUIButtonList : public MythUIType
     MythUIButtonList(MythUIType *parent, const QString &name);
     MythUIButtonList(MythUIType *parent, const QString &name,
                    const QRect &area, bool showArrow = true,
-                   bool showScrollArrows = false);
+                   bool showScrollArrows = false,
+                   bool showScrollBar = false);
     ~MythUIButtonList();
 
     virtual bool keyPressEvent(QKeyEvent *);
@@ -227,6 +229,7 @@ class MUI_PUBLIC MythUIButtonList : public MythUIType
     bool DistributeButtons(void);
     void SetPosition(void);
     void SetPositionArrowStates(void);
+    void SetScrollBarPosition(void);
     void ItemVisible(MythUIButtonListItem *item);
 
     void SetActive(bool active);
@@ -277,7 +280,9 @@ class MUI_PUBLIC MythUIButtonList : public MythUIType
 
     bool m_active;
     bool m_showArrow;
+    bool m_showScrollBar;
 
+    MythUIScrollBar *m_scrollBar;
     MythUIStateType *m_upArrow;
     MythUIStateType *m_downArrow;
 

mythtv/libs/libmythui/mythuiscrollbar.cpp
@@ -1,0 +1,183 @@
+// Own Header
+#include "mythuiscrollbar.h"
+
+// QT
+#include <QCoreApplication>
+#include <QDomDocument>
+
+// myth
+#include "mythlogging.h"
+
+const int kDefaultMaxValue = 99;
+const int kDefaultPageStep = 10;
+
+MythUIScrollBar::MythUIScrollBar(MythUIType *parent, const QString &name)
+    : MythUIType(parent, name),
+      m_layout(LayoutVertical), m_pageStep(kDefaultPageStep),
+      m_sliderPosition(0), m_maximum(kDefaultMaxValue),
+      m_hideDelay(0), m_timerId(0)
+{
+}
+
+MythUIScrollBar::~MythUIScrollBar()
+{
+}
+
+void MythUIScrollBar::Reset()
+{
+    m_pageStep = kDefaultPageStep;
+    m_sliderPosition = 0;
+    m_maximum = kDefaultMaxValue;
+
+    CalculatePosition();
+
+    MythUIType::Reset();
+}
+
+bool MythUIScrollBar::ParseElement(
+    const QString &filename, QDomElement &element, bool showWarnings)
+{
+    if (element.tagName() == "layout")
+    {
+        QString layout = getFirstText(element).toLower();
+
+        if (layout == "vertical")
+            m_layout = LayoutVertical;
+        else
+            m_layout = LayoutHorizontal;
+    }
+    else if (element.tagName() == "hidedelay")
+        m_hideDelay = getFirstText(element).toInt();
+    else
+        return MythUIType::ParseElement(filename, element, showWarnings);
+
+    return true;
+}
+
+void MythUIScrollBar::SetPageStep(int value)
+{
+    if (value == m_pageStep)
+        return;
+
+    if (value < 1)
+        value = kDefaultPageStep;
+
+    m_pageStep = value;
+    CalculatePosition();
+}
+
+void MythUIScrollBar::SetMaximum(int value)
+{
+    if (value - 1 == m_maximum)
+        return;
+
+    if (value < 1)
+        value = 1;
+
+    m_maximum = value - 1;
+    CalculatePosition();
+}
+
+void MythUIScrollBar::SetSliderPosition(int value)
+{
+    if (value == m_sliderPosition)
+        return;
+
+    if (value < 0)
+        value = 0;
+
+    if (value > m_maximum)
+        value = m_maximum;
+
+    m_sliderPosition = value;
+    CalculatePosition();
+}
+
+void MythUIScrollBar::CalculatePosition(void)
+{
+    if (m_maximum > 0)
+        Show();
+    else
+    {
+        Hide();
+        return;
+    }
+
+    MythUIType *slider = GetChild("slider");
+
+    if (!slider)
+    {
+        LOG(VB_GENERAL, LOG_ERR, "Slider element doesn't exist");
+        return;
+    }
+
+    float percentage = (float)m_sliderPosition / m_maximum;
+    float relativeSize = (float)m_pageStep / (m_maximum + m_pageStep);
+
+    MythRect newSliderArea = slider->GetArea();
+    MythRect fillArea = GetArea();
+    QPoint endPos(newSliderArea.left(), newSliderArea.top());
+
+    if (m_layout == LayoutHorizontal)
+    {
+        int width = qMax((int)(fillArea.width() * relativeSize + 0.5),
+                         m_sliderArea.width());
+        newSliderArea.setWidth(width);
+        endPos.setX((int)((fillArea.width() - width) * percentage + 0.5));
+    }
+    else
+    {
+        int height = qMax((int)(fillArea.height() * relativeSize + 0.5),
+                          m_sliderArea.height());
+        newSliderArea.setHeight(height);
+        endPos.setY((int)((fillArea.height() - height) * percentage + 0.5));
+    }
+
+    slider->SetArea(newSliderArea);
+    slider->SetPosition(endPos);
+
+    if (m_hideDelay > 0)
+    {
+        if (m_timerId)
+            killTimer(m_timerId);
+        m_timerId = startTimer(m_hideDelay);
+
+        AdjustAlpha(1, 10, 0, 255);
+    }
+}
+
+void MythUIScrollBar::Finalize()
+{
+    MythUIType *slider = GetChild("slider");
+    if (slider)
+        m_sliderArea = slider->GetArea();
+
+    CalculatePosition();
+}
+
+void MythUIScrollBar::CopyFrom(MythUIType *base)
+{
+    MythUIScrollBar *scrollbar = dynamic_cast<MythUIScrollBar *>(base);
+    if (!scrollbar)
+        return;
+
+    m_layout = scrollbar->m_layout;
+    m_sliderArea = scrollbar->m_sliderArea;
+    m_hideDelay = scrollbar->m_hideDelay;
+
+    MythUIType::CopyFrom(base);
+}
+
+void MythUIScrollBar::CreateCopy(MythUIType *parent)
+{
+    MythUIScrollBar *scrollbar = new MythUIScrollBar(parent, objectName());
+    scrollbar->CopyFrom(this);
+}
+
+void MythUIScrollBar::timerEvent(QTimerEvent *)
+{
+    if (m_timerId)
+        killTimer(m_timerId);
+    m_timerId = 0;
+    AdjustAlpha(1, -10, 0, 255);
+}

mythtv/libs/libmythui/mythuiscrollbar.h
@@ -1,0 +1,51 @@
+#ifndef MYTHUI_SCROLLBAR_H_
+#define MYTHUI_SCROLLBAR_H_
+
+#include "mythuitype.h"
+#include "mythuiimage.h"
+#include "mythuishape.h"
+
+/** \class MythUIScrollBar
+ *
+ * \brief Scroll bar widget.
+ *
+ * \ingroup MythUI_Widgets
+ */
+class MUI_PUBLIC MythUIScrollBar : public MythUIType
+{
+    Q_OBJECT
+
+  public:
+    MythUIScrollBar(MythUIType *parent, const QString &name);
+   ~MythUIScrollBar();
+
+    void Reset(void);
+
+    enum LayoutType { LayoutVertical, LayoutHorizontal };
+
+    void SetPageStep(int);
+    void SetSliderPosition(int);
+    void SetMaximum(int);
+
+  protected:
+    virtual bool ParseElement(
+        const QString &filename, QDomElement &element, bool showWarnings);
+    virtual void CopyFrom(MythUIType *base);
+    virtual void CreateCopy(MythUIType *parent);
+    virtual void Finalize(void);
+
+    void CalculatePosition(void);
+    void timerEvent(QTimerEvent *);
+
+    LayoutType m_layout;
+
+    MythRect m_sliderArea;
+    int m_pageStep;
+    int m_sliderPosition;
+    int m_maximum;
+
+    int m_hideDelay;
+    int m_timerId;
+};
+
+#endif

mythtv/libs/libmythui/xmlparsebase.cpp
@@ -31,6 +31,7 @@
 #include "mythuispinbox.h"
 #include "mythuicheckbox.h"
 #include "mythuiprogressbar.h"
+#include "mythuiscrollbar.h"
 #include "mythuigroup.h"
 #include "mythuiwebbrowser.h"
 #include "mythuiguidegrid.h"
@@ -388,6 +389,7 @@ void XMLParseBase::ParseChildren(const QString &filename,
                      type == "statetype" ||
                      type == "clock" ||
                      type == "progressbar" ||
+                     type == "scrollbar" ||
                      type == "webbrowser" ||
                      type == "guidegrid" ||
                      type == "shape" ||
@@ -484,6 +486,8 @@ MythUIType *XMLParseBase::ParseUIType(
         uitype = new MythUIClock(parent, name);
     else if (type == "progressbar")
         uitype = new MythUIProgressBar(parent, name);
+    else if (type == "scrollbar")
+        uitype = new MythUIScrollBar(parent, name);
     else if (type == "webbrowser")
         uitype = new MythUIWebBrowser(parent, name);
     else if (type == "guidegrid")
@@ -583,6 +587,7 @@ MythUIType *XMLParseBase::ParseUIType(
                      info.tagName() == "statetype" ||
                      info.tagName() == "clock" ||
                      info.tagName() == "progressbar" ||
+                     info.tagName() == "scrollbar" ||
                      info.tagName() == "webbrowser" ||
                      info.tagName() == "guidegrid" ||
                      info.tagName() == "shape" ||
@@ -785,6 +790,7 @@ bool XMLParseBase::doLoad(const QString &windowname,
                          type == "window" ||
                          type == "clock" ||
                          type == "progressbar" ||
+                         type == "scrollbar" ||
                          type == "webbrowser" ||
                          type == "guidegrid" ||
                          type == "shape" ||

commit d95479d83b3f5f3b51bc0ed92d704c3d327f600e

MythUI: Fix itemVisible issue introduced in 9b7eae2f
The changes related to m_needsUpdate in 9b7eae2f caused issues with updates
triggered by the itemVisible signal.  This restores the previous handling of
m_needsUpdate.

mythtv/libs/libmythui/mythuibuttonlist.cpp
@@ -1377,6 +1377,8 @@ void MythUIButtonList::SetPositionArrowStates()
     if (m_clearing)
         return;
 
+    m_needsUpdate = false;
+
     // set topitem, top position
     SanitizePosition();
     m_ButtonToItem.clear();
@@ -1605,7 +1607,6 @@ uint MythUIButtonList::GetVisibleCount()
     {
         SetPositionArrowStates();
         SetScrollBarPosition();
-        m_needsUpdate = false;
     }
 
     return m_itemsVisible;
@@ -2656,7 +2657,6 @@ void MythUIButtonList::DrawSelf(MythPainter *, int, int, int, QRect)
     {
         SetPositionArrowStates();
         SetScrollBarPosition();
-        m_needsUpdate = false;
     }
 }
 