99512fb5229179cf65cf00649b7f7c0271aa9372
Jim Stichnoth <jstichnoth@mythtv.org>
2012-02-28 21:44:50 -0800

    Refs #10386.  Don't sent EXIT_TO_MAIN event to Live TV jump point.
    While 7e6f5fa66 fixes a segfault, that section of code shouldn't have
    been executed -- it's not a good idea to dispatch an EXIT_TO_MAIN
    event right before starting playback.  Therefore, don't dispatch the
    event if the jump point starts from the main menu.

--- mythtv/libs/libmythui/mythmainwindow.cpp
+++ mythtv/libs/libmythui/mythmainwindow.cpp
@@ -1439,11 +1439,11 @@ void MythMainWindow::ExitToMainMenu(void)
     MythScreenStack *toplevel = GetMainStack();
     if (toplevel && d->popwindows)
     {
-        MythEvent xe("EXIT_TO_MENU");
-        gCoreContext->dispatch(xe);
         MythScreenType *screen = toplevel->GetTopScreen();
         if (screen && screen->objectName() != QString("mainmenu"))
         {
+            MythEvent xe("EXIT_TO_MENU");
+            gCoreContext->dispatch(xe);
             if (screen->objectName() == QString("video playback window"))
             {
                 MythEvent *me = new MythEvent("EXIT_TO_MENU");
