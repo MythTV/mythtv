bfa84c54166a606e4c7b587aeb10e5e79a2d0e50
Raymond Wagner <rwagner@mythtv.org>
2012-03-04 02:21:39 -0500

    Add __init__

---
+++ tmdb3/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from tmdb_api import Configuration, searchMovie, searchPerson, Person, \
+                     Movie, Collection, __version__
+from request import set_key
+from tmdb_exceptions import *
+
