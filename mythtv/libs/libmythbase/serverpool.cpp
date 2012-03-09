23f3e6ff07fc80d4c4f95729c3f782707a326eb7
Raymond Wagner <rwagner@mythtv.org>
2012-03-09 13:02:04 -0500

    Revert to using Any address.
    This bypasses the startup IP address selector in ServerPool, instead
    listening on Any and AnyIPv6.  This leaves the ServerPool itself in
    place to handle systems with the bindv6only sysctl set.  The behavior is
    being reverted over concerns with systems that may have not completed
    network configuration by the time MythTV starts, or would otherwise
    change network behavior during normal operation.

--- mythtv/libs/libmythbase/serverpool.cpp
+++ mythtv/libs/libmythbase/serverpool.cpp
@@ -202,6 +202,9 @@ QList<QHostAddress> ServerPool::DefaultListen(void)
 
 QList<QHostAddress> ServerPool::DefaultListenIPv4(void)
 {
+    QList<QHostAddress> alist;
+    alist << QHostAddress::Any;
+#if 0
     SelectDefaultListen();
     QReadLocker rlock(&naLock);
 
@@ -210,12 +213,15 @@ QList<QHostAddress> ServerPool::DefaultListenIPv4(void)
     for (it = naList_4.begin(); it != naList_4.end(); ++it)
         if (!alist.contains(it->ip()))
             alist << it->ip();
-
+#endif
     return alist;
 }
 
 QList<QHostAddress> ServerPool::DefaultListenIPv6(void)
 {
+    QList<QHostAddress> alist;
+    alist << QHostAddress::AnyIPv6;
+#if 0
     SelectDefaultListen();
     QReadLocker rlock(&naLock);
 
@@ -224,7 +230,7 @@ QList<QHostAddress> ServerPool::DefaultListenIPv6(void)
     for (it = naList_6.begin(); it != naList_6.end(); ++it)
         if (!alist.contains(it->ip()))
             alist << it->ip();
-
+#endif
     return alist;
 }
 
@@ -237,6 +243,9 @@ QList<QHostAddress> ServerPool::DefaultBroadcast(void)
 
 QList<QHostAddress> ServerPool::DefaultBroadcastIPv4(void)
 {
+    QList<QHostAddress> blist;
+    blist << QHostAddress::Any;
+#if 0
     SelectDefaultListen();
     QReadLocker rlock(&naLock);
 
@@ -246,7 +255,7 @@ QList<QHostAddress> ServerPool::DefaultBroadcastIPv4(void)
         if (!blist.contains(it->broadcast()) && (it->prefixLength() != 32) &&
                 (it->ip() != QHostAddress::LocalHost))
             blist << it->broadcast();
-
+#endif
     return blist;
 }
 
