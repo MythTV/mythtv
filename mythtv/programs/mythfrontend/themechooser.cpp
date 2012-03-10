c114dd5eab07f502d6909fed3c25d951abdc286d
Chris Pinkham <cpinkham@mythtv.org>
2012-03-09 23:01:10 -0500

    If the downloadable themes cache dir doesn't exist, force redownload.
    This should fix an issue with the Theme Chooser sometimes showing
    only locally installed themes by default forcing the user to manually
    refresh the downloadable themes list via the popup menu.  If the
    downloadable themes cache dir does not exist, we trigger a download
    attempt automatically when entering the Theme Chooser.

--- mythtv/programs/mythfrontend/themechooser.cpp
+++ mythtv/programs/mythfrontend/themechooser.cpp
@@ -195,6 +195,8 @@ void ThemeChooser::Load(void)
     QString themeSite = QString("%1/%2")
         .arg(gCoreContext->GetSetting("ThemeRepositoryURL",
              "http://themes.mythtv.org/themes/repository")).arg(MythVersion);
+    QDir remoteThemesDir(GetMythUI()->GetThemeCacheDir()
+                             .append("/themechooser/").append(MythVersion));
 
     int downloadFailures =
         gCoreContext->GetNumSetting("ThemeInfoDownloadFailures", 0);
@@ -208,6 +210,9 @@ void ThemeChooser::Load(void)
                         "remote theme list download").arg(remoteThemesFile));
             m_refreshDownloadableThemes = true;
         }
+
+        if (!remoteThemesDir.exists())
+            m_refreshDownloadableThemes = true;
     }
     else if (downloadFailures < 2) // (and themes.zip does not exist)
     {
@@ -242,9 +247,6 @@ void ThemeChooser::Load(void)
                                       downloadFailures);
         }
     }
-
-    QDir remoteThemesDir(GetMythUI()->GetThemeCacheDir()
-                             .append("/themechooser/").append(MythVersion));
 
     if ((QFile::exists(remoteThemesFile)) &&
         (remoteThemesDir.exists()))
