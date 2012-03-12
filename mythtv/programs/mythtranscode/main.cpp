cc55c12a1f2ac1f19f68f2b65285bf3e4a490fac
Raymond Wagner <rwagner@mythtv.org>
2012-03-12 02:59:54 -0400

    Add some sanity checking to manual cutlist input.

--- mythtv/programs/mythtranscode/main.cpp
+++ mythtv/programs/mythtranscode/main.cpp
@@ -240,8 +240,8 @@ int main(int argc, char *argv[])
         {
             if (!cmdline.toBool("inputfile") && !cmdline.toBool("hls"))
             {
-                cerr << "External cutlists are only allowed when using" << endl
-                     << "the --infile option." << endl;
+                LOG(VB_GENERAL, LOG_CRIT, "External cutlists are only allowed "
+                                          "when using the --infile option.");
                 return GENERIC_EXIT_INVALID_CMDLINE;
             }
 
@@ -259,16 +259,18 @@ int main(int argc, char *argv[])
 
                     if (cmdline.toBool("inversecut"))
                     {
-                        cerr << "Cutting from: " << last
-                             << " to: " << start << "\n";
+                        LOG(VB_GENERAL, LOG_DEBUG,
+                                QString("Cutting section %1-%2.")
+                                    .arg(last).arg(start));
                         deleteMap[start] = MARK_CUT_END;
                         deleteMap[end] = MARK_CUT_START;
                         last = end;
                     }
                     else
                     {
-                        cerr << "Cutting from: " << start
-                             << " to: " << end << "\n";
+                        LOG(VB_GENERAL, LOG_DEBUG,
+                                QString("Cutting section %1-%2.")
+                                    .arg(start).arg(end));
                         deleteMap[start] = MARK_CUT_START;
                         deleteMap[end] = MARK_CUT_END;
                     }
@@ -277,8 +279,51 @@ int main(int argc, char *argv[])
 
             if (cmdline.toBool("inversecut"))
             {
-                deleteMap[0] = MARK_CUT_START;
+                if (deleteMap.contains(0) && (deleteMap[0] == MARK_CUT_END))
+                    deleteMap.remove(0);
+                else
+                    deleteMap[0] = MARK_CUT_START;
                 deleteMap[999999999] = MARK_CUT_END;
+                LOG(VB_GENERAL, LOG_DEBUG,
+                    QString("Cutting section %1-999999999.")
+                                    .arg(last));
+            }
+
+            // sanitize cutlist
+            if (deleteMap.count() >= 2)
+            {
+                frm_dir_map_t::iterator cur = deleteMap.begin(), prev;
+                prev = cur++;
+                while (cur != deleteMap.end())
+                {
+                    if (prev.value() == cur.value())
+                    {
+                        // two of the same type next to each other
+                        QString err("Cut %1points found at %3 and %4, with no "
+                                    "%2 point in between.");
+                        if (prev.value() == MARK_CUT_END)
+                            err = err.arg("end").arg("start");
+                        else
+                            err = err.arg("start").arg("end");
+                        LOG(VB_GENERAL, LOG_CRIT, "Invalid cutlist defined!");
+                        LOG(VB_GENERAL, LOG_CRIT, err.arg(prev.key())
+                                                     .arg(cur.key()));
+                        return GENERIC_EXIT_INVALID_CMDLINE;
+                    }
+                    else if ( (prev.value() == MARK_CUT_START) &&
+                              ((cur.key() - prev.key()) < 2) )
+                    {
+                        LOG(VB_GENERAL, LOG_WARNING, QString("Discarding "
+                                          "insufficiently long cut: %1-%2")
+                                            .arg(prev.key()).arg(cur.key()));
+                        prev = deleteMap.erase(prev);
+                        cur  = deleteMap.erase(cur);
+
+                        if (cur == deleteMap.end())
+                            continue;
+                    }
+                    prev = cur++;
+                }
             }
         }
         else if (cmdline.toBool("inversecut"))
