2451dc574b8072a38d54d6ef7e9342a2de3f4f73
Kenni Lund <kenni@mythtv.org>
2012-03-21 00:54:41 +0100

    Add Steppes-narrow to the list of translatable themes. The strings seems identical to Steppes at the moment, but let's include it just to be on the safe side.

--- themestringstool/run.sh
+++ themestringstool/run.sh
@@ -75,7 +75,7 @@ done
 #####################################################################
 
 # Select the themes that should be translatable (theme name = directory name after extraction)
-TRANSLATABLE_THEMES="Arclight Childish Graphite Mythbuntu MythCenter MythCenter-wide Steppes Terra"
+TRANSLATABLE_THEMES="Arclight Childish Graphite Mythbuntu MythCenter MythCenter-wide Steppes Stepped-narrow Terra"
 #TRANSLATABLE_THEMES=$(ls ${DOWNLOAD_DIR}/themes/ --file-type |grep "/$"|tr '/' ' ') #All themes
 
 #Remove the extracted themes which shouldn't be translatable
