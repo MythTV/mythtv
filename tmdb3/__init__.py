d5d5cba82e09328435431edcac536bd289476515
Raymond Wagner <rwagner@mythtv.org>
2012-03-25 17:52:48 -0400

    Initial changes for improved locale support.

--- tmdb3/__init__.py
+++ tmdb3/__init__.py
@@ -3,5 +3,6 @@
 from tmdb_api import Configuration, searchMovie, searchPerson, Person, \
                      Movie, Collection, __version__
 from request import set_key, set_cache
+from locales import Locale, set_locale
 from tmdb_exceptions import *
 
