83671d53e1844a3264cd806e70a1771608c5acec
Nigel Pearson <npearson@mythtv.org>
2012-04-07 16:55:44 +1000

    Revert "Better handling of unmounted volumes. Refs #10450"
    This reverts commit 58e8506b9506f9b2390cb70343eb5f9c044cf345.
    It had some extra rubbish in it :-(

--- mythtv/libs/libmyth/mediamonitor-darwin.cpp
+++ mythtv/libs/libmyth/mediamonitor-darwin.cpp
@@ -399,7 +399,6 @@ void MonitorThreadDarwin::diskInsert(const char *devName,
                                      const char *volName,
                                      QString model, bool isCDorDVD)
 {
-    QString           devPath = "/dev/"; devPath += devName;
     MythMediaDevice  *media;
     QString           msg = "MonitorThreadDarwin::diskInsert";
 
@@ -407,9 +406,9 @@ void MonitorThreadDarwin::diskInsert(const char *devName,
                       .arg(devName).arg(volName).arg(model).arg(isCDorDVD));
 
     if (isCDorDVD)
-        media = MythCDROM::get(NULL, devPath.toAscii(), true, m_Monitor->m_AllowEject);
+        media = MythCDROM::get(NULL, devName, true, m_Monitor->m_AllowEject);
     else
-        media = MythHDD::Get(NULL, devPath.toAscii(), true, false);
+        media = MythHDD::Get(NULL, devName, true, false);
 
     if (!media)
     {
@@ -425,22 +424,14 @@ void MonitorThreadDarwin::diskInsert(const char *devName,
     QString mnt = "/Volumes/"; mnt += volName;
     media->setMountPath(mnt.toAscii());
 
-    int  attempts = 0;
     QDir d(mnt);
     while (!d.exists())
     {
         LOG(VB_MEDIA, LOG_WARNING,
             (msg + "() - Waiting for mount '%1' to become stable.").arg(mnt));
         usleep(120000);
-        if ( ++attempts > 4 )
-            usleep(200000);
-        if ( attempts > 8 )
-        {
-            delete media;
-            LOG(VB_MEDIA, LOG_ALERT, msg + "() - Giving up");
-            return;
-        }
     }
+
     media->setStatus(MEDIASTAT_MOUNTED);
 
     // This is checked in AddDevice(), but checking earlier means
