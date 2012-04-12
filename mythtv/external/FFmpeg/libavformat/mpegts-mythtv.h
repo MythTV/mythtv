2d7fdc18bacf2e1ac8f66f6bfbd7723225ebdf99
Gavin Hurlbut <ghurlbut@mythtv.org>
2012-04-11 22:27:36 -0700

    Argh, missed a spot, sorry

--- mythtv/external/FFmpeg/libavformat/mpegts-mythtv.h
+++ mythtv/external/FFmpeg/libavformat/mpegts-mythtv.h
@@ -140,7 +140,7 @@ typedef struct
  * @param desc_list_end             End of buffer
  * @return <0 to stop processing
  */
-int ff_mythtv_parse_mpeg2_descriptor(AVFormatContext *fc, pmt_entry_t *item, int stream_type,
+int ff_parse_mpeg2_descriptor(AVFormatContext *fc, pmt_entry_t *item, int stream_type,
                               const uint8_t **pp, const uint8_t *desc_list_end,
                               Mp4Descr *mp4_descr, int mp4_descr_count, int pid,
                               MpegTSContext *ts, dvb_caption_info_t *dvbci);
