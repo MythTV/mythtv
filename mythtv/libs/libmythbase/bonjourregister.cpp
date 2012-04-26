fd430e9bcf88e1f99eea278fd715632def858524
Jean-Yves Avenard <jyavenard@mythtv.org>
2012-04-26 12:26:58 +1000

    Add ability to override Bonjour's service name.
    This is a work around a more complex issue.
    By default, Bonjour would automatically detect the computer name and use it to advertise the server.
    
    From Bonjour man-page:
    "If non-NULL, specifies the service name to be registered. Most applications will not specify a name, in which case the computer name is used (this name is communicated to the client via the callback). If a name is specified, it must be 1-63 bytes of UTF-8 text. If the name is longer than 63 bytes it will be automatically truncated to a legal length"
    
    However, for some people with broken DNS configuration, they can't resolve that name or it resolves improperly.
    So add a BonjourHostname setting to override the name used by Bonjour.
    
    Use it as an extra argument: -O BonjourHostname=mycomputer.
    
    [cherry-picked from 48d84958a93e94e97591f0f239902fecfca8607f]

--- mythtv/libs/libmythbase/bonjourregister.cpp
+++ mythtv/libs/libmythbase/bonjourregister.cpp
@@ -5,6 +5,7 @@
 
 #include "mythlogging.h"
 #include "bonjourregister.h"
+#include "mythcorecontext.h"
 
 #define LOC QString("Bonjour: ")
 
@@ -41,11 +42,13 @@ bool BonjourRegister::Register(uint16_t port, const QByteArray &type,
         return true;
     }
 
+    QByteArray host(gCoreContext->GetSetting("BonjourHostname", "").toUtf8());
+    const char *host_ptr = host.size() > 0 ? host.constData() : NULL;
     uint16_t qport = qToBigEndian(port);
     DNSServiceErrorType res =
         DNSServiceRegister(&m_dnssref, 0, 0, (const char*)name.data(),
                            (const char*)type.data(),
-                           NULL, 0, qport, txt.size(), (void*)txt.data(),
+                           NULL, host_ptr, qport, txt.size(), (void*)txt.data(),
                            BonjourCallback, this);
 
     if (kDNSServiceErr_NoError != res)
