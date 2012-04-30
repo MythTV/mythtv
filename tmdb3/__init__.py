361e06851f02f5a70f554fcee0890a7a37e6ad95
Raymond Wagner <rwagner@mythtv.org>
2012-04-30 02:12:12 -0400

    Correct missed changes in __init__.py

--- tmdb3/__init__.py
+++ tmdb3/__init__.py
@@ -4,6 +4,7 @@ from tmdb_api import Configuration, searchMovie, searchPerson, Person, \
                      Movie, Collection, __version__
 from request import set_key, set_cache
 from locales import get_locale, set_locale
-from cache import CacheEngine
+from tmdb_auth import get_session, set_session
+from cache_engine import CacheEngine
 from tmdb_exceptions import *
 
