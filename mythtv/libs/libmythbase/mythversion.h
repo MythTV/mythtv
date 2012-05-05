0d836c7357d4357451f25bf993384136b189a408
Daniel Kristjansson <danielk@cuymedia.net>
2012-05-05 19:36:53 -0400

    Add UTC conversion DB update.

--- mythtv/libs/libmythbase/mythversion.h
+++ mythtv/libs/libmythbase/mythversion.h
@@ -57,7 +57,7 @@
  *      mythtv/bindings/php/MythBackend.php
 #endif
 
-#define MYTH_DATABASE_VERSION "1302"
+#define MYTH_DATABASE_VERSION "1303"
 
 
  MBASE_PUBLIC  const char *GetMythSourceVersion();
