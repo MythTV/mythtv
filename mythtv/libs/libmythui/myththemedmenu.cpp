fcfd8e27fdc3a32f825ba346faf43f422440b876
Daniel Kristjansson <danielk@cuymedia.net>
2012-05-05 18:57:57 -0400

    Fix breakage from previous merge

--- mythtv/libs/libmythui/myththemedmenu.cpp
+++ mythtv/libs/libmythui/myththemedmenu.cpp
@@ -896,13 +896,8 @@ bool MythThemedMenu::checkPinCode(const QString &password_setting)
         QDateTime last_time = MythDate::fromString(last_time_stamp);
         if (!last_time.isValid() || last_time.secsTo(curr_time) < 120)
         {
-<<<<<<< HEAD
             last_time_stamp = MythDate::toString(
                 curr_time, MythDate::kDatabase);
-            GetMythDB()->SetSetting(timestamp_setting, last_time_stamp);
-=======
-            last_time_stamp = curr_time.toString(Qt::TextDate);
->>>>>>> master
             GetMythDB()->SaveSetting(timestamp_setting, last_time_stamp);
             return true;
         }
