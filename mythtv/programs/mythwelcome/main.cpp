933cc092f2fd5192f98fe939e8c0138760ef1844
Jean-Yves Avenard <jyavenard@mythtv.org>
2012-05-06 21:26:00 +1000

    Fix mac compilation following 3fb9d6eb779e08d91df8a849a5be38e219c34bed
    Shouldn't be needed anymore (hopefully).

--- mythtv/programs/mythwelcome/main.cpp
+++ mythtv/programs/mythwelcome/main.cpp
@@ -97,11 +97,6 @@ int main(int argc, char **argv)
 
     GetMythUI()->LoadQtConfig();
 
-#ifdef Q_WS_MACX
-    // Mac OS 10.4 and Qt 4.4 have window-focus problems
-    gCoreContext->SetSetting("RunFrontendInWindow", "1");
-#endif
-
     MythMainWindow *mainWindow = GetMythMainWindow();
     mainWindow->Init();
 
