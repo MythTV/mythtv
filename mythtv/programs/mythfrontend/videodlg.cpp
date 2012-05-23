MythTV/mythtv

commit 7c8e45f655c376955588a626b6c191263e1d7582

Prompt the user to scan for content if no videos are in the database.

mythtv/programs/mythfrontend/videodlg.cpp
@@ -1006,9 +1006,6 @@ bool VideoDialog::Create()
 
     BuildFocusList();
 
-    CheckedSet(m_novideoText,
-               tr("Video dialog loading, or no videos available..."));
-
     if (m_d->m_type == DLG_TREE)
     {
         SetFocusWidget(m_videoButtonTree);
@@ -1030,18 +1027,24 @@ bool VideoDialog::Create()
                 SLOT(UpdateText(MythUIButtonListItem *)));
     }
 
-    connect(&m_d->m_parentalLevel, SIGNAL(SigLevelChanged()),
-            SLOT(reloadData()));
-
     return true;
 }
 
 void VideoDialog::Init()
 {
-
     m_d->m_parentalLevel.SetLevel(ParentalLevel(gCoreContext->
                     GetNumSetting("VideoDefaultParentalLevel",
                             ParentalLevel::plLowest)));
+    connect(&m_d->m_parentalLevel, SIGNAL(SigLevelChanged()),
+            SLOT(reloadData()));
+}
+
+void VideoDialog::Load()
+{
+    reloadData();
+    // We only want to prompt once, on startup, hence we this is done in Load()
+    if (m_d->m_rootNode->getInt() != kNoFilesFound)
+        PromptToScan();
 }
 
 /** \fn VideoDialog::refreshData()
@@ -1057,7 +1060,7 @@ void VideoDialog::refreshData()
             ParentalLevelToState(m_d->m_parentalLevel.GetLevel()));
 
     if (m_novideoText)
-        m_novideoText->SetVisible(!m_d->m_treeLoaded);
+        m_novideoText->SetVisible(m_d->m_rootNode->getInt() != kNoFilesFound);
 }
 
 void VideoDialog::reloadAllData(bool dbChanged)
@@ -1065,9 +1068,8 @@ void VideoDialog::reloadAllData(bool dbChanged)
     delete m_d->m_scanner;
     m_d->m_scanner = 0;
 
-    if (dbChanged) {
+    if (dbChanged)
         m_d->m_videoList->InvalidateCache();
-    }
 
     m_d->m_currentNode = NULL;
     reloadData();
@@ -3252,7 +3254,17 @@ void VideoDialog::customEvent(QEvent *levent)
     }
     else if (levent->type() == DialogCompletionEvent::kEventType)
     {
-        m_menuPopup = NULL;
+        DialogCompletionEvent *dce = static_cast<DialogCompletionEvent *>(levent);
+        QString id = dce->GetId();
+
+        if (id == "scanprompt")
+        {
+            int result = dce->GetResult();
+            if (result == 1)
+                doVideoScan();
+        }
+        else
+            m_menuPopup = NULL;
     }
 }
 
@@ -3721,6 +3733,20 @@ void VideoDialog::doVideoScan()
         m_d->m_scanner = new VideoScanner();
     connect(m_d->m_scanner, SIGNAL(finished(bool)), SLOT(reloadAllData(bool)));
     m_d->m_scanner->doScan(GetVideoDirs());
+}
+
+void VideoDialog::PromptToScan()
+{
+    QString message = tr("There are no videos in the database, would you like "
+                         "to scan your video directories now?");
+    MythConfirmationDialog *dialog = new MythConfirmationDialog(m_popupStack,
+                                                                message,
+                                                                true);
+    dialog->SetReturnEvent(this, "scanprompt");
+    if (dialog->Create())
+        m_popupStack->AddScreen(dialog);
+    else
+        delete dialog;
 }
 
 #include "videodlg.moc"

mythtv/programs/mythfrontend/videodlg.h
@@ -64,6 +64,7 @@ class VideoDialog : public MythScreenType
 
   protected slots:
     void Init(); /// Called after the screen is created by MythScreenStack
+    void Load(); /// Called after the screen is created by MythScreenStack
 
   private slots:
     void UpdatePosition();
@@ -110,6 +111,8 @@ class VideoDialog : public MythScreenType
     MythMenu* CreateViewMenu();
     MythMenu* CreateSettingsMenu();
     MythMenu* CreateMetadataBrowseMenu();
+
+    void PromptToScan();
 
     void ChangeFilter();
 

commit 6fe9ad066571d2d83600191182123b5dc41152e4

Ensure the default parental level is set before we Load() the video data. This was seemingly always backwards which caused the data to be loaded twice. It was then broken by 7c8e45f which moved the signal connection to the right place but overlooked the misplaced ParentalLevel::SetLevel(). To recap, we want to connect the SigLevelChanged() signal _after_ the initial load otherwise we'd always trigger a second unwanted load.

mythtv/programs/mythfrontend/videodlg.cpp
@@ -852,6 +852,10 @@ VideoDialog::VideoDialog(MythScreenStack *lparent, QString lname,
     m_d->m_videoList->setCurrentVideoFilter(VideoFilterSettings(true,
                     lname));
 
+    m_d->m_parentalLevel.SetLevel(ParentalLevel(gCoreContext->
+                    GetNumSetting("VideoDefaultParentalLevel",
+                            ParentalLevel::plLowest)));
+    
     StorageGroup::ClearGroupToUseCache();
 }
 
@@ -1032,9 +1036,6 @@ bool VideoDialog::Create()
 
 void VideoDialog::Init()
 {
-    m_d->m_parentalLevel.SetLevel(ParentalLevel(gCoreContext->
-                    GetNumSetting("VideoDefaultParentalLevel",
-                            ParentalLevel::plLowest)));
     connect(&m_d->m_parentalLevel, SIGNAL(SigLevelChanged()),
             SLOT(reloadData()));
 }