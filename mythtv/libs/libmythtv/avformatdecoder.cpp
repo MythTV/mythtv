acbf55411aa0b6104a3b17d544e7fbe91d38719a
Jim Stichnoth <jstichnoth@mythtv.org>
2012-05-23 08:59:25 -0700

    Subtitles: Enable forced subtitle tracks.  Refs #10735
    If the subtitle track is marked as forced, mark each individual
    subtitle as forced even if it isn't individually marked in the stream.
    Some mkv rips of DVDs appear to be structured this way.
    (cherry picked from commit d5ce98e26d71500a48e5a40f544abb1fd9ca7684)

--- mythtv/libs/libmythtv/avformatdecoder.cpp
+++ mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -3443,6 +3443,7 @@ bool AvFormatDecoder::ProcessSubtitlePacket(AVStream *curstream, AVPacket *pkt)
 
     avcodeclock->lock();
     int subIdx = selectedTrack[kTrackTypeSubtitle].av_stream_index;
+    bool isForcedTrack = selectedTrack[kTrackTypeSubtitle].forced;
     avcodeclock->unlock();
 
     int gotSubtitles = 0;
@@ -3475,6 +3476,8 @@ bool AvFormatDecoder::ProcessSubtitlePacket(AVStream *curstream, AVPacket *pkt)
 
     if (gotSubtitles)
     {
+        if (isForcedTrack)
+            subtitle.forced = true;
         subtitle.start_display_time += pts;
         subtitle.end_display_time += pts;
         LOG(VB_PLAYBACK | VB_TIMESTAMP, LOG_INFO, LOC +
@@ -3486,7 +3489,7 @@ bool AvFormatDecoder::ProcessSubtitlePacket(AVStream *curstream, AVPacket *pkt)
         bool forcedon = m_parent->GetSubReader(pkt->stream_index)->AddAVSubtitle(
                subtitle, curstream->codec->codec_id == CODEC_ID_XSUB,
                m_parent->GetAllowForcedSubtitles());
-        m_parent->EnableForcedSubtitles(forcedon);
+        m_parent->EnableForcedSubtitles(forcedon || isForcedTrack);
     }
 
     return true;
