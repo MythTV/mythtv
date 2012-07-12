09d9504694eab544f993c0caa8f79872311f1d89
Chris Pinkham <cpinkham@mythtv.org>
2012-07-12 00:53:19 -0700

    Squelch an inuseprograms DB warning during HLS transcode.
    If the file's hostname isn't known when we go to insert into
    inuseprograms, use our own hostname for the rechost column.
    
    References #10520.  Carries over e7a6d7e from master.

--- mythtv/libs/libmyth/programinfo.cpp
+++ mythtv/libs/libmyth/programinfo.cpp
@@ -4180,7 +4180,9 @@ void ProgramInfo::MarkAsInUse(bool inuse, QString usedFor)
         query.bindValue(":HOSTNAME",   gCoreContext->GetHostName());
         query.bindValue(":RECUSAGE",   inUseForWhat);
         query.bindValue(":UPDATETIME", inUseTime);
-        query.bindValue(":RECHOST",    hostname);
+        query.bindValue(":RECHOST",
+                        hostname.isEmpty() ? gCoreContext->GetHostName()
+                                           : hostname);
         query.bindValue(":RECDIR",     recDir);
 
         if (!query.exec())
