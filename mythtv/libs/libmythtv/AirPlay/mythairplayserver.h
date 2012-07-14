a519a4df1608a3f1dec226674777803f5d7bc796
Jean-Yves Avenard <jyavenard@mythtv.org>
2012-07-14 21:50:25 +1000

    Allows multiple sessions to Airplay and RAOP
    This is done by stopping the one currently running or stopping any current playback.
    
    This change allows to always be able to start airplay and not silently fail should a playback be currently going. If a current playback is currently going (either RAOP, AirPlay or standard playback), it will be stopped and a new session will be automatically started.
    AirPlay now waits synchronously for any events to be completed: this result in much smoother user interaction and also follow Apple's user experience guidelines with their MFi vendors.
    
    A side effect is that since iOS 5.1, a typical AirPlay session would open both a RAOP session and an AirPlay one, to drop the RAOP connection a few seconds later. This caused the audio card to be already in use for AirPlay and resulted in no audio during playback
    
    There is an issue with iOS devices when using RAOP, they don't detect that the connection got interrupted and continue sending data. iTunes behaves properly.
    
    Fixes #10885

--- mythtv/libs/libmythtv/AirPlay/mythairplayserver.h
+++ mythtv/libs/libmythtv/AirPlay/mythairplayserver.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QUrl>
+#include <stdint.h>     // for uintxx_t
 
 #include "serverpool.h"
 #include "mythtvexp.h"
@@ -14,6 +15,9 @@ class BonjourRegister;
 #define AIRPLAY_PORT_RANGE 100
 #define AIRPLAY_HARDWARE_ID_SIZE 6
 QString AirPlayHardwareId();
+
+#define AIRPLAY_NEWCONNECTION "AIRPLAY_NEWCONNECTION"
+#define AIRPLAY_DISCONNECTED  "AIRPLAY_DISCONNECTED"
 
 enum AirplayEvent
 {
@@ -30,7 +34,8 @@ class AirplayConnection
     AirplayConnection()
       : controlSocket(NULL), reverseSocket(NULL), speed(1.0f),
         position(0.0f), initial_position(-1.0f), url(QUrl()),
-        lastEvent(AP_EVENT_NONE), stopped(false), was_playing(false)
+        lastEvent(AP_EVENT_NONE), stopped(false), was_playing(false),
+        initialized(false)
     { }
     QTcpSocket  *controlSocket;
     QTcpSocket  *reverseSocket;
@@ -41,6 +46,7 @@ class AirplayConnection
     AirplayEvent lastEvent;
     bool         stopped;
     bool         was_playing;
+    bool         initialized;
 };
 
 class APHTTPRequest;
@@ -52,14 +58,19 @@ class MTV_PUBLIC MythAirplayServer : public ServerPool
   public:
     static bool Create(void);
     static void Cleanup(void);
+    static MythAirplayServer *AirplaySharedInstance(void)
+    { return gMythAirplayServer; }
 
     MythAirplayServer();
+
+  public slots:
+    void GotNewConnection(void);
 
   private slots:
     void Start();
     void newConnection(QTcpSocket *client);
     void deleteConnection();
-    void read();
+    void read(void);
 
   private:
     virtual ~MythAirplayServer(void);
@@ -67,13 +78,23 @@ class MTV_PUBLIC MythAirplayServer : public ServerPool
     void HandleResponse(APHTTPRequest *req, QTcpSocket *socket);
     QByteArray StatusToString(int status);
     QString eventToString(AirplayEvent event);
-    void GetPlayerStatus(bool &playing, float &speed, double &position,
-                         double &duration);
+    void GetPlayerStatus(bool &playing, float &speed,
+                         double &position, double &duration,
+                         QString &pathname);
     QString GetMacAddress();
     bool SendReverseEvent(QByteArray &session, AirplayEvent event);
     void SendResponse(QTcpSocket *socket,
                       int status, QByteArray header,
                       QByteArray content_type, QString body);
+
+    void deleteConnection(QTcpSocket *socket);
+    void DisconnectAllClients(const QByteArray &session);
+    void StopSession(const QByteArray &session);
+    void StartPlayback(const QString &pathname);
+    void StopPlayback(void);
+    void SeekPosition(uint64_t position);
+    void PausePlayback(void);
+    void UnpausePlayback(void);
 
     // Globals
     static MythAirplayServer *gMythAirplayServer;
@@ -88,6 +109,7 @@ class MTV_PUBLIC MythAirplayServer : public ServerPool
     int              m_setupPort;
     QList<QTcpSocket*> m_sockets;
     QHash<QByteArray,AirplayConnection> m_connections;
+    QString          m_pathname;
 };
 
 #endif // MYTHAIRPLAYSERVER_H
