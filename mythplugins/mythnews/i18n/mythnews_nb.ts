bf557a8adf77287143164aba06d2029ee32245db
Rune Evjen <rune.evjen@gmail.com>
2012-07-22 13:33:08 -0400

    Updated Normwegian Bokmal MythFrontend, MythGame, MythMusic, MythNews and MythWeather Translation from Rune Evjen
    Thank you Rune!
    
    Refs #10929
    
    Signed-off-by: Nicolas Riendeau <nriendeau@mythtv.org>

--- mythplugins/mythnews/i18n/mythnews_nb.ts
+++ mythplugins/mythnews/i18n/mythnews_nb.ts
@@ -37,6 +37,11 @@
         <translation>Ukjent</translation>
     </message>
     <message>
+        <location filename="../mythnews/mythnews.cpp" line="703"/>
+        <source>Downloading media...</source>
+        <translation>Laster ned media...</translation>
+    </message>
+    <message>
         <location filename="../mythnews/mythnews.cpp" line="900"/>
         <source>Manage Feeds</source>
         <translation>Administrer matinger</translation>
@@ -140,34 +145,34 @@
 <context>
     <name>NewsSite</name>
     <message>
-        <location filename="../mythnews/newssite.cpp" line="218"/>
+        <location filename="../mythnews/newssite.cpp" line="227"/>
         <source>Retrieve Failed. </source>
         <translation>Klarte ikke å hente. </translation>
     </message>
     <message>
-        <location filename="../mythnews/newssite.cpp" line="225"/>
         <location filename="../mythnews/newssite.cpp" line="234"/>
         <location filename="../mythnews/newssite.cpp" line="243"/>
+        <location filename="../mythnews/newssite.cpp" line="252"/>
         <source>Failed to retrieve news</source>
         <translation>Klarte ikke å hente nyheter</translation>
     </message>
     <message>
-        <location filename="../mythnews/newssite.cpp" line="226"/>
+        <location filename="../mythnews/newssite.cpp" line="235"/>
         <source>No Cached News.</source>
         <translation>Ingen nyheter i hurtiglager.</translation>
     </message>
     <message>
-        <location filename="../mythnews/newssite.cpp" line="245"/>
+        <location filename="../mythnews/newssite.cpp" line="254"/>
         <source>Failed to read downloaded file.</source>
         <translation>Kunne ikke lese nedlastet fil.</translation>
     </message>
     <message>
-        <location filename="../mythnews/newssite.cpp" line="253"/>
+        <location filename="../mythnews/newssite.cpp" line="262"/>
         <source>Showing Cached News.</source>
         <translation>Viser nyheter fra hurtiglager.</translation>
     </message>
     <message>
-        <location filename="../mythnews/newssite.cpp" line="274"/>
+        <location filename="../mythnews/newssite.cpp" line="283"/>
         <source>XML-file is not valid RSS-feed</source>
         <translation>XML-filen er ikke en gyldig RSS mating</translation>
     </message>
@@ -175,9 +180,8 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../mythnews/mythnews.cpp" line="703"/>
         <source>Downloading media...</source>
-        <translation>Laster ned media...</translation>
+        <translation type="obsolete">Laster ned media...</translation>
     </message>
 </context>
 <context>
@@ -188,12 +192,12 @@
         <translation>Avbryt</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="9"/>
+        <location filename="themestrings.h" line="10"/>
         <source>Edit RSS News Feeds</source>
         <translation>Rediger RSS nyhetsmatinger</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="10"/>
+        <location filename="themestrings.h" line="11"/>
         <source>You can edit the name, URL, and icon path for the selected RSS Feed on this screen.  You can also designate the item as a podcast.
 
 When you have finished editing, select &quot;OK&quot; or &quot;Cancel&quot; to continue.</source>
@@ -202,32 +206,32 @@ When you have finished editing, select &quot;OK&quot; or &quot;Cancel&quot; to c
 Når du er ferdig å redigere, velg &quot;OK&quot; eller &quot;Avbryt&quot; for å fortsette.</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="11"/>
+        <location filename="themestrings.h" line="12"/>
         <source>Name:</source>
         <translation>Navn:</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="12"/>
+        <location filename="themestrings.h" line="13"/>
         <source>URL:</source>
         <translation>URL:</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="13"/>
+        <location filename="themestrings.h" line="14"/>
         <source>Icon:</source>
         <translation>Ikon:</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="14"/>
+        <location filename="themestrings.h" line="15"/>
         <source>Podcast:</source>
         <translation>Podkast:</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="15"/>
+        <location filename="themestrings.h" line="16"/>
         <source>Genre:</source>
         <translation>Sjanger:</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="16"/>
+        <location filename="themestrings.h" line="17"/>
         <source>Channel:</source>
         <translation>Kanal:</translation>
     </message>
@@ -242,12 +246,12 @@ Når du er ferdig å redigere, velg &quot;OK&quot; eller &quot;Avbryt&quot; for 
         <translation>Bokmerkebehandler</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="17"/>
+        <location filename="themestrings.h" line="18"/>
         <source>News Subscriptions</source>
         <translation>Nyhetsabbonementer</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="18"/>
+        <location filename="themestrings.h" line="19"/>
         <source>Choose the news sites you would like to read.</source>
         <translation>Velg hvilke nyhetssiter du vil lese.</translation>
     </message>
@@ -275,17 +279,17 @@ Når du er ferdig å redigere, velg &quot;OK&quot; eller &quot;Avbryt&quot; for 
         <translation type="obsolete">Minutter</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="19"/>
+        <location filename="themestrings.h" line="20"/>
         <source>News Feeds</source>
         <translation>Nyhetsmatinger</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="8"/>
+        <location filename="themestrings.h" line="9"/>
         <source>%DATE%, %TIME%</source>
         <translation>%DATE%, %TIME%</translation>
     </message>
     <message>
-        <location filename="themestrings.h" line="20"/>
+        <location filename="themestrings.h" line="8"/>
         <source>News Settings</source>
         <translation>Innstillinger for nyhetssted</translation>
     </message>
