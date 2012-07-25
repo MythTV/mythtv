cbf4d8dacb7ce21b3c45a495f7ded7f46c6d2c10
Stuart Morgan <smorgan@mythtv.org>
2012-07-25 18:17:32 +0100

    Fix for Synaesthesia memory corruption and segfault

--- mythplugins/mythmusic/mythmusic/polygon.h
+++ mythplugins/mythmusic/mythmusic/polygon.h
@@ -15,12 +15,12 @@ struct Bitmap {
     delete[] data;
     width = w;
     height = h;
-    data = new Pixel[w*h+extra];
+    data = new Pixel[2*w*h+extra];
     clear();
   }
   
   void clear() {
-    memset(data,0,sizeof(Pixel)*(width*height+extra));
+    memset(data,0,sizeof(Pixel)*(2*width*height+extra));
   }
 };
 #endif
