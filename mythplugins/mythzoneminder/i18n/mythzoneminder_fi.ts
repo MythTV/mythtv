7dadee04c8ec2b3eef830e270f39f5133f504205
Ilkka Tengvall <ilkka.tengvall@iki.fi>
2012-08-15 21:23:00 +0200

    Update the Finnish translation of mythfrontend and plugins.
    Fixes #11007.
    
    Signed-off-by: Kenni Lund <kenni@mythtv.org>

--- mythplugins/mythzoneminder/i18n/mythzoneminder_fi.ts
+++ mythplugins/mythzoneminder/i18n/mythzoneminder_fi.ts
@@ -4,6 +4,7 @@
 <context>
     <name>FunctionDialog</name>
     <message>
+        <location filename="../mythzoneminder/zmconsole.cpp" line="70"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
@@ -11,14 +12,17 @@
 <context>
     <name>MythControls</name>
     <message>
+        <location filename="../mythzoneminder/main.cpp" line="130"/>
         <source>ZoneMinder Console</source>
         <translation>ZoneMinder Konsoli</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/main.cpp" line="132"/>
         <source>ZoneMinder Live View</source>
         <translation>ZoneMinder Livekuva</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/main.cpp" line="134"/>
         <source>ZoneMinder Events</source>
         <translation>ZoneMinder Tapahtumat</translation>
     </message>
@@ -26,54 +30,57 @@
 <context>
     <name>QObject</name>
     <message>
-        <source>IP address of the mythzoneminder server</source>
-        <translation type="obsolete">Mythzoneminder palvelimen IP osoite</translation>
-    </message>
-    <message>
-        <source>Enter the IP address of the mythzoneminder server that this frontend should connect to.</source>
-        <translation type="obsolete">Aseta mythzoneminder palvelimen IP osoite johon tämän käyttöliittymän tulisi kytkeytyä.</translation>
-    </message>
-    <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="18"/>
         <source>IP address of the MythZoneMinder server</source>
         <translation>Mythzoneminder palvelimen IP osoite</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="20"/>
         <source>Enter the IP address of the MythZoneMinder server that this frontend should connect to.</source>
         <translation>Aseta mythzoneminder palvelimen IP osoite johon tämän käyttöliittymän tulisi kytkeytyä.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="28"/>
         <source>Port the server runs on</source>
         <translation>Palvelimen portti</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="30"/>
         <source>Unless you&apos;ve got good reason to, don&apos;t change this.</source>
         <translation>Älä muuta tätä ilman hyvää syytä.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="38"/>
         <source>Date format</source>
         <translation>Päiväyksen Muoto</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="42"/>
         <source>Samples are shown using today&apos;s date.</source>
         <translation>Näytteitä näytetään käyttämällä tämän päivän päivämäärää.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="48"/>
         <source>Samples are shown using tomorrow&apos;s date.</source>
         <translation>Näytteitä näytetään käyttämälää huomisen päivämäärää.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="64"/>
         <source>Your preferred date format to use on the events screens.</source>
         <translation>Suosimasi päiväyksen muoto tapahtumat -sivulla.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="72"/>
         <source>Time format</source>
         <translation>Ajan Esitys</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="80"/>
         <source>Your preferred time format to display on the events screens.</source>
         <translation>Suosimasi näytettä päiväyksen muoto tapahtumat -sivulla.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmsettings.cpp" line="88"/>
         <source>MythZoneMinder Settings</source>
         <translation>MythZoneMinder Asetukset</translation>
     </message>
@@ -81,208 +88,239 @@
 <context>
     <name>ThemeUI</name>
     <message>
+        <location filename="themestrings.h" line="5"/>
         <source>Edit Monitor Function</source>
         <translation>Muokkaa Monitoria</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="6"/>
         <source>Enabled</source>
         <translation>Päällä</translation>
     </message>
     <message>
-        <source>Zone Minder Console</source>
-        <translation type="obsolete">Zone Minder Konsoli</translation>
-    </message>
-    <message>
+        <location filename="themestrings.h" line="8"/>
         <source>Status</source>
         <translation>Tila</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="9"/>
         <source>Load: 0.12</source>
         <translation>Kuorma: 0.12</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="10"/>
         <source>Disk: 100%</source>
         <translation>Levy: 100%</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="11"/>
         <source>Camera</source>
         <translation>Kamera</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="12"/>
         <source>Function</source>
         <translation>Toiminto</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="13"/>
         <source>Source</source>
         <translation>Lähde</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="14"/>
         <source>Events</source>
         <translation>Tapahtumat</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="26"/>
         <source>ZoneMinder Event Player</source>
         <translation>Zone Minder tapahtumien toistin</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="36"/>
         <source>ZoneMinder Live Player</source>
         <translation>Zone Minder live toisto</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="48"/>
         <source>[x]</source>
         <translation>[x]</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="15"/>
         <source>[R] = Running [S] = Stopped</source>
         <translation>[R] = Käynnissä [S] = Pysäytetty</translation>
     </message>
     <message>
-        <source>Zone Minder Events</source>
-        <translation type="obsolete">Zone Minder Tapahtumat</translation>
-    </message>
-    <message>
+        <location filename="themestrings.h" line="7"/>
         <source>ZoneMinder Console</source>
         <translation>ZoneMinder konsoli</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="16"/>
         <source>ZoneMinder Events</source>
         <translation>ZoneMinder tapahtumat</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="17"/>
         <source>Select Camera</source>
         <translation>Valitse Kamera</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="18"/>
         <source>Select Date</source>
         <translation>Valitse Päivämäärä</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="19"/>
         <source>0/0</source>
         <translation>0/0</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="20"/>
         <source>Event Name</source>
         <translation>Tapahtuman Nimi</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="21"/>
         <source>Date/Time</source>
         <translation>Pvm/Aika</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="22"/>
         <source>Length</source>
         <translation>Pituus</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="23"/>
         <source>Name</source>
         <translation>Nimi</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="24"/>
         <source>Date</source>
         <translation>Päivämäärä</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="25"/>
         <source>Duration</source>
         <translation>Kesto</translation>
     </message>
     <message>
-        <source>Zone Minder Event Player</source>
-        <translation type="obsolete">Zone Minder Tapahtumien Toistin</translation>
-    </message>
-    <message>
+        <location filename="themestrings.h" line="27"/>
         <source>Event:</source>
         <translation>Tapahtuma:</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="28"/>
         <source>Event Name (1/123)</source>
         <translation>Tapahtuman Nimi (1/123)</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="29"/>
         <source>Camera:</source>
         <translation>Kamera:</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="30"/>
         <source>Camera Name</source>
         <translation>Kameran Nimi</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="31"/>
         <source>Frame:</source>
         <translation>Kehys:</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="32"/>
         <source>1/123</source>
         <translation>1/123</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="33"/>
         <source>Date:</source>
         <translation>Päivämäärä:</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="34"/>
         <source>Mon 01 Nov 2006 12:34</source>
         <translation>Maa 01 Mar 2006 12:34</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="35"/>
         <source>There are no events to show</source>
         <translation>Ei ole tapahtumia näytettäväksi</translation>
     </message>
     <message>
-        <source>Zone Minder Live Player</source>
-        <translation type="obsolete">Zone Minder Live Esitys</translation>
-    </message>
-    <message>
+        <location filename="themestrings.h" line="37"/>
         <source>Monitor 1</source>
         <translation>Monitori 1</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="38"/>
         <source>Monitor 2</source>
         <translation>Monitori 2</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="39"/>
         <source>Monitor 3</source>
         <translation>Monitori 3</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="40"/>
         <source>Monitor 4</source>
         <translation>Monitori 4</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="49"/>
         <source>Show Console</source>
         <translation>Näytä Konsoli</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="50"/>
         <source>Show Live View</source>
         <translation>Näytä Livenäkymä</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="51"/>
         <source>Show Events</source>
         <translation>Näytä Tapahtumat</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="41"/>
         <source>[R] = Running
 [S] = Stopped</source>
         <translation>[R] = Käynnissä
 [S] = Pysäytetty</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="42"/>
         <source>Select Camera:</source>
         <translation>Valitse kamera:</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="43"/>
         <source>Select Date:</source>
         <translation>Valitse päivämäärä:</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="44"/>
         <source>Test</source>
         <translation>Kokeile</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="45"/>
         <source>Record</source>
         <translation>Tallenna</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="46"/>
         <source>Cancel</source>
-        <translation></translation>
+        <translation>Peru</translation>
     </message>
     <message>
+        <location filename="themestrings.h" line="47"/>
         <source>Store</source>
         <translation>Tallenna</translation>
     </message>
@@ -290,10 +328,12 @@
 <context>
     <name>ZMClient</name>
     <message>
+        <location filename="../mythzoneminder/zmclient.cpp" line="117"/>
         <source>Cannot connect to the mythzmserver - Is it running? Have you set the correct IP and port in the settings?</source>
         <translation>Ei voida yhdistää mythzmpalvelimeen - Onko se päällä? Oletko asettanut IP osoitteen ja portin oikein asetuksissa?</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmclient.cpp" line="194"/>
         <source>The mythzmserver didn&apos;t respond to our request to get the protocol version!!</source>
         <translation>Mythzmpalvelin ei vastannut protokollaversiokyselyysi!!</translation>
     </message>
@@ -301,10 +341,12 @@
 <context>
     <name>ZMConsole</name>
     <message>
+        <location filename="../mythzoneminder/zmconsole.cpp" line="212"/>
         <source>Running</source>
         <translation>Ajossa</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmconsole.cpp" line="217"/>
         <source>Stopped</source>
         <translation>Pysäytetty</translation>
     </message>
@@ -312,38 +354,50 @@
 <context>
     <name>ZMEvents</name>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="96"/>
         <source>Play</source>
         <translation>Toista</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="103"/>
         <source>Delete</source>
         <translation>Poista</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="179"/>
+        <location filename="../mythzoneminder/zmevents.cpp" line="354"/>
+        <location filename="../mythzoneminder/zmevents.cpp" line="369"/>
         <source>All Cameras</source>
         <translation>Kaikki kamerat</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="182"/>
+        <location filename="../mythzoneminder/zmevents.cpp" line="378"/>
         <source>All Dates</source>
         <translation>Kaikki päivämäärät</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="454"/>
         <source>Refresh</source>
         <translation>Päivitä</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="455"/>
         <source>Change View</source>
         <translation>Vaihda Näkymää</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="456"/>
         <source>Delete All</source>
         <translation>Poista Kaikki</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="468"/>
         <source>Delete All Events?</source>
         <translation>Poistetaanko kaikki tapahtumat?</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmevents.cpp" line="469"/>
         <source>Deleting %1 events in this view.</source>
         <translation>Poistetaan %1 tapahtumaa tästä näkymästä.</translation>
     </message>
@@ -351,16 +405,12 @@
 <context>
     <name>ZMLivePlayer</name>
     <message>
-        <source>Can&apos;t show live view.
-You don&apos;t have any monitors defined!</source>
-        <translation type="obsolete">Ei voi näyttää Live näkymää.
-Et ole määrittänyt yhtään monitoria!</translation>
-    </message>
-    <message>
+        <location filename="../mythzoneminder/zmliveplayer.cpp" line="124"/>
         <source>Can&apos;t show live view.</source>
         <translation>Ei voida näyttää live näkymää.</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmliveplayer.cpp" line="125"/>
         <source>You don&apos;t have any monitors defined!</source>
         <translation>Et ole määritellyt yhtään monitoria!</translation>
     </message>
@@ -368,22 +418,29 @@ Et ole määrittänyt yhtään monitoria!</translation>
 <context>
     <name>ZMPlayer</name>
     <message>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="107"/>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="291"/>
         <source>Pause</source>
         <translation>Paussi</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="113"/>
         <source>Delete</source>
         <translation>Poista</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="119"/>
         <source>Previous</source>
         <translation>Edellinen</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="125"/>
         <source>Next</source>
         <translation>Seuraava</translation>
     </message>
     <message>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="298"/>
+        <location filename="../mythzoneminder/zmplayer.cpp" line="377"/>
         <source>Play</source>
         <translation>Toista</translation>
     </message>
