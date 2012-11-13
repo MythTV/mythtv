c57b92740ac15e708bd8a68eb767b484d8b26f4a
David Engel <dengel@mythtv.org>
2012-11-13 09:47:48 -0600

    Fix a possible reace condition when a recording finishes.
    There was a slight, albeit very unlikely, possibility the scheduler
    could run before the recorded table was updated.  This avoids it by
    updating the database before notifying the scheduler.
    
    Refs #11224
    (cherry picked from commit 729e3d56d3e984d35b4fecd249da7c3e1bdcbfb9)

--- mythtv/libs/libmythtv/tv_rec.cpp
+++ mythtv/libs/libmythtv/tv_rec.cpp
@@ -918,6 +918,9 @@ void TVRec::FinishedRecording(RecordingInfo *curRec, RecordingQuality *recq)
         PreviewGeneratorQueue::GetPreviewImage(*curRec, "");
     }
 
+    // store recording in recorded table
+    curRec->FinishedRecording(!is_good || (recgrp == "LiveTV"));
+
     // send out UPDATE_RECORDING_STATUS message
     if (recgrp != "LiveTV")
     {
@@ -929,9 +932,6 @@ void TVRec::FinishedRecording(RecordingInfo *curRec, RecordingQuality *recq)
                      .arg(curRec->GetRecordingEndTime(MythDate::ISODate)));
         gCoreContext->dispatch(me);
     }
-
-    // store recording in recorded table
-    curRec->FinishedRecording(!is_good || (recgrp == "LiveTV"));
 
     // send out REC_FINISHED message
     SendMythSystemRecEvent("REC_FINISHED", curRec);
