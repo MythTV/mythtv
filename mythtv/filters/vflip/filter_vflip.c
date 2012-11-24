MythTV/mythtv

commit ee0ebae7593f42b239441290b83bd3a829864bd9

Add explicit const casts in filters code to quiet some gcc warnings.

mythtv/filters/vflip/filter_vflip.c
@@ -140,12 +140,12 @@ static FmtConv FmtList[] =
     FMT_NULL
 };
 
-const FilterInfo filter_table[] =
+const FilterInfo filter_table_priv[] =
 {
     {
         filter_init: &new_filter,
-        name:       "vflip",
-        descript:   "flips the video image vertically",
+        name:       (char*)"vflip",
+        descript:   (char*)"flips the video image vertically",
         formats:    FmtList,
         libname:    NULL
     },

mythtv/filters/adjust/filter_adjust.c
@@ -316,8 +316,8 @@ const FilterInfo filter_table[] =
 {
     {
         filter_init: &newAdjustFilter,
-        name:       "adjust",
-        descript:   "adjust range and gamma of video",
+        name:       (char*)"adjust",
+        descript:   (char*)"adjust range and gamma of video",
         formats:    FmtList,
         libname:    NULL
     },

mythtv/filters/bobdeint/filter_bobdeint.c
@@ -144,8 +144,10 @@ const FilterInfo filter_table[] =
 {
     {
         filter_init: &new_filter,
-        name:       "bobdeint",
-        descript:   "bob deinterlace filter; splits fields to top and bottom of buffer",
+        name:       (char*)"bobdeint",
+        descript:   (char*)
+        "bob deinterlace filter; "
+        "splits fields to top and bottom of buffer",
         formats:    FmtList,
         libname:    NULL,
     },

mythtv/filters/crop/filter_crop.c
@@ -304,8 +304,8 @@ const FilterInfo filter_table[] =
 {
     {
         filter_init: &new_filter,
-        name:       "crop",
-        descript:   "crops picture by macroblock intervals",
+        name:       (char*)"crop",
+        descript:   (char*)"crops picture by macroblock intervals",
         formats:    FmtList,
         libname:    NULL
     },

mythtv/filters/denoise3d/filter_denoise3d.c
@@ -475,8 +475,9 @@ const FilterInfo filter_table[] =
 {
     {
         filter_init: &NewDenoise3DFilter,
-        name:       "denoise3d",
-        descript:   "removes noise with a spatial and temporal low-pass filter",
+        name:       (char*)"denoise3d",
+        descript:   (char*)
+        "removes noise with a spatial and temporal low-pass filter",
         formats:    FmtList,
         libname:    NULL
     },

mythtv/filters/fieldorder/filter_fieldorder.c
@@ -230,8 +230,8 @@ const FilterInfo filter_table[] =
 {
     {
         filter_init: &FieldorderDeintFilter,
-        name:       "fieldorderdoubleprocessdeint",
-        descript:   "avoids synchronisation problems when matching an "
+        name:       (char*)"fieldorderdoubleprocessdeint",
+        descript:   (char*)"avoids synchronisation problems when matching an "
                     "interlaced video mode to an interlaced source",
         formats:    FmtList,
         libname:    NULL

mythtv/filters/force/filter_force.c
@@ -99,29 +99,29 @@ const FilterInfo filter_table[] =
 {
     {
         filter_init: &new_force_yv12,
-        name:       "forceyv12",
-        descript:   "forces use of YV12 video format",
+        name:       (char*)"forceyv12",
+        descript:   (char*)"forces use of YV12 video format",
         formats:    Fmt_List_YV12,
         libname:    NULL
     },
     {
         filter_init: &new_force_yuv422p,
-        name:       "forceyuv422p",
-        descript:   "forces use of YUV422P video format",
+        name:       (char*)"forceyuv422p",
+        descript:   (char*)"forces use of YUV422P video format",
         formats:    Fmt_List_YUV422P,
         libname:    NULL
     },
     {
         filter_init: &new_force_rgb24,
-        name:       "forcergb24",
-        descript:   "forces use of RGB24 video format",
+        name:       (char*)"forcergb24",
+        descript:   (char*)"forces use of RGB24 video format",
         formats:    Fmt_List_RGB24,
         libname:    NULL
     },
     {
         filter_init: &new_force_argb32,
-        name:       "forceargb32",
-        descript:   "forces use of ARGB32 video format",
+        name:       (char*)"forceargb32",
+        descript:   (char*)"forces use of ARGB32 video format",
         formats:    Fmt_List_ARGB32,
         libname:    NULL
     },

mythtv/filters/greedyhdeint/filter_greedyhdeint.c
@@ -277,15 +277,15 @@ const FilterInfo filter_table[] =
 {
     {
             filter_init: &GreedyHDeintFilter,
-            name:       "greedyhdeint",
-            descript:   "combines data from several fields to deinterlace with less motion blur",
+            name:       (char*)"greedyhdeint",
+            descript:   (char*)"combines data from several fields to deinterlace with less motion blur",
             formats:    FmtList,
             libname:    NULL
     },
     {
             filter_init: &GreedyHDeintFilter,
-            name:       "greedyhdoubleprocessdeint",
-            descript:   "combines data from several fields to deinterlace with less motion blur",
+            name:       (char*)"greedyhdoubleprocessdeint",
+            descript:   (char*)"combines data from several fields to deinterlace with less motion blur",
             formats:    FmtList,
             libname:    NULL
     },FILT_NULL

commit 3cbd9d9200fde990c97ee296755f2b81b124e655

Fixes #11173.  Rename filter_table_priv back to filter_table.
Bug accidentally introduced in ee0ebae7593f42b239441290b83bd3a829864bd9

mythtv/filters/vflip/filter_vflip.c
@@ -140,7 +140,7 @@ static FmtConv FmtList[] =
     FMT_NULL
 };
 
-const FilterInfo filter_table_priv[] =
+const FilterInfo filter_table[] =
 {
     {
         filter_init: &new_filter,