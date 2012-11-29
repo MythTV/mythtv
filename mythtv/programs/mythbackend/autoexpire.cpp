8a7892b1ca1ec307d32a78153b57e012a721e9d3
Ian Dall <ian@beware.dropbear.id.au>
2012-11-29 19:00:10 +0000

    Fix deletes on Slave backends. Remove references to deleted_set and initialise dont_expire_set properly which conflicted with 'deleted' recgroup behaviour since 0.25. Fixes #10704
    Signed-off-by: Stuart Morgan <smorgan@mythtv.org>
    (cherry picked from commit e150a19030a81e3c099eff76b280b886122529b1)

--- mythtv/programs/mythbackend/autoexpire.cpp
+++ mythtv/programs/mythbackend/autoexpire.cpp
@@ -645,8 +645,6 @@ void AutoExpire::SendDeleteMessages(pginfolist_t &deleteList)
                      .arg((*it)->GetRecordingStartTime(MythDate::ISODate)));
         gCoreContext->dispatch(me);
 
-        deleted_set.insert((*it)->MakeUniqueKey());
-
         ++it; // move on to next program
     }
 }
@@ -1104,7 +1102,7 @@ void AutoExpire::Update(int encoder, int fsID, bool immediately)
 
 void AutoExpire::UpdateDontExpireSet(void)
 {
-    dont_expire_set = deleted_set;
+    dont_expire_set.clear();
 
     MSqlQuery query(MSqlQuery::InitCon());
     query.prepare(
