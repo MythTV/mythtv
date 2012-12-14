1eaecea6dd38db0c20b94f8ecb549b4c29491297
Richard <peper03@yahoo.com>
2012-12-14 20:59:53 +0100

    Use the right highlight area in DVD menus when 'wide' is not the first in the list.
    Signed-off-by: Stuart Morgan <smorgan@mythtv.org>
    
    Fixes #11291

--- mythtv/libs/libmythtv/DVD/dvdringbuffer.cpp
+++ mythtv/libs/libmythtv/DVD/dvdringbuffer.cpp
@@ -1430,7 +1430,7 @@ bool DVDRingBuffer::DVDButtonUpdate(bool b_mode)
     dvdnav_highlight_area_t hl;
     dvdnav_get_current_highlight(m_dvdnav, &button);
     pci = dvdnav_get_current_nav_pci(m_dvdnav);
-    dvdRet = dvdnav_get_highlight_area(pci, button, b_mode, &hl);
+    dvdRet = dvdnav_get_highlight_area_from_group(pci, DVD_BTN_GRP_Wide, button, b_mode, &hl);
 
     if (dvdRet == DVDNAV_STATUS_ERR)
         return false;
