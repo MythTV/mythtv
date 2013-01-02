MythTV/mythtv

commit 49dbed5be0729b04a5f0fd0426a32fceb2dd7935

Provide accurate position/duration/seeking with non-constant framerates.
The recordedseek and filemarkup tables are enhanced to hold timestamp
data in addition to the existing file offset data.  The millisecond
timestamps are produced by all recorders that subclass DTVRecorder, as
well as mythtranscode and mythcommflag --rebuild.  These timestamps
are relative to the start of the recording/video.

A new command is added to the myth protocol, "QUERY_RECORDER
FILL_DURATION_MAP", modeled after FILL_POSITION_MAP, to send updated
timestamp info for playback of an in-progress recording.

The timestamp markup is used during playback to give accurate position
and duration information in the OSD wherever possible, and to provide
accurate time-based seeking, such as "skip forward 30 seconds" or
"jump to the 5-minute mark".  Timestamps are linearly interpolated
from a frame's nearest neighbors in the map, and extrapolated based on
the current frame rate when the map is missing (e.g. legacy
recordings) or incomplete (e.g. in-progress recordings).  Other than
that, the frame rate is not used for seeking or duration calculations.
(With the exception of a handful of areas that still need some
attention, including seeking based on commskipmap, seeking across
program boundaries during Live TV, and the watched flag calculation.)

The cutlist continues to be taken into account for seeking and
displaying timestamps, for the most part making cutlists
indistinguishable from the result of lossless transcoding.

Note that to get the benefit of these changes for preexisting
recordings, it may be necessary to run "mythcommflag --rebuild" on
such recordings.

Bumps the ABI and protocol versions.  "make distclean" is recommended.

Fixes #10104.

mythtv/libs/libmythtv/tv_play.cpp
@@ -4214,14 +4214,15 @@ bool TV::ActiveHandleAction(PlayerContext *ctx,
     {
         ctx->LockDeletePlayer(__FILE__, __LINE__);
         uint64_t bookmark  = ctx->player->GetBookmark();
-        float     rate     = ctx->player->GetFrameRate();
-        float seekloc = ctx->player->TranslatePositionAbsToRel(bookmark) / rate;
         ctx->UnlockDeletePlayer(__FILE__, __LINE__);
 
-        if (bookmark > rate)
-            DoSeek(ctx, seekloc, tr("Jump to Bookmark"),
-                   /*timeIsOffset*/false,
-                   /*honorCutlist*/true);
+        if (bookmark)
+        {
+            DoPlayerSeekToFrame(ctx, bookmark);
+            ctx->LockDeletePlayer(__FILE__, __LINE__);
+            UpdateOSDSeekMessage(ctx, tr("Jump to Bookmark"), kOSDTimeout_Med);
+            ctx->UnlockDeletePlayer(__FILE__, __LINE__);
+        }
     }
     else if (has_action(ACTION_JUMPSTART,actions))
     {
@@ -5046,7 +5047,7 @@ void TV::ProcessNetworkControlCommand(PlayerContext *ctx,
             if (ctx->player)
             {
                 fplay = ctx->player->GetFramesPlayed();
-                rate  = ctx->player->GetFrameRate();
+                rate  = ctx->player->GetFrameRate(); // for display only
             }
             ctx->UnlockDeletePlayer(__FILE__, __LINE__);
 
@@ -6130,6 +6131,37 @@ bool TV::DoPlayerSeek(PlayerContext *ctx, float time)
     return res;
 }
 
+bool TV::DoPlayerSeekToFrame(PlayerContext *ctx, uint64_t target)
+{
+    if (!ctx || !ctx->buffer)
+        return false;
+
+    LOG(VB_PLAYBACK, LOG_INFO, LOC +
+        QString("DoPlayerSeekToFrame %1").arg(target));
+
+    ctx->LockDeletePlayer(__FILE__, __LINE__);
+    if (!ctx->player)
+    {
+        ctx->UnlockDeletePlayer(__FILE__, __LINE__);
+        return false;
+    }
+
+    if (!ctx->buffer->IsSeekingAllowed())
+    {
+        ctx->UnlockDeletePlayer(__FILE__, __LINE__);
+        return false;
+    }
+
+    if (ctx == GetPlayer(ctx, 0))
+        PauseAudioUntilBuffered(ctx);
+
+    bool res = ctx->player->JumpToFrame(target);
+
+    ctx->UnlockDeletePlayer(__FILE__, __LINE__);
+
+    return res;
+}
+
 bool TV::SeekHandleAction(PlayerContext *actx, const QStringList &actions,
                           const bool isDVD)
 {
@@ -6162,18 +6194,24 @@ bool TV::SeekHandleAction(PlayerContext *actx, const QStringList &actions,
     {
         if (!isDVD)
         {
-            float rate = 30.0f;
-            actx->LockDeletePlayer(__FILE__, __LINE__);
-            if (actx->player)
-                rate = actx->player->GetFrameRate();
-            actx->UnlockDeletePlayer(__FILE__, __LINE__);
-            float time = (flags & kAbsolute) ?  direction :
-                             direction * (1.001 / rate);
             QString message = (flags & kRewind) ? tr("Rewind") :
                                                   tr("Forward");
-            DoSeek(actx, time, message,
-                   /*timeIsOffset*/true,
-                   /*honorCutlist*/!(flags & kIgnoreCutlist));
+            actx->LockDeletePlayer(__FILE__, __LINE__);
+            uint64_t frameAbs = actx->player->GetFramesPlayed();
+            uint64_t frameRel =
+                actx->player->TranslatePositionAbsToRel(frameAbs);
+            uint64_t targetRel = frameRel + direction;
+            if (frameRel == 0 && direction < 0)
+                targetRel = 0;
+            uint64_t maxAbs = actx->player->GetCurrentFrameCount();
+            uint64_t maxRel = actx->player->TranslatePositionAbsToRel(maxAbs);
+            if (targetRel > maxRel)
+                targetRel = maxRel;
+            uint64_t targetAbs =
+                actx->player->TranslatePositionRelToAbs(targetRel);
+            actx->UnlockDeletePlayer(__FILE__, __LINE__);
+            DoPlayerSeekToFrame(actx, targetAbs);
+            UpdateOSDSeekMessage(actx, message, kOSDTimeout_Med);
         }
     }
     else if (flags & kSticky)
@@ -6213,30 +6251,27 @@ void TV::DoSeek(PlayerContext *ctx, float time, const QString &mesg,
     ctx->LockDeletePlayer(__FILE__, __LINE__);
     if (ctx->player->GetLimitKeyRepeat())
         limitkeys = true;
-    ctx->UnlockDeletePlayer(__FILE__, __LINE__);
 
     if (!limitkeys || (keyRepeatTimer.elapsed() > (int)kKeyRepeatTimeout))
     {
         keyRepeatTimer.start();
         NormalSpeed(ctx);
         time += StopFFRew(ctx);
-        float framerate = ctx->player->GetFrameRate();
         uint64_t currentFrameAbs = ctx->player->GetFramesPlayed();
-        uint64_t currentFrameRel = honorCutlist ?
-            ctx->player->TranslatePositionAbsToRel(currentFrameAbs) :
-            currentFrameAbs;
-        int64_t desiredFrameRel = (timeIsOffset ? currentFrameRel : 0) +
-            time * framerate + 0.5;
-        if (desiredFrameRel < 0)
-            desiredFrameRel = 0;
-        uint64_t desiredFrameAbs = honorCutlist ?
-            ctx->player->TranslatePositionRelToAbs(desiredFrameRel) :
-            desiredFrameRel;
-        time = ((int64_t)desiredFrameAbs - (int64_t)currentFrameAbs) /
-            framerate;
-        DoPlayerSeek(ctx, time);
+        if (timeIsOffset)
+            time +=
+                ctx->player->TranslatePositionFrameToMs(currentFrameAbs,
+                                                        honorCutlist) / 1000.0;
+        if (time < 0)
+            time = 0;
+        uint64_t desiredFrameRel =
+            ctx->player->TranslatePositionMsToFrame(time * 1000, honorCutlist);
+        ctx->UnlockDeletePlayer(__FILE__, __LINE__);
+        DoPlayerSeekToFrame(ctx, desiredFrameRel);
         UpdateOSDSeekMessage(ctx, mesg, kOSDTimeout_Med);
     }
+    else
+        ctx->UnlockDeletePlayer(__FILE__, __LINE__);
 }
 
 void TV::DoSeekAbsolute(PlayerContext *ctx, long long seconds,
@@ -6281,11 +6316,11 @@ void TV::DoArbSeek(PlayerContext *ctx, ArbSeekWhence whence,
             ctx->UnlockDeletePlayer(__FILE__, __LINE__);
             return;
         }
-        time = (ctx->player->CalcMaxFFTime(LONG_MAX, false) /
-                ctx->player->GetFrameRate()) - time;
+        uint64_t total_frames = ctx->player->GetCurrentFrameCount();
+        float dur = ctx->player->ComputeSecs(total_frames, honorCutlist);
+        time = max(0.0f, dur - time);
         ctx->UnlockDeletePlayer(__FILE__, __LINE__);
-        DoSeek(ctx, time, tr("Jump To"),
-               /*timeIsOffset*/(whence != ARBSEEK_SET), honorCutlist);
+        DoSeek(ctx, time, tr("Jump To"), /*timeIsOffset*/false, honorCutlist);
     }
     else
         DoSeekAbsolute(ctx, time, honorCutlist);

mythtv/bindings/perl/MythTV.pm
@@ -107,8 +107,8 @@ package MythTV;
 # Note: as of July 21, 2010, this is actually a string, to account for proto
 # versions of the form "58a".  This will get used if protocol versions are 
 # changed on a fixes branch ongoing.
-    our $PROTO_VERSION = "76";
-    our $PROTO_TOKEN = "FireWilde";
+    our $PROTO_VERSION = "77";
+    our $PROTO_TOKEN = "WindMark";
 
 # currentDatabaseVersion is defined in libmythtv in
 # mythtv/libs/libmythtv/dbcheck.cpp and should be the current MythTV core

mythtv/bindings/php/MythBackend.php
@@ -11,8 +11,8 @@ class MythBackend {
 
 // MYTH_PROTO_VERSION is defined in libmyth in mythtv/libs/libmyth/mythcontext.h
 // and should be the current MythTV protocol version.
-    static $protocol_version        = '76';
-    static $protocol_token          = 'FireWilde';
+    static $protocol_version        = '77';
+    static $protocol_token          = 'WindMark';
 
 // The character string used by the backend to separate records
     static $backend_separator       = '[]:[]';

mythtv/bindings/python/MythTV/static.py
@@ -8,8 +8,8 @@ OWN_VERSION = (0,27,-1,0)
 SCHEMA_VERSION = 1310
 NVSCHEMA_VERSION = 1007
 MUSICSCHEMA_VERSION = 1018
-PROTO_VERSION = '76'
-PROTO_TOKEN = 'FireWilde'
+PROTO_VERSION = '77'
+PROTO_TOKEN = 'WindMark'
 BACKEND_SEP = '[]:[]'
 INSTALL_PREFIX = '/usr/local'
 
@@ -34,6 +34,8 @@ class MARKUP( object ):
     MARK_ASPECT_CUSTOM  = 14
     MARK_VIDEO_WIDTH    = 30
     MARK_VIDEO_HEIGHT   = 31
+    MARK_VIDEO_RATE     = 32
+    MARK_DURATION_MS    = 33
 
 class RECTYPE( object ):
     kNotRecording       = 0

mythtv/libs/libmythbase/mythversion.h
@@ -12,7 +12,7 @@
 /// Update this whenever the plug-in API changes.
 /// Including changes in the libmythbase, libmyth, libmythtv, libmythav* and
 /// libmythui class methods used by plug-ins.
-#define MYTH_BINARY_VERSION "0.27.20121227-2"
+#define MYTH_BINARY_VERSION "0.27.20121231-1"
 
 /** \brief Increment this whenever the MythTV network protocol changes.
  *
@@ -34,9 +34,13 @@
  *   MythTV Python Bindings
  *       mythtv/bindings/python/MythTV/static.py (version number)
  *       mythtv/bindings/python/MythTV/mythproto.py (layout)
+ *
+ *   Be kind and update the wiki as well.
+ *       http://www.mythtv.org/wiki/Category:Myth_Protocol_Commands
+ *       http://www.mythtv.org/wiki/Category:Myth_Protocol
  */
-#define MYTH_PROTO_VERSION "76"
-#define MYTH_PROTO_TOKEN "FireWilde"
+#define MYTH_PROTO_VERSION "77"
+#define MYTH_PROTO_TOKEN "WindMark"
 
 /** \brief Increment this whenever the MythTV core database schema changes.
  *

mythtv/libs/libmythtv/DVD/mythdvdplayer.cpp
@@ -390,17 +390,6 @@ long long MythDVDPlayer::CalcMaxFFTime(long long ff, bool setjump) const
     return MythPlayer::CalcMaxFFTime(ff, setjump);
 }
 
-int64_t MythDVDPlayer::GetSecondsPlayed(void)
-{
-    if (!player_ctx->buffer->IsDVD())
-        return 0;
-
-    return (m_stillFrameLength > 0) ?
-                (m_stillFrameTimer.elapsed() / 1000) :
-                (player_ctx->buffer->DVD()->GetCurrentTime());
-
-}
-
 int64_t MythDVDPlayer::GetTotalSeconds(void) const
 {
     return (m_stillFrameLength > 0) ? m_stillFrameLength: totalLength;

mythtv/libs/libmythtv/DVD/mythdvdplayer.h
@@ -19,7 +19,6 @@ class MythDVDPlayer : public MythPlayer
 
     // Gets
     virtual uint64_t GetBookmark(void);
-    virtual  int64_t GetSecondsPlayed(void);
     virtual  int64_t GetTotalSeconds(void) const;
 
     // DVD public stuff

mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -2945,7 +2945,18 @@ void AvFormatDecoder::HandleGopStart(
             PosMapEntry entry = {framesRead, framesRead, startpos};
 
             QMutexLocker locker(&m_positionMapLock);
+            // Create a dummy positionmap entry for frame 0 so that
+            // seeking will work properly.  (See
+            // DecoderBase::FindPosition() which subtracts
+            // DecoderBase::indexOffset from each frame number.)
+            if (m_positionMap.empty())
+            {
+                PosMapEntry dur = {0, 0, 0};
+                m_positionMap.push_back(dur);
+            }
             m_positionMap.push_back(entry);
+            m_frameToDurMap[framesRead] = totalDuration / 1000;
+            m_durToFrameMap[m_frameToDurMap[framesRead]] = framesRead;
         }
 
 #if 0

commit f9768e1814dff5e0de268297fe25c7cd9b1657de

Fix FFWDSTICKY/RWNDSTICKY behavior while paused.
Thanks to David Engel for spotting this regression introduced in
49dbed5be0729b04a5f0fd0426a32fceb2dd7935.

mythtv/libs/libmythtv/tv_play.cpp
@@ -598,9 +598,9 @@ void TV::InitKeys(void)
     REG_KEY("TV Playback", ACTION_JUMPBKMRK, QT_TRANSLATE_NOOP("MythControls",
             "Jump to bookmark"), "K");
     REG_KEY("TV Playback", "FFWDSTICKY", QT_TRANSLATE_NOOP("MythControls",
-            "Fast Forward (Sticky) or Forward one frame while paused"), ">,.");
+            "Fast Forward (Sticky) or Forward one second while paused"), ">,.");
     REG_KEY("TV Playback", "RWNDSTICKY", QT_TRANSLATE_NOOP("MythControls",
-            "Rewind (Sticky) or Rewind one frame while paused"), ",,<");
+            "Rewind (Sticky) or Rewind one second while paused"), ",,<");
     REG_KEY("TV Playback", "NEXTSOURCE", QT_TRANSLATE_NOOP("MythControls",
             "Next Video Source"), "Y");
     REG_KEY("TV Playback", "PREVSOURCE", QT_TRANSLATE_NOOP("MythControls",
@@ -6196,22 +6196,33 @@ bool TV::SeekHandleAction(PlayerContext *actx, const QStringList &actions,
         {
             QString message = (flags & kRewind) ? tr("Rewind") :
                                                   tr("Forward");
-            actx->LockDeletePlayer(__FILE__, __LINE__);
-            uint64_t frameAbs = actx->player->GetFramesPlayed();
-            uint64_t frameRel =
-                actx->player->TranslatePositionAbsToRel(frameAbs);
-            uint64_t targetRel = frameRel + direction;
-            if (frameRel == 0 && direction < 0)
-                targetRel = 0;
-            uint64_t maxAbs = actx->player->GetCurrentFrameCount();
-            uint64_t maxRel = actx->player->TranslatePositionAbsToRel(maxAbs);
-            if (targetRel > maxRel)
-                targetRel = maxRel;
-            uint64_t targetAbs =
-                actx->player->TranslatePositionRelToAbs(targetRel);
-            actx->UnlockDeletePlayer(__FILE__, __LINE__);
-            DoPlayerSeekToFrame(actx, targetAbs);
-            UpdateOSDSeekMessage(actx, message, kOSDTimeout_Med);
+            if (flags & kAbsolute) // FFWDSTICKY/RWNDSTICKY
+            {
+                float time = direction;
+                DoSeek(actx, time, message,
+                       /*timeIsOffset*/true,
+                       /*honorCutlist*/!(flags & kIgnoreCutlist));
+            }
+            else
+            {
+                actx->LockDeletePlayer(__FILE__, __LINE__);
+                uint64_t frameAbs = actx->player->GetFramesPlayed();
+                uint64_t frameRel =
+                    actx->player->TranslatePositionAbsToRel(frameAbs);
+                uint64_t targetRel = frameRel + direction;
+                if (frameRel == 0 && direction < 0)
+                    targetRel = 0;
+                uint64_t maxAbs = actx->player->GetCurrentFrameCount();
+                uint64_t maxRel =
+                    actx->player->TranslatePositionAbsToRel(maxAbs);
+                if (targetRel > maxRel)
+                    targetRel = maxRel;
+                uint64_t targetAbs =
+                    actx->player->TranslatePositionRelToAbs(targetRel);
+                actx->UnlockDeletePlayer(__FILE__, __LINE__);
+                DoPlayerSeekToFrame(actx, targetAbs);
+                UpdateOSDSeekMessage(actx, message, kOSDTimeout_Med);
+            }
         }
     }
     else if (flags & kSticky)