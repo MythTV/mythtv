MythTV/mythtv

commit 49dbed5be0729b04a5f0fd0426a32fceb2dd7935

Provide accurate position/duration/seeking with non-constant framerates.
The recordedseek and filemarkup tables are enhanced to hold timestamp
data in addition to the existing file offset data.  The millisecond
timestamps are produced by all recorders that subclass DTVRecorder, as
well as mythtranscode and mythcommflag --rebuild.  These timestamps
are relative to the start of the recording/video.

A new command is added to the myth protocol, "QUERY_RECORDER
FILL_DURATION_MAP", modeled after FILL_POSITION_MAP, to send updated
timestamp info for playback of an in-progress recording.

The timestamp markup is used during playback to give accurate position
and duration information in the OSD wherever possible, and to provide
accurate time-based seeking, such as "skip forward 30 seconds" or
"jump to the 5-minute mark".  Timestamps are linearly interpolated
from a frame's nearest neighbors in the map, and extrapolated based on
the current frame rate when the map is missing (e.g. legacy
recordings) or incomplete (e.g. in-progress recordings).  Other than
that, the frame rate is not used for seeking or duration calculations.
(With the exception of a handful of areas that still need some
attention, including seeking based on commskipmap, seeking across
program boundaries during Live TV, and the watched flag calculation.)

The cutlist continues to be taken into account for seeking and
displaying timestamps, for the most part making cutlists
indistinguishable from the result of lossless transcoding.

Note that to get the benefit of these changes for preexisting
recordings, it may be necessary to run "mythcommflag --rebuild" on
such recordings.

Bumps the ABI and protocol versions.  "make distclean" is recommended.

Fixes #10104.

mythtv/libs/libmythtv/DVD/mythdvdplayer.cpp
@@ -390,17 +390,6 @@ long long MythDVDPlayer::CalcMaxFFTime(long long ff, bool setjump) const
     return MythPlayer::CalcMaxFFTime(ff, setjump);
 }
 
-int64_t MythDVDPlayer::GetSecondsPlayed(void)
-{
-    if (!player_ctx->buffer->IsDVD())
-        return 0;
-
-    return (m_stillFrameLength > 0) ?
-                (m_stillFrameTimer.elapsed() / 1000) :
-                (player_ctx->buffer->DVD()->GetCurrentTime());
-
-}
-
 int64_t MythDVDPlayer::GetTotalSeconds(void) const
 {
     return (m_stillFrameLength > 0) ? m_stillFrameLength: totalLength;

mythtv/libs/libmythtv/DVD/mythdvdplayer.h
@@ -19,7 +19,6 @@ class MythDVDPlayer : public MythPlayer
 
     // Gets
     virtual uint64_t GetBookmark(void);
-    virtual  int64_t GetSecondsPlayed(void);
     virtual  int64_t GetTotalSeconds(void) const;
 
     // DVD public stuff

mythtv/libs/libmythtv/mythplayer.h
@@ -180,8 +180,8 @@ class MTV_PUBLIC MythPlayer
     float   GetNextPlaySpeed(void) const      { return next_play_speed; }
     int     GetLength(void) const             { return totalLength; }
     uint64_t GetTotalFrameCount(void) const   { return totalFrames; }
+    uint64_t GetCurrentFrameCount(void) const;
     uint64_t GetFramesPlayed(void) const      { return framesPlayed; }
-    virtual  int64_t GetSecondsPlayed(void);
     virtual  int64_t GetTotalSeconds(void) const;
     virtual  uint64_t GetBookmark(void);
     QString   GetError(void) const;
@@ -321,8 +321,9 @@ class MTV_PUBLIC MythPlayer
     virtual void GoToDVDProgram(bool direction) { (void) direction; }
 
     // Position Map Stuff
-    bool PosMapFromEnc(unsigned long long          start,
-                       QMap<long long, long long> &posMap);
+    bool PosMapFromEnc(uint64_t start,
+                       frm_pos_map_t &posMap,
+                       frm_pos_map_t &durMap);
 
     // OSD locking for TV class
     bool TryLockOSD(void) { return osdLock.tryLock(50); }
@@ -391,12 +392,31 @@ class MTV_PUBLIC MythPlayer
     virtual long long CalcMaxFFTime(long long ff, bool setjump = true) const;
     long long CalcRWTime(long long rw) const;
     virtual void calcSliderPos(osdInfo &info, bool paddedFields = false);
-    uint64_t TranslatePositionAbsToRel(uint64_t absPosition) const {
-        return deleteMap.TranslatePositionAbsToRel(absPosition);
+    uint64_t TranslatePositionFrameToMs(uint64_t position,
+                                        bool use_cutlist) const {
+        return deleteMap.TranslatePositionFrameToMs(position,
+                                                    GetFrameRate(),
+                                                    use_cutlist);
     }
-    uint64_t TranslatePositionRelToAbs(uint64_t relPosition) const {
-        return deleteMap.TranslatePositionRelToAbs(relPosition);
+    uint64_t TranslatePositionMsToFrame(uint64_t position,
+                                        bool use_cutlist) const {
+        return deleteMap.TranslatePositionMsToFrame(position,
+                                                    GetFrameRate(),
+                                                    use_cutlist);
     }
+    // TranslatePositionAbsToRel and TranslatePositionRelToAbs are
+    // used for frame calculations when seeking relative to a number
+    // of frames rather than by time.
+    uint64_t TranslatePositionAbsToRel(uint64_t position) const {
+        return deleteMap.TranslatePositionAbsToRel(position);
+    }
+    uint64_t TranslatePositionRelToAbs(uint64_t position) const {
+        return deleteMap.TranslatePositionRelToAbs(position);
+    }
+    float ComputeSecs(uint64_t position, bool use_cutlist) const {
+        return TranslatePositionFrameToMs(position, use_cutlist) / 1000.0;
+    }
+    uint64_t FindFrame(float offset, bool use_cutlist) const;
 
     // Commercial stuff
     void SetAutoCommercialSkip(CommSkipMode autoskip)
@@ -555,6 +575,8 @@ class MTV_PUBLIC MythPlayer
     // The "inaccuracy" argument is generally one of the kInaccuracy* values.
     bool DoFastForward(uint64_t frames, double inaccuracy);
     bool DoRewind(uint64_t frames, double inaccuracy);
+    bool DoFastForwardSecs(float secs, double inaccuracy, bool use_cutlist);
+    bool DoRewindSecs(float secs, double inaccuracy, bool use_cutlist);
     void DoJumpToFrame(uint64_t frame, double inaccuracy);
 
     // Private seeking stuff

mythtv/bindings/perl/MythTV.pm
@@ -107,8 +107,8 @@ package MythTV;
 # Note: as of July 21, 2010, this is actually a string, to account for proto
 # versions of the form "58a".  This will get used if protocol versions are 
 # changed on a fixes branch ongoing.
-    our $PROTO_VERSION = "76";
-    our $PROTO_TOKEN = "FireWilde";
+    our $PROTO_VERSION = "77";
+    our $PROTO_TOKEN = "WindMark";
 
 # currentDatabaseVersion is defined in libmythtv in
 # mythtv/libs/libmythtv/dbcheck.cpp and should be the current MythTV core

mythtv/bindings/php/MythBackend.php
@@ -11,8 +11,8 @@ class MythBackend {
 
 // MYTH_PROTO_VERSION is defined in libmyth in mythtv/libs/libmyth/mythcontext.h
 // and should be the current MythTV protocol version.
-    static $protocol_version        = '76';
-    static $protocol_token          = 'FireWilde';
+    static $protocol_version        = '77';
+    static $protocol_token          = 'WindMark';
 
 // The character string used by the backend to separate records
     static $backend_separator       = '[]:[]';

mythtv/bindings/python/MythTV/static.py
@@ -8,8 +8,8 @@ OWN_VERSION = (0,27,-1,0)
 SCHEMA_VERSION = 1310
 NVSCHEMA_VERSION = 1007
 MUSICSCHEMA_VERSION = 1018
-PROTO_VERSION = '76'
-PROTO_TOKEN = 'FireWilde'
+PROTO_VERSION = '77'
+PROTO_TOKEN = 'WindMark'
 BACKEND_SEP = '[]:[]'
 INSTALL_PREFIX = '/usr/local'
 
@@ -34,6 +34,8 @@ class MARKUP( object ):
     MARK_ASPECT_CUSTOM  = 14
     MARK_VIDEO_WIDTH    = 30
     MARK_VIDEO_HEIGHT   = 31
+    MARK_VIDEO_RATE     = 32
+    MARK_DURATION_MS    = 33
 
 class RECTYPE( object ):
     kNotRecording       = 0

mythtv/libs/libmythbase/mythversion.h
@@ -12,7 +12,7 @@
 /// Update this whenever the plug-in API changes.
 /// Including changes in the libmythbase, libmyth, libmythtv, libmythav* and
 /// libmythui class methods used by plug-ins.
-#define MYTH_BINARY_VERSION "0.27.20121227-2"
+#define MYTH_BINARY_VERSION "0.27.20121231-1"
 
 /** \brief Increment this whenever the MythTV network protocol changes.
  *
@@ -34,9 +34,13 @@
  *   MythTV Python Bindings
  *       mythtv/bindings/python/MythTV/static.py (version number)
  *       mythtv/bindings/python/MythTV/mythproto.py (layout)
+ *
+ *   Be kind and update the wiki as well.
+ *       http://www.mythtv.org/wiki/Category:Myth_Protocol_Commands
+ *       http://www.mythtv.org/wiki/Category:Myth_Protocol
  */
-#define MYTH_PROTO_VERSION "76"
-#define MYTH_PROTO_TOKEN "FireWilde"
+#define MYTH_PROTO_VERSION "77"
+#define MYTH_PROTO_TOKEN "WindMark"
 
 /** \brief Increment this whenever the MythTV core database schema changes.
  *

commit 5d8b1953b2c9313c8bac2048258bea89979156cc

Fix display of current position for DVD playback.
Regression introduced in 49dbed5be0729b04a5f0fd0426a32fceb2dd7935.

mythtv/libs/libmythtv/DVD/dvdringbuffer.cpp
@@ -988,7 +988,7 @@ void DVDRingBuffer::prevTrack(void)
  */
 uint DVDRingBuffer::GetTotalTimeOfTitle(void)
 {
-    return m_pgcLength / 90000;
+    return m_pgcLength / 90000.0 + 0.5;
 }
 
 /** \brief get the start of the cell in seconds

mythtv/libs/libmythtv/DVD/mythdvdplayer.cpp
@@ -390,6 +390,17 @@ long long MythDVDPlayer::CalcMaxFFTime(long long ff, bool setjump) const
     return MythPlayer::CalcMaxFFTime(ff, setjump);
 }
 
+int64_t MythDVDPlayer::GetSecondsPlayed(bool)
+{
+    if (!player_ctx->buffer->IsDVD())
+        return 0;
+
+    return (m_stillFrameLength > 0) ?
+                (m_stillFrameTimer.elapsed() / 1000) :
+                (player_ctx->buffer->DVD()->GetCurrentTime());
+
+}
+
 int64_t MythDVDPlayer::GetTotalSeconds(void) const
 {
     return (m_stillFrameLength > 0) ? m_stillFrameLength: totalLength;

mythtv/libs/libmythtv/DVD/mythdvdplayer.h
@@ -19,6 +19,7 @@ class MythDVDPlayer : public MythPlayer
 
     // Gets
     virtual uint64_t GetBookmark(void);
+    virtual  int64_t GetSecondsPlayed(bool honorCutList);
     virtual  int64_t GetTotalSeconds(void) const;
 
     // DVD public stuff

mythtv/libs/libmythtv/mythplayer.cpp
@@ -4658,6 +4658,11 @@ int MythPlayer::GetSecondsBehind(void) const
     return (int)((float)(written - played) / video_frame_rate);
 }
 
+int64_t MythPlayer::GetSecondsPlayed(bool honorCutList)
+{
+    return TranslatePositionFrameToMs(framesPlayed, honorCutList) / 1000;
+}
+
 int64_t MythPlayer::GetTotalSeconds(void) const
 {
     return totalDuration;
@@ -4761,15 +4766,14 @@ void MythPlayer::calcSliderPos(osdInfo &info, bool paddedFields)
     // Set the raw values, followed by the translated values.
     for (int i = 0; i < 2 ; ++i)
     {
-        QString relPrefix = (i == 0 ? "" : "rel");
+        bool honorCutList = (i > 0);
+        QString relPrefix = (honorCutList ? "rel" : "");
         if (!fixed_playbacklen)
             playbackLen =
-                TranslatePositionFrameToMs(total_frames, (i > 0))
+                TranslatePositionFrameToMs(total_frames, honorCutList)
             / 1000;
         playbackLen = max(playbackLen, 1);
-        float secsplayed =
-            TranslatePositionFrameToMs(frames_played, (i > 0))
-            / 1000;
+        float secsplayed = GetSecondsPlayed(honorCutList);
         secsplayed = min((float)playbackLen, max(secsplayed, 0.0f));
 
         info.values.insert(relPrefix + "secondsplayed", (int)secsplayed);

mythtv/libs/libmythtv/mythplayer.h
@@ -182,6 +182,7 @@ class MTV_PUBLIC MythPlayer
     uint64_t GetTotalFrameCount(void) const   { return totalFrames; }
     uint64_t GetCurrentFrameCount(void) const;
     uint64_t GetFramesPlayed(void) const      { return framesPlayed; }
+    virtual  int64_t GetSecondsPlayed(bool honorCutList);
     virtual  int64_t GetTotalSeconds(void) const;
     virtual  uint64_t GetBookmark(void);
     QString   GetError(void) const;