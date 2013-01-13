MythTV/mythtv

commit 92016b94983a4b8a6cb75dbc48893f5c05ce4620

Fixes #11029.  Ensure AVPacket's data and size fields are initialized.
Without this initialization, mpegts_push_section() may try to access
uninitialized data and size fields from the AVPacket structure.

mythtv/external/FFmpeg/libavformat/utils.c
@@ -709,7 +709,7 @@ int ff_read_packet(AVFormatContext *s, AVPacket *pkt)
             }
         }
 
-        av_init_packet(pkt);
+        av_new_packet(pkt, 0);
         ret= s->iformat->read_packet(s, pkt);
         if (ret < 0) {
             if (!pktl || ret == AVERROR(EAGAIN))

commit 430900f7bce45d031ec948d867f0903e20544750

Fix a memory leak introduced in 92016b949.  Fixes #11329.
Signed-off-by: Jim Stichnoth <jstichnoth@mythtv.org>

mythtv/external/FFmpeg/libavformat/utils.c
@@ -709,7 +709,9 @@ int ff_read_packet(AVFormatContext *s, AVPacket *pkt)
             }
         }
 
-        av_new_packet(pkt, 0);
+        pkt->data = NULL;
+        pkt->size = 0;
+        av_init_packet(pkt);
         ret= s->iformat->read_packet(s, pkt);
         if (ret < 0) {
             if (!pktl || ret == AVERROR(EAGAIN))