de5dd3b5181869d234b67abc8a5fb112830f3f94
Richard <peper03@yahoo.com>
2013-02-19 19:33:36 +0100

    Sync libdvdnav up to R1251.
    * Fix failed assertion when there's no menu.
    * Make sure we get nav packets for all cells in multi angle features
    * Fix strict aliasing warnings.
    
    Signed-off-by: Stuart Morgan <smorgan@mythtv.org>

--- mythtv/libs/libmythdvdnav/dvdnav/vm/vm.c
+++ mythtv/libs/libmythdvdnav/dvdnav/vm/vm.c
@@ -1794,7 +1794,8 @@ static int set_PGCN(vm_t *vm, int pgcN) {
   pgcit_t *pgcit;
 
   pgcit = get_PGCIT(vm);
-  assert(pgcit != NULL);  /* ?? Make this return -1 instead */
+  if (pgcit == NULL)
+    return 0;
 
   if(pgcN < 1 || pgcN > pgcit->nr_of_pgci_srp) {
 #ifdef TRACE
