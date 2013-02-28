MythTV/mythtv

commit 8fe38245aae3729a1728f43d857a254823102eef

Partially resolve unicode filename issue in ftopen().
This resolves a unicode character issue when trying to use the ftopen()
helper function to access files. The ftopen() function accepts a
standard myth:// URI, and parses it for the necessary information,
however URIs traditionally only accept a limited, safe subset of the
ASCII character set. This adds a temporary work around where by the
various object .open() methods will internally pass the necessary data
in a tuple, bypassing the need to encode and decode about the limited
character set. Myth URIs with unsafe or escaped characters will still
not be handled properly by ftopen().

mythtv/bindings/python/MythTV/mythproto.py
@@ -229,13 +229,16 @@ def ftopen(file, mode, forceremote=False, nooverwrite=False, db=None, \
 
     # process URI (myth://<group>@<host>[:<port>]/<path/to/file>)
     match = reuri.match(file)
-    if match is None:
+    if match:
+        host = match.group('host')
+        filename = match.group('file')
+        sgroup = match.group('group')
+        if sgroup is None:
+            sgroup = 'Default'
+    elif len(file) == 3:
+        host, sgroup, filename = file
+    else:
         raise MythError('Invalid FileTransfer input string: '+file)
-    host = match.group('host')
-    filename = match.group('file')
-    sgroup = match.group('group')
-    if sgroup is None:
-        sgroup = 'Default'
 
     # get full system name
     host = host.strip('[]')
@@ -953,11 +956,9 @@ class Program( CMPRecord, DictData, RECSTATUS, AUDIO_PROPS, \
         return res
 
     def _openProto(self):
-        if not self.filename.startswith('myth://'):
-            self.filename = 'myth://%s@%s/%s' % (self.storagegroup, \
-                                                 self.hostname, \
-                                                 self.filename)
-        return ftopen(self.filename, 'r', db=self._db, chanid=self.chanid, \
+        filename = self.filename if self.filename.startswith('myth://') else \
+                    (self.hostname, self.storagegroup, self.filename)
+        return ftopen(filename, 'r', db=self._db, chanid=self.chanid, \
                       starttime=self.recstartts)
 
     def _openXML(self):

mythtv/bindings/python/MythTV/dataheap.py
@@ -104,7 +104,7 @@ class Artwork( MutableString ):
         be.downloadTo(url, self.imagetype, self)
 
     def open(self, mode='r'):
-        return ftopen('myth://{0.imagetype}@{0.hostname}/{0}'.format(self), mode)
+        return ftopen((self.hostname, self.imagetype, str(self)), mode)
 
 class Record( CMPRecord, DBDataWrite, RECTYPE ):
     """
@@ -332,11 +332,9 @@ class Recorded( CMPRecord, DBDataWrite ):
 
     def open(self, type='r'):
         """Recorded.open(type='r') -> file or FileTransfer object"""
-        return ftopen("myth://%s@%s/%s" % ( self.storagegroup,
-                                            self.hostname,
-                                            self.basename),
-                      type, db=self._db,
-                      chanid=self.chanid, starttime=self.starttime)
+        return ftopen((self.hostname, self.storagegroup, self.basename),
+                      type, db=self._db, chanid=self.chanid,
+                      starttime=self.starttime)
 
     def getProgram(self):
         """Recorded.getProgram() -> Program object"""
@@ -930,7 +928,7 @@ class Video( CMPVideo, VideoSchema, DBDataWrite ):
     trailer              = Artwork('trailer')
 
     def open(self, mode='r', nooverwrite=False):
-        return ftopen('myth://Videos@{0.host}/{0.filename}'.format(self),
+        return ftopen((self.host, 'Videos', self.filename),
                     mode, False, nooverwrite, self._db)
 
     def getHash(self):

commit 33ad9e1d3320507bf9ad69a3689e6879aac71183

Fix file opening in python bindings.
This corrects an issue introduced in 8fe38245 where the alternate input
to ftopen() was not accepted as the regular expression match would error
instead of simply failing.

mythtv/bindings/python/MythTV/mythproto.py
@@ -228,7 +228,12 @@ def ftopen(file, mode, forceremote=False, nooverwrite=False, db=None, \
                       FileTransfer(host, file, storagegroup, mode, db)
 
     # process URI (myth://<group>@<host>[:<port>]/<path/to/file>)
-    match = reuri.match(file)
+    match = None
+    try:
+        match = reuri.match(file)
+    except:
+        pass
+
     if match:
         host = match.group('host')
         filename = match.group('file')