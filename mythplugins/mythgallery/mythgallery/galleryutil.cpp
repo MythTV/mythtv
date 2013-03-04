MythTV/mythtv

commit a05b4d48da71f1e65f01cde7693d539644635d5e

Simplify some file list usage using filters new to QT4
QT4 introduced a 'QDir::NoDotAndDotDot' filter for use with QDir::setFilter(),
QDir::entryList() and QDir::entryInfoList(). This allows us to
simplify a few places in the code by removing our own . and .. checks.

Thanks to Rolf Eike Beer <eike@sf-mail.de> for spotting the problem
and providing an example patch. Since there were many more examples in
the code than the one he highlighted I decided to fix them all in one
commit instead of applying his patch.

mythplugins/mythgallery/mythgallery/galleryutil.cpp
@@ -260,9 +260,10 @@ bool GalleryUtil::LoadDirectory(ThumbList& itemList, const QString& dir,
     // Create .thumbcache dir if neccesary
     if (thumbGen)
         thumbGen->getThumbcacheDir(currDir);
-
+    
     QFileInfoList list = d.entryInfoList(GetMediaFilter(),
-                                         QDir::Files | QDir::AllDirs,
+                                         QDir::Files | QDir::AllDirs |
+                                         QDir::NoDotAndDotDot,
                                          (QDir::SortFlag)flt.getSort());
 
     if (list.isEmpty())
@@ -286,8 +287,6 @@ bool GalleryUtil::LoadDirectory(ThumbList& itemList, const QString& dir,
     {
         fi = &(*it);
         ++it;
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
 
         // remove these already-resized pictures.
         if (isGallery && (
@@ -585,16 +584,14 @@ bool GalleryUtil::CopyDirectory(const QFileInfo src, QFileInfo &dst)
 
     bool ok = true;
     QDir dstDir(dst.absoluteFilePath());
+    srcDir.setFilter(QDir::NoDotAndDotDot);
     QFileInfoList list = srcDir.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     for (; it != list.end(); ++it)
     {
         const QString fn = it->fileName();
-        if (fn != "." && fn != "..")
-        {
-            QFileInfo dfi(dstDir, fn);
-            ok &= Copy(*it, dfi);
-        }
+        QFileInfo dfi(dstDir, fn);
+        ok &= Copy(*it, dfi);
     }
 
     return ok;
@@ -616,16 +613,14 @@ bool GalleryUtil::MoveDirectory(const QFileInfo src, QFileInfo &dst)
 
     bool ok = true;
     QDir dstDir(dst.absoluteFilePath());
+    srcDir.setFilter(QDir::NoDotAndDotDot);
     QFileInfoList list = srcDir.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     for (; it != list.end(); ++it)
     {
         const QString fn = it->fileName();
-        if (fn != "." && fn != "..")
-        {
-            QFileInfo dfi(dstDir, fn);
-            ok &= Move(*it, dfi);
-        }
+        QFileInfo dfi(dstDir, fn);
+        ok &= Move(*it, dfi);
     }
 
     return ok && FileDelete(src);
@@ -637,13 +632,13 @@ bool GalleryUtil::DeleteDirectory(const QFileInfo &dir)
         return false;
 
     QDir srcDir(dir.absoluteFilePath());
+    srcDir.setFilter(QDir::NoDotAndDotDot);
     QFileInfoList list = srcDir.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     for (; it != list.end(); ++it)
     {
         const QString fn = it->fileName();
-        if (fn != "." && fn != "..")
-            Delete(*it);
+        Delete(*it);
     }
 
     return FileDelete(dir);

mythplugins/mythgame/mythgame/gamehandler.cpp
@@ -561,18 +561,13 @@ int GameHandler::buildFileCount(QString directory, GameHandler *handler)
     if (!RomDir.isReadable())
         return 0;
 
+    RomDir.setFilter(QDir::NoDotAndDotDot);
     QFileInfoList List = RomDir.entryInfoList();
     for (QFileInfoList::const_iterator it = List.begin();
          it != List.end(); ++it)
     {
         QFileInfo Info = *it;
         QString RomName = Info.fileName();
-
-        if (RomName == "." ||
-            RomName == "..")
-        {
-            continue;
-        }
 
         if (Info.isDir())
         {
@@ -633,6 +628,7 @@ void GameHandler::buildFileList(QString directory, GameHandler *handler,
         return;
 
     RomDir.setSorting( QDir:: DirsFirst | QDir::Name );
+    RomDir.setFilter(QDir::NoDotAndDotDot);
     QFileInfoList List = RomDir.entryInfoList();
     for (QFileInfoList::const_iterator it = List.begin();
          it != List.end(); ++it)
@@ -640,12 +636,6 @@ void GameHandler::buildFileList(QString directory, GameHandler *handler,
         QFileInfo Info = *it;
         QString RomName = Info.fileName();
         QString GameName = Info.completeBaseName();
-
-        if (RomName == "." ||
-            RomName == "..")
-        {
-            continue;
-        }
 
         if (Info.isDir())
         {

mythplugins/mythmusic/mythmusic/filescanner.cpp
@@ -87,6 +87,7 @@ void FileScanner::BuildFileList(QString &directory, MusicLoadedMap &music_files,
     if (!d.exists())
         return;
 
+    d.setFilter(QDir::NoDotAndDotDot);
     QFileInfoList list = d.entryInfoList();
     if (list.isEmpty())
         return;
@@ -102,8 +103,6 @@ void FileScanner::BuildFileList(QString &directory, MusicLoadedMap &music_files,
     {
         fi = &(*it);
         ++it;
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
         QString filename = fi->absoluteFilePath();
         if (fi->isDir())
         {

mythplugins/mythmusic/mythmusic/importmusic.cpp
@@ -554,6 +554,7 @@ void ImportMusicDialog::scanDirectory(QString &directory, vector<TrackInfo*> *tr
     if (!d.exists())
         return;
 
+    d.setFilter(QDir::NoDotAndDotDot);
     const QFileInfoList list = d.entryInfoList();
     if (list.isEmpty())
         return;
@@ -565,8 +566,6 @@ void ImportMusicDialog::scanDirectory(QString &directory, vector<TrackInfo*> *tr
     {
         fi = &(*it);
         ++it;
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
         QString filename = fi->absoluteFilePath();
         if (fi->isDir())
             scanDirectory(filename, tracks);
@@ -1028,7 +1027,8 @@ void ImportCoverArtDialog::scanDirectory()
     QString nameFilter = gCoreContext->GetSetting("AlbumArtFilter",
                                               "*.png;*.jpg;*.jpeg;*.gif;*.bmp");
 
-    QFileInfoList list = d.entryInfoList(nameFilter.split(";"));
+    QFileInfoList list = d.entryInfoList(nameFilter.split(";"),
+                                         QDir::NoDotAndDotDot);
     if (list.isEmpty())
         return;
 
@@ -1039,8 +1039,6 @@ void ImportCoverArtDialog::scanDirectory()
     {
         fi = &(*it);
         ++it;
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
         QString filename = fi->absoluteFilePath();
         if (!fi->isDir())
         {

mythplugins/mythgallery/mythgallery/galleryfilter.cpp
@@ -73,7 +73,8 @@ bool GalleryFilter::TestFilter(const QString& dir, const GalleryFilter& flt,
     QDir d(dir);
     QString currDir = d.absolutePath();
     QFileInfoList list = d.entryInfoList(GalleryUtil::GetMediaFilter(),
-                                         QDir::Files | QDir::AllDirs,
+                                         QDir::Files | QDir::AllDirs |
+                                         QDir::NoDotAndDotDot,
                                          (QDir::SortFlag)flt.getSort());
 
     if (list.isEmpty())
@@ -86,8 +87,6 @@ bool GalleryFilter::TestFilter(const QString& dir, const GalleryFilter& flt,
          it != list.end(); ++it)
     {
         fi = &(*it);
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
 
         // remove these already-resized pictures.
         if ((fi->fileName().indexOf(".thumb.") > 0) ||

mythplugins/mythgallery/mythgallery/iconview.cpp
@@ -1388,7 +1388,8 @@ void IconView::ImportFromDir(const QString &fromDir, const QString &toDir)
     d.setNameFilters(GalleryUtil::GetMediaFilter());
     d.setSorting((QDir::SortFlag)m_sortorder);
     d.setFilter(QDir::Files       | QDir::AllDirs |
-                QDir::NoSymLinks  | QDir::Readable);
+                QDir::NoSymLinks  | QDir::Readable |
+                QDir::NoDotAndDotDot);
     QFileInfoList list = d.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     const QFileInfo *fi;
@@ -1397,8 +1398,6 @@ void IconView::ImportFromDir(const QString &fromDir, const QString &toDir)
     {
         fi = &(*it);
         ++it;
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
 
         if (fi->isDir())
         {
@@ -1566,7 +1565,8 @@ int ChildCountThread::getChildCount(const QString &filepath)
     isGallery = (gList.count() != 0);
 
     QFileInfoList list = d.entryInfoList(GalleryUtil::GetMediaFilter(),
-                                         QDir::Files | QDir::AllDirs);
+                                         QDir::Files | QDir::AllDirs |
+                                         QDir::NoDotAndDotDot);
 
     if (list.isEmpty())
         return 0;
@@ -1579,8 +1579,6 @@ int ChildCountThread::getChildCount(const QString &filepath)
     {
         fi = &(*it);
         ++it;
-        if (fi->fileName() == "." || fi->fileName() == "..")
-            continue;
 
         // remove these already-resized pictures.
         if (isGallery && (

mythplugins/mythgallery/mythgallery/thumbgenerator.cpp
@@ -262,7 +262,7 @@ void ThumbGenerator::loadDir(QImage& image, const QFileInfo& fi)
 
     // if we didn't find the image yet
     // go into subdirs and keep looking
-    dir.setFilter(QDir::Dirs);
+    dir.setFilter(QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList dirlist = dir.entryInfoList();
     if (dirlist.isEmpty())
         return;
@@ -271,10 +271,6 @@ void ThumbGenerator::loadDir(QImage& image, const QFileInfo& fi)
          it != dirlist.end() && image.isNull() && !m_cancel; ++it)
     {
         const QFileInfo *f = &(*it);
-
-        if (f->fileName() == "." || f->fileName() == "..")
-            continue;
-
         loadDir(image, *f);
     }
 }

mythplugins/mythweather/mythweather/sourceManager.cpp
@@ -375,7 +375,8 @@ void SourceManager::recurseDirs( QDir dir )
     if (!dir.exists())
         return;
 
-    dir.setFilter(QDir::Executable | QDir::Files | QDir::Dirs);
+    dir.setFilter(QDir::Executable | QDir::Files | QDir::Dirs |
+                  QDir::NoDotAndDotDot);
     QFileInfoList files = dir.entryInfoList();
     QFileInfo file;
 
@@ -385,8 +386,6 @@ void SourceManager::recurseDirs( QDir dir )
         file = files.at(x);
         if (file.isDir())
         {
-            if (file.fileName() == QString("..")) continue;
-            if (file.fileName() == QString("."))  continue;
             QDir recurseTo(file.filePath());
             recurseDirs(recurseTo);
         }

commit 6e088edd05a06becf724ba385feb11dfdf1d6882

Fix some more breakage caused by [a05b4d48]. Calling QDir::setFilter() with a negative filter overwrites the default positive filters of QDir::Files | QDir::Dirs.

mythplugins/mythgallery/mythgallery/galleryutil.cpp
@@ -584,7 +584,7 @@ bool GalleryUtil::CopyDirectory(const QFileInfo src, QFileInfo &dst)
 
     bool ok = true;
     QDir dstDir(dst.absoluteFilePath());
-    srcDir.setFilter(QDir::NoDotAndDotDot);
+    srcDir.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList list = srcDir.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     for (; it != list.end(); ++it)
@@ -613,7 +613,7 @@ bool GalleryUtil::MoveDirectory(const QFileInfo src, QFileInfo &dst)
 
     bool ok = true;
     QDir dstDir(dst.absoluteFilePath());
-    srcDir.setFilter(QDir::NoDotAndDotDot);
+    srcDir.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList list = srcDir.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     for (; it != list.end(); ++it)
@@ -632,7 +632,7 @@ bool GalleryUtil::DeleteDirectory(const QFileInfo &dir)
         return false;
 
     QDir srcDir(dir.absoluteFilePath());
-    srcDir.setFilter(QDir::NoDotAndDotDot);
+    srcDir.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList list = srcDir.entryInfoList();
     QFileInfoList::const_iterator it = list.begin();
     for (; it != list.end(); ++it)

mythplugins/mythgame/mythgame/gamehandler.cpp
@@ -561,7 +561,7 @@ int GameHandler::buildFileCount(QString directory, GameHandler *handler)
     if (!RomDir.isReadable())
         return 0;
 
-    RomDir.setFilter(QDir::NoDotAndDotDot);
+    RomDir.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList List = RomDir.entryInfoList();
     for (QFileInfoList::const_iterator it = List.begin();
          it != List.end(); ++it)
@@ -628,7 +628,7 @@ void GameHandler::buildFileList(QString directory, GameHandler *handler,
         return;
 
     RomDir.setSorting( QDir:: DirsFirst | QDir::Name );
-    RomDir.setFilter(QDir::NoDotAndDotDot);
+    RomDir.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList List = RomDir.entryInfoList();
     for (QFileInfoList::const_iterator it = List.begin();
          it != List.end(); ++it)

mythplugins/mythmusic/mythmusic/filescanner.cpp
@@ -87,7 +87,7 @@ void FileScanner::BuildFileList(QString &directory, MusicLoadedMap &music_files,
     if (!d.exists())
         return;
 
-    d.setFilter(QDir::NoDotAndDotDot);
+    d.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     QFileInfoList list = d.entryInfoList();
     if (list.isEmpty())
         return;

mythplugins/mythmusic/mythmusic/importmusic.cpp
@@ -554,7 +554,7 @@ void ImportMusicDialog::scanDirectory(QString &directory, vector<TrackInfo*> *tr
     if (!d.exists())
         return;
 
-    d.setFilter(QDir::NoDotAndDotDot);
+    d.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     const QFileInfoList list = d.entryInfoList();
     if (list.isEmpty())
         return;
@@ -1028,6 +1028,7 @@ void ImportCoverArtDialog::scanDirectory()
                                               "*.png;*.jpg;*.jpeg;*.gif;*.bmp");
 
     QFileInfoList list = d.entryInfoList(nameFilter.split(";"),
+                                         QDir::Files | QDir::Dirs |
                                          QDir::NoDotAndDotDot);
     if (list.isEmpty())
         return;