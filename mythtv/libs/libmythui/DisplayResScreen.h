7a70ac13a46d95bee874ce9c4e5d1bbd5e0bdac8
Jean-Yves Avenard <jyavenard@mythtv.org>
2013-04-19 02:22:27 +1000

    Avoid un-necessary video mode changes.
    Fixes #11494
    (cherry picked from commit 8aca90207ffffc9dd512fa11cd9d88d1f4b5884f)

--- mythtv/libs/libmythui/DisplayResScreen.h
+++ mythtv/libs/libmythui/DisplayResScreen.h
@@ -61,6 +61,12 @@ class MUI_PUBLIC DisplayResScreen
         std::sort(refreshRates.begin(), refreshRates.end());
     }
     void ClearRefreshRates(void) { refreshRates.clear(); }
+    void SetRefreshRate(double rr) 
+    {
+        ClearRefreshRates();
+        AddRefreshRate(rr);
+    }
+
     void SetCustom(bool b) { custom = b; }
 
     // Map for matching real rates and xrandr rate;
