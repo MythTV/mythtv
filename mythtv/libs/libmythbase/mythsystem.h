22bf75bd809749ecd0533e1abe47174c12a6a815
Daniel Kristjansson <danielk@cuymedia.net>
2013-05-18 15:43:04 -0400

    Formatting cleanup of mythsystem.h

--- mythtv/libs/libmythbase/mythsystem.h
+++ mythtv/libs/libmythbase/mythsystem.h
@@ -1,3 +1,5 @@
+/// -*- Mode: c++ -*-
+
 #ifndef MYTHSYSTEM_H_
 #define MYTHSYSTEM_H_
 
@@ -49,154 +51,154 @@ class MBASE_PUBLIC MythSystem : public QObject
 {
     Q_OBJECT
 
-    public:
-        MythSystem();
-        MythSystem(const QString &, uint);
-        MythSystem(const QString &, const QStringList &, uint);
-        MythSystem(const MythSystem &other);
-        ~MythSystem(void);
+  public:
+    MythSystem();
+    MythSystem(const QString &, uint);
+    MythSystem(const QString &, const QStringList &, uint);
+    MythSystem(const MythSystem &other);
+    ~MythSystem(void);
 
-        void SetCommand(const QString &, uint);
-        void SetCommand(const QString &, const QStringList &, uint);
-        void SetDirectory(const QString &);
-        bool SetNice(int nice);
-        bool SetIOPrio(int prio);
+    void SetCommand(const QString &, uint);
+    void SetCommand(const QString &, const QStringList &, uint);
+    void SetDirectory(const QString &);
+    bool SetNice(int nice);
+    bool SetIOPrio(int prio);
 
-        void Run(time_t timeout = 0);
-        uint Wait(time_t timeout = 0);
+    void Run(time_t timeout = 0);
+    uint Wait(time_t timeout = 0);
 
-        int Write(const QByteArray&);
-        QByteArray Read(int size);
-        QByteArray ReadErr(int size);
-        QByteArray& ReadAll();
-        QByteArray& ReadAllErr();
+    int Write(const QByteArray&);
+    QByteArray Read(int size);
+    QByteArray ReadErr(int size);
+    QByteArray& ReadAll();
+    QByteArray& ReadAllErr();
 
-        void Term(bool force=false);
-        void Kill()   { Signal(SIGKILL); };
-        void Stop()   { Signal(SIGSTOP); };
-        void Cont()   { Signal(SIGCONT); };
-        void HangUp() { Signal(SIGHUP);  };
-        void USR1()   { Signal(SIGUSR1); };
-        void USR2()   { Signal(SIGUSR2); };
-        void Signal(int sig);
+    void Term(bool force = false);
+    void Kill(void)   { Signal(SIGKILL); }
+    void Stop(void)   { Signal(SIGSTOP); }
+    void Cont(void)   { Signal(SIGCONT); }
+    void HangUp(void) { Signal(SIGHUP);  }
+    void USR1(void)   { Signal(SIGUSR1); }
+    void USR2(void)   { Signal(SIGUSR2); }
+    void Signal(int sig);
 
-        void JumpAbort(void);
+    void JumpAbort(void);
 
-        bool isBackground()   { return GetSetting("RunInBackground"); };
-        bool doAutoCleanup()  { return GetSetting("AutoCleanup"); };
-        bool onlyLowExitVal() { return GetSetting("OnlyLowExitVal"); };
-        void HandlePreRun();
-        void HandlePostRun();
+    bool isBackground(void)   { return GetSetting("RunInBackground"); }
+    bool doAutoCleanup(void)  { return GetSetting("AutoCleanup"); }
+    bool onlyLowExitVal(void) { return GetSetting("OnlyLowExitVal"); }
+    void HandlePreRun(void);
+    void HandlePostRun(void);
 
-        uint GetStatus()             { return m_status; };
-        void SetStatus(uint status)  { m_status = status; };
+    uint GetStatus(void)             { return m_status; }
+    void SetStatus(uint status)      { m_status = status; }
 
-        QString& GetLogCmd()         { return m_logcmd; };
-        QString& GetDirectory()      { return m_directory; };
+    QString& GetLogCmd(void)         { return m_logcmd; }
+    QString& GetDirectory(void)      { return m_directory; }
 
-        bool GetSetting(const char *setting) 
-            { return m_settings.value(QString(setting)); };
+    bool GetSetting(const char *setting) 
+    { return m_settings.value(QString(setting)); }
 
-        QString& GetCommand()        { return m_command; };
-        void SetCommand(QString &cmd) { m_command = cmd; };
+    QString& GetCommand(void)        { return m_command; }
+    void SetCommand(QString &cmd)    { m_command = cmd; }
 
-        QStringList &GetArgs()       { return m_args; };
-        void SetArgs(QStringList &args)  { m_args = args; };
+    QStringList &GetArgs(void)       { return m_args; }
+    void SetArgs(QStringList &args)  { m_args = args; }
 
-        int GetNice()                { return m_nice; };
-        int GetIOPrio()              { return m_ioprio; };
+    int GetNice(void)                { return m_nice; }
+    int GetIOPrio(void)              { return m_ioprio; }
 
-        QBuffer *GetBuffer(int index) { return &m_stdbuff[index]; };
+    QBuffer *GetBuffer(int index)    { return &m_stdbuff[index]; }
 
-        void Unlock() { m_semReady.release(1); };
+    void Unlock(void)                { m_semReady.release(1); }
 
-        friend class MythSystemPrivate;
+    friend class MythSystemPrivate;
 
-        static QString ShellEscape(const QString &str);
+    static QString ShellEscape(const QString &str);
 
-    signals:
-        void started();
-        void finished();
-        void error(uint status);
-        void readDataReady(int fd);
+  signals:
+    void started(void);
+    void finished(void);
+    void error(uint status);
+    void readDataReady(int fd);
 
-    private:
-        void initializePrivate(void);
-        MythSystemPrivate *d;
+  private:
+    void initializePrivate(void);
+    MythSystemPrivate *d;
 
-    protected:
-        void ProcessFlags(uint flags);
+  protected:
+    void ProcessFlags(uint flags);
 
-        uint        m_status;
-        QSemaphore  m_semReady;
+    uint        m_status;
+    QSemaphore  m_semReady;
 
-        QString     m_command;
-        QString     m_logcmd;
-        QStringList m_args;
-        QString     m_directory;
+    QString     m_command;
+    QString     m_logcmd;
+    QStringList m_args;
+    QString     m_directory;
 
-        int         m_nice;
-        int         m_ioprio;
+    int         m_nice;
+    int         m_ioprio;
 
-        Setting_t   m_settings;
-        QBuffer     m_stdbuff[3];
+    Setting_t   m_settings;
+    QBuffer     m_stdbuff[3];
 };
 
-class MBASE_PUBLIC MythSystemPrivate : public QObject, public ReferenceCounter
+class MythSystemPrivate : public QObject, public ReferenceCounter
 {
     Q_OBJECT
 
-    public:
-        MythSystemPrivate(const QString &debugName);
+  public:
+    MythSystemPrivate(const QString &debugName);
 
-        virtual void Fork(time_t timeout) = 0;
-        virtual void Manage() = 0;
+    virtual void Fork(time_t timeout) = 0;
+    virtual void Manage(void) = 0;
 
-        virtual void Term(bool force=false) = 0;
-        virtual void Signal(int sig) = 0;
-        virtual void JumpAbort(void) = 0;
+    virtual void Term(bool force=false) = 0;
+    virtual void Signal(int sig) = 0;
+    virtual void JumpAbort(void) = 0;
 
-        virtual bool ParseShell(const QString &cmd, QString &abscmd,
-                                QStringList &args) = 0;
+    virtual bool ParseShell(const QString &cmd, QString &abscmd,
+                            QStringList &args) = 0;
 
-    protected:
-        QPointer<MythSystem> m_parent;
+  protected:
+    QPointer<MythSystem> m_parent;
 
-        uint GetStatus()             { return m_parent->GetStatus(); };
-        void SetStatus(uint status)  { m_parent->SetStatus(status); };
+    uint GetStatus(void)             { return m_parent->GetStatus(); }
+    void SetStatus(uint status)      { m_parent->SetStatus(status); }
 
-        QString& GetLogCmd()         { return m_parent->GetLogCmd(); };
-        QString& GetDirectory()      { return m_parent->GetDirectory(); };
+    QString& GetLogCmd(void)         { return m_parent->GetLogCmd(); }
+    QString& GetDirectory(void)      { return m_parent->GetDirectory(); }
 
-        bool GetSetting(const char *setting) 
-            { return m_parent->GetSetting(setting); };
+    bool GetSetting(const char *setting) 
+        { return m_parent->GetSetting(setting); }
 
-        QString& GetCommand()        { return m_parent->GetCommand(); };
-        void SetCommand(QString &cmd) { m_parent->SetCommand(cmd); };
+    QString& GetCommand(void)        { return m_parent->GetCommand(); }
+    void SetCommand(QString &cmd)    { m_parent->SetCommand(cmd); }
 
-        QStringList &GetArgs()       { return m_parent->GetArgs(); };
-        void SetArgs(QStringList &args)  { m_parent->SetArgs(args); };
+    QStringList &GetArgs(void)       { return m_parent->GetArgs(); }
+    void SetArgs(QStringList &args)  { m_parent->SetArgs(args); }
 
-        QBuffer *GetBuffer(int index) { return m_parent->GetBuffer(index); };
-        void Unlock() { m_parent->Unlock(); };
+    QBuffer *GetBuffer(int index)    { return m_parent->GetBuffer(index); }
+    void Unlock(void)                { m_parent->Unlock(); }
 
-    signals:
-        void started();
-        void finished();
-        void error(uint status);
-        void readDataReady(int fd);
+  signals:
+    void started(void);
+    void finished(void);
+    void error(uint status);
+    void readDataReady(int fd);
 };
 
 
- MBASE_PUBLIC  unsigned int myth_system(const QString &command, 
-                                 uint flags = kMSNone,
-                                 uint timeout = 0);
- MBASE_PUBLIC  void myth_system_jump_abort(void);
+MBASE_PUBLIC uint myth_system(const QString &command, 
+                              uint flags = kMSNone,
+                              uint timeout = 0);
+MBASE_PUBLIC void myth_system_jump_abort(void);
 extern "C" {
 #endif
 
 /* C prototype */
- MBASE_PUBLIC  unsigned int myth_system_c(char *command, uint flags, uint timeout);
+MBASE_PUBLIC uint myth_system_c(char *command, uint flags, uint timeout);
 
 #ifdef __cplusplus
 }
