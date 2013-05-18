55c6497b2df73c0aad221275612ecbf2ff746898
Daniel Kristjansson <danielk@cuymedia.net>
2013-05-18 15:51:17 -0400

    Formatting cleanup of mythsystem.cpp

--- mythtv/libs/libmythbase/mythsystem.cpp
+++ mythtv/libs/libmythbase/mythsystem.cpp
@@ -1,3 +1,4 @@
+/// -*- Mode: c++ -*-
 
 // compat header
 #include "compat.h"
@@ -61,13 +62,15 @@ MythSystem::MythSystem(const QString &command, uint flags)
     SetCommand(command, flags);
 }
 
-/** \fn MythSystem::setCommand(const QString &command) 
+/** \fn MythSystem::setCommand(const QString &command)
  *  \brief Resets an existing MythSystem object to a new command
  */
 void MythSystem::SetCommand(const QString &command, uint flags)
 {
     if (flags & kMSRunShell)
+    {
         SetCommand(command, QStringList(), flags);
+    }
     else
     {
         QString abscommand;
@@ -86,7 +89,7 @@ void MythSystem::SetCommand(const QString &command, uint flags)
 }
 
 
-MythSystem::MythSystem(const QString &command, 
+MythSystem::MythSystem(const QString &command,
                        const QStringList &args, uint flags)
 {
     m_semReady.release(1);  // initialize
@@ -94,11 +97,11 @@ MythSystem::MythSystem(const QString &command,
     SetCommand(command, args, flags);
 }
 
-/** \fn MythSystem::setCommand(const QString &command, 
+/** \fn MythSystem::setCommand(const QString &command,
                                const QStringList &args)
  *  \brief Resets an existing MythSystem object to a new command
  */
-void MythSystem::SetCommand(const QString &command, 
+void MythSystem::SetCommand(const QString &command,
                             const QStringList &args, uint flags)
 {
     m_status = GENERIC_EXIT_START;
@@ -127,14 +130,15 @@ void MythSystem::SetCommand(const QString &command,
     // check for execute rights
     if (!GetSetting("UseShell") && access(command.toUtf8().constData(), X_OK))
     {
-        LOG(VB_GENERAL, LOG_ERR, QString("MythSystem(%1) command not executable, ")
-                .arg(command) + ENO);
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("MythSystem(%1) command not executable, ")
+            .arg(command) + ENO);
         m_status = GENERIC_EXIT_CMD_NOT_FOUND;
     }
 
     m_logcmd = (m_command + " " + m_args.join(" ")).trimmed();
 
-    if( GetSetting("AnonLog") )
+    if (GetSetting("AnonLog"))
     {
         m_logcmd.truncate(m_logcmd.indexOf(" "));
         m_logcmd.append(" (anonymized)");
@@ -176,7 +180,7 @@ void MythSystem::SetDirectory(const QString &directory)
 
 bool MythSystem::SetNice(int nice)
 {
-    if( !d || (GetStatus() != GENERIC_EXIT_START) )
+    if (!d || (GetStatus() != GENERIC_EXIT_START))
         return false;
 
     m_nice = nice;
@@ -185,22 +189,20 @@ bool MythSystem::SetNice(int nice)
 
 bool MythSystem::SetIOPrio(int prio)
 {
-    if( !d || (GetStatus() != GENERIC_EXIT_START) )
+    if (!d || (GetStatus() != GENERIC_EXIT_START))
         return false;
 
     m_ioprio = prio;
     return true;
 }
 
-/** \fn MythSystem::Run()
- *  \brief Runs a command inside the /bin/sh shell. Returns immediately
- */
+/// \brief Runs a command inside the /bin/sh shell. Returns immediately
 void MythSystem::Run(time_t timeout)
 {
-    if( !d )
+    if (!d)
         m_status = GENERIC_EXIT_NO_HANDLER;
 
-    if( GetStatus() != GENERIC_EXIT_START )
+    if (GetStatus() != GENERIC_EXIT_START)
     {
         emit error(GetStatus());
         return;
@@ -211,7 +213,7 @@ void MythSystem::Run(time_t timeout)
 
     d->Fork(timeout);
 
-    if( GetStatus() == GENERIC_EXIT_RUNNING )
+    if (GetStatus() == GENERIC_EXIT_RUNNING)
     {
         m_semReady.acquire(1);
         emit started();
@@ -227,21 +229,21 @@ void MythSystem::Run(time_t timeout)
 // Wait() for that purpose sufficient?
 uint MythSystem::Wait(time_t timeout)
 {
-    if( !d )
+    if (!d)
         m_status = GENERIC_EXIT_NO_HANDLER;
 
-    if( (GetStatus() != GENERIC_EXIT_RUNNING) || GetSetting("RunInBackground") )
+    if ((GetStatus() != GENERIC_EXIT_RUNNING) || GetSetting("RunInBackground"))
         return GetStatus();
 
-    if( GetSetting("ProcessEvents") )
+    if (GetSetting("ProcessEvents"))
     {
-        if( timeout > 0 )
+        if (timeout > 0)
             timeout += time(NULL);
 
-        while( !timeout || time(NULL) < timeout )
+        while (!timeout || time(NULL) < timeout)
         {
             // loop until timeout hits or process ends
-            if( m_semReady.tryAcquire(1,100) )
+            if (m_semReady.tryAcquire(1,100))
             {
                 m_semReady.release(1);
                 break;
@@ -252,9 +254,9 @@ uint MythSystem::Wait(time_t timeout)
     }
     else
     {
-        if( timeout > 0 )
+        if (timeout > 0)
         {
-            if( m_semReady.tryAcquire(1, timeout*1000) )
+            if (m_semReady.tryAcquire(1, timeout*1000))
                 m_semReady.release(1);
         }
         else
@@ -268,21 +270,21 @@ uint MythSystem::Wait(time_t timeout)
 
 void MythSystem::Term(bool force)
 {
-    if( !d )
+    if (!d)
         m_status = GENERIC_EXIT_NO_HANDLER;
 
-    if( m_status != GENERIC_EXIT_RUNNING )
+    if (m_status != GENERIC_EXIT_RUNNING)
         return;
 
     d->Term(force);
 }
 
-void MythSystem::Signal( int sig )
+void MythSystem::Signal(int sig)
 {
-    if( !d )
+    if (!d)
         m_status = GENERIC_EXIT_NO_HANDLER;
 
-    if( m_status != GENERIC_EXIT_RUNNING )
+    if (m_status != GENERIC_EXIT_RUNNING)
         return;
 
     d->Signal(sig);
@@ -291,7 +293,7 @@ void MythSystem::Signal( int sig )
 
 void MythSystem::ProcessFlags(uint flags)
 {
-    if( m_status != GENERIC_EXIT_START )
+    if (m_status != GENERIC_EXIT_START)
     {
         LOG(VB_SYSTEM, LOG_DEBUG, QString("status: %1").arg(m_status));
         return;
@@ -299,10 +301,10 @@ void MythSystem::ProcessFlags(uint flags)
 
     m_settings["IsInUI"] = gCoreContext->HasGUI() && gCoreContext->IsUIThread();
 
-    if( flags & kMSRunBackground )
+    if (flags & kMSRunBackground)
         m_settings["RunInBackground"] = true;
 
-    if( m_command.endsWith("&") )
+    if (m_command.endsWith("&"))
     {
         if (!GetSetting("RunInBackground"))
             LOG(VB_SYSTEM, LOG_DEBUG, "Adding background flag");
@@ -315,7 +317,7 @@ void MythSystem::ProcessFlags(uint flags)
         m_settings["IsInUI"]          = false;
     }
 
-    if( GetSetting("IsInUI") )
+    if (GetSetting("IsInUI"))
     {
         // Check for UI-only locks
         m_settings["BlockInputDevs"] = !(flags & kMSDontBlockInputDevs);
@@ -324,46 +326,46 @@ void MythSystem::ProcessFlags(uint flags)
         m_settings["DisableUDP"]     = flags & kMSDisableUDPListener;
     }
 
-    if( flags & kMSStdIn )
+    if (flags & kMSStdIn)
         m_settings["UseStdin"] = true;
-    if( flags & kMSStdOut )
+    if (flags & kMSStdOut)
         m_settings["UseStdout"] = true;
-    if( flags & kMSStdErr )
+    if (flags & kMSStdErr)
         m_settings["UseStderr"] = true;
-    if( flags & kMSRunShell )
+    if (flags & kMSRunShell)
         m_settings["UseShell"] = true;
-    if( flags & kMSNoRunShell ) // override for use with myth_system
+    if (flags & kMSNoRunShell) // override for use with myth_system
         m_settings["UseShell"] = false;
-    if( flags & kMSAbortOnJump )
+    if (flags & kMSAbortOnJump)
         m_settings["AbortOnJump"] = true;
-    if( flags & kMSSetPGID )
+    if (flags & kMSSetPGID)
         m_settings["SetPGID"] = true;
-    if( flags & kMSAutoCleanup && GetSetting("RunInBackground") )
+    if (flags & kMSAutoCleanup && GetSetting("RunInBackground"))
         m_settings["AutoCleanup"] = true;
-    if( flags & kMSAnonLog )
+    if (flags & kMSAnonLog)
         m_settings["AnonLog"] = true;
-    if( flags & kMSLowExitVal )
+    if (flags & kMSLowExitVal)
         m_settings["OnlyLowExitVal"] = true;
-    if( flags & kMSPropagateLogs )
+    if (flags & kMSPropagateLogs)
         m_settings["PropagateLogs"] = true;
 }
 
 QByteArray  MythSystem::Read(int size)
-{ 
-    return m_stdbuff[1].read(size); 
+{
+    return m_stdbuff[1].read(size);
 }
 
 QByteArray  MythSystem::ReadErr(int size)
-{ 
-    return m_stdbuff[2].read(size); 
+{
+    return m_stdbuff[2].read(size);
 }
 
-QByteArray& MythSystem::ReadAll()
+QByteArray& MythSystem::ReadAll(void)
 {
     return m_stdbuff[1].buffer();
 }
 
-QByteArray& MythSystem::ReadAllErr()
+QByteArray& MythSystem::ReadAllErr(void)
 {
     return m_stdbuff[2].buffer();
 }
@@ -377,12 +379,12 @@ int MythSystem::Write(const QByteArray &ba)
     return ba.size();
 }
 
-void MythSystem::HandlePreRun()
+void MythSystem::HandlePreRun(void)
 {
     // This needs to be a send event so that the MythUI locks the input devices
     // immediately instead of after existing events are processed
     // since this function could be called inside one of those events.
-    if( GetSetting("BlockInputDevs") )
+    if (GetSetting("BlockInputDevs"))
     {
         QEvent event(MythEvent::kLockInputDevicesEventType);
         QCoreApplication::sendEvent(gCoreContext->GetGUIObject(), &event);
@@ -392,7 +394,7 @@ void MythSystem::HandlePreRun()
     // immediately instead of after existing events are processed, since the
     // listen server must be terminated before the spawned application tries
     // to start its own
-    if( GetSetting("DisableUDP") )
+    if (GetSetting("DisableUDP"))
     {
         QEvent event(MythEvent::kDisableUDPListenerEventType);
         QCoreApplication::sendEvent(gCoreContext->GetGUIObject(), &event);
@@ -401,18 +403,18 @@ void MythSystem::HandlePreRun()
     // This needs to be a send event so that the MythUI m_drawState change is
     // flagged immediately instead of after existing events are processed
     // since this function could be called inside one of those events.
-    if( GetSetting("DisableDrawing") )
+    if (GetSetting("DisableDrawing"))
     {
         QEvent event(MythEvent::kPushDisableDrawingEventType);
         QCoreApplication::sendEvent(gCoreContext->GetGUIObject(), &event);
     }
 }
 
-void MythSystem::HandlePostRun()
+void MythSystem::HandlePostRun(void)
 {
     // Since this is *not* running in the UI thread (but rather the signal
     // handler thread), we need to use postEvents
-    if( GetSetting("DisableDrawing") )
+    if (GetSetting("DisableDrawing"))
     {
         QEvent *event = new QEvent(MythEvent::kPopDisableDrawingEventType);
         QCoreApplication::postEvent(gCoreContext->GetGUIObject(), event);
@@ -420,7 +422,7 @@ void MythSystem::HandlePostRun()
 
     // This needs to be a post event so we do not try to start listening on
     // the UDP ports before the child application has stopped and terminated
-    if( GetSetting("DisableUDP") )
+    if (GetSetting("DisableUDP"))
     {
         QEvent event(MythEvent::kEnableUDPListenerEventType);
         QCoreApplication::sendEvent(gCoreContext->GetGUIObject(), &event);
@@ -428,7 +430,7 @@ void MythSystem::HandlePostRun()
 
     // This needs to be a post event so that the MythUI unlocks input devices
     // after all existing (blocked) events are processed and ignored.
-    if( GetSetting("BlockInputDevs") )
+    if (GetSetting("BlockInputDevs"))
     {
         QEvent *event = new QEvent(MythEvent::kUnlockInputDevicesEventType);
         QCoreApplication::postEvent(gCoreContext->GetGUIObject(), event);
