d7a8c532a45476aa10263838430922a37ab4961d
Daniel Kristjansson <danielk@cuymedia.net>
2013-05-23 21:42:42 -0400

    Add missing header include

--- mythtv/libs/libmythtv/HLS/httplivestream.cpp
+++ mythtv/libs/libmythtv/HLS/httplivestream.cpp
@@ -19,7 +19,11 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <stdio.h>
+// POSIX headers
+#include <unistd.h> // for usleep
+
+// C headers
+#include <cstdio>
 
 #include <QDir>
 #include <QFile>
