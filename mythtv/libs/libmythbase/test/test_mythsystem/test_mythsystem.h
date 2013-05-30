f2e117c090369104930a61fe7e7dfdfccef2f9c4
Daniel Kristjansson <danielk@cuymedia.net>
2013-05-29 21:06:59 -0400

    General improvements to mythsystem unit tests.

--- mythtv/libs/libmythbase/test/test_mythsystem/test_mythsystem.h
+++ mythtv/libs/libmythbase/test/test_mythsystem/test_mythsystem.h
@@ -47,6 +47,14 @@ class TestMythSystem: public QObject
 {
     Q_OBJECT
 
+    QDateTime m_before;
+
+    void Go(MythSystem &s)
+    {
+        s.Run();
+        (void) s.Wait();
+    }
+
   private slots:
     // called at the beginning of these sets of tests
     void initTestCase(void)
@@ -67,28 +75,27 @@ class TestMythSystem: public QObject
     // called before each test case
     void init(void)
     {
+        m_before = QDateTime::currentDateTime();
     }
 
     // called after each test case
     void cleanup(void)
     {
+        m_before = QDateTime();
     }
 
     void constructed_command_is_run(void)
     {
         MythSystem cmd(QString("echo %1").arg(__FUNCTION__), kMSStdOut);
-        cmd.Run();
-        int ret = cmd.Wait();
-        QVERIFY(ret == 0);
+        Go(cmd);
         QVERIFY(QString(cmd.ReadAll()).contains(__FUNCTION__));
     }
 
-    void exit_code_works(void)
+    void wait_returns_exit_code(void)
     {
-        MythSystem cmd("exit 34", kMSNone | kMSRunShell);
+        MythSystem cmd("exit 200", kMSNone | kMSRunShell);
         cmd.Run();
-        int ret = cmd.Wait();
-        QVERIFY(ret == 34);
+        QVERIFY(cmd.Wait() == 200);
     }
 
     // TODO kMSDontBlockInputDevs -- avoid blocking LIRC & Joystick Menu
@@ -98,11 +105,8 @@ class TestMythSystem: public QObject
     void run_in_background_works(void)
     {
         MythSystem cmd("sleep 1", kMSRunBackground);
-        QDateTime before = QDateTime::currentDateTime();
-        cmd.Run();
-        (void) cmd.Wait();
-        QDateTime after = QDateTime::currentDateTime();
-        QVERIFY(before.msecsTo(after) < 500);
+        Go(cmd);
+        QVERIFY(m_before.msecsTo(QDateTime::currentDateTime()) < 500);
     }
 
     // TODO kMSProcessEvents      -- process events while waiting
@@ -118,9 +122,8 @@ class TestMythSystem: public QObject
         MythSystem cmd(QString("cat - > %1").arg(tempfile.fileName()),
                        kMSStdIn);
         cmd.Write(in);
-        cmd.Run();
-        int ret = cmd.Wait();
-        QVERIFY(ret == 0);
+        Go(cmd);
+        QVERIFY(cmd.GetStatus() == 0);
         QByteArray out = tempfile.readAll();
         QVERIFY(QString(out).contains(QString(in)));
     }
@@ -129,9 +132,8 @@ class TestMythSystem: public QObject
     void stdout_works(void)
     {
         MythSystem cmd(QString("echo %1").arg(__FUNCTION__), kMSStdOut);
-        cmd.Run();
-        int ret = cmd.Wait();
-        QVERIFY(ret == 0);
+        Go(cmd);
+        QVERIFY(cmd.GetStatus() == 0);
         QVERIFY(QString(cmd.ReadAll()).contains(__FUNCTION__));
     }
 
@@ -140,9 +142,8 @@ class TestMythSystem: public QObject
     {
         MythSystem cmd(QString("echo %1 >&2").arg(__FUNCTION__),
                        kMSRunShell | kMSStdErr);
-        cmd.Run();
-        int ret = cmd.Wait();
-        QVERIFY(ret == 0);
+        Go(cmd);
+        QVERIFY(cmd.GetStatus() == 0);
         QVERIFY(QString(cmd.ReadAllErr()).contains(__FUNCTION__));
     }
     // TODO kMSBuffered           -- buffer the IO channels
@@ -152,8 +153,7 @@ class TestMythSystem: public QObject
     {
         MythSystem cmd("if [ x != y ] ; then echo X ; else echo Y ; fi",
                        kMSRunShell | kMSStdOut);
-        cmd.Run();
-        (void) cmd.Wait();
+        Go(cmd);
         QVERIFY(QString(cmd.ReadAll()).contains("X"));
     }
 
@@ -161,8 +161,7 @@ class TestMythSystem: public QObject
     {
         MythSystem cmd("if [ x != y ] ; then echo X ; else echo Y ; fi",
                        kMSStdOut);
-        cmd.Run();
-        (void) cmd.Wait();
+        Go(cmd);
         QVERIFY(!QString(cmd.ReadAll()).contains("X"));
     }
 
@@ -175,8 +174,7 @@ class TestMythSystem: public QObject
     {
         console_dbg()->Clear();
         MythSystem cmd(QString("echo %1").arg(__FUNCTION__), kMSAnonLog);
-        cmd.Run();
-        (void) cmd.Wait();
+        Go(cmd);
         DebugLogHandlerEntry l = console_dbg()->LastEntry(kHandleLog);
         QVERIFY(!l.entry().GetMessage().contains(__FUNCTION__));
         QVERIFY(!cmd.GetLogCmd().contains(__FUNCTION__));
@@ -185,28 +183,24 @@ class TestMythSystem: public QObject
     // kMSAbortOnJump        -- abort this process on a jumppoint
     void abort_on_jump_works_when_requested(void)
     {
-        MythSystem cmd(QString("sleep 1"), kMSAbortOnJump);
-        QDateTime before = QDateTime::currentDateTime();
+        MythSystem cmd(QString("sleep 0.50"), kMSAbortOnJump);
         cmd.Run();
         usleep(5000);
         cmd.JumpAbort();
         int ret = cmd.Wait();
-        QDateTime after = QDateTime::currentDateTime();
-        QVERIFY(before.msecsTo(after) < 500);
+        QVERIFY(m_before.msecsTo(QDateTime::currentDateTime()) < 400);
         QVERIFY(0 != ret);
     }
 
     // ! kMSAbortOnJump      -- ! abort this process on a jumppoint
     void abort_on_jump_noop_works_when_not_requested(void)
     {
-        MythSystem cmd(QString("sleep 1"), kMSNone);
-        QDateTime before = QDateTime::currentDateTime();
+        MythSystem cmd(QString("sleep 0.50"), kMSNone);
         cmd.Run();
         usleep(5000);
         cmd.JumpAbort();
         int ret = cmd.Wait();
-        QDateTime after = QDateTime::currentDateTime();
-        QVERIFY(before.msecsTo(after) > 900);
+        QVERIFY(m_before.msecsTo(QDateTime::currentDateTime()) > 400);
         QVERIFY(0 == ret);
     }
 
