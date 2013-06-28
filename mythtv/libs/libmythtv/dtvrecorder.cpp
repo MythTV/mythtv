10e87f5d318e68bf2067fc22e24d31443b778717
John Poet <jpoet@mythtv.org>
2013-06-28 17:18:41 -0600

    dtvrecorder: Flush the payload buffer whenever a new payload starts.
    We start buffering at the beginning of each payload, and don't write out the
    data until we know if this payload has a keyframe.  This is so a new file
    (for back to back recordings) would start on a keyframe.
    
    Further, the keyframe offset recorded in the DB, is the offset to the
    beginning of the payload with the keyframe.  Or at least it was supposed to
    be.  Since we were not actually flushing any data currently in the buffer at
    the start of a payload, that offset could be pointing to a completely
    different payload!
    
    (cherry picked from commit 09b787346e5de8809b3905e64c1887dd273a860e)

--- mythtv/libs/libmythtv/dtvrecorder.cpp
+++ mythtv/libs/libmythtv/dtvrecorder.cpp
@@ -1318,6 +1318,14 @@ bool DTVRecorder::ProcessVideoTSPacket(const TSPacket &tspacket)
 
     if (tspacket.HasPayload() && tspacket.PayloadStart())
     {
+        if (_buffer_packets && _first_keyframe >= 0 && !_payload_buffer.empty())
+        {
+            // Flush the buffer
+            if (ringBuffer)
+                ringBuffer->Write(&_payload_buffer[0], _payload_buffer.size());
+            _payload_buffer.clear();
+        }
+
         // buffer packets until we know if this is a keyframe
         _buffer_packets = true;
     }
@@ -1341,6 +1349,14 @@ bool DTVRecorder::ProcessAudioTSPacket(const TSPacket &tspacket)
 
     if (tspacket.HasPayload() && tspacket.PayloadStart())
     {
+        if (_buffer_packets && _first_keyframe >= 0 && !_payload_buffer.empty())
+        {
+            // Flush the buffer
+            if (ringBuffer)
+                ringBuffer->Write(&_payload_buffer[0], _payload_buffer.size());
+            _payload_buffer.clear();
+        }
+
         // buffer packets until we know if this is a keyframe
         _buffer_packets = true;
     }
