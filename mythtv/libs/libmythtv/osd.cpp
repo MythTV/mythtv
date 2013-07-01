3ccfb8162a04113aed98b3c6a917e2b7452d59dd
Jean-Yves Avenard <jyavenard@mythtv.org>
2013-07-01 16:05:45 +1000

    Display Notification Center cards during video playback
    We do so by manually drawing the notification cards within the OSD.
    
    Only tested with OpenGL rendering

--- mythtv/libs/libmythtv/osd.cpp
+++ mythtv/libs/libmythtv/osd.cpp
@@ -15,6 +15,7 @@
 #include "mythuibutton.h"
 #include "mythuieditbar.h"
 #include "mythuistatetype.h"
+#include "mythuinotificationcenter.h"
 
 // libmythtv
 #include "channelutil.h"
@@ -649,7 +650,10 @@ bool OSD::DrawDirect(MythPainter* painter, QSize size, bool repaint)
         painter->End();
     }
 
-    return visible;
+    bool visible2 =
+        MythUINotificationCenter::GetInstance()->DrawDirect(painter, size, repaint);
+
+    return visible || visible2;
 }
 
 QRegion OSD::Draw(MythPainter* painter, QPaintDevice *device, QSize size,
@@ -734,6 +738,14 @@ QRegion OSD::Draw(MythPainter* painter, QPaintDevice *device, QSize size,
 
     if (visible.isEmpty() || (!alignx && !aligny))
         return visible;
+
+    // display notifications stack
+    QRegion changed2;
+    QRegion notification =
+        MythUINotificationCenter::GetInstance()->Draw(painter, device, size,
+                                                      changed2, alignx, aligny);
+    changed = changed.united(changed2);
+    visible = visible.united(notification);
 
     // assist yuv blending with some friendly alignments
     QRegion aligned;
