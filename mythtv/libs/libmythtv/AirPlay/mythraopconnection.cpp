085cce7a81033e02234f6708f1a7c954ea13efe8
Jean-Yves Avenard <jyavenard@mythtv.org>
2013-07-06 00:00:27 +1000

    Make RAOP notifications timeout if not updated after 5s

--- mythtv/libs/libmythtv/AirPlay/mythraopconnection.cpp
+++ mythtv/libs/libmythtv/AirPlay/mythraopconnection.cpp
@@ -1784,6 +1784,7 @@ void MythRAOPConnection::SendNotification(bool update)
     }
     n->SetId(m_id);
     n->SetParent(this);
+    n->SetDuration(5);
     MythUINotificationCenter::GetInstance()->Queue(*n);
     m_firstsend = true;
     delete n;
