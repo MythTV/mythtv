MythTV/mythtv

commit 98602d94e33537cf683c0640027685e3aae9f5b8

Only set text if a template replacement was made.
Fixes #11664

mythtv/libs/libmythui/mythuitext.cpp
@@ -169,6 +169,7 @@ void MythUIText::SetTextFromMap(QHash<QString, QString> &map)
                                              QCoreApplication::UnicodeUTF8);
 
         QString tempString = translatedTemplate;
+        bool replaced = false;
 
         while ((pos = regexp.indexIn(translatedTemplate, pos)) != -1)
         {
@@ -182,12 +183,16 @@ void MythUIText::SetTextFromMap(QHash<QString, QString> &map)
                               .arg(regexp.cap(3))
                               .arg(map.value(key))
                               .arg(regexp.cap(6));
+                replaced = true;
             }
 
             tempString.replace(regexp.cap(0), replacement);
             pos += regexp.matchedLength();
         }
-        SetText(tempString);
+        if (replaced)
+        {
+            SetText(tempString);
+        }
     }
     else if (map.contains(objectName()))
     {

commit 27c9c102d60b019f008762756b173d292c96c6a6

Fix notification display
98602d94e335 broke string replacement should the text being replaced be empty...

mythtv/libs/libmythui/mythuitext.cpp
@@ -176,7 +176,7 @@ void MythUIText::SetTextFromMap(QHash<QString, QString> &map)
             QString key = regexp.cap(4).toLower().trimmed();
             QString replacement;
 
-            if (!map.value(key).isEmpty())
+            if (map.contains(key))
             {
                 replacement = QString("%1%2%3%4")
                               .arg(regexp.cap(2))