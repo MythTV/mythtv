cd314dc6cab8b212221d6318876b2c3f46503a3e
Gary Buhrmaster <gary.buhrmaster@gmail.com>
2013-07-13 19:46:07 +0100

    Eliminate memory leak in MythRenderOpenGL
    In commit 07c54f07872c764f5f355775b27d9ee6f93daf0c
    a memory leak was introduced.  This patch eliminates
    the use of the pointer where the lock member object
    is lost and replaces it by appropriate creation in
    the initializer.  This is a "minimal" fix to the issue.
    
    Refs #11660
    
    Signed-off-by: Paul Harrison <pharrison@mythtv.org>

--- mythtv/libs/libmythui/mythrender_opengl.cpp
+++ mythtv/libs/libmythui/mythrender_opengl.cpp
@@ -104,14 +104,14 @@ MythRenderOpenGL* MythRenderOpenGL::Create(const QString &painter,
 
 MythRenderOpenGL::MythRenderOpenGL(const QGLFormat& format, QPaintDevice* device,
                                    RenderType type)
-  : QGLContext(format, device), MythRender(type)
+  : QGLContext(format, device), MythRender(type), m_lock(QMutex::Recursive)
 {
     ResetVars();
     ResetProcs();
 }
 
 MythRenderOpenGL::MythRenderOpenGL(const QGLFormat& format, RenderType type)
-  : QGLContext(format), MythRender(type)
+  : QGLContext(format), MythRender(type), m_lock(QMutex::Recursive)
 {
     ResetVars();
     ResetProcs();
@@ -119,7 +119,6 @@ MythRenderOpenGL::MythRenderOpenGL(const QGLFormat& format, RenderType type)
 
 MythRenderOpenGL::~MythRenderOpenGL()
 {
-    delete m_lock;
 }
 
 void MythRenderOpenGL::Init(void)
@@ -161,7 +160,7 @@ bool MythRenderOpenGL::IsRecommendedRenderer(void)
 
 void MythRenderOpenGL::makeCurrent()
 {
-    m_lock->lock();
+    m_lock.lock();
     if (this != MythRenderOpenGL::currentContext())
         QGLContext::makeCurrent();
     m_lock_level++;
@@ -174,7 +173,7 @@ void MythRenderOpenGL::doneCurrent()
         QGLContext::doneCurrent();
     if (m_lock_level < 0)
         LOG(VB_GENERAL, LOG_ERR, LOC + "Mis-matched calls to makeCurrent()");
-    m_lock->unlock();
+    m_lock.unlock();
 }
 
 void MythRenderOpenGL::Release(void)
@@ -966,7 +965,6 @@ void MythRenderOpenGL::ResetVars(void)
 {
     m_fence           = 0;
 
-    m_lock            = new QMutex(QMutex::Recursive);
     m_lock_level      = 0;
 
     m_extensions      = QString();
