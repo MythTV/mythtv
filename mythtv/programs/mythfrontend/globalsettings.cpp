e99f064cf15fd5299f58a85505fa88e8bab4f9e1
Jean-Yves Avenard <jyavenard@mythtv.org>
2013-07-16 14:30:01 +1000

    Add GUI to force the use of FFmpeg's own MPEG-TS demuxer.
    Option will only show on debug build

--- mythtv/programs/mythfrontend/globalsettings.cpp
+++ mythtv/programs/mythfrontend/globalsettings.cpp
@@ -59,6 +59,18 @@ static HostCheckBox *DecodeExtraAudio()
     return gc;
 }
 
+#if CONFIG_DEBUGTYPE
+static HostCheckBox *FFmpegDemuxer()
+{
+    HostCheckBox *gc = new HostCheckBox("FFMPEGTS");
+    gc->setLabel(QObject::tr("Use FFmpeg's original MPEG-TS demuxer"));
+    gc->setValue(false);
+    gc->setHelpText(QObject::tr("Experimental: Enable this setting to use FFmpeg's native demuxer. "
+                                "Things will be broken."));
+    return gc;
+}
+#endif
+
 static HostComboBox *PIPLocationComboBox()
 {
     HostComboBox *gc = new HostComboBox("PIPLocation");
@@ -3175,6 +3187,9 @@ PlaybackSettings::PlaybackSettings()
     column1->addChild(RealtimePriority());
     column1->addChild(DecodeExtraAudio());
     column1->addChild(JumpToProgramOSD());
+#if CONFIG_DEBUGTYPE
+    column1->addChild(FFmpegDemuxer());
+#endif
     columns->addChild(column1);
 
     VerticalConfigurationGroup *column2 =
