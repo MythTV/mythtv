MythTV/mythtv

commit f3f0252d9b6313b7977a40be321b789dc2965bb1

ResetMap should reset textarea widget using the same logic they were first set
Fixes #11687

mythtv/libs/libmythui/mythuitext.cpp
@@ -125,6 +125,45 @@ void MythUIText::Reset()
     MythUIType::Reset();
 }
 
+void MythUIText::ResetMap(InfoMap &map)
+{
+    QString newText = GetTemplateText();
+
+    if (newText.isEmpty())
+        newText = GetDefaultText();
+
+    QRegExp regexp("%(([^\\|%]+)?\\||\\|(.))?(\\w+)(\\|(.+))?%");
+    regexp.setMinimal(true);
+
+    bool replaced = map.contains(objectName());
+
+    if (!replaced && !newText.isEmpty() && newText.contains(regexp))
+    {
+        int pos = 0;
+
+        QString translatedTemplate = qApp->translate("ThemeUI",
+                                                     newText.toUtf8(),
+                                                     NULL,
+                                                     QCoreApplication::UnicodeUTF8);
+
+        while ((pos = regexp.indexIn(translatedTemplate, pos)) != -1)
+        {
+            QString key = regexp.cap(4).toLower().trimmed();
+
+            if (map.contains(key))
+            {
+                replaced = true;
+                break;
+            }
+        }
+    }
+
+    if (replaced)
+    {
+        Reset();
+    }
+}
+
 void MythUIText::SetText(const QString &text)
 {
     QString newtext = text;

mythtv/libs/libmythui/mythuicomposite.cpp
@@ -38,8 +38,8 @@ void MythUIComposite::ResetMap(InfoMap &infoMap)
         MythUIType *type = i.next();
 
         MythUIText *textType = dynamic_cast<MythUIText *> (type);
-        if (textType && infoMap.contains(textType->objectName()))
-            textType->Reset();
+        if (textType)
+            textType->ResetMap(infoMap);
 
         MythUIComposite *group = dynamic_cast<MythUIComposite *> (type);
         if (group)

mythtv/libs/libmythui/mythuitext.h
@@ -34,6 +34,7 @@ class MUI_PUBLIC MythUIText : public MythUIType, public StorageUser
     ~MythUIText();
 
     void Reset(void);
+    void ResetMap(InfoMap &map);
 
     virtual void SetText(const QString &text);
     QString GetText(void) const;

commit 01be22c7d033a3dfc508528e5372a19631d56266

Fix infinite loop introduced with f3f0252d9b6313b7977a40be321b789dc2965bb1
If the template content didn't match the regex, it would loop indefinitely

mythtv/libs/libmythui/mythuitext.cpp
@@ -155,6 +155,7 @@ void MythUIText::ResetMap(const InfoMap &map)
                 replaced = true;
                 break;
             }
+            pos += regexp.matchedLength();
         }
     }
 