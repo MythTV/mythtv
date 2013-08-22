d6262ca91aa63a0e9bba12b5c5bc8a2c0530b00f
David Blain <dblain@mythtv.org>
2013-08-22 15:23:11 -0400

    Replace MSVC specific headers with more complete versions

--- mythtv/platform/win32/msvc/include-2010/inttypes.h
+++ mythtv/platform/win32/msvc/include-2010/inttypes.h
@@ -1,117 +1,305 @@
+// ISO C9x  compliant inttypes.h for Microsoft Visual Studio
+// Based on ISO/IEC 9899:TC2 Committee draft (May 6, 2005) WG14/N1124 
+// 
+//  Copyright (c) 2006 Alexander Chemeris
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//   1. Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+// 
+//   2. Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+// 
+//   3. The name of the author may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _MSC_VER // [
+#error "Use this header only with Microsoft Visual C++ compilers!"
+#endif // _MSC_VER ]
+
+#ifndef _MSC_INTTYPES_H_ // [
+#define _MSC_INTTYPES_H_
+
+#if _MSC_VER > 1000
 #pragma once
-
-#include <stdint.h>
-
-#define _PFX_8   "hh"
-#define _PFX_16  "h"
-#define _PFX_32  "l"
-#define _PFX_64  "ll"
-
-#ifdef _WIN64
-#define _PFX_PTR  "ll"
-#else
-#define _PFX_PTR  "l"
 #endif
 
-#ifdef _FAST16_IS_32 /* compiler test */
-#define _PFX_F16  _PFX_32
-#else /* _FAST16_IS_32 */
-#define _PFX_F16  _PFX_16
-#endif /* _FAST16_IS_32 */
+#include "stdint.h"
 
-/* PRINT FORMAT MACROS */
-#define PRId8        _PFX_8 "d"
-#define PRId16       _PFX_16 "d"
-#define PRId32       _PFX_32 "d"
-#define PRIdLEAST8   _PFX_8 "d"
-#define PRIdLEAST16  _PFX_16 "d"
-#define PRIdLEAST32  _PFX_32 "d"
-#define PRIdFAST8    _PFX_8 "d"
-#define PRIdFAST16   _PFX_F16 "d"
-#define PRIdFAST32   _PFX_32 "d"
+// 7.8 Format conversion of integer types
 
-#define PRIi8        _PFX_8 "i"
-#define PRIi16       _PFX_16 "i"
-#define PRIi32       _PFX_32 "i"
-#define PRIiLEAST8   _PFX_8 "i"
-#define PRIiLEAST16  _PFX_16 "i"
-#define PRIiLEAST32  _PFX_32 "i"
-#define PRIiFAST8    _PFX_8 "i"
-#define PRIiFAST16   _PFX_F16 "i"
-#define PRIiFAST32   _PFX_32 "i"
+typedef struct {
+   intmax_t quot;
+   intmax_t rem;
+} imaxdiv_t;
 
-#define PRIo8        _PFX_8 "o"
-#define PRIo16       _PFX_16 "o"
-#define PRIo32       _PFX_32 "o"
-#define PRIoLEAST8   _PFX_8 "o"
-#define PRIoLEAST16  _PFX_16 "o"
-#define PRIoLEAST32  _PFX_32 "o"
-#define PRIoFAST8    _PFX_8 "o"
-#define PRIoFAST16   _PFX_F16 "o"
-#define PRIoFAST32   _PFX_32 "o"
+// 7.8.1 Macros for format specifiers
 
-#define PRIu8        _PFX_8 "u"
-#define PRIu16       _PFX_16 "u"
-#define PRIu32       _PFX_32 "u"
-#define PRIuLEAST8   _PFX_8 "u"
-#define PRIuLEAST16  _PFX_16 "u"
-#define PRIuLEAST32  _PFX_32 "u"
-#define PRIuFAST8    _PFX_8 "u"
-#define PRIuFAST16   _PFX_F16 "u"
-#define PRIuFAST32   _PFX_32 "u"
+#if !defined(__cplusplus) || defined(__STDC_FORMAT_MACROS) // [   See footnote 185 at page 198
 
-#define PRIx8        _PFX_8 "x"
-#define PRIx16       _PFX_16 "x"
-#define PRIx32       _PFX_32 "x"
-#define PRIxLEAST8   _PFX_8 "x"
-#define PRIxLEAST16  _PFX_16 "x"
-#define PRIxLEAST32  _PFX_32 "x"
-#define PRIxFAST8    _PFX_8 "x"
-#define PRIxFAST16   _PFX_F16 "x"
-#define PRIxFAST32   _PFX_32 "x"
+// The fprintf macros for signed integers are:
+#define PRId8       "d"
+#define PRIi8       "i"
+#define PRIdLEAST8  "d"
+#define PRIiLEAST8  "i"
+#define PRIdFAST8   "d"
+#define PRIiFAST8   "i"
 
-#define PRIX8        _PFX_8 "X"
-#define PRIX16       _PFX_16 "X"
-#define PRIX32       _PFX_32 "X"
-#define PRIXLEAST8   _PFX_8 "X"
-#define PRIXLEAST16  _PFX_16 "X"
-#define PRIXLEAST32  _PFX_32 "X"
-#define PRIXFAST8    _PFX_8 "X"
-#define PRIXFAST16   _PFX_F16 "X"
-#define PRIXFAST32   _PFX_32 "X"
+#define PRId16       "hd"
+#define PRIi16       "hi"
+#define PRIdLEAST16  "hd"
+#define PRIiLEAST16  "hi"
+#define PRIdFAST16   "hd"
+#define PRIiFAST16   "hi"
 
-#define PRId64       _PFX_64 "d"
-#define PRIdLEAST64  _PFX_64 "d"
-#define PRIdFAST64   _PFX_64 "d"
-#define PRIdMAX      _PFX_64 "d"
-#define PRIdPTR      _PFX_PTR "d"
+#define PRId32       "I32d"
+#define PRIi32       "I32i"
+#define PRIdLEAST32  "I32d"
+#define PRIiLEAST32  "I32i"
+#define PRIdFAST32   "I32d"
+#define PRIiFAST32   "I32i"
 
-#define PRIi64       _PFX_64 "i"
-#define PRIiLEAST64  _PFX_64 "i"
-#define PRIiFAST64   _PFX_64 "i"
-#define PRIiMAX      _PFX_64 "i"
-#define PRIiPTR      _PFX_PTR "i"
+#define PRId64       "I64d"
+#define PRIi64       "I64i"
+#define PRIdLEAST64  "I64d"
+#define PRIiLEAST64  "I64i"
+#define PRIdFAST64   "I64d"
+#define PRIiFAST64   "I64i"
 
-#define PRIo64       _PFX_64 "o"
-#define PRIoLEAST64  _PFX_64 "o"
-#define PRIoFAST64   _PFX_64 "o"
-#define PRIoMAX      _PFX_64 "o"
-#define PRIoPTR      _PFX_PTR "o"
+#define PRIdMAX     "I64d"
+#define PRIiMAX     "I64i"
 
-#define PRIu64       _PFX_64 "u"
-#define PRIuLEAST64  _PFX_64 "u"
-#define PRIuFAST64   _PFX_64 "u"
-#define PRIuMAX      _PFX_64 "u"
-#define PRIuPTR      _PFX_PTR "u"
+#define PRIdPTR     "Id"
+#define PRIiPTR     "Ii"
 
-#define PRIx64       _PFX_64 "x"
-#define PRIxLEAST64  _PFX_64 "x"
-#define PRIxFAST64   _PFX_64 "x"
-#define PRIxMAX      _PFX_64 "x"
-#define PRIxPTR      _PFX_PTR "x"
+// The fprintf macros for unsigned integers are:
+#define PRIo8       "o"
+#define PRIu8       "u"
+#define PRIx8       "x"
+#define PRIX8       "X"
+#define PRIoLEAST8  "o"
+#define PRIuLEAST8  "u"
+#define PRIxLEAST8  "x"
+#define PRIXLEAST8  "X"
+#define PRIoFAST8   "o"
+#define PRIuFAST8   "u"
+#define PRIxFAST8   "x"
+#define PRIXFAST8   "X"
 
-#define PRIX64       _PFX_64 "X"
-#define PRIXLEAST64  _PFX_64 "X"
-#define PRIXFAST64   _PFX_64 "X"
-#define PRIXMAX      _PFX_64 "X"
-#define PRIXPTR      _PFX_PTR "X"
+#define PRIo16       "ho"
+#define PRIu16       "hu"
+#define PRIx16       "hx"
+#define PRIX16       "hX"
+#define PRIoLEAST16  "ho"
+#define PRIuLEAST16  "hu"
+#define PRIxLEAST16  "hx"
+#define PRIXLEAST16  "hX"
+#define PRIoFAST16   "ho"
+#define PRIuFAST16   "hu"
+#define PRIxFAST16   "hx"
+#define PRIXFAST16   "hX"
+
+#define PRIo32       "I32o"
+#define PRIu32       "I32u"
+#define PRIx32       "I32x"
+#define PRIX32       "I32X"
+#define PRIoLEAST32  "I32o"
+#define PRIuLEAST32  "I32u"
+#define PRIxLEAST32  "I32x"
+#define PRIXLEAST32  "I32X"
+#define PRIoFAST32   "I32o"
+#define PRIuFAST32   "I32u"
+#define PRIxFAST32   "I32x"
+#define PRIXFAST32   "I32X"
+
+#define PRIo64       "I64o"
+#define PRIu64       "I64u"
+#define PRIx64       "I64x"
+#define PRIX64       "I64X"
+#define PRIoLEAST64  "I64o"
+#define PRIuLEAST64  "I64u"
+#define PRIxLEAST64  "I64x"
+#define PRIXLEAST64  "I64X"
+#define PRIoFAST64   "I64o"
+#define PRIuFAST64   "I64u"
+#define PRIxFAST64   "I64x"
+#define PRIXFAST64   "I64X"
+
+#define PRIoMAX     "I64o"
+#define PRIuMAX     "I64u"
+#define PRIxMAX     "I64x"
+#define PRIXMAX     "I64X"
+
+#define PRIoPTR     "Io"
+#define PRIuPTR     "Iu"
+#define PRIxPTR     "Ix"
+#define PRIXPTR     "IX"
+
+// The fscanf macros for signed integers are:
+#define SCNd8       "d"
+#define SCNi8       "i"
+#define SCNdLEAST8  "d"
+#define SCNiLEAST8  "i"
+#define SCNdFAST8   "d"
+#define SCNiFAST8   "i"
+
+#define SCNd16       "hd"
+#define SCNi16       "hi"
+#define SCNdLEAST16  "hd"
+#define SCNiLEAST16  "hi"
+#define SCNdFAST16   "hd"
+#define SCNiFAST16   "hi"
+
+#define SCNd32       "ld"
+#define SCNi32       "li"
+#define SCNdLEAST32  "ld"
+#define SCNiLEAST32  "li"
+#define SCNdFAST32   "ld"
+#define SCNiFAST32   "li"
+
+#define SCNd64       "I64d"
+#define SCNi64       "I64i"
+#define SCNdLEAST64  "I64d"
+#define SCNiLEAST64  "I64i"
+#define SCNdFAST64   "I64d"
+#define SCNiFAST64   "I64i"
+
+#define SCNdMAX     "I64d"
+#define SCNiMAX     "I64i"
+
+#ifdef _WIN64 // [
+#  define SCNdPTR     "I64d"
+#  define SCNiPTR     "I64i"
+#else  // _WIN64 ][
+#  define SCNdPTR     "ld"
+#  define SCNiPTR     "li"
+#endif  // _WIN64 ]
+
+// The fscanf macros for unsigned integers are:
+#define SCNo8       "o"
+#define SCNu8       "u"
+#define SCNx8       "x"
+#define SCNX8       "X"
+#define SCNoLEAST8  "o"
+#define SCNuLEAST8  "u"
+#define SCNxLEAST8  "x"
+#define SCNXLEAST8  "X"
+#define SCNoFAST8   "o"
+#define SCNuFAST8   "u"
+#define SCNxFAST8   "x"
+#define SCNXFAST8   "X"
+
+#define SCNo16       "ho"
+#define SCNu16       "hu"
+#define SCNx16       "hx"
+#define SCNX16       "hX"
+#define SCNoLEAST16  "ho"
+#define SCNuLEAST16  "hu"
+#define SCNxLEAST16  "hx"
+#define SCNXLEAST16  "hX"
+#define SCNoFAST16   "ho"
+#define SCNuFAST16   "hu"
+#define SCNxFAST16   "hx"
+#define SCNXFAST16   "hX"
+
+#define SCNo32       "lo"
+#define SCNu32       "lu"
+#define SCNx32       "lx"
+#define SCNX32       "lX"
+#define SCNoLEAST32  "lo"
+#define SCNuLEAST32  "lu"
+#define SCNxLEAST32  "lx"
+#define SCNXLEAST32  "lX"
+#define SCNoFAST32   "lo"
+#define SCNuFAST32   "lu"
+#define SCNxFAST32   "lx"
+#define SCNXFAST32   "lX"
+
+#define SCNo64       "I64o"
+#define SCNu64       "I64u"
+#define SCNx64       "I64x"
+#define SCNX64       "I64X"
+#define SCNoLEAST64  "I64o"
+#define SCNuLEAST64  "I64u"
+#define SCNxLEAST64  "I64x"
+#define SCNXLEAST64  "I64X"
+#define SCNoFAST64   "I64o"
+#define SCNuFAST64   "I64u"
+#define SCNxFAST64   "I64x"
+#define SCNXFAST64   "I64X"
+
+#define SCNoMAX     "I64o"
+#define SCNuMAX     "I64u"
+#define SCNxMAX     "I64x"
+#define SCNXMAX     "I64X"
+
+#ifdef _WIN64 // [
+#  define SCNoPTR     "I64o"
+#  define SCNuPTR     "I64u"
+#  define SCNxPTR     "I64x"
+#  define SCNXPTR     "I64X"
+#else  // _WIN64 ][
+#  define SCNoPTR     "lo"
+#  define SCNuPTR     "lu"
+#  define SCNxPTR     "lx"
+#  define SCNXPTR     "lX"
+#endif  // _WIN64 ]
+
+#endif // __STDC_FORMAT_MACROS ]
+
+// 7.8.2 Functions for greatest-width integer types
+
+// 7.8.2.1 The imaxabs function
+#define imaxabs _abs64
+
+// 7.8.2.2 The imaxdiv function
+
+// This is modified version of div() function from Microsoft's div.c found
+// in %MSVC.NET%\crt\src\div.c
+#ifdef STATIC_IMAXDIV // [
+static
+#else // STATIC_IMAXDIV ][
+_inline
+#endif // STATIC_IMAXDIV ]
+imaxdiv_t __cdecl imaxdiv(intmax_t numer, intmax_t denom)
+{
+   imaxdiv_t result;
+
+   result.quot = numer / denom;
+   result.rem = numer % denom;
+
+   if (numer < 0 && result.rem > 0) {
+      // did division wrong; must fix up
+      ++result.quot;
+      result.rem -= denom;
+   }
+
+   return result;
+}
+
+// 7.8.2.3 The strtoimax and strtoumax functions
+#define strtoimax _strtoi64
+#define strtoumax _strtoui64
+
+// 7.8.2.4 The wcstoimax and wcstoumax functions
+#define wcstoimax _wcstoi64
+#define wcstoumax _wcstoui64
+
+
+#endif // _MSC_INTTYPES_H_ ]
