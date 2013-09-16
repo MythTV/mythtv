8bcf40c9ea93efafdf10af672ae4e3cf7f3b300c
Paul Harrison <pharrison@mythtv.org>
2013-09-16 23:39:55 +0100

    MythMusic: fix extracting the tracks metadata from the filename
    There was a problem with the tracks file extension not being removed from the
    filename before parsing it to extract the title, album, artist etc
    
    (cherry picked from commit 086230eb3bdebee37dca1436fda74a04b7b3a871)

--- mythtv/libs/libmythmetadata/metaio.cpp
+++ mythtv/libs/libmythmetadata/metaio.cpp
@@ -108,11 +108,11 @@ void MetaIO::readFromFilename(const QString &filename,
     title.clear();
     genre.clear();
     tracknum = 0;
-    
+
     int part_num = 0;
     // Replace 
     lfilename.replace('_', ' ');
-    lfilename.section('.', 0, -2);
+    lfilename = lfilename.section('.', 0, -2);
     QStringList fmt_list = mFilenameFormat.split("/");
     QStringList::iterator fmt_it = fmt_list.begin();
 
