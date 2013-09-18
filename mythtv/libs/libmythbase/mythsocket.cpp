836eb11d96abfaa5986e7382103db69769b0ae47
Stuart Morgan <smorgan@mythtv.org>
2013-09-18 14:50:27 +0100

    Prevent the use of SendReceiveStringList() on a socket with callbacks. Fixes #11777

--- mythtv/libs/libmythbase/mythsocket.cpp
+++ mythtv/libs/libmythbase/mythsocket.cpp
@@ -330,6 +330,19 @@ bool MythSocket::ReadStringList(QStringList &list, uint timeoutMS)
 bool MythSocket::SendReceiveStringList(
     QStringList &strlist, uint min_reply_length, uint timeoutMS)
 {
+    if (m_callback)
+    {
+        // If callbacks are enabled then SendReceiveStringList() will conflict
+        // causing failed reads and socket disconnections - see #11777
+        // SendReceiveStringList() should NOT be used with an event socket, only
+        // the control socket
+        LOG(VB_GENERAL, LOG_EMERG, QString("Programmer Error! "
+                                           "SendReceiveStringList(%1) used on "
+                                           "socket with callbacks.")
+                                .arg(strlist.isEmpty() ? "empty" : strlist[0]));
+        return false;
+    }
+
     if (!WriteStringList(strlist))
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + "Failed to send command.");
