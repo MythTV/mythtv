e1b5bb027dd4e179e021e613de50906188a4ece4
Jim Stichnoth <jstichnoth@mythtv.org>
2013-10-20 15:58:50 -0700

    Allow Live TV to be entered from the top-level Program Guide.
    This allows the user to start Live TV on a specific channel from the
    Program Guide grid.  Otherwise, the user needs to wait for Live TV to
    start and tune the initial channel, then find the channel actually
    wanted, and wait for it to be tuned.  This also provides a workaround
    when the default initial channel is untunable for some reason.
    
    Currently, if the desired channel is not tunable (e.g., all capable
    recorders are already busy), it will fail.
    
    Refs #11913.
    
    (cherry picked from commit c0c5299ab82f638157b17ba13af154f73c27a40d)

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -307,7 +307,8 @@ void TV::StopPlayback(void)
 /**
  * \brief returns true if the recording completed when exiting.
  */
-bool TV::StartTV(ProgramInfo *tvrec, uint flags)
+bool TV::StartTV(ProgramInfo *tvrec, uint flags,
+                 const ChannelInfoList &selection)
 {
     TV *tv = GetTV();
     if (!tv)
@@ -376,7 +377,7 @@ bool TV::StartTV(ProgramInfo *tvrec, uint flags)
         else if (RemoteGetFreeRecorderCount())
         {
             LOG(VB_PLAYBACK, LOG_INFO, LOC + "tv->LiveTV() -- begin");
-            if (!tv->LiveTV(showDialogs))
+            if (!tv->LiveTV(showDialogs, selection))
             {
                 tv->SetExitPlayer(true, true);
                 quitAll = true;
@@ -1024,6 +1025,7 @@ TV::TV(void)
       asInputMode(false),
       // Channel changing state variables
       queuedChanNum(""),
+      initialChanID(0),
       lockTimerOn(false),
       // channel browsing
       browsehelper(NULL),
@@ -1680,7 +1682,7 @@ TVState TV::GetState(const PlayerContext *actx) const
  *  \brief Starts LiveTV
  *  \param showDialogs if true error dialogs are shown, if false they are not
  */
-bool TV::LiveTV(bool showDialogs)
+bool TV::LiveTV(bool showDialogs, const ChannelInfoList &selection)
 {
     requestDelete = false;
     allowRerecord = false;
@@ -1688,7 +1690,7 @@ bool TV::LiveTV(bool showDialogs)
 
     PlayerContext *actx = GetPlayerReadLock(-1, __FILE__, __LINE__);
     if (actx->GetState() == kState_None &&
-        RequestNextRecorder(actx, showDialogs))
+        RequestNextRecorder(actx, showDialogs, selection))
     {
         actx->SetInitialTVState(true);
         HandleStateChange(actx, actx);
@@ -1717,7 +1719,8 @@ int TV::GetLastRecorderNum(int player_idx) const
     return ret;
 }
 
-bool TV::RequestNextRecorder(PlayerContext *ctx, bool showDialogs)
+bool TV::RequestNextRecorder(PlayerContext *ctx, bool showDialogs,
+                             const ChannelInfoList &selection)
 {
     if (!ctx)
         return false;
@@ -1730,6 +1733,24 @@ bool TV::RequestNextRecorder(PlayerContext *ctx, bool showDialogs)
         // If this is set we, already got a new recorder in SwitchCards()
         testrec = switchToRec;
         switchToRec = NULL;
+    }
+    else if (!selection.empty())
+    {
+        for (uint i = 0; i < selection.size(); i++)
+        {
+            uint    chanid  = selection[i].chanid;
+            QString channum = selection[i].channum;
+            if (!chanid || channum.isEmpty())
+                continue;
+            QSet<uint> cards = IsTunableOn(ctx, chanid, false, true);
+
+            if (chanid && !channum.isEmpty() && !cards.isEmpty())
+            {
+                testrec = RemoteGetExistingRecorder(*(cards.begin()));
+                initialChanID = chanid;
+                break;
+            }
+        }
     }
     else
     {
@@ -2225,7 +2246,9 @@ void TV::HandleStateChange(PlayerContext *mctx, PlayerContext *ctx)
 
         SET_NEXT();
 
-        uint chanid = gCoreContext->GetNumSetting("DefaultChanid", 0);
+        uint chanid = initialChanID;
+        if (!chanid)
+            chanid = gCoreContext->GetNumSetting("DefaultChanid", 0);
 
         if (chanid && !IsTunable(ctx, chanid))
             chanid = 0;
