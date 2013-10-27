02c66c8d61e4adaade50a41922ccf0f2c6820b11
Paul Harrison <pharrison@mythtv.org>
2013-10-27 22:31:22 +0000

    MythZoneMinder: Add six and eight camera layouts to the live view screen
    The live view can now show 1, 2, 4, 6, or 8 cameras at the same time.
    
    Note to themers: You will need to add the new layouts to the zmliveplayer
    window definition if you have themed it.

--- mythplugins/mythzoneminder/mythzoneminder/zmliveplayer.cpp
+++ mythplugins/mythzoneminder/mythzoneminder/zmliveplayer.cpp
@@ -98,6 +98,22 @@ bool ZMLivePlayer::hideAll(void)
             GetMythUIType(QString("status3-%1").arg(x))->SetVisible(false);
             GetMythUIType(QString("frame3-%1").arg(x))->SetVisible(false);
         }
+
+        // six player layout
+        for (int x = 1; x < 7; x++)
+        {
+            GetMythUIType(QString("name4-%1").arg(x))->SetVisible(false);
+            GetMythUIType(QString("status4-%1").arg(x))->SetVisible(false);
+            GetMythUIType(QString("frame4-%1").arg(x))->SetVisible(false);
+        }
+
+        // eight player layout
+        for (int x = 1; x < 9; x++)
+        {
+            GetMythUIType(QString("name5-%1").arg(x))->SetVisible(false);
+            GetMythUIType(QString("status5-%1").arg(x))->SetVisible(false);
+            GetMythUIType(QString("frame5-%1").arg(x))->SetVisible(false);
+        }
     }
     catch (const QString &name)
     {
@@ -189,7 +205,7 @@ bool ZMLivePlayer::keyPressEvent(QKeyEvent *event)
         else if (action == "INFO")
         {
             m_monitorLayout++;
-            if (m_monitorLayout > 3)
+            if (m_monitorLayout > 5)
                 m_monitorLayout = 1;
             setMonitorLayout(m_monitorLayout);
         }
@@ -319,7 +335,9 @@ void ZMLivePlayer::setMonitorLayout(int layout, bool restore)
     else if (layout == 3)
         m_monitorCount = 4;
     else if (layout == 4)
-        m_monitorCount = 9;
+        m_monitorCount = 6;
+    else if (layout == 5)
+        m_monitorCount = 8;
 
     hideAll();
 
