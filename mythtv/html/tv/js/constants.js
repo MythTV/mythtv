7e537131972d5fc7859856069891394ea0dafdcf
Stuart Morgan <smorgan@mythtv.org>
2013-10-29 17:23:49 +0000

    Initial work on the Recordings page for the internal server
    Please be aware that the first time you view the page it will create
    previews for all the listed programs, this may take a very long
    time and create a high system load. A fix for this will hopefully be
    devised, although it should only be an issue the first time you view the page.

---
+++ mythtv/html/tv/js/constants.js
@@ -0,0 +1,45 @@
+
+// AudioProp
+AUD_STEREO          =  1;
+AUD_MONO            =  2;
+AUD_SURROUND        =  4;
+AUD_DOLBY           =  8;
+AUD_HARDHEAR        = 16;
+AUD_VISUALIMPAIR    = 32;
+
+// VideoProp
+var VID_HDTV        =  1;
+var VID_WIDESCREEN  =  2;
+var VID_AVC         =  4;
+var VID_720         =  8;
+var VID_1080        = 16;
+var VID_DAMAGED     = 32;
+var VID_3DTV        = 64;
+
+// Subtitles
+var SUB_HARDHEAR    =  1;
+var SUB_NORMAL      =  2;
+var SUB_ONSCREEN    =  4;
+var SUB_SIGNED      =  8;
+
+// Program Flags
+var FL_COMMFLAG       =  1;
+var FL_CUTLIST        =  2;
+var FL_AUTOEXP        =  4;
+var FL_EDITING        =  8;
+var FL_BOOKMARK       = 16;
+var FL_REALLYEDITING  = 32;
+var FL_COMMPROCESSING = 64;
+var FL_DELETEPENDING  = 128;
+var FL_TRANSCODED     = 256;
+var FL_WATCHED        = 512;
+var FL_PRESERVED      = 1024;
+var FL_CHANCOMMFREE   = 2048;
+var FL_REPEAT         = 4096;
+var FL_DUPLICATE      = 8192;
+var FL_REACTIVATE     = 16384;
+var FL_IGNOREBOOKMARK = 32768;
+var FL_TYPEMASK       = 983040;
+var FL_INUSERECORDING = 1048576;
+var FL_INUSEPLAYING   = 2097152;
+var FL_INUSEOTHER     = 4194304;
