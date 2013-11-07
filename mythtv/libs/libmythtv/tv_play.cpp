77ddcc1b0f7b3fe791eadeb0fd111eb834d9c1ad
Jim Stichnoth <jstichnoth@mythtv.org>
2013-11-06 16:54:07 -0800

    Prefer the DefaultChanid setting when picking EPG channel.
    This more closely matches the logic of picking the starting channel
    during Live TV startup.  Refs #11922.

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -8327,7 +8327,12 @@ void TV::ShowLCDDVDInfo(const PlayerContext *ctx)
 
 bool TV::IsTunable(const PlayerContext *ctx, uint chanid, bool use_cache)
 {
-    return !IsTunableOn(ctx,chanid,use_cache,true).empty();
+    return !IsTunableOn(this, ctx,chanid,use_cache,true).empty();
+}
+
+bool TV::IsTunable(uint chanid)
+{
+    return !IsTunableOn(NULL, NULL, chanid, false, true).empty();
 }
 
 static QString toCommaList(const QSet<uint> &list)
@@ -8345,7 +8350,16 @@ static QString toCommaList(const QSet<uint> &list)
 QSet<uint> TV::IsTunableOn(
     const PlayerContext *ctx, uint chanid, bool use_cache, bool early_exit)
 {
+    return IsTunableOn(this, ctx, chanid, use_cache, early_exit);
+}
+
+QSet<uint> TV::IsTunableOn(TV *tv,
+    const PlayerContext *ctx, uint chanid, bool use_cache, bool early_exit)
+{
     QSet<uint> tunable_cards;
+
+    if (tv == NULL)
+        use_cache = false;
 
     if (!chanid)
     {
@@ -8359,7 +8373,7 @@ QSet<uint> TV::IsTunableOn(
     mplexid = (32767 == mplexid) ? 0 : mplexid;
 
     vector<uint> excluded_cards;
-    if (ctx->recorder && ctx->pseudoLiveTVState == kPseudoNormalLiveTV)
+    if (ctx && ctx->recorder && ctx->pseudoLiveTVState == kPseudoNormalLiveTV)
         excluded_cards.push_back(ctx->GetCardID());
 
     uint sourceid = ChannelUtil::GetSourceIDForChannel(chanid);
@@ -8396,10 +8410,10 @@ QSet<uint> TV::IsTunableOn(
         bool used_cache = false;
         if (use_cache)
         {
-            QMutexLocker locker(&is_tunable_cache_lock);
-            if (is_tunable_cache_inputs.contains(cardids[i]))
+            QMutexLocker locker(&tv->is_tunable_cache_lock);
+            if (tv->is_tunable_cache_inputs.contains(cardids[i]))
             {
-                inputs = is_tunable_cache_inputs[cardids[i]];
+                inputs = tv->is_tunable_cache_inputs[cardids[i]];
                 used_cache = true;
             }
         }
@@ -8407,8 +8421,11 @@ QSet<uint> TV::IsTunableOn(
         if (!used_cache)
         {
             inputs = RemoteRequestFreeInputList(cardids[i], excluded_cards);
-            QMutexLocker locker(&is_tunable_cache_lock);
-            is_tunable_cache_inputs[cardids[i]] = inputs;
+            if (tv)
+            {
+                QMutexLocker locker(&tv->is_tunable_cache_lock);
+                tv->is_tunable_cache_inputs[cardids[i]] = inputs;
+            }
         }
 
 #if 0
