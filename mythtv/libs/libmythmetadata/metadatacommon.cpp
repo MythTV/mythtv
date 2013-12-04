d95a237d6cf7e79b4b5f807600fd635f37337e2f
Stuart Morgan <smorgan@mythtv.org>
2013-12-04 18:19:49 +0000

    Fix ordering of Artwork in selection dialogs. Best matches should now appear first, not last.

--- mythtv/libs/libmythmetadata/metadatacommon.cpp
+++ mythtv/libs/libmythmetadata/metadatacommon.cpp
@@ -400,7 +400,17 @@ QList<PersonInfo> MetadataLookup::GetPeople(PeopleType type) const
 ArtworkList MetadataLookup::GetArtwork(VideoArtworkType type) const
 {
     ArtworkList ret;
-    ret = m_artwork.values(type);
+
+    // QMultiMap::values() returns items in reverse order which we need to
+    // correct by iterating back over the list
+    // See http://qt-project.org/doc/qt-4.8/qmultimap.html#details
+    // Specifically "The items that share the same key are available from "
+    //              "most recently to least recently inserted."
+    QListIterator<ArtworkInfo> it(m_artwork.values(type));
+    it.toBack();
+    while (it.hasPrevious())
+        ret.append(it.previous());
+
     return ret;
 }
 
