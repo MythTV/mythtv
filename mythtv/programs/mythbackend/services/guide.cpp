732ed18caa947951d0e85d81838c79ef4b376f50
Stuart Morgan <smorgan@mythtv.org>
2013-12-17 18:54:52 +0000

    Some builds of mysql don't accept INT as a castable type, use UNSIGNED instead.

--- mythtv/programs/mythbackend/services/guide.cpp
+++ mythtv/programs/mythbackend/services/guide.cpp
@@ -111,7 +111,7 @@ DTC::ProgramGuide *Guide::GetProgramGuide( const QDateTime &rawStartTime ,
                 "AND program.endtime >= :StartDate "
                 "AND program.starttime <= :EndDate "
                 "GROUP BY program.starttime, channel.chanid "
-                "ORDER BY LPAD(CAST(channum AS INT), 10, 0), "
+                "ORDER BY LPAD(CAST(channum AS UNSIGNED), 10, 0), "
                 "         LPAD(channum,  10, 0),             "
                 "         callsign,                          "
                 "         LPAD(program.chanid, 10, 0),       "
