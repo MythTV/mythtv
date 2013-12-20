MythTV/mythtv

commit 66fbffa29de034e5c55d72d69c9f137626817871

Services: Fix an off-by one error when using the StartIndex arg with GetRecordedList.
This also optimises things so that we aren't performing redundant
string/regexp comparisons after reaching Count.

mythtv/programs/mythbackend/services/dvr.cpp
@@ -97,6 +97,9 @@ DTC::ProgramList* Dvr::GetRecordedList( bool           bDescending,
 
     for( unsigned int n = 0; n < progList.size(); n++)
     {
+        if (nCount >= nMax)
+            break;
+
         ProgramInfo *pInfo = progList[ n ];
 
         if ((!sTitleRegEx.isEmpty() && !pInfo->GetTitle().contains(rTitleRegEx)) ||
@@ -104,12 +107,13 @@ DTC::ProgramList* Dvr::GetRecordedList( bool           bDescending,
             (!sStorageGroup.isEmpty() && sStorageGroup != pInfo->GetStorageGroup()))
             continue;
 
-        ++nAvailable;
-
-        if ((nAvailable < nStartIndex) ||
-            (nCount >= nMax))
+        if (nAvailable < nStartIndex)
+        {
+            ++nAvailable;
             continue;
+        }
 
+        ++nAvailable;
         ++nCount;
 
         DTC::Program *pProgram = pPrograms->AddNewProgram();

commit 715367fdb2a73ed4f9cb3fdaab21385f8122be7f

Webfrontend: Fix recordings list paging. An 'optimisation' in 66fbffa2 broke the TotalAvailable count so I've reverted it.

mythtv/programs/mythbackend/services/dvr.cpp
@@ -101,9 +101,6 @@ DTC::ProgramList* Dvr::GetRecordedList( bool           bDescending,
 
     for( unsigned int n = 0; n < progList.size(); n++)
     {
-        if (nCount >= nMax)
-            break;
-
         ProgramInfo *pInfo = progList[ n ];
 
         if ((!sTitleRegEx.isEmpty() && !pInfo->GetTitle().contains(rTitleRegEx)) ||
@@ -111,7 +108,8 @@ DTC::ProgramList* Dvr::GetRecordedList( bool           bDescending,
             (!sStorageGroup.isEmpty() && sStorageGroup != pInfo->GetStorageGroup()))
             continue;
 
-        if (nAvailable < nStartIndex)
+        if ((nAvailable < nStartIndex) ||
+            (nCount >= nMax))
         {
             ++nAvailable;
             continue;