c7115a359bfac44fdee8dc447c0da654109f66fe
Raymond Wagner <rwagner@mythtv.org>
2014-01-13 09:44:02 -0500

    Filter database age to reasonable limits
    This makes smolt ignore recordings claimed to have been made before
    MythTV was released, when computing the age of the database.

--- mythtv/programs/scripts/hardwareprofile/distros/mythtv_data/data_mythtv.py
+++ mythtv/programs/scripts/hardwareprofile/distros/mythtv_data/data_mythtv.py
@@ -140,8 +140,10 @@ class _Mythtv_data:
         oldest = recs[0]
         shows = []
 
+        maxage = MythTV.utility.datetime(2001,1,1,0,0)
+
         for rec in recs:
-            if rec.starttime < oldest.starttime:
+            if (rec.starttime < oldest.starttime) and (rec.starttime > maxage):
                 oldest = rec
             data.rectime += self.td_to_secs(rec.endtime - rec.starttime)
             if rec.recstatus == -3:
