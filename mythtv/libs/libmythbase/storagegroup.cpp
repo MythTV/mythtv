MythTV/mythtv

commit 907d9fe5111ee2a164d04e40405d9e1dd4162165

Silence some cppcheck warnings about const variables not passed by reference.

mythtv/libs/libmythbase/storagegroup.cpp
@@ -43,7 +43,7 @@ const QStringList StorageGroup::kSpecialGroups = QStringList()
  *  \param allowFallback allow the storage group search code to fall back if
  *                  no dirs exist for the specified group/host
  */
-StorageGroup::StorageGroup(const QString group, const QString hostname,
+StorageGroup::StorageGroup(const QString &group, const QString &hostname,
                            bool allowFallback) :
     m_groupname(group), m_hostname(hostname), m_allowFallback(allowFallback)
 {
@@ -99,7 +99,7 @@ void StorageGroup::StaticInit(void)
  *  \param group    The name of the Storage Group
  *  \param hostname The host whose Storage Group definition is desired
  */
-void StorageGroup::Init(const QString group, const QString hostname,
+void StorageGroup::Init(const QString &group, const QString &hostname,
                         const bool allowFallback)
 {
     bool found = false;
@@ -201,10 +201,12 @@ QString StorageGroup::GetFirstDir(bool appendSlash) const
     return tmp;
 }
 
-QStringList StorageGroup::GetDirFileList(QString dir, QString base,
+QStringList StorageGroup::GetDirFileList(const QString &dir,
+                                         const QString &lbase,
                                          bool recursive, bool onlyDirs)
 {
     QStringList files;
+    QString base = lbase;
     QDir d(dir);
 
     if (!d.exists())
@@ -253,7 +255,7 @@ QStringList StorageGroup::GetDirFileList(QString dir, QString base,
     return files;
 }
 
-QStringList StorageGroup::GetDirList(QString Path, bool recursive)
+QStringList StorageGroup::GetDirList(const QString &Path, bool recursive)
 {
     QStringList files;
     QString tmpDir;
@@ -268,7 +270,7 @@ QStringList StorageGroup::GetDirList(QString Path, bool recursive)
     return files;
 }
 
-QStringList StorageGroup::GetFileList(QString Path, bool recursive)
+QStringList StorageGroup::GetFileList(const QString &Path, bool recursive)
 {
     QStringList files;
     QString tmpDir;
@@ -286,7 +288,7 @@ QStringList StorageGroup::GetFileList(QString Path, bool recursive)
     return files;
 }
 
-QStringList StorageGroup::GetFileInfoList(QString Path)
+QStringList StorageGroup::GetFileInfoList(const QString &Path)
 {
     QStringList files;
     QString relPath;
@@ -348,7 +350,7 @@ QStringList StorageGroup::GetFileInfoList(QString Path)
     return files;
 }
 
-bool StorageGroup::FileExists(QString filename)
+bool StorageGroup::FileExists(const QString &filename)
 {
     LOG(VB_FILE, LOG_DEBUG, LOC +
         QString("FileExist: Testing for '%1'").arg(filename));
@@ -380,8 +382,9 @@ bool StorageGroup::FileExists(QString filename)
 
 // Returns a string list of details about the file
 // in the order EXISTS, DATE, SIZE
-QStringList StorageGroup::GetFileInfo(QString filename)
+QStringList StorageGroup::GetFileInfo(const QString &lfilename)
 {
+    QString filename;
     LOG(VB_FILE, LOG_DEBUG, LOC +
         QString("GetFileInfo: For '%1'") .arg(filename));
 
@@ -531,7 +534,7 @@ QString StorageGroup::GetRelativePathname(const QString &filename)
  *  \param dirlist  Optional pointer to a QStringList to hold found dir list
  *  \return         true if directories were found
  */
-bool StorageGroup::FindDirs(const QString group, const QString hostname,
+bool StorageGroup::FindDirs(const QString &group, const QString &hostname,
                             QStringList *dirlist)
 {
     bool found = false;
@@ -597,7 +600,7 @@ bool StorageGroup::FindDirs(const QString group, const QString hostname,
     return found;
 }
 
-QString StorageGroup::FindFile(QString filename)
+QString StorageGroup::FindFile(const QString &filename)
 {
     LOG(VB_FILE, LOG_DEBUG, LOC + QString("FindFile: Searching for '%1'")
                                       .arg(filename));
@@ -620,7 +623,7 @@ QString StorageGroup::FindFile(QString filename)
     return result;
 }
 
-QString StorageGroup::FindFileDir(QString filename)
+QString StorageGroup::FindFileDir(const QString &filename)
 {
     QString result = "";
     QFileInfo checkFile("");
@@ -817,7 +820,8 @@ QStringList StorageGroup::getRecordingsGroups(void)
     return groups;
 }
 
-QStringList StorageGroup::getGroupDirs(QString groupname, QString host)
+QStringList StorageGroup::getGroupDirs(const QString &groupname,
+                                       const QString &host)
 {
     QStringList groups;
     QString addHost;

mythtv/libs/libmythbase/mythversion.h
@@ -12,7 +12,7 @@
 /// Update this whenever the plug-in ABI changes.
 /// Including changes in the libmythbase, libmyth, libmythtv, libmythav* and
 /// libmythui class methods in exported headers.
-#define MYTH_BINARY_VERSION "0.28.20131206-1"
+#define MYTH_BINARY_VERSION "0.28.20131216-1"
 
 /** \brief Increment this whenever the MythTV network protocol changes.
  *

mythtv/libs/libmythbase/storagegroup.h
@@ -11,11 +11,11 @@
 class MBASE_PUBLIC StorageGroup
 {
   public:
-    StorageGroup(const QString group = "", const QString hostname = "",
+    StorageGroup(const QString &group = "", const QString &hostname = "",
                  const bool allowFallback = true);
 
-    void    Init(const QString group = "Default",
-                 const QString hostname = "",
+    void    Init(const QString &group = "Default",
+                 const QString &hostname = "",
                  const bool allowFallback = true);
 
     QString getName(void) const
@@ -25,20 +25,20 @@ class MBASE_PUBLIC StorageGroup
         { QStringList tmp = m_dirlist; tmp.detach(); return tmp; }
     QString GetFirstDir(bool appendSlash = false) const;
 
-    QStringList GetDirFileList(QString dir, QString base,
+    QStringList GetDirFileList(const QString &dir, const QString &base,
                                bool recursive = false, bool onlyDirs = false);
-    QStringList GetDirList(QString Path, bool recursive = false);
-    QStringList GetFileList(QString Path, bool recursive = false);
-    QStringList GetFileInfoList(QString Path);
-    bool FileExists(QString filename);
-    QStringList GetFileInfo(QString filename);
+    QStringList GetDirList(const QString &Path, bool recursive = false);
+    QStringList GetFileList(const QString &Path, bool recursive = false);
+    QStringList GetFileInfoList(const QString &Path);
+    bool FileExists(const QString &filename);
+    QStringList GetFileInfo(const QString &filename);
     static QString GetRelativePathname(const QString &filename);
-    static bool FindDirs(const QString group = "Default",
-                         const QString hostname = "",
+    static bool FindDirs(const QString &group = "Default",
+                         const QString &hostname = "",
                          QStringList *dirlist = NULL);
 
-    QString FindFile(QString filename);
-    QString FindFileDir(QString filename);
+    QString FindFile(const QString &filename);
+    QString FindFileDir(const QString &filename);
 
     QString FindNextDirMostFree(void);
 
@@ -48,7 +48,8 @@ class MBASE_PUBLIC StorageGroup
     static const QStringList kSpecialGroups;
 
     static QStringList getRecordingsGroups(void);
-    static QStringList getGroupDirs(QString groupname, QString host);
+    static QStringList getGroupDirs(const QString &groupname,
+                                    const QString &host);
 
     static void ClearGroupToUseCache(void);
     static QString GetGroupToUse(

mythtv/libs/libmythmetadata/metadatacommon.cpp
@@ -119,10 +119,10 @@ MetadataLookup::MetadataLookup(
     const QString &seriesid,
     const QString &programid,
     const QString &storagegroup,
-    const QDateTime startts,
-    const QDateTime endts,
-    const QDateTime recstartts,
-    const QDateTime recendts,
+    const QDateTime &startts,
+    const QDateTime &endts,
+    const QDateTime &recstartts,
+    const QDateTime &recendts,
     uint programflags,
     uint audioproperties,
     uint videoproperties,
@@ -136,8 +136,8 @@ MetadataLookup::MetadataLookup(
     uint tracknum,
     const QString &system,
     const uint year,
-    const QDate releasedate,
-    const QDateTime lastupdated,
+    const QDate &releasedate,
+    const QDateTime &lastupdated,
     const uint runtime,
     const uint runtimesecs,
     const QString &inetref,
@@ -247,17 +247,17 @@ MetadataLookup::MetadataLookup(
     const QString &seriesid,
     const QString &programid,
     const QString &storagegroup,
-    const QDateTime startts,
-    const QDateTime endts,
-    const QDateTime recstartts,
-    const QDateTime recendts,
+    const QDateTime &startts,
+    const QDateTime &endts,
+    const QDateTime &recstartts,
+    const QDateTime &recendts,
     uint programflags,
     uint audioproperties,
     uint videoproperties,
     uint subtitletype,
     const uint year,
-    const QDate releasedate,
-    const QDateTime lastupdated,
+    const QDate &releasedate,
+    const QDateTime &lastupdated,
     const uint runtime,
     const uint runtimesecs) :
     ReferenceCounter("MetadataLookup"),

mythtv/libs/libmythmetadata/metadatacommon.h
@@ -119,10 +119,10 @@ class META_PUBLIC MetadataLookup : public QObject, public ReferenceCounter
         const QString &seriesid,
         const QString &programid,
         const QString &storagegroup,
-        const QDateTime startts,
-        const QDateTime endts,
-        const QDateTime recstartts,
-        const QDateTime recendts,
+        const QDateTime &startts,
+        const QDateTime &endts,
+        const QDateTime &recstartts,
+        const QDateTime &recendts,
         const uint programflags,
         const uint audioproperties,
         const uint videoproperties,
@@ -136,8 +136,8 @@ class META_PUBLIC MetadataLookup : public QObject, public ReferenceCounter
         uint tracknum,
         const QString &system,
         const uint year,
-        const QDate releasedate,
-        const QDateTime lastupdated,
+        const QDate &releasedate,
+        const QDateTime &lastupdated,
         const uint runtime,
         const uint runtimesecs,
         const QString &inetref,
@@ -179,17 +179,17 @@ class META_PUBLIC MetadataLookup : public QObject, public ReferenceCounter
         const QString &seriesid,
         const QString &programid,
         const QString &storagegroup,
-        const QDateTime startts,
-        const QDateTime endts,
-        const QDateTime recstartts,
-        const QDateTime recendts,
+        const QDateTime &startts,
+        const QDateTime &endts,
+        const QDateTime &recstartts,
+        const QDateTime &recendts,
         uint programflags,
         uint audioproperties,
         uint videoproperties,
         uint subtitletype,
         const uint year,
-        const QDate releasedate,
-        const QDateTime lastupdated,
+        const QDate &releasedate,
+        const QDateTime &lastupdated,
         const uint runtime,
         const uint runtimesecs);
 

commit 72224c7b85b351fa83e47d88fab305c1ec2e5e42

StorageGroup: Fix a bug introduced in 907d9fe5 to silence cppcheck warnings

mythtv/libs/libmythbase/storagegroup.cpp
@@ -386,7 +386,7 @@ bool StorageGroup::FileExists(const QString &filename)
 // in the order EXISTS, DATE, SIZE
 QStringList StorageGroup::GetFileInfo(const QString &lfilename)
 {
-    QString filename;
+    QString filename = lfilename;
     LOG(VB_FILE, LOG_DEBUG, LOC +
         QString("GetFileInfo: For '%1'") .arg(filename));
 