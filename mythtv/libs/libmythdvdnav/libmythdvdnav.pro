MythTV/mythtv

commit e3a596417cae99b8879106aaf5fc8acab3493ad5

Move libdvdnav installation header files to a subdirectory to match R1160 of libdvdnav (and make it easier to compare our version with the official version)

mythtv/libs/libmythtv/libmythtv.pro
@@ -257,7 +257,7 @@ INSTALLS += inc
 #DVD stuff
 DEPENDPATH  += ../libmythdvdnav/
 DEPENDPATH  += ../libmythdvdnav/dvdread # for dvd_reader.h & dvd_input.h
-INCLUDEPATH += ../libmythdvdnav
+INCLUDEPATH += ../libmythdvdnav/dvdnav
 
 !win32-msvc*:POST_TARGETDEPS += ../libmythdvdnav/libmythdvdnav-$${MYTH_LIB_EXT}
 

mythtv/programs/programs-libs.pro
@@ -2,7 +2,7 @@ INCLUDEPATH += ../.. ../../libs/ ../../libs/libmyth ../../libs/libmyth/audio
 INCLUDEPATH +=  ../../libs/libmythtv ../../external/FFmpeg
 INCLUDEPATH += ../../libs/libmythupnp ../../libs/libmythui ../../libs/libmythmetadata
 INCLUDEPATH += ../../libs/libmythlivemedia ../../libs/libmythbase
-INCLUDEPATH += ../../libs/libmythdvdnav ../../external/libmythbluray
+INCLUDEPATH += ../../libs/libmythdvdnav/dvdnav ../../external/libmythbluray
 INCLUDEPATH += ../../external/libsamplerate
 INCLUDEPATH += ../../libs/libmythtv/mpeg
 INCLUDEPATH += ../../libs/libmythtv/vbitext

commit 414a3b2f0174962131a45aa814184348ef0a299a

Fix compilation problems caused by [e3a596417c] and [df2fdd325b]

mythtv/libs/libmythdvdnav/libmythdvdnav.pro
@@ -7,7 +7,7 @@ CONFIG -= qt
 target.path = $${LIBDIR}
 
 INCLUDEPATH += . ../../
-INCLUDEPATH += ./dvdnav
+INCLUDEPATH += ./dvdnav ./dvdread
 INCLUDEPATH += ../libmythbase
 INCLUDEPATH += ../libmythtv
 

mythtv/libs/libmythtv/libmythtv.pro
@@ -258,6 +258,7 @@ INSTALLS += inc
 DEPENDPATH  += ../libmythdvdnav/
 DEPENDPATH  += ../libmythdvdnav/dvdread # for dvd_reader.h & dvd_input.h
 INCLUDEPATH += ../libmythdvdnav/dvdnav
+INCLUDEPATH += ../libmythdvdnav/dvdread
 
 !win32-msvc*:POST_TARGETDEPS += ../libmythdvdnav/libmythdvdnav-$${MYTH_LIB_EXT}
 

mythtv/programs/programs-libs.pro
@@ -2,7 +2,8 @@ INCLUDEPATH += ../.. ../../libs/ ../../libs/libmyth ../../libs/libmyth/audio
 INCLUDEPATH +=  ../../libs/libmythtv ../../external/FFmpeg
 INCLUDEPATH += ../../libs/libmythupnp ../../libs/libmythui ../../libs/libmythmetadata
 INCLUDEPATH += ../../libs/libmythlivemedia ../../libs/libmythbase
-INCLUDEPATH += ../../libs/libmythdvdnav/dvdnav ../../external/libmythbluray
+INCLUDEPATH += ../../libs/libmythdvdnav/dvdnav ../../libs/libmythdvdnav/dvdread
+INCLUDEPATH += ../../external/libmythbluray
 INCLUDEPATH += ../../external/libsamplerate
 INCLUDEPATH += ../../libs/libmythtv/mpeg
 INCLUDEPATH += ../../libs/libmythtv/vbitext