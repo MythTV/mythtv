c10b9bcc6f5f3c76c86b12144ce76c2f4119660c
Paul Harrison <pharrison@mythtv.org>
2014-01-22 10:49:06 +0000

    bump the API version so both core and plugins need to be recompiled.

--- mythtv/libs/libmythbase/mythversion.h
+++ mythtv/libs/libmythbase/mythversion.h
@@ -12,7 +12,7 @@
 /// Update this whenever the plug-in ABI changes.
 /// Including changes in the libmythbase, libmyth, libmythtv, libmythav* and
 /// libmythui class methods in exported headers.
-#define MYTH_BINARY_VERSION "0.28.20140117-1"
+#define MYTH_BINARY_VERSION "0.28.20140122-1"
 
 /** \brief Increment this whenever the MythTV network protocol changes.
  *
