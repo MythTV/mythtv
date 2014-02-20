8c22370d1c83bbd4cadd7e921507571356864e45
Stuart Morgan <smorgan@mythtv.org>
2014-02-20 12:10:26 +0000

    Improve objectDebug() formatting

--- mythtv/html/js/utility.js
+++ mythtv/html/js/utility.js
@@ -32,17 +32,9 @@ function isValidObject(variable)
 }
 
 // For debugging only, this isn't JSON
-function objectDump(arr,level)
+function objectDump(arr)
 {
-    var objectString = "<br/>\n";
-    if (!level)
-        level = 0;
-
-    var indentation = "";
-    for (var j = 0; j < (level + 1); j++)
-    {
-        indentation += "    ";
-    }
+    var objectString = "<dl>\n";
 
     var count = 1;
     if (typeof(arr) == 'object')
@@ -51,22 +43,23 @@ function objectDump(arr,level)
         {
             var value = arr[item];
 
-            objectString += count + ". ";
+            objectString += "<dt>" + count + ". ";
             if (typeof(value) == 'object')
             {
-                objectString += indentation + "'" + item + "' ...<br/>\n";
-                objectString += objectDump(value, level+1);
+                objectString += "'" + item + "' \n";
+                objectString += "<dd>" + objectDump(value) + "</dd></dt>";
             }
             else
             {
-                objectString += indentation + "'" + item + "' => \"" + value + "\"<br/>\n";
+                objectString += "'" + item + "' => \"" + value + "\"</dt>\n";
             }
             count++;
         }
     }
     else
     {
-        objectString = "===>"+arr+"<===("+typeof(arr)+")";
+        objectString = "<dt>===>"+arr+"<===("+typeof(arr)+")</dt>";
     }
+    objectString += "</dl>\n";
     return objectString;
 }
