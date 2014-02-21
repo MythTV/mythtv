1034586db74d3ef87874730445a18f05cec37738
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-02-22 01:01:08 +1100

    Unregister file only after it was closed
    Just in case we attempt to read just at that time

--- mythtv/libs/libmythtv/fileringbuffer.cpp
+++ mythtv/libs/libmythtv/fileringbuffer.cpp
@@ -93,11 +93,6 @@ FileRingBuffer::~FileRingBuffer()
 {
     KillReadAheadThread();
 
-    if (writemode)
-    {
-        UnregisterFileForWrite();
-    }
-
     delete remotefile;
     remotefile = NULL;
 
@@ -108,6 +103,11 @@ FileRingBuffer::~FileRingBuffer()
     {
         close(fd2);
         fd2 = -1;
+    }
+
+    if (writemode)
+    {
+        UnregisterFileForWrite();
     }
 }
 
@@ -411,12 +411,12 @@ bool FileRingBuffer::ReOpen(QString newFilename)
 
     rwlock.lockForWrite();
 
-    UnregisterFileForWrite();
-
     if (tfw && tfw->ReOpen(newFilename))
         result = true;
     else if (remotefile && remotefile->ReOpen(newFilename))
         result = true;
+
+    UnregisterFileForWrite();
 
     if (result)
     {
