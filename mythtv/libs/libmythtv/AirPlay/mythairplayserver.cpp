d567ccb51761f0306b1930a2f56abb11c4b81112
Dave Badia <dbadia@gmail.com>
2014-03-20 17:21:56 -0400

    Trigger system events when devices connect and disconnect to AirPlay and AirTunes
    Fixes #11182
    Signed-off-by: Jean-Yves Avenard <jyavenard@mythtv.org>
    
    (cherry picked from commit c5306f3ba063de7b25ce3cfc3757f9b8998b251b)

--- mythtv/libs/libmythtv/AirPlay/mythairplayserver.cpp
+++ mythtv/libs/libmythtv/AirPlay/mythairplayserver.cpp
@@ -520,6 +520,7 @@ void MythAirplayServer::newConnection(QTcpSocket *client)
     LOG(VB_GENERAL, LOG_INFO, LOC + QString("New connection from %1:%2")
         .arg(client->peerAddress().toString()).arg(client->peerPort()));
 
+    gCoreContext->SendSystemEvent(QString("AIRPLAY_NEW_CONNECTION"));
     m_sockets.append(client);
     connect(client, SIGNAL(disconnected()), this, SLOT(deleteConnection()));
     connect(client, SIGNAL(readyRead()), this, SLOT(read()));
@@ -543,6 +544,7 @@ void MythAirplayServer::deleteConnection(QTcpSocket *socket)
     // must have lock
     LOG(VB_GENERAL, LOG_INFO, LOC + QString("Removing connection %1:%2")
         .arg(socket->peerAddress().toString()).arg(socket->peerPort()));
+    gCoreContext->SendSystemEvent(QString("AIRPLAY_DELETE_CONNECTION"));
     m_sockets.removeOne(socket);
 
     QByteArray remove;
