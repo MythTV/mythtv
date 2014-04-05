1b68d626f44818ccf55ed603fe799487ba8f75bb
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-04-06 00:15:04 +1100

    My analysis for the problem in 96ecd539de8e76a880f0013b4b8854da216e17b1 was wrong. The actual issue is that QPalette inheritance appears broken on a mac; if you set the palette to the top widget container, it doesn’t do anything on its children. However, setting the palette to the main application itself does.
    Now things look exactly the same between mac and linux for better or worse. Including font sizes and colors.
    A downside, is that the selection color appears to be misused in most themes including the default. So you can't tell if a text is selected or simply having focus
    Qt bug: QTBUG-32067, QTBUG-26144 (over 224 bugs related to palette use in Qt as of 2014-04-05)
    
    (cherry picked from commit 103d4651e68215e661c843ca5862d7d64c052f93)

--- mythtv/libs/libmythui/mythmainwindow.cpp
+++ mythtv/libs/libmythui/mythmainwindow.cpp
@@ -991,6 +991,11 @@ void MythMainWindow::Init(QString forcedpainter)
     setFixedSize(QSize(d->screenwidth, d->screenheight));
 
     GetMythUI()->ThemeWidget(this);
+#ifdef Q_OS_MAC
+    // QPalette inheritance appears broken on mac, so there's no point setting the palette
+    // to the top widget each time. Instead we apply the default palette to the whole application
+    qApp->setPalette(palette());
+#endif
     Show();
 
     if (!GetMythDB()->GetNumSetting("HideMouseCursor", 0))
