cfa3260ecd71adf2dedcd58187a57d973715846b
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-04-07 13:55:55 +1000

    Fixes Video Playback Profile Editor.
    Two corrected issues:
    1- Adding or Editing an existing profile, did nothing visually, but would actually update the database. Regression was introduced in 9686f034ac54
    2- When deleting a profile, the newly selected one wasn’t actually saved as default
    
    (cherry picked from commit c2b22cb99313cbcd496cdbd7593edc853bac41df)
    
    Conflicts:
    	mythtv/libs/libmyth/mythconfiggroups.h

--- mythtv/libs/libmyth/mythconfiggroups.h
+++ mythtv/libs/libmyth/mythconfiggroups.h
@@ -93,7 +93,7 @@ class MPUBLIC VerticalConfigurationGroup : public ConfigurationGroup
 
   private:
     vector<QWidget*>    childwidget;
-    QGroupBox          *widget;
+    QWidget            *widget;
     ConfigurationGroup *confgrp;
     QVBoxLayout        *layout;
 };
