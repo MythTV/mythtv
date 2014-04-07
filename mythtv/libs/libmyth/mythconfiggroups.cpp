MythTV/mythtv

commit 9686f034ac54c6af579a9c209554f678e9192395

Refs #5596. Use Qt4 layout managers instead of Qt3 versions in libmyth. Note this does require relinking the plugins.
git-svn-id: http://svn.mythtv.org/svn/trunk@18605 7dbf422c-18fa-0310-86e9-fd20926502f2

mythtv/libs/libmyth/mythconfiggroups.cpp
@@ -2,6 +2,7 @@
 
 #include <q3widgetstack.h>
 #include <q3tabdialog.h>
+#include <QGroupBox>
 
 #include "mythconfiggroups.h"
 #include "mythcontext.h"
@@ -28,7 +29,7 @@ ConfigurationGroup::ConfigurationGroup(bool luselabel,   bool luseframe,
     // Pre-calculate the margin and spacing that all sub-classes will use:
 
     if (lzeroMargin)
-        margin = 4;
+        margin = 4 * 0.5;
     else
     {
         float wmult = 0, hmult = 0;
@@ -36,12 +37,12 @@ ConfigurationGroup::ConfigurationGroup(bool luselabel,   bool luseframe,
         GetMythUI()->GetScreenSettings(wmult, hmult);
 
         if (luselabel)
-            margin = (int)(28 * hmult);
+            margin = (int)(28 * hmult * 0.5);
         else
-            margin = (int)(10 * hmult);
+            margin = (int)(10 * hmult * 0.5);
     }
 
-    space = (lzeroSpace) ? 4 : -1;
+    space = (lzeroSpace) ? 4 * 0.5 : -1;
 }
 
 ConfigurationGroup::~ConfigurationGroup()
@@ -107,31 +108,22 @@ void ConfigurationGroup::Save(QString destination)
             (*it)->GetStorage()->Save(destination);
 }
 
-QWidget* VerticalConfigurationGroup::configWidget(ConfigurationGroup *cg, 
-                                                  QWidget* parent,
-                                                  const char* widgetName) 
+QWidget *VerticalConfigurationGroup::configWidget(
+    ConfigurationGroup *cg, 
+    QWidget            *parent,
+    const char         *widgetName) 
 {
-    widget = new Q3GroupBox(parent, widgetName);
-    connect(widget, SIGNAL(destroyed(QObject*)),
-            this,   SLOT(widgetDeleted(QObject*)));
-
-    widget->setBackgroundOrigin(QWidget::WindowOrigin);
-
-    if (!useframe)
-        widget->setFrameShape(Q3GroupBox::NoFrame);
-
-    layout = new QVBoxLayout(widget, margin, space);
-
-    if (uselabel)
-        widget->setTitle(getLabel());
+    layout = new QVBoxLayout();
+    layout->setMargin(margin);
+    layout->setSpacing((space<0) ? margin : space);
 
     childwidget.resize(children.size());
     for (uint i = 0; i < children.size(); i++)
     {
         if (children[i] && children[i]->isVisible())
         {
-            childwidget[i] = children[i]->configWidget(cg, widget, NULL);
-            layout->add(childwidget[i]);
+            childwidget[i] = children[i]->configWidget(cg, NULL, NULL);
+            layout->addWidget(childwidget[i]);
             children[i]->setEnabled(children[i]->isEnabled());
         }
     }
@@ -143,6 +135,30 @@ QWidget* VerticalConfigurationGroup::configWidget(ConfigurationGroup *cg,
         confgrp = cg;
     } 
 
+    QWidget *widget = NULL;
+    if (uselabel)
+    {
+        QGroupBox *groupbox = new QGroupBox(parent);
+        groupbox->setObjectName(QString("VCG(%1)_groupbox").arg(widgetName));
+        groupbox->setTitle(getLabel());
+        widget = groupbox;
+    }
+    else if (useframe)
+    {
+        QFrame *frame = new QFrame(parent);
+        frame->setFrameStyle(QFrame::Box);
+        frame->setObjectName(QString("VCG(%1)_frame").arg(widgetName));
+        widget = frame;
+    }
+    else
+    {
+        widget = new QWidget(parent);
+        widget->setObjectName(QString("VCG(%1)_widget").arg(widgetName));
+    }
+
+    widget->setLayout(layout);
+    connect(widget, SIGNAL(destroyed(QObject*)),
+            this,   SLOT(widgetDeleted(QObject*)));
     return widget;
 }
 
@@ -177,7 +193,7 @@ bool VerticalConfigurationGroup::replaceChild(
         if (childwidget[i])
         {
             old_child->widgetInvalid(childwidget[i]);
-            layout->remove(childwidget[i]);
+            layout->removeWidget(childwidget[i]);
             childwidget[i]->deleteLater();
             childwidget[i] = NULL;
         }
@@ -190,7 +206,7 @@ bool VerticalConfigurationGroup::replaceChild(
         if (was_visible)
         {
             childwidget[i] = new_child->configWidget(confgrp, widget, NULL);
-            layout->add(childwidget[i]);
+            layout->addWidget(childwidget[i]);
             new_child->setEnabled(was_enabled);
             childwidget[i]->resize(1,1);
             childwidget[i]->show();
@@ -208,78 +224,103 @@ void VerticalConfigurationGroup::repaint(void)
         widget->repaint();
 }
 
-QWidget* HorizontalConfigurationGroup::configWidget(ConfigurationGroup *cg, 
-                                                    QWidget* parent, 
-                                                    const char* widgetName) 
+QWidget *HorizontalConfigurationGroup::configWidget(
+    ConfigurationGroup *cg, 
+    QWidget            *parent, 
+    const char         *widgetName) 
 {
-    Q3GroupBox* widget = new Q3GroupBox(parent, widgetName);
-    widget->setBackgroundOrigin(QWidget::WindowOrigin);
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->setMargin(margin);
+    layout->setSpacing((space<0) ? margin : space);
 
-    if (!useframe)
-        widget->setFrameShape(Q3GroupBox::NoFrame);
-
-    QHBoxLayout *layout = new QHBoxLayout(widget, margin, space);
-
-    if (uselabel)
+    for (uint i = 0 ; i < children.size() ; ++i)
     {
-        // This makes weird and bad things happen in qt -mdz 2002/12/28
-        //widget->setInsideMargin(20);
-        widget->setTitle(getLabel());
-    }
-
-    for(unsigned i = 0 ; i < children.size() ; ++i)
-        if (children[i]->isVisible())
+        if (children[i] && children[i]->isVisible())
         {
-            QWidget *child = children[i]->configWidget(cg, widget, NULL);
-            layout->add(child);
+            QWidget *child = children[i]->configWidget(cg, NULL, NULL);
+            layout->addWidget(child);
             children[i]->setEnabled(children[i]->isEnabled());
         }
+    }
 
     if (cg)
     {
-        connect(this, SIGNAL(changeHelpText(QString)), cg,
-                SIGNAL(changeHelpText(QString)));
+        connect(this, SIGNAL(changeHelpText(QString)),
+                cg,   SIGNAL(changeHelpText(QString)));
     }
 
+    QWidget *widget = NULL;
+    if (uselabel)
+    {
+        QGroupBox *groupbox = new QGroupBox(parent);
+        groupbox->setObjectName(QString("HCG(%1)_groupbox").arg(widgetName));
+        groupbox->setTitle(getLabel());
+        widget = groupbox;
+    }
+    else if (useframe)
+    {
+        QFrame *frame = new QFrame(parent);
+        frame->setFrameStyle(QFrame::Box);
+        frame->setObjectName(QString("HCG(%1)_frame").arg(widgetName));
+        widget = frame;
+    }
+    else
+    {
+        widget = new QWidget(parent);
+        widget->setObjectName(QString("HCG(%1)_widget").arg(widgetName));
+    }
+
+    widget->setLayout(layout);
     return widget;
 }
 
-QWidget* GridConfigurationGroup::configWidget(ConfigurationGroup *cg, 
-                                              QWidget* parent, 
-                                              const char* widgetName)
+QWidget* GridConfigurationGroup::configWidget(
+    ConfigurationGroup *cg,
+    QWidget            *parent,
+    const char         *widgetName)
 {
-    Q3GroupBox* widget = new Q3GroupBox(parent, widgetName);
-    widget->setBackgroundOrigin(QWidget::WindowOrigin);
+    QGridLayout *layout = new QGridLayout();
+    layout->setMargin(margin);
+    layout->setSpacing(space < 0 ? margin : space);
 
-    if (!useframe)
-        widget->setFrameShape(Q3GroupBox::NoFrame);
-
-    QGridLayout *layout = NULL;
-
-    int rows = (children.size()+columns-1) / columns;
-
-    layout = new QGridLayout(widget, rows, columns, margin, space);
-
-    if (uselabel)
+    for (uint i = 0; i < children.size(); i++)
     {
-        // This makes weird and bad things happen in qt -mdz 2002/12/28
-        //widget->setInsideMargin(20);
-        widget->setTitle(getLabel());
-    }
-
-    for (unsigned i = 0 ; i < children.size() ; ++i)
-        if (children[i]->isVisible())
+        if (children[i] && children[i]->isVisible())
         {
-            QWidget *child = children[i]->configWidget(cg, widget, NULL);
+            QWidget *child = children[i]->configWidget(cg, NULL, NULL);
             layout->addWidget(child, i / columns, i % columns);
             children[i]->setEnabled(children[i]->isEnabled());
         }
+    }
 
     if (cg)
     {
-        connect(this, SIGNAL(changeHelpText(QString)), cg,
-                SIGNAL(changeHelpText(QString)));
+        connect(this, SIGNAL(changeHelpText(QString)),
+                cg,   SIGNAL(changeHelpText(QString)));
     }
+
+    QWidget *widget = NULL;
+    if (uselabel)
+    {
+        QGroupBox *groupbox = new QGroupBox(parent);
+        groupbox->setObjectName(QString("GCG(%1)_groupbox").arg(widgetName));
+        groupbox->setTitle(getLabel());
+        widget = groupbox;
+    }
+    else if (useframe)
+    {
+        QFrame *frame = new QFrame(parent);
+        frame->setFrameStyle(QFrame::Box);
+        frame->setObjectName(QString("GCG(%1)_frame").arg(widgetName));
+        widget = frame;
+    }
+    else
+    {
+        widget = new QWidget(parent);
+        widget->setObjectName(QString("GCG(%1)_widget").arg(widgetName));
+    }
+
+    widget->setLayout(layout);
 
     return widget;
 }

mythtv/libs/libmyth/mythconfiggroups.h
@@ -3,6 +3,9 @@
 #ifndef MYTH_CONFIG_GROUPS_H
 #define MYTH_CONFIG_GROUPS_H
 
+#include <QVBoxLayout>
+#include <QGroupBox>
+
 // MythTV headers
 #include "mythexp.h"
 #include "mythstorage.h"
@@ -87,7 +90,7 @@ class MPUBLIC VerticalConfigurationGroup : public ConfigurationGroup
 
   private:
     vector<QWidget*>    childwidget;
-    Q3GroupBox          *widget;
+    QGroupBox          *widget;
     ConfigurationGroup *confgrp;
     QVBoxLayout        *layout;
 };

mythplugins/mythzoneminder/mythzoneminder/zmevents.cpp
@@ -607,8 +607,8 @@ void ZMEvents::showMenu()
 
     QLabel *splitter = popup->addLabel(" ", MythPopupBox::Small);
     splitter->setLineWidth(2);
-    splitter->setFrameShape(Q3Frame::HLine);
-    splitter->setFrameShadow(Q3Frame::Sunken);
+    splitter->setFrameShape(QLabel::HLine);
+    splitter->setFrameShadow(QLabel::Sunken);
     splitter->setMinimumHeight((int) (25 * hmult));
     splitter->setMaximumHeight((int) (25 * hmult));
 

mythtv/libs/libmyth/dialogbox.cpp
@@ -9,25 +9,29 @@ using namespace std;
 #undef DialogBox
 #endif
 
+#include <Q3ButtonGroup>
+#include <QBoxLayout>
+
 DialogBox::DialogBox(MythMainWindow *parent, const QString &text, 
                      const char *checkboxtext,
                      const char *name)
          : MythDialog(parent, name)
 {
     QLabel *maintext = new QLabel(text, this);
-    maintext->setBackgroundOrigin(WindowOrigin);
-    maintext->setAlignment(Qt::WordBreak | Qt::AlignLeft | Qt::AlignTop);
+    maintext->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    maintext->setWordWrap(true);
 
-    box = new Q3VBoxLayout(this, (int)(60 * wmult), (int)(0 * hmult));
-
+    box = new QVBoxLayout(this);
+    box->setContentsMargins((int)(60 * wmult),(int)(60 * wmult),
+                           (int)(60 * wmult),(int)(60 * wmult));
+    box->setSpacing(0);
     box->addWidget(maintext, 1);
 
     checkbox = NULL;
     if (checkboxtext)
     {
         checkbox = new MythCheckBox(this);
-        checkbox->setText(checkboxtext); 
-        checkbox->setBackgroundOrigin(WindowOrigin);
+        checkbox->setText(checkboxtext);
         box->addWidget(checkbox, 0);
     }
 

mythtv/libs/libmyth/dialogbox.h
@@ -4,10 +4,10 @@
 #include <QObject>
 #include <QCheckBox>
 #include <Q3ButtonGroup>
-#include <Q3VBoxLayout>
 
 #include "mythdialogs.h"
 #include "compat.h" // to undef DialogBox
+class QVBoxLayout;
 
 class MPUBLIC DialogBox : public MythDialog
 {
@@ -28,7 +28,7 @@ class MPUBLIC DialogBox : public MythDialog
     ~DialogBox() {} // use deleteLater() for thread safety
 
   private:
-    Q3VBoxLayout *box;
+    QVBoxLayout *box;
     Q3ButtonGroup *buttongroup;
 
     QCheckBox *checkbox;

mythtv/libs/libmyth/mythconfigdialogs.cpp
@@ -6,7 +6,8 @@
 
 #include "mythuihelper.h"
 
-#include <q3hbox.h>
+#include <QWidget>
+#include <QHBoxLayout>
 
 static void clear_widgets(vector<Configurable*> &children,
                           vector<QWidget*>      &childwidget)
@@ -52,16 +53,19 @@ MythDialog* ConfigurationPopupDialog::dialogWidget(MythMainWindow* parent,
 
     if (getLabel() != "")
     {
-        Q3HBox* box = new Q3HBox(dialog);
-        box->setBackgroundOrigin(QWidget::WindowOrigin);
-        box->setSizePolicy(QSizePolicy(QSizePolicy::Minimum,
-                                       QSizePolicy::Maximum));
-
-        label = new QLabel(box);
+        label = new QLabel();
         label->setText(getLabel());
         label->setAlignment(Qt::AlignHCenter);
         label->setSizePolicy(QSizePolicy(QSizePolicy::Minimum,
                                          QSizePolicy::Maximum));
+
+        QHBoxLayout *layout = new QHBoxLayout;
+        layout->addWidget(label);
+
+        QWidget *box = new QWidget(dialog);
+        box->setSizePolicy(QSizePolicy(QSizePolicy::Minimum,
+                                       QSizePolicy::Maximum));
+        box->setLayout(layout);
 
         dialog->addWidget(box);
     }

mythtv/libs/libmyth/mythdialogs.cpp
@@ -6,17 +6,13 @@ using namespace std;
 #include <QCursor>
 #include <QDialog>
 #include <QDir>
-#include <q3vbox.h>
 #include <QApplication>
 #include <QLayout>
 #include <QRegExp>
-#include <q3accel.h>
 #include <QImageReader>
-#include <QEventLoop>
 #include <QLabel>
 #include <QPixmap>
 #include <QKeyEvent>
-#include <Q3VBoxLayout>
 #include <QFrame>
 #include <QPaintEvent>
 #include <QPainter>
@@ -862,32 +858,24 @@ MythProgressDialog::MythProgressDialog(
     setGeometry(xoff, yoff, screenwidth - xoff * 2, yoff);
     setFixedSize(QSize(screenwidth - xoff * 2, yoff));
 
-    Q3VBoxLayout *lay = new Q3VBoxLayout(this, 0);
-
-    Q3VBox *vbox = new Q3VBox(this);
-    lay->addWidget(vbox);
-
-    vbox->setLineWidth(3);
-    vbox->setMidLineWidth(3);
-    vbox->setFrameShape(Q3Frame::Panel);
-    vbox->setFrameShadow(Q3Frame::Raised);
-    vbox->setMargin((int)(15 * wmult));
-
-    msglabel = new QLabel(vbox);
-    msglabel->setBackgroundOrigin(ParentOrigin);
+    msglabel = new QLabel();
     msglabel->setText(message);
-    vbox->setStretchFactor(msglabel, 5);
 
-    Q3HBox *hbox = new Q3HBox(vbox);
-    hbox->setSpacing(5);
+    QVBoxLayout *vlayout = new QVBoxLayout();
+    vlayout->addWidget(msglabel);
 
-    progress = new Q3ProgressBar(totalSteps, hbox);
-    progress->setBackgroundOrigin(ParentOrigin);
+    progress = new QProgressBar();
+    progress->setRange(0, totalSteps);
+
+    QHBoxLayout *hlayout = new QHBoxLayout();
+    hlayout->addWidget(progress);
 
     if (cancelButton && slot && target)
     {
-        MythPushButton *button = new MythPushButton("Cancel", hbox, 0);
+        MythPushButton *button = new MythPushButton(
+            NULL, QObject::tr("Cancel"));
         button->setFocus();
+        hlayout->addWidget(button);
         connect(button, SIGNAL(pressed()), target, slot);
     }
 
@@ -901,6 +889,27 @@ MythProgressDialog::MythProgressDialog(
                                      false));
         lcddev->switchToGeneric(textItems);
     }
+
+    hlayout->setSpacing(5);
+
+    vlayout->setMargin((int)(15 * wmult));
+    vlayout->setStretchFactor(msglabel, 5);
+
+    QWidget *hbox = new QWidget();
+    hbox->setLayout(hlayout);
+    hlayout->addWidget(hbox);
+
+    QFrame *vbox = new QFrame(this);
+    vbox->setObjectName(objectName() + "_vbox");
+    vbox->setLineWidth(3);
+    vbox->setMidLineWidth(3);
+    vbox->setFrameShape(QFrame::Panel);
+    vbox->setFrameShadow(QFrame::Raised);
+    vbox->setLayout(vlayout);
+
+    QVBoxLayout *lay = new QVBoxLayout();
+    lay->addWidget(vbox);
+    setLayout(lay);
 
     show();
 
@@ -931,7 +940,7 @@ void MythProgressDialog::Close(void)
 
 void MythProgressDialog::setProgress(int curprogress)
 {
-    progress->setProgress(curprogress);
+    progress->setValue(curprogress);
     if (curprogress % steps == 0)
     {
         qApp->processEvents();
@@ -969,7 +978,7 @@ void MythProgressDialog::keyPressEvent(QKeyEvent *e)
 void MythProgressDialog::setTotalSteps(int totalSteps)
 {
     m_totalSteps = totalSteps;
-    progress->setTotalSteps(totalSteps);
+    progress->setRange(0, totalSteps);
     steps = totalSteps / 1000;
     if (steps == 0)
         steps = 1;
@@ -981,6 +990,7 @@ MythBusyDialog::MythBusyDialog(const QString &title, bool cancelButton,
                          cancelButton, target, slot),
                          timer(NULL)
 {
+    setObjectName("MythBusyDialog");
 }
 
 MythBusyDialog::~MythBusyDialog()
@@ -1029,7 +1039,7 @@ void MythBusyDialog::Close(void)
 
 void MythBusyDialog::setProgress(void)
 {
-    progress->setProgress(progress->progress () + 10);
+    progress->setValue(progress->value() + 10);
     qApp->processEvents();
     if (LCD *lcddev = LCD::Get())
         lcddev->setGenericBusy();
@@ -1415,9 +1425,8 @@ void MythThemedDialog::paintEvent(QPaintEvent *e)
     if (redrawRect.width() > 0 && redrawRect.height() > 0)
         ReallyUpdateForeground(redrawRect);
 
-    bitBlt(this, e->rect().left(), e->rect().top(),
-           &my_foreground, e->rect().left(), e->rect().top(),
-           e->rect().width(), e->rect().height());
+    QPainter p(this);
+    p.drawPixmap(e->rect().topLeft(), my_foreground, e->rect());
 
     MythDialog::paintEvent(e);
 }

mythtv/libs/libmyth/mythdialogs.h
@@ -5,8 +5,6 @@
 #include <QLabel>
 #include <QAbstractButton>
 #include <QObject>
-#include <Q3VBoxLayout>
-#include <Q3ProgressBar>
 #include <QFrame>
 #include <QVector>
 #include <QList>
@@ -42,6 +40,8 @@ class MythRemoteLineEdit;
 class MythListBox;
 struct fontProp;
 class MythMainWindow;
+class QVBoxLayout;
+class QProgressBar;
 
 #include "libmythui/mythmainwindow.h"
 
@@ -273,7 +273,7 @@ class MPUBLIC MythProgressDialog: public MythDialog
 
   protected:
     ~MythProgressDialog(); // use deleteLater() instead for thread safety
-    Q3ProgressBar *progress;
+    QProgressBar *progress;
     QLabel *msglabel;
 
   private:

commit c2b22cb99313cbcd496cdbd7593edc853bac41df

Fixes Video Playback Profile Editor.
Two corrected issues:
1- Adding or Editing an existing profile, did nothing visually, but would actually update the database. Regression was introduced in 9686f034ac54
2- When deleting a profile, the newly selected one wasn’t actually saved as default

mythtv/libs/libmyth/mythconfiggroups.cpp
@@ -141,7 +141,6 @@ QWidget *VerticalConfigurationGroup::configWidget(
         confgrp = cg;
     } 
 
-    QWidget *widget = NULL;
     if (uselabel)
     {
         MythGroupBox *groupbox = new MythGroupBox(parent);

mythtv/libs/libmyth/mythconfiggroups.h
@@ -93,7 +93,7 @@ class MPUBLIC VerticalConfigurationGroup : public ConfigurationGroup
 
   private:
     vector<QWidget*>    childwidget;
-    MythGroupBox       *widget;
+    QWidget            *widget;
     ConfigurationGroup *confgrp;
     QVBoxLayout        *layout;
 };

mythtv/programs/mythfrontend/globalsettings.cpp
@@ -1078,10 +1078,14 @@ void PlaybackProfileConfig::pressed(QString cmd)
         PlaybackProfileItemConfig itemcfg(items[i]);
 
         if (itemcfg.exec() != kDialogCodeAccepted)
+        {
             LOG(VB_GENERAL, LOG_ERR, QString("edit #%1").arg(i) + " rejected");
-
-        InitLabel(i);
-        needs_save = true;
+        }
+        else
+        {
+            InitLabel(i);
+            needs_save = true;
+        }
     }
     else if (cmd.startsWith("del"))
     {
@@ -1098,11 +1102,15 @@ void PlaybackProfileConfig::pressed(QString cmd)
         PlaybackProfileItemConfig itemcfg(item);
 
         if (itemcfg.exec() != kDialogCodeAccepted)
+        {
             LOG(VB_GENERAL, LOG_ERR, "addentry rejected");
-
-        items.push_back(item);
-        InitUI();
-        needs_save = true;
+        }
+        else
+        {
+            items.push_back(item);
+            InitUI();
+            needs_save = true;
+        }
     }
 
     repaint();
@@ -1334,6 +1342,10 @@ void PlaybackProfileConfigs::btnPress(QString cmd)
             removeTarget(name);
             VideoDisplayProfile::DeleteProfileGroup(
                 name, gCoreContext->GetHostName());
+            // This would be better done in TriggeredConfigurationGroup::removeTarget
+            // however, as removeTarget is used elsewhere, limit the changes to this
+            // case only
+            grouptrigger->setValue(grouptrigger->getSelectionLabel());
         }
     }
 