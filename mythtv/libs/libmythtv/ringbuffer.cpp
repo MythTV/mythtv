1d6e3b2ee5f3b4fe47ba4b3e375293dd479969d2
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-05-02 00:42:17 +1000

    Don’t prevent read should we reached EOF or an error
    Prevented to play the last few seconds of a recording.
    
    (cherry picked from commit 904b65bdab15796b3b43102ef1bc0ab3215a2634)

--- mythtv/libs/libmythtv/ringbuffer.cpp
+++ mythtv/libs/libmythtv/ringbuffer.cpp
@@ -1388,15 +1388,6 @@ int RingBuffer::ReadPriv(void *buf, int count, bool peek)
         return -1;
     }
 
-    if (commserror)
-    {
-        LOG(VB_GENERAL, LOG_ERR, LOC + loc_desc +
-            ": Attempt to read after commserror set");
-        errno = EIO;
-        rwlock.unlock();
-        return -1;
-    }
-
     if (!readInternalMode &&
         (request_pause || stopreads || !readaheadrunning || (ignorereadpos>=0)))
     {
