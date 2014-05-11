57b64c4af16aedee1608119f8e68ee01e2343d6f
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-05-11 21:08:11 +0930

    Rework IPTV and RTSP recorders
    Lots of issues fixed and addition of missing (but required) features
    
    - Add IPv6 support
    - Handle RTCP channel (not 100% sure it’s the right information sent, but it allows to keep alive the connection with some server, and doesn’t impact playback)
    - Handle Keep Alive and maintain the RTSP TCP connection life so the rtsp server doesn’t drop us (seen with VLC rtsp server and FreeBox IPTV stream)
    - Can use hostnames in URL instead of just IP addresses
    - Proper support of rtsp’s SDP
    
    Fixes #11949

--- mythtv/libs/libmythtv/recorders/cetonrtsp.h
+++ mythtv/libs/libmythtv/recorders/cetonrtsp.h
@@ -7,40 +7,58 @@
 #ifndef CETONRTSP_H
 #define CETONRTSP_H
 
-#include <QObject>
-#include <QHash>
+#include <QMap>
 #include <QString>
 #include <QMutex>
+#include <QUrl>
 
-class QUrl;
+class QTcpSocket;
+class QUdpSocket;
 
-class CetonRTSP
+typedef QMap<QString, QString> Params;
+
+class CetonRTSP : QObject
 {
+    Q_OBJECT
+
   public:
     explicit CetonRTSP(const QString &ip, uint tuner, ushort port);
     explicit CetonRTSP(const QUrl&);
+    ~CetonRTSP();
 
     bool GetOptions(QStringList &options);
     bool Describe(void);
-    bool Setup(ushort clientPort1, ushort clientPort2);
+    bool Setup(ushort clientPort1, ushort clientPort2,
+               ushort &rtpPort, ushort &rtcpPort, uint32_t &ssrc);
     bool Play(void);
     bool Teardown(void);
 
-  protected:
+    void StartKeepAlive(void);
+    void StopKeepAlive(void);
+
+protected:
     bool ProcessRequest(
-        const QString &method, const QStringList *headers = NULL);
+        const QString &method, const QStringList *headers = NULL,
+                        bool use_control = false, bool waitforanswer = true);
 
   private:
-    QString     _ip;
-    ushort      _port;
+    QStringList splitLines(const QByteArray &lines);
+    QString readParamaters(const QString &key, Params &parameters);
+    QUrl GetBaseUrl(void);
+    void timerEvent(QTimerEvent*);
+
+    QTcpSocket *_socket;
     uint        _sequenceNumber;
     QString     _sessionId;
-    QString     _requestUrl;
+    QUrl        _requestUrl;
+    QUrl        _controlUrl;
 
     int                     _responseCode;
     QString                 _responseMessage;
-    QHash<QString,QString>  _responseHeaders;
+    Params                  _responseHeaders;
     QByteArray              _responseContent;
+    int                     _timeout;
+    int                     _timer;
 
     static QMutex _rtspMutex;
 
