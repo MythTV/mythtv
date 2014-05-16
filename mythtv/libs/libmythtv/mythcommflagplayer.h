18853446d6c106ffa904907bd57161cdcfd5d078
John Poet <jpoet@mythtv.org>
2014-05-15 18:25:01 -0600

    Since we are not allowing MythPlayer to be constructed with a copy constructor, don't let MythCommFlagPlayer be constructed that way either. This hopefully fixes compiling under windows.

--- mythtv/libs/libmythtv/mythcommflagplayer.h
+++ mythtv/libs/libmythtv/mythcommflagplayer.h
@@ -7,6 +7,7 @@ class MTV_PUBLIC MythCommFlagPlayer : public MythPlayer
 {
   public:
     MythCommFlagPlayer(PlayerFlags flags = kNoFlags) : MythPlayer(flags) { }
+    MythCommFlagPlayer(MythCommFlagPlayer& rhs);
     bool RebuildSeekTable(bool showPercentage = true, StatusCallback cb = NULL,
                           void* cbData = NULL);
 };
