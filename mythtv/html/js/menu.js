f36eccf2bd8501f2b1c88d0733f848734bb703f1
Stuart Morgan <smorgan@mythtv.org>
2014-05-29 15:40:34 +0100

    WebFrontend: Don't resize the page content for the menu, instead let the menu overlay it. The effect of resizing didn't work well enough, especially on slower browsers/devices.

--- mythtv/html/js/menu.js
+++ mythtv/html/js/menu.js
@@ -102,13 +102,13 @@ function hideMainMenu()
     if (showing)
         return;
 
-    $("#content").animate({left: '30px'}, {duration: 600, queue: false});
+//     $("#content").animate({left: '30px'}, {duration: 600, queue: false});
     $("#menu-bg").hide('slide',{direction:'left'}, {duration: 700, queue: false});
 }
 
 function showMainMenu()
 {
     showing = true;
-    $("#content").animate({left: '190px'}, {duration:600, queue: false});
+//     $("#content").animate({left: '190px'}, {duration:600, queue: false});
     $("#menu-bg").show('slide',{direction:'left'}, {duration: 700, queue: false}, function () { showing = false; });
 }
