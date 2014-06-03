MythTV/mythtv

commit b4be8e24f9fab5b6a71f62df8e2c0403bedf4805

Speed up ProgramInfo::SavePositionMap() database writes.
Use a single SQL query to save all new position map entries.
This speeds up SavePositionMap by an order of magnitude.

Fixes #12016

Signed-off-by: Jean-Yves Avenard <jyavenard@mythtv.org>

mythtv/libs/libmyth/programinfo.cpp
@@ -3683,25 +3683,28 @@ void ProgramInfo::SavePositionMap(
     if (!query.exec())
         MythDB::DBError("position map clear", query);
 
+    // Use the multi-value insert syntax to reduce database I/O
+    QStringList q("INSERT INTO ");
+    QString qfields;
     if (IsVideo())
     {
-        query.prepare(
-            "INSERT INTO "
-            "filemarkup (filename, mark, type, offset) "
-            "VALUES ( :PATH , :MARK , :TYPE , :OFFSET )");
-        query.bindValue(":PATH", videoPath);
+        q << "filemarkup (filename, type, mark, offset)";
+        qfields = QString("('%1',%2,") .
+            // ideally, this should be escaped
+            arg(videoPath) .
+            arg(type);
     }
     else // if (IsRecording())
     {
-        query.prepare(
-            "INSERT INTO "
-            "recordedseek (chanid, starttime, mark, type, offset) "
-            " VALUES ( :CHANID , :STARTTIME , :MARK , :TYPE , :OFFSET )");
-        query.bindValue(":CHANID",    chanid);
-        query.bindValue(":STARTTIME", recstartts);
+        q << "recordedseek (chanid, starttime, type, mark, offset)";
+        qfields = QString("(%1,'%2',%3,") .
+            arg(chanid) .
+            arg(recstartts.toString(Qt::ISODate)) .
+            arg(type);
     }
-    query.bindValue(":TYPE", type);
+    q << " VALUES ";
 
+    bool add_comma = false;
     frm_pos_map_t::iterator it;
     for (it = posMap.begin(); it != posMap.end(); ++it)
     {
@@ -3715,14 +3718,20 @@ void ProgramInfo::SavePositionMap(
 
         uint64_t offset = *it;
 
-        query.bindValue(":MARK",   (quint64)frame);
-        query.bindValue(":OFFSET", (quint64)offset);
-
-        if (!query.exec())
+        if (add_comma)
         {
-            MythDB::DBError("position map insert", query);
-            break;
+            q << ",";
         }
+        else
+        {
+            add_comma = true;
+        }
+        q << qfields << QString("%1,%2)").arg(frame).arg(offset);
+    }
+    query.prepare(q.join(""));
+    if (!query.exec())
+    {
+        MythDB::DBError("position map insert", query);
     }
 }
 
@@ -3741,45 +3750,54 @@ void ProgramInfo::SavePositionMapDelta(
         return;
     }
 
-    MSqlQuery query(MSqlQuery::InitCon());
-
+    // Use the multi-value insert syntax to reduce database I/O
+    QStringList q("INSERT INTO ");
+    QString qfields;
     if (IsVideo())
     {
-        query.prepare(
-            "INSERT INTO "
-            "filemarkup (filename, mark, type, offset) "
-            "VALUES ( :PATH , :MARK , :TYPE , :OFFSET )");
-        query.bindValue(":PATH", StorageGroup::GetRelativePathname(pathname));
+        q << "filemarkup (filename, type, mark, offset)";
+        qfields = QString("('%1',%2,") .
+            // ideally, this should be escaped
+            arg(StorageGroup::GetRelativePathname(pathname)) .
+            arg(type);
     }
     else if (IsRecording())
     {
-        query.prepare(
-            "INSERT INTO "
-            "recordedseek (chanid, starttime, mark, type, offset) "
-            " VALUES ( :CHANID , :STARTTIME , :MARK , :TYPE , :OFFSET )");
-        query.bindValue(":CHANID",    chanid);
-        query.bindValue(":STARTTIME", recstartts);
+        q << "recordedseek (chanid, starttime, type, mark, offset)";
+        qfields = QString("(%1,'%2',%3,") .
+            arg(chanid) .
+            arg(recstartts.toString(Qt::ISODate)) .
+            arg(type);
     }
     else
     {
         return;
     }
-    query.bindValue(":TYPE", type);
+    q << " VALUES ";
 
+    bool add_comma = false;
     frm_pos_map_t::iterator it;
     for (it = posMap.begin(); it != posMap.end(); ++it)
     {
         uint64_t frame  = it.key();
         uint64_t offset = *it;
 
-        query.bindValue(":MARK",   (quint64)frame);
-        query.bindValue(":OFFSET", (quint64)offset);
-
-        if (!query.exec())
+        if (add_comma)
         {
-            MythDB::DBError("delta position map insert", query);
-            break;
+            q << ",";
         }
+        else
+        {
+            add_comma = true;
+        }
+        q << qfields << QString("%1,%2)").arg(frame).arg(offset);
+    }
+
+    MSqlQuery query(MSqlQuery::InitCon());
+    query.prepare(q.join(""));
+    if (!query.exec())
+    {
+        MythDB::DBError("delta position map insert", query);
     }
 }
 

commit d90af2d6b09bc5197a39d1e1d09baff2aafe9a2a

Don't attempt to issue DB query if there's nothing to do
Prevent harmless SQL statement error messages to be logged when the position map is empty

Regression introduced in b4be8e24f9fab5b6a71f62df8e2c0403bedf4805

Fixes #12161

mythtv/libs/libmyth/programinfo.cpp
@@ -3710,6 +3710,9 @@ void ProgramInfo::SavePositionMap(
     if (!query.exec())
         MythDB::DBError("position map clear", query);
 
+    if (posMap.isEmpty())
+        return;
+
     // Use the multi-value insert syntax to reduce database I/O
     QStringList q("INSERT INTO ");
     QString qfields;
@@ -3765,6 +3768,9 @@ void ProgramInfo::SavePositionMap(
 void ProgramInfo::SavePositionMapDelta(
     frm_pos_map_t &posMap, MarkTypes type) const
 {
+    if (posMap.isEmpty())
+        return;
+
     if (positionMapDBReplacement)
     {
         QMutexLocker locker(positionMapDBReplacement->lock);