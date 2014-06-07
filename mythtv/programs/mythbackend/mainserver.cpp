034d4abb3ad0018341b0796f2f6221418b656c30
Gregorio Gervasio, Jr <gregorio.gervasio@gmail.com>
2014-06-06 21:23:50 -0700

    Fixed path name for slave recording.
    Signed-off-by: Jean-Yves Avenard <jyavenard@mythtv.org>
    (cherry picked from commit 0b92e79b45abf685551d5f6aa58f7ceea6dc2212)

--- mythtv/programs/mythbackend/mainserver.cpp
+++ mythtv/programs/mythbackend/mainserver.cpp
@@ -1865,7 +1865,7 @@ void MainServer::HandleQueryRecordings(QString type, PlaybackSock *pbs)
                         gCoreContext->GetBackendServerPort(hostname);
                 p->SetPathname(gCoreContext->GenMythURL(backendIpMap[hostname],
                                                         backendPortMap[hostname],
-                                                        hostname));
+                                                        p->GetBasename()));
             }
         }
 
