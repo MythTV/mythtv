7f5965bd6152c8313dd77e6fad18d58bfeef896f
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-06-10 13:06:28 +1000

    Fix VAAPI accelerated PiP
    PiP wasn't possible with VAAPI playback. There was the issue that only one VAAPI context was allowed at a time, and then the frame retrieved couldn't be used as-is (fixed with 520b8bceb57235bba397fe7c15e12bc25f365ca5)
    
    Drop VAAPI NV12 frame format support, the conversion to YV12 as it is is buggy and from a quick search/test, all VAAPI backends support either YV12 or YUV420P format, so there's no use for it anyway.
    
    (cherry picked from commit ef54e6602da8e64b155337929e626030a62c835f)

--- mythtv/libs/libmythtv/videoout_nullvaapi.cpp
+++ mythtv/libs/libmythtv/videoout_nullvaapi.cpp
@@ -45,7 +45,7 @@ bool VideoOutputNullVAAPI::CreateVAAPIContext(QSize size)
         DeleteVAAPIContext();
 
     m_ctx = new VAAPIContext(kVADisplayX11, video_codec_id);
-    if (m_ctx && m_ctx->CreateDisplay(size))
+    if (m_ctx && m_ctx->CreateDisplay(size, true))
         return true;
     return false;
 }
