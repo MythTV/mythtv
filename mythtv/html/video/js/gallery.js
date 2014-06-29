83c3dd90b4528caa62eaa99d52813bbbadcc6db2
Jamie Blummer <jamieb122@gmail.com>
2014-06-28 21:20:11 -0400

    Video gallery rough draft

--- mythtv/html/video/js/gallery.js
+++ mythtv/html/video/js/gallery.js
@@ -1,7 +1,23 @@
+
+$(document).mouseup(function (e)
+{
+    var container = $("#videoOverlay");
+    var doneButton = $("#doneButton");
+
+    //if we click the overlay(dark part) or the doneButton
+    if ((container.is(e.target) && container.has(e.target).length === 0) || doneButton.is(e.target))
+    {
+        $("#videoDetailBox").html("");  //reset the detail box
+        container.hide();
+    }
+});
+
 function showVideoDetail(videoId)
 {
-    alert("Showing you detail for: " + videoId);
+    $("#videoOverlay").show();
 
-
-
+    $.get("video/gallery_detail.qsp?Id="+videoId)
+        .done(function(data) {
+                 $("#videoDetailBox").html(data);
+              });
 }
