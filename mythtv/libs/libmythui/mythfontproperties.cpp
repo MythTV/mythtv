82077e0eeccf0a8b9af89254c9b346432671cc29
Paul Harrison <pharrison@mythtv.org>
2014-07-11 15:22:35 +0100

    MythFontProperties: don't normalise the shadowoffset property
    This fixes a problem with the position of the shadow being wrong when using a
    theme at a resolution other than the themes base resolution.

--- mythtv/libs/libmythui/mythfontproperties.cpp
+++ mythtv/libs/libmythui/mythfontproperties.cpp
@@ -256,7 +256,7 @@ MythFontProperties *MythFontProperties::ParseFromXml(
             else if (info.tagName() == "shadowoffset")
             {
                 newFont->m_hasShadow = true;
-                newFont->m_shadowOffset = parsePoint(info);
+                newFont->m_shadowOffset = parsePoint(info, false);
             }
             else if (info.tagName() == "shadowalpha")
             {
