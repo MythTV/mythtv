MythTV/mythtv

commit 5d3d319936513cac22557f9a3969f7210b8ac049

metadata: fix incorrect tests
would always return false

similar to a50a24b0f408957885ff928569a061df07e5e993

mythtv/programs/mythmetadatalookup/lookup.cpp
@@ -172,7 +172,7 @@ void LookerUpper::HandleAllArtwork(bool aggressive)
         if (type == kProbableMovie)
            maxartnum = 2;
 
-        if ((!aggressive && type == kProbableGenericTelevision) ||
+        if ((aggressive && type != kProbableGenericTelevision) ||
              pginfo->GetRecordingGroup() == "Deleted" ||
              pginfo->GetRecordingGroup() == "LiveTV")
             dolookup = false;
@@ -252,7 +252,7 @@ void LookerUpper::customEvent(QEvent *levent)
                     pginfo->GetSeriesID() == (list[p])->GetTMSref())
                 {
                     MetadataLookup *lookup = list[p];
-                    if (!lookup->GetSubtype() == kProbableGenericTelevision)
+                    if (lookup->GetSubtype() != kProbableGenericTelevision)
                         pginfo->SaveSeasonEpisode(lookup->GetSeason(), lookup->GetEpisode());
                     pginfo->SaveInetRef(lookup->GetInetref());
                     m_busyRecList.removeAll(pginfo);
@@ -281,7 +281,7 @@ void LookerUpper::customEvent(QEvent *levent)
             {
                 MetadataLookup *lookup = list[yearindex];
                 ProgramInfo *pginfo = lookup->GetData().value<ProgramInfo *>();
-                if (!lookup->GetSubtype() == kProbableGenericTelevision)
+                if (lookup->GetSubtype() != kProbableGenericTelevision)
                     pginfo->SaveSeasonEpisode(lookup->GetSeason(), lookup->GetEpisode());
                 pginfo->SaveInetRef(lookup->GetInetref());
                 m_busyRecList.removeAll(pginfo);
@@ -338,7 +338,7 @@ void LookerUpper::customEvent(QEvent *levent)
         LOG(VB_GENERAL, LOG_DEBUG,
             QString("        User Rating: %1").arg(lookup->GetUserRating()));
 
-        if (!lookup->GetSubtype() == kProbableGenericTelevision)
+        if (lookup->GetSubtype() != kProbableGenericTelevision)
             pginfo->SaveSeasonEpisode(lookup->GetSeason(), lookup->GetEpisode());
         pginfo->SaveInetRef(lookup->GetInetref());
 

commit e0155b9bb2b84b9574e587d65cf22f45d124d202

metadatalookup: fix logic error introduced in 5d3d319936513cac22557f9a3969f7210b8ac049

mythtv/programs/mythmetadatalookup/lookup.cpp
@@ -172,7 +172,7 @@ void LookerUpper::HandleAllArtwork(bool aggressive)
         if (type == kProbableMovie)
            maxartnum = 2;
 
-        if ((aggressive && type != kProbableGenericTelevision) ||
+        if ((!aggressive && type == kProbableGenericTelevision) ||
              pginfo->GetRecordingGroup() == "Deleted" ||
              pginfo->GetRecordingGroup() == "LiveTV")
             dolookup = false;