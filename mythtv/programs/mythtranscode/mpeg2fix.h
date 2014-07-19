302dde18b440a52f8c53744129a4c9adfee3ef71
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-07-19 15:13:21 +1000

    mythtranscode: always check if audio stream still exists
    This doesn’t cater for when there’s a change in audio format or new audio stream, but it prevents the crash when that occurs.
    
    Fixes #12207
    
    (cherry picked from commit 43eed00660447d327b8dc13f3daa38e8e5ae8038)

--- mythtv/programs/mythtranscode/mpeg2fix.h
+++ mythtv/programs/mythtranscode/mpeg2fix.h
@@ -217,10 +217,14 @@ class MPEG2fixup
     }
     AVCodecContext *getCodecContext(int id)
     {
+        if (id >= inputFC->nb_streams)
+            return NULL;
         return inputFC->streams[id]->codec;
     }
     AVCodecParserContext *getCodecParserContext(int id)
     {
+        if (id >= inputFC->nb_streams)
+            return NULL;
         return inputFC->streams[id]->parser;
     }
 
