32a99166a5363a09b8972511707919e27ded1030
Stuart Morgan <smorgan@mythtv.org>
2014-09-02 21:01:56 +0100

    Alter the schema of the unused recordedfile table. Use recordedid instead of chanid/starttime, change the names of two columns for clarity.

--- mythtv/libs/libmythbase/mythversion.h
+++ mythtv/libs/libmythbase/mythversion.h
@@ -61,7 +61,7 @@
  *      mythtv/bindings/php/MythBackend.php
 #endif
 
-#define MYTH_DATABASE_VERSION "1328"
+#define MYTH_DATABASE_VERSION "1329"
 
 
  MBASE_PUBLIC  const char *GetMythSourceVersion();
