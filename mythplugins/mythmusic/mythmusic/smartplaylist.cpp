43b799a5fc57214102d586a01fc755e541e29ccd
Stuart Morgan <smorgan@mythtv.org>
2014-09-07 12:37:11 +0100

    MythMusic: Fix NULL insert into NOT NULL column in SmartPlaylist editor.

--- mythplugins/mythmusic/mythmusic/smartplaylist.cpp
+++ mythplugins/mythmusic/mythmusic/smartplaylist.cpp
@@ -332,7 +332,7 @@ bool SmartPLCriteriaRow::saveToDatabase(int smartPlaylistID)
     query.bindValue(":FIELD", Field);
     query.bindValue(":OPERATOR", Operator);
     query.bindValue(":VALUE1", Value1);
-    query.bindValue(":VALUE2", Value2);
+    query.bindValue(":VALUE2", Value2.isNull() ? "" : Value2);
 
     if (!query.exec())
     {
