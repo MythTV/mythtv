5bd6688cc814e9cdd06c4ef18345e4d2230a2626
lwoggardner <grant@lastweekend.com.au>
2014-09-07 15:30:42 +1000

    fix trailing spaces

--- mythtv/programs/mythbackend/main.cpp
+++ mythtv/programs/mythbackend/main.cpp
@@ -122,7 +122,7 @@ int main(int argc, char **argv)
     SignalHandler::Init(signallist);
     signal(SIGHUP, SIG_IGN);
 #endif
-   
+
     gContext = new MythContext(MYTH_BINARY_VERSION);
     if (!gContext->Init(false))
     {
