629f7112ec77ddd6732ae00378ccf1b7283cbeec
Stuart Auchterlonie <stuarta@squashedfrog.net>
2014-09-23 17:35:32 +0100

    Handle [HD] in UK EIT fixups part 2 44c85ad369a9d2bd564728a65f4f8b1164df2483 didn't include HD in the regexp which picked up the HD notification in the description.
    Hand cherry-picked from de131518b294fa75895b0de91a1f8dcf9f7ab274

--- mythtv/libs/libmythtv/eitfixup.cpp
+++ mythtv/libs/libmythtv/eitfixup.cpp
@@ -42,7 +42,7 @@ EITFixUp::EITFixUp()
       m_ukSpaceColonStart("^[ |:]*"),
       m_ukSpaceStart("^ "),
       m_ukSeries("\\s*\\(?\\s*(?:Episode|Part|Pt)?\\s*(\\d{1,2})\\s*(?:of|/)\\s*(\\d{1,2})\\s*\\)?\\s*(?:\\.|:)?", Qt::CaseInsensitive),
-      m_ukCC("\\[(?:(AD|SL|S|W),?)+\\]"),
+      m_ukCC("\\[(?:(AD|SL|S|W|HD),?)+\\]"),
       m_ukYear("[\\[\\(]([\\d]{4})[\\)\\]]"),
       m_uk24ep("^\\d{1,2}:00[ap]m to \\d{1,2}:00[ap]m: "),
       m_ukStarring("(?:Western\\s)?[Ss]tarring ([\\w\\s\\-']+)[Aa]nd\\s([\\w\\s\\-']+)[\\.|,](?:\\s)*(\\d{4})?(?:\\.\\s)?"),
