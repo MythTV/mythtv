ad634638ffbef394256ac329003a6c7b310f2bb0
Stuart Morgan <smorgan@mythtv.org>
2015-01-17 17:47:12 +0000

    Services: Add name and version information to Frontend/GetStatus

--- mythtv/programs/mythfrontend/services/frontend.cpp
+++ mythtv/programs/mythfrontend/services/frontend.cpp
@@ -11,7 +11,8 @@
 #include "mythuihelper.h"
 #include "mythmainwindow.h"
 #include "tv_play.h"
-#include <recordinginfo.h>
+#include "recordinginfo.h"
+#include "mythversion.h"
 
 #include "videometadatalistmanager.h"
 #include "videometadata.h"
@@ -28,6 +29,10 @@ DTC::FrontendStatus* Frontend::GetStatus(void)
 {
     DTC::FrontendStatus *status = new DTC::FrontendStatus();
     MythUIStateTracker::GetFreshState(status->State());
+
+    status->setName(gCoreContext->GetHostName());
+    status->setVersion(MYTH_SOURCE_VERSION);
+
     status->Process();
     return status;
 }
