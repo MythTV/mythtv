eb2f96e9d5e8a9707476809346f3312348645d19
Gary Buhrmaster <gary.buhrmaster@gmail.com>
2015-01-27 18:29:43 +0000

    C++11 literal string suffix compliance
    Signed-off-by: Stuart Morgan <smorgan@mythtv.org>
    
    Fixes #12359

--- mythtv/external/libmythsoundtouch/cpu_detect_x86_gcc.cpp
+++ mythtv/external/libmythsoundtouch/cpu_detect_x86_gcc.cpp
@@ -82,9 +82,9 @@ void disableExtensions(uint dwDisableMask)
 /* ebx saving is necessary for PIC. gcc seems unable to see it alone */
 #define cpuid(index,eax,ebx,ecx,edx)\
     __asm __volatile\
-        ("mov %%"REG_b", %%"REG_S"\n\t"\
+        ("mov %%" REG_b ", %%" REG_S "\n\t"\
          "cpuid\n\t"\
-         "xchg %%"REG_b", %%"REG_S\
+         "xchg %%" REG_b ", %%" REG_S\
          : "=a" (eax), "=S" (ebx),\
            "=c" (ecx), "=d" (edx)\
          : "0" (index));
