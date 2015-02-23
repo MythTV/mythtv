4e4a78541d9ab632552eafb35dce827f86994d88
Stuart Auchterlonie <stuarta@squashedfrog.net>
2015-02-23 16:42:45 +0000

    Add 29.5Msys/s to the preset list of possible symbol rates. Found on several dvb-s2 multiplexes across Europe
    (cherry picked from commit e26980483f392e7f13c614201a757a74af25112c)

--- mythtv/libs/libmythtv/channelscan/channelscanmiscsettings.h
+++ mythtv/libs/libmythtv/channelscan/channelscanmiscsettings.h
@@ -174,6 +174,7 @@ class ScanSymbolRateDVBS: public ComboBoxSetting, public TransientStorage
         addSelection("27500000", "27500000", true);
         addSelection("28000000");
         addSelection("28500000");
+        addSelection("29500000");
         addSelection("29900000");
     }
 };
