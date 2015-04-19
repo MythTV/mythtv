MythTV/mythtv

commit ac3a988f2e9140322b1a15c34d6ac87071c69d7e

Subtitles: Fix automatic display of captions/subtitles.
There was something of a race condition in the previous code.  At
startup of playback, the decoder thread runs ahead and updates track
lists as it encounters them, including audio, cc608, cc708, etc.  If
no subtitle tracks are found before the UI thread gets to a certain
point, the UI thread commits to keeping subtitles disabled, regardless
of the "automatically display subtitles" setting.

In the new version, at startup of playback, the UI thread keeps the
original intention alive so that subtitles will automatically start
displaying when they eventually arrive.

Note that this fix doesn't address the problem where the player ought
to switch to a "better" subtitle track type when it comes along, such
as switching from cc608 to cc708.

Refs #10740 (only because this fixes a fix with that ref)

mythtv/libs/libmythtv/mythplayer.cpp
@@ -4017,7 +4017,7 @@ void MythPlayer::ClearAfterSeek(bool clearvideobuffers)
     // nothing except to call ResetCaptions() to erase any captions
     // currently on-screen.  The key is that the erasing is done in
     // the UI thread, not the decoder thread.
-    EnableSubtitles(GetCaptionsEnabled());
+    EnableSubtitles(textDesired);
     deleteMap.TrackerReset(framesPlayed);
     commBreakMap.SetTracker(framesPlayed);
     commBreakMap.ResetLastSkip();

commit 26f6437b874d84c87a52df6d33e449c0d5d9a4bc

Subtitles: Don't reset subtitle state after seeking.
After a seek, ac3a988 caused the subtitle-enabled state to be reset to
the desired state at playback start, rather than left unchanged.

mythtv/libs/libmythtv/mythplayer.cpp
@@ -1374,6 +1374,7 @@ void MythPlayer::DisableCaptions(uint mode, bool osd_msg)
 
     QMutexLocker locker(&osdLock);
 
+    textDesired = false;
     QString msg = "";
     if (kDisplayNUVTeletextCaptions & mode)
         msg += tr("TXT CAP");
@@ -1410,7 +1411,8 @@ void MythPlayer::DisableCaptions(uint mode, bool osd_msg)
 void MythPlayer::EnableCaptions(uint mode, bool osd_msg)
 {
     QMutexLocker locker(&osdLock);
-    QString msg;
+    textDesired = true;
+    QString msg = "";
     if ((kDisplayCC608 & mode) || (kDisplayCC708 & mode) ||
         (kDisplayAVSubtitle & mode) || kDisplayRawTextSubtitle & mode)
     {