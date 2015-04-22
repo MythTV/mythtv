8f14d3d1e8c798f8a45b3338a33bb71aec877dc7
Jim Stichnoth <jstichnoth@mythtv.org>
2015-04-22 15:05:18 -0700

    Send REC_STARTED_WRITING event for NuppelVideoRecorder.
    This recorder doesn't go through the usual path of the other
    recorders, where keyframes are identified (and where frames prior to
    the first keyframe are discarded) and the event is sent after the
    first keyframe is found.
    
    Refs #12328.
    
    (cherry picked from commit c07c21e6c5b7f1380cf7e36e7cbcc8763a5cb225)

--- mythtv/libs/libmythtv/recorders/NuppelVideoRecorder.cpp
+++ mythtv/libs/libmythtv/recorders/NuppelVideoRecorder.cpp
@@ -64,6 +64,7 @@ extern "C" {
 #include "RTjpegN.h"
 
 #include "programinfo.h"
+#include "mythsystemevent.h"
 
 #define LOC QString("NVR(%1): ").arg(videodevice)
 
@@ -3075,6 +3076,9 @@ void NuppelVideoRecorder::WriteVideo(VideoFrame *frame, bool skipsync,
         WriteFrameheader(&frameheader);
         ringBuffer->Write(out, out_len);
     }
+
+    if (framesWritten == 0)
+        SendMythSystemRecEvent("REC_STARTED_WRITING", curRecording);
 
     frameofgop++;
     framesWritten++;
