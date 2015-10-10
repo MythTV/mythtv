MythTV/mythtv

commit f00d5ca464450a49c3c809089763688c68696d4b

Bug 12505: P1. Remove Carbon + Quartz support
Qt5 only supports 64 bits . Carbon and Quartz are 32 bits only frameworks and can no long be used. this change drops support for those

mythtv/libs/libmythtv/libmythtv.pro
@@ -58,17 +58,14 @@ INCLUDEPATH += $$DEPENDPATH
 
 macx {
     # Mac OS X Frameworks
-    FWKS = AGL ApplicationServices Carbon Cocoa CoreServices CoreFoundation OpenGL QuickTime IOKit
-    using_quartz_video {
-        FWKS += QuartzCore
-    } else {
-        FWKS += CoreVideo
-    }
-
-    FC = $$join(FWKS,",")
-
-    QMAKE_CXXFLAGS += -F$${FC}
-    LIBS           += -framework $$join(FWKS," -framework ")
+    libs += -framework ApplicationServices
+    libs += -framework Cocoa
+    libs += -framework CoreServices
+    libs += -framework CoreFoundation
+    libs += -framework OpenGL
+    libs += -framework QuickTime
+    libs += -framework IOKit
+    libs += -framework CoreVideo
 
     using_firewire:using_backend {
         QMAKE_CXXFLAGS += -F$${CONFIG_MAC_AVC}
@@ -417,10 +414,6 @@ using_frontend {
         using_opengl: HEADERS += visualisations/videovisualcircles.h
         using_opengl: SOURCES += visualisations/videovisualcircles.cpp
     }
-
-    using_quartz_video: DEFINES += USING_QUARTZ_VIDEO
-    using_quartz_video: HEADERS += videoout_quartz.h
-    using_quartz_video: SOURCES += videoout_quartz.cpp
 
     using_x11:DEFINES += USING_X11
 

mythtv/configure
@@ -135,7 +135,6 @@ Advanced options (experts only):
   --disable-vaapi          disable VAAPI hardware accelerated video decoding
   --disable-dxva2          disable hardware accelerated decoding on windows
   --disable-opengl-video   disable OpenGL based video display
-  --disable-quartz-video   disable Mac OS X CoreVideo based video display
   --disable-libass         disable libass SSA/ASS subtitle support
 
   --enable-mac-bundle      produce standalone OS X apps (e.g. mythfrontend.app)
@@ -2132,7 +2131,6 @@ MYTHTV_CONFIG_LIST='
     qtwebkit
     qtscript
     qtdbus
-    quartz_video
     sdl
     taglib
     v4l2
@@ -3160,7 +3158,6 @@ enable mythtranscode
 enable opengl
 enable opengles
 enable opengl_video
-enable quartz_video
 enable symbol_visibility
 enable v4l1
 enable v4l2
@@ -3368,10 +3365,6 @@ for opt do
         --disable-ccache) disable ccache
         ;;
         --disable-distcc) disable distcc
-        ;;
-        --enable-quartz-video) enable quartz_video
-        ;;
-        --disable-quartz-video) disable quartz_video
         ;;
 # disabled due to abuse in Gentoo ebuild
         --disable-frontend)
@@ -6332,6 +6325,9 @@ enabled exiv2 && $(pkg-config --exists exiv2) ||
 if test $target_os = mingw32 ; then
     qt_inc="-I`cd $(${qmake} -query QT_INSTALL_HEADERS); pwd`"
     qt_libs="-L`cd $(${qmake} -query QT_INSTALL_LIBS) ; pwd`"
+elif test $target_os = darwin ; then
+    qt_inc="-I`cd $(${qmake} -query QT_INSTALL_HEADERS); pwd` -F`cd $(${qmake} -query QT_INSTALL_LIBS); pwd`"
+    qt_libs="-L`cd $(${qmake} -query QT_INSTALL_LIBS) ; pwd`"
 else
     qt_inc="-I${sysroot}/$(${qmake} -query QT_INSTALL_HEADERS)"
     qt_libs="-L${sysroot}/$(${qmake} -query QT_INSTALL_LIBS)"
@@ -6476,14 +6472,10 @@ if enabled x11; then
     fi
 fi
 
-# Quartz video is Mac only and requires Carbon which is 32bit only
-enabled quartz_video && test $target_os = darwin && check_cpp_condition QtCore/qglobal.h "!defined(QT_MAC_USE_COCOA)" || disable quartz_video
-
 # CoreVideo in only available on Mac OS 10.4, or on 10.3 with QuickTime 7.
 # This checks for it, and some other calls that the module needs
 if test $target_os = darwin; then
     cat > $TMPC << EOF
-#include <QuartzCore/CVDisplayLink.h>
 #include <AGL/agl.h>
 int main( void ) { return aglGetCGLContext(0,0L); }
 EOF
@@ -7046,7 +7038,6 @@ if enabled x11 ; then
 fi
   echo "OpenGL video              ${opengl_video-no}"
   if test x"$target_os" = x"darwin" ; then
-    echo "Quartz video              ${quartz_video-no}"
     echo "VDA support               ${vda-no}"
   fi
   if test x"$target_os" = x"mingw32" ; then

mythtv/docs/doxygen-create-developer-docs.cfg
@@ -1957,7 +1957,6 @@ PREDEFINED             = USING_XV \
                          POWERPC \
                          Q_OS_MAC \
                          Q_OS_MACX \
-                         QT_MAC_USE_COCOA \
                          QWS \
                          Q_WS_MACX-DSDL_SUPPORT \
                          USE_ALSA \
@@ -1990,7 +1989,6 @@ PREDEFINED             = USING_XV \
                          USING_OSX_FIREWIRE \
                          USING_PULSE \
                          USING_PULSEOUTPUT \
-                         USING_QUARTZ_VIDEO \
                          USING_V4L1 \
                          USING_V4L2 \
                          USING_VAAPI \

mythtv/libs/libmyth/libmyth.pro
@@ -207,16 +207,12 @@ macx {
     }
 
     # Mac OS X Frameworks
-    FWKS = ApplicationServices AudioUnit AudioToolbox Carbon CoreAudio IOKit
-
-    darwin_da : FWKS += DiskArbitration
-
-    # The following trick is tidier, and shortens the command line, but it
-    # depends on the shell expanding Csh-style braces. Luckily, Bash & Zsh do.
-    FC = $$join(FWKS,",","{","}")
-
-    QMAKE_CXXFLAGS += -F/System/Library/Frameworks/$${FC}.framework/Frameworks
-    LIBS           += -framework $$join(FWKS," -framework ")
+    darwin_da : LIBS += -framework DiskArbitration
+    LIBS += -framework ApplicationServices
+    LIBS += -framework AudioUnit
+    LIBS += -framework AudioToolbox
+    LIBS += -framework CoreAudio
+    LIBS += -framework IOKit
 }
 
 INSTALLS += inc inc2

mythtv/libs/libmythtv/avformatdecoder.cpp
@@ -42,8 +42,6 @@ using namespace std;
 
 #include "lcddevice.h"
 
-#include "videoout_quartz.h"  // For VOQ::GetBestSupportedCodec()
-
 #ifdef USING_VDPAU
 #include "videoout_vdpau.h"
 extern "C" {

mythtv/libs/libmythtv/privatedecoder_vda.cpp
@@ -9,10 +9,6 @@
 #include "util-osx-cocoa.h"
 #include "privatedecoder_vda.h"
 
-#ifdef USING_QUARTZ_VIDEO
-#undef CodecType
-#import  "QuickTime/ImageCompression.h"
-#endif
 #include "H264Parser.h"
 
 #include <CoreServices/CoreServices.h>
@@ -453,11 +449,7 @@ bool PrivateDecoderVDA::Init(const QString &decoder,
         CFDictionaryCreateMutable(kCFAllocatorDefault, 1,
                                   &kCFTypeDictionaryKeyCallBacks,
                                   &kCFTypeDictionaryValueCallBacks);
-#ifdef USING_QUARTZ_VIDEO
-    OSType cvPixelFormatType = k422YpCbCr8PixelFormat;
-#else
     OSType cvPixelFormatType = kCVPixelFormatType_422YpCbCr8;
-#endif
     CFNumberRef pixelFormat  = CFNumberCreate(kCFAllocatorDefault,
                                               kCFNumberSInt32Type,
                                               &cvPixelFormatType);

mythtv/libs/libmythtv/privatedecoder_vda.h
@@ -6,11 +6,7 @@
 #include <QList>
 
 #import  "CoreFoundation/CoreFoundation.h"
-#ifdef USING_QUARTZ_VIDEO
-#import  "QuartzCore/CoreVideo.h"
-#else
 #import  "CoreVideo/CoreVideo.h"
-#endif
 #include "privatedecoder_vda_defs.h"
 #include "privatedecoder.h"
 

mythtv/libs/libmythtv/videodisplayprofile.cpp
@@ -1031,12 +1031,6 @@ void VideoDisplayProfile::CreateNewProfiles(const QString &hostname)
     CreateProfile(groupid, 2, ">", 0, 0, "", 0, 0,
                   "ffmpeg", 1, true, "xv-blit", "softblend", true,
                   "yadifdoubleprocessdeint", "yadifdeint", "");
-    CreateProfile(groupid, 3, ">=", 1920, 1080, "", 0, 0,
-                  "ffmpeg", 2, true, "quartz-blit", "softblend", true,
-                  "linearblend", "linearblend", "");
-    CreateProfile(groupid, 4, ">", 0, 0, "", 0, 0,
-                  "ffmpeg", 1, true, "quartz-blit", "softblend", true,
-                  "yadifdoubleprocessdeint", "yadifdeint", "");
 
     (void) QObject::tr("Normal", "Sample: average quality");
     DeleteProfileGroup("Normal", hostname);
@@ -1047,12 +1041,6 @@ void VideoDisplayProfile::CreateNewProfiles(const QString &hostname)
     CreateProfile(groupid, 2, ">", 0, 0, "", 0, 0,
                   "ffmpeg", 1, true, "xv-blit", "softblend", true,
                   "greedyhdoubleprocessdeint", "kerneldeint", "");
-    CreateProfile(groupid, 3, ">=", 1280, 720, "", 0, 0,
-                  "ffmpeg", 1, true, "quartz-blit", "softblend", false,
-                  "linearblend", "linearblend", "");
-    CreateProfile(groupid, 4, ">", 0, 0, "", 0, 0,
-                  "ffmpeg", 1, true, "quartz-blit", "softblend", true,
-                  "greedyhdoubleprocessdeint", "kerneldeint", "");
 
     (void) QObject::tr("Slim", "Sample: low CPU usage");
     DeleteProfileGroup("Slim", hostname);
@@ -1062,12 +1050,6 @@ void VideoDisplayProfile::CreateNewProfiles(const QString &hostname)
                   "onefield", "onefield", "");
     CreateProfile(groupid, 2, ">", 0, 0, "", 0, 0,
                   "ffmpeg", 1, true, "xv-blit", "softblend", false,
-                  "linearblend", "linearblend", "");
-    CreateProfile(groupid, 3, ">=", 1280, 720, "", 0, 0,
-                  "ffmpeg", 1, true, "quartz-blit", "softblend", false,
-                  "onefield", "onefield", "");
-    CreateProfile(groupid, 4, ">", 0, 0, "", 0, 0,
-                  "ffmpeg", 1, true, "quartz-blit", "softblend", false,
                   "linearblend", "linearblend", "");
 }
 
@@ -1239,14 +1221,6 @@ QString VideoDisplayProfile::GetVideoRendererHelp(const QString &renderer)
             "Windows video renderer based on Direct3D. Requires "
             "video card compatible with Direct3D 9. This is the preferred "
             "renderer for current Windows systems.");
-
-    if (renderer == "quartz-blit")
-        msg = QObject::tr(
-            "This is the standard video render for Macintosh OS X systems.");
-
-    if (renderer == "quartz-accel")
-        msg = QObject::tr(
-            "This is the only video renderer for the MacAccel decoder.");
 
     if (renderer == "opengl")
     {

commit 602fdacc2beebcfa0b3edede99eaf5a5f370b843

Fix makefile error introduced in f00d5ca464450a49c3c809089763688c68696d4b

mythtv/libs/libmythtv/libmythtv.pro
@@ -61,14 +61,14 @@ macx {
     OBJECTIVE_SOURCES += util-osx.mm
 
     # Mac OS X Frameworks
-    libs += -framework ApplicationServices
-    libs += -framework Cocoa
-    libs += -framework CoreServices
-    libs += -framework CoreFoundation
-    libs += -framework OpenGL
-    libs += -framework QuickTime
-    libs += -framework IOKit
-    libs += -framework CoreVideo
+    LIBS += -framework ApplicationServices
+    LIBS += -framework Cocoa
+    LIBS += -framework CoreServices
+    LIBS += -framework CoreFoundation
+    LIBS += -framework OpenGL
+    LIBS += -framework QuickTime
+    LIBS += -framework IOKit
+    LIBS += -framework CoreVideo
 
     using_firewire:using_backend {
         QMAKE_CXXFLAGS += -F$${CONFIG_MAC_AVC}