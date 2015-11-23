999aa8f0af15a67432742074aabbb1a1c1ce2e2c
Roger Siddons <dizygotheca@ntlworld.com>
2015-11-23 11:45:58 -0600

    Copy hostname from scheduler too when creating a new ProgramInfo.
    The hostname is needed to be able to stop the recording in some cases.
    
    Signed-off-by: David Engel <dengel@mythtv.org>
    
    Refs #12281
    
    Backported from master to fixes/0.27.

--- mythtv/libs/libmyth/programinfo.cpp
+++ mythtv/libs/libmyth/programinfo.cpp
@@ -681,6 +681,7 @@ ProgramInfo::ProgramInfo(
         dupin       = s.dupin;
         dupmethod   = s.dupmethod;
         findid      = s.findid;
+        hostname    = s.hostname;
 
         if (chanstr != s.chanstr)
         {
