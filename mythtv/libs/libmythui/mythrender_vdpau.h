f8c6665b2cb71222bb0bca53b562b5715f3f6b28
Jean-Yves Avenard <jyavenard@mythtv.org>
2016-01-10 20:42:57 +1100

    Check that VDPAU hardware supports HEVC.
    Fixes #12591

--- mythtv/libs/libmythui/mythrender_vdpau.h
+++ mythtv/libs/libmythui/mythrender_vdpau.h
@@ -61,9 +61,11 @@ class MUI_PUBLIC MythRenderVDPAU : public MythRender
     static bool gVDPAUSupportChecked;
     static uint gVDPAUBestScaling;
     static bool gVDPAUMPEG4Accel;
+    static bool gVDPAUHEVCAccel;
     static bool gVDPAUNVIDIA;
     static bool IsVDPAUAvailable(void);
     static bool IsMPEG4Available(void);
+    static bool IsHEVCAvailable(void);
     static bool H264DecoderSizeSupported(uint width, uint height);
     bool        CreateDummy(void);
 
