MythTV/mythtv

commit 7da9166e9d7cd62b68cd799e68411c8811e81446

Many years before the lowres option was added to improve performance, a default value for the CommDetectBorder? setting was set to 20. It is used to exclude CommDetectBorder? pixels from the top, bottom, left and right of the frame from examination. It should, for example, exclude any VBI data that might be visible at the top of a frame or parts of the video that would be hidden due to overscan. The default value was never adjusted to account for lowres decoding, nor did it take into consideration the variety of video widths and heights that exist today. The patch, mcf-border.patch, assumes the CommDetectBorder? setting was established for 720 height video and uses those two numbers and the lowres video height to compute the border used. For example: If video source height = 480 then border = 20 * 480 / 4 / 720 = 2 If video source height = 720 then border = 20 * 720 / 4 / 720 = 5 If video source height = 1080 then border = 20 * 1080 / 4 / 720 = 7 mcf-border.patch improves accuracy to 67%.
Signed-off-by: John Poet <jpoet@mythtv.org>

mythtv/programs/mythcommflag/ClassicCommDetector.cpp
@@ -152,8 +152,6 @@ ClassicCommDetector::ClassicCommDetector(SkipType commDetectMethod_in,
     fps(0.0),                                  framesProcessed(0),
     preRoll(0),                                postRoll(0)
 {
-    commDetectBorder =
-        gCoreContext->GetNumSetting("CommDetectBorder", 20);
     commDetectBlankFrameMaxDiff =
         gCoreContext->GetNumSetting("CommDetectBlankFrameMaxDiff", 25);
     commDetectDarkBrightness =
@@ -188,6 +186,16 @@ void ClassicCommDetector::Init()
         max(int64_t(0), int64_t(recordingStartedAt.secsTo(startedAt))) * fps);
     postRoll = (long long)(
         max(int64_t(0), int64_t(stopsAt.secsTo(recordingStopsAt))) * fps);
+
+    // CommDetectBorder's default value of 20 predates the change to use
+    // ffmpeg's lowres decoding capability by 5 years.
+    // I believe it should be adjusted based on the height of the lowres video
+    // CommDetectBorder * height / 720 seems to produce reasonable results.
+    // source height =  480 gives border = 20 *  480 / 4 / 720 = 2
+    // source height =  720 gives border = 20 *  720 / 4 / 720 = 5
+    // source height = 1080 gives border = 20 * 1080 / 4 / 720 = 7
+    commDetectBorder =
+        gCoreContext->GetNumSetting("CommDetectBorder", 20) * height / 720;
 
 #ifdef SHOW_DEBUG_WIN
     comm_debug_init(width, height);

commit 8e00233035a8af2da04e2a3df1040b0967749f87

Silence Coverity warning about unitialized commDetectBorder
Fallout from 7da9166e9d7cd62b68cd799e68411c8811e81446
Coverity ID 1343274;

mythtv/programs/mythcommflag/ClassicCommDetector.cpp
@@ -135,6 +135,7 @@ ClassicCommDetector::ClassicCommDetector(SkipType commDetectMethod_in,
 
     commDetectMethod(commDetectMethod_in),
     commBreakMapUpdateRequested(false),        sendCommBreakMapUpdates(false),
+    commDetectBorder(0),
     verboseDebugging(false),
     lastFrameNumber(0),                        curFrameNumber(0),
     width(0),                                  height(0),