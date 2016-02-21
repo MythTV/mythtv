2c75af2d35de1e592d338f2fcbbc994ba0c0b9fb
Jean-Yves Avenard <jyavenard@mythtv.org>
2016-02-22 10:57:42 +1100

    Fix mytharchive following ffmpeg resync Replace avpicture_deinterlace with new MythPictureDeinterlacer

--- mythplugins/mytharchive/mytharchivehelper/main.cpp
+++ mythplugins/mytharchive/mytharchivehelper/main.cpp
@@ -1639,6 +1639,7 @@ static int grabThumbnail(QString inFile, QString thumbList, QString outFile, int
     memset(&orig, 0, sizeof(AVPicture));
     memset(&retbuf, 0, sizeof(AVPicture));
     MythAVCopy copyframe;
+    MythPictureDeinterlacer deinterlacer(codecCtx->pix_fmt, width, height);
 
     int bufflen = width * height * 4;
     unsigned char *outputbuf = new unsigned char[bufflen];
@@ -1693,14 +1694,13 @@ static int grabThumbnail(QString inFile, QString thumbList, QString outFile, int
                             filename = filename.arg(thumbCount);
 
                         avpicture_fill(&retbuf, outputbuf,
-                                       PIX_FMT_RGB32, width, height);
+                                       AV_PIX_FMT_RGB32, width, height);
 
                         AVFrame *tmp = frame;
-                        avpicture_deinterlace((AVPicture*)tmp,
-                                              (AVPicture*)tmp,
-                                              codecCtx->pix_fmt, width, height);
+                        deinterlacer.Deinterlace((AVPicture*)tmp,
+                                                 (AVPicture*)tmp);
 
-                        copyframe.Copy(&retbuf, PIX_FMT_RGB32,
+                        copyframe.Copy(&retbuf, AV_PIX_FMT_RGB32,
                                        (AVPicture*) tmp,
                                        codecCtx->pix_fmt, width, height);
 
