9df773ad3fc6b37e682666ca1fb4131e19fdc9c3
David Engel <dengel@mythtv.org>
2016-02-23 10:10:17 -0600

    Fix saving of diseqcid in some situations.
    When saving a new card, there are some situations where the
    videodevice and hostname have not been saved yet.  In those cases, we
    do have a cardid, so use it too.
    
    Refs #12499

--- mythtv/libs/libmythtv/videosource.cpp
+++ mythtv/libs/libmythtv/videosource.cpp
@@ -4382,7 +4382,7 @@ void DVBConfigurationGroup::Load(void)
 void DVBConfigurationGroup::Save(void)
 {
     VerticalConfigurationGroup::Save();
-    diseqc_tree->Store(cardnum->getValue());
+    diseqc_tree->Store(cardnum->getValue(), parent.getCardID());
     DiSEqCDev trees;
     trees.InvalidateTrees();
 }
