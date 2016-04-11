MythTV/mythtv

commit 916e43bb52ba30379447cb2672dccb99d5d7d6e5

BE: Avoid SEGV when accessing discarded MythSocket
1. When a client connects, MainServer::NewConnection is called which
inserts a new MythSocket in controlSocketList.

2. When data is received, MainServer::readyRead creates a new thread to
process the request and saves the MythSocket to read from.

3. If the client disconnects before the thread handles the request then
the MythSocket is added to decrRefSocketList and removed from
controlSocketList.

4. When the thread runs and handles an 'ANN' request it creates a
PlaybackSock that references the MythSocket.

5. When MainServer::customEvent runs, it deletes any MythSockets
listed in decrRefSocketList.  This results in the PlaybackSock having
a dangling reference to the socket.  If the event is to be broadcast,
all client PlaybackSock's are sent the message which results in a reference
to the deleted socket and a SEGV.

This fix adds a test in MainServer::ProcessRequestWork to ensure the
socket is still in the controlSocketList and some additional code
in MainServer::HandleAnnounce to also check controlSocketList before
a PlaybackSock is created.

Signed-off-by: Lawrence Rust <lvr@softsystem.co.uk>

mythtv/programs/mythbackend/mainserver.cpp
@@ -30,6 +30,7 @@ using namespace std;
 #include <QFile>
 #include <QDir>
 #include <QWaitCondition>
+#include <QWriteLocker>
 #include <QRegExp>
 #include <QEvent>
 #include <QUrl>
@@ -465,6 +466,8 @@ void MainServer::ProcessRequestWork(MythSocket *sock)
     PlaybackSock *pbs = GetPlaybackBySock(sock);
     if (pbs)
         pbs->IncrRef();
+
+    bool bIsControl = (pbs) ? false : controlSocketList.contains(sock);
     sockListLock.unlock();
 
     QStringList listline;
@@ -477,6 +480,11 @@ void MainServer::ProcessRequestWork(MythSocket *sock)
             return;
         }
         pbs->DecrRef();
+    }
+    else if (!bIsControl)
+    {
+        // The socket has been disconnected
+        return;
     }
     else if (!sock->ReadStringList(listline) || listline.empty())
     {
@@ -1671,25 +1679,30 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             socket->WriteStringList(errlist);
             return;
         }
+
         // Monitor connections are same as Playback but they don't
         // block shutdowns. See the Scheduler event loop for more.
 
         PlaybackSockEventsMode eventsMode =
             (PlaybackSockEventsMode)commands[3].toInt();
-        LOG(VB_NETWORK, LOG_INFO, LOC + QString("MainServer::ANN %1")
-                                            .arg(commands[1]));
-        LOG(VB_NETWORK, LOG_INFO, LOC +
-            QString("adding: %1 as a client (events: %2)")
-                .arg(commands[2]).arg(eventsMode));
+
+        QWriteLocker lock(&sockListLock);
+        if (!controlSocketList.remove(socket))
+            return; // socket was disconnected
         PlaybackSock *pbs = new PlaybackSock(this, socket, commands[2],
                                              eventsMode);
+        playbackList.push_back(pbs);
+        lock.unlock();
+
+        LOG(VB_GENERAL, LOG_INFO, LOC + QString("MainServer::ANN %1")
+                                      .arg(commands[1]));
+        LOG(VB_GENERAL, LOG_INFO, LOC +
+            QString("adding: %1(%2) as a client (events: %3)")
+                                      .arg(commands[2])
+                                      .arg(quintptr(socket),0,16)
+                                      .arg(eventsMode));
         pbs->setBlockShutdown((commands[1] == "Playback") ||
                               (commands[1] == "Frontend"));
-
-        sockListLock.lockForWrite();
-        controlSocketList.remove(socket);
-        playbackList.push_back(pbs);
-        sockListLock.unlock();
 
         if (commands[1] == "Frontend")
         {
@@ -1721,14 +1734,15 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             return;
         }
 
+        QWriteLocker lock(&sockListLock);
+        if (!controlSocketList.remove(socket))
+            return; // socket was disconnected
         PlaybackSock *pbs = new PlaybackSock(this, socket, commands[2],
                                               kPBSEvents_Normal);
         pbs->setAsMediaServer();
         pbs->setBlockShutdown(false);
-        sockListLock.lockForWrite();
-        controlSocketList.remove(socket);
         playbackList.push_back(pbs);
-        sockListLock.unlock();
+        lock.unlock();
 
         gCoreContext->SendSystemEvent(
             QString("CLIENT_CONNECTED HOSTNAME %1").arg(commands[2]));
@@ -1745,11 +1759,17 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             return;
         }
 
+        QWriteLocker lock(&sockListLock);
+        if (!controlSocketList.remove(socket))
+            return; // socket was disconnected
+        PlaybackSock *pbs = new PlaybackSock(this, socket, commands[2],
+                                             kPBSEvents_None);
+        playbackList.push_back(pbs);
+        lock.unlock();
+
         LOG(VB_GENERAL, LOG_INFO, LOC +
             QString("adding: %1 as a slave backend server")
                                .arg(commands[2]));
-        PlaybackSock *pbs = new PlaybackSock(this, socket, commands[2],
-                                             kPBSEvents_None);
         pbs->setAsSlaveBackend();
         pbs->setIP(commands[3]);
 
@@ -1792,11 +1812,6 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
         gCoreContext->dispatch(me);
 
         pbs->setBlockShutdown(false);
-
-        sockListLock.lockForWrite();
-        controlSocketList.remove(socket);
-        playbackList.push_back(pbs);
-        sockListLock.unlock();
 
         autoexpireUpdateTimer->start(1000);
 
@@ -1922,11 +1937,19 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
                     return;
                 }
             }
+            QWriteLocker lock(&sockListLock);
+            if (!controlSocketList.remove(socket))
+                return; // socket was disconnected
             ft = new FileTransfer(filename, socket, writemode);
+            fileTransferList.push_back(ft);
         }
         else
         {
+            QWriteLocker lock(&sockListLock);
+            if (!controlSocketList.remove(socket))
+                return; // socket was disconnected
             ft = new FileTransfer(filename, socket, usereadahead, timeout_ms);
+            fileTransferList.push_back(ft);
         }
 
         if (!ft->isOpen())
@@ -1940,10 +1963,6 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             return;
         }
         ft->IncrRef();
-        sockListLock.lockForWrite();
-        controlSocketList.remove(socket);
-        fileTransferList.push_back(ft);
-        sockListLock.unlock();
 
         retlist << QString::number(socket->GetSocketDescriptor());
         retlist << QString::number(ft->GetFileSize());
@@ -7480,7 +7499,7 @@ void MainServer::deferredDeleteSlot(void)
     DeferredDeleteStruct dds = deferredDeleteList.front();
     while (dds.ts.secsTo(MythDate::current()) > 30)
     {
-        delete dds.sock;
+        dds.sock->DecrRef();
         deferredDeleteList.pop_front();
         if (deferredDeleteList.empty())
             return;
@@ -7601,6 +7620,10 @@ void MainServer::connectionClosed(MythSocket *socket)
                 }
             }
 
+            LOG(VB_GENERAL, LOG_INFO, QString("%1 sock(%2) '%3' disconnected")
+                .arg(pbs->getBlockShutdown() ? "Playback" : "Monitor")
+                .arg(quintptr(socket),0,16)
+                .arg(pbs->getHostname()) );
             pbs->SetDisconnected();
             playbackList.erase(it);
 
@@ -7627,6 +7650,8 @@ void MainServer::connectionClosed(MythSocket *socket)
         MythSocket *sock = (*ft)->getSocket();
         if (sock == socket)
         {
+            LOG(VB_GENERAL, LOG_INFO, QString("FileTransfer sock(%1) disconnected")
+                .arg(quintptr(socket),0,16) );
             (*ft)->DecrRef();
             fileTransferList.erase(ft);
             sockListLock.unlock();
@@ -7637,6 +7662,8 @@ void MainServer::connectionClosed(MythSocket *socket)
     QSet<MythSocket*>::iterator cs = controlSocketList.find(socket);
     if (cs != controlSocketList.end())
     {
+        LOG(VB_GENERAL, LOG_INFO, QString("Control sock(%1) disconnected")
+            .arg(quintptr(socket),0,16) );
         (*cs)->DecrRef();
         controlSocketList.erase(cs);
         sockListLock.unlock();

mythtv/programs/mythbackend/playbacksock.h
@@ -30,7 +30,6 @@ class PlaybackSock : public ReferenceCounter
   public:
     PlaybackSock(MainServer *parent, MythSocket *lsock,
                  QString lhostname, PlaybackSockEventsMode eventsMode);
-    virtual ~PlaybackSock();
 
     void SetDisconnected(void) { disconnected = true; }
     bool IsDisconnected(void) const { return disconnected; }
@@ -106,6 +105,10 @@ class PlaybackSock : public ReferenceCounter
     QStringList ForwardRequest(const QStringList&);
 
     bool ReadStringList(QStringList &list);
+
+    // Enforce reference counting
+  protected:
+    virtual ~PlaybackSock();
 
   private:
     bool SendReceiveStringList(QStringList &strlist, uint min_reply_length = 0);

commit ea5fdd45bdab01aa6f3d577db7e344a90aae98a6

Avoid crash when file transfer can't open files
916e43bb inadvertently adds a failed File Transfer to the fileTransferList
causing a segfault when a subsequent socket disconnection attempts to clean up and
delete the File Transfer again.

Fixes #12650

mythtv/programs/mythbackend/mainserver.cpp
@@ -1941,7 +1941,6 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             if (!controlSocketList.remove(socket))
                 return; // socket was disconnected
             ft = new FileTransfer(filename, socket, writemode);
-            fileTransferList.push_back(ft);
         }
         else
         {
@@ -1949,7 +1948,6 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             if (!controlSocketList.remove(socket))
                 return; // socket was disconnected
             ft = new FileTransfer(filename, socket, usereadahead, timeout_ms);
-            fileTransferList.push_back(ft);
         }
 
         if (!ft->isOpen())
@@ -1963,6 +1961,7 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             return;
         }
         ft->IncrRef();
+        fileTransferList.push_back(ft);
 
         retlist << QString::number(socket->GetSocketDescriptor());
         retlist << QString::number(ft->GetFileSize());