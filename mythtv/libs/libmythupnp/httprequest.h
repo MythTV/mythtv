778ded0a3ff5c24e02bb764c09651501adb96189
Bill Meek <bmeek@mythtv.org>
2016-06-24 12:04:21 -0500

    HTTP Server: Provide additional CORS support
    This commit adds a new AllowedOriginsList setting that
    stores a comma separated list of Origins. If the setting
    is missing, the list defaults to:
    
        https://chromecast.mythtv.org
        http://chromecast.mythtvcast.com
    
    Access-Control-Allow -Credentials and -Headers were also
    added to the response to support Chromecast.
    
    A TODO item is closed by adding the hostname to
    the existing list of IP addresses (no change in
    the ports checked.)
    
    Note that previously, if ResponseTypeFile was found, the
    CORS header wasn't being added. This caused the Chromecast
    feature to fail.
    
    There is no GUI. Clients have the ability to change the value
    with the Services API. May need to rethink using settings as
    this is a security risk.
    
    For example, it a valid Origin exists in the request, the
    following will appear in the response:
    
        Access-Control-Allow-Origin: https://example.com
        Access-Control-Allow-Credentials: true
        Access-Control-Allow-Headers: Content-Type
    
    If there is no match, an existing critical log will fire:
    
        Cross-origin request received with origin (https://example.net)
    
    Missing http://, https:// or a '*' character in list entries will
    be ignored, and the following will be logged:
    
        Illegal AllowedOriginsList entry '*'. Must start with http[s]:// and not be *
    
    (cherry picked from commit 5292483764357d86f6dc2b155b262c2c072748da)

--- mythtv/libs/libmythupnp/httprequest.h
+++ mythtv/libs/libmythupnp/httprequest.h
@@ -3,10 +3,10 @@
 // Created     : Oct. 21, 2005
 //
 // Purpose     : Http Request/Response
-//                                                                            
+//
 // Copyright (c) 2005 David Blain <dblain@mythtv.org>
-//                                          
-// Licensed under the GPL v2 or later, see COPYING for details                    
+//
+// Licensed under the GPL v2 or later, see COPYING for details
 //
 //////////////////////////////////////////////////////////////////////////////
 
@@ -36,7 +36,7 @@
 // Typedefs / Defines
 /////////////////////////////////////////////////////////////////////////////
 
-typedef enum 
+typedef enum
 {
     RequestTypeUnknown      = 0x0000,
     // HTTP 1.1
@@ -56,9 +56,9 @@ typedef enum
     // Not a request type
     RequestTypeResponse     = 0x1000
 
-} RequestType;                
+} RequestType;
 
-typedef enum 
+typedef enum
 {
     ContentType_Unknown    = 0,
     ContentType_Urlencoded = 1,
@@ -66,7 +66,7 @@ typedef enum
 
 } ContentType;
 
-typedef enum 
+typedef enum
 {
     ResponseTypeNone     = -1,
     ResponseTypeUnknown  =  0,
@@ -99,7 +99,7 @@ class IPostProcess
 };
 
 /////////////////////////////////////////////////////////////////////////////
-// 
+//
 /////////////////////////////////////////////////////////////////////////////
 
 class UPNP_PUBLIC HTTPRequest
@@ -111,8 +111,8 @@ class UPNP_PUBLIC HTTPRequest
         QRegExp             m_procReqLineExp;
         QRegExp             m_parseRangeExp;
 
-    public:    
-        
+    public:
+
         RequestType         m_eType;
         ContentType         m_eContentType;
 
@@ -175,9 +175,9 @@ class UPNP_PUBLIC HTTPRequest
         QString         GetResponseType     ( void );
         QString         GetResponseHeaders  ( void );
 
-        bool            ParseRange          ( QString sRange, 
-                                              long long   llSize, 
-                                              long long *pllStart, 
+        bool            ParseRange          ( QString sRange,
+                                              long long   llSize,
+                                              long long *pllStart,
                                               long long *pllEnd   );
 
         bool            ParseKeepAlive      ( void );
@@ -198,16 +198,17 @@ class UPNP_PUBLIC HTTPRequest
 
         bool            BasicAuthentication ();
         bool            DigestAuthentication ();
+        void            AddCORSHeaders ( const QString &sOrigin );
 
     public:
-        
+
                         HTTPRequest     ();
         virtual        ~HTTPRequest     () {};
 
         bool            ParseRequest    ();
 
-        void            FormatErrorResponse ( bool  bServerError, 
-                                              const QString &sFaultString, 
+        void            FormatErrorResponse ( bool  bServerError,
+                                              const QString &sFaultString,
                                               const QString &sDetails );
 
         void            FormatActionResponse( Serializer *ser );
@@ -265,17 +266,17 @@ class UPNP_PUBLIC HTTPRequest
 };
 
 /////////////////////////////////////////////////////////////////////////////
-// 
+//
 /////////////////////////////////////////////////////////////////////////////
 
 class BufferedSocketDeviceRequest : public HTTPRequest
 {
-    public:    
+    public:
 
         QTcpSocket    *m_pSocket;
 
     public:
-        
+
         explicit BufferedSocketDeviceRequest( QTcpSocket *pSocket );
         virtual ~BufferedSocketDeviceRequest() {};
 
@@ -290,7 +291,7 @@ class BufferedSocketDeviceRequest : public HTTPRequest
 };
 
 /////////////////////////////////////////////////////////////////////////////
-// 
+//
 /////////////////////////////////////////////////////////////////////////////
 
 class UPNP_PUBLIC HttpException
@@ -299,12 +300,12 @@ class UPNP_PUBLIC HttpException
         int     code;
         QString msg;
 
-        HttpException( int nCode = -1, const QString &sMsg = "") 
-               : code( nCode ), msg ( sMsg  ) 
+        HttpException( int nCode = -1, const QString &sMsg = "")
+               : code( nCode ), msg ( sMsg  )
         {}
 
         // Needed to force a v-table.
-        virtual ~HttpException() 
+        virtual ~HttpException()
         {}
 };
 
@@ -315,13 +316,13 @@ class UPNP_PUBLIC HttpRedirectException : public HttpException
         QString hostName;
       //int     port;
 
-        HttpRedirectException( const QString &sHostName = "", 
-                                     int      nCode     = -1, 
-                               const QString &sMsg      = "" ) 
+        HttpRedirectException( const QString &sHostName = "",
+                                     int      nCode     = -1,
+                               const QString &sMsg      = "" )
                : HttpException( nCode, sMsg ), hostName( sHostName )
         {}
 
-        virtual ~HttpRedirectException() 
+        virtual ~HttpRedirectException()
         {}
 
 };
