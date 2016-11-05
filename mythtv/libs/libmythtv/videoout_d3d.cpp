MythTV/mythtv

commit 28fe92a74d0cd903ad0c154fa3cd9a74019d1ca8

Fix Seg Fault due to OSD painter being deleted while image loader threads are still using it.
Image loading for the OSD uses ImageLoadThread and continues
loading while the video is playing. If the video playback
encounters an "InputChanged" situation it deletes the OSD and its
painter and re-creates it.  If the Image load is still running and
using the painter, it gets a Seg Fault calling a virtual function
using a corrupted object

Exception Details:
1   in MythPainter::GetFormatImage at mythpainter.cpp:540

Fix: Keep the old osd painter valid until either it changes again
or the OSD is deleted.

Refs #8096

mythtv/libs/libmythtv/videoout_d3d.cpp
@@ -82,7 +82,14 @@ void VideoOutputD3D::TearDown(void)
     }
 
     delete m_osd_painter;
-    m_osd_painter = NULL;
+    {
+        // Hack to ensure that the osd painter is not
+        // deleted while image load thread is still busy
+        // loading images with that painter
+        if (invalid_osd_painter)
+            delete invalid_osd_painter;
+        invalid_osd_painter = m_osd_painter;
+    }
 
     DeleteDecoder();
     DestroyContext();

mythtv/libs/libmythtv/videoout_opengl.cpp
@@ -152,7 +152,14 @@ void VideoOutputOpenGL::DestroyGPUResources(void)
 #endif
 
     if (gl_created_painter)
-        delete gl_painter;
+    {
+        // Hack to ensure that the osd painter is not
+        // deleted while image load thread is still busy
+        // loading images with that painter
+        if (invalid_osd_painter)
+            delete invalid_osd_painter;
+        invalid_osd_painter = gl_painter;
+    }
     else if (gl_painter)
         gl_painter->SetSwapControl(true);
 

mythtv/libs/libmythtv/videoout_vdpau.cpp
@@ -158,7 +158,14 @@ void VideoOutputVDPAU::DeleteRender(void)
     QMutexLocker locker(&m_lock);
 
     if (m_osd_painter)
-        delete m_osd_painter;
+    {
+        // Hack to ensure that the osd painter is not
+        // deleted while image load thread is still busy
+        // loading images with that painter
+        if (invalid_osd_painter)
+            delete invalid_osd_painter;
+        invalid_osd_painter = m_osd_painter;
+    }
 
     if (m_render)
     {

mythtv/libs/libmythtv/videooutbase.cpp
@@ -427,6 +427,7 @@ VideoOutput::VideoOutput() :
 
     // OSD
     osd_painter(NULL),                  osd_image(NULL),
+    invalid_osd_painter(0),
 
     // Visualisation
     m_visual(NULL),
@@ -459,6 +460,9 @@ VideoOutput::~VideoOutput()
         osd_image->DecrRef();
     if (osd_painter)
         delete osd_painter;
+    if (invalid_osd_painter)
+        delete invalid_osd_painter;
+    invalid_osd_painter = 0;
 
     ShutdownPipResize();
 

mythtv/libs/libmythtv/videooutbase.h
@@ -355,6 +355,8 @@ class VideoOutput
     // OSD painter and surface
     MythYUVAPainter *osd_painter;
     MythImage       *osd_image;
+    // Hack to ensure osd painter not deleted to soon
+    MythPainter     *invalid_osd_painter;
 
     // Visualisation
     VideoVisual     *m_visual;

commit e0d15446e7df50ec5f671a068a4e4392c54fe628

Fix Artifacts that appear on VDPAU setups in Live TV.
This problem was caused by commit 28fe92a (Fix Seg Fault due to OSD
painter being deleted while image loader threads are still using it)

Leaving the OSD painter undeleted after the OSD has been reinitialized
results in artifacts displayed in the screen when the video has a
black image over the place where the initial Live TV OSD showed. This
only affect Live TV because only Live TV starts out with an OSD
display before the video starts running.

Fix: Call Teardown on the osd painter when deleting the VDPAU
renderer. Leave the osd painter still allocated but ignoring
all requests, so that image loads in other threads do not crash.

mythtv/libs/libmythtv/videoout_d3d.cpp
@@ -81,14 +81,16 @@ void VideoOutputD3D::TearDown(void)
         m_pauseFrame.buf = NULL;
     }
 
-    delete m_osd_painter;
+    if (m_osd_painter)
     {
         // Hack to ensure that the osd painter is not
         // deleted while image load thread is still busy
         // loading images with that painter
+        m_osd_painter->Teardown();
         if (invalid_osd_painter)
             delete invalid_osd_painter;
         invalid_osd_painter = m_osd_painter;
+        m_osd_painter = NULL;
     }
 
     DeleteDecoder();

mythtv/libs/libmythtv/videoout_opengl.cpp
@@ -156,6 +156,7 @@ void VideoOutputOpenGL::DestroyGPUResources(void)
         // Hack to ensure that the osd painter is not
         // deleted while image load thread is still busy
         // loading images with that painter
+        gl_painter->Teardown();
         if (invalid_osd_painter)
             delete invalid_osd_painter;
         invalid_osd_painter = gl_painter;

mythtv/libs/libmythtv/videoout_vdpau.cpp
@@ -162,6 +162,7 @@ void VideoOutputVDPAU::DeleteRender(void)
         // Hack to ensure that the osd painter is not
         // deleted while image load thread is still busy
         // loading images with that painter
+        m_osd_painter->Teardown();
         if (invalid_osd_painter)
             delete invalid_osd_painter;
         invalid_osd_painter = m_osd_painter;

mythtv/libs/libmythui/mythpainter_d3d9.h
@@ -12,6 +12,7 @@ class MythRenderD3D9;
 
 class MUI_PUBLIC MythD3D9Painter : public MythPainter
 {
+    friend class VideoOutputD3D;
   public:
     explicit MythD3D9Painter(MythRenderD3D9 *render = NULL);
    ~MythD3D9Painter();

mythtv/libs/libmythui/mythpainter_ogl.h
@@ -13,6 +13,7 @@ class MythRenderOpenGL;
 
 class MUI_PUBLIC MythOpenGLPainter : public MythPainter
 {
+    friend class VideoOutputOpenGL;
   public:
     MythOpenGLPainter(MythRenderOpenGL *render =  NULL, QWidget *parent = NULL);
    ~MythOpenGLPainter();

mythtv/libs/libmythui/mythpainter_vdpau.h
@@ -12,6 +12,7 @@ class MythRenderVDPAU;
 
 class MUI_PUBLIC MythVDPAUPainter : public MythPainter
 {
+    friend class VideoOutputVDPAU;
   public:
     explicit MythVDPAUPainter(MythRenderVDPAU *render = NULL);
    ~MythVDPAUPainter();