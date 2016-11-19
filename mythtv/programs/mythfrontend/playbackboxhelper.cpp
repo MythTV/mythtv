efbedfb142ede80ee9d1e6e7ba6bce61e9998d31
Richard Hulme <peper03@mythtv.org>
2016-11-19 19:21:48 +0100

    Fix getting stuck in an infinite loop when undeleting a recording.
    Fixes #12518
    (cherry picked from commit 86bd8a84e47a3ca183437d053a28764be26e5376)

--- mythtv/programs/mythfrontend/playbackboxhelper.cpp
+++ mythtv/programs/mythfrontend/playbackboxhelper.cpp
@@ -217,6 +217,7 @@ bool PBHEventHandler::event(QEvent *e)
                 QStringList &res = (ok) ? successes : failures;
 
                 res.push_back(QString::number(recordingID));
+                list.pop_front();
             }
             if (!successes.empty())
             {
