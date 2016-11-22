MythTV/mythtv

commit 89bc8e7c2673ed6e4002ccffd1805bd6ea5ee5b2

Fix QT 5.6.1 seg fault in ~QNetworkConfiguration during exit from program.
Make a copy of the QNetworkConfiguration in the QNetworkAccessManager object
in mythuiwebbrowser, in case the default QNetworkConfiguration object is
already deleted when the QNetworkAccessManager is deleted.

Fixes #12917

mythtv/libs/libmythui/mythuiwebbrowser.cpp
@@ -35,6 +35,7 @@
 #include <QKeyEvent>
 #include <QDomDocument>
 #include <QNetworkCookieJar>
+#include <QNetworkConfiguration>
 
 #include <unistd.h> // for usleep()
 
@@ -108,6 +109,9 @@ static QNetworkAccessManager *GetNetworkAccessManager(void)
         return networkManager;
 
     networkManager = new MythNetworkAccessManager();
+    QNetworkConfiguration currConf = networkManager->configuration();
+    QNetworkConfiguration *newConf = new QNetworkConfiguration(currConf);
+    networkManager->setConfiguration(*newConf);
     LOG(VB_GENERAL, LOG_DEBUG, "Copying DLManager's Cookie Jar");
     GetMythDownloadManager()->loadCookieJar(GetConfDir() + "/MythBrowser/cookiejar.txt");
     networkManager->setCookieJar(GetMythDownloadManager()->copyCookieJar());

commit 169fc9b39c2b7ad94dcb02702796e6a9c9bbf012

Fix small memory leak caused by commit 89bc8e7
Refs #12917

mythtv/libs/libmythui/mythuiwebbrowser.cpp
@@ -109,9 +109,10 @@ static QNetworkAccessManager *GetNetworkAccessManager(void)
         return networkManager;
 
     networkManager = new MythNetworkAccessManager();
-    QNetworkConfiguration currConf = networkManager->configuration();
-    QNetworkConfiguration *newConf = new QNetworkConfiguration(currConf);
-    networkManager->setConfiguration(*newConf);
+//  This next line prevents seg fault at program exit in
+//  QNetworkConfiguration::~QNetworkConfiguration()
+//  when destructor is called by DestroyNetworkAccessManager
+    networkManager->setConfiguration(networkManager->configuration());
     LOG(VB_GENERAL, LOG_DEBUG, "Copying DLManager's Cookie Jar");
     GetMythDownloadManager()->loadCookieJar(GetConfDir() + "/MythBrowser/cookiejar.txt");
     networkManager->setCookieJar(GetMythDownloadManager()->copyCookieJar());