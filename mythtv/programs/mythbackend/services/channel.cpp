75e68f54904f5edf0af8166a0c0190618e24b9b2
Paul Harrison <pharrison@mythtv.org>
2017-01-07 13:03:13 +0000

    services: fix Channel/GetChannelInfoList always returning and error
    After 0054b513 this service was always returning a 'Channel ID appears invalid'
    error because of missing curly brackets.

--- mythtv/programs/mythbackend/services/channel.cpp
+++ mythtv/programs/mythbackend/services/channel.cpp
@@ -82,8 +82,10 @@ DTC::ChannelInfoList* Channel::GetChannelInfoList( uint nSourceID,
         ChannelInfo channelInfo = (*chanIt);
 
         if (!FillChannelInfo(pChannelInfo, channelInfo, bDetails))
+        {
             delete pChannelInfo;
             throw( QString("Channel ID appears invalid."));
+        }
     }
 
     int nCurPage = 0, nTotalPages = 0;
