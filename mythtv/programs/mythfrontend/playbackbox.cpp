72b0fda4724bc6eb9687a5bfbc17d75a795948dd
Karl Dietz <dekarl@mythtv.org>
2017-01-16 20:21:40 +0100

    merge multiple translatable substrings that form one sentence
    This avoids a bad german translation due to the used storage not being
    at the very end of the string.
    
    "which consume %1" + "GB" in the source code
    "die %1 belegen" + "GB" in the german translation
    
    Also show used storage when there is only one recording in the group.

--- mythtv/programs/mythfrontend/playbackbox.cpp
+++ mythtv/programs/mythfrontend/playbackbox.cpp
@@ -679,6 +679,7 @@ void PlaybackBox::updateGroupInfo(const QString &groupname,
 {
     InfoMap infoMap;
     int countInGroup;
+    QString desc;
 
     infoMap["group"] = m_groupDisplayName;
     infoMap["title"] = grouplabel;
@@ -712,10 +713,8 @@ void PlaybackBox::updateGroupInfo(const QString &groupname,
         }
     }
 
-    QString desc = tr("There is/are %n recording(s) in this display group",
-                      "", countInGroup);
 
-    if (countInGroup > 1)
+    if (countInGroup >= 1)
     {
         ProgramList  group     = m_progLists[groupname];
         float        groupSize = 0.0;
@@ -736,10 +735,13 @@ void PlaybackBox::updateGroupInfo(const QString &groupname,
             }
         }
 
-        desc += tr(", which consume %1");
-        desc += tr("GB", "GigaBytes");
-
-        desc = desc.arg(groupSize / 1024.0 / 1024.0 / 1024.0, 0, 'f', 2);
+        desc = tr("There is/are %n recording(s) in this display "
+                  "group, which consume(s) %1 GiB.", "", countInGroup)
+               .arg(groupSize / 1024.0 / 1024.0 / 1024.0, 0, 'f', 2);
+    }
+    else
+    {
+        desc = tr("There is no recording in this display group.");
     }
 
     infoMap["description"] = desc;
