36e946d74522045302ee2ab543fa15f5d5e7cdbc
Rune Petersen <rune@megahurts.dk>
2017-02-03 16:21:48 +0000

    Fixes #11098 - Rework table section accounting to remove code duplication
    Reworked by:
    - Roger James <roger@beardandsandals.co.uk>
    - Stuart Auchterlonie <stuarta@mythtv.org>
    
    Signed-off-by: Stuart Auchterlonie <stuarta@mythtv.org>

--- mythtv/libs/libmythtv/mpeg/dvbstreamdata.h
+++ mythtv/libs/libmythtv/mpeg/dvbstreamdata.h
@@ -5,6 +5,7 @@
 
 #include "mpegstreamdata.h"
 #include "mythtvexp.h"
+#include "tablestatus.h"
 
 typedef NetworkInformationTable* nit_ptr_t;
 typedef NetworkInformationTable const* nit_const_ptr_t;
@@ -57,130 +58,26 @@ class MTV_PUBLIC DVBStreamData : virtual public MPEGStreamData
                           uint_vec_t &del_pids) const;
 
     // Table versions
-    void SetVersionNIT(int version, uint last_section)
-    {
-        if (_nit_version == version)
-            return;
-        _nit_version = version;
-        init_sections(_nit_section_seen, last_section);
-    }
-    int  VersionNIT() const { return _nit_version; }
-
-    void SetVersionNITo(int version, uint last_section)
-    {
-        if (_nito_version == version)
-            return;
-        _nito_version = version;
-        init_sections(_nito_section_seen, last_section);
-    }
-    int  VersionNITo() const { return _nito_version; }
-
     void SetVersionSDT(uint tsid, int version, uint last_section)
     {
-        if (VersionSDT(tsid) == version)
-            return;
-        _sdt_versions[tsid] = version;
-        init_sections(_sdt_section_seen[tsid], last_section);
-    }
-    int VersionSDT(uint tsid) const
-    {
-        const QMap<uint, int>::const_iterator it = _sdt_versions.find(tsid);
-        if (it == _sdt_versions.end())
-            return -1;
-        return *it;
+        _sdt_status.SetVersion(tsid, version, last_section);
     }
 
     void SetVersionSDTo(uint tsid, int version, uint last_section)
     {
-        if (_sdto_versions[tsid] == version)
-            return;
-        _sdto_versions[tsid] = version;
-        init_sections(_sdto_section_seen[tsid], last_section);
-    }
-    int VersionSDTo(uint tsid) const
-    {
-        const QMap<uint, int>::const_iterator it = _sdto_versions.find(tsid);
-        if (it == _sdto_versions.end())
-            return -1;
-        return *it;
-    }
-
-    void SetVersionEIT(uint tableid, uint serviceid, int version, uint last_section)
-    {
-        if (VersionEIT(tableid, serviceid) == version)
-            return;
-        uint key = (tableid << 16) | serviceid;
-        _eit_version[key] = version;
-        init_sections(_eit_section_seen[key], last_section);
-    }
-
-    int VersionEIT(uint tableid, uint serviceid) const
-    {
-        uint key = (tableid << 16) | serviceid;
-        const QMap<uint, int>::const_iterator it = _eit_version.find(key);
-        if (it == _eit_version.end())
-            return -1;
-        return *it;
-    }
-
-    void SetVersionBAT(uint bid, int version, uint last_section)
-    {
-        if (_bat_versions[bid] == version)
-            return;
-        _bat_versions[bid] = version;
-        init_sections(_bat_section_seen[bid], last_section);
-    }
-    int VersionBAT(uint bid) const
-    {
-        const QMap<uint, int>::const_iterator it = _bat_versions.find(bid);
-        if (it == _bat_versions.end())
-            return -1;
-        return *it;
-    }
-
-    // Premiere private ContentInformationTable
-    void SetVersionCIT(uint contentid, int version)
-    {
-        if (VersionCIT(contentid) == version)
-            return;
-        _cit_version[contentid] = version;
-    }
-
-    int VersionCIT(uint contentid) const
-    {
-        const QMap<uint, int>::const_iterator it = _cit_version.find(contentid);
-        if (it == _cit_version.end())
-            return -1;
-        return *it;
+        _sdto_status.SetVersion(tsid, version, last_section);
     }
 
     // Sections seen
-    void SetNITSectionSeen(uint section);
-    bool NITSectionSeen(uint section) const;
     bool HasAllNITSections(void) const;
 
-    void SetNIToSectionSeen(uint section);
-    bool NIToSectionSeen(uint section) const;
     bool HasAllNIToSections(void) const;
 
-    void SetSDTSectionSeen(uint tsid, uint section);
-    bool SDTSectionSeen(uint tsid, uint section) const;
     bool HasAllSDTSections(uint tsid) const;
 
-    void SetSDToSectionSeen(uint tsid, uint section);
-    bool SDToSectionSeen(uint tsid, uint section) const;
     bool HasAllSDToSections(uint tsid) const;
 
-    void SetEITSectionSeen(uint tableid, uint serviceid, uint section);
-    bool EITSectionSeen(uint tableid, uint serviceid, uint section) const;
-
-    void SetBATSectionSeen(uint bid, uint section);
-    bool BATSectionSeen(uint bid, uint section) const;
     bool HasAllBATSections(uint bid) const;
-
-    // Premiere private ContentInformationTable
-    void SetCITSectionSeen(uint contentid, uint section);
-    bool CITSectionSeen(uint contentid, uint section) const;
 
     // Caching
     bool HasCachedAnyNIT(bool current = true) const;
@@ -233,22 +130,14 @@ class MTV_PUBLIC DVBStreamData : virtual public MPEGStreamData
     dvb_eit_listener_vec_t    _dvb_eit_listeners;
 
     // Table versions
-    int                       _nit_version;
-    QMap<uint, int>           _sdt_versions;
-    sections_t                _nit_section_seen;
-    sections_map_t            _sdt_section_seen;
-    QMap<uint, int>           _eit_version;
-    sections_map_t            _eit_section_seen;
-    // Premiere private ContentInformationTable
-    QMap<uint, int>           _cit_version;
-    sections_map_t            _cit_section_seen;
+    TableStatus               _nit_status;
+    TableStatusMap            _sdt_status;
+    TableStatusMap            _eit_status;
+    TableStatusMap            _cit_status;
 
-    int                       _nito_version;
-    QMap<uint, int>           _sdto_versions;
-    sections_t                _nito_section_seen;
-    sections_map_t            _sdto_section_seen;
-    QMap<uint, int>           _bat_versions;
-    sections_map_t            _bat_section_seen;
+    TableStatus               _nito_status;
+    TableStatusMap            _sdto_status;
+    TableStatusMap            _bat_status;
 
     // Caching
     mutable nit_cache_t       _cached_nit;  // section -> sdt
---
+++ mythtv/libs/libmythtv/mpeg/tablestatus.h
@@ -0,0 +1,48 @@
+// -*- Mode: c++ -*-
+// Copyright (c) 2003-2012
+#ifndef TABLESTATUS_H_
+#define TABLESTATUS_H_
+
+// POSIX
+#include <stdint.h>  // uint64_t
+
+// C++
+#include <vector>
+using namespace std;
+
+// Qt
+#include "qmap.h"
+
+class TableStatus
+{
+public:
+    typedef vector<uint8_t>   sections_t;
+    static void InitSections(sections_t &sect, uint32_t last_section);
+
+    TableStatus() : m_version(-2) {}
+    void SetVersion(int32_t version, uint32_t last_section);
+    void SetSectionSeen(int32_t version, uint32_t section,
+                        uint32_t last_section, uint32_t segment_last_section = 0xffff);
+    bool IsSectionSeen(int32_t version, uint32_t section) const;
+    bool HasAllSections() const;
+
+    int32_t     m_version;
+    sections_t  m_sections;
+
+private:
+    static const uint8_t init_bits[8];
+};
+
+
+class TableStatusMap : public QMap<uint32_t, TableStatus>
+{
+public:
+    void SetVersion(uint32_t key, int32_t version, uint32_t last_section);
+    void SetSectionSeen(uint32_t key, int32_t version, uint32_t section,
+                        uint32_t last_section, uint32_t segment_last_section = 0xffff);
+    bool IsSectionSeen(uint32_t key, int32_t version, uint32_t section) const;
+    bool HasAllSections(uint32_t key) const;
+};
+
+#endif // TABLESTATUS_H_
+
