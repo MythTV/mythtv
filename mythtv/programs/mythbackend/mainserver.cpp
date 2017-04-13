9f6fb795325d7c380cc22d163fa3f3b49fee4e19
Brian J. Murrell <brian@interlinx.bc.ca>
2017-04-13 11:26:48 -0400

    Only make strings without a scheme local file URLs
    When converting a string to a URL, only make it a file:// URL if it does not already have a scheme.

--- mythtv/programs/mythbackend/mainserver.cpp
+++ mythtv/programs/mythbackend/mainserver.cpp
@@ -1867,7 +1867,12 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
         LOG(VB_NETWORK, LOG_INFO, LOC +
             QString("adding: %1 as a remote file transfer") .arg(commands[2]));
         QStringList::const_iterator it = slist.begin();
-        QUrl qurl = QUrl::fromLocalFile(*(++it)); 
+        QUrl qurl;
+        QString s = *(++it)
+        if (QUrl(s).scheme() == "")
+            qurl = QUrl::fromLocalFile(s);
+        else
+            qurl = s
         QString wantgroup = *(++it);
         QString filename;
         QStringList checkfiles;
