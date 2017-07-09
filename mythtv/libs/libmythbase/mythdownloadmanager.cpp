752a0070cbeed18eccb322c1c103eb51370bc665
David Hampton <mythtv@love2code.net>
2017-07-09 12:06:01 -0400

    Eliminate MythCookieJar copy constructor.
    MythCookieJar fix attempt #3.
    
    The MythCookieJar object is based on QObject and therefore shouldn't
    have a public copy constructor. Unfortunately its two main purposes
    are to load/save cookies from/to a file, and to copy cookies between
    multiple cookie jars.  This fix simply splits the copy constructor
    into its two components; creating a new jar and copying cookies from
    one jar to another.  It is probably not the ideal solution, but it has
    the smallest impact on the existing code base.
    
    General: Instances of QObject (and its subclasses) should be
    considered unique items that cannot be copied.  Knowing that each
    object has only one parent and an arbitrary number of children makes
    it easier to understand this. Even if you "clone" the object by
    setting all the member variables to the same values, you can't set the
    parent and child variables to the same values without screwing up the
    one-to-many relationship of parent to children.
    
    See:
    http://doc.qt.io/qt-5/qobject.html#Q_DISABLE_COPY
    https://www.ics.com/designpatterns/book/qobject.html
    
    Refs #13069

--- mythtv/libs/libmythbase/mythdownloadmanager.cpp
+++ mythtv/libs/libmythbase/mythdownloadmanager.cpp
@@ -119,7 +119,7 @@ class MythCookieJar : public QNetworkCookieJar
 {
   public:
     MythCookieJar();
-    MythCookieJar(MythCookieJar &old);
+    void copyAllCookies(MythCookieJar &old);
     void load(const QString &filename);
     void save(const QString &filename);
 };
@@ -1712,7 +1712,8 @@ QNetworkCookieJar *MythDownloadManager::copyCookieJar(void)
         return NULL;
 
     MythCookieJar *inJar = static_cast<MythCookieJar *>(m_manager->cookieJar());
-    MythCookieJar *outJar = new MythCookieJar(*inJar);
+    MythCookieJar *outJar = new MythCookieJar;
+    outJar->copyAllCookies(*inJar);
 
     return static_cast<QNetworkCookieJar *>(outJar);
 }
@@ -1727,7 +1728,8 @@ void MythDownloadManager::refreshCookieJar(QNetworkCookieJar *jar)
         delete m_inCookieJar;
 
     MythCookieJar *inJar = static_cast<MythCookieJar *>(jar);
-    MythCookieJar *outJar = new MythCookieJar(*inJar);
+    MythCookieJar *outJar = new MythCookieJar;
+    outJar->copyAllCookies(*inJar);
     m_inCookieJar = static_cast<QNetworkCookieJar *>(outJar);
 
     QMutexLocker locker2(&m_queueWaitLock);
@@ -1741,7 +1743,8 @@ void MythDownloadManager::updateCookieJar(void)
     QMutexLocker locker(&m_cookieLock);
 
     MythCookieJar *inJar = static_cast<MythCookieJar *>(m_inCookieJar);
-    MythCookieJar *outJar = new MythCookieJar(*inJar);
+    MythCookieJar *outJar = new MythCookieJar;
+    outJar->copyAllCookies(*inJar);
     m_manager->setCookieJar(static_cast<QNetworkCookieJar *>(outJar));
 
     delete m_inCookieJar;
@@ -1784,10 +1787,10 @@ QString MythDownloadManager::getHeader(const QNetworkCacheMetaData &cacheData,
 }
 
 
-/** \brief Creates a MythCookieJar from another MythCookieJar
+/** \brief Copies all cookies from one MythCookieJar to another
  *  \param old the MythCookieJar to copy
  */
-MythCookieJar::MythCookieJar(MythCookieJar &old)
+void MythCookieJar::copyAllCookies(MythCookieJar &old)
 {
     const QList<QNetworkCookie> cookieList = old.allCookies();
     setAllCookies(cookieList);
