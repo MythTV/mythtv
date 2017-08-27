a60e8c63bb185abc0741827349957f4d60222cc8
David Engel <dengel@mythtv.org>
2017-08-27 15:31:43 -0500

    Fix invocation of Previously Recorded from Recording Rules.
    When the Schedule Editor is entered from Recording Rules, there is no
    ProgramInfo to use.  Fix it calling Previously Recorded with the
    recording rule ID and title.
    
    Fixes #13112

--- mythtv/programs/mythfrontend/schedulecommon.h
+++ mythtv/programs/mythfrontend/schedulecommon.h
@@ -36,6 +36,7 @@ class ScheduleCommon : public MythScreenType
     virtual void EditRecording(bool may_watch_now = false);
     virtual void QuickRecord(void);
     virtual void ShowPrevious(void) const;
+    virtual void ShowPrevious(uint ruleid, const QString &title) const;
     virtual void ShowUpcoming(void) const;
     virtual void ShowUpcomingScheduled(void) const;
     virtual void ShowChannelSearch(void) const;
