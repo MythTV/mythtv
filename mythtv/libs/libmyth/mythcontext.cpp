ce2789645c69d20cce29f1dd47677bc932c6076a
Peter Bennett <pbennett@mythtv.org>
2017-11-03 15:33:03 -0400

    Startup: Fix failure of Retry option
    Ensure that the DB host name is valued after a Retry of startup
    
    Fixes #13162
    
    (cherry picked from commit 7d0b484c8372cda7b3166a8005f6806a0b5901f0)

--- mythtv/libs/libmyth/mythcontext.cpp
+++ mythtv/libs/libmyth/mythcontext.cpp
@@ -784,7 +784,7 @@ QString MythContextPrivate::TestDBconnection(bool prompt)
     // Jan 20, 2017
     // Changed to use port check instead of ping
 
-    int port;
+    int port = 0;
 
     // 1 = db awake, 2 = db listening, 3 = db connects,
     // 4 = backend awake, 5 = backend listening
@@ -806,7 +806,10 @@ QString MythContextPrivate::TestDBconnection(bool prompt)
 
     do
     {
-        host = m_DBparams.dbHostName;
+        if (m_DBparams.dbHostName.isNull() && m_DBhostCp.length())
+            host = m_DBhostCp;
+        else
+            host = m_DBparams.dbHostName;
         port = m_DBparams.dbPort;
         if (port == 0)
             port = 3306;
