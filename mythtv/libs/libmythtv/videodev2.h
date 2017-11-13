13f0bbd65828df2f89cb2e78a68e80c15cab61ab
Stephen Hurd <deuce@synchro.net>
2017-11-13 10:29:01 -0800

    FreeBSD does not have endian types
    Add __le32 and friends for FreeBSD.

--- mythtv/libs/libmythtv/videodev2.h
+++ mythtv/libs/libmythtv/videodev2.h
@@ -58,8 +58,20 @@
 
 #include <sys/time.h>
 
+#ifdef __FreeBSD__
+#include <linux/input.h>	// For __[us][0-9]+ types
+#define __le64 __u64
+#define __le32 __u32
+#define __le16 __u16
+#define __le8  __u8
+#define __be64 __u64
+#define __be32 __u32
+#define __be16 __u16
+#define __be8  __u8
+#else
 #include <linux/ioctl.h>
 #include <linux/types.h>
+#endif
 #include <linux/v4l2-common.h>
 #include <linux/v4l2-controls.h>
 
