7588be7fc27e6a7d41f136255a08beb95d325561
David Engel <dengel@mythtv.org>
2017-12-19 13:41:57 -0600

    Add missing else cases in several tvremoteutil functions.
    In the "called from backend context" cases, a missing TVRec would
    cause the backend to try to connect back to itself which isn't
    allowed.
    
    Note that the error case for RemoteIsBusy is handled differently
    between the backend and frontend cases.  In the backend case, we don't
    want a missing input block other actions.

--- mythtv/libs/libmythtv/tvremoteutil.cpp
+++ mythtv/libs/libmythtv/tvremoteutil.cpp
@@ -16,6 +16,8 @@ uint RemoteGetFlags(uint inputid)
         const TVRec *rec = TVRec::GetTVRec(inputid);
         if (rec)
             return rec->GetFlags();
+        else
+            return 0;
     }
 
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(inputid));
@@ -33,6 +35,8 @@ uint RemoteGetState(uint inputid)
         const TVRec *rec = TVRec::GetTVRec(inputid);
         if (rec)
             return rec->GetState();
+        else
+            return kState_ChangingState;
     }
 
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(inputid));
@@ -55,6 +59,8 @@ bool RemoteRecordPending(uint inputid, const ProgramInfo *pginfo,
             rec->RecordPending(pginfo, secsleft, hasLater);
             return true;
         }
+        else
+            return false;
     }
 
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(inputid));
@@ -79,6 +85,8 @@ bool RemoteStopLiveTV(uint inputid)
             rec->StopLiveTV();
             return true;
         }
+        else
+            return false;
     }
 
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(inputid));
@@ -100,6 +108,8 @@ bool RemoteStopRecording(uint inputid)
             rec->StopRecording();
             return true;
         }
+        else
+            return false;
     }
 
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(inputid));
@@ -370,6 +380,16 @@ bool RemoteIsBusy(uint inputid, InputInfo &busy_input)
         const TVRec *rec = TVRec::GetTVRec(inputid);
         if (rec)
             return rec->IsBusy(&busy_input);
+        else
+        {
+            // Note this value is intentionally different than the
+            // non-backend, error value below.  There is a small
+            // window when adding an input where an input can exist in
+            // the database, but not yet have a TVRec.  In such cases,
+            // we don't want it to be considered busy and block other
+            // actions.
+            return false;
+        }
     }
 
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(inputid));
