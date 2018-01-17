4442e68c14f4342a5693325fe527c7ce13c55fea
David Hampton <mythtv@love2code.net>
2018-01-16 21:22:19 -0500

    Replace deprecated qBinaryFind function with std::lower_bound.
    According to the Qt5.0 documentation, you should use standard library
    functions instead of the Qt supplied qBinaryFind function.  The docs
    recommend std::binary_search (for testing if an item is in a
    container) or std::lower_bound (for finding an item in a container).
    The old function has been deprecated.

--- mythtv/libs/libmythtv/dvdstream.cpp
+++ mythtv/libs/libmythtv/dvdstream.cpp
@@ -180,7 +180,8 @@ int DVDStream::safe_read(void *data, uint size)
     int ret = 0;
 
     // Are any blocks in the range encrypted?
-    list_t::const_iterator it = qBinaryFind(m_list, BlockRange(m_pos, lb, -1)); // FIXME: qBinaryFind is deprecated
+    list_t::const_iterator it;
+    it = std::lower_bound(m_list.begin(), m_list.end(), BlockRange(m_pos, lb, -1));
     uint32_t b = it == m_list.end() ? lb : m_pos < it->Start() ? it->Start() - m_pos : 0;
     if (b)
     {
