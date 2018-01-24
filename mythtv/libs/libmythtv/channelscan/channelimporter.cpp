02faf9a77b68060f4f3c4e27cbe518dc66647ac0
Roger Siddons <rsiddons@mythtv.org>
2018-01-24 13:26:11 +0000

    Fix crash when scanning channels
    mythtv-setup always crashes after user enters a channel number
    to resolve a channel conflict.
    
    The dialog gets destroyed twice.
    
    (cherry picked from commit 26b06f45708391183992fb91230f99b17c7143bc)

--- mythtv/libs/libmythtv/channelscan/channelimporter.cpp
+++ mythtv/libs/libmythtv/channelscan/channelimporter.cpp
@@ -1575,8 +1575,6 @@ OkCancelType ChannelImporter::ShowManualChannelPopup(
         }
         else
             delete textEdit;
-        delete popup;
-        popup = NULL;
     }
 
     bool ok = (0 == dc);
