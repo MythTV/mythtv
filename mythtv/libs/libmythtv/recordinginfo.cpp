1edade8a59af5286f30f2749d46e6ca76db642c9
David Hampton <mythtv@love2code.net>
2018-02-01 19:19:51 -0500

    Qt5.8 QDateTime: Update RecordingInfo::FinishedRecording to use the new API.
    This is a straight substitution of calls to ::toTime_t with calls to
    ::toSecsSinceEpoch with change of variables from uint to qint64.
    
    The QDateTime {to,from}Time_t functions were obsoleted in Qt5.8 and
    replaced by {to,from}SecsSinceEpoch functions.  The two sets of
    functions perform the same operation, only the latter set takes a
    wider range of values than the former (uint vs. qint64).

--- mythtv/libs/libmythtv/recordinginfo.cpp
+++ mythtv/libs/libmythtv/recordinginfo.cpp
@@ -1176,9 +1176,15 @@ void RecordingInfo::FinishedRecording(bool allowReRecord)
     {
         recstatus = RecStatus::Recorded;
 
+#if QT_VERSION < QT_VERSION_CHECK(5,8,0)
         uint starttime = recstartts.toTime_t();
         uint endtime   = recendts.toTime_t();
         int64_t duration = ((int64_t)endtime - (int64_t)starttime) * 1000000;
+#else
+        qint64 starttime = recstartts.toSecsSinceEpoch();
+        qint64 endtime   = recendts.toSecsSinceEpoch();
+        int64_t duration = (endtime - starttime) * 1000000;
+#endif
         SaveTotalDuration(duration);
 
         QString msg = "Finished recording";
