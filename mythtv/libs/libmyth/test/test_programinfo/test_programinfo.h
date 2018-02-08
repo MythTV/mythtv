d738c22be61dd304a956882a1d40baf41744c920
David Hampton <mythtv@love2code.net>
2018-02-08 17:17:48 -0500

    Make sure all testing QDateTime values are in UTC.
    Leaving them as localtime will cause test failures on the buildslaves
    that aren't in the same timezone as where the test was written.

--- mythtv/libs/libmyth/test/test_programinfo/test_programinfo.h
+++ mythtv/libs/libmyth/test/test_programinfo/test_programinfo.h
@@ -96,8 +96,8 @@ class TestProgramInfo : public QObject
     }
 
     QString draculalist = "Dracula||Its a movie.|0|0|0|||4294967295|||||0|946684800|946690200|4294967295||0|0|0|0|0|4294967295|0|0|0|946684800|946690200|0|Default|||tt0051554|11868|4294967295|0||Default|0|0|Default|0|0|0|1958|0|0|1|0||4294967295";
-    QString flash34list = "The Flash (2014)|The New Rogues|Barry continues to train Jesse ...|3|4|23|syndicatedepisode|Drama|1514|514|WNUVDT|WNUBDT (WNUV-DT)|/recordings/1514_20161025235800.ts|6056109800|1477439880|1477443720|0|localhost|0|0|0|0|0|0|0|15|8|1477439880|1477443720|0|Default||EP01922936|EP019229360055|ttvdb.py_279121|1477458754|0|2016-10-25|Default|0|0|Default|0|0|0|2016|0|0|4|715|Prime A-1|4294967295";
-    QString supergirl23list = "Supergirl|Welcome to Earth|An attack is made on the President as hot-button...|2|3|23|syndicatedepisode|Drama|1514|514|WNUVDT|WNUBDT (WNUV-DT)|/recordings/1514_20161024235800.ts|6056109670|1477353480|1477357320|0|localhost|0|0|0|0|0|0|0|15|8|1477353480|1477357320|0|Default||EP02185451|EP021854510025|ttvdb.py_295759|1477458754|0|2016-10-24|Default|0|0|Default|0|0|0|2016|0|0|4|711|Prime A-0|4294967295";
+    QString flash34list = "The Flash (2014)|The New Rogues|Barry continues to train Jesse ...|3|4|23|syndicatedepisode|Drama|1514|514|WNUVDT|WNUBDT (WNUV-DT)|/recordings/1514_20161025235800.ts|6056109800|1477439880|1477443720|0|localhost|0|0|0|0|0|0|0|15|8|1477439880|1477443720|0|Default||EP01922936|EP019229360055|ttvdb.py_279121|1477444354|0|2016-10-25|Default|0|0|Default|0|0|0|2016|0|0|4|715|Prime A-1|4294967295";
+    QString supergirl23list = "Supergirl|Welcome to Earth|An attack is made on the President as hot-button...|2|3|23|syndicatedepisode|Drama|1514|514|WNUVDT|WNUBDT (WNUV-DT)|/recordings/1514_20161024235800.ts|6056109670|1477353480|1477357320|0|localhost|0|0|0|0|0|0|0|15|8|1477353480|1477357320|0|Default||EP02185451|EP021854510025|ttvdb.py_295759|1477444354|0|2016-10-24|Default|0|0|Default|0|0|0|2016|0|0|4|711|Prime A-0|4294967295";
     ProgramInfo dracula;
     ProgramInfo flash34;
     ProgramInfo supergirl23;
@@ -123,7 +123,7 @@ class TestProgramInfo : public QObject
              MythDate::fromString("2016-10-25 23:58:00"),
              MythDate::fromString("2016-10-26 01:02:00"),
              0.0, 2016, 0, 0, QDate(2016,10,25),
-             QDateTime(QDate(2016,10,26),QTime(1,12,34)),
+             MythDate::fromString("2016-10-26 01:12:34"),
              RecStatus::Unknown, 0,
              kDupsInAll, kDupCheckSubThenDesc,
              0, 0, 0, 0, 0, "Prime A-1",
@@ -145,7 +145,7 @@ class TestProgramInfo : public QObject
              MythDate::fromString("2016-10-24 23:58:00"),
              MythDate::fromString("2016-10-25 01:02:00"),
              0.0, 2016, 0, 0, QDate(2016,10,24),
-             QDateTime(QDate(2016,10,26),QTime(1,12,34)),
+             MythDate::fromString("2016-10-26 01:12:34"),
              RecStatus::Unknown, 0,
              kDupsInAll, kDupCheckSubThenDesc,
              0, 0, 0, 0, 0, "Prime A-0",
