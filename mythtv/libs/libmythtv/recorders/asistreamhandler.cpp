fbc6d32358f2b94153834c85d16b4071a8b02a0f
John Poet <jpoet@mythtv.org>
2018-03-08 15:53:29 -0700

    Implement ASIStreamHandler::PriorityEvent.
    Log events from the ASI driver.

--- mythtv/libs/libmythtv/recorders/asistreamhandler.cpp
+++ mythtv/libs/libmythtv/recorders/asistreamhandler.cpp
@@ -349,7 +349,38 @@ void ASIStreamHandler::Close(void)
     }
 }
 
-void ASIStreamHandler::PriorityEvent(int /*fd*/)
+void ASIStreamHandler::PriorityEvent(int fd)
 {
-    // TODO report on buffer overruns, etc.
+    int val;
+    if(ioctl(fd, ASI_IOC_RXGETEVENTS, &val) < 0)
+    {
+        LOG(VB_GENERAL, LOG_ERR, LOC + QString("Failed to open device %1: ")
+            .arg(_device) + ENO);
+        //TODO: Handle error
+        return;
+    }
+    if(val & ASI_EVENT_RX_BUFFER)
+        LOG(VB_RECORD, LOG_ERR, LOC +
+            QString("Driver receive buffer queue overrun detected %1")
+            .arg(_device));
+    if(val & ASI_EVENT_RX_FIFO)
+        LOG(VB_RECORD, LOG_ERR, LOC +
+            QString("Driver receive FIFO overrun detected %1")
+            .arg(_device));
+    if(val & ASI_EVENT_RX_CARRIER)
+        LOG(VB_RECORD, LOG_NOTICE, LOC +
+            QString("Career Status change detected %1")
+            .arg(_device));
+    if(val & ASI_EVENT_RX_LOS)
+        LOG(VB_RECORD, LOG_ERR, LOC +
+            QString("Loss of Packet Sync detected %1")
+            .arg(_device));
+    if(val & ASI_EVENT_RX_AOS)
+        LOG(VB_RECORD, LOG_NOTICE, LOC +
+            QString("Acquisition of Sync detected %1")
+            .arg(_device));
+    if(val & ASI_EVENT_RX_DATA)
+        LOG(VB_RECORD, LOG_NOITCE, LOC +
+            QString("Receive data status change detected %1")
+            .arg(_device));
 }
