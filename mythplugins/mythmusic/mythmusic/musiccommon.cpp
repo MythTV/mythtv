MythTV/mythtv

commit a2cb1e42fbc1979d7a995a2d9a4862a66fc73cea

MusicCommon::keyPressEvent should call its parent.
This cleans up the code, since each of its callers doesn't have to
explicitly call both its parent (this function) and its grandparent.

This issue was pointed out by the clang-tidy
bugprone-parent-virtual-call check.

Fixes #13323

mythplugins/mythmusic/mythmusic/musiccommon.cpp
@@ -890,6 +890,8 @@ bool MusicCommon::keyPressEvent(QKeyEvent *e)
             handled = false;
     }
 
+    if (!handled && MythScreenType::keyPressEvent(e))
+        handled = true;
     return handled;
 }
 

mythplugins/mythmusic/mythmusic/lyricsview.cpp
@@ -330,9 +330,6 @@ bool LyricsView::keyPressEvent(QKeyEvent *event)
     if (!handled && MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }
 

mythplugins/mythmusic/mythmusic/miniplayer.cpp
@@ -99,8 +99,5 @@ bool MiniPlayer::keyPressEvent(QKeyEvent *event)
     if (!handled && MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }

mythplugins/mythmusic/mythmusic/playlisteditorview.cpp
@@ -535,9 +535,6 @@ bool PlaylistEditorView::keyPressEvent(QKeyEvent *event)
     if (!handled && MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }
 

mythplugins/mythmusic/mythmusic/playlistview.cpp
@@ -53,8 +53,5 @@ bool PlaylistView::keyPressEvent(QKeyEvent *event)
     if (MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }

mythplugins/mythmusic/mythmusic/searchview.cpp
@@ -254,9 +254,6 @@ bool SearchView::keyPressEvent(QKeyEvent *event)
     if (!handled && MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }
 

mythplugins/mythmusic/mythmusic/streamview.cpp
@@ -369,9 +369,6 @@ bool StreamView::keyPressEvent(QKeyEvent *event)
     if (!handled && MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }
 

mythplugins/mythmusic/mythmusic/visualizerview.cpp
@@ -84,9 +84,6 @@ bool VisualizerView::keyPressEvent(QKeyEvent *event)
     if (!handled && MusicCommon::keyPressEvent(event))
         handled = true;
 
-    if (!handled && MythScreenType::keyPressEvent(event))
-        handled = true;
-
     return handled;
 }
 

commit 3aebace9cc6ebee676ac95d071e7a24f7483e40e

Fix jump point handling in mythmusic broken by a2cb1e42fb.
Before a2cb1e42fb the special handling for jump points in
MusicCommon::keyPressEvent always returned false, which caused its
caller to subsequently call MythScreenType::keyPressEvent.  Now that
MusicCommon is calling up to MythScreenType instead the caller, it
needs to call up for jump point handling as well.

mythplugins/mythmusic/mythmusic/musiccommon.cpp
@@ -613,7 +613,7 @@ bool MusicCommon::keyPressEvent(QKeyEvent *e)
         if (gPlayer->isPlaying() && gCoreContext->GetSetting("MusicJumpPointAction", "stop") == "stop")
             gPlayer->stop(true);
 
-        return false;
+        return MythScreenType::keyPressEvent(e);
     }
 
     QStringList actions;