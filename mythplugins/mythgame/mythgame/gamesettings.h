MythTV/mythtv

commit b12855645900473f322fbf045a951345280e8bb7

MythWelcome: Port settings to MythUI
Themed setting dialogs were removed by d14db4bd3e.
Subsequently settings appeared as Qt dialogs.

Also removes MythWelcome DateFormat setting, which is no longer used.

No new translations, 1 or 2 removed.

Refs #10092

mythtv/programs/mythwelcome/welcomesettings.h
@@ -1,23 +1,20 @@
 #ifndef WELCOMESETTINGS_H
 #define WELCOMESETTINGS_H
 
-// Qt headers
-#include <QCoreApplication>
-
 // MythTV headers
-#include <mythconfigdialogs.h>
+#include <standardsettings.h>
 
-class MythWelcomeSettings : public ConfigurationWizard
+class MythWelcomeSettings : public GroupSetting
 {
-    Q_DECLARE_TR_FUNCTIONS(MythWelcomeSettings);
+    Q_DECLARE_TR_FUNCTIONS(MythWelcomeSettings)
 
   public:
     MythWelcomeSettings();
 };
 
-class MythShutdownSettings : public ConfigurationWizard
+class MythShutdownSettings : public GroupSetting
 {
-    Q_DECLARE_TR_FUNCTIONS(MythShutdownSettings);
+    Q_DECLARE_TR_FUNCTIONS(MythShutdownSettings)
 
   public:
     MythShutdownSettings();

mythtv/libs/libmyth/standardsettings.h
@@ -281,6 +281,30 @@ class MPUBLIC HostTimeBoxSetting : public HostComboBoxSetting
     }
 };
 
+class MPUBLIC GlobalTimeBoxSetting : public GlobalComboBoxSetting
+{
+  public:
+    GlobalTimeBoxSetting(const QString &name,
+                       const QString &defaultTime = "00:00",
+                       const int interval = 1) :
+        GlobalComboBoxSetting(name, false)
+    {
+        int hour;
+        int minute;
+        QString timeStr;
+
+        for (hour = 0; hour < 24; hour++)
+        {
+            for (minute = 0; minute < 60; minute += interval)
+            {
+                timeStr = timeStr.sprintf("%02d:%02d", hour, minute);
+                addSelection(timeStr, timeStr,
+                             timeStr == defaultTime);
+            }
+        }
+    }
+};
+
 /*******************************************************************************
 *                           SpinBox Setting                                    *
 *******************************************************************************/

mythtv/programs/mythwelcome/main.cpp
@@ -1,25 +1,14 @@
-#include <cstdlib>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <chrono> // for milliseconds
-#include <thread> // for sleep_for
-
 // Qt
 #include <QApplication>
-#include <QFileInfo>
-#include <QDir>
 
 // MythTV
 #include "mythcontext.h"
 #include "mythversion.h"
 #include "mythtranslation.h"
-#include "mythdbcon.h"
 #include "exitcodes.h"
 #include "compat.h"
 #include "lcddevice.h"
 #include "commandlineparser.h"
-#include "tv.h"
 #include "loggingserver.h"
 #include "mythlogging.h"
 #include "signalhandling.h"
@@ -132,32 +121,26 @@ int main(int argc, char **argv)
     // Provide systemd ready notification (for type=notify units)
     mw_sd_notify("READY=1");
 
+    MythScreenStack *mainStack = mainWindow->GetMainStack();
+
+    MythScreenType *screen;
     if (bShowSettings)
-    {
-        MythShutdownSettings settings;
-        settings.exec();
-    }
+        screen = new StandardSettingDialog(mainStack, "shutdown",
+                                           new MythShutdownSettings());
     else
+        screen = new WelcomeDialog(mainStack, "mythwelcome");
+
+    bool ok = screen->Create();
+    if (ok)
     {
-        MythScreenStack *mainStack = GetMythMainWindow()->GetMainStack();
+        mainStack->AddScreen(screen, false);
 
-        WelcomeDialog *welcome = new WelcomeDialog(mainStack, "mythwelcome");
-
-        if (welcome->Create())
-            mainStack->AddScreen(welcome, false);
-        else
-        {
-            DestroyMythMainWindow();
-            delete gContext;
-            SignalHandler::Done();
-            return -1;
-        }
-
-        do
-        {
-            qApp->processEvents();
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        } while (mainStack->TotalScreens() > 0);
+        // Block by running an event loop until last screen is removed
+        QEventLoop block;
+        QObject::connect(mainStack, &MythScreenStack::topScreenChanged,
+                         &block, [&](MythScreenType* screen)
+        { if (!screen) block.quit(); });
+        block.exec();
     }
 
     mw_sd_notify("STOPPING=1\nSTATUS=Exiting");
@@ -167,5 +150,5 @@ int main(int argc, char **argv)
 
     SignalHandler::Done();
 
-    return 0;
+    return ok ? 0 : -1;
 }

mythtv/programs/mythwelcome/welcomedialog.cpp
@@ -46,10 +46,6 @@ WelcomeDialog::WelcomeDialog(MythScreenStack *parent, const char *name)
     m_idleWaitForRecordingTime =
                        gCoreContext->GetNumSetting("idleWaitForRecordingTime", 15);
 
-    m_timeFormat = gCoreContext->GetSetting("TimeFormat", "h:mm AP");
-    m_dateFormat = gCoreContext->GetSetting("MythWelcomeDateFormat", "dddd\\ndd MMM yyyy");
-    m_dateFormat.replace("\\n", "\n");
-
     // if idleTimeoutSecs is 0, the user disabled the auto-shutdown feature
     m_bWillShutdown = (gCoreContext->GetNumSetting("idleTimeoutSecs", 0) != 0);
 
@@ -216,6 +212,17 @@ void WelcomeDialog::customEvent(QEvent *e)
     }
 }
 
+void WelcomeDialog::ShowSettings(GroupSetting *screen)
+{
+    MythScreenStack *mainStack = GetMythMainWindow()->GetMainStack();
+    StandardSettingDialog *ssd = new StandardSettingDialog(mainStack, "settings",
+                                                           screen);
+    if (ssd->Create())
+        mainStack->AddScreen(ssd);
+    else
+        delete ssd;
+}
+
 bool WelcomeDialog::keyPressEvent(QKeyEvent *event)
 {
     if (GetFocusWidget()->keyPressEvent(event))
@@ -243,22 +250,11 @@ bool WelcomeDialog::keyPressEvent(QKeyEvent *event)
         }
         else if (action == "INFO")
         {
-            MythWelcomeSettings settings;
-            if (kDialogCodeAccepted == settings.exec())
-            {
-                gCoreContext->SendMessage("CLEAR_SETTINGS_CACHE");
-                updateStatus();
-                updateScreen();
-
-                m_dateFormat = gCoreContext->GetSetting("MythWelcomeDateFormat", "dddd\\ndd MMM yyyy");
-                m_dateFormat.replace("\\n", "\n");
-            }
+            ShowSettings(new MythWelcomeSettings());
         }
         else if (action == "SHOWSETTINGS")
         {
-            MythShutdownSettings settings;
-            if (kDialogCodeAccepted == settings.exec())
-                gCoreContext->SendMessage("CLEAR_SETTINGS_CACHE");
+            ShowSettings(new MythShutdownSettings());
         }
         else if (action == "0")
         {

mythtv/programs/mythwelcome/welcomedialog.h
@@ -16,6 +16,8 @@
 #include "mythuitext.h"
 #include "mythdialogbox.h"
 
+class GroupSetting;
+
 class WelcomeDialog : public MythScreenType
 {
 
@@ -50,6 +52,7 @@ class WelcomeDialog : public MythScreenType
     bool checkConnectionToServer(void);
     void checkAutoStart(void);
     void runMythFillDatabase(void);
+    void ShowSettings(GroupSetting* screen);
 
     //
     //  GUI stuff
@@ -67,8 +70,6 @@ class WelcomeDialog : public MythScreenType
     QTimer        *m_updateScreenTimer; // audited ref #5318
 
     QString        m_appBinDir;
-    QString        m_timeFormat;
-    QString        m_dateFormat;
     bool           m_isRecording;
     bool           m_hasConflicts;
     bool           m_bWillShutdown;

mythtv/programs/mythwelcome/welcomesettings.cpp
@@ -1,5 +1,3 @@
-#include <unistd.h>
-
 #include "welcomesettings.h"
 #include "mythcorecontext.h"
 #include "mythdirs.h"
@@ -8,208 +6,164 @@
 //  daily wakeup/shutdown settings
 ///////////////////////////////////////////////////////////////////
 
-static GlobalTimeBox *DailyWakeupStart1()
-{
-    GlobalTimeBox *gc = new GlobalTimeBox("DailyWakeupStartPeriod1", "00:00");
-    gc->setLabel(MythWelcomeSettings::tr("Period %1 start time").arg("1"));
-    gc->setHelpText(MythWelcomeSettings::tr("Period %1 start time. "
-        "Defines a period the master backend should be awake. " 
-        "Set both Start & End times to 00:00 to disable.").arg("1"));
-    return gc;
-};
+#define TR MythWelcomeSettings::tr
 
-static GlobalTimeBox *DailyWakeupEnd1()
+static StandardSetting *DailyWakeupStart(QString n)
 {
-    GlobalTimeBox *gc = new GlobalTimeBox("DailyWakeupEndPeriod1", "00:00");
-    gc->setLabel(MythWelcomeSettings::tr("Period %1 end time").arg("1"));
-    gc->setHelpText(MythWelcomeSettings::tr("Period %1 end time. "
-        "Defines a period the master backend should be awake. "
-        "Set both Start & End times to 00:00 to disable.").arg("1"));
+    auto gc = new GlobalTimeBoxSetting("DailyWakeupStartPeriod" + n, "00:00");
+    gc->setLabel(TR("Period %1 start time").arg(n));
+    gc->setHelpText(TR("Period %1 start time. "
+                       "Defines a period the master backend should be awake. "
+                       "Set both Start & End times to 00:00 to disable.").arg(n));
     return gc;
-};
+}
 
-static GlobalTimeBox *DailyWakeupStart2()
+static StandardSetting *DailyWakeupEnd(QString n)
 {
-    GlobalTimeBox *gc = new GlobalTimeBox("DailyWakeupStartPeriod2", "00:00");
-    gc->setLabel(MythWelcomeSettings::tr("Period %1 start time").arg("2"));
-    gc->setHelpText(MythWelcomeSettings::tr("Period %1 start time. "
-        "Defines a period the master backend should be awake. "
-        "Set both Start & End times to 00:00 to disable.").arg("2"));
+    auto *gc = new GlobalTimeBoxSetting("DailyWakeupEndPeriod" + n, "00:00");
+    gc->setLabel(TR("Period %1 end time").arg(n));
+    gc->setHelpText(TR("Period %1 end time. "
+                       "Defines a period the master backend should be awake. "
+                       "Set both Start & End times to 00:00 to disable.").arg(n));
     return gc;
-};
+}
 
-static GlobalTimeBox *DailyWakeupEnd2()
+static StandardSetting *DailyWakeup(QString n)
 {
-    GlobalTimeBox *gc = new GlobalTimeBox("DailyWakeupEndPeriod2", "00:00");
-    gc->setLabel(MythWelcomeSettings::tr("Period %1 end time").arg("2"));
-    gc->setHelpText(MythWelcomeSettings::tr("Period %1 end time. "
-        "Defines a period the master backend should be awake. "
-        "Set both Start & End times to 00:00 to disable.").arg("2"));
+    auto gc = new GroupSetting();
+
+    gc->setLabel(TR("Daily Wakeup/ShutDown Period %1").arg(n));
+    gc->addChild(DailyWakeupStart(n));
+    gc->addChild(DailyWakeupEnd(n));
     return gc;
-};
+}
 
-static HostCheckBox *AutoStartFrontend()
+static StandardSetting *AutoStartFrontend()
 {
-    HostCheckBox *gc = new HostCheckBox("AutoStartFrontend");
-    gc->setLabel(MythWelcomeSettings::tr("Automatically Start mythfrontend"));
+    auto gc = new HostCheckBoxSetting("AutoStartFrontend");
+    gc->setLabel(TR("Automatically Start mythfrontend"));
     gc->setValue(true);
-    gc->setHelpText(MythWelcomeSettings::tr("Mythwelcome will automatically "
-                        "start mythfrontend if it is determined that it was "
-                        "not started to record a program."));
+    gc->setHelpText(TR("Mythwelcome will automatically "
+                       "start mythfrontend if it is determined that it was "
+                       "not started to record a program."));
     return gc;
-};
+}
 
-static HostCheckBox *ShutdownWithBE()
+static StandardSetting *ShutdownWithBE()
 {
-    HostCheckBox *gc = new HostCheckBox("ShutdownWithMasterBE");
-    gc->setLabel(MythWelcomeSettings::tr("Shutdown with Master Backend"));
+    auto gc = new HostCheckBoxSetting("ShutdownWithMasterBE");
+    gc->setLabel(TR("Shutdown with Master Backend"));
     gc->setValue(false);
-    gc->setHelpText(MythWelcomeSettings::tr("Mythwelcome will automatically "
-                        "shutdown this computer when the master backend shuts "
-                        "down. Should only be set on frontend only machines"));
+    gc->setHelpText(TR("Mythwelcome will automatically "
+                       "shutdown this computer when the master backend shuts "
+                       "down. Should only be set on frontend only machines"));
     return gc;
-};
+}
 
-static HostLineEdit *MythWelcomeDateFormat()
+MythWelcomeSettings::MythWelcomeSettings() : GroupSetting()
 {
-    HostLineEdit *gc = new HostLineEdit("MythWelcomeDateFormat");
-    gc->setLabel(MythWelcomeSettings::tr("Date Format"));
-    gc->setValue("dddd\ndd MMM yyyy");
-    gc->setHelpText(MythWelcomeSettings::tr("This is the format to use to "
-                        "display the date. "
-                        "See http://doc.trolltech.com/3.3/qdate.html#toString "
-                        "for a list of valid format specifiers."));
-    return gc;
-};
+    setLabel(tr("MythWelcome Settings"));
 
-MythWelcomeSettings::MythWelcomeSettings()
-{
-    HorizontalConfigurationGroup* hcg1 =
-              new HorizontalConfigurationGroup(true, true);
-    HorizontalConfigurationGroup* hcg2 =
-              new HorizontalConfigurationGroup(true, true);
-    VerticalConfigurationGroup* vcg = 
-              new VerticalConfigurationGroup(false);
+    addChild(DailyWakeup("1"));
+    addChild(DailyWakeup("2"));
 
-    vcg->setLabel(tr("MythWelcome Settings"));
-
-    hcg1->setLabel(tr("Daily Wakeup/ShutDown Period %1").arg("1"));
-    hcg1->addChild(DailyWakeupStart1());
-    hcg1->addChild(DailyWakeupEnd1());
-    vcg->addChild(hcg1);
-
-    hcg2->setLabel(tr("Daily Wakeup/ShutDown Period %1").arg("2"));
-    hcg2->addChild(DailyWakeupStart2());
-    hcg2->addChild(DailyWakeupEnd2());
-    vcg->addChild(hcg2);
-
-    vcg->addChild(AutoStartFrontend());
+    addChild(AutoStartFrontend());
 
     // this setting only makes sense on frontend only machines
     if (gCoreContext->IsFrontendOnly())
-    {
-        vcg->addChild(ShutdownWithBE());
-    }
-
-    vcg->addChild(MythWelcomeDateFormat());
-
-    addChild(vcg);
+        addChild(ShutdownWithBE());
 }
 
 ///////////////////////////////////////////////////////////////////
 //  mythshutdown script settings
 ///////////////////////////////////////////////////////////////////
 
-static HostLineEdit *MythShutdownNvramCmd()
-{
-    HostLineEdit *gc = new HostLineEdit("MythShutdownNvramCmd");
-    gc->setLabel(MythShutdownSettings::tr("Command to Set Wakeup Time"));
-    gc->setValue("/usr/bin/nvram-wakeup --settime $time");
-    gc->setHelpText(MythShutdownSettings::tr("Command to set the wakeup time "
-                        "in the BIOS. See the README file for more examples."));
-    return gc;
-};
+#undef TR
+#define TR MythShutdownSettings::tr
 
-static HostComboBox *WakeupTimeFormat()
+static StandardSetting *MythShutdownNvramCmd()
 {
-    HostComboBox *gc = new HostComboBox("MythShutdownWakeupTimeFmt", true);
-    gc->setLabel(MythShutdownSettings::tr("Wakeup time format"));
+    auto gc = new HostTextEditSetting("MythShutdownNvramCmd");
+    gc->setLabel(TR("Command to Set Wakeup Time"));
+    gc->setValue("/usr/bin/nvram-wakeup --settime $time");
+    gc->setHelpText(TR("Command to set the wakeup time "
+                       "in the BIOS. See the README file for more examples."));
+    return gc;
+}
+
+static StandardSetting *WakeupTimeFormat()
+{
+    auto gc = new HostComboBoxSetting("MythShutdownWakeupTimeFmt", true);
+    gc->setLabel(TR("Wakeup time format"));
     gc->addSelection("time_t");
     gc->addSelection("yyyy-MM-dd hh:mm:ss");
-    gc->setHelpText(MythShutdownSettings::tr("The format of the time string "
-                        "passed to the \'Set Wakeup Time Command\' as $time. "
-                        "See QT::QDateTime.toString() for details. Set to "
-                        "'time_t' for seconds since epoch (use time_t for "
-                        "nvram_wakeup)."));
+    gc->setHelpText(TR("The format of the time string "
+                       "passed to the \'Set Wakeup Time Command\' as $time. "
+                       "See QT::QDateTime.toString() for details. Set to "
+                       "'time_t' for seconds since epoch (use time_t for "
+                       "nvram_wakeup)."));
     return gc;
-};
+}
 
-static HostLineEdit *MythShutdownNvramRestartCmd()
+static StandardSetting *MythShutdownNvramRestartCmd()
 {
-    HostLineEdit *gc = new HostLineEdit("MythShutdownNvramRestartCmd");
-    gc->setLabel(MythShutdownSettings::tr("nvram-wakeup Restart Command"));
+    auto gc = new HostTextEditSetting("MythShutdownNvramRestartCmd");
+    gc->setLabel(TR("nvram-wakeup Restart Command"));
     gc->setValue("/sbin/grub-set-default 1");
-    gc->setHelpText(MythShutdownSettings::tr("Command to run if your bios "
-                        "requires you to reboot to allow nvram-wakeup settings "
-                        "to take effect. Leave blank if your bios does not "
-                        "require a reboot. See the README file for more "
-                        "examples."));
+    gc->setHelpText(TR("Command to run if your bios "
+                       "requires you to reboot to allow nvram-wakeup settings "
+                       "to take effect. Leave blank if your bios does not "
+                       "require a reboot. See the README file for more "
+                       "examples."));
     return gc;
-};
+}
 
-static HostLineEdit *MythShutdownReboot()
+static StandardSetting *MythShutdownReboot()
 {
-    HostLineEdit *gc = new HostLineEdit("MythShutdownReboot");
-    gc->setLabel(MythShutdownSettings::tr("Command to reboot"));
+    auto gc = new HostTextEditSetting("MythShutdownReboot");
+    gc->setLabel(TR("Command to reboot"));
     gc->setValue("/sbin/reboot");
-    gc->setHelpText(MythShutdownSettings::tr("Command to reboot computer.")); 
+    gc->setHelpText(TR("Command to reboot computer."));
     return gc;
-};
+}
 
-static HostLineEdit *MythShutdownPowerOff()
+static StandardSetting *MythShutdownPowerOff()
 {
-    HostLineEdit *gc = new HostLineEdit("MythShutdownPowerOff");
-    gc->setLabel(MythShutdownSettings::tr("Command to shutdown"));
+    auto gc = new HostTextEditSetting("MythShutdownPowerOff");
+    gc->setLabel(TR("Command to shutdown"));
     gc->setValue("/sbin/poweroff");
-    gc->setHelpText(MythShutdownSettings::tr("Command to shutdown computer."));
+    gc->setHelpText(TR("Command to shutdown computer."));
     return gc;
-};
+}
 
-static HostLineEdit *MythShutdownStartFECmd()
+static StandardSetting *MythShutdownStartFECmd()
 {
-    HostLineEdit *gc = new HostLineEdit("MythWelcomeStartFECmd");
-    gc->setLabel(MythShutdownSettings::tr("Command to run to start the "
-                                          "Frontend"));
+    auto gc = new HostTextEditSetting("MythWelcomeStartFECmd");
+    gc->setLabel(TR("Command to run to start the Frontend"));
     gc->setValue(GetAppBinDir() + "mythfrontend");
-    gc->setHelpText(MythShutdownSettings::tr("Command to start mythfrontend."));
+    gc->setHelpText(TR("Command to start mythfrontend."));
     return gc;
-};
+}
 
-static HostLineEdit *MythShutdownXTermCmd()
+static StandardSetting *MythShutdownXTermCmd()
 {
-    HostLineEdit *gc = new HostLineEdit("MythShutdownXTermCmd");
-    gc->setLabel(MythShutdownSettings::tr("Command to run Xterm"));
+    auto gc = new HostTextEditSetting("MythShutdownXTermCmd");
+    gc->setLabel(TR("Command to run Xterm"));
     gc->setValue("xterm");
-    gc->setHelpText(MythShutdownSettings::tr("Command to start an Xterm. Can "
-                                             "be disabled by leaving this "
-                                             "setting blank.")); 
+    gc->setHelpText(TR("Command to start an Xterm. Can "
+                       "be disabled by leaving this "
+                       "setting blank."));
     return gc;
-};
-
+}
 
 MythShutdownSettings::MythShutdownSettings()
 {
-    VerticalConfigurationGroup* vcg = 
-              new VerticalConfigurationGroup(false);
-    
-    vcg->setLabel(tr("MythShutdown/MythWelcome Settings"));
-    vcg->addChild(MythShutdownNvramCmd());
-    vcg->addChild(WakeupTimeFormat());
-    vcg->addChild(MythShutdownNvramRestartCmd());
-    vcg->addChild(MythShutdownReboot());
-    vcg->addChild(MythShutdownPowerOff());
-    vcg->addChild(MythShutdownXTermCmd());
-    vcg->addChild(MythShutdownStartFECmd());
-
-    addChild(vcg);
+    setLabel(tr("MythShutdown/MythWelcome Settings"));
+    addChild(MythShutdownNvramCmd());
+    addChild(WakeupTimeFormat());
+    addChild(MythShutdownNvramRestartCmd());
+    addChild(MythShutdownReboot());
+    addChild(MythShutdownPowerOff());
+    addChild(MythShutdownXTermCmd());
+    addChild(MythShutdownStartFECmd());
 }

commit a09c30df9a81a94080e8bac54ebc2dfa12dff244

MythWelcome & MythGame: Fix Qt5.3 translations (again)
1a79c2197 & b12855645 broke builds when using Qt5.3

Refs #10092

mythplugins/mythgame/mythgame/gamesettings.h
@@ -9,6 +9,8 @@ const QString GetGameTypeExtensions(const QString &GameType);
 
 struct MPUBLIC GameGeneralSettings : public GroupSetting
 {
+    Q_OBJECT
+public:
     GameGeneralSettings();
 };
 
@@ -22,6 +24,7 @@ struct PlayerId : public AutoIncrementSetting
 
 class GamePlayerSetting : public GroupSetting
 {
+    Q_OBJECT
 public:
     GamePlayerSetting(QString name, uint id = 0);
 
@@ -35,6 +38,7 @@ private:
 
 class MPUBLIC GamePlayersList : public GroupSetting
 {
+    Q_OBJECT
 public:
     GamePlayersList();
 

mythtv/programs/mythwelcome/welcomesettings.h
@@ -4,19 +4,17 @@
 // MythTV headers
 #include <standardsettings.h>
 
-class MythWelcomeSettings : public GroupSetting
+class MPUBLIC MythWelcomeSettings : public GroupSetting
 {
-    Q_DECLARE_TR_FUNCTIONS(MythWelcomeSettings);
-
+    Q_OBJECT
   public:
     MythWelcomeSettings();
 };
 
-class MythShutdownSettings : public GroupSetting
+class MPUBLIC MythShutdownSettings : public GroupSetting
 {
-    Q_DECLARE_TR_FUNCTIONS(MythShutdownSettings);
-
-  public:
+    Q_OBJECT
+public:
     MythShutdownSettings();
 };
 