MythTV/mythtv

commit f049e518b2f2d5eabf9676d2b412d7858a38e663

MythWelcome: Fix Debian semicolons
b128556459 removed superfluous semicolons & broke Debian

Refs: #10092

mythtv/programs/mythwelcome/welcomesettings.h
@@ -6,16 +6,12 @@
 
 class MythWelcomeSettings : public GroupSetting
 {
-    Q_DECLARE_TR_FUNCTIONS(MythWelcomeSettings)
-
   public:
     MythWelcomeSettings();
 };
 
 class MythShutdownSettings : public GroupSetting
 {
-    Q_DECLARE_TR_FUNCTIONS(MythShutdownSettings)
-
   public:
     MythShutdownSettings();
 };

commit 2cc0cab60ffab01a553f7e65f2c5db7a55873330

MythWelcome: Fix settings translations
f049e518b broke translations

Refs: #10092

mythtv/programs/mythwelcome/welcomesettings.h
@@ -6,12 +6,16 @@
 
 class MythWelcomeSettings : public GroupSetting
 {
+    Q_DECLARE_TR_FUNCTIONS(MythWelcomeSettings);
+
   public:
     MythWelcomeSettings();
 };
 
 class MythShutdownSettings : public GroupSetting
 {
+    Q_DECLARE_TR_FUNCTIONS(MythShutdownSettings);
+
   public:
     MythShutdownSettings();
 };