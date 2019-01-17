a39c8a9165e7e463391d2a90a59def7ec07e8b81
Mark Kendall <mark.kendall@gmail.com>
2019-01-17 10:05:58 +0000

    MythRenderOpenGL: Move shaders into a seperate file

--- mythtv/libs/libmythui/mythrender_opengl.cpp
+++ mythtv/libs/libmythui/mythrender_opengl.cpp
@@ -11,6 +11,7 @@ using std::min;
 
 // MythTV
 #include "mythrender_opengl.h"
+#include "mythrender_opengl_shaders.h"
 #include "mythlogging.h"
 #include "mythuitype.h"
 #include "mythxdisplay.h"
@@ -30,108 +31,6 @@ using std::min;
 static const GLuint kVertexOffset  = 0;
 static const GLuint kTextureOffset = 8 * sizeof(GLfloat);
 static const GLuint kVertexSize    = 16 * sizeof(GLfloat);
-
-static const QString kDefaultVertexShader =
-"attribute vec2 a_position;\n"
-"attribute vec4 a_color;\n"
-"attribute vec2 a_texcoord0;\n"
-"varying   vec4 v_color;\n"
-"varying   vec2 v_texcoord0;\n"
-"uniform   mat4 u_projection;\n"
-"uniform   mat4 u_transform;\n"
-"void main() {\n"
-"    gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);\n"
-"    v_texcoord0 = a_texcoord0;\n"
-"    v_color     = a_color;\n"
-"}\n";
-
-static const QString kDefaultFragmentShader =
-"uniform sampler2D s_texture0;\n"
-"varying highp vec4 v_color;\n"
-"varying highp vec2 v_texcoord0;\n"
-"void main(void)\n"
-"{\n"
-"    gl_FragColor = texture2D(s_texture0, v_texcoord0) * v_color;\n"
-"}\n";
-
-static const QString kSimpleVertexShader =
-"attribute vec2 a_position;\n"
-"attribute vec4 a_color;\n"
-"varying   vec4 v_color;\n"
-"uniform   mat4 u_projection;\n"
-"uniform   mat4 u_transform;\n"
-"void main() {\n"
-"    gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);\n"
-"    v_color     = a_color;\n"
-"}\n";
-
-static const QString kSimpleFragmentShader =
-"varying highp vec4 v_color;\n"
-"void main(void)\n"
-"{\n"
-"    gl_FragColor = v_color;\n"
-"}\n";
-
-static const QString kDrawVertexShader =
-"attribute vec2 a_position;\n"
-"attribute vec4 a_color;\n"
-"varying   vec4 v_color;\n"
-"varying   vec2 v_position;\n"
-"uniform   mat4 u_projection;\n"
-"uniform   mat4 u_transform;\n"
-"void main() {\n"
-"    gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);\n"
-"    v_color     = a_color;\n"
-"    v_position  = a_position;\n"
-"}\n";
-
-static const QString kCircleFragmentShader =
-"varying highp vec4 v_color;\n"
-"varying highp vec2 v_position;\n"
-"uniform highp mat4 u_parameters;\n"
-"void main(void)\n"
-"{\n"
-"    highp float dis = distance(v_position.xy, u_parameters[0].xy);\n"
-"    highp float mult = smoothstep(u_parameters[0].z, u_parameters[0].w, dis);\n"
-"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
-"}\n";
-
-static const QString kCircleEdgeFragmentShader =
-"varying highp vec4 v_color;\n"
-"varying highp vec2 v_position;\n"
-"uniform highp mat4 u_parameters;\n"
-"void main(void)\n"
-"{\n"
-"    highp float dis = distance(v_position.xy, u_parameters[0].xy);\n"
-"    highp float rad = u_parameters[0].z;\n"
-"    highp float wid = u_parameters[0].w;\n"
-"    highp float mult = smoothstep(rad + wid, rad + (wid - 1.0), dis) * smoothstep(rad - (wid + 1.0), rad - wid, dis);\n"
-"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
-"}\n";
-
-static const QString kVertLineFragmentShader =
-"varying highp vec4 v_color;\n"
-"varying highp vec2 v_position;\n"
-"uniform highp mat4 u_parameters;\n"
-"void main(void)\n"
-"{\n"
-"    highp float dis = abs(u_parameters[0].x - v_position.x);\n"
-"    highp float y = u_parameters[0].y * 2.0;\n"
-"    highp float mult = smoothstep(y, y - 0.1, dis) * smoothstep(-0.1, 0.0, dis);\n"
-"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
-"}\n";
-
-static const QString kHorizLineFragmentShader =
-"varying highp vec4 v_color;\n"
-"varying highp vec2 v_position;\n"
-"uniform highp mat4 u_parameters;\n"
-"void main(void)\n"
-"{\n"
-"    highp float dis = abs(u_parameters[0].x - v_position.y);\n"
-"    highp float x = u_parameters[0].y * 2.0;\n"
-"    highp float mult = smoothstep(x, x - 0.1, dis) * smoothstep(-0.1, 0.0, dis);\n"
-"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
-"}\n";
 
 static inline int __glCheck__(const QString &loc, const char* fileName, int n)
 {
---
+++ mythtv/libs/libmythui/mythrender_opengl_shaders.h
@@ -0,0 +1,106 @@
+#ifndef MYTHRENDER_OPENGL_SHADERS_H
+#define MYTHRENDER_OPENGL_SHADERS_H
+
+static const QString kDefaultVertexShader =
+"attribute vec2 a_position;\n"
+"attribute vec4 a_color;\n"
+"attribute vec2 a_texcoord0;\n"
+"varying   vec4 v_color;\n"
+"varying   vec2 v_texcoord0;\n"
+"uniform   mat4 u_projection;\n"
+"uniform   mat4 u_transform;\n"
+"void main() {\n"
+"    gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);\n"
+"    v_texcoord0 = a_texcoord0;\n"
+"    v_color     = a_color;\n"
+"}\n";
+
+static const QString kDefaultFragmentShader =
+"uniform sampler2D s_texture0;\n"
+"varying highp vec4 v_color;\n"
+"varying highp vec2 v_texcoord0;\n"
+"void main(void)\n"
+"{\n"
+"    gl_FragColor = texture2D(s_texture0, v_texcoord0) * v_color;\n"
+"}\n";
+
+static const QString kSimpleVertexShader =
+"attribute vec2 a_position;\n"
+"attribute vec4 a_color;\n"
+"varying   vec4 v_color;\n"
+"uniform   mat4 u_projection;\n"
+"uniform   mat4 u_transform;\n"
+"void main() {\n"
+"    gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);\n"
+"    v_color     = a_color;\n"
+"}\n";
+
+static const QString kSimpleFragmentShader =
+"varying highp vec4 v_color;\n"
+"void main(void)\n"
+"{\n"
+"    gl_FragColor = v_color;\n"
+"}\n";
+
+static const QString kDrawVertexShader =
+"attribute vec2 a_position;\n"
+"attribute vec4 a_color;\n"
+"varying   vec4 v_color;\n"
+"varying   vec2 v_position;\n"
+"uniform   mat4 u_projection;\n"
+"uniform   mat4 u_transform;\n"
+"void main() {\n"
+"    gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);\n"
+"    v_color     = a_color;\n"
+"    v_position  = a_position;\n"
+"}\n";
+
+static const QString kCircleFragmentShader =
+"varying highp vec4 v_color;\n"
+"varying highp vec2 v_position;\n"
+"uniform highp mat4 u_parameters;\n"
+"void main(void)\n"
+"{\n"
+"    highp float dis = distance(v_position.xy, u_parameters[0].xy);\n"
+"    highp float mult = smoothstep(u_parameters[0].z, u_parameters[0].w, dis);\n"
+"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
+"}\n";
+
+static const QString kCircleEdgeFragmentShader =
+"varying highp vec4 v_color;\n"
+"varying highp vec2 v_position;\n"
+"uniform highp mat4 u_parameters;\n"
+"void main(void)\n"
+"{\n"
+"    highp float dis = distance(v_position.xy, u_parameters[0].xy);\n"
+"    highp float rad = u_parameters[0].z;\n"
+"    highp float wid = u_parameters[0].w;\n"
+"    highp float mult = smoothstep(rad + wid, rad + (wid - 1.0), dis) * smoothstep(rad - (wid + 1.0), rad - wid, dis);\n"
+"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
+"}\n";
+
+static const QString kVertLineFragmentShader =
+"varying highp vec4 v_color;\n"
+"varying highp vec2 v_position;\n"
+"uniform highp mat4 u_parameters;\n"
+"void main(void)\n"
+"{\n"
+"    highp float dis = abs(u_parameters[0].x - v_position.x);\n"
+"    highp float y = u_parameters[0].y * 2.0;\n"
+"    highp float mult = smoothstep(y, y - 0.1, dis) * smoothstep(-0.1, 0.0, dis);\n"
+"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
+"}\n";
+
+static const QString kHorizLineFragmentShader =
+"varying highp vec4 v_color;\n"
+"varying highp vec2 v_position;\n"
+"uniform highp mat4 u_parameters;\n"
+"void main(void)\n"
+"{\n"
+"    highp float dis = abs(u_parameters[0].x - v_position.y);\n"
+"    highp float x = u_parameters[0].y * 2.0;\n"
+"    highp float mult = smoothstep(x, x - 0.1, dis) * smoothstep(-0.1, 0.0, dis);\n"
+"    gl_FragColor = v_color * vec4(1.0, 1.0, 1.0, mult);\n"
+"}\n";
+
+#endif // MYTHRENDER_OPENGL_SHADERS_H
