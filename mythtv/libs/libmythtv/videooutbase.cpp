MythTV/mythtv

commit 733de04f6d1512c57661c78287b21d1acc8f6593

Use new Qt5.8 features of QRegion.
In Qt5.8 QRegion gained begin() and end() functions that allow it to
be used as an iterator.  Using a QRegion as an iterator can be used as
a replacement for the rects() function, which was obsoleted in Qt5.12.

mythtv/libs/libmythtv/videooutbase.cpp
@@ -1560,10 +1560,22 @@ bool VideoOutput::DisplayOSD(VideoFrame *frame, OSD *osd)
     // Split visible region for greater concurrency
     QRect r = osd_image->rect();
     QPoint c = r.center();
-    QVector<QRect> vis = visible.intersected(QRect(r.topLeft(),c)).rects();
+    QVector<QRect> vis;
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
+    vis += visible.intersected(QRect(r.topLeft(),c)).rects();
     vis += visible.intersected(QRect(c,r.bottomRight())).rects();
     vis += visible.intersected(QRect(r.bottomLeft(),c).normalized()).rects();
     vis += visible.intersected(QRect(c,r.topRight()).normalized()).rects();
+#else
+    for (const QRect &tmp : visible.intersected(QRect(r.topLeft(),c)))
+        vis += tmp;
+    for (const QRect &tmp : visible.intersected(QRect(c,r.bottomRight())))
+        vis += tmp;
+    for (const QRect &tmp : visible.intersected(QRect(r.bottomLeft(),c).normalized()))
+        vis += tmp;
+    for (const QRect &tmp : visible.intersected(QRect(c,r.topRight()).normalized()))
+        vis += tmp;
+#endif
     for (int i = 0; i < vis.size(); i++)
     {
         OsdRender *job = new OsdRender(frame, osd_image, video_dim, vis[i]);
@@ -1572,10 +1584,15 @@ bool VideoOutput::DisplayOSD(VideoFrame *frame, OSD *osd)
     }
     s_pool.waitForDone();
 #else
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
     QVector<QRect> vis = visible.rects();
     for (int i = 0; i < vis.size(); i++)
     {
         const QRect& r2 = vis[i];
+#else
+    for (const QRect& r2 : dirty)
+    {
+#endif
         int left   = min(r2.left(), osd_image->width());
         int top    = min(r2.top(), osd_image->height());
         int right  = min(left + r2.width(), osd_image->width());

mythtv/libs/libmythtv/mhi.cpp
@@ -699,11 +699,16 @@ void MHIContext::UpdateOSD(InteractiveScreen *osdWindow,
         // Replace this item with a set of cut-outs.
         it = m_display.erase(it);
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
         QVector<QRect> rects =
             (QRegion(imageRect) - QRegion(m_videoDisplayRect)).rects();
         for (uint j = 0; j < (uint)rects.size(); j++)
         {
             const QRect &rect = rects[j];
+#else
+        for (const QRect& rect : QRegion(imageRect)-QRegion(m_videoDisplayRect))
+        {
+#endif
             QImage image =
                 data->m_image.copy(rect.x()-data->m_x, rect.y()-data->m_y,
                                    rect.width(), rect.height());

mythtv/libs/libmythtv/osd.cpp
@@ -942,10 +942,15 @@ QRegion OSD::Draw(MythPainter* painter, QPaintDevice *device, QSize size,
 
     // assist yuv blending with some friendly alignments
     QRegion aligned;
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
     QVector<QRect> rects = visible.rects();
     for (int i = 0; i < rects.size(); i++)
     {
         const QRect& r = rects[i];
+#else
+    for (const QRect& r : visible)
+    {
+#endif
         int left  = r.left() & ~(alignx - 1);
         int top   = r.top()  & ~(aligny - 1);
         int right = (r.left() + r.width());

mythtv/libs/libmythtv/osdchromakey.cpp
@@ -284,10 +284,15 @@ bool ChromaKeyOSD::ProcessOSD(OSD *osd)
     QRect bot   = QRect(0, video_rect.top() + video_rect.height(), osd_rect.width(),
                         osd_rect.height() - video_rect.top() - video_rect.height());
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
     QVector<QRect> update = dirty.rects();
     for (int i = 0; i < update.size(); i++)
     {
         const QRect& r = update[i];
+#else
+    for (const QRect& r : dirty)
+    {
+#endif
         BlendOrCopy(letterbox, r.intersected(top));
         BlendOrCopy(letterbox, r.intersected(left));
         BlendOrCopy(colorkey,  r.intersected(video_rect));

mythtv/libs/libmythui/mythmainwindow.cpp
@@ -750,6 +750,7 @@ void MythMainWindow::drawScreen(void)
             {
                 if ((*screenit)->NeedsRedraw())
                 {
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
                     QRegion topDirty = (*screenit)->GetDirtyArea();
                     QVector<QRect> wrects = topDirty.rects();
                     for (int i = 0; i < wrects.size(); i++)
@@ -758,6 +759,9 @@ void MythMainWindow::drawScreen(void)
                         QVector<QRect> drects = d->repaintRegion.rects();
                         for (int j = 0; j < drects.size(); j++)
                         {
+                            // Can't use QRegion::contains because it only
+                            // checks for overlap.  QRect::contains checks
+                            // if fully contained.
                             if (drects[j].contains(wrects[i]))
                             {
                                 foundThisRect = true;
@@ -768,6 +772,26 @@ void MythMainWindow::drawScreen(void)
                         if (!foundThisRect)
                             return;
                     }
+#else
+                    for (const QRect& wrect: (*screenit)->GetDirtyArea())
+                    {
+                        bool foundThisRect = false;
+                        for (const QRect& drect: d->repaintRegion)
+                        {
+                            // Can't use QRegion::contains because it only
+                            // checks for overlap.  QRect::contains checks
+                            // if fully contained.
+                            if (drect.contains(wrect))
+                            {
+                                foundThisRect = true;
+                                break;
+                            }
+                        }
+
+                        if (!foundThisRect)
+                            return;
+                    }
+#endif
                 }
             }
         }
@@ -792,10 +816,15 @@ void MythMainWindow::draw(MythPainter *painter /* = 0 */)
     if (!painter->SupportsClipping())
         d->repaintRegion = QRegion(d->uiScreenRect);
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
     QVector<QRect> rects = d->repaintRegion.rects();
     for (int i = 0; i < rects.size(); i++)
     {
         const QRect& r = rects[i];
+#else
+    for (const QRect& r : d->repaintRegion)
+    {
+#endif
         if (r.width() == 0 || r.height() == 0)
             continue;
 

mythtv/libs/libmythui/mythpainter_qimage.cpp
@@ -124,10 +124,15 @@ void MythQImagePainter::Clear(QPaintDevice *device, const QRegion &region)
     int img_width  = dev->size().width();
     int img_height = dev->size().height();
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
     QVector<QRect> rects = region.rects();
     for (int i = 0; i < rects.size(); i++)
     {
         const QRect& r = rects[i];
+#else
+    for (const QRect& r : region)
+    {
+#endif
         if (r.top() > img_height || r.left() > img_width)
             continue;
 

commit 144ac28c0082eeda0b07e9d537a12b7080f5b652

Playback: Fix bug where OSD is not displayed when using xvideo renderer
Bug was introduced in 733de04f6d15 - code should use visible area
not dirty area

mythtv/libs/libmythtv/videooutbase.cpp
@@ -1589,7 +1589,7 @@ bool VideoOutput::DisplayOSD(VideoFrame *frame, OSD *osd)
     {
         const QRect& r2 = vis[i];
 #else
-    for (const QRect& r2 : dirty)
+    for (const QRect& r2 : visible)
     {
 #endif
         int left   = min(r2.left(), osd_image->width());