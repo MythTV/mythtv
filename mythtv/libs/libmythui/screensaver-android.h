0823653b4390d7b01d36bf8a88851df696ee34b3
David Hampton <mythtv@love2code.net>
2019-02-26 21:17:38 -0800

    cppcheck: Clean up "not marked with a 'override' specifier" messages.

--- mythtv/libs/libmythui/screensaver-android.h
+++ mythtv/libs/libmythui/screensaver-android.h
@@ -13,11 +13,11 @@ class ScreenSaverAndroid : public QObject, public ScreenSaver
     ScreenSaverAndroid();
     ~ScreenSaverAndroid();
 
-    void Disable(void);
-    void Restore(void);
-    void Reset(void);
+    void Disable(void) override; // ScreenSaver
+    void Restore(void) override; // ScreenSaver
+    void Reset(void) override; // ScreenSaver
 
-    bool Asleep(void);
+    bool Asleep(void) override; // ScreenSaver
 
 };
 
