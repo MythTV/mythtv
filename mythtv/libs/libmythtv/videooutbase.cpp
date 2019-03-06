MythTV/mythtv

commit b2c82a36656a277b6fedbd304af9dd6be139ac2a

Convert from QDesktopWindow to QScreen.
The QDesktopWindow widget is deprecated in Qt5.11.  Since the QScreen
code has been available since Qt5.0, unconditionally convert to using
the new widgets.  One changes made in this conversion is to the screen
setting.  Instead of using a numerical screen number, use the screen
names which are easily available.

mythtv/libs/libmythtv/videooutbase.cpp
@@ -1,7 +1,6 @@
 #include <cmath>
 #include <cstdlib>
 
-#include <QDesktopWidget>
 #include <QRunnable>
 
 #include "osd.h"

mythtv/libs/libmythtv/videooutwindow.cpp
@@ -22,7 +22,6 @@
 
 #include <cmath>
 
-#include <QDesktopWidget>
 #include <QApplication>
 
 #include "mythconfig.h"
@@ -58,7 +57,7 @@ VideoOutWindow::VideoOutWindow() :
     db_pip_size(26),
     db_scaling_allowed(true),
 
-    using_xinerama(false), screen_num(0), screen_geom(0, 0, 1024, 768),
+    using_xinerama(false), screen_geom(0, 0, 1024, 768),
 
     // Manual Zoom
     mz_scale_v(1.0f), mz_scale_h(1.0f), mz_move(0, 0),
@@ -100,25 +99,23 @@ VideoOutWindow::VideoOutWindow() :
 void VideoOutWindow::populateGeometry(void)
 {
     qApp->processEvents();
-    QDesktopWidget *desktop = nullptr;
-    if (qobject_cast<QApplication*>(qApp))
-        desktop = QApplication::desktop();
+    if (not qobject_cast<QApplication*>(qApp))
+        return;
 
-    if (desktop)
+    QScreen *screen = MythDisplay::GetScreen();
+    if (MythDisplay::SpanAllScreens())
     {
-        screen_num = desktop->primaryScreen();
-        using_xinerama  = MythDisplay::GetNumberXineramaScreens() > 1;
-        if (using_xinerama)
-        {
-            screen_num = gCoreContext->GetNumSetting("XineramaScreen", screen_num);
-            if (screen_num >= desktop->screenCount())
-                screen_num = 0;
-        }
-
-        screen_geom = desktop->geometry();
-        if (screen_num >= 0)
-            screen_geom = desktop->screenGeometry(screen_num);
+        using_xinerama = true;
+        screen_geom = screen->virtualGeometry();
+        LOG(VB_PLAYBACK, LOG_INFO, QString("Window using all screens %2x%3")
+            .arg(screen_geom.width()).arg(screen_geom.height()));
+        return;
     }
+
+    screen_geom = screen->geometry();
+    LOG(VB_PLAYBACK, LOG_INFO, QString("Window using screen %2 %3x%4")
+        .arg(screen->name())
+        .arg(screen_geom.width()).arg(screen_geom.height()));
 }
 
 /**

mythtv/libs/libmythtv/videooutwindow.h
@@ -133,7 +133,6 @@ class VideoOutWindow
     bool    db_use_gui_size;  ///< Use the gui size for video window
 
     bool    using_xinerama;   ///< Display is using multiple screens
-    int     screen_num;       ///< Screen that contains playback window
     QRect   screen_geom;      ///< Full screen geometry
 
     // Manual Zoom

mythtv/libs/libmythui/mythdisplay.cpp
@@ -1,9 +1,9 @@
 #include "compat.h"
+#include "mythcorecontext.h"
 #include "mythdisplay.h"
 #include "mythmainwindow.h"
 
 #include <QApplication>
-#include <QDesktopWidget>
 
 #if defined(Q_OS_MAC)
 #import "util-osx.h"
@@ -148,12 +148,83 @@ DisplayInfo MythDisplay::GetDisplayInfo(int video_rate)
     return ret;
 }
 
-int MythDisplay::GetNumberXineramaScreens(void)
+/**
+ * \brief Return the number of available screens.
+ *
+ * This is the number of screens configured to be part of the users
+ * desktop, at the time the call is made.  If the user closes the
+ * laptop lid, or connects an external screen, this number may be
+ * different the next time this function is called.
+ */
+int MythDisplay::GetNumberOfScreens(void)
 {
-    int nr_xinerama_screens = 0;
-    QDesktopWidget *m_desktop = QApplication::desktop();
-    if (m_desktop) {
-        nr_xinerama_screens = m_desktop->numScreens();
+    return qGuiApp->screens().size();
+}
+
+/**
+ * \brief Return true if the MythTV windows should span all screens.
+ */
+bool MythDisplay::SpanAllScreens(void)
+{
+    return gCoreContext->GetSetting("XineramaScreen", nullptr) == "-1";
+}
+
+/**
+ * \brief Return a pointer to the screen to use.
+ *
+ * This function looks at the users screen preference, and will return
+ * that screen if possible.  If not, i.e. the screen isn't plugged in,
+ * then this function returns the system's primary screen.
+ *
+ * Note: There is no special case here for the case of MythTV spanning
+ * all screens, as all screen have access to the virtual desktop
+ * attributes.  The check for spanning screens must be made when the
+ * screen size/geometry accessed, and the proper physical/virtual
+ * size/geometry retrieved.
+ */
+QScreen* MythDisplay::GetScreen(void)
+{
+    // Lookup by name
+    QString name = gCoreContext->GetSetting("XineramaScreen", nullptr);
+    foreach (QScreen *qscreen, qGuiApp->screens()) {
+        if (name == qscreen->name()) {
+            LOG(VB_GUI, LOG_INFO, LOC +
+                QString("found screen %1").arg(name));
+            return qscreen;
+        }
     }
-    return nr_xinerama_screens;
+
+    // No name match.  These were previously numbers.
+    bool ok;
+    int screen_num = name.toInt(&ok);
+    QList<QScreen *>screens = qGuiApp->screens();
+    if (ok && (screen_num >= 0) && (screen_num < screens.size())) {
+        LOG(VB_GUI, LOG_INFO, LOC +
+            QString("found screen number %1 (%2)")
+            .arg(name).arg(screens[screen_num]->name()));
+        return screens[screen_num];
+    }
+
+    // For aything else, return the primary screen.
+    QScreen *primary = qGuiApp->primaryScreen();
+    if (name != "-1")
+        LOG(VB_GUI, LOG_INFO,
+            QString("screen %1 not found, defaulting to primary screen (%2)")
+            .arg(name).arg(primary->name()));
+    return primary;
+}
+
+QString MythDisplay::GetExtraScreenInfo(QScreen *qscreen)
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
+    QString mfg = qscreen->manufacturer();
+    if (mfg.isEmpty())
+        mfg = "unknown";
+    QString model = qscreen->model();
+    if (model.isEmpty())
+        model = "unknown";
+    return QString("(%1 %2)").arg(mfg).arg(model);
+#else
+    return QString();
+#endif
 }

mythtv/libs/libmythui/mythdisplay.h
@@ -5,6 +5,7 @@
 
 #include <QWidget> // for WId
 #include <QSize>
+#include <QScreen>
 
 #include "mythuiexp.h"
 
@@ -25,7 +26,10 @@ class MUI_PUBLIC MythDisplay
   public:
     static DisplayInfo GetDisplayInfo(int video_rate = 0);
     static WId GetWindowID(void);
-    static int GetNumberXineramaScreens(void);
+    static int GetNumberOfScreens(void);
+    static QScreen* GetScreen(void);
+    static bool SpanAllScreens(void);
+    static QString GetExtraScreenInfo(QScreen *qscreen);
 };
 
 #endif // MYTHDISPLAY_H

mythtv/libs/libmythui/mythmainwindow.cpp
@@ -17,7 +17,6 @@ using namespace std;
 #include <QWaitCondition>
 #include <QApplication>
 #include <QTimer>
-#include <QDesktopWidget>
 #include <QHash>
 #include <QFile>
 #include <QDir>
@@ -25,6 +24,7 @@ using namespace std;
 #include <QKeyEvent>
 #include <QKeySequence>
 #include <QSize>
+#include <QWindow>
 
 // Platform headers
 #include "unistd.h"
@@ -58,6 +58,7 @@ using namespace std;
 #include "mythuiactions.h"
 #include "mythrect.h"
 #include "mythuidefines.h"
+#include "mythdisplay.h"
 
 #ifdef USING_APPLEREMOTE
 #include "AppleRemoteListener.h"
@@ -880,9 +881,14 @@ void MythMainWindow::GrabWindow(QImage &image)
     if (active)
         winid = active->winId();
     else
-        winid = QApplication::desktop()->winId();
+        // According to the following we page, you "just pass 0 as the
+        // window id, indicating that we want to grab the entire screen".
+        //
+        // https://doc.qt.io/qt-5/qtwidgets-desktop-screenshot-example.html#screenshot-class-implementation
+        winid = 0;
 
-    QPixmap p = QPixmap::grabWindow(winid);
+    QScreen *screen = MythDisplay::GetScreen();
+    QPixmap p = screen->grabWindow(winid);
     image = p.toImage();
 }
 
@@ -1044,6 +1050,10 @@ void MythMainWindow::Init(QString forcedpainter, bool mayReInit)
     }
 
     setWindowFlags(flags);
+    if (this->windowHandle()) {
+        LOG(VB_GENERAL, LOG_INFO, QString("Have window handle, setting screen"));
+        this->windowHandle()->setScreen(MythDisplay::GetScreen());
+    }
     QTimer::singleShot(1000, this, SLOT(DelayedAction()));
 
     d->screenRect = QRect(d->xbase, d->ybase, d->screenwidth, d->screenheight);

mythtv/libs/libmythui/mythuihelper.cpp
@@ -12,13 +12,13 @@
 #include <QFileInfo>
 #include <QApplication>
 #include <QPainter>
-#include <QDesktopWidget>
 #include <QStyleFactory>
 #include <QSize>
 #include <QFile>
 #include <QAtomicInt>
 #include <QEventLoop>
 #include <QTimer>
+#include <QScreen>
 
 // mythbase headers
 #include "mythdirs.h"
@@ -228,91 +228,64 @@ void MythUIHelperPrivate::Init(void)
 /**
  * \brief Get screen size from Qt, respecting for user's multiple screen prefs
  *
- * If the windowing system environment has multiple screens
- * (%e.g. Xinerama or Mac OS X), QApplication::desktop()->%width() will span
- * all of them, so we usually need to get the geometry of a specific screen.
+ * If the windowing system environment has multiple screens, then use
+ * QScreen::virtualSize() to get the size of the virtual desktop.
+ * Otherwise QScreen::size() or QScreen::availableSize() will provide
+ * the size of an individual screen.
  */
 void MythUIHelperPrivate::GetScreenBounds()
 {
-    QDesktopWidget *desktop = QApplication::desktop();
-    bool             hasXinerama = MythDisplay::GetNumberXineramaScreens() > 1;
-    int              numScreens  = desktop->numScreens();
-    int              screen;
-
-    if (hasXinerama)
+    foreach (QScreen *screen, qGuiApp->screens())
     {
-        LOG(VB_GUI, LOG_INFO, LOC +
-            QString("Total desktop dim: %1x%2, over %3 screen[s].")
-            .arg(desktop->width()).arg(desktop->height()).arg(numScreens));
+        QRect dim = screen->geometry();
+        QString extra = MythDisplay::GetExtraScreenInfo(screen);
+        LOG(VB_GUI, LOG_INFO, LOC + QString("Screen %1 dim: %2x%3 %4")
+            .arg(screen->name())
+            .arg(dim.width()).arg(dim.height())
+            .arg(extra));
     }
 
-    if (numScreens > 1)
+    QScreen *primary = qGuiApp->primaryScreen();
+    LOG(VB_GUI, LOG_INFO, LOC +
+        QString("Primary screen: %1.").arg(primary->name()));
+
+    int numScreens = MythDisplay::GetNumberOfScreens();
+    QSize dim = primary->virtualSize();
+    LOG(VB_GUI, LOG_INFO, LOC +
+        QString("Total desktop dim: %1x%2, over %3 screen[s].")
+        .arg(dim.width()).arg(dim.height()).arg(numScreens));
+
+    if (MythDisplay::SpanAllScreens())
     {
-        for (screen = 0; screen < numScreens; ++screen)
-        {
-            QRect dim = desktop->screenGeometry(screen);
-            LOG(VB_GUI, LOG_INFO, LOC + QString("Screen %1 dim: %2x%3.")
-                .arg(screen).arg(dim.width()).arg(dim.height()));
-        }
-    }
-
-    screen = desktop->primaryScreen();
-    LOG(VB_GUI, LOG_INFO, LOC + QString("Primary screen: %1.").arg(screen));
-
-    if (hasXinerama)
-        screen = GetMythDB()->GetNumSetting("XineramaScreen", screen);
-
-    if (screen == -1)       // Special case - span all screens
-    {
+        LOG(VB_GUI, LOG_INFO, LOC + QString("Using entire desktop."));
         m_xbase  = 0;
         m_ybase  = 0;
-        m_width  = desktop->width();
-        m_height = desktop->height();
-
-        LOG(VB_GUI, LOG_INFO, LOC +
-            QString("Using all %1 screens. ").arg(numScreens) +
-            QString("Dimensions: %1x%2").arg(m_width).arg(m_height));
-
+        m_width  = dim.width();
+        m_height = dim.height();
         return;
     }
 
-    if (hasXinerama)        // User specified a single screen
+    QRect bounds;
+    QScreen *screen = MythDisplay::GetScreen();
+    if (GetMythDB()->GetBoolSetting("RunFrontendInWindow", false))
     {
-        if (screen < 0 || screen >= numScreens)
-        {
-            LOG(VB_GENERAL, LOG_WARNING, LOC +
-                QString("Xinerama screen %1 was specified,"
-                        " but only %2 available, so using screen 0.")
-                .arg(screen).arg(numScreens));
-            screen = 0;
-        }
+        LOG(VB_GUI, LOG_INFO, LOC + "Running in a window");
+        // This doesn't include the area occupied by the
+        // Windows taskbar, or the Mac OS X menu bar and Dock
+        bounds = screen->availableGeometry();
     }
-
-
+    else
     {
-        QRect bounds;
-
-        bool inWindow = GetMythDB()->GetBoolSetting("RunFrontendInWindow", false);
-
-        if (inWindow)
-            LOG(VB_GUI, LOG_INFO, LOC + "Running in a window");
-
-        if (inWindow)
-            // This doesn't include the area occupied by the
-            // Windows taskbar, or the Mac OS X menu bar and Dock
-            bounds = desktop->availableGeometry(screen);
-        else
-            bounds = desktop->screenGeometry(screen);
-
-        m_xbase  = bounds.x();
-        m_ybase  = bounds.y();
-        m_width  = bounds.width();
-        m_height = bounds.height();
-
-        LOG(VB_GUI, LOG_INFO, LOC + QString("Using screen %1, %2x%3 at %4,%5")
-            .arg(screen).arg(m_width).arg(m_height)
-            .arg(m_xbase).arg(m_ybase));
+        bounds = screen->geometry();
     }
+    m_xbase  = bounds.x();
+    m_ybase  = bounds.y();
+    m_width  = bounds.width();
+    m_height = bounds.height();
+
+    LOG(VB_GUI, LOG_INFO, LOC + QString("Using screen %1, %2x%3 at %4,%5")
+        .arg(screen->name()).arg(m_width).arg(m_height)
+        .arg(m_xbase).arg(m_ybase));
 }
 
 /**
@@ -415,14 +388,14 @@ void MythUIHelperPrivate::WaitForScreenChange(void) const
     // Wait for screen signal change, so we later get updated screen resolution
     QEventLoop loop;
     QTimer timer;
-    QDesktopWidget *desktop = QApplication::desktop();
+    QScreen *screen = MythDisplay::GetScreen();
 
     timer.setSingleShot(true);
     QObject::connect(&timer, SIGNAL(timeout()),
                      &loop, SLOT(quit()));
-    QObject::connect(desktop, SIGNAL(resized(int)),
+    QObject::connect(screen, SIGNAL(geometryChanged(const QRect&)),
                      &loop, SLOT(quit()));
-    QObject::connect(desktop, SIGNAL(workAreaResized(int)),
+    QObject::connect(screen, SIGNAL(availableGeometryChanged(const QRect&)),
                      &loop, SLOT(quit()));
     timer.start(300); //300ms maximum wait
     loop.exec();

mythtv/programs/mythfrontend/globalsettings.cpp
@@ -8,7 +8,7 @@
 #include <sys/stat.h>
 
 // Qt headers
-#include <QCoreApplication>
+#include <QApplication>
 #include <QEvent>
 #include <QFileInfo>
 #include <QFile>
@@ -2000,10 +2000,12 @@ static HostTextEditSetting *SetupPinCode()
 static HostComboBoxSetting *XineramaScreen()
 {
     HostComboBoxSetting *gc = new HostComboBoxSetting("XineramaScreen", false);
-    int num = MythDisplay::GetNumberXineramaScreens();
 
-    for (int i=0; i<num; ++i)
-        gc->addSelection(QString::number(i), QString::number(i));
+    foreach (QScreen *qscreen, qGuiApp->screens())
+    {
+        QString extra = MythDisplay::GetExtraScreenInfo(qscreen);
+        gc->addSelection(qscreen->name() + extra, qscreen->name());
+    }
 
     gc->addSelection(AppearanceSettings::tr("All"), QString::number(-1));
 
@@ -4526,7 +4528,7 @@ AppearanceSettings::AppearanceSettings()
 #endif
     screen->addChild(MenuTheme());
 
-    if (MythDisplay::GetNumberXineramaScreens() > 1)
+    if (MythDisplay::GetNumberOfScreens() > 1)
     {
         screen->addChild(XineramaScreen());
         screen->addChild(XineramaMonitorAspectRatio());

commit 5de99ad3d7172dc496d63eea579fef8b33a1080f

Video aspect ratio wrong on systems with multiple monitors
On a system with two monitors, positioned horizontally next to each other,
the vertical size of the video in a window is correct but the horizontal size
is about half what it should be.
The same is true for full-screen video, without an enclosing window.

This problem is most likely introduced in commit b2c82a3, dated Feb 1,
"Convert from QDesktopWindow to QScreen".

Before this commit a system with multiple monitors was treaded as "Xinerama".
After this commit a system with mulitple monitors is seen as one big screen
with an area which is the bounding box of the screens of all monitors.
The name display as used in the code now refers to the big "bounding box" screen.

The video aspect ratio computations use the size of the display in mm.
The bug was here that the display size in mm was used in conjunction
with the size in pixels of the first screen.
In order to scale correct the size in pixels of the display has to be used.

Note: the change from QDesktopWidget to QScreen is a Good Thing because
the QDesktopWidget is declared obsolete.
Note: with this fix included the video scaling in a multiple monitor
system is now exact which is better than before, in the "Xinerama" mode.

Fixes #13400

mythtv/libs/libmythtv/videooutbase.cpp
@@ -1912,7 +1912,7 @@ void VideoOutput::InitDisplayMeasurements(uint width, uint height, bool resize)
         ResizeForVideo(width, height);
 
     // Determine window and screen dimensions in pixels
-    QSize screen_size = window.GetScreenGeometry().size();
+    QSize screen_size = disp.m_res;
     QSize window_size = window.GetDisplayVisibleRect().size();
 
     float pixel_aspect = (float)screen_size.width() /