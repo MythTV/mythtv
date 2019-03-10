62ae08961cd949506a1b8d6a6b4a8bb58bdf3fd2
Klaas de Waal <kdewaal@mythtv.org>
2019-03-10 16:49:43 +0100

    Update guide title/subtitle/description with new DVB EIT data
    The title/subtitle/description in the guide is now updated
    with the latest information received from DVB EIT. The title
    is now updated with the newly received data unless the new
    title is an empty string.
    Previously, the title was only updated if the new title was
    longer than the old title. This caused missed recordings when
    recording rules are used that refer to the program title.
    The same for the subtitle and the description.
    
    Fixes #13057

--- mythtv/libs/libmythtv/programdata.cpp
+++ mythtv/libs/libmythtv/programdata.cpp
@@ -625,19 +625,19 @@ uint DBEvent::UpdateDB(
     QString  linetref   = inetref;
     QDate loriginalairdate = originalairdate;
 
-    if (match.title.length() >= ltitle.length())
+    if (ltitle.isEmpty() && !match.title.isEmpty())
         ltitle = match.title;
 
-    if (match.subtitle.length() >= lsubtitle.length())
+    if (lsubtitle.isEmpty() && !match.subtitle.isEmpty())
         lsubtitle = match.subtitle;
 
-    if (match.description.length() >= ldesc.length())
+    if (ldesc.isEmpty() && !match.description.isEmpty())
         ldesc = match.description;
 
     if (lcategory.isEmpty() && !match.category.isEmpty())
         lcategory = match.category;
 
-    if (!lairdate && !match.airdate)
+    if (!lairdate && match.airdate)
         lairdate = match.airdate;
 
     if (!loriginalairdate.isValid() && match.originalairdate.isValid())
