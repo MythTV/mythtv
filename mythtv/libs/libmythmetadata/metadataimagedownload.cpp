3b7ab7b47e8b753672e556016b1f53f92ac6b740
David Hampton <mythtv@love2code.net>
2019-03-24 11:51:48 -0400

    tidy: Fix 'value stored is never read' in MetadataImageDownload::run.
    The clang-tidy "dead stores" check pointed out that this function
    resets the 'errored' variable to false at the very end of a large
    while loop, and then initializes it to false at the top of the next
    iteration of the loop.  Remove the unnecessary assignments.

--- mythtv/libs/libmythmetadata/metadataimagedownload.cpp
+++ mythtv/libs/libmythmetadata/metadataimagedownload.cpp
@@ -312,7 +312,6 @@ void MetadataImageDownload::run()
         {
             QCoreApplication::postEvent(m_parent,
                     new ImageDLFailureEvent(lookup));
-            errored = false;
         }
         lookup->SetDownloads(downloaded);
         QCoreApplication::postEvent(m_parent, new ImageDLEvent(lookup));
