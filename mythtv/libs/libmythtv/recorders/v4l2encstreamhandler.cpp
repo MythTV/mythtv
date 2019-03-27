56b22da302d24271e8dbfe99750b857295002a4a
David Hampton <mythtv@love2code.net>
2019-03-27 19:29:49 -0400

    Use correct error variable in the V4L2encStreamHandler class.
    This structure inherits error variables from two different base
    structure.  Fix three places where commit 266f1eb11a (which changed
    member names to start with m_) used the wrong error variable, since
    one of the two conflicting names in the base classes had to be
    changes.

--- mythtv/libs/libmythtv/recorders/v4l2encstreamhandler.cpp
+++ mythtv/libs/libmythtv/recorders/v4l2encstreamhandler.cpp
@@ -150,7 +150,7 @@ V4L2encStreamHandler::V4L2encStreamHandler(const QString & device,
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + QString("-- Failed to open %1: ")
             .arg(m_device) + ENO);
-        m_error = true;
+        m_bError = true;
         return;
     }
     LOG(VB_RECORD, LOG_INFO, LOC + QString("'%1' open").arg(m_device));
@@ -177,7 +177,7 @@ void V4L2encStreamHandler::run(void)
             LOG(VB_GENERAL, LOG_ERR, LOC +
                 QString("run() -- Failed to open %1: ")
                 .arg(m_device) + ENO);
-            m_error = true;
+            m_bError = true;
             return;
         }
     }
@@ -238,7 +238,7 @@ void V4L2encStreamHandler::run(void)
         else if (m_drb->IsEOF())
         {
             LOG(VB_GENERAL, LOG_ERR, LOC + "run() -- Device EOF detected");
-            m_error = true;
+            m_bError = true;
         }
         else
         {
