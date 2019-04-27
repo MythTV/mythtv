8705643065cc68307cb5f146d2e3c54aa0b41e68
Klaas de Waal <kdewaal@mythtv.org>
2019-04-27 23:11:29 +0200

    Label value between quotes in mythtv-setup debug log output
    The debug messages from standardSetting::haveChanged do now have
    the label value between quotes for improved clarity.
    Example: change this
    2019-04-27 21:25:06.872850 D  Setting Use DVB card for active EIT scan changed to 0
    to
    2019-04-27 21:25:06.872850 D  Setting 'Use DVB card for active EIT scan' changed to 0

--- mythtv/libs/libmyth/standardsettings.cpp
+++ mythtv/libs/libmyth/standardsettings.cpp
@@ -182,7 +182,7 @@ bool StandardSetting::haveChanged()
     if (m_haveChanged)
     {
         LOG(VB_GENERAL, LOG_DEBUG,
-            QString("Setting %1 changed to %2").arg(getLabel())
+            QString("Setting '%1' changed to %2").arg(getLabel())
             .arg(getValue()));
         return true;
     }
