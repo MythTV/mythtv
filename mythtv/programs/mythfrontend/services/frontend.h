ed9e3829e7c799ed5c7a6659f8337d2a0df9f329
Justin Randall <2840408+JRRandall@users.noreply.github.com>
2019-05-07 13:05:20 -0400

    Resolving conflicts from previous pull request.

--- mythtv/programs/mythfrontend/services/frontend.h
+++ mythtv/programs/mythfrontend/services/frontend.h
@@ -15,9 +15,9 @@ class Frontend : public FrontendServices
     Q_INVOKABLE explicit Frontend(QObject *parent = nullptr) : FrontendServices(parent) { }
 
   public:
-    DTC::FrontendStatus* GetStatus(void);
+    DTC::FrontendStatus* GetStatus(void) override; // FrontendServices
     bool                 SendMessage(const QString &Message,
-                                     uint Timeout);
+                                     uint Timeout) override; // FrontendServices
     bool                 SendNotification(bool  Error,
                                           const QString &Type,
                                           const QString &Message,
@@ -29,22 +29,22 @@ class Frontend : public FrontendServices
                                           float Progress,
                                           int   Timeout,
                                           bool  Fullscreen,
-                                          uint  Visibility, uint Priority);
+                                          uint  Visibility, uint Priority) override; // FrontendServices
 
     bool                 SendAction(const QString &Action,
                                     const QString &Value,
-                                    uint Width, uint Height);
+                                    uint Width, uint Height) override; // FrontendServices
     virtual bool         PlayRecording(int RecordedId, int ChanId,
-                                       const QDateTime &StartTime);
-    bool                 PlayLiveTV(int ChanId);
-    bool                 PlayVideo(const QString &Id, bool UseBookmark);
-    QStringList          GetContextList(void);
-    DTC::FrontendActionList* GetActionList(const QString &Context);
+                                       const QDateTime &StartTime) override; // FrontendServices
+    bool                 PlayLiveTV(int ChanId) override; // FrontendServices
+    bool                 PlayVideo(const QString &Id, bool UseBookmark) override; // FrontendServices
+    QStringList          GetContextList(void) override; // FrontendServices
+    DTC::FrontendActionList* GetActionList(const QString &Context) override; // FrontendServices
 
 
     static bool          IsValidAction(const QString &action);
     static void          InitialiseActions(void);
-    bool                 SendKey(const QString &Key);
+    bool                 SendKey(const QString &Key) override; // FrontendServices
 
   protected:
     static QStringList gActionList;
