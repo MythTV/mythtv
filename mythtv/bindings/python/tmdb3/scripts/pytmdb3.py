MythTV/mythtv

commit a90e2db32896ba1ca0851144006ba01a3fd6ae56

Add compatibility to python3 to tmdb3 module.
How to check and reproduce:

Check out mythtv from git.

Set the pythonpath to the folder holding the bindings "MythTV":
Example:
export PYTHONPATH=$HOME/dnalor_mythtv/mythtv/mythtv/bindings/python

Create a symlink for tmdb3
$ cd MythTV
$ ln -s ../tmdb3/tmdb3 tmdb3

Copy the folder
mythtv/mythtv/programs/scripts/metadata/Movie
to
$PYTHONPATH/share/mythtv/metadata/Movie

Note: This is noa absolutely necessary, but it is needed for testing
the 'tmdb3' api together with the 'MythTV' bindings.

Clear the tmdb3 cache:
Create a script:
CF=$HOME/.mythtv/cache/pytmdb3.cache
rm $CF
touch $CF

Delete all '*.pyc' files:
find . -name "*.pyc" -type f -delete

Switch to python3:

Make python3 the default when called via `python` or `#!/usr/bin/env python`:
Put a symlink from python3 to python on top of the `PATH` environment:
As root, type
 mkdir -p /opt/python3
 ln -s `which python3` /opt/python3/python

Then in the terminal add the path as first entry:
$ export PATH=/opt/python3/:$PATH

test with
$ env python --version

Run the following tests from the path $PYTHONPATH :

$ python ./share/mythtv/metadata/Movie/tmdb3.py --version
$ python ./share/mythtv/metadata/Movie/tmdb3.py --test
$ python ./share/mythtv/metadata/Movie/tmdb3.py -M -l en 'Indiana Jones'
$ python ./share/mythtv/metadata/Movie/tmdb3.py -M -l de 'Indiana Jones'
$ python ./share/mythtv/metadata/Movie/tmdb3.py -D -l de 217
$ python ./share/mythtv/metadata/Movie/tmdb3.py -l en -C 10
$ python ./share/mythtv/metadata/Movie/tmdb3.py -l en -a US -C 10
$ python ./share/mythtv/metadata/Movie/tmdb3.py -l en -a de -C 10
$ python ./share/mythtv/metadata/Movie/tmdb3.py -l en -a de -C 10 --debug

mythtv/bindings/python/tmdb3/scripts/pytmdb3.py
@@ -23,10 +23,10 @@ if __name__ == '__main__':
 
     if opts.version:
         from tmdb3.tmdb_api import __title__, __purpose__, __version__, __author__
-        print __title__
-        print ""
-        print __purpose__
-        print "Version: "+__version__
+        print (__title__)
+        print ("")
+        print (__purpose)__
+        print ("Version: "+__version__)
         sys.exit(0)
 
     if opts.nocache:

mythtv/programs/scripts/metadata/Movie/tmdb3.py
@@ -26,10 +26,19 @@ __version__ = "0.3.7"
 #       resolved upstream.
 # 0.3.7 Add handling for TMDB site returning insufficient results from a
 #       query
+# 0.3.7.a : Added compatibiliy to python3, tested with python 3.6 and 2.7
 
 from optparse import OptionParser
 import sys
 import signal
+
+def print_etree(etostr):
+    """lxml.etree.tostring is a bytes object in python3, and a str in python2.
+    """
+    if sys.version_info[0] == 3:
+        sys.stdout.write(etostr.decode())
+    else:
+        sys.stdout.write(etostr)
 
 def timeouthandler(signal, frame):
     raise RuntimeError("Timed out")
@@ -58,14 +67,14 @@ def buildSingle(inetref, opts):
             if getattr(movie, j):
                 setattr(m, i, getattr(movie, j))
         except TMDBRequestInvalid:
-            sys.stdout.write(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
+            print_etree(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
                                             xml_declaration=True))
             sys.exit()
 
     if movie.title:
         m.title = movie.title
 
-    releases = movie.releases.items()
+    releases = list(movie.releases.items())
 
 # get the release date for the wanted country
 # TODO if that is not part of the reply use the primary release date (Primary=true)
@@ -73,14 +82,14 @@ def buildSingle(inetref, opts):
 # if there is not a single release date in the reply, then leave it empty
     if len(releases) > 0:
         if opts.country:
-            # resort releases with selected country at top to ensure it 
-            # is selected by the metadata libraries 
-            r = zip(*releases) 
-            if opts.country in r[0]: 
-                index = r[0].index(opts.country) 
-                releases.insert(0, releases.pop(index)) 
+            # resort releases with selected country at top to ensure it
+            # is selected by the metadata libraries
+            r = list(zip(*releases))
+            if opts.country in r[0]:
+                index = r[0].index(opts.country)
+                releases.insert(0, releases.pop(index))
 
-        m.releasedate = releases[0][1].releasedate 
+        m.releasedate = releases[0][1].releasedate
 
     m.inetref = str(movie.id)
     if movie.collection:
@@ -117,7 +126,7 @@ def buildSingle(inetref, opts):
                         'height':str(poster.height),
                         'width':str(poster.width)})
     tree.append(m.toXML())
-    sys.stdout.write(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
+    print_etree(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
                                     xml_declaration=True))
     sys.exit()
 
@@ -182,7 +191,7 @@ def buildList(query, opts):
             # page limiter, dont want to overload the server
             break
 
-    sys.stdout.write(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
+    print_etree(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
                                     xml_declaration=True))
     sys.exit(0)
 
@@ -198,7 +207,7 @@ def buildCollection(inetref, opts):
     try:
         m.title = collection.name
     except TMDBRequestInvalid:
-        sys.stdout.write(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
+        print_etree(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
                                         xml_declaration=True))
         sys.exit()
     if collection.backdrop:
@@ -210,7 +219,7 @@ def buildCollection(inetref, opts):
         m.images.append({'type':'coverart', 'url':p.geturl(),
                   'thumb':p.geturl(p.sizes()[0])})
     tree.append(m.toXML())
-    sys.stdout.write(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
+    print_etree(etree.tostring(tree, encoding='UTF-8', pretty_print=True,
                                     xml_declaration=True))
     sys.exit()
 
@@ -227,7 +236,7 @@ def buildVersion():
     etree.SubElement(version, "version").text = __version__
     etree.SubElement(version, "accepts").text = 'tmdb.py'
     etree.SubElement(version, "accepts").text = 'tmdb.pl'
-    sys.stdout.write(etree.tostring(version, encoding='UTF-8', pretty_print=True,
+    print_etree(etree.tostring(version, encoding='UTF-8', pretty_print=True,
                                     xml_declaration=True))
     sys.exit(0)
 
@@ -250,10 +259,10 @@ def performSelfTest():
         import lxml
     except:
         err = 1
-        print "Failed to import python lxml library."
+        print ("Failed to import python lxml library.")
 
     if not err:
-        print "Everything appears in order."
+        print ("Everything appears in order.")
     sys.exit(err)
 
 def main():
@@ -301,7 +310,7 @@ def main():
         if (not confdir) or (confdir == '/'):
             confdir = os.environ.get('HOME', '')
             if (not confdir) or (confdir == '/'):
-                print "Unable to find MythTV directory for metadata cache."
+                print ("Unable to find MythTV directory for metadata cache.")
                 sys.exit(1)
             confdir = os.path.join(confdir, '.mythtv')
         cachedir = os.path.join(confdir, 'cache')

mythtv/bindings/python/tmdb3/scripts/populate_locale.py
@@ -23,7 +23,7 @@ fd = open(fpath, 'r')
 while True:
     line = fd.readline()
     if len(line) == 0:
-        print "code endpoint not found, aborting!"
+        print ("code endpoint not found, aborting!")
         sys.exit(1)
     if line.startswith('########'):
         endpt = fd.tell()
@@ -44,7 +44,7 @@ for row in root.getroot().getchildren()[3].getchildren()[2].getchildren()[0]\
         # skip empty 639-1 code
         continue
     name, _, _, iso639_2, iso639_1 = [t.text for t in row]
-    
+
     fd.write('Language("{0}", "{1}", u"{2}")\n'.format(iso639_1, iso639_2, sanitize(name).encode('utf8')))
 
 root = lxml.html.parse('http://www.iso.org/iso/country_codes/iso_3166_code_lists/country_names_and_code_elements.htm').getroot()

mythtv/bindings/python/tmdb3/tmdb3/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from tmdb_api import Configuration, searchMovie, searchMovieWithYear, \
+from .tmdb_api import Configuration, searchMovie, searchMovieWithYear, \
                      searchPerson, searchStudio, searchList, searchCollection, \
                      searchSeries, Person, Movie, Collection, Genre, List, \
                      Series, Studio, Network, Episode, Season, __version__
-from request import set_key, set_cache
-from locales import get_locale, set_locale
-from tmdb_auth import get_session, set_session
-from cache_engine import CacheEngine
-from tmdb_exceptions import *
+from .request import Request, set_key, set_cache
+from .locales import get_locale, set_locale
+from .tmdb_auth import get_session, set_session
+from .cache_engine import CacheEngine
+from .tmdb_exceptions import *
 

mythtv/bindings/python/tmdb3/tmdb3/cache.py
@@ -10,11 +10,11 @@
 import time
 import os
 
-from tmdb_exceptions import *
-from cache_engine import Engines
+from .tmdb_exceptions import *
+from .cache_engine import Engines
 
-import cache_null
-import cache_file
+from . import cache_null
+from . import cache_file
 
 
 class Cache(object):
@@ -44,7 +44,7 @@ class Cache(object):
                 self._age = max(self._age, obj.creation)
 
     def _expire(self):
-        for k, v in self._data.items():
+        for k, v in list(self._data.items()):
             if v.expired:
                 del self._data[k]
 

mythtv/bindings/python/tmdb3/tmdb3/cache_engine.py
@@ -9,7 +9,9 @@
 
 import time
 from weakref import ref
+import sys
 
+from future.utils import with_metaclass
 
 class Engines(object):
     """
@@ -43,8 +45,8 @@ class CacheEngineType(type):
             Engines.register(cls)
 
 
-class CacheEngine(object):
-    __metaclass__ = CacheEngineType
+class CacheEngine(with_metaclass(CacheEngineType, object)):
+
     name = 'unspecified'
 
     def __init__(self, parent):

mythtv/bindings/python/tmdb3/tmdb3/cache_file.py
@@ -16,10 +16,14 @@ import time
 import os
 import io
 
-from cStringIO import StringIO
+try:
+    from cStringIO import StringIO
+except ImportError:
+    # from io import BytesIO as StringIO
+    from io import StringIO
 
-from tmdb_exceptions import *
-from cache_engine import CacheEngine, CacheObject
+from .tmdb_exceptions import *
+from .cache_engine import CacheEngine, CacheObject
 
 ####################
 # Cache File Format
@@ -123,7 +127,7 @@ except ImportError:
         if filename.startswith('~'):
             # check for home directory
             return os.path.expanduser(filename)
-        elif (ord(filename[0]) in (range(65, 91) + range(99, 123))) \
+        elif (ord(filename[0]) in (list(range(65, 91) + range(99, 123)))) \
                 and (filename[1:3] == ':\\'):
             # check for absolute drive path (e.g. C:\...)
             return filename
@@ -195,7 +199,7 @@ class FileCacheObject(CacheObject):
     def load(self, fd):
         fd.seek(self.position)
         self._buff.seek(0)
-        self._buff.write(fd.read(self.size))
+        self._buff.write(fd.read(self.size).decode('utf-8'))
 
     def dumpslot(self, fd):
         pos = fd.tell()
@@ -204,7 +208,7 @@ class FileCacheObject(CacheObject):
     def dumpdata(self, fd):
         self.size
         fd.seek(self.position)
-        fd.write(self._buff.getvalue())
+        fd.write(self._buff.getvalue().encode('utf-8'))
 
 
 class FileEngine( CacheEngine ):
@@ -266,7 +270,7 @@ class FileEngine( CacheEngine ):
     def get(self, date):
         self._init_cache()
         self._open('r+b')
-        
+
         with Flock(self.cachefd, Flock.LOCK_SH):
             # return any new objects in the cache
             return self._read(date)
@@ -371,7 +375,7 @@ class FileEngine( CacheEngine ):
         else:
             # rewrite cache file from scratch
             # pull data from parent cache
-            data.extend(self.parent()._data.values())
+            data.extend(list(self.parent()._data.values()))
             data.sort(key=lambda x: x.creation)
             # write header
             size = len(data) + self.preallocate

mythtv/bindings/python/tmdb3/tmdb3/cache_null.py
@@ -7,7 +7,7 @@
 # Purpose: Null caching engine for debugging purposes
 #-----------------------
 
-from cache_engine import CacheEngine
+from .cache_engine import CacheEngine
 
 
 class NullEngine(CacheEngine):

commit 8883b8204e797612f12a1e7f0f33599627acf234

Fix a leftover in tmdb3 binding and a typo introduced in commit a90e2db
This fixes a typo introduced in one of the previous commits and
corrects python3 compatibiliy in the grabber script tmdb3.py.

mythtv/bindings/python/tmdb3/scripts/pytmdb3.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         from tmdb3.tmdb_api import __title__, __purpose__, __version__, __author__
         print (__title__)
         print ("")
-        print (__purpose)__
+        print (__purpose__)
         print ("Version: "+__version__)
         sys.exit(0)
 

mythtv/programs/scripts/metadata/Movie/tmdb3.py
@@ -337,7 +337,7 @@ def main():
 
         if opts.collectiondata:
             buildCollection(args[0], opts)
-    except RuntimeError, exc:
+    except RuntimeError as exc:
         sys.stdout.write('ERROR: ' + str(exc) + ' exception')
         sys.exit(1)
 