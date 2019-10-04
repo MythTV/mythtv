2d9065e8cc55e2c8c97fcea2ecf41b331c562f3c
Ian Campbell <ijc@hellion.org.uk>
2019-10-05 00:52:14 +0100

    HLSStreamHandler : add debug log to the constructor mirroring the destructor's
    Refs #13488
    Signed-off-by: Paul Harrison <pharrison@mythtv.org>
    (cherry picked from commit 88b3020b303fb46a419de8185e2000c789782940)

--- mythtv/libs/libmythtv/recorders/hlsstreamhandler.cpp
+++ mythtv/libs/libmythtv/recorders/hlsstreamhandler.cpp
@@ -100,6 +100,7 @@ void HLSStreamHandler::Return(HLSStreamHandler* & ref, int inputid)
 HLSStreamHandler::HLSStreamHandler(const IPTVTuningData& tuning, int inputid)
     : IPTVStreamHandler(tuning, inputid)
 {
+    LOG(VB_GENERAL, LOG_INFO, LOC + "ctor");
     m_hls        = new HLSReader();
     m_readbuffer = new uint8_t[BUFFER_SIZE];
 }
