70dd3005f3240996b35698701c76e78d0aec8ee0
Mark Kendall <mark.kendall@gmail.com>
2020-02-26 17:42:22 +0000

    MythDeinterlacer: Fix 32bit x86 round 2

--- mythtv/libs/libmythtv/mythdeinterlacer.cpp
+++ mythtv/libs/libmythtv/mythdeinterlacer.cpp
@@ -9,7 +9,7 @@ extern "C" {
 #include "libavfilter/buffersink.h"
 }
 
-#if HAVE_SSE2
+#if (HAVE_SSE2 && ARCH_X86_64)
 #include "libavutil/x86/cpu.h"
 #include <emmintrin.h>
 bool MythDeinterlacer::s_haveSIMD = av_get_cpu_flags() & AV_CPU_FLAG_SSE2;
@@ -516,7 +516,7 @@ static inline void BlendC4x4(unsigned char *Src, int Width, int FirstRow, int La
     }
 }
 
-#if HAVE_SSE2 || HAVE_INTRINSICS_NEON
+#if (HAVE_SSE2 && ARCH_X86_64) || HAVE_INTRINSICS_NEON
 // SIMD optimised version with 16x4 alignment
 static inline void BlendSIMD16x4(unsigned char *Src, int Width, int FirstRow, int LastRow, int Pitch,
                                  unsigned char *Dst, int DstPitch, bool Second)
@@ -549,7 +549,7 @@ static inline void BlendSIMD16x4(unsigned char *Src, int Width, int FirstRow, in
         }
         for (int col = 0; col < Width; col += 16)
         {
-#if HAVE_SSE2
+#if (HAVE_SSE2 && ARCH_X86_64)
             __m128i mid = *reinterpret_cast<__m128i*>(&middle[col]);
             *reinterpret_cast<__m128i*>(&dest1[col]) =
                     _mm_avg_epu8(*reinterpret_cast<__m128i*>(&above[col]), mid);
@@ -604,7 +604,7 @@ static inline void BlendSIMD8x4(unsigned char *Src, int Width, int FirstRow, int
         }
         for (int col = 0; col < Width; col += 16)
         {
-#if HAVE_SSE2 && ARCH_X86_64
+#if (HAVE_SSE2 && ARCH_X86_64)
             __m128i mid = *reinterpret_cast<__m128i*>(&middle[col]);
             *reinterpret_cast<__m128i*>(&dest1[col]) =
                     _mm_avg_epu16(*reinterpret_cast<__m128i*>(&above[col]), mid);
@@ -659,7 +659,7 @@ void MythDeinterlacer::Blend(VideoFrame *Frame, FrameScanType Scan)
         bool width4  = (src->pitches[plane] % 4) == 0;
         // N.B. all frames allocated by MythTV should have 16 byte alignment
         // for all planes
-#if HAVE_SSE2 || HAVE_INTRINSICS_NEON
+#if (HAVE_SSE2 && ARCH_X86_64) || HAVE_INTRINSICS_NEON
         bool width16 = (src->pitches[plane] % 16) == 0;
         // profiling SSE2 suggests it is usually 4x faster - as expected
         if (s_haveSIMD && height4 && width16)
