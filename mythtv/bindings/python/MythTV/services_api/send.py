MythTV/mythtv

commit 3131ddb52b732f92aa4f9643063249ff5c39eb6a

Services API: Return reason for Send errors as part of raised exception.

mythtv/bindings/python/MythTV/services_api/send.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from os import fdopen
 
+from xml.etree import ElementTree
 import re
 import sys
 import tempfile
@@ -249,8 +250,12 @@ class Send(object):
         # TODO: Should handle redirects here (mostly for remote backends.)
         if response.status_code > 299:
             self.logger.debug('%s', response.text)
-            raise RuntimeError('Unexpected status returned: {}: URL was: {}'
-                               .format(response.status_code, url))
+            reason = (ElementTree.fromstring(response.text)
+                      .find('errorDescription').text)
+            raise RuntimeError('Unexpected status returned: {}: Reason: "{}" '
+                               'URL was: {}'
+                               .format(response.status_code,
+                                       reason, url))
 
         self._validate_header(response.headers['Server'])
 

commit 5f384d3c8f482a4348e8ccdd953e067e6b95e02f

Python bindings: prevent exception for 404 errors
3131ddb52b caused ElementTree.ParseError exceptions for errors like
these:

    <!DOCTYPE html><HTML><HEAD><TITLE>Error 404</TITLE><META
    HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
    </HEAD><BODY><H1>404 Not Found.</H1></BODY></HTML>

mythtv/bindings/python/MythTV/services_api/send.py
@@ -250,8 +250,11 @@ class Send(object):
         # TODO: Should handle redirects here (mostly for remote backends.)
         if response.status_code > 299:
             self.logger.debug('%s', response.text)
-            reason = (ElementTree.fromstring(response.text)
-                      .find('errorDescription').text)
+            try:
+                reason = (ElementTree.fromstring(response.text)
+                          .find('errorDescription').text)
+            except ElementTree.ParseError:
+                reason = 'N/A'
             raise RuntimeError('Unexpected status returned: {}: Reason: "{}" '
                                'URL was: {}'
                                .format(response.status_code,
@@ -410,13 +413,14 @@ class Send(object):
                 if self.postdata:
                     saved_endpoint = self.endpoint
                     saved_postdata = self.postdata
-                    # Need to adjust this if a service other than Frontend is
-                    # added.
+                    saved_opts = self.opts
                     self.send(endpoint='{}/version'.format(
-                        'Myth' if self.endpoint[:8] != 'Frontend'
-                        else 'Frontend'), opts=self.opts)
+                        self.endpoint[:self.endpoint.find('/')],
+                        opts=self.opts))
                     self.endpoint = saved_endpoint
                     self.postdata = saved_postdata
+                    self.opts = saved_opts
+
         except KeyError:
             # Proceed without authentication.
             pass