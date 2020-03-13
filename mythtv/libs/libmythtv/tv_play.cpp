3ef7ab39f7793e392b476d926af9471e0ad44f89
Mark Kendall <mark.kendall@gmail.com>
2020-03-13 12:24:16 +0000

    TV: Revert recent PiP swap fixes
    - this code will never work correctly.
    - need to fix the lower level locking instead

--- mythtv/libs/libmythtv/tv_play.cpp
+++ mythtv/libs/libmythtv/tv_play.cpp
@@ -1360,47 +1360,6 @@ void TV::PlaybackLoop(void)
             const PlayerContext *mctx = GetPlayerReadLock(i, __FILE__, __LINE__);
             if (mctx)
             {
-                // If the master player is using hardware decoding then it may
-                // callback to the UI thread at any point when creating or destroying
-                // a hardware context and may be holding avcodeclock. If this happens
-                // the secondary players will almost certainly block so try and
-                // catch callbacks before we enter EventLoop and VideoLoop for
-                // the secondary players.
-                // NOTE this is not perfect. It should catch most cases but if
-                // the master player calls back during EventLoop or VideoLoop
-                // it may still deadlock. This needs a complete re-work of avcodeclock
-                // which is now ubiquitous throughout the decoding code - and is
-                // probably now used inappropriately (avcodeclock is a global static).
-                if (i > 0)
-                {
-                    // ensure we don't introduce another lockup. If we fail to get
-                    // the lock, just continue. We will miss a pip frame but that
-                    // is not fatal.
-                    int tries = 10;
-                    while (--tries && !avcodeclock->tryLock(10))
-                    {
-                        PlayerContext *master = GetPlayerReadLock(0, __FILE__, __LINE__);
-                        if (master && master->m_player)
-                        {
-                            master->LockDeletePlayer(__FILE__, __LINE__);
-                            master->m_player->ProcessCallbacks();
-                            master->UnlockDeletePlayer(__FILE__, __LINE__);
-                        }
-                        ReturnPlayerLock(master);
-                    }
-
-                    if (tries)
-                    {
-                        avcodeclock->unlock();
-                    }
-                    else
-                    {
-                        LOG(VB_GENERAL, LOG_INFO, LOC + "Failed to get PiP lock");
-                        ReturnPlayerLock(mctx);
-                        continue;
-                    }
-                }
-
                 mctx->LockDeletePlayer(__FILE__, __LINE__);
                 if (mctx->m_player && !mctx->m_player->IsErrored())
                 {
@@ -6183,17 +6142,6 @@ void TV::RestartAllPlayers(PlayerContext *lctx,
         pipctx->SetNullVideo(true);
         pipctx->SetNoHardwareDecoders(true);
 
-        // if the main context is using a hardware decoder, it will callback to
-        // the main thread at some point while holding avcodeclock, which causes
-        // a deadlock. If we can't obtain the lock, try and process the callback.
-        while (!avcodeclock->tryLock(10))
-        {
-            mctx->LockDeletePlayer(__FILE__, __LINE__);
-            if (mctx->m_player)
-                mctx->m_player->ProcessCallbacks();
-            mctx->UnlockDeletePlayer(__FILE__, __LINE__);
-        }
-
         ok = StartPlayer(mctx, pipctx, pipctx->GetState());
 
         if (ok)
@@ -6212,8 +6160,6 @@ void TV::RestartAllPlayers(PlayerContext *lctx,
                 "Failed to restart new pip context (was main context)");
             ForceNextStateNone(pipctx);
         }
-
-        avcodeclock->unlock();
     }
 
     // If old main player had a kMuteAll | kMuteOff setting,
