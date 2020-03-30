MythTV/mythtv

commit ba5cfd3f13476b6356e9e779ec7c75d3538e0ad9

mythbackend: Fix missing lock on file transfer list
adding elements to m_fileTransferList in MainServer::HandleAnnounce
was no longer protected by a lock since ea5fdd45bdab01aa6f3d577db7e344a90aae98a6.

Refs #13598

(cherry picked from commit 53e30ce4f5d8182ffb02ec96fabe809cfc6927c7)

mythtv/programs/mythbackend/mainserver.cpp
@@ -2045,7 +2045,9 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             QString("adding: %1(%2) as a file transfer")
                                       .arg(commands[2])
                                       .arg(quintptr(socket),0,16));
-        fileTransferList.push_back(ft);
+        m_sockListLock.lockForWrite();
+        m_fileTransferList.push_back(ft);
+        m_sockListLock.unlock();
 
         retlist << QString::number(socket->GetSocketDescriptor());
         retlist << QString::number(ft->GetFileSize());

commit 3151002ca0803f578ae529092a7a01d72c0d2809

Fix compile error caused by commit ba5cfd3f13

mythtv/programs/mythbackend/mainserver.cpp
@@ -2045,9 +2045,9 @@ void MainServer::HandleAnnounce(QStringList &slist, QStringList commands,
             QString("adding: %1(%2) as a file transfer")
                                       .arg(commands[2])
                                       .arg(quintptr(socket),0,16));
-        m_sockListLock.lockForWrite();
-        m_fileTransferList.push_back(ft);
-        m_sockListLock.unlock();
+        sockListLock.lockForWrite();
+        fileTransferList.push_back(ft);
+        sockListLock.unlock();
 
         retlist << QString::number(socket->GetSocketDescriptor());
         retlist << QString::number(ft->GetFileSize());