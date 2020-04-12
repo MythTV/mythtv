8e540a9cb514a0e1c38f31830f3675f21cb3e3f3
Nigel Jewell <nige@grufty.co.uk>
2020-04-12 17:55:46 +0100

    Modify UPNP identity for local installation.

--- mythtv/libs/libmythupnp/upnpdevice.h
+++ mythtv/libs/libmythupnp/upnpdevice.h
@@ -284,7 +284,7 @@ class UPNP_PUBLIC DeviceLocation : public ReferenceCounter
 
             QString sName = pDevice->m_rootDevice.m_sFriendlyName;
 
-            if (sName == "mythtv: MythTV AV Media Server")
+            if (sName == "mythtv: CAN MythTV")
                 return sName + " (" + pDevice->m_sHostName + ")";
 
             return sName;
