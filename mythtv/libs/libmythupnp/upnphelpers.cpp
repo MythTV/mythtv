MythTV/mythtv

commit 77b560f3cc100717729ed1836367e57ff16c7eb6

Replace QString::sprintf with QString::asprintf or QString::arg.
Sprintf has been obsoleted in Qt5.14 and will be removed in Qt6.  The
recommended replacement is to use QString::arg in new code, although
there is a QString::asprintf function for easy backwards
compatibility.  Most instances converted to use QString::arg, a few
switched to QString::asprintf.

mythtv/libs/libmythupnp/upnphelpers.cpp
@@ -36,13 +36,10 @@ QString TimeFormat(const QTime &time)
 
 QString TimeFormat(uint32_t msec)
 {
-    QString timeStr;
-    timeStr.sprintf("%02d:%02d:%02d",
-                        (msec / (1000 * 60 * 60)) % 24, // Hours
-                        (msec / (1000 * 60)) % 60,      // Minutes
-                        (msec / 1000) % 60);            // Seconds
-
-    return timeStr;
+    return QString("%1:%2:%3")
+        .arg((msec / (1000 * 60 * 60)) % 24, 2,10,QChar('0'))  // Hours
+        .arg((msec / (1000 * 60)) % 60,      2,10,QChar('0'))  // Minutes
+        .arg((msec / 1000) % 60,             2,10,QChar('0')); // Seconds
 }
 
 QString DateTimeFormat(const QDateTime &dateTime)
@@ -88,19 +85,16 @@ QString resDurationFormat(uint32_t msec)
     // Appendix B.2 Resource Encoding Characteristics Properties
     //          B.2.1.4 res@duration
 
-    QString resDurationStr;
     // H[H]:MM:SS[.FS]
     // H = Hours (1-2 digits),
     // M = Minutes (2 digits, 0 prefix)
     // S = Seconds (2 digits, 0 prefix)
     // FS = Fractional Seconds (milliseconds)
-    resDurationStr.sprintf("%01u:%02u:%02u.%01u",
-                (msec / (1000 * 60 * 60)) % 24, // Hours
-                (msec / (1000 * 60)) % 60,      // Minutes
-                (msec / 1000) % 60,             // Seconds
-                msec % 1000);
-
-    return resDurationStr;
+    return QString("%01u:%02u:%02u.%01u")
+        .arg((msec / (1000 * 60 * 60)) % 24, 1,10,QChar('0')) // Hours
+        .arg((msec / (1000 * 60)) % 60,      2,10,QChar('0')) // Minutes
+        .arg((msec / 1000) % 60,             2,10,QChar('0')) // Seconds
+        .arg(msec % 1000,                    1,10,QChar('0'));
 }
 
 };
@@ -393,8 +387,8 @@ QString FlagsString(uint32_t flags)
     if (flags & ~DLNA::kv1_5_flag)
         flags |= DLNA::kv1_5_flag;
 
-    str.sprintf("DLNA.ORG_FLAGS=%08X000000000000000000000000", flags); // 8 HEX Digits, following by 24 zeros
-    return str;
+    return QString("DLNA.ORG_FLAGS=%1") // 8 HEX Digits, following by 24 zeros
+        .arg(flags,8,10,QChar('0')) + "000000000000000000000000";
 }
 
 QString OpParamString(UPNPProtocol::TransferProtocol protocol)

mythtv/libs/libmyth/mythcontext.cpp
@@ -168,10 +168,9 @@ static void exec_program_tv_cb(const QString &cmd)
 
     if (cardid >= 0)
     {
-        s = s.sprintf(qPrintable(s),
-                      qPrintable(strlist[1]),
-                      qPrintable(strlist[2]),
-                      qPrintable(strlist[3]));
+        s = s.arg(qPrintable(strlist[1]))
+            .arg(qPrintable(strlist[2]))
+            .arg(qPrintable(strlist[3]));
 
         myth_system(s);
 

mythtv/libs/libmyth/programinfo.cpp
@@ -1655,9 +1655,9 @@ void ProgramInfo::ToMap(InfoMap &progMap,
         if (((m_recEndTs > timeNow) && (m_recStatus <= RecStatus::WillRecord)) ||
             (m_recStatus == RecStatus::Conflict) || (m_recStatus == RecStatus::LaterShowing))
         {
-            tmp_rec += QString().sprintf(" %+d", m_recPriority);
+            tmp_rec += QString(" %1%2").arg(m_recPriority > 0 ? "+" : "").arg(m_recPriority);
             if (m_recPriority2)
-                tmp_rec += QString().sprintf("/%+d", m_recPriority2);
+                tmp_rec += QString("/%1%2").arg(m_recPriority2 > 0 ? "+" : "").arg(m_recPriority2);
             tmp_rec += " ";
         }
         else

mythtv/libs/libmyth/rssparse.cpp
@@ -123,8 +123,8 @@ void ResultItem::toMap(InfoMap &metadataMap)
     else
         metadataMap["filesize"] = QString::number(m_filesize);
 
-    QString tmpSize;
-    tmpSize.sprintf("%0.2f ", m_filesize / 1024.0 / 1024.0);
+    QString tmpSize = QString("%1 ")
+        .arg(m_filesize / 1024.0 / 1024.0, 0,'f',2);
     tmpSize += QObject::tr("MB", "Megabytes");
     if (m_filesize == -1)
         metadataMap["filesize_str"] = QString();

mythtv/libs/libmyth/standardsettings.h
@@ -283,13 +283,14 @@ class MPUBLIC HostTimeBoxSetting : public HostComboBoxSetting
     {
         int hour;
         int minute;
-        QString timeStr;
 
         for (hour = 0; hour < 24; hour++)
         {
             for (minute = 0; minute < 60; minute += interval)
             {
-                timeStr = timeStr.sprintf("%02d:%02d", hour, minute);
+                QString timeStr = QString("%1:%2")
+                    .arg(hour,  2,10,QChar('0'))
+                    .arg(minute,2,10,QChar('0'));
                 addSelection(timeStr, timeStr,
                              timeStr == defaultTime);
             }
@@ -307,13 +308,14 @@ class MPUBLIC GlobalTimeBoxSetting : public GlobalComboBoxSetting
     {
         int hour;
         int minute;
-        QString timeStr;
 
         for (hour = 0; hour < 24; hour++)
         {
             for (minute = 0; minute < 60; minute += interval)
             {
-                timeStr = timeStr.sprintf("%02d:%02d", hour, minute);
+                QString timeStr = QString("%1:%2")
+                    .arg(hour,  2,10,QChar('0'))
+                    .arg(minute,2,10,QChar('0'));
                 addSelection(timeStr, timeStr,
                              timeStr == defaultTime);
             }

mythtv/libs/libmythmetadata/lyricsdata.h
@@ -41,8 +41,10 @@ class META_PUBLIC LyricsLine
         int seconds = m_time  % (1000 * 60) / 1000;
         int hundredths = (m_time % 1000) / 10;
 
-        res.sprintf("[%02d:%02d.%02d]", minutes, seconds, hundredths);
-        return res;
+        return QString("[%1:%2.%3]")
+            .arg(minutes,    2,10,QChar('0'))
+            .arg(seconds,    2,10,QChar('0'))
+            .arg(hundredths, 2,10,QChar('0'));
     }
 };
 

mythtv/libs/libmythmetadata/musicmetadata.cpp
@@ -1097,9 +1097,14 @@ void MusicMetadata::toMap(InfoMap &metadataMap, const QString &prefix)
     int em = (len / 60) % 60;
     int es = len % 60;
     if (eh > 0)
-        metadataMap[prefix + "length"] = QString().sprintf("%d:%02d:%02d", eh, em, es);
+        metadataMap[prefix + "length"] = QString("%1:%2:%3")
+            .arg(eh,1,10)
+            .arg(em,2,10,QChar('0'))
+            .arg(es,2,10,QChar('0'));
     else
-        metadataMap[prefix + "length"] = QString().sprintf("%02d:%02d", em, es);
+        metadataMap[prefix + "length"] = QString("%1:%2")
+            .arg(em,2,10,QChar('0'))
+            .arg(es,2,10,QChar('0'));
 
     if (m_lastPlay.isValid())
     {

mythtv/libs/libmythtv/Bluray/bdringbuffer.cpp
@@ -946,9 +946,9 @@ bool BDRingBuffer::UpdateTitleInfo(void)
     int minutes = ((int)total_secs / 60) - (hours * 60);
     double secs = (double)total_secs - (double)(hours * 60 * 60 + minutes * 60);
     QString duration = QString("%1:%2:%3")
-                        .arg(QString().sprintf("%02d", hours))
-                        .arg(QString().sprintf("%02d", minutes))
-                        .arg(QString().sprintf("%02.1f", secs));
+        .arg(hours,   2,10,QChar('0'))
+        .arg(minutes, 2,10,QChar('0'))
+        .arg(secs,    2,'f',1,QChar('0'));
     LOG(VB_GENERAL, LOG_INFO, LOC +
         QString("New title info: Index %1 Playlist: %2 Duration: %3 "
                 "Chapters: %5")
@@ -973,11 +973,11 @@ bool BDRingBuffer::UpdateTitleInfo(void)
                           (double)(hours * 60 * 60 + minutes * 60);
             LOG(VB_PLAYBACK, LOG_INFO, LOC +
                 QString("Chapter %1 found @ [%2:%3:%4]->%5")
-                    .arg(QString().sprintf("%02d", i + 1))
-                    .arg(QString().sprintf("%02d", hours))
-                    .arg(QString().sprintf("%02d", minutes))
-                    .arg(QString().sprintf("%06.3f", secs))
-                    .arg(framenum));
+                .arg(i + 1,   2,10,QChar('0'))
+                .arg(hours,   2,10,QChar('0'))
+                .arg(minutes, 2,10,QChar('0'))
+                .arg(secs,    6,'f',3,QChar('0'))
+                .arg(framenum));
         }
     }
 

commit 5f1993304e35042192aed059dd7cf8717b76c6a7

Fix incorrect data provided to UPnP client.
This bug was introduced in 77b560f3cc when converting from the
obsolete QString::sprintf function to typical QString formatting using
QString::arg.  In one case the arguments were all converted, but the
format string wasn't .  Fix that format string.

Fixes #13612.

mythtv/libs/libmythupnp/upnphelpers.cpp
@@ -90,7 +90,7 @@ QString resDurationFormat(uint32_t msec)
     // M = Minutes (2 digits, 0 prefix)
     // S = Seconds (2 digits, 0 prefix)
     // FS = Fractional Seconds (milliseconds)
-    return QString("%01u:%02u:%02u.%01u")
+    return QString("%1:%2:%3.%4")
         .arg((msec / (1000 * 60 * 60)) % 24, 1,10,QChar('0')) // Hours
         .arg((msec / (1000 * 60)) % 60,      2,10,QChar('0')) // Minutes
         .arg((msec / 1000) % 60,             2,10,QChar('0')) // Seconds