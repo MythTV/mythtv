ff0067852bf199f49a517fa79bf7176a2927b788
Gary Buhrmaster <gary.buhrmaster@gmail.com>
2020-07-31 15:20:32 -0500

    Utilize Schedules Direct metadata if grabber makes it available
    Schedules Direct provides some additional metadata that at least one
    upstream XMLTV grabber now optionally makes available.  Use that
    metadata if it is provided by the grabber (typically the grabber will
    need to be told to provide the additional information via a grabber
    --configure).
    
    In particular, with this patch, the original air date and a new
    episode flag is available and can be useful to better schedule
    recordings.
    
    The (real soon now) next XMLTV release will include the new grabber,
    and one can obtain the upstream master code now.
    
    If the additional metadata information is not available (or not
    configured in the grabber to be made available) the results are the
    same as today.
    
    Fixes #13655
    
    Signed-off-by: David Engel <dengel@mythtv.org>

--- mythtv/programs/mythfilldatabase/xmltvparser.cpp
+++ mythtv/programs/mythfilldatabase/xmltvparser.cpp
@@ -467,7 +467,7 @@ bool XMLTVParser::parseFile(
                     {
                         pginfo->m_previouslyshown = true;
                         QString prevdate = xml.attributes().value( "start").toString();
-                        if (!prevdate.isEmpty())
+                        if ((!prevdate.isEmpty()) && (pginfo->m_originalairdate.isNull()))
                         {
                             QDateTime date;
                             fromXMLTVDate(prevdate, date);
@@ -634,6 +634,29 @@ bool XMLTVParser::parseFile(
                                 QString inetref(QString ("ttvdb.py_") + inetrefRaw.section('/',1,1).trimmed());
                                 pginfo->m_inetref = inetref;
                                 // ProgInfo does not have a collectionref, so we don't set any
+                            }
+                        }
+                        else if (system == "schedulesdirect.org")
+                        {
+                            QString details(xml.readElementText(QXmlStreamReader::SkipChildElements));
+                            if (details.startsWith(QString("originalAirDate/")))
+                            {
+                                QString value(details.section('/', 1, 1).trimmed());
+                                QDateTime datetime;
+                                fromXMLTVDate(value, datetime);
+                                pginfo->m_originalairdate = datetime.date();
+                            }
+                            else if (details.startsWith(QString("newEpisode/")))
+                            {
+                                QString value(details.section('/', 1, 1).trimmed());
+                                if (value == QString("true"))
+                                {
+                                    pginfo->m_previouslyshown = false;
+                                }
+                                else if (value == QString("false"))
+                                {
+                                    pginfo->m_previouslyshown = true;
+                                }
                             }
                         }
                     }//episode-num
