MythTV/mythtv

commit a3ae3a8288beaafbac71acd9d4300792711b025d

GUI: Fix bug with buttons vanishing when Reset used
Reset action on a screen should restore everything to the startup
state. Fix the bug that if there are buttons on the screen they
would vanish since they were set to invalid state "default"
instead of valid state "active".

Refs #13234

mythtv/libs/libmythui/mythuistatetype.cpp
@@ -99,7 +99,7 @@ bool MythUIStateType::DisplayState(const QString &name)
     if (i != m_ObjectsByName.end())
         m_CurrentState = i.value();
     else
-        m_CurrentState = NULL;
+        return false;
 
     if (m_CurrentState != old)
     {
@@ -202,7 +202,7 @@ void MythUIStateType::Clear()
  */
 void MythUIStateType::Reset()
 {
-    if (!DisplayState("default"))
+    if (!DisplayState("default") && !DisplayState("active"))
     {
         if (!DisplayState(None))
         {

commit dbf9baa1b6caefe4132cbd9a06f8cf74a07f1d92

Fix bug caused by commit a3ae3a8
Problem was that in Terra theme, watched icon was not disappearing
when an unwatched show was selected.

This reverts part of the above mentioned commit that was causing
this problem.

mythtv/libs/libmythui/mythuistatetype.cpp
@@ -93,7 +93,7 @@ bool MythUIStateType::DisplayState(const QString &name)
     if (i != m_objectsByName.end())
         m_currentState = i.value();
     else
-        return false;
+        m_currentState = nullptr;
 
     if (m_currentState != old)
     {