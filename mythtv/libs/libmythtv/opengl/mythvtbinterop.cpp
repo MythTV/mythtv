57eabc8867f5bf2bd17b713378dcc09f62f07890
Mark Kendall <mark.kendall@gmail.com>
2020-10-05 13:24:07 +0100

    MythVTBInterop: Use std::copy for creating reference frames

--- mythtv/libs/libmythtv/opengl/mythvtbinterop.cpp
+++ mythtv/libs/libmythtv/opengl/mythvtbinterop.cpp
@@ -315,9 +315,7 @@ vector<MythVideoTexture*> MythVTBSurfaceInterop::GetReferenceFrames(void)
     }
 
     result = m_openglTextures[last];
-    for (MythVideoTexture* tex : qAsConst(m_openglTextures[current]))
-        result.push_back(tex);
-    for (MythVideoTexture* tex : qAsConst(m_openglTextures[next]))
-        result.push_back(tex);
+    std::copy(m_openglTextures[current].cbegin(), m_openglTextures[current].cend(), std::back_inserter(result));
+    std::copy(m_openglTextures[next].cbegin(), m_openglTextures[next].cend(), std::back_inserter(result));
     return result;
 }
