1cd6fa58b626dd5c8199021563cd2f3affc48599
Klaas de Waal <kdewaal@mythtv.org>
2020-12-27 21:43:08 +0100

    Channelscan dialog default choice for popup menu
    In mythtv-setup there can be duplicate channel numbers found after a scan.
    It is possible to resolve this by responding to a popup menu message with
    a question for each conflicting channel.
    If this question is asked for a number of channels the default choice
    is now the answer that has been given for the previous channel.
    Previously, the default choice was always the first menu entry.

--- mythtv/libs/libmythtv/channelscan/channelimporter.cpp
+++ mythtv/libs/libmythtv/channelscan/channelimporter.cpp
@@ -1889,6 +1889,7 @@ ChannelImporter::QueryUserInsert(const QString &msg)
         }
     }
 
+    m_functorRetval = 0;    // Reset default menu choice to first item for next menu
     return action;
 }
 
@@ -1959,7 +1960,7 @@ ChannelImporter::QueryUserUpdate(const QString &msg)
                 .toLatin1().constData() << std::endl;
         }
     }
-
+    m_functorRetval = 0;    // Reset default menu choice to first item for next menu
     return action;
 }
 
@@ -1967,6 +1968,7 @@ OkCancelType ChannelImporter::ShowManualChannelPopup(
     MythMainWindow *parent, const QString& title,
     const QString& message, QString &text)
 {
+    int dmc = m_functorRetval;      // Default menu choice
     m_functorRetval = -1;
     MythScreenStack *popupStack = parent->GetStack("popup stack");
     auto *popup = new MythDialogBox(title, message, popupStack,
@@ -1974,10 +1976,10 @@ OkCancelType ChannelImporter::ShowManualChannelPopup(
 
     if (popup->Create())
     {
-        popup->AddButton(QCoreApplication::translate("(Common)", "OK"));
-        popup->AddButton(tr("Edit"));
-        popup->AddButton(QCoreApplication::translate("(Common)", "Cancel"));
-        popup->AddButton(QCoreApplication::translate("(Common)", "Cancel All"));
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "OK"), 0 == dmc);
+        popup->AddButtonD(tr("Edit"), 1 == dmc);
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "Cancel"), 2 == dmc);
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "Cancel All"), 3 == dmc);
         QObject::connect(popup, &MythDialogBox::Closed, this,
                          [this](const QString & /*resultId*/, int result)
                          {
@@ -2036,6 +2038,7 @@ OkCancelType ChannelImporter::ShowResolveChannelPopup(
     MythMainWindow *parent, const QString& title,
     const QString& message, QString &text)
 {
+    int dmc = m_functorRetval;      // Default menu choice
     m_functorRetval = -1;
     MythScreenStack *popupStack = parent->GetStack("popup stack");
     auto *popup = new MythDialogBox(title, message, popupStack,
@@ -2043,11 +2046,11 @@ OkCancelType ChannelImporter::ShowResolveChannelPopup(
 
     if (popup->Create())
     {
-        popup->AddButton(QCoreApplication::translate("(Common)", "OK"));
-        popup->AddButton(QCoreApplication::translate("(Common)", "OK All"));
-        popup->AddButton(tr("Edit"));
-        popup->AddButton(QCoreApplication::translate("(Common)", "Cancel"));
-        popup->AddButton(QCoreApplication::translate("(Common)", "Cancel All"));
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "OK"), 0 == dmc);
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "OK All"), 1 == dmc);
+        popup->AddButtonD(tr("Edit"), 2 == dmc);
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "Cancel"), 3 == dmc);
+        popup->AddButtonD(QCoreApplication::translate("(Common)", "Cancel All"), 4 == dmc);
         QObject::connect(popup, &MythDialogBox::Closed, this,
                          [this](const QString & /*resultId*/, int result)
                          {
