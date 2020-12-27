290d497c9524e42d90fcdf5e3aa22781c345e4f4
Klaas de Waal <kdewaal@mythtv.org>
2020-12-27 19:52:53 +0100

    Channelscan dialog default choice for popup menu
    In mythtv-setup there can be duplicate channel numbers found after a scan.
    It is possible to resolve this by responding to a popup menu message with
    a question for each conflicting channel.
    If this question is asked for a number of channels the default choice
    is now the answer that has been given for the previous channel.
    Previously, the default choice was always the first menu entry.

--- mythtv/libs/libmythui/mythdialogbox.h
+++ mythtv/libs/libmythui/mythdialogbox.h
@@ -194,7 +194,10 @@ class MUI_PUBLIC MythDialogBox : public MythScreenType
 
     void AddButtonV(const QString &title, QVariant data = 0,
                    bool newMenu = false, bool setCurrent = false);
-    void AddButton(const QString &title) { AddButtonV(title, 0,false, false); };
+    void AddButton(const QString &title, bool setCurrent = false)
+    {
+        AddButtonV(title, 0,false, setCurrent);
+    }
     // For non-class, static class, or lambda functions.
     void AddButton(const QString &title, const MythUICallbackNMF &slot,
                    bool newMenu = false, bool setCurrent = false)
