0bc3cdfbf7b649e6060db3fa6b67be54f788b00e
David Engel <dengel@mythtv.org>
2020-12-28 17:18:13 -0600

    Fix handling of deleted channels in Scheduler::GetAllScheduled()
    Deleted channels should not be joined at all.  Joining them only on
    chanid and then filtering in the where clause caused valid rules to
    not be included.
    
    Fixes #295
    
    (cherry picked from commit 6157a1772ac4f005aa144c9f97cb317b3d341746)

--- mythtv/programs/mythbackend/scheduler.cpp
+++ mythtv/programs/mythbackend/scheduler.cpp
@@ -4899,7 +4899,7 @@ void Scheduler::GetAllScheduled(RecList &proglist, SchedSortColumn sortBy,
         "       channel.commmethod                      " // 25
         "FROM record "
         "LEFT JOIN channel ON channel.callsign = record.station "
-        "WHERE deleted IS NULL "
+        "                     AND deleted IS NULL "
         "GROUP BY recordid "
         "ORDER BY %1 %2");
 
