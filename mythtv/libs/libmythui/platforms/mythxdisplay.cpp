a04b37d79cd3bf0dd2a7007cbaa03f59284ef91a
Mark Kendall <mark.kendall@gmail.com>
2021-01-01 20:12:28 +0000

    MythXDisplay: Remove unused code

--- mythtv/libs/libmythui/platforms/mythxdisplay.cpp
+++ mythtv/libs/libmythui/platforms/mythxdisplay.cpp
@@ -1,25 +1,6 @@
 // MythTV
-#include "config.h"
 #include "mythlogging.h"
-#include "mythuihelper.h"
 #include "mythxdisplay.h"
-
-// Std
-#include <map>
-#include <vector>
-
-using XErrorCallbackType = int (*)(Display *, XErrorEvent *);
-using XErrorVectorType = std::vector<XErrorEvent>;
-static std::map<Display*, XErrorVectorType>   xerrors;
-static std::map<Display*, XErrorCallbackType> xerror_handlers;
-
-QString MythXDisplay::s_QtX11Display;
-
-static int ErrorHandler(Display *d, XErrorEvent *xeev)
-{
-    xerrors[d].push_back(*xeev);
-    return 0;
-}
 
 class MythXLocker
 {
@@ -38,33 +19,31 @@ class MythXLocker
     }
 
   private:
-    MythXDisplay *m_disp { nullptr };
+    MythXDisplay* m_disp { nullptr };
 };
 
 MythXDisplay* MythXDisplay::OpenMythXDisplay(bool Warn /*= true*/)
 {
-    auto *disp = new MythXDisplay();
-    if (disp && disp->Open())
-        return disp;
+    auto * display = new MythXDisplay();
+    if (display && display->Open())
+        return display;
 
     if (Warn)
         LOG(VB_GENERAL, LOG_CRIT, "MythXOpenDisplay() failed");
-    delete disp;
+    delete display;
     return nullptr;
 }
 
-void MythXDisplay::SetQtX11Display(const QString &_Display)
+void MythXDisplay::SetQtX11Display(const QString& DisplayStr)
 {
-    s_QtX11Display = _Display;
+    s_QtX11Display = DisplayStr;
 }
 
 /// \brief Determine if we are running a remote X11 session
-bool MythXDisplay::DisplayIsRemote(void)
+bool MythXDisplay::DisplayIsRemote()
 {
     bool result = false;
-    auto * display = MythXDisplay::OpenMythXDisplay(false);
-
-    if (display)
+    if (auto * display = MythXDisplay::OpenMythXDisplay(false); display != nullptr)
     {
         QString displayname(DisplayString(display->GetDisplay()));
 
@@ -80,10 +59,8 @@ bool MythXDisplay::DisplayIsRemote(void)
         {
             result = true;
         }
-
         delete display;
     }
-
     return result;
 }
 
@@ -91,11 +68,7 @@ MythXDisplay::~MythXDisplay()
 {
     MythXLocker locker(this);
     if (m_disp)
-    {
-        StopLog();
         XCloseDisplay(m_disp);
-        m_disp = nullptr;
-    }
 }
 
 /*! \brief Open the display
@@ -123,10 +96,10 @@ bool MythXDisplay::Open(void)
     if (!m_disp)
         return false;
 
-    m_screenNum  = DefaultScreen(m_disp);
-    m_screen     = DefaultScreenOfDisplay(m_disp);
-    m_depth      = DefaultDepthOfScreen(m_screen);
-    m_root       = DefaultRootWindow(m_disp);
+    m_screenNum = DefaultScreen(m_disp);
+    m_screen    = DefaultScreenOfDisplay(m_disp);
+    m_depth     = DefaultDepthOfScreen(m_screen);
+    m_root      = DefaultRootWindow(m_disp);
 
     return true;
 }
@@ -146,71 +119,5 @@ QSize MythXDisplay::GetDisplayDimensions(void)
 
 void MythXDisplay::Sync(bool Flush)
 {
-    XLOCK(this, XSync(m_disp, Flush));
-}
-
-void MythXDisplay::StartLog(void)
-{
-    if (!m_disp || xerror_handlers.count(m_disp))
-        return;
-
-    Sync();
-    XLOCK(this, xerror_handlers[m_disp] = XSetErrorHandler(ErrorHandler));
-}
-
-bool MythXDisplay::StopLog(void)
-{
-    if (!(m_disp && xerror_handlers.count(m_disp)))
-        return false;
-
-    Sync();
-    XErrorCallbackType old_handler = xerror_handlers[m_disp];
-    XLOCK(this, XSetErrorHandler(old_handler));
-    xerror_handlers.erase(m_disp);
-    return CheckErrors();
-}
-
-bool MythXDisplay::CheckErrors(Display *Disp)
-{
-    if (!Disp)
-        CheckOrphanedErrors();
-
-    Display *d = Disp ? Disp : m_disp;
-    if (!d)
-        return false;
-
-    if (!xerrors.count(d))
-        return true;
-
-    MythXLocker locker(this);
-    Sync();
-    const std::vector<XErrorEvent>& events = xerrors[d];
-
-    if (events.empty())
-        return true;
-
-    for (const auto & event : events)
-    {
-        std::string buf(200,'\0');
-        XGetErrorText(d, event.error_code, buf.data(), buf.size());
-        LOG(VB_GENERAL, LOG_ERR,
-            QString("XError type: %1\nSerial no: %2\nErr code: %3 (%4)\n"
-                   "Req code: %5\nmMinor code: %6\nResource id: %7\n")
-                   .arg(event.type).arg(event.serial)
-                   .arg(event.error_code).arg(buf.data())
-                   .arg(event.request_code).arg(event.minor_code)
-                   .arg(event.resourceid));
-    }
-    xerrors.erase(d);
-    return false;
-}
-
-void MythXDisplay::CheckOrphanedErrors(void)
-{
-    if (xerrors.empty())
-        return;
-
-    for (auto & xerror : xerrors)
-        if (!xerror_handlers.count(xerror.first))
-            CheckErrors(xerror.first);
+    XLOCK(this, XSync(m_disp, Flush))
 }
--- mythtv/libs/libmythui/platforms/mythxdisplay.h
+++ mythtv/libs/libmythui/platforms/mythxdisplay.h
@@ -5,16 +5,12 @@
 #include <QString>
 #include <QMutex>
 #include <QSize>
-#include <QRect>
 
 // MythTV
 #include "mythuiexp.h"
 
 // X11
 #include <X11/Xlib.h>
-
-// Std
-#include <vector>
 
 #define XLOCK(dpy, arg) { (dpy)->Lock(); arg; (dpy)->Unlock(); }
 
@@ -23,41 +19,36 @@ class MUI_PUBLIC MythXDisplay
   public:
 
     static MythXDisplay* OpenMythXDisplay(bool Warn = true);
-    static void          SetQtX11Display (const QString &Display);
-    static bool          DisplayIsRemote (void);
+    static void          SetQtX11Display (const QString &DisplayStr);
+    static bool          DisplayIsRemote ();
 
     MythXDisplay() = default;
     ~MythXDisplay();
-    Display *GetDisplay(void)          { return m_disp;        }
-    QString  GetDisplayName(void) const{ return m_displayName; }
-    int      GetScreen(void) const     { return m_screenNum;   }
-    void     Lock(void)                { m_lock.lock();        }
-    void     Unlock(void)              { m_lock.unlock();      }
-    int      GetDepth(void) const      { return m_depth;       }
-    Window   GetRoot(void) const       { return m_root;        }
-    bool     Open(void);
-    QSize    GetDisplayDimensions(void);
+    Display* GetDisplay()          { return m_disp;        }
+    QString  GetDisplayName() const{ return m_displayName; }
+    int      GetScreen() const     { return m_screenNum;   }
+    void     Lock()                { m_lock.lock();        }
+    void     Unlock()              { m_lock.unlock();      }
+    int      GetDepth() const      { return m_depth;       }
+    Window   GetRoot() const       { return m_root;        }
+    bool     Open();
+    QSize    GetDisplayDimensions();
     void     Sync(bool Flush = false);
-    void     StartLog(void);
-    bool     StopLog(void);
 
   private:
-    bool CheckErrors(Display *Disp = nullptr);
-    void CheckOrphanedErrors(void);
+    static inline QString s_QtX11Display;
 
-    static QString s_QtX11Display;
-
-    Display      *m_disp       { nullptr };
-    int           m_screenNum  { 0 };
-    Screen       *m_screen     { nullptr };
-    int           m_depth      { 0 };
-    Window        m_root       { 0 };
-    QMutex        m_lock       { QMutex::Recursive };
-    QString       m_displayName{ };
+    Display* m_disp        { nullptr };
+    int      m_screenNum   { 0 };
+    Screen*  m_screen      { nullptr };
+    int      m_depth       { 0 };
+    Window   m_root        { 0 };
+    QMutex   m_lock        { QMutex::Recursive };
+    QString  m_displayName { };
 };
 
 // These X11 defines conflict with the QT key event enum values
 #undef KeyPress
 #undef KeyRelease
 
-#endif // MYTHXDISPLAY_X_
+#endif
