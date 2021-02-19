df94e0085387fd1e82206c287e439286e91ba56b
David Hampton <mythtv@love2code.net>
2021-02-19 17:15:53 -0500

    Remove a use of QRegExp from commflag main.cpp.

--- mythtv/programs/mythcommflag/main.cpp
+++ mythtv/programs/mythcommflag/main.cpp
@@ -22,7 +22,6 @@
 // Qt headers
 #include <QCoreApplication>
 #include <QString>
-#include <QRegExp>
 #include <QDir>
 #include <QEvent>
 
@@ -236,7 +235,7 @@ static int SetCutList(uint chanid, const QDateTime& starttime, QString newCutLis
 {
     frm_dir_map_t cutlist;
 
-    newCutList.replace(QRegExp(" "), "");
+    newCutList.remove(" ");
 
 #if QT_VERSION < QT_VERSION_CHECK(5,14,0)
     QStringList tokens = newCutList.split(",", QString::SkipEmptyParts);
