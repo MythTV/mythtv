a2b0c3bbe0cd8d8c926729402afad82d2efa216c
David Hampton <mythtv@love2code.net>
2021-03-11 12:30:34 -0500

    qt5.14: Convert mheg to QRecursiveMutex.

--- mythtv/libs/libmythtv/mheg/netstream.h
+++ mythtv/libs/libmythtv/mheg/netstream.h
@@ -13,6 +13,9 @@
 #include <QThread>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#if QT_VERSION >= QT_VERSION_CHECK(5,14,0)
+#include <QRecursiveMutex>
+#endif
 #include <QSslError>
 #include <QWaitCondition>
 #include <QQueue>
@@ -124,7 +127,11 @@ public:
     static inline void PostEvent(QEvent *e) { manager().Post(e); }
     void Post(QEvent *event);
 
+#if QT_VERSION < QT_VERSION_CHECK(5,14,0)
     static inline QMutex* GetMutex() { return &manager().m_mutexNAM; }
+#else
+    static inline QRecursiveMutex* GetMutex() { return &manager().m_mutexNAM; }
+#endif
 
     static bool isAvailable(); // is network usable
     static QDateTime GetLastModified(const QUrl &url);
@@ -147,7 +154,11 @@ private:
 
     volatile bool          m_bQuit    {false};
     QSemaphore             m_running;
+#if QT_VERSION < QT_VERSION_CHECK(5,14,0)
     mutable QMutex         m_mutexNAM {QMutex::Recursive}; // Provides recursive access to m_nam
+#else
+    mutable QRecursiveMutex m_mutexNAM; // Provides recursive access to m_nam
+#endif
     QNetworkAccessManager *m_nam      {nullptr};
     mutable QMutex         m_mutex; // Protects r/w access to the following data
     QQueue< QEvent * >     m_workQ;
