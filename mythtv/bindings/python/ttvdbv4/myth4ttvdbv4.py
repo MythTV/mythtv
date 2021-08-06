9d12abb773259e23704525a0200eb86c7e7e1207
Roland Ernst <rcrernst@gmail.com>
2021-08-06 19:22:23 +0200

    Set unique http User-Agent for thetvdb v4 api

--- mythtv/bindings/python/ttvdbv4/myth4ttvdbv4.py
+++ mythtv/bindings/python/ttvdbv4/myth4ttvdbv4.py
@@ -207,8 +207,7 @@ class Myth4TTVDBv4(object):
 
         self.session.headers.update({'Accept': 'application/json',
                                      'Accept-Language': self.language,
-                                     ### XXX ToDo define User Agent
-                                     # 'User-Agent': 'mythtv ttvdb4 grabber %s' % __version__,
+                                     'User-Agent': 'mythtv.org ttvdb v.4 grabber',
                                      'Authorization': "%s" % (token)
                                      })
         # set the session for the tvbd4 api, enable json debug logs
