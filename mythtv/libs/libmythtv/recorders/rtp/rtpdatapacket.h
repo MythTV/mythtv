8233fe0f3f96f5390b6253984bbaca4c706e7998
David Hampton <mythtv@love2code.net>
2021-08-20 15:02:43 -0400

    Use a Qt function for converting byte order.

--- mythtv/libs/libmythtv/recorders/rtp/rtpdatapacket.h
+++ mythtv/libs/libmythtv/recorders/rtp/rtpdatapacket.h
@@ -6,7 +6,7 @@
 #ifndef RTP_DATA_PACKET_H
 #define RTP_DATA_PACKET_H
 
-#include <arpa/inet.h> // for ntohs()/ntohl()
+#include <QtEndian>
 
 #include "udppacket.h"
 #include "mythlogging.h"
@@ -94,24 +94,24 @@ class RTPDataPacket : public UDPPacket
 
     uint GetSequenceNumber(void) const
     {
-        return ntohs(*reinterpret_cast<const uint16_t*>(m_data.data()+2));
+        return qFromBigEndian(*reinterpret_cast<const uint16_t*>(m_data.data()+2));
     }
 
     uint GetTimeStamp(void) const
     {
-        return ntohl(*reinterpret_cast<const uint32_t*>(m_data.data()+4));
+        return qFromBigEndian(*reinterpret_cast<const uint32_t*>(m_data.data()+4));
     }
 
     uint GetSynchronizationSource(void) const
     {
-        return ntohl(*reinterpret_cast<const uint32_t*>(m_data.data()+8));
+        return qFromBigEndian(*reinterpret_cast<const uint32_t*>(m_data.data()+8));
     }
 
     uint GetContributingSource(uint i) const
     {
         const uint32_t tmp =
             *reinterpret_cast<const uint32_t*>(m_data.data() + 12 + 4 * i);
-        return ntohl(tmp);
+        return qFromBigEndian(tmp);
     }
 
     uint GetPayloadOffset(void) const { return m_off; }
