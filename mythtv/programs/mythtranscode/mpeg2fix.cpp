5d2c7c3dc31315d3840b441f1238efad90a82b63
ulmus-scott <scott.the.elm@gmail.com>
2021-11-22 20:27:34 -0500

    mpeg2fix.cpp: remove use of internal FFmpeg header
    This file still needs to be completely rewritten to use only FFmpeg
    and enable "lossless" cutting of other codecs.

--- mythtv/programs/mythtranscode/mpeg2fix.cpp
+++ mythtv/programs/mythtranscode/mpeg2fix.cpp
@@ -27,8 +27,7 @@
 
 extern "C" {
 #include "libavutil/cpu.h"
-#include "libavcodec/mathops.h"
-#include "libmythmpeg2/attributes.h"     // for ATTR_ALIGN()
+#include "libmythmpeg2/attributes.h"     // for ATTR_ALIGN() in mpeg2_internal.h
 #include "libmythmpeg2/mpeg2.h"          // for mpeg2_decoder_t, mpeg2_fbuf_t, et c
 #include "libmythmpeg2/mpeg2_internal.h"
 }
@@ -1153,7 +1152,7 @@ void MPEG2fixup::WriteData(const QString& filename, uint8_t *data, int size)
 
 bool MPEG2fixup::BuildFrame(AVPacket *pkt, const QString& fname)
 {
-    std::array<uint16_t,64> intra_matrix {} ATTR_ALIGN(16);
+    alignas(16) std::array<uint16_t,64> intra_matrix {};
     int64_t savedPts = pkt->pts; // save the original pts
 
     const mpeg2_info_t *info = mpeg2_info(m_imgDecoder);
@@ -1194,17 +1193,28 @@ bool MPEG2fixup::BuildFrame(AVPacket *pkt, const QString& fname)
 
     m_picture->opaque = info->display_fbuf->id;
 
+    static constexpr uint8_t zigzag_scan[64] = {
+        0,   1,  8, 16,  9,  2,  3, 10,
+        17, 24, 32, 25, 18, 11,  4,  5,
+        12, 19, 26, 33, 40, 48, 41, 34,
+        27, 20, 13,  6,  7, 14, 21, 28,
+        35, 42, 49, 56, 57, 50, 43, 36,
+        29, 22, 15, 23, 30, 37, 44, 51,
+        58, 59, 52, 45, 38, 31, 39, 46,
+        53, 60, 61, 54, 47, 55, 62, 63
+    };
+
     //copy_quant_matrix(m_imgDecoder, intra_matrix);
     if (!m_zigzagInit)
     {
         for (int i = 0; i < 64; i++)
         {
-            m_invZigzagDirect16[ff_zigzag_direct[i]] = i + 1;
+            m_invZigzagDirect16[zigzag_scan[i]] = i;
         }
     }
     for (int i = 0; i < 64; i++)
     {
-        intra_matrix[m_invZigzagDirect16[i] - 1] = m_imgDecoder->quantizer_matrix[0][i];
+        intra_matrix[m_invZigzagDirect16[i]] = m_imgDecoder->quantizer_matrix[0][i];
     }
 
     if (info->display_picture->nb_fields % 2)
