93d99c021639abe1bbc53b26b2020a0e195f782a
ulmus-scott <scott.the.elm@gmail.com>
2021-12-10 19:05:02 -0500

    freesurround/el_processor.h: convert tabs to spaces, fix ° mangling from 3d1e2e5eef6fddde3701cd1c9af27acf08752ff6

--- mythtv/libs/libmythfreesurround/el_processor.h
+++ mythtv/libs/libmythfreesurround/el_processor.h
@@ -22,52 +22,52 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // the Free Surround decoder
 class fsurround_decoder {
 public:
-	// create an instance of the decoder
-	//  blocksize is fixed over the lifetime of this object for performance reasons
-	explicit fsurround_decoder(unsigned blocksize=8192);
-	// destructor
-	~fsurround_decoder();
-	
+    // create an instance of the decoder
+    //  blocksize is fixed over the lifetime of this object for performance reasons
+    explicit fsurround_decoder(unsigned blocksize=8192);
+    // destructor
+    ~fsurround_decoder();
+    
     float ** getInputBuffers();
     float ** getOutputBuffers();
 
-	// decode a chunk of stereo sound, has to contain exactly blocksize samples
-	//  center_width [0..1] distributes the center information towards the front left/right channels, 1=full distribution, 0=no distribution
-	//  dimension [0..1] moves the soundfield backwards, 0=front, 1=side
-	//  adaption_rate [0..1] determines how fast the steering gets adapted, 1=instantaneous, 0.1 = very slow adaption
-	//void decode(float *input[2], float *output[6], float center_width=1, float dimension=0, float adaption_rate=1);	
-	void decode(float center_width=1, float dimension=0, float adaption_rate=1);	
-	
-	// flush the internal buffers
-	void flush();
+    // decode a chunk of stereo sound, has to contain exactly blocksize samples
+    //  center_width [0..1] distributes the center information towards the front left/right channels, 1=full distribution, 0=no distribution
+    //  dimension [0..1] moves the soundfield backwards, 0=front, 1=side
+    //  adaption_rate [0..1] determines how fast the steering gets adapted, 1=instantaneous, 0.1 = very slow adaption
+    //void decode(float *input[2], float *output[6], float center_width=1, float dimension=0, float adaption_rate=1);   
+    void decode(float center_width=1, float dimension=0, float adaption_rate=1);
+    
+    // flush the internal buffers
+    void flush();
 
-	// --- advanced configuration ---
+    // --- advanced configuration ---
 
-	// override the surround coefficients
-	//  a is the coefficient of left rear in left total, b is the coefficient of left rear in right total; the same is true for right.
-	void surround_coefficients(float a, float b);
+    // override the surround coefficients
+    //  a is the coefficient of left rear in left total, b is the coefficient of left rear in right total; the same is true for right.
+    void surround_coefficients(float a, float b);
 
-	// set the phase shifting mode for decoding
-	// 0 = (+0ﾰ,+0ﾰ)   - music mode
-	// 1 = (+0ﾰ,+180ﾰ) - PowerDVD compatibility
-	// 2 = (+180ﾰ,+0ﾰ) - BeSweet compatibility
-	// 3 = (-90ﾰ,+90ﾰ) - This seems to work. I just don't know why.
-	void phase_mode(unsigned mode);
+    // set the phase shifting mode for decoding
+    // 0 = (+0°,+0°)   - music mode
+    // 1 = (+0°,+180°) - PowerDVD compatibility
+    // 2 = (+180°,+0°) - BeSweet compatibility
+    // 3 = (-90°,+90°) - This seems to work. I just don't know why.
+    void phase_mode(unsigned mode);
 
-	// override the steering mode
-	//  false = simple non-linear steering (old)
-	//  true  = advanced linear steering (new)
-	void steering_mode(bool mode);
+    // override the steering mode
+    //  false = simple non-linear steering (old)
+    //  true  = advanced linear steering (new)
+    void steering_mode(bool mode);
 
-	// set front/rear stereo separation
-	//  1.0 is default, 0.0 is mono
-	void separation(float front,float rear);
+    // set front/rear stereo separation
+    //  1.0 is default, 0.0 is mono
+    void separation(float front,float rear);
 
     // set samplerate for lfe filter
     void sample_rate(unsigned int samplerate);
 
 private:
-	class decoder_impl *m_impl; // private implementation (details hidden)
+    class decoder_impl *m_impl; // private implementation (details hidden)
 };
 
 
