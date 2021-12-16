a698789a566d4f9b4b0d12ed8b0dad18c171f05c
ulmus-scott <scott.the.elm@gmail.com>
2021-12-16 16:11:59 -0500

    merge bswap.h into its last user NuppelVideoRecorder.cpp (may need distclean)
    All other users have been converted to QtEndian.
    
    NuppelVideoRecorder will be removed soon, so I saw no reason to convert it.

--- mythtv/libs/libmythtv/recorders/NuppelVideoRecorder.cpp
+++ mythtv/libs/libmythtv/recorders/NuppelVideoRecorder.cpp
@@ -35,7 +35,36 @@
 
 #if HAVE_BIGENDIAN
 extern "C" {
-#include "bswap.h"
+#ifndef MYTHTV_BSWAP_H
+#define MYTHTV_BSWAP_H
+
+#include <cstdint> /* uint32_t */
+
+#define bswap_dbl(x) bswap_64(x)
+
+#if HAVE_BYTESWAP_H
+#  include <byteswap.h> /* bswap_16|32|64 */
+#elif HAVE_SYS_ENDIAN_H
+#  include <sys/endian.h>
+#  if !defined(bswap_16) && defined(bswap16)
+#    define bswap_16(x) bswap16(x)
+#  endif
+#  if !defined(bswap_32) && defined(bswap32)
+#    define bswap_32(x) bswap32(x)
+#  endif
+#  if !defined(bswap_64) && defined(bswap64)
+#    define bswap_64(x) bswap64(x)
+#  endif
+#elif CONFIG_DARWIN
+#  include <libkern/OSByteOrder.h>
+#  define bswap_16(x) OSSwapInt16(x)
+#  define bswap_32(x) OSSwapInt32(x)
+#  define bswap_64(x) OSSwapInt64(x)
+#elif HAVE_BIGENDIAN
+#  error Byte swapping functions not defined for this platform
+#endif
+
+#endif /* ndef MYTHTV_BSWAP_H */
 }
 #endif
 
