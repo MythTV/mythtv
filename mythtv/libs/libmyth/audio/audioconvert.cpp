7ec36790619e708c12fbf3f7c553dafe0e489a51
ulmus-scott <scott.the.elm@gmail.com>
2021-12-23 23:55:11 -0500

    audioconvert.cpp: remove definition of lrint()
    standardized in C99/C++11
    
    Also, remove the now unused include and the unused define.

--- mythtv/libs/libmyth/audio/audioconvert.cpp
+++ mythtv/libs/libmyth/audio/audioconvert.cpp
@@ -36,13 +36,10 @@ extern "C" {
 
 #include <QtGlobal>
 
-#include "mythconfig.h"
 #include "mythlogging.h"
 #include "mythaverror.h"
 
 #define LOC QString("AudioConvert: ")
-
-#define ISALIGN(x) (((unsigned long)(x) & 0xf) == 0)
 
 #ifdef Q_PROCESSOR_X86
 // Check cpuid for SSE2 support on x86 / x86_64
@@ -76,13 +73,6 @@ static inline bool sse2_check()
     return (bool)has_sse2;
 }
 #endif //Q_PROCESSOR_X86
-
-#if !HAVE_LRINTF
-static av_always_inline av_const long int lrintf(float x)
-{
-    return (int)(rint(x));
-}
-#endif /* HAVE_LRINTF */
 
 static inline float clipcheck(float f)
 {
