a126412109eb92248c5d5615a8450609be5332c1
Paul Harrison <paul@mythqml.net>
2022-01-14 11:39:03 +0000

    WebApp: catch any errors from GetConnectionInfo in HomeComponent
    This fixes a problem if you have set a PIN for access to the backend where
    the request would fail and the shown connection info would be stuck showing
    Loading...
    
    The error condition is now detected and the text now changes to show an
    error message.

--- mythtv/html/backend/src/app/home/home.component.ts
+++ mythtv/html/backend/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 import { MythService } from '../services/myth.service';
 import { MythHostName, MythTimeZone, MythConnectionInfo, GetSettingResponse } from '../services/interfaces/myth.interface';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -16,18 +17,27 @@ export class HomeComponent implements OnInit {
   m_connectionInfo$!: Observable<MythConnectionInfo>;
   m_setting$!: Observable<GetSettingResponse>;
 
+  public errorRes!: HttpErrorResponse;
+
   constructor(private mythService: MythService) { }
 
   ngOnInit(): void {
     this.m_hostname$ = this.mythService.GetHostName().pipe(
       tap(data => console.log(data)),
     )
+
     this.m_timezone$ = this.mythService.GetTimeZone().pipe(
       tap(data => console.log(data)),
     )
+
     this.m_connectionInfo$ = this.mythService.GetConnectionInfo().pipe(
-      tap(data => console.log(data)),
+      catchError((err: HttpErrorResponse) => {
+        console.error("error getting connection info", err);
+        this.errorRes = err;
+        return throwError(err);
+      })
     )
+
     this.m_setting$ = this.mythService.GetSetting({HostName: "localhost", Key: "TestSetting"}).pipe(
       tap(data => console.log(data)),
     )
