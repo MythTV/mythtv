2c33d4c4f9746ce4119c22c6114a970a66ae6531
David Hampton <mythtv@love2code.net>
2022-01-22 10:41:20 -0500

    Only show recorded time on OSD for recordings.

--- mythtv/libs/libmythtv/mythplayeroverlayui.cpp
+++ mythtv/libs/libmythtv/mythplayeroverlayui.cpp
@@ -205,7 +205,6 @@ void MythPlayerOverlayUI::UpdateSliderInfo(osdInfo &Info, bool PaddedFields)
         Info.values.insert(relPrefix + "totalseconds", static_cast<int>(playbackLen.count()));
         Info.values[relPrefix + "position"] = pos;
 
-        QDateTime recordedtime = m_playerCtx->m_playingRecStart.addSecs(static_cast<qint64>(secsplayed.count()));
         QString text1;
         QString text2;
         QString text3;
@@ -237,7 +236,13 @@ void MythPlayerOverlayUI::UpdateSliderInfo(osdInfo &Info, bool PaddedFields)
         Info.text[relPrefix + "behindtime"] = islive ? text3 : QString();
         QString dtformat = gCoreContext->GetSetting("DateFormat", "ddd MMMM d yyyy")
             + ", " + gCoreContext->GetSetting("TimeFormat", "hh:mm");
-        Info.text[relPrefix + "recordedtime"] = recordedtime.toLocalTime().toString(dtformat);
+
+        if (m_playerCtx->GetState() == kState_WatchingPreRecorded )
+        {
+            QDateTime recordedtime =
+                m_playerCtx->m_playingRecStart.addSecs(static_cast<qint64>(secsplayed.count()));
+            Info.text[relPrefix + "recordedtime"] = recordedtime.toLocalTime().toString(dtformat);
+        }
     }
 }
 
