4e5b66eaab3936c90f4de1d2e8318f024c771f0b
Scott Theisen <scott.the.elm@gmail.com>
2022-02-06 01:12:22 -0500

    FFmpeg: remove now unecessary added exports

--- mythtv/external/FFmpeg/libavcodec/libavcodec.v
+++ mythtv/external/FFmpeg/libavcodec/libavcodec.v
@@ -1,13 +1,6 @@
 LIBAVCODEC_MAJOR {
     global:
         av*;
-        ff_ue_golomb_vlc_code;
-        ff_golomb_vlc_len;
-        ff_se_golomb_vlc_code;
-        ff_zigzag_direct;
-        avpriv_find_start_code;
-        ff_fft_init;
-        ff_fft_end;
     local:
         *;
 };
