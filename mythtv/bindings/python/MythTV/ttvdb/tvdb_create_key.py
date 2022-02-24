7e4ce1ba987cf8420c1c293c6bafe665a0ff3d98
Roland Ernst <rcrernst@gmail.com>
2022-02-24 19:46:21 +0100

    TV-Grabber ttvdb.py: fix compatibility with requests-cache
    with newer requests-cache packages.
    
    The provided monkey patch is only valid for versions < 0.6 of the
    package 'requests-cache'.
    
    Fixes #498

--- mythtv/bindings/python/MythTV/ttvdb/tvdb_create_key.py
+++ mythtv/bindings/python/MythTV/ttvdb/tvdb_create_key.py
@@ -9,28 +9,31 @@ explicitly or implicitly
 '''
 
 import requests_cache
-
 import hashlib
-def create_key(self, request):
-    try:
-        if self._ignored_parameters:
-            url, body = self._remove_ignored_parameters(request)
-        else:
-            url, body = request.url, request.body
-    except AttributeError:
-        url, body = request.url, request.body
-    key = hashlib.sha256()
-    key.update(requests_cache.backends.base._to_bytes(request.method.upper()))
-    key.update(requests_cache.backends.base._to_bytes(url))
-    if request.body:
-        key.update(requests_cache.backends.base._to_bytes(body))
-    else:
-        if self._include_get_headers and request.headers != requests_cache.backends.base._DEFAULT_HEADERS:
-            for name, value in sorted(request.headers.items()):
-                # include only Accept-Language as it is important for context
-                if name in ['Accept-Language']:
-                    key.update(requests_cache.backends.base._to_bytes(name))
-                    key.update(requests_cache.backends.base._to_bytes(value))
-    return key.hexdigest()
 
-requests_cache.backends.base.BaseCache.create_key = create_key
+rc_version_info = tuple(int(x) for x in requests_cache.__version__.split('.'))
+# patch only older versions than '0.6.0'
+if rc_version_info < (0, 6, 0):
+    def create_key(self, request):
+        try:
+            if self._ignored_parameters:
+                url, body = self._remove_ignored_parameters(request)
+            else:
+                url, body = request.url, request.body
+        except AttributeError:
+            url, body = request.url, request.body
+        key = hashlib.sha256()
+        key.update(requests_cache.backends.base._to_bytes(request.method.upper()))
+        key.update(requests_cache.backends.base._to_bytes(url))
+        if request.body:
+            key.update(requests_cache.backends.base._to_bytes(body))
+        else:
+            if self._include_get_headers and request.headers != requests_cache.backends.base._DEFAULT_HEADERS:
+                for name, value in sorted(request.headers.items()):
+                    # include only Accept-Language as it is important for context
+                    if name in ['Accept-Language']:
+                        key.update(requests_cache.backends.base._to_bytes(name))
+                        key.update(requests_cache.backends.base._to_bytes(value))
+        return key.hexdigest()
+
+    requests_cache.backends.base.BaseCache.create_key = create_key
