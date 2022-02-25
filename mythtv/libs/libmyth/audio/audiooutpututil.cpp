cb65c9344d3b72ff1d55e536eb748b994583382c
Scott Theisen <scott.the.elm@gmail.com>
2022-02-25 13:18:50 -0500

    fixup QtEndian template use (s390x)
    The error was:
    /usr/bin/ld: ../../libs/libmyth/libmyth-32.so: undefined reference to `unsigned long qbswap<unsigned long>(unsigned long)'
    
    This must be from eldutils.cpp: uint64_t is unsigned long, but Qt must
    be using quint64 = unsigned long long, which causes the link error.
    
    To fix this, follow the Qt documentation and only use the allowed
    types, i.e. quint16, qint16, quint32, qint32, quint64, or qint64.
    
    (cherry picked from commit 8960641b32f751b8198ace9bbfc3378dac20c061)
    Signed-off-by: Klaas de Waal <klaas@kldo.nl>

--- mythtv/libs/libmyth/audio/audiooutpututil.cpp
+++ mythtv/libs/libmyth/audio/audiooutpututil.cpp
@@ -212,9 +212,9 @@ char *AudioOutputUtil::GeneratePinkFrames(char *frames, int channels,
                     static_cast<float>(0x03fffffff);
                 int32_t ires = res;
                 if (bits == 16)
-                    *samp16++ = qToLittleEndian<int16_t>(ires >> 16);
+                    *samp16++ = qToLittleEndian<qint16>(ires >> 16);
                 else
-                    *samp32++ = qToLittleEndian<int32_t>(ires);
+                    *samp32++ = qToLittleEndian<qint32>(ires);
             }
             else
             {
