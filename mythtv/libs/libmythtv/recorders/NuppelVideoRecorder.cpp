8960641b32f751b8198ace9bbfc3378dac20c061
Scott Theisen <scott.the.elm@gmail.com>
2022-02-25 13:18:50 -0500

    fixup QtEndian template use (s390x)
    The error was:
    /usr/bin/ld: ../../libs/libmyth/libmyth-32.so: undefined reference to `unsigned long qbswap<unsigned long>(unsigned long)'
    
    This must be from eldutils.cpp: uint64_t is unsigned long, but Qt must
    be using quint64 = unsigned long long, which causes the link error.
    
    To fix this, follow the Qt documentation and only use the allowed
    types, i.e. quint16, qint16, quint32, qint32, quint64, or qint64.

--- mythtv/libs/libmythtv/recorders/NuppelVideoRecorder.cpp
+++ mythtv/libs/libmythtv/recorders/NuppelVideoRecorder.cpp
@@ -16,6 +16,7 @@
 #include <cmath>
 
 #include <QStringList>
+#include <QtEndian>
 
 #include <iostream>
 
@@ -2558,7 +2559,7 @@ void NuppelVideoRecorder::WriteAudio(unsigned char *buf, int fnum, std::chrono::
 #if (Q_BYTE_ORDER == Q_BIG_ENDIAN)
         auto buf16 = reinterpret_cast<uint16_t *>(buf);
         for (int i = 0; i < m_audioChannels * sample_cnt; i++)
-            buf16[i] = qToLittleEndian<uint16_t>(buf16[i]);
+            buf16[i] = qToLittleEndian<quint16>(buf16[i]);
 #endif
         if (m_audioChannels == 2)
         {
