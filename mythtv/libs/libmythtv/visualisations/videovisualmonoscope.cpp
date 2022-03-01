7731798fbd09f0deb8d09e80dd794f417c24de06
Scott Theisen <scott.the.elm@gmail.com>
2022-02-28 20:20:39 -0500

    libmythtv: use std::(min|max)

--- mythtv/libs/libmythtv/visualisations/videovisualmonoscope.cpp
+++ mythtv/libs/libmythtv/visualisations/videovisualmonoscope.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 // MythTV
 #include "videovisualmonoscope.h"
 
@@ -25,7 +27,7 @@ void VideoVisualMonoScope::InitCommon(QRect Area)
     m_area = Area;
     m_rate = 1.0;
     m_lastTime = nowAsDuration<std::chrono::milliseconds>();
-    m_lineWidth = qMax(1.0F, m_area.height() * 0.004F);
+    m_lineWidth = std::max(1.0F, m_area.height() * 0.004F);
 }
 
 bool VideoVisualMonoScope::UpdateVertices(float* Buffer)
@@ -57,7 +59,7 @@ bool VideoVisualMonoScope::UpdateVertices(float* Buffer)
             double temp = (static_cast<double>(node->m_left[s]) +
                           (node->m_right ? static_cast<double>(node->m_right[s]) : 0.0) *
                           (static_cast<double>(m_area.height())) ) / 65536.0;
-            value = temp > 0.0 ? qMax(temp, value) : qMin(temp, value);
+            value = temp > 0.0 ? std::max(temp, value) : std::min(temp, value);
         }
 
         index += step;
