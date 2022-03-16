MythTV/mythtv

commit 91e4ba63ebc51b557dbf8fab8f2d67e649801236

configure: Allow disabling of qtwebkit
Support configure option --disable-qtwebkit . qtwebkit will be
enabled by default but disabled if you supply this option.
mythuiwebbrowser will not be built when qtwebkit is disabled.

mythtv/libs/libmythui/libmythui.pro
@@ -108,7 +108,7 @@ inc.files += mythuitext.h mythuibutton.h mythlistbutton.h xmlparsebase.h
 inc.files += myththemedmenu.h mythdialogbox.h mythfontproperties.h
 inc.files += mythuiclock.h mythgesture.h mythuitextedit.h mythprogressdialog.h
 inc.files += mythuispinbox.h mythuicheckbox.h mythuibuttonlist.h mythuigroup.h
-inc.files += mythuiprogressbar.h mythuiwebbrowser.h mythuiutils.h
+inc.files += mythuiprogressbar.h mythuiutils.h
 inc.files += x11colors.h mythgenerictree.h mythuibuttontree.h
 inc.files += mythvirtualkeyboard.h mythuishape.h mythuiguidegrid.h
 inc.files += mythuieditbar.h mythuifilebrowser.h mythuivideo.h

mythtv/configure
@@ -2801,6 +2801,7 @@ enable system_libexiv2
 enable system_libbluray
 enable system_libudfread
 enable waylandextras
+enable qtwebkit
 
 # mythtv paths
 dvb_path_default="${sysinclude:-$sysroot/usr/include}"
@@ -6614,7 +6615,7 @@ case $target_os in
 esac
 
 # Omit Qt5 modules that aren't in Qt6.
-if [ $qt_version -lt $(version2num "6.0.0") ]; then
+if [ $qt_version -lt $(version2num "6.0.0") ] && enabled qtwebkit ; then
 if $(pkg-config --exists Qt5WebKit) || $(pkg-config --exists QtWebKit) ; then
     enable qtwebkit
 else

commit 1ee5ba3c2989faf0bb77d52aec16d3b2a68965c3

Fix build failure caused by 91e4ba63eb
mythuiwebbrowser.h needs to be deployed. Accidentally removed in prior
commit.

mythtv/libs/libmythui/libmythui.pro
@@ -108,7 +108,7 @@ inc.files += mythuitext.h mythuibutton.h mythlistbutton.h xmlparsebase.h
 inc.files += myththemedmenu.h mythdialogbox.h mythfontproperties.h
 inc.files += mythuiclock.h mythgesture.h mythuitextedit.h mythprogressdialog.h
 inc.files += mythuispinbox.h mythuicheckbox.h mythuibuttonlist.h mythuigroup.h
-inc.files += mythuiprogressbar.h mythuiutils.h
+inc.files += mythuiprogressbar.h mythuiwebbrowser.h mythuiutils.h
 inc.files += x11colors.h mythgenerictree.h mythuibuttontree.h
 inc.files += mythvirtualkeyboard.h mythuishape.h mythuiguidegrid.h
 inc.files += mythuieditbar.h mythuifilebrowser.h mythuivideo.h