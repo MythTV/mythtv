b7a6433dafd82d431015f29f462d011dc7244f93
Scott Theisen <scott.the.elm@gmail.com>
2022-03-16 15:08:04 -0400

    remove unused StringUtil::hasUtf8()
    If needed StringUtil::isValidUTF8() could replace it.

--- mythtv/libs/libmythbase/stringutil.h
+++ mythtv/libs/libmythbase/stringutil.h
@@ -15,8 +15,6 @@ inline QString indentSpaces(unsigned int level, unsigned int size = 4)
     return QString(level * size, QChar(' '));
 }
 
-MBASE_PUBLIC bool hasUtf8(const char *str); // unused
-
 /**
 This is equivalent to QVariant(bool).toString()
 */
