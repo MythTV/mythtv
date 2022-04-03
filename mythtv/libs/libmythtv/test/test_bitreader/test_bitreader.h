a0f653a2cce0416c1d7d4c1ba22dd1ffb37b315e
Scott Theisen <scott.the.elm@gmail.com>
2022-04-03 19:37:29 -0400

    create class BitReader to replace internal ffmpeg headers
    All inline because that is how FFmpeg's is.
    
    The unit tests are not the most extensive but are sufficient to
    convince me that it works properly.

---
+++ mythtv/libs/libmythtv/test/test_bitreader/test_bitreader.h
@@ -0,0 +1,36 @@
+/*
+ *  Class TestBitReader
+ *
+ *  Copyright (c) Scott Theisen 2022
+ *
+ * This file is part of MythTV.
+ *
+ * MythTV is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * MythTV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MythTV; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <QtTest/QtTest>
+
+#include "libmythtv/bitreader.h"
+
+class TestBitReader : public QObject
+{
+    Q_OBJECT
+
+  private slots:
+    static void get_bits();
+    static void get_ue_golomb_long();
+    static void get_se_golomb1();
+    static void get_se_golomb2();
+};
