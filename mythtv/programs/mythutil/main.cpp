eca2ec5c748c4a13671bad1e8dccfd8e001a8617
Scott Theisen <scott.the.elm@gmail.com>
2022-04-09 16:36:02 -0400

    mythutil: merge mythutil.cpp into main.cpp
    It was only one function and this is preparation for renaming main.cpp
    to mythutil.cpp.

--- mythtv/programs/mythutil/main.cpp
+++ mythtv/programs/mythutil/main.cpp
@@ -5,6 +5,8 @@
 #else
 #include <QApplication>
 #endif
+#include <QDateTime>
+#include <QString>
 
 // libmyth* includes
 #include "mythconfig.h"
@@ -27,6 +29,45 @@
 #include "recordingutils.h"
 #include "signalhandling.h"
 
+bool GetProgramInfo(const MythUtilCommandLineParser &cmdline,
+                    ProgramInfo &pginfo)
+{
+    if (cmdline.toBool("video"))
+    {
+        QString video = cmdline.toString("video");
+        pginfo = ProgramInfo(video);
+        return true;
+    }
+    if (!cmdline.toBool("chanid"))
+    {
+        LOG(VB_GENERAL, LOG_ERR, "No chanid specified");
+        return false;
+    }
+
+    if (!cmdline.toBool("starttime"))
+    {
+        LOG(VB_GENERAL, LOG_ERR, "No start time specified");
+        return false;
+    }
+
+    uint chanid = cmdline.toUInt("chanid");
+    QDateTime starttime = cmdline.toDateTime("starttime");
+    QString startstring = starttime.toString("yyyyMMddhhmmss");
+
+    const ProgramInfo tmpInfo(chanid, starttime);
+
+    if (!tmpInfo.GetChanID())
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("No program data exists for channel %1 at %2")
+                .arg(chanid).arg(startstring));
+        return false;
+    }
+
+    pginfo = tmpInfo;
+
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
