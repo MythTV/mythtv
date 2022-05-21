087bc7c868e33f6c1700f7a9341f8b4c8b8b3ed3
Scott Theisen <scott.the.elm@gmail.com>
2022-05-21 17:38:47 -0400

    lavf: merge utils-mythtv.c back into utils.c

--- mythtv/external/FFmpeg/libavformat/utils.c
+++ mythtv/external/FFmpeg/libavformat/utils.c
@@ -5956,3 +5956,60 @@ void mythtv_flush_packet_queue(AVFormatContext *s)
 {
     flush_packet_queue(s);
 }
+
+/**
+ * @brief Remove a stream from a media stream.
+ *
+ * This is used by mpegts, so we can track streams as indicated by the PMT.
+ *
+ * @param s MPEG media stream handle
+ * @param id stream id of stream to remove
+ * @param remove_ts if true, remove any matching MPEG-TS filter as well
+ */
+void av_remove_stream(AVFormatContext *s, int id, int remove_ts) {
+    int i;
+    int changes = 0;
+
+    for (i=0; i<s->nb_streams; i++) {
+        if (s->streams[i]->id != id)
+            continue;
+
+        av_log(NULL, AV_LOG_DEBUG, "av_remove_stream 0x%x\n", id);
+#if FF_API_LAVF_AVCTX
+FF_DISABLE_DEPRECATION_WARNINGS
+        /* close codec context */
+        {
+        AVCodecContext *codec_ctx = s->streams[i]->codec;
+        if (codec_ctx->codec) {
+            avcodec_close(codec_ctx);
+            av_free(codec_ctx);
+        }
+        }
+FF_ENABLE_DEPRECATION_WARNINGS
+#endif
+        av_log(NULL, AV_LOG_DEBUG, "av_remove_stream: removing... "
+               "s->nb_streams=%d i=%d\n", s->nb_streams, i);
+        /* actually remove av stream */
+        s->nb_streams--;
+        if ((s->nb_streams - i) > 0) {
+            memmove(&s->streams[i], &s->streams[i+1],
+                    (s->nb_streams-i)*sizeof(AVFormatContext *));
+        }
+        else
+            s->streams[i] = NULL;
+
+        changes = 1;
+    }
+    if (changes)
+    {
+        // flush queued packets after a stream change (might need to make smarter)
+        mythtv_flush_packet_queue(s);
+
+        /* renumber the streams */
+        av_log(NULL, AV_LOG_DEBUG, "av_remove_stream: renumbering streams\n");
+        for (i=0; i<s->nb_streams; i++)
+            s->streams[i]->index=i;
+    }
+
+    (void)remove_ts;
+}
