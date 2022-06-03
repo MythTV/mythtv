MythTV/mythtv

commit 3456950b6f0a13e1b6f6aef7d9adadb3ac090ae5

webapp: Warnings if user leaves the page without saving.
Two things implemented if the user has unsaved changes (dirty
component).
- CanDeactivate Guard in case user goes to another setup page.
- HostListener in case the user reloads or goes to another website.

mythtv/html/backend/src/app/config/settings/settings.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
+import { CanComponentDeactivate } from 'src/app/can-deactivate-guard.service';
 
 import { SetupService } from 'src/app/services/setup.service';
 
@@ -9,7 +11,7 @@ import { SetupService } from 'src/app/services/setup.service';
     templateUrl: './settings.component.html',
     styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, CanComponentDeactivate {
 
     m_showHelp: boolean = false;
     currentTab: number= -1;
@@ -17,9 +19,11 @@ export class SettingsComponent implements OnInit {
     dirtyMessages : string[] = ["","","","","","","","","","","","","","","",""];
     forms : any [] = [,,,,,,,,,,,,,,,,];
     dirtyText = 'settings.unsaved';
+    warningText = 'settings.warning';
 
     constructor(private setupService: SetupService,  private translate: TranslateService) {
         translate.get(this.dirtyText).subscribe(data => this.dirtyText = data);
+        translate.get(this.warningText).subscribe(data => this.warningText = data);
     }
 
     ngOnInit(): void {
@@ -60,10 +64,26 @@ export class SettingsComponent implements OnInit {
         this.m_showHelp = true;
     }
 
-    saveForm() {
-        console.log("save form clicked");
-    }
-    testConnection() {
+    confirm(message?: string): Observable<boolean> {
+        const confirmation = window.confirm(message);
+            return of(confirmation);
+    };
 
+    canDeactivate(): Observable<boolean> | boolean {
+        if ((<NgForm>this.forms[this.currentTab]).dirty
+            || this.dirtyMessages.find(element => element.length > 0)) {
+            return this.confirm(this.warningText);
+        }
+        return true;
     }
+
+    @HostListener('window:beforeunload', ['$event'])
+    onWindowClose(event: any): void {
+        if ((<NgForm>this.forms[this.currentTab]).dirty
+            || this.dirtyMessages.find(element => element.length > 0)) {
+            event.preventDefault();
+            event.returnValue = false;
+        }
+    }
+
 }

mythtv/html/backend/src/app/app-routing.module.ts
@@ -6,18 +6,21 @@ import { GuideComponent } from './guide/guide.component';
 import { StatusComponent } from './status/status.component';
 import { TestbedComponent } from './testbed/testbed.component';
 import { SettingsComponent } from './config/settings/settings.component';
+import { CanDeactivateGuardService } from './can-deactivate-guard.service';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'status', component: StatusComponent },
   { path: 'setupwizard', component: SetupWizardComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'settings', component: SettingsComponent,
+      canDeactivate: [CanDeactivateGuardService] },
   { path: 'testbed', component: TestbedComponent },
   { path: 'guide', component: GuideComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [CanDeactivateGuardService]
 })
 export class AppRoutingModule { }

mythtv/html/backend/src/app/can-deactivate-guard.service.spec.ts
@@ -1,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CanDeactivateGuardService } from './can-deactivate-guard.service';
+
+describe('CanDeactivateGuardService', () => {
+  let service: CanDeactivateGuardService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CanDeactivateGuardService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

mythtv/html/backend/src/app/can-deactivate-guard.service.ts
@@ -1,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface CanComponentDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanDeactivateGuardService implements CanDeactivate<CanComponentDeactivate> {
+
+  constructor() { }
+
+  canDeactivate(component: CanComponentDeactivate, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot){
+    return component.canDeactivate ? component.canDeactivate() : true;
+  }
+}

commit 3237f03d79245a1bc16de88bdccb5a75d61fb24d

webapp: Fix bug caused by 3456950b6f
If no general tab has been opened, navigation was broken due to
an uninitialized variable.

mythtv/html/backend/src/app/config/settings/settings.component.ts
@@ -70,7 +70,7 @@ export class SettingsComponent implements OnInit, CanComponentDeactivate {
     };
 
     canDeactivate(): Observable<boolean> | boolean {
-        if ((<NgForm>this.forms[this.currentTab]).dirty
+        if (this.forms[this.currentTab] && (<NgForm>this.forms[this.currentTab]).dirty
             || this.dirtyMessages.find(element => element.length > 0)) {
             return this.confirm(this.warningText);
         }
@@ -79,7 +79,7 @@ export class SettingsComponent implements OnInit, CanComponentDeactivate {
 
     @HostListener('window:beforeunload', ['$event'])
     onWindowClose(event: any): void {
-        if ((<NgForm>this.forms[this.currentTab]).dirty
+        if (this.forms[this.currentTab] && (<NgForm>this.forms[this.currentTab]).dirty
             || this.dirtyMessages.find(element => element.length > 0)) {
             event.preventDefault();
             event.returnValue = false;