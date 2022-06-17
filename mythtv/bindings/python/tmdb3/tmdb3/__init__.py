1544bc29779433b68ac3eac23b0e95cbe3e28123
Roland Ernst <rcrernst@gmail.com>
2022-06-17 19:09:37 +0200

    Python tmdb3: Add API to get correct release dates
    The Movie Database deprecates the API call to `movie/{id}/releases`
    and favours `movie/{id}/release_dates` instead.
    
    The fetched dates are now sorted according the rules of themoviedb,
    if a release for a country is available.
    Otherwise, the primaray release date reported by `movie/{id}` is used.
    
    See
    https://www.themoviedb.org/
    https://developers.themoviedb.org/3/
    https://developers.themoviedb.org/3/movies/get-movie-release-dates
    https://developers.themoviedb.org/3/movies/get-movie-details
    
    (cherry picked from commit 6c3260c8b911a1f006eaf9a2130c4b014d7f9cf2)

--- mythtv/bindings/python/tmdb3/tmdb3/__init__.py
+++ mythtv/bindings/python/tmdb3/tmdb3/__init__.py
@@ -2,7 +2,8 @@
 from .tmdb_api import Configuration, searchMovie, searchMovieWithYear, \
                      searchPerson, searchStudio, searchList, searchCollection, \
                      searchSeries, Person, Movie, Collection, Genre, List, \
-                     Series, Studio, Network, Episode, Season, __version__
+                     Series, Studio, Network, Episode, Season, ReleaseType, \
+                     __version__
 from .request import Request, set_key, set_cache
 from .locales import get_locale, set_locale
 from .tmdb_auth import get_session, set_session
