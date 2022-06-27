53798ae1c7f2fad0139534c31c931e72a34593d7
David Hampton <mythtv@love2code.net>
2022-06-27 14:23:21 -0400

    tidy: Convert the REG_MEDIA_HANDLER macro into a static inline function.

--- mythtv/libs/libmyth/mythmediamonitor.h
+++ mythtv/libs/libmyth/mythmediamonitor.h
@@ -140,7 +140,16 @@ class MPUBLIC MediaMonitor : public QObject
     static MediaMonitor         *s_monitor;
 };
 
-#define REG_MEDIA_HANDLER(a, b, c, d, e) \
-    MediaMonitor::GetMediaMonitor()->RegisterMediaHandler(a, b, c, d, e)
+static inline void
+REG_MEDIA_HANDLER (const QString&  destination,
+                   const QString&  description,
+                   void          (*callback)(MythMediaDevice*),
+                   int             mediaType,
+                   const QString&  extensions)
+{
+    MediaMonitor::GetMediaMonitor()->RegisterMediaHandler(destination,  description,
+                                                          callback, mediaType, extensions);
+}
+
 
 #endif // MYTH_MEDIA_MONITOR_H
