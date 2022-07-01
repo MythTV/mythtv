909c53041c6bd91934eb5a0478d658e0f6b2b129
Scott Theisen <scott.the.elm@gmail.com>
2022-07-01 14:12:21 -0400

    XmlConfiguration: Style and variable names

--- mythtv/libs/libmythbase/configuration.h
+++ mythtv/libs/libmythbase/configuration.h
@@ -33,54 +33,55 @@
 
 class MBASE_PUBLIC XmlConfiguration
 {
-    protected:
+  protected:
 
-        QString      m_sPath     {GetConfDir()};
-        QString      m_sFileName {k_default_filename};
+    QString      m_path     {GetConfDir()};
+    QString      m_fileName {k_default_filename};
 
-        QDomDocument m_config;
-        QDomNode     m_rootNode;
+    QDomDocument m_config;
+    QDomNode     m_rootNode;
 
-        QDomNode FindNode( const QString &sName, bool bCreate = false );
-        QDomNode FindNode( QStringList &sParts, QDomNode &curNode, bool bCreate = false );
+    QDomNode FindNode(const QString &name, bool create);
+    QDomNode FindNode(QStringList &path, QDomNode &current, bool create);
 
-    public:
-        static constexpr auto k_default_filename = "config.xml";
+  public:
 
-        XmlConfiguration() { Load(); }
-        explicit XmlConfiguration(QString fileName)
-            : m_sFileName(std::move(fileName))
-        {
-            Load();
-        }
+    static constexpr auto k_default_filename = "config.xml";
 
-        bool    Load();
-        bool    Save();
+    XmlConfiguration() { Load(); }
+    explicit XmlConfiguration(QString fileName)
+        : m_fileName(std::move(fileName))
+    {
+        Load();
+    }
 
-        int     GetValue(const QString &sSetting, int     Default);
-        QString GetValue(const QString &sSetting, const QString &Default);
-        bool    GetBoolValue(const QString &sSetting, bool Default )
-            {return static_cast<bool>(GetValue(sSetting, static_cast<int>(Default))); }
+    bool Load();
+    bool Save();
 
-        void    SetValue(const QString &sSetting, int value);
-        void    SetValue(const QString &sSetting, const QString &value);
-        void    ClearValue(const QString &sSetting );
-        void    SetBoolValue(const QString &sSetting, bool value )
-            {SetValue(sSetting, static_cast<int>(value)); }
+    int     GetValue(const QString &setting, int defaultValue);
+    QString GetValue(const QString &setting, const QString &defaultValue);
+    bool    GetBoolValue(const QString &setting, bool defaultValue)
+        {return static_cast<bool>(GetValue(setting, static_cast<int>(defaultValue)));}
 
-        template <typename T>
-        typename std::enable_if_t<std::chrono::__is_duration<T>::value, T>
-        GetDuration(const QString &sSetting, T defaultval = T::zero())
-        {
-            return T(GetValue(sSetting, static_cast<int>(defaultval.count())));
-        }
+    void    SetValue(const QString &setting, int value);
+    void    SetValue(const QString &setting, const QString &value);
+    void    ClearValue(const QString &setting);
+    void    SetBoolValue(const QString &setting, bool value)
+        {SetValue(setting, static_cast<int>(value));}
 
-        template <typename T>
-        typename std::enable_if_t<std::chrono::__is_duration<T>::value>
-        SetDuration(const QString &sSetting, T value)
-        {
-            SetValue(sSetting, static_cast<int>(value.count()));
-        }
+    template <typename T>
+    typename std::enable_if_t<std::chrono::__is_duration<T>::value, T>
+    GetDuration(const QString &setting, T defaultValue = T::zero())
+    {
+        return T(GetValue(setting, static_cast<int>(defaultValue.count())));
+    }
+
+    template <typename T>
+    typename std::enable_if_t<std::chrono::__is_duration<T>::value>
+    SetDuration(const QString &setting, T value)
+    {
+        SetValue(setting, static_cast<int>(value.count()));
+    }
 };
 
 #endif // CONFIGURATION_H
