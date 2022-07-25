99a6e078027db5df7353dc2883d57dbb380acb92
Roland Ernst <rcrernst@gmail.com>
2022-07-25 18:47:30 +0200

    Fix fileinfo.xml generation in mytharchivehelper
    Plugin mytharchive:
    When ffmpeg cannot determine the codec of the provided stream,
    the whole buffer provided by `avcodec_string` is taken in the function
    `getFileInfo` to create an xml entry. Cleanup this buffer by removing
    '\0' characters. Otherwise, the return codec in the xml sequence will
    hold 256 zeros and mythburn.py fails to read this xml file.
    
    Refs #609

--- mythplugins/mytharchive/mytharchivehelper/mytharchivehelper.cpp
+++ mythplugins/mytharchive/mytharchivehelper/mytharchivehelper.cpp
@@ -1967,7 +1967,7 @@ static int getFileInfo(const QString& inFile, const QString& outFile, int lenMet
 #else
                 QStringList param = QString::fromStdString(buf).split(',', Qt::SkipEmptyParts);
 #endif
-                QString codec = param[0].remove("Video:", Qt::CaseInsensitive);
+                QString codec = param[0].remove("Video:", Qt::CaseInsensitive).remove(QChar::Null);
                 QDomElement stream = doc.createElement("video");
                 stream.setAttribute("streamindex", i);
                 stream.setAttribute("ffmpegindex", ffmpegIndex++);
@@ -2098,7 +2098,7 @@ static int getFileInfo(const QString& inFile, const QString& outFile, int lenMet
 #else
                 QStringList param = QString::fromStdString(buf).split(',', Qt::SkipEmptyParts);
 #endif
-                QString codec = param[0].remove("Audio:", Qt::CaseInsensitive);
+                QString codec = param[0].remove("Audio:", Qt::CaseInsensitive).remove(QChar::Null);
 
                 QDomElement stream = doc.createElement("audio");
                 stream.setAttribute("streamindex", i);
@@ -2172,7 +2172,7 @@ static int getFileInfo(const QString& inFile, const QString& outFile, int lenMet
             {
                 QDomElement stream = doc.createElement("data");
                 stream.setAttribute("streamindex", i);
-                stream.setAttribute("codec", QString::fromStdString(buf));
+                stream.setAttribute("codec", QString::fromStdString(buf).remove(QChar::Null));
                 streams.appendChild(stream);
 
                 break;
