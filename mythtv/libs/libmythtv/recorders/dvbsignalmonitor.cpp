MythTV/mythtv

commit 7c6cee5b031e1eafe609b0740bb7b930ad7b7f03

Fix a couple more recent tidy/clazy warnings.
https://clang.llvm.org/extra/clang-tidy/checks/cppcoreguidelines/init-variables.html
https://clang.llvm.org/extra/clang-tidy/checks/performance/unnecessary-value-param.html
https://github.com/KDE/clazy/blob/master/docs/checks/README-function-args-by-ref.md

mythtv/libs/libmythtv/recorders/dvbsignalmonitor.cpp
@@ -93,29 +93,29 @@ DVBSignalMonitor::DVBSignalMonitor(int db_cardnum, DVBChannel* _channel,
     // Determine the signal monitoring capabilities from the card and
     // do not use the capabilities that are not present.
     uint64_t rmflags = 0;
+    bool mok = false;
 
-    auto log_message = [](bool ok, QString loc, QString msg)
+    auto log_message = [mok](const QString& loc, const QString& msg)
     { 
-        if (ok)
+        if (mok)
             LOG(VB_CHANNEL, LOG_INFO, loc + "Can " + msg);
         else
             LOG(VB_GENERAL, LOG_WARNING, loc + "Cannot " + msg + ENO);
     };
 
-    auto update_rmflags = [&](bool ok, uint64_t flag)
+    auto update_rmflags = [mok, &rmflags](uint64_t flag)
     {
-        if (!ok)
+        if (!mok)
             rmflags |= flag;
     };
 
     // Signal strength
     auto flag = kSigMon_WaitForSig;
-    bool mok;
     if (HasFlags(flag))
     {
         _channel->GetSignalStrength(&mok);
-        update_rmflags(mok, flag);
-        log_message(mok, LOC, "measure Signal Strength");
+        update_rmflags(flag);
+        log_message(LOC, "measure Signal Strength");
     }
 
     // Signal/Noise ratio
@@ -123,8 +123,8 @@ DVBSignalMonitor::DVBSignalMonitor(int db_cardnum, DVBChannel* _channel,
     if (HasFlags(flag))
     {
         _channel->GetSNR(&mok);
-        update_rmflags(mok, flag);
-        log_message(mok, LOC, "measure S/N");
+        update_rmflags(flag);
+        log_message(LOC, "measure S/N");
     }
 
     // Bit error rate
@@ -132,8 +132,8 @@ DVBSignalMonitor::DVBSignalMonitor(int db_cardnum, DVBChannel* _channel,
     if (HasFlags(flag))
     {
         _channel->GetBitErrorRate(&mok);
-        update_rmflags(mok, flag);
-        log_message(mok, LOC, "measure Bit Error Rate");
+        update_rmflags(flag);
+        log_message(LOC, "measure Bit Error Rate");
     }
 
     // Uncorrected block count
@@ -141,8 +141,8 @@ DVBSignalMonitor::DVBSignalMonitor(int db_cardnum, DVBChannel* _channel,
     if (HasFlags(flag))
     {
         _channel->GetUncorrectedBlockCount(&mok);
-        update_rmflags(mok, flag);
-        log_message(mok, LOC, "count Uncorrected Blocks");
+        update_rmflags(flag);
+        log_message(LOC, "count Uncorrected Blocks");
     }
 
     RemoveFlags(rmflags);

commit fd2df57aeaed1d0acc34d278734d11f94a033ad4

Fix warning message logic in DVBSignalMonitor
The status value mok is passed to the lambda functions log_message and
update_rmflags via capture. This however captures the value at
the line where the lambda function is defined; it does not copy
the actual value of variable mok resulting in erroneous error messages.
This is now fixed by adding a & to the capture variable, specifying
capture by reference which means that the actual variable value is used.
This bug is introduced in commit 7c6cee5b031e1eafe609b0740bb7b930ad7b7f03
as part of the continuous fixing of clazy/tidy warnings.

mythtv/libs/libmythtv/recorders/dvbsignalmonitor.cpp
@@ -95,7 +95,7 @@ DVBSignalMonitor::DVBSignalMonitor(int db_cardnum, DVBChannel* _channel,
     uint64_t rmflags = 0;
     bool mok = false;
 
-    auto log_message = [mok](const QString& loc, const QString& msg)
+    auto log_message = [&mok](const QString& loc, const QString& msg)
     { 
         if (mok)
             LOG(VB_CHANNEL, LOG_INFO, loc + "Can " + msg);
@@ -103,7 +103,7 @@ DVBSignalMonitor::DVBSignalMonitor(int db_cardnum, DVBChannel* _channel,
             LOG(VB_GENERAL, LOG_WARNING, loc + "Cannot " + msg + ENO);
     };
 
-    auto update_rmflags = [mok, &rmflags](uint64_t flag)
+    auto update_rmflags = [&mok, &rmflags](uint64_t flag)
     {
         if (!mok)
             rmflags |= flag;