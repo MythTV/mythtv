MythTV/mythtv

commit f97e15aa0d4b5be34c340924faec2f869fce027d

webapp: Add a symbolic link to allow access to settings sub-urls.
URLs such as http://localhost:6744/settings/general were giving a 404
error if bookmarked or refeshed. Adding a link to settings in the html
directory avoids this problem.

mythtv/html/html.pro
@@ -9,4 +9,8 @@ html.files += mythbackend.html mythfrontend.html
 html.files += css images js misc setup samples tv
 html.files += 3rdParty xslt video debug apps assets
 
+# This solves a problem with refresh or bookmark of http://localhost:6744/settings/xxxxx
+# where xxxxx can be any of several angular settings URLs
+html.extra = ln -sf $(INSTALL_ROOT)/usr/share/mythtv/html $(INSTALL_ROOT)/usr/share/mythtv/html/settings
+
 INSTALLS += html

commit 4e2a37805f336d3a6dfb7866f7ca52e866f22bc0

Fix build error caused by f97e15aa0d

mythtv/html/html.pro
@@ -11,6 +11,6 @@ html.files += 3rdParty xslt video debug apps assets
 
 # This solves a problem with refresh or bookmark of http://localhost:6744/settings/xxxxx
 # where xxxxx can be any of several angular settings URLs
-html.extra = ln -sf $(INSTALL_ROOT)/usr/share/mythtv/html $(INSTALL_ROOT)/usr/share/mythtv/html/settings
+html.extra = mkdir -p $(INSTALL_ROOT)/usr/share/mythtv/html ; ln -sf $(INSTALL_ROOT)/usr/share/mythtv/html $(INSTALL_ROOT)/usr/share/mythtv/html/settings
 
 INSTALLS += html