6495c930fb108940f2818443bd4d1b0c70d06555
Scott Theisen <scott.the.elm@gmail.com>
2022-09-25 10:01:09 -0400

    libavcodec.v: re-add exports required to use golomb.h
    For reverting the BitReader change.

--- mythtv/external/FFmpeg/libavcodec/libavcodec.v
+++ mythtv/external/FFmpeg/libavcodec/libavcodec.v
@@ -4,6 +4,9 @@ LIBAVCODEC_MAJOR {
         avcodec_*;
         avpriv_*;
         avsubtitle_free;
+        ff_ue_golomb_vlc_code;
+        ff_golomb_vlc_len;
+        ff_se_golomb_vlc_code;
     local:
         *;
 };
