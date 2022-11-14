ef0519da4d5533123e72dbd5b393c81df89592b7
Scott Theisen <scott.the.elm@gmail.com>
2022-11-13 19:53:42 -0500

    use FileSystemInfo instead of getDiskSpace()
    Also, for FileSystemInfo:
    combine PopulateDiskSpace() and PopulateFSProp() into refresh() with comment from
    getDiskSpace().
    
    inline constructors and use compiler default copy assignment and copy constructor.
    
    Add a shorter constructor to replace getDiskSpace().
    
    Add doxygen comments.
    
    Note: ZMServer::getDiskSpace() has been left as is, since zmserver is standard C++
    only. i.e. does not use Qt.

--- mythtv/programs/mythbackend/servicesv2/v2myth.cpp
+++ mythtv/programs/mythbackend/servicesv2/v2myth.cpp
@@ -8,10 +8,10 @@
 
 // MythTV
 #include "libmythbase/dbutil.h"
+#include "libmythbase/filesysteminfo.h"
 #include "libmythbase/hardwareprofile.h"
 #include "libmythbase/http/mythhttpmetaservice.h"
 #include "libmythbase/mythcorecontext.h"
-#include "libmythbase/mythcoreutil.h"
 #include "libmythbase/mythdate.h"
 #include "libmythbase/mythdb.h"
 #include "libmythbase/mythdbcon.h"
@@ -345,11 +345,7 @@ V2StorageGroupDirList* V2Myth::GetStorageGroupDirs( const QString &sGroupName,
     {
         V2StorageGroupDir *pStorageGroupDir = pList->AddNewStorageGroupDir();
         QFileInfo fi(query.value(3).toString());
-        int64_t free = 0;
-        int64_t total = 0;
-        int64_t used = 0;
-
-        free = getDiskSpace(query.value(3).toString(), total, used);
+        auto fsInfo = FileSystemInfo(QString(), query.value(3).toString());
 
         pStorageGroupDir->setId            ( query.value(0).toInt()       );
         pStorageGroupDir->setGroupName     ( query.value(1).toString()    );
@@ -357,7 +353,7 @@ V2StorageGroupDirList* V2Myth::GetStorageGroupDirs( const QString &sGroupName,
         pStorageGroupDir->setDirName       ( query.value(3).toString()    );
         pStorageGroupDir->setDirRead       ( fi.isReadable()              );
         pStorageGroupDir->setDirWrite      ( fi.isWritable()              );
-        pStorageGroupDir->setKiBFree       ( free                         );
+        pStorageGroupDir->setKiBFree       ( fsInfo.getFreeSpace()        );
     }
 
     return pList;
