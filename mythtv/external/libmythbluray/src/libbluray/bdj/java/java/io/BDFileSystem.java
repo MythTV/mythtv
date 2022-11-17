b5cdb65071cffd3f17bfdc8aa754406a0d707833
David Hampton <mythtv@love2code.net>
2022-11-17 11:22:00 -0500

    Fix libbluray build failures with recent java.
    The error message is:
    BDFileSystemImpl is not abstract and does not override abstract method isInvalid(File) in FileSystem
    
    Cherry-pick fix 8f26777b from the libbluray repository at
    https://code.videolan.org/videolan/libbluray.git

--- mythtv/external/libmythbluray/src/libbluray/bdj/java/java/io/BDFileSystem.java
+++ mythtv/external/libmythbluray/src/libbluray/bdj/java/java/io/BDFileSystem.java
@@ -227,6 +227,17 @@ public abstract class BDFileSystem extends FileSystem {
         return fs.isAbsolute(f);
     }
 
+    public boolean isInvalid(File f) {
+        try {
+            Method m = fs.getClass().getDeclaredMethod("isInvalid", new Class[] { File.class });
+            Object[] args = new Object[] {(Object)f};
+            Boolean result = (Boolean)m.invoke(fs, args);
+            return result.booleanValue();
+        } finally {
+            return false;
+        }
+    }
+
     public String resolve(File f) {
         if (!booted)
             return fs.resolve(f);
