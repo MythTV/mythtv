438b4e48da0acde4598757574e6289b6386aa085
Bill Meek <bmeek@mythtv.org>
2022-12-27 14:33:10 -0600

    Revert "Update httprequest.h"
    This reverts commit e4db195d4d39e157bc468540184276618e6f8f52.

--- mythtv/libs/libmythupnp/httprequest.h
+++ mythtv/libs/libmythupnp/httprequest.h
@@ -30,11 +30,10 @@
 #include "upnputil.h"
 #include "serializers/serializer.h"
 
-static constexpr const char* SOAP_ENVELOPE_BEGIN
-{ "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" " \
-  "s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"      \
-  "<s:Body>" };
-static constexpr const char* SOAP_ENVELOPE_END { "</s:Body>\r\n</s:Envelope>" };
+#define SOAP_ENVELOPE_BEGIN  "<s:Envelope xmlns:s=\"htstp://schemas.xmlsoap.org/soap/envelope/\" " \
+                             "s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"     \
+                             "<s:Body>"
+#define SOAP_ENVELOPE_END    "</s:Body>\r\n</s:Envelope>";
 
 
 /////////////////////////////////////////////////////////////////////////////
