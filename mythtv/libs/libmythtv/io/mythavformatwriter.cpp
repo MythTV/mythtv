73f5d73e4bb59dd60ad5fbf713eb3d3810269e18
Adam Stylinski <kungfujesus06@gmail.com>
2022-12-27 15:04:59 -0500

    Fixed linking issue on big endian machines
    This code would compile but due to the way C++ handles templated types,
    it would look for this inlined routine to be exported at link time since
    the only templated types defined are Qt flavored integers.
    
    Ideally this is generating single instruction byteswaps similar to what
    happens when you use compiler builtins but admittedly I never checked.

--- mythtv/libs/libmythtv/io/mythavformatwriter.cpp
+++ mythtv/libs/libmythtv/io/mythavformatwriter.cpp
@@ -270,7 +270,7 @@ int MythAVFormatWriter::WriteAudioFrame(unsigned char *Buffer, int /*FrameNumber
 #if (Q_BYTE_ORDER == Q_BIG_ENDIAN)
     auto buf16 = reinterpret_cast<uint16_t *>(Buffer);
     for (int i = 0; i < m_audioChannels * m_audioFrameSize; i++)
-        buf16[i] = qFromLittleEndian<uint16_t>(buf16[i]);
+        buf16[i] = qFromLittleEndian<quint16>(buf16[i]);
 #endif
 
     AVCodecContext *avctx   = m_codecMap.GetCodecContext(m_audioStream);
