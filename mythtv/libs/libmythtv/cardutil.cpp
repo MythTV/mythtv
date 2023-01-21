beed2cac8311651e54b6b1ae308b1f41bd5db81b
Klaas de Waal <klaas@kldo.nl>
2023-01-21 20:54:41 +0100

    Minor indentation fix in cardutil.cpp

--- mythtv/libs/libmythtv/cardutil.cpp
+++ mythtv/libs/libmythtv/cardutil.cpp
@@ -910,7 +910,7 @@ DTVModulationSystem CardUtil::ProbeCurrentDeliverySystem(int fd_frontend)
             QString("FE_GET_PROPERTY ioctl failed (fd_frontend:%1)")
                 .arg(fd_frontend) + ENO);
         return delsys;
-        }
+    }
 
     delsys = prop.u.data;
 
@@ -1183,7 +1183,7 @@ int CardUtil::SetDeliverySystem(uint inputid, DTVModulationSystem delsys, int fd
             QString("[%1] FE_SET_PROPERTY ioctl failed")
                 .arg(inputid) + ENO);
         return ret;
-        }
+    }
 #else
     Q_UNUSED(inputid);
     Q_UNUSED(delsys);
