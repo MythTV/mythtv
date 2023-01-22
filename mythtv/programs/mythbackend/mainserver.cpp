MythTV/mythtv

commit adca306bb12a9182fe746e4c73cbba8ead866a07

Convert mythbackend IsBusy() to std::chrono. (30)

mythtv/programs/mythbackend/mainserver.cpp
@@ -4976,7 +4976,8 @@ void MainServer::HandleRemoteEncoder(QStringList &slist, QStringList &commands,
     }
     else if (command == "IS_BUSY")
     {
-        int time_buffer = (slist.size() >= 3) ? slist[2].toInt() : 5;
+        auto arg2 = std::chrono::seconds(slist[2].toInt());
+        std::chrono::seconds time_buffer = (slist.size() >= 3) ? arg2 : 5s;
         InputInfo busy_input;
         retlist << QString::number((int)enc->IsBusy(&busy_input, time_buffer));
         busy_input.ToStringList(retlist);

mythtv/libs/libmythtv/tv_rec.cpp
@@ -2477,12 +2477,12 @@ bool TVRec::IsReallyRecording(void)
             HasFlags(kFlagDummyRecorderRunning));
 }
 
-/** \fn TVRec::IsBusy(TunedInputInfo*,int) const
+/**
  *  \brief Returns true if the recorder is busy, or will be within
  *         the next time_buffer seconds.
  *  \sa EncoderLink::IsBusy(TunedInputInfo*, int time_buffer)
  */
-bool TVRec::IsBusy(InputInfo *busy_input, int time_buffer) const
+bool TVRec::IsBusy(InputInfo *busy_input, std::chrono::seconds time_buffer) const
 {
     InputInfo dummy;
     if (!busy_input)
@@ -2517,8 +2517,7 @@ bool TVRec::IsBusy(InputInfo *busy_input, int time_buffer) const
 
     if (!busy_input->m_inputId && has_pending)
     {
-        int timeLeft = MythDate::current()
-            .secsTo(pendinfo.m_recordingStart);
+        auto timeLeft = MythDate::secsInFuture(pendinfo.m_recordingStart);
 
         if (timeLeft <= time_buffer)
         {

mythtv/libs/libmythtv/tv_rec.h
@@ -180,7 +180,7 @@ class MTV_PUBLIC TVRec : public SignalMonitorListener, public QRunnable
 
     bool SetVideoFiltersForChannel(uint sourceid, const QString &channum);
 
-    bool IsBusy(InputInfo *busy_input = nullptr, int time_buffer = 5) const;
+    bool IsBusy(InputInfo *busy_input = nullptr, std::chrono::seconds time_buffer = 5s) const;
     bool IsReallyRecording(void);
 
     float GetFramerate(void);

mythtv/programs/mythbackend/encoderlink.cpp
@@ -144,12 +144,12 @@ void EncoderLink::SetSleepStatus(SleepStatus newStatus)
     m_sleepStatusTime = MythDate::current();
 }
 
-/** \fn EncoderLink::IsBusy(InputInfo*,int)
+/**
  *  \brief  Returns true if the recorder is busy, or will be within the
  *          next time_buffer seconds.
  *  \sa IsBusyRecording(void), TVRec::IsBusy(InputInfo*)
  */
-bool EncoderLink::IsBusy(InputInfo *busy_input, int time_buffer)
+bool EncoderLink::IsBusy(InputInfo *busy_input, std::chrono::seconds time_buffer)
 {
     if (m_local)
         return m_tv->IsBusy(busy_input, time_buffer);

mythtv/programs/mythbackend/encoderlink.h
@@ -86,7 +86,7 @@ class EncoderLink
     long long GetMaxBitrate(void);
     int SetSignalMonitoringRate(int rate, int notifyFrontend);
 
-    bool IsBusy(InputInfo *busy_input = nullptr, int time_buffer = 5);
+    bool IsBusy(InputInfo *busy_input = nullptr, std::chrono::seconds time_buffer = 5s);
     bool IsBusyRecording(void);
 
     TVState GetState();

mythtv/programs/mythbackend/playbacksock.cpp
@@ -334,12 +334,12 @@ bool PlaybackSock::CheckFile(ProgramInfo *pginfo)
 }
 
 bool PlaybackSock::IsBusy(int capturecardnum, InputInfo *busy_input,
-                          int time_buffer)
+                          std::chrono::seconds time_buffer)
 {
     QStringList strlist(QString("QUERY_REMOTEENCODER %1").arg(capturecardnum));
 
     strlist << "IS_BUSY";
-    strlist << QString::number(time_buffer);
+    strlist << QString::number(time_buffer.count());
 
     if (!SendReceiveStringList(strlist, 1))
     {

mythtv/programs/mythbackend/playbacksock.h
@@ -87,7 +87,7 @@ class PlaybackSock : public ReferenceCounter
 
     bool IsBusy(int        capturecardnum,
                 InputInfo *busy_input  = nullptr,
-                int        time_buffer = 5);
+                std::chrono::seconds time_buffer = 5s);
     int GetEncoderState(int capturecardnum);
     long long GetMaxBitrate(int capturecardnum);
     ProgramInfo *GetRecording(uint cardid);

mythtv/programs/mythbackend/scheduler.cpp
@@ -1903,7 +1903,7 @@ bool Scheduler::IsBusyRecording(const RecordingInfo *rcinfo)
 
     EncoderLink *rctv1 = (*m_tvList)[rcinfo->GetInputID()];
     // first check the input we will be recording on...
-    bool is_busy = rctv1->IsBusy(&busy_input, -1);
+    bool is_busy = rctv1->IsBusy(&busy_input, -1s);
     if (is_busy &&
         (rcinfo->GetRecordingStatus() == RecStatus::Pending ||
          !m_sinputInfoMap[rcinfo->GetInputID()].m_schedGroup ||
@@ -1930,7 +1930,7 @@ bool Scheduler::IsBusyRecording(const RecordingInfo *rcinfo)
         }
 
         EncoderLink *rctv2 = (*m_tvList)[id];
-        if (rctv2->IsBusy(&busy_input, -1))
+        if (rctv2->IsBusy(&busy_input, -1s))
         {
             if ((!busy_input.m_mplexId ||
                  busy_input.m_mplexId != rcinfo->m_mplexId) &&
@@ -2995,7 +2995,7 @@ bool Scheduler::AssignGroupInput(RecordingInfo &ri,
         InputInfo busy_info;
         EncoderLink *rctv = (*m_tvList)[inputid];
         m_schedLock.unlock();
-        bool isbusy = rctv->IsBusy(&busy_info, -1);
+        bool isbusy = rctv->IsBusy(&busy_info, -1s);
         m_schedLock.lock();
         if (m_recListChanged)
             return false;

commit ba1debc6fbcd6b2402c99f8b34bdb470b9c2c512

QList out-of-range error in mythmainserver
In HandleRemoteEncoder in the IS_BUSY command handler, access the value of
the 3rd Qlist element, containing an optional time value, only after checking that this value is present.
This fixes a bug that was introduced in commit adca306bb12a9182fe746e4c73cbba8ead866a07 on Jan 25, 2021.

Refs #693
Refs #699

mythtv/programs/mythbackend/mainserver.cpp
@@ -4993,8 +4993,9 @@ void MainServer::HandleRemoteEncoder(QStringList &slist, QStringList &commands,
     }
     else if (command == "IS_BUSY")
     {
-        auto arg2 = std::chrono::seconds(slist[2].toInt());
-        std::chrono::seconds time_buffer = (slist.size() >= 3) ? arg2 : 5s;
+        std::chrono::seconds time_buffer = 5s;
+        if (slist.size() >= 3)
+            time_buffer = std::chrono::seconds(slist[2].toInt());
         InputInfo busy_input;
         retlist << QString::number((int)enc->IsBusy(&busy_input, time_buffer));
         busy_input.ToStringList(retlist);