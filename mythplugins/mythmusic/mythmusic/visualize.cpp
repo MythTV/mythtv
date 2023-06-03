MythTV/mythtv

commit d34c1995efeac6a7625221a4d9bea812e3839088

tidy: Explicitly convert variables to be the same width as a pointer.
In the case of a CPU with a 32-bit integer and a 64-bit pointer, this
eliminates warnings about the implicit widening of a multiplication
result.  Calculations should always widen first and then multiply to
prevent an overflow of the smaller sized type.

mythplugins/mythmusic/mythmusic/visualize.cpp
@@ -647,13 +647,14 @@ bool WaveForm::process_all_types(VisualNode *node, bool displayed)
             short int val = node->m_left[i];
             if (val > m_maxl) m_maxl = val;
             if (val < m_minl) m_minl = val;
-            m_sqrl += val * val;
+            long valSquared = static_cast<long>(val) * static_cast<long>(val);
+            m_sqrl += valSquared;
             if (m_right)
             {
                 val = node->m_right[i];
                 if (val > m_maxr) m_maxr = val;
                 if (val < m_minr) m_minr = val;
-                m_sqrr += val * val;
+                m_sqrr += valSquared;
             }
             m_position++;
         }

mythtv/programs/mythtranscode/external/replex/multiplex.cpp
@@ -202,7 +202,7 @@ static void writeout_video(multiplex_t *mx)
 	    viu->frame == I_FRAME){
 		if (!mx->startup && mx->is_ts){
 			write_ts_patpmt(mx->ext.data(), mx->extcnt, 1, outbuf.data());
-			write(mx->fd_out, outbuf.data(), mx->pack_size*2);
+			write(mx->fd_out, outbuf.data(), static_cast<size_t>(mx->pack_size)*2);
 			ptsinc(&mx->SCR, mx->SCRinc*2);
 		} else if (!mx->startup && mx->navpack){
 			write_nav_pack(mx->pack_size, mx->extcnt, 
@@ -875,7 +875,7 @@ void setup_multiplex(multiplex_t *mx)
 	if (mx->is_ts) {
 		std::array<uint8_t,2048> outbuf {};
 		write_ts_patpmt(mx->ext.data(), mx->extcnt, 1, outbuf.data());
-		write(mx->fd_out, outbuf.data(), mx->pack_size*2);
+		write(mx->fd_out, outbuf.data(), static_cast<size_t>(mx->pack_size)*2);
 		ptsinc(&mx->SCR, mx->SCRinc*2);
 		mx->startup = 1;
 	} else if (mx->navpack){

mythtv/programs/mythtranscode/mpeg2fix.cpp
@@ -1101,8 +1101,9 @@ void MPEG2fixup::WriteYUV(const QString& filename, const mpeg2_info_t *info)
         return;
     }
 
-    int ret = write(fh, info->display_fbuf->buf[0],
-                    info->sequence->width * info->sequence->height);
+    ssize_t ret = write(fh, info->display_fbuf->buf[0],
+		       static_cast<size_t>(info->sequence->width) *
+		       static_cast<size_t>(info->sequence->height));
     if (ret < 0)
     {
         LOG(VB_GENERAL, LOG_ERR, QString("write failed %1: ").arg(filename) +
@@ -1110,7 +1111,8 @@ void MPEG2fixup::WriteYUV(const QString& filename, const mpeg2_info_t *info)
         goto closefd;
     }
     ret = write(fh, info->display_fbuf->buf[1],
-                info->sequence->chroma_width * info->sequence->chroma_height);
+                static_cast<size_t>(info->sequence->chroma_width) *
+                static_cast<size_t>(info->sequence->chroma_height));
     if (ret < 0)
     {
         LOG(VB_GENERAL, LOG_ERR, QString("write failed %1: ").arg(filename) +
@@ -1118,7 +1120,8 @@ void MPEG2fixup::WriteYUV(const QString& filename, const mpeg2_info_t *info)
         goto closefd;
     }
     ret = write(fh, info->display_fbuf->buf[2],
-                info->sequence->chroma_width * info->sequence->chroma_height);
+                static_cast<size_t>(info->sequence->chroma_width) *
+                static_cast<size_t>(info->sequence->chroma_height));
     if (ret < 0)
     {
         LOG(VB_GENERAL, LOG_ERR, QString("write failed %1: ").arg(filename) +

commit 429749b817400be4b95d3b045fd36547ba0e9195

Square the proper left/right value in the WaveForm visualization.
Fixes bug introduced in d34c1995ef.

mythplugins/mythmusic/mythmusic/visualize.cpp
@@ -647,14 +647,13 @@ bool WaveForm::process_all_types(VisualNode *node, bool displayed)
             short int val = node->m_left[i];
             if (val > m_maxl) m_maxl = val;
             if (val < m_minl) m_minl = val;
-            long valSquared = static_cast<long>(val) * static_cast<long>(val);
-            m_sqrl += valSquared;
+            m_sqrl += static_cast<long>(val) * static_cast<long>(val);
             if (m_right)
             {
                 val = node->m_right[i];
                 if (val > m_maxr) m_maxr = val;
                 if (val < m_minr) m_minr = val;
-                m_sqrr += valSquared;
+                m_sqrr += static_cast<long>(val) * static_cast<long>(val);
             }
             m_position++;
         }