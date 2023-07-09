219dfeb2d38471e6059bae56d8d29d95472d98db
Peter Bennett <pbennett@mythtv.org>
2023-07-09 09:41:58 -0400

    web app: Add delete icons to recording rules page

--- mythtv/html/backend/src/app/dashboard/recrules/recrules.component.ts
+++ mythtv/html/backend/src/app/dashboard/recrules/recrules.component.ts
@@ -18,6 +18,7 @@ interface StringAssociativeArray {
 export class RecrulesComponent implements OnInit, SchedulerSummary {
 
   recRules: RecRule[] = [];
+  recRule?: RecRule;
 
   inter: ScheduleLink = { summaryComponent: this };
 
@@ -38,6 +39,8 @@ export class RecrulesComponent implements OnInit, SchedulerSummary {
 
   rulesLoaded = false;
   errorCount = 0;
+  successCount = 0;
+  displayDelete = false;
 
   constructor(private dvrService: DvrService, private translate: TranslateService,
     public utility: UtilityService) {
@@ -86,4 +89,53 @@ export class RecrulesComponent implements OnInit, SchedulerSummary {
   newTemplate() {
     this.updateRecRule( <RecRule>{ Type: 'Recording Template' });
   }
+
+  saveObserver = {
+    next: (x: any) => {
+      if (this.recRule) {
+        if (this.recRule.Id && x.bool) {
+          // Delete
+          this.successCount++;
+          this.displayDelete = false;
+          setTimeout(() => this.refresh(), 3000);
+        }
+        else if (!this.recRule.Id && x.uint) {
+          // Add
+          this.successCount++;
+          setTimeout(() => this.inter.summaryComponent.refresh(), 1000);
+          this.recRule.Id = x.uint;
+        }
+        else {
+          this.errorCount++;
+        }
+      }
+      else {
+        // Should not happen
+        console.log("ERROR: recRule is undefined")
+        this.errorCount++;
+      }
+    },
+    error: (err: any) => {
+      console.error(err);
+      // if (err.status == 400) {
+      //   let parts = (<string>err.error).split(this.htmlRegex);
+      //   if (parts.length > 1)
+      //     this.errortext = parts[1];
+      // }
+      this.errorCount++;
+    },
+  };
+
+
+  deleteRequest(recRule: RecRule) {
+    this.recRule = recRule;
+    this.displayDelete = true;
+  }
+
+  deleteRule(recRule: RecRule) {
+    this.dvrService.RemoveRecordSchedule(recRule.Id)
+    .subscribe(this.saveObserver);
+  }
+
+
 }
