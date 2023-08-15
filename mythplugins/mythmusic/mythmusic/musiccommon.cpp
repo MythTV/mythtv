37fded65aaa02732d2f279633ce0cae6bb93df4d
Timothy D Witham <twitham@sbcglobal.net>
2023-08-15 00:24:40 -0500

    MythMusic: show altered speed by appending to [playlist]time

--- mythplugins/mythmusic/mythmusic/musiccommon.cpp
+++ mythplugins/mythmusic/mythmusic/musiccommon.cpp
@@ -2136,10 +2136,15 @@ QString MusicCommon::getTimeString(std::chrono::seconds exTime, std::chrono::sec
 {
     if (maxTime <= 0ms)
         return MythDate::formatTime(exTime,
-                              (exTime >= 1h) ? "H:mm:ss" : "mm:ss");
+                                    (exTime >= 1h) ? "H:mm:ss" : "mm:ss");
 
     QString fmt = (maxTime >= 1h) ? "H:mm:ss" : "mm:ss";
-    return MythDate::formatTime(exTime, fmt) + " / " + MythDate::formatTime(maxTime, fmt);
+    QString out = MythDate::formatTime(exTime, fmt)
+        + " / " + MythDate::formatTime(maxTime, fmt);
+    float speed = gPlayer->getSpeed();
+    if (int(speed * 100.0F + 0.5F) != 100) // v34 - show altered speed
+        out += QString(", %1").arg(speed);
+    return out;
 }
 
 void MusicCommon::searchButtonList(void)
