cddc0ab08e4a1896eb6356ed25f94930ccf20be4
Peter Bennett <pbennett@mythtv.org>
2023-11-03 21:13:01 -0400

    frontend web app: Fix direct URL for remote control
    URL http://frontend:6547/MythFE/ was not being directed to the SPA and
    was giving a 404 return code. Now fixed.
    
    Fixes #811

--- mythtv/libs/libmythbase/http/mythhttprewrite.cpp
+++ mythtv/libs/libmythbase/http/mythhttprewrite.cpp
@@ -56,7 +56,8 @@ HTTPResponse MythHTTPRewrite::RewriteToSPA(const HTTPRequest2& Request, const QS
     LOG(VB_HTTP, LOG_INFO, QString("Rewriting request to web app '%1'").arg(File));
     if (!File.isEmpty()
         && (Request->m_path.isEmpty() || Request->m_path == "/"
-            || Request->m_path == "/dashboard/" || Request->m_path == "/setupwizard/"))
+            || Request->m_path == "/dashboard/" || Request->m_path == "/setupwizard/"
+            || Request->m_path == "/MythFE/"))
     {
         Request->m_fileName = File;
         Request->m_status = HTTPOK;
