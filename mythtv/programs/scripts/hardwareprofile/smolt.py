MythTV/mythtv

commit 1df343e9ab7defa284a73390210a65cf2112f17e

Hardware Profile: Now works python2 and python3
New packages required:
    python-uritools
    python3-uritools
    python-mailer
    python3-mailer
    python-simplejson
    python3-simplejson
    python3-mysqldb # required for bindings too
    python3-future  # required for bindings too

mythtv/configure
@@ -6991,11 +6991,14 @@ fi
 
 # Check for python dependencies
 if enabled bindings_python; then
-    is_python3                 && python=python2
-    check_python               || disable_bindings_python "Python 2.6"
+    check_python               || disable_bindings_python "Python 2.6 or greater"
     check_py_lib MySQLdb       || disable_bindings_python "MySQLdb"
     check_py_lib lxml          || disable_bindings_python "lxml"
-    check_py_lib urlgrabber    || disable_bindings_python "urlgrabber"
+    check_py_lib requests      || disable_bindings_python "requests"
+    check_py_lib uritools      || disable_bindings_python "uritools"
+    check_py_lib email         || disable_bindings_python "email"
+    check_py_lib simplejson    || disable_bindings_python "simplejson"
+    check_py_lib future        || disable_bindings_python "future"
 fi
 
 # Check for perl dependencies

mythtv/programs/scripts/hardwareprofile/MultipartPostHandler.py
@@ -3,18 +3,18 @@
 
 ####
 # 02/2006 Will Holcomb <wholcomb@gmail.com>
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
-# 7/26/07 Slightly modified by Brian Schneider  
+# 7/26/07 Slightly modified by Brian Schneider
 # in order to support unicode files ( multipart_encode function )
 """
 Usage:
@@ -40,14 +40,19 @@ Further Example:
   The main function of this file is a sample which downloads a page and
   then uploads it to the W3C validator.
 """
+from __future__ import print_function
 
-import urllib
-import urllib2
-import mimetools, mimetypes
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+import urllib.request, urllib.error, urllib.parse
+from email.generator import _make_boundary
+import mimetypes
 import os, stat
-from cStringIO import StringIO
+import sys
+from io import StringIO
 
-class Callable:
+class Callable(object):
     def __init__(self, anycallable):
         self.__call__ = anycallable
 
@@ -55,45 +60,43 @@ class Callable:
 #  assigning a sequence.
 doseq = 1
 
-class MultipartPostHandler(urllib2.BaseHandler):
-    handler_order = urllib2.HTTPHandler.handler_order - 10 # needs to run first
+class MultipartPostHandler(urllib.request.BaseHandler):
+    handler_order = urllib.request.HTTPHandler.handler_order - 10 # needs to run first
 
     def http_request(self, request):
-        data = request.get_data()
+        data = request.data
         if data is not None and type(data) != str:
             v_files = []
             v_vars = []
             try:
-                 for(key, value) in data.items():
-                     if type(value) == file:
-                         v_files.append((key, value))
-                     else:
-                         v_vars.append((key, value))
+                 for(key, value) in list(data.items()):
+                     # 'file' type doesn't exist in python3 and we didn't use it. Removed if:
+                     v_vars.append((key, value))
             except TypeError:
                 systype, value, traceback = sys.exc_info()
-                raise TypeError, "not a valid non-string sequence or mapping object", traceback
+                raise TypeError("not a valid non-string sequence or mapping object", traceback)
 
             if len(v_files) == 0:
-                data = urllib.urlencode(v_vars, doseq)
+                data = urllib.parse.urlencode(v_vars, doseq)
             else:
                 boundary, data = self.multipart_encode(v_vars, v_files)
 
                 contenttype = 'multipart/form-data; boundary=%s' % boundary
                 if(request.has_header('Content-Type')
                    and request.get_header('Content-Type').find('multipart/form-data') != 0):
-                    print "Replacing %s with %s" % (request.get_header('content-type'), 'multipart/form-data')
+                    print("Replacing %s with %s" % (request.get_header('content-type'), 'multipart/form-data'))
                 request.add_unredirected_header('Content-Type', contenttype)
 
-            request.add_data(data)
-        
+            request.data = bytearray(data, 'latin1')
+
         return request
 
-    def multipart_encode(vars, files, boundary = None, buf = None):
+    def multipart_encode(self, these_vars, files, boundary = None, buf = None):
         if boundary is None:
-            boundary = mimetools.choose_boundary()
+            boundary = _make_boundary(os.getuid + os.getpid())
         if buf is None:
             buf = StringIO()
-        for(key, value) in vars:
+        for(key, value) in these_vars:
             buf.write('--%s\r\n' % boundary)
             buf.write('Content-Disposition: form-data; name="%s"' % key)
             buf.write('\r\n\r\n' + value + '\r\n')
@@ -118,7 +121,7 @@ def main():
     import tempfile, sys
 
     validatorURL = "http://validator.w3.org/check"
-    opener = urllib2.build_opener(MultipartPostHandler)
+    opener = urllib.request.build_opener(MultipartPostHandler)
 
     def validateFile(url):
         temp = tempfile.mkstemp(suffix=".html")
@@ -126,7 +129,7 @@ def main():
         params = { "ss" : "0",            # show source
                    "doctype" : "Inline",
                    "uploaded_file" : open(temp[1], "rb") }
-        print opener.open(validatorURL, params).read()
+        print(opener.open(validatorURL, params).read())
         os.remove(temp[1])
 
     if len(sys.argv[1:]) > 0:

mythtv/programs/scripts/hardwareprofile/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import os,re
-import commands
+import os, re, sys
+from subprocess import Popen, PIPE
 import os_detect
 
 SMOON_URL = "http://smolt.mythtv.org/"
@@ -10,7 +10,7 @@ SECURE = 0
 from MythTV import MythDB
 confdir = os.path.join(MythDB().dbconfig.confdir, 'HardwareProfile')
 try:
-    os.mkdir(confdir, 0700)
+    os.mkdir(confdir, 0o700)
 except OSError:
     pass
 
@@ -27,8 +27,13 @@ ADMIN_TOKEN = os.path.join(confdir, 'smolt_token')
 
 FS_T_FILTER=False
 FS_M_FILTER=True
-FS_MOUNTS=commands.getoutput('rpm -ql filesystem').split('\n')
 
+try:
+    p = Popen(['rpm', '-ql', 'filesystem'], stdout=PIPE)
+    p.wait()
+    FS_MOUNTS = p.stdout.read().strip()
+except OSError:
+    FS_MOUNTS = ''
 
 #This will attempt to find the distro.
 #Uncomment any of the OS lines below to hardcode.

mythtv/programs/scripts/hardwareprofile/deleteProfile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 # smolt - Fedora hardware profiler
 #
@@ -19,10 +19,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
+from __future__ import print_function
+from builtins import str
 import sys
-import urlgrabber.grabber
+import requests
 from optparse import OptionParser
-from urlparse import urljoin
 
 
 def main():
@@ -36,7 +37,7 @@ def main():
     def serverMessage(page):
         for line in page.split("\n"):
             if 'ServerMessage:' in line:
-                error(_('Server Message: "%s"') % line.split('ServerMessage: ')[1])
+                error(_('Server Message: "%s"' % line.split('ServerMessage: ')[1]))
                 if 'Critical' in line:
                     sys.exit(3)
 
@@ -78,10 +79,13 @@ def main():
     smolt.DEBUG = opts.DEBUG
     smolt.hw_uuid_file = opts.uuidFile
 
-    grabber = urlgrabber.grabber.URLGrabber(user_agent=opts.user_agent, timeout=opts.timeout)
-
     uuid = smolt.read_uuid()
     delHostString = 'uuid=%s' % uuid
+
+    session = requests.Session()
+    session.headers.update({'USER-AGENT': opts.user_agent})
+    session.headers.update({'Content-type': 'application/x-www-form-urlencoded'})
+    session.headers.update({'Content-length': '%i' % len(delHostString)})
 
     # Try retrieving current pub_uuid  (from cache or remotely if necessary)
     pub_uuid = None
@@ -90,25 +94,29 @@ def main():
     except PubUUIDError:
         pass
 
+    exceptions = (requests.exceptions.HTTPError,
+                  requests.exceptions.URLRequired,
+                  requests.exceptions.Timeout,
+                  requests.exceptions.ConnectionError,
+                  requests.exceptions.InvalidURL)
 
     try:
-        o=grabber.urlopen(urljoin(opts.smoonURL + '/', '/client/delete'), data=delHostString, http_headers=(
-                        ('Content-length', '%i' % len(delHostString)),
-                        ('Content-type', 'application/x-www-form-urlencoded')))
-    except urlgrabber.grabber.URLGrabError, e:
+        o = session.post(opts.smoonURL + 'client/delete', data=delHostString,
+                         timeout=opts.timeout)
+    except exceptions as e:
         sys.stderr.write(_('Error contacting Server:'))
         sys.stderr.write(str(e))
         sys.stderr.write('\n')
         sys.exit(1)
     else:
-        serverMessage(o.read())
-        o.close()
+        serverMessage(o.text)
+        session.close()
 
     if pub_uuid is None:
-        profile_url = urljoin(opts.smoonURL + '/', '/client/show?%s' % delHostString)
+        profile_url = opts.smoonURL + 'client/show?%s' % delHostString
     else:
         profile_url = get_profile_link(opts.smoonURL, pub_uuid)
-    print _('Profile removed, please verify at'), profile_url
+    print((_('Profile removed, please verify at'), profile_url))
 
 
 if __name__ == '__main__':

mythtv/programs/scripts/hardwareprofile/devicelist.py
@@ -19,6 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
+from builtins import object
 import os
 from hwdata import DeviceMap
 
@@ -92,8 +93,8 @@ DATA_LIST = [   'vendor',
                 'subsystem_vendor',
                 'subsystem_device']
 
-def cat(file):
-    fd = open(file, 'r')
+def cat(file_name):
+    fd = open(file_name, 'r')
     results = fd.readlines()
     fd.close()
     return results
@@ -160,7 +161,7 @@ class USBDevice( Device ):
             desc                =     cat(PATH +          'product')[0].strip().decode('ASCII',  errors='ignore')
         except:
             #The fedora python pkg is broken and doesn't like decode with options, so this is a fallback
-            desc                =     cat(PATH +          'product')[0].strip().decode()
+            desc                =     cat(PATH +          'product')[0].strip()
         self.description    = usb.device(self.vendorid,
                                          self.deviceid,
                                          alt=desc)

mythtv/programs/scripts/hardwareprofile/distros/all.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
 #from gentoo.main import create_gentoo
-from mythtv_data.main import create_mythtv
+from distros.mythtv_data.main import create_mythtv
 
 def get():
     #res = [create_gentoo(),create_mythtv()]

mythtv/programs/scripts/hardwareprofile/distros/distro.py
@@ -16,7 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
-class Distro:
+from builtins import object
+class Distro(object):
     def _raise_abstract(self):
         raise Exception('Function must be overwritten.')
 

commit 6348dc01a45af6ed63c09326ec94cd425db1d6d7

hardwareprofile: remove external dependency on simplejson
This uses the requests module and converts requests responses to json
using requests' own `.json()` method on responses. For incomprehensible
reasons, requests has spent about a decade using either simplejson or
the standard library's json module more or less at will, and returning
either one or the other exception types. They don't know why they use
simplejson, we don't know why they use simplejson. In requests 3 (which
will be released in the Year Of The Linux Desktop or when pigs fly,
whichever one comes later) simplejson is dropped entirely.

There are innumerable issues discussing the problem on the requests
bugtracker, with the general consensus being that it's better to
randomly return either one of two different libraries and two different
library return types in errors -- because it was historically done that
way and people might be depending on it. ??????

Bugs:

https://github.com/psf/requests/pull/710
https://github.com/psf/requests/pull/2516
https://github.com/psf/requests/issues/3052
https://github.com/psf/requests/issues/4169
https://github.com/psf/requests/issues/4842
https://github.com/psf/requests/issues/5794
https://github.com/psf/requests/issues/6084

The awkward workaround is to guarantee that requests' silent behavior of
using simplejson *if it is installed* is forcibly triggered by forcibly
depending on simplejson, and then catching the simplejson exception.

The better solution here is pretty simple: do not rely on the requests
module's automatic json conversion, this is as simple as using the
already-imported json module and calling json.loads() on the retrieved
content.

Fixes: 1df343e9ab7defa284a73390210a65cf2112f17e
Reimplements: bb154a843b737cc3ad8c1a45fa04a1a3609aff05

.github/workflows/buildmaster.yml
@@ -48,7 +48,7 @@ jobs:
         sudo apt install ccache qt5-qmake qtscript5-dev nasm libsystemd-dev libfreetype6-dev libmp3lame-dev libx264-dev libx265-dev libxrandr-dev libxml2-dev
         sudo apt install libavahi-compat-libdnssd-dev libasound2-dev liblzo2-dev libhdhomerun-dev libsamplerate0-dev libva-dev libdrm-dev libvdpau-dev
         sudo apt install libass-dev libpulse-dev libcec-dev libssl-dev libtag1-dev libbluray-dev libbluray-bdj libgnutls28-dev libqt5webkit5-dev
-        sudo apt install libvpx-dev python3-mysqldb python3-lxml python3-simplejson python3-future python3-setuptools libdbi-perl libdbd-mysql-perl libnet-upnp-perl
+        sudo apt install libvpx-dev python3-mysqldb python3-lxml python3-future python3-setuptools libdbi-perl libdbd-mysql-perl libnet-upnp-perl
         sudo apt install libio-socket-inet6-perl libxml-simple-perl libqt5sql5-mysql libwayland-dev qtbase5-private-dev libzip-dev libsoundtouch-dev
       if: runner.os == 'Linux'
 

mythtv/configure
@@ -5535,7 +5535,7 @@ if enabled bdjava; then
         java_code_version=1.4
 
         javac_version=`"${JAVAC}" -version 2>&1 | head -n 1`
-        
+
         echo "${javac_version}" | grep -E -q '^javac (9|1[0-1])' && java_code_version=1.6
         echo "${javac_version}" | grep -E -q '^javac (1[2-9])' && java_code_version=1.7
         echo "${javac_version}" | grep -E -q '^javac (2.*)' && java_code_version=1.7
@@ -6563,7 +6563,6 @@ if enabled bindings_python; then
     check_py_lib MySQLdb       || disable_bindings_python "MySQLdb"
     check_py_lib lxml          || disable_bindings_python "lxml"
     check_py_lib requests      || disable_bindings_python "requests"
-    check_py_lib simplejson    || disable_bindings_python "simplejson"
     check_py_lib future        || disable_bindings_python "future"
     check_python "(3,11,1)" && check_py_lib wheel && check_py_lib_version pip "(23,0,1)" && USE_PYTHON_PIP="yes"
 fi

mythtv/programs/scripts/hardwareprofile/smolt.py
@@ -50,7 +50,6 @@ except ImportError:
     from urlparse import urlparse
 import json
 from json import JSONEncoder
-from simplejson import errors as sje
 import datetime
 import logging
 
@@ -790,8 +789,8 @@ class _HardwareProfile:
                 sys.exit(1)
 
             try:
-                admin_obj = admin_token.json()
-            except sje.JSONDecodeError:
+                admin_obj = json.loads(admin_token.content)
+            except json.JSONDecodeError:
                 self.session.close()
                 error(_('Incorrect server response. Expected a JSON string'))
                 return (1, None, None)