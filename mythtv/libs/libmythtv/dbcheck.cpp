512d723c8374ee3cf5a1b63ce5290a71dc3beee9
Patrick J. Maloney <120519414+PatrickJMaloney@users.noreply.github.com>
2024-01-13 09:12:00 -0500

    Fix update of older MythTV databases   (#844)
    * Add method CheckTableColumnExists which checks for existence of a column in a database table.
    
    * Execute some blocks of SQL earlier in the migration process to allow older database versions to migrate properly.
    
    * Changes per PR #844 code review.
    
    * Add requested comments.

--- mythtv/libs/libmythtv/dbcheck.cpp
+++ mythtv/libs/libmythtv/dbcheck.cpp
@@ -1970,6 +1970,66 @@ static bool doUpgradeTVDatabaseSchema(void)
     if (dbver == "1301")
     {
         LOG(VB_GENERAL, LOG_CRIT, "Upgrading to MythTV schema version 1302");
+
+        // Adding the default template now requires database table recgroups which was
+        // initially introduced in the upgrade to schema version 1320.
+        // The initial database created with a "clean install" has schema version 1307
+        // so upgrading to schema version 1302 is not often done.
+        DBUpdates updates {
+            "CREATE TABLE IF NOT EXISTS recgroups ("
+                "recgroupid  SMALLINT(4) NOT NULL AUTO_INCREMENT, "
+                "recgroup    VARCHAR(64) NOT NULL DEFAULT '', "
+                "displayname VARCHAR(64) NOT NULL DEFAULT '', "
+                "password    VARCHAR(40) NOT NULL DEFAULT '', "
+                "special     TINYINT(1) NOT NULL DEFAULT '0',"
+                "PRIMARY KEY (recgroupid), "
+                "UNIQUE KEY recgroup ( recgroup )"
+                ") ENGINE=MyISAM DEFAULT CHARSET=utf8;",
+
+            // Create the built-in, 'special', groups
+            "INSERT IGNORE INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 1, 'Default', '1' );",
+            "INSERT IGNORE INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 2, 'LiveTV', '1' );",
+            "INSERT IGNORE INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 3, 'Deleted', '1' );",
+            // Copy in the passwords for the built-in groups
+            "DELETE FROM recgrouppassword WHERE password = '';",
+            "UPDATE recgroups r, recgrouppassword p SET r.password = p.password WHERE r.recgroup = p.recgroup;",
+            // Copy over all existing recording groups, this information may be split over three tables!
+            "INSERT IGNORE INTO recgroups ( recgroup, displayname, password ) SELECT DISTINCT recgroup, recgroup, password FROM recgrouppassword;",
+            "INSERT IGNORE INTO recgroups ( recgroup, displayname ) SELECT DISTINCT recgroup, recgroup FROM record;",
+            "INSERT IGNORE INTO recgroups ( recgroup, displayname ) SELECT DISTINCT recgroup, recgroup FROM recorded;",
+
+            // Create recgroupid columns in record and recorded tables
+            (!DBUtil::CheckTableColumnExists(QString("record"), QString("recgroupid")) ?
+                 "ALTER TABLE record ADD COLUMN recgroupid SMALLINT(4) NOT NULL DEFAULT '1', ADD INDEX ( recgroupid );" : ""),
+            (!DBUtil::CheckTableColumnExists(QString("recorded"), QString("recgroupid")) ?
+                "ALTER TABLE recorded ADD COLUMN recgroupid SMALLINT(4) NOT NULL DEFAULT '1', ADD INDEX ( recgroupid );" : ""),
+
+            // Create autoextend column in record
+            (!DBUtil::CheckTableColumnExists(QString("record"), QString("autoextend")) ?
+                "ALTER TABLE record ADD COLUMN autoextend TINYINT UNSIGNED DEFAULT 0;" : ""),
+
+            "ALTER TABLE record MODIFY COLUMN startdate DATE DEFAULT NULL",
+            "ALTER TABLE record MODIFY COLUMN enddate DATE DEFAULT NULL",
+            "ALTER TABLE record MODIFY COLUMN starttime TIME DEFAULT NULL",
+            "ALTER TABLE record MODIFY COLUMN endtime TIME DEFAULT NULL",
+
+            // All next_record, last_record and last_delete to be NULL.
+            "ALTER TABLE record MODIFY next_record DATETIME NULL",
+            // NOLINTNEXTLINE(bugprone-suspicious-missing-comma)
+            "UPDATE record SET next_record = NULL "
+            "    WHERE next_record = '0000-00-00 00:00:00'",
+            "ALTER TABLE record MODIFY last_record DATETIME NULL",
+            "UPDATE record SET last_record = NULL "
+            "    WHERE last_record = '0000-00-00 00:00:00'",
+            "ALTER TABLE record MODIFY last_delete DATETIME NULL",
+            "UPDATE record SET last_delete = NULL "
+            "    WHERE last_delete = '0000-00-00 00:00:00'",
+        };
+
+        // We don't want to update version yet
+        if (!performUpdateSeries("MythTV", updates))
+            return false;
+
         // Create the Default recording rule template
         RecordingRule record;
         record.MakeTemplate("Default");
@@ -2003,8 +2063,10 @@ static bool doUpgradeTVDatabaseSchema(void)
             gCoreContext->GetBoolSetting("AutoRunUserJob4", false);
         record.m_autoMetadataLookup =
             gCoreContext->GetBoolSetting("AutoMetadataLookup", true);
-        record.Save(false);
+        if (!record.Save(false))
+            return false;
 
+        // Both updates worked, so update version
         if (!UpdateDBVersionNumber("MythTV", "DBSchemaVer", "1302", dbver))
             return false;
     }
@@ -2511,9 +2573,9 @@ static bool doUpgradeTVDatabaseSchema(void)
                 "UNIQUE KEY recgroup ( recgroup )"
                 ") ENGINE=MyISAM DEFAULT CHARSET=utf8;",
             // Create the built-in, 'special', groups
-            "INSERT INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 1, 'Default', '1' );",
-            "INSERT INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 2, 'LiveTV', '1' );",
-            "INSERT INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 3, 'Deleted', '1' );",
+            "INSERT IGNORE INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 1, 'Default', '1' );",
+            "INSERT IGNORE INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 2, 'LiveTV', '1' );",
+            "INSERT IGNORE INTO recgroups ( recgroupid, recgroup, special ) VALUES ( 3, 'Deleted', '1' );",
             // Copy in the passwords for the built-in groups
             "DELETE FROM recgrouppassword WHERE password = '';",
             "UPDATE recgroups r, recgrouppassword p SET r.password = p.password WHERE r.recgroup = p.recgroup;",
@@ -2522,8 +2584,10 @@ static bool doUpgradeTVDatabaseSchema(void)
             "INSERT IGNORE INTO recgroups ( recgroup, displayname ) SELECT DISTINCT recgroup, recgroup FROM record;",
             "INSERT IGNORE INTO recgroups ( recgroup, displayname ) SELECT DISTINCT recgroup, recgroup FROM recorded;",
             // Create recgroupid columns in record and recorded tables
-            "ALTER TABLE record ADD COLUMN recgroupid SMALLINT(4) NOT NULL DEFAULT '1', ADD INDEX ( recgroupid );",
-            "ALTER TABLE recorded ADD COLUMN recgroupid SMALLINT(4) NOT NULL DEFAULT '1', ADD INDEX ( recgroupid );",
+            (!DBUtil::CheckTableColumnExists(QString("record"), QString("recgroupid")) ?
+                 "ALTER TABLE record ADD COLUMN recgroupid SMALLINT(4) NOT NULL DEFAULT '1', ADD INDEX ( recgroupid );" : ""),
+            (!DBUtil::CheckTableColumnExists(QString("recorded"), QString("recgroupid")) ?
+                "ALTER TABLE recorded ADD COLUMN recgroupid SMALLINT(4) NOT NULL DEFAULT '1', ADD INDEX ( recgroupid );" : ""),
             // Populate those columns with the corresponding recgroupid from the new recgroups table
             "UPDATE recorded, recgroups SET recorded.recgroupid = recgroups.recgroupid WHERE recorded.recgroup = recgroups.recgroup;",
             "UPDATE record, recgroups SET record.recgroupid = recgroups.recgroupid WHERE record.recgroup = recgroups.recgroup;"
@@ -2696,6 +2760,20 @@ static bool doUpgradeTVDatabaseSchema(void)
 
     if (dbver == "1330")
     {
+        // Adding field lastplay because it is now required in the upgrade to database schema 1331
+        // but only when the are recordings present in the database.
+        // Field lastplay is initially introduced in the upgrade to database schema 1373.
+        if (!DBUtil::CheckTableColumnExists(QString("recorded"), QString("lastplay")))
+        {
+            DBUpdates updates {
+                "ALTER TABLE recorded ADD COLUMN lastplay TINYINT UNSIGNED DEFAULT 0 AFTER bookmark;",
+            };
+
+            // We don't want to update version yet
+            if (!performUpdateSeries("MythTV", updates))
+                return false;
+        }
+
         MSqlQuery query(MSqlQuery::InitCon());
         query.prepare("SELECT recordedid FROM recorded");
         query.exec();
@@ -2729,10 +2807,13 @@ static bool doUpgradeTVDatabaseSchema(void)
                             recInfo->QueryAverageHeight());
             recFile->m_videoResolution = resolution;
             recFile->m_videoFrameRate = (double)recInfo->QueryAverageFrameRate() / 1000.0;
-            recFile->Save();
+            bool result = recFile->Save();
             delete recInfo;
+            if (!result)
+                return false;
         }
 
+        // Both updates worked, so update version
         if (!UpdateDBVersionNumber("MythTV", "DBSchemaVer", "1331", dbver))
             return false;
     }
@@ -3867,22 +3948,38 @@ static bool doUpgradeTVDatabaseSchema(void)
     {
         // Recording extender tables are now created later.
 
-        // If that worked, modify existing tables.
-        DBUpdates updates = getRecordingExtenderDbInfo(0);
-        if (!performActualUpdate("MythTV", "DBSchemaVer",
-                                 updates, "1372", dbver))
-            return false;
+        if (!DBUtil::CheckTableColumnExists(QString("record"), QString("autoextend")))
+        {
+            // If that worked, modify existing tables.
+            DBUpdates updates = getRecordingExtenderDbInfo(0);
+            if (!performActualUpdate("MythTV", "DBSchemaVer",
+                                     updates, "1372", dbver))
+                return false;
+        }
+        else
+        {
+            if (!UpdateDBVersionNumber("MythTV", "DBSchemaVer", "1372", dbver))
+                return false;
+        }
     }
 
     if (dbver == "1372")
     {
-        DBUpdates updates {
-            "ALTER TABLE recorded ADD COLUMN lastplay "
-            "    TINYINT UNSIGNED DEFAULT 0 AFTER bookmark;",
-        };
-        if (!performActualUpdate("MythTV", "DBSchemaVer",
-                                 updates, "1373", dbver))
-            return false;
+        if (!DBUtil::CheckTableColumnExists(QString("recorded"), QString("lastplay")))
+        {
+            DBUpdates updates {
+                "ALTER TABLE recorded ADD COLUMN lastplay "
+                "    TINYINT UNSIGNED DEFAULT 0 AFTER bookmark;",
+            };
+            if (!performActualUpdate("MythTV", "DBSchemaVer",
+                                     updates, "1373", dbver))
+                return false;
+        }
+        else
+        {
+            if (!UpdateDBVersionNumber("MythTV", "DBSchemaVer", "1373", dbver))
+                return false;
+        }
     }
 
     if (dbver == "1373")
