256b1dcccc8e706214cdb98c541388ae9bb8418f
Scott Theisen <scott.the.elm@gmail.com>
2024-06-22 21:49:07 -0400

    BitReader::refill_cache(): m_cache is uint64_t, so match the type cast with it

--- mythtv/libs/libmythtv/bitreader.h
+++ mythtv/libs/libmythtv/bitreader.h
@@ -248,14 +248,14 @@ inline void BitReader::refill_cache(unsigned min_bits)
         int bits = CHAR_BIT - m_bitIndex;
         if (shift > bits)
         {
-            m_cache |= int64_t(*m_buffer & ((1 << bits) - 1)) << (shift - bits);
+            m_cache |= static_cast<uint64_t>(*m_buffer & ((1 << bits) - 1)) << (shift - bits);
             m_bitIndex   = 0;
             m_buffer++;
             m_cacheSize += bits;
         }
         else
         {
-            m_cache |= int64_t(*m_buffer & ((1 << bits) - 1)) >> (bits - shift);
+            m_cache |= static_cast<uint64_t>(*m_buffer & ((1 << bits) - 1)) >> (bits - shift);
             m_bitIndex  += shift;
             m_cacheSize += shift;
         }
