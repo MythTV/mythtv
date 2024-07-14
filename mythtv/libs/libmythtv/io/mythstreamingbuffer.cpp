447499922051834d4226ac9f7e9b3364d4c85226
Scott Theisen <scott.the.elm@gmail.com>
2024-07-13 21:37:54 -0400

    convert use of internal libavformat/url.h to public libavformat/avio.h (part 2)

--- mythtv/libs/libmythtv/io/mythstreamingbuffer.cpp
+++ mythtv/libs/libmythtv/io/mythstreamingbuffer.cpp
@@ -1,10 +1,16 @@
+#include "io/mythstreamingbuffer.h"
+
 // Qt
 #include <QUrl>
 
 // MythTV
 #include "libmythbase/mythcorecontext.h"
 #include "libmythbase/mythlogging.h"
-#include "io/mythstreamingbuffer.h"
+
+// FFmpeg
+extern "C" {
+#include "libavformat/avio.h"
+}
 
 #define LOC QString("StreamRingBuf(%1): ").arg(m_filename)
 
@@ -21,7 +27,7 @@ MythStreamingBuffer::~MythStreamingBuffer()
 
     m_rwLock.lockForWrite();
     if (m_context)
-        ffurl_close(m_context);
+        avio_closep(&m_context);
     m_rwLock.unlock();
 }
 
@@ -61,23 +67,21 @@ bool MythStreamingBuffer::OpenFile(const QString &Filename, std::chrono::millise
     if (url.path().endsWith(QLatin1String("m3u8"), Qt::CaseInsensitive))
         url.setScheme("hls+http");
 
-    int res = ffurl_open_whitelist(&m_context, url.toString().toLatin1(), AVIO_FLAG_READ, nullptr, nullptr, nullptr, nullptr, nullptr);
-    if (res >= 0 && m_context && !m_context->is_streamed && ffurl_seek(m_context, 0, SEEK_SET) >= 0)
-    {
-        m_streamed   = false;
-        m_allowSeeks = true;
-    }
-
-    LOG(VB_GENERAL, LOG_INFO, LOC + QString("Trying %1 (allow seeks: %2")
-        .arg(m_filename).arg(m_allowSeeks));
-
+    int res = avio_open(&m_context, url.toString().toLatin1(), AVIO_FLAG_READ);
     if (res < 0 || !m_context)
     {
-        LOG(VB_GENERAL, LOG_ERR, LOC + QString("Failed to open stream (error %1)") .arg(res));
+        LOG(VB_GENERAL, LOG_ERR, LOC + QString("Failed to open stream (%1) (error %2)")
+            .arg(m_filename, QString::number(res)));
         m_lastError = QObject::tr("Failed to open stream (%1)").arg(res);
         m_rwLock.unlock();
         return false;
     }
+
+    m_streamed   = (m_context->seekable & AVIO_SEEKABLE_NORMAL) == 0;
+    m_allowSeeks = !m_streamed && avio_seek(m_context, 0, SEEK_SET) >= 0;
+
+    LOG(VB_GENERAL, LOG_INFO, LOC + QString("Opened %1 (allow seeks: %2")
+        .arg(m_filename, QVariant(m_allowSeeks).toString()));
 
     m_rwLock.unlock();
     return true;
@@ -89,7 +93,7 @@ long long MythStreamingBuffer::SeekInternal(long long Position, int Whence)
         return 0;
 
     m_posLock.lockForWrite();
-    int seek = static_cast<int>(ffurl_seek(m_context, Position, Whence));
+    int64_t seek = avio_seek(m_context, Position, Whence);
     m_posLock.unlock();
 
     if (seek < 0)
@@ -97,7 +101,7 @@ long long MythStreamingBuffer::SeekInternal(long long Position, int Whence)
         m_ateof = true;
         return 0;
     }
-    return Position;
+    return seek;
 }
 
 int MythStreamingBuffer::SafeRead(void *Buffer, uint Size)
@@ -108,7 +112,7 @@ int MythStreamingBuffer::SafeRead(void *Buffer, uint Size)
     {
         while (len < static_cast<int>(Size))
         {
-            int ret = ffurl_read(m_context, static_cast<unsigned char*>(Buffer) + len,
+            int ret = avio_read(m_context, static_cast<unsigned char*>(Buffer) + len,
                                  static_cast<int>(Size) - len);
             if (ret < 0)
             {
@@ -130,7 +134,7 @@ long long MythStreamingBuffer::GetRealFileSizeInternal(void) const
     long long result = -1;
     m_rwLock.lockForRead();
     if (m_context)
-        result = ffurl_size(m_context);
+        result = avio_size(m_context);
     m_rwLock.unlock();
     return result;
 }
