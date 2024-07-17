08d10cef5005fba76cf61e0c1251b88ef0373753
Scott Theisen <scott.the.elm@gmail.com>
2024-07-16 20:09:14 -0400

    libavformat.v: remove now unused added exports

--- mythtv/external/FFmpeg/libavformat/libavformat.v
+++ mythtv/external/FFmpeg/libavformat/libavformat.v
@@ -1,12 +1,6 @@
 LIBAVFORMAT_MAJOR {
     global:
         av*;
-        ffurl_read;
-        ffurl_seek;
-        ffurl_size;
-        ffurl_open_whitelist;
-        ffurl_close;
-        ffurl_write;
     local:
         *;
 };
