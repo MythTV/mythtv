6a944276771354b80849abccf294eb1944679210
David Hampton <mythtv@love2code.net>
2024-07-27 00:21:59 -0400

    tidy: Quiet 'macro to enum warnings' in idct.c.
    This set of defines looks like it could be an enum, but its actually
    seven constants with similar names.  Disable this warning.
    
    https://clang.llvm.org/extra/clang-tidy/checks/modernize/macro-to-enum.html

--- mythtv/libs/libmythmpeg2/idct.c
+++ mythtv/libs/libmythmpeg2/idct.c
@@ -30,12 +30,14 @@
 #include "attributes.h"
 #include "mpeg2_internal.h"
 
+/* NOLINTBEGIN(modernize-macro-to-enum) */
 #define W1 2841 /* 2048 * sqrt (2) * cos (1 * pi / 16) */
 #define W2 2676 /* 2048 * sqrt (2) * cos (2 * pi / 16) */
 #define W3 2408 /* 2048 * sqrt (2) * cos (3 * pi / 16) */
 #define W5 1609 /* 2048 * sqrt (2) * cos (5 * pi / 16) */
 #define W6 1108 /* 2048 * sqrt (2) * cos (6 * pi / 16) */
 #define W7 565  /* 2048 * sqrt (2) * cos (7 * pi / 16) */
+/* NOLINTEND(modernize-macro-to-enum) */
 
 /* idct main entry point  */
 void (* mpeg2_idct_copy) (int16_t * block, uint8_t * dest, int stride);
