55e67f1d7ffe01526d57d872e4c94780535eca3b
David Hampton <mythtv@love2code.net>
2024-08-07 11:06:48 -0400

    tidy: Fix several undefined binary operator warnings.
    There are several functions in this file that build an integer from a
    series of bytes.  These functions need to explicitly expand the size
    of the variable before shifting.  Whether or not shifting before
    expanding works is compiler dependent.
    
    https://clang.llvm.org/extra/clang-tidy/checks/clang-analyzer/core.UndefinedBinaryOperatorResult.html

--- mythplugins/mytharchive/mytharchivehelper/external/pxsup2dast.c
+++ mythplugins/mytharchive/mytharchivehelper/external/pxsup2dast.c
@@ -297,22 +297,30 @@ static bool dexists(const char * filename)
 
 static fu32 get_uint32_be(const eu8 * bytes)  
 {
-    return (bytes[0] << 24) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3]; 
+    return ((fu32)(bytes[0]) << 24) +
+           ((fu32)(bytes[1]) << 16) +
+           ((fu32)(bytes[2]) << 8) +
+            (fu32)(bytes[3]);
 }
 
 static fu16 get_uint16_be(const eu8 * bytes)
 {
-    return (bytes[0] << 8) + bytes[1]; 
+    return ((fu16)(bytes[0]) << 8) +
+            (fu16)(bytes[1]);
 }
 
 static fu32 get_uint32_le(const eu8 * bytes)  
 {
-    return (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0]; 
+    return ((fu32)(bytes[3]) << 24) +
+           ((fu32)(bytes[2]) << 16) +
+           ((fu32)(bytes[1]) << 8) +
+            (fu32)(bytes[0]);
 }
 
 #if 0  /* protoline */
 static fu32 get_uint16_le(const eu8 * bytes)  {
-    return (bytes[1] << 8) + bytes[0]; }
+    return ((fu16)(bytes[1]) << 8) +
+            (fu16)(bytes[0]); }
 #endif  /* protoline */
 
 
