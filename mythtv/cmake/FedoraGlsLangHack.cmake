b0dc874ebbf58545e804a8b6686e62dc52ea5d53
David Hampton <mythtv@love2code.net>
2024-09-17 18:01:35 -0400

    cmake: Fedora has released a fix for the glslang issues.
    The linking problem introduced in Fedora 39 has been fixed.  Remove
    the workaround, and inform users to upgrade to upgrade their
    distribution package.

--- mythtv/cmake/FedoraGlsLangHack.cmake
+++ mythtv/cmake/FedoraGlsLangHack.cmake
@@ -25,24 +25,26 @@ if(LSB_RELEASE_VERSION_ID LESS_EQUAL 38)
                     COMMAND_ERROR_IS_FATAL ANY)
     endif()
   endforeach()
-else()
+else(LSB_RELEASE_VERSION_ID LESS_EQUAL 41)
   # Fedora fixed the above error in version 39, but they introduced a new one.
   # The version of glslang shipped in this versions no longer contains the HLSL
   # or OGLCompiler libraries, but those libraries are still listed in the
   # pkg-config file as libraries that need to be linked.
   # https://bugzilla.redhat.com/show_bug.cgi?id=2308904
-  file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/share/pkgconfig)
-  foreach(_name IN ITEMS glslang spirv)
-    find_file(
-      _pc_${_name} ${_name}.pc
-      PATHS /usr/lib64/pkgconfig /usr/lib/pkgconfig /usr/share/pkgconfig
-            /usr/lib/x86_64-linux-gnu/pkgconfig
-            /usr/lib64/x86_64-linux-gnu/pkgconfig)
-    if(EXISTS ${_pc_${_name}})
-      execute_process(
-        COMMAND sed "-e" "s/-lHLSL -lOGLCompiler //g" "${_pc_${_name}}"
-        OUTPUT_FILE ${PROJECT_BINARY_DIR}/share/pkgconfig/${_name}.pc
-                    COMMAND_ERROR_IS_FATAL ANY)
+  find_file(
+    _pc_glslang glslang.pc
+    PATHS /usr/lib64/pkgconfig /usr/lib/pkgconfig /usr/share/pkgconfig
+          /usr/lib/x86_64-linux-gnu/pkgconfig
+          /usr/lib64/x86_64-linux-gnu/pkgconfig)
+  if(EXISTS ${_pc_glslang})
+    file(READ ${_pc_glslang} glslang_content)
+    if("${glslang_content}" MATCHES ".*HLSL.*")
+      if(LSB_RELEASE_VERSION_ID EQUAL 39)
+        set(_fix "14.0.0-2")
+      else()
+        set(_fix "14.3.0-2")
+      endif()
+      message(FATAL_ERROR "Please update glslang-devel to ${_fix} or later.")
     endif()
-  endforeach()
+  endif()
 endif()
