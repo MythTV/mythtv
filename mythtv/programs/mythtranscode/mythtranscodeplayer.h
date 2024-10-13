e3558dfbb22567a4358d74e2701790789ac68b99
Scott Theisen <scott.the.elm@gmail.com>
2024-10-13 19:07:45 -0400

    remove unused MythTranscodePlayer::m_rawAudio and m_rawVideo

--- mythtv/programs/mythtranscode/mythtranscodeplayer.h
+++ mythtv/programs/mythtranscode/mythtranscodeplayer.h
@@ -10,19 +10,9 @@ class MythTranscodePlayer : public MythPlayer
     explicit MythTranscodePlayer(PlayerContext* Context, PlayerFlags Flags = kNoFlags);
 
     void SetTranscoding        (bool Transcoding);
-    void InitForTranscode      (bool CopyAudio, bool CopyVideo);
+    void InitForTranscode      ();
     void SetCutList            (const frm_dir_map_t& CutList);
     bool TranscodeGetNextFrame (int& DidFF, bool& KeyFrame, bool HonorCutList);
-
-    void SetRawAudioState(bool state) { m_rawAudio = state; }
-    bool GetRawAudioState() const     { return m_rawAudio ; }
-
-    void SetRawVideoState(bool state) { m_rawVideo = state; }
-    bool GetRawVideoState() const     { return m_rawVideo; }
-
-  private:
-    bool m_rawAudio {false};
-    bool m_rawVideo {false};
 };
 
 #endif
