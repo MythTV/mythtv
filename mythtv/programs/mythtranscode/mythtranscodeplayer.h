e22097945eca2f5c412a9daed808c2e24f10f365
Scott Theisen <scott.the.elm@gmail.com>
2024-10-13 17:20:34 -0400

    remove DecoderBase::m_getRawFrames and m_getRawVideo
    They were only used by MythTranscodePlayer so move them there.

--- mythtv/programs/mythtranscode/mythtranscodeplayer.h
+++ mythtv/programs/mythtranscode/mythtranscodeplayer.h
@@ -15,6 +15,16 @@ class MythTranscodePlayer : public MythPlayer
     bool TranscodeGetNextFrame (int& DidFF, bool& KeyFrame, bool HonorCutList);
     bool WriteStoredData       (MythMediaBuffer* OutBuffer, bool WriteVideo, std::chrono::milliseconds TimecodeOffset);
     long UpdateStoredFrameNum  (long CurrentFrameNum);
+
+    void SetRawAudioState(bool state) { m_rawAudio = state; }
+    bool GetRawAudioState() const     { return m_rawAudio ; }
+
+    void SetRawVideoState(bool state) { m_rawVideo = state; }
+    bool GetRawVideoState() const     { return m_rawVideo; }
+
+  private:
+    bool m_rawAudio {false};
+    bool m_rawVideo {false};
 };
 
 #endif
