6c4c2684e2524f0251823f427f96cc66f5f7d78c
Scott Theisen <scott.the.elm@gmail.com>
2024-10-14 21:53:55 -0400

    remove support for LIBAVFORMAT_VERSION_MAJOR < 61

--- mythtv/libs/libmyth/audio/spdifencoder.cpp
+++ mythtv/libs/libmyth/audio/spdifencoder.cpp
@@ -183,11 +183,7 @@ bool SPDIFEncoder::SetMaxHDRate(int rate)
 /**
  * funcIO: Internal callback function that will receive encoded frames
  */
-#if (LIBAVFORMAT_VERSION_MAJOR < 61)
-int SPDIFEncoder::funcIO(void *opaque, unsigned char *buf, int size)
-#else
 int SPDIFEncoder::funcIO(void *opaque, const uint8_t *buf, int size)
-#endif
 {
     auto *enc = static_cast<SPDIFEncoder *>(opaque);
 
