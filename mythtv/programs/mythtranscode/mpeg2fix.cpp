MythTV/mythtv

commit d4e6b3cabd159421ea5431bb8fdc7dd3e0588f77

Replace custom aligned allocation function with the C standard function.
The C11 standard introduced the aligned_alloc() function.  Replace
older custom alignment code with this standard function.

mythtv/programs/mythtranscode/mpeg2fix.cpp
@@ -49,17 +49,12 @@ static void *my_malloc(unsigned size, [[maybe_unused]] mpeg2_alloc_t reason)
 {
     if (size)
     {
-        char *buf = (char *) malloc (size + 63 + sizeof (void **));
-        if (buf)
-        {
-            memset(buf, 0, size + 63 + sizeof (void **));
-            char *align_buf = buf + 63 + sizeof (void **);
-            align_buf -= (long)align_buf & 63;
-            *(((void **)align_buf) - 1) = buf;
-            return align_buf;
-        }
+#ifdef _WIN32
+        return _aligned_malloc( 64, size );
+#else
+        return aligned_alloc( 64, size );
+#endif
     }
-
     return nullptr;
 }
 

commit 1c75c6ad7b152a9d030321975cff334786e3c56a

Fix crashes in lossless transcode.
An attempt to simplify code in d4e6b3cabd caused mythtranscode to
start crashing.  Thanks to Scott Theisen for discovering that the
function being modified is completely unnecessary, so just remove the
entire function.

Fixes #952.

mythtv/programs/mythtranscode/mpeg2fix.cpp
@@ -45,19 +45,6 @@ extern "C" {
 #define O_LARGEFILE 0
 #endif
 
-static void *my_malloc(unsigned size, [[maybe_unused]] mpeg2_alloc_t reason)
-{
-    if (size)
-    {
-#ifdef _WIN32
-        return _aligned_malloc( 64, size );
-#else
-        return aligned_alloc( 64, size );
-#endif
-    }
-    return nullptr;
-}
-
 static void my_av_print([[maybe_unused]] void *ptr,
                         int level, const char* fmt, va_list vl)
 {
@@ -266,7 +253,6 @@ MPEG2fixup::MPEG2fixup(const QString &inf, const QString &outf,
         m_useSecondary = true;
     }
 
-    mpeg2_malloc_hooks(my_malloc, nullptr);
     m_headerDecoder = mpeg2_init();
     m_imgDecoder = mpeg2_init();
 