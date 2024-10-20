MythTV/mythtv

commit 728caf282f09f52c5dc388bea0098e7c15d6eb71

tidy: Move assignments to constructor member initialization list. (libs 2)
Promote member initialization by assignment in the body of the
constructor to be part of the constructor initialization list.  If
possible, further promote these values to default member initializers.

Changes made by clang-tidy.  White space cleanup and some reordering
by hand.

https://clang.llvm.org/extra/clang-tidy/checks/cppcoreguidelines/prefer-member-initializer.html
https://clang.llvm.org/extra/clang-tidy/checks/modernize/use-default-member-init.html

mythtv/libs/libmythtv/decoders/mythvdpauhelper.cpp
@@ -234,10 +234,10 @@ static void vdpau_preemption_callback(VdpDevice /*unused*/, void* Opaque)
  * \brief A simple wrapper around VDPAU functionality.
 */
 MythVDPAUHelper::MythVDPAUHelper(AVVDPAUDeviceContext* Context)
-  : m_device(Context->device),
+  : m_valid(InitProcs()),
+    m_device(Context->device),
     m_vdpGetProcAddress(Context->get_proc_address)
 {
-    m_valid = InitProcs();
     if (m_valid)
     {
         INIT_ST
@@ -254,9 +254,9 @@ static const char* DummyGetError(VdpStatus /*status*/)
 }
 
 MythVDPAUHelper::MythVDPAUHelper(void)
-  : m_createdDevice(true)
+  : m_createdDevice(true),
+    m_display(MythXDisplay::OpenMythXDisplay(false))
 {
-    m_display = MythXDisplay::OpenMythXDisplay(false);
     if (!m_display)
         return;
 

mythtv/libs/libmythtv/AirPlay/mythraopconnection.cpp
@@ -77,15 +77,16 @@ MythRAOPConnection::MythRAOPConnection(QObject *parent, QTcpSocket *socket,
   : QObject(parent),
     m_socket(socket),
     m_hardwareId(std::move(id)),
-    m_dataPort(port)
+    m_dataPort(port),
+    m_cctx(EVP_CIPHER_CTX_new()),
+    m_id(GetNotificationCenter()->Register(this))
 {
-    m_id = GetNotificationCenter()->Register(this);
 #if OPENSSL_VERSION_NUMBER < 0x030000000L
     m_cipher = EVP_aes_128_cbc();
 #else
+    //NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
     m_cipher = EVP_CIPHER_fetch(nullptr, "AES-128-CBC", nullptr);
 #endif
-    m_cctx = EVP_CIPHER_CTX_new();
 }
 
 MythRAOPConnection::~MythRAOPConnection()

mythtv/libs/libmythtv/Bluray/mythbdbuffer.cpp
@@ -78,10 +78,10 @@ static int BDRead(void *Handle, void *Buf, int LBA, int NumBlocks)
 
 MythBDBuffer::MythBDBuffer(const QString &Filename)
   : MythOpticalBuffer(kMythBufferBD),
-    m_overlayPlanes(2, nullptr)
+    m_tryHDMVNavigation(qEnvironmentVariableIsSet("MYTHTV_HDMV")),
+    m_overlayPlanes(2, nullptr),
+    m_mainThread(QThread::currentThread())
 {
-    m_tryHDMVNavigation = qEnvironmentVariableIsSet("MYTHTV_HDMV");
-    m_mainThread = QThread::currentThread();
     MythBDBuffer::OpenFile(Filename);
 }
 

mythtv/libs/libmythtv/HLS/httplivestreambuffer.cpp
@@ -128,6 +128,7 @@ class HLSSegment
         m_url(std::move(uri))
     {
 #ifdef USING_LIBCRYPTO
+        //NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
         m_pszKeyPath = std::move(current_key_path);
 #endif
     }

mythtv/libs/libmythtv/captions/cc608reader.cpp
@@ -5,9 +5,9 @@
 #include "recorders/vbitext/vbi.h"
 
 CC608Reader::CC608Reader(MythPlayer *parent)
-  : m_parent(parent)
+  : m_parent(parent),
+    m_maxTextSize(8 * (sizeof(teletextsubtitle) + VT_WIDTH))
 {
-    m_maxTextSize = 8 * (sizeof(teletextsubtitle) + VT_WIDTH);
     for (int i = 0; i < MAXTBUFFER; i++)
         m_inputBuffers[i].buffer = new unsigned char[m_maxTextSize + 1];
 }

mythtv/libs/libmythtv/captions/textsubtitleparser.cpp
@@ -172,9 +172,11 @@ struct local_buffer_t {
 };
 
 TextSubtitleParser::TextSubtitleParser(SubtitleReader *parent, QString fileName, TextSubtitles *target)
-    : m_parent(parent), m_target(target), m_fileName(std::move(fileName))
+    : m_parent(parent),
+      m_target(target),
+      m_fileName(std::move(fileName)),
+      m_pkt(av_packet_alloc())
 {
-    m_pkt = av_packet_alloc();
 }
 
 TextSubtitleParser::~TextSubtitleParser()

mythtv/libs/libmythtv/decoders/avformatdecoder.cpp
@@ -379,18 +379,18 @@ AvFormatDecoder::AvFormatDecoder(MythPlayer *parent,
       // Closed Caption & Teletext decoders
       m_ccd608(new CC608Decoder(parent->GetCC608Reader())),
       m_ccd708(new CC708Decoder(parent->GetCC708Reader())),
-      m_ttd(new TeletextDecoder(parent->GetTeletextReader()))
+      m_ttd(new TeletextDecoder(parent->GetTeletextReader())),
+      m_itv(parent->GetInteractiveTV()),
+      m_audioSamples((uint8_t *)av_mallocz(AudioOutput::kMaxSizeBuffer))
 {
     // this will be deleted and recreated once decoder is set up
     m_mythCodecCtx = new MythCodecContext(this, kCodec_NONE);
 
-    m_audioSamples = (uint8_t *)av_mallocz(AudioOutput::kMaxSizeBuffer);
     m_ccd608->SetIgnoreTimecode(true);
 
     av_log_set_callback(myth_av_log);
 
     m_audioIn.m_sampleSize = -32;// force SetupAudioStream to run once
-    m_itv = m_parent->GetInteractiveTV();
 
     AvFormatDecoder::SetIdrOnlyKeyframes(true);
     m_audioReadAhead = gCoreContext->GetDurSetting<std::chrono::milliseconds>("AudioReadAhead", 100ms);

mythtv/libs/libmythtv/diseqcsettings.cpp
@@ -253,18 +253,18 @@ class SwitchPortsSetting : public TransTextEditSetting
 //////////////////////////////////////// SwitchConfig
 
 SwitchConfig::SwitchConfig(DiSEqCDevSwitch &switch_dev, StandardSetting *parent)
+  : m_deviceDescr(new DeviceDescrSetting(switch_dev)),
+    m_type(new SwitchTypeSetting(switch_dev)),
+    m_ports(new SwitchPortsSetting(switch_dev)),
+    m_address(new SwitchAddressSetting(switch_dev))
 {
     setLabel(DeviceTree::tr("Switch Configuration"));
 
     parent = this;
-    m_deviceDescr = new DeviceDescrSetting(switch_dev);
     parent->addChild(m_deviceDescr);
     parent->addChild(new DeviceRepeatSetting(switch_dev));
-    m_type = new SwitchTypeSetting(switch_dev);
     parent->addChild(m_type);
-    m_address = new SwitchAddressSetting(switch_dev);
     parent->addChild(m_address);
-    m_ports = new SwitchPortsSetting(switch_dev);
     parent->addChild(m_ports);
 
     connect(m_type, qOverload<const QString&>(&StandardSetting::valueChanged),
@@ -541,7 +541,8 @@ void RotorPosMap::PopulateList(void)
 //////////////////////////////////////// RotorConfig
 
 RotorConfig::RotorConfig(DiSEqCDevRotor &rotor, StandardSetting *parent)
-    : m_rotor(rotor)
+    : m_rotor(rotor),
+      m_pos(new RotorPosMap(rotor))
 {
     setLabel(DeviceTree::tr("Rotor Configuration"));
     setValue(rotor.GetDescription());
@@ -555,7 +556,6 @@ RotorConfig::RotorConfig(DiSEqCDevRotor &rotor, StandardSetting *parent)
             this,  &RotorConfig::SetType);
     parent->addChild(rtype);
 
-    m_pos = new RotorPosMap(rotor);
     m_pos->setLabel(DeviceTree::tr("Positions"));
     m_pos->setHelpText(DeviceTree::tr("Rotor position setup."));
     parent->addChild(m_pos);
@@ -920,6 +920,12 @@ class LNBPolarityInvertedSetting : public MythUICheckBoxSetting
 //////////////////////////////////////// LNBConfig
 
 LNBConfig::LNBConfig(DiSEqCDevLNB &lnb, StandardSetting *parent)
+  : m_preset(new LNBPresetSetting(lnb)),
+    m_type(new LNBTypeSetting(lnb)),
+    m_lofSwitch(new LNBLOFSwitchSetting(lnb)),
+    m_lofLo(new LNBLOFLowSetting(lnb)),
+    m_lofHi(new LNBLOFHighSetting(lnb)),
+    m_polInv(new LNBPolarityInvertedSetting(lnb))
 {
     setLabel(DeviceTree::tr("LNB Configuration"));
     setValue(lnb.GetDescription());
@@ -927,17 +933,11 @@ LNBConfig::LNBConfig(DiSEqCDevLNB &lnb, StandardSetting *parent)
     parent = this;
     auto *deviceDescr = new DeviceDescrSetting(lnb);
     parent->addChild(deviceDescr);
-    m_preset = new LNBPresetSetting(lnb);
     parent->addChild(m_preset);
-    m_type = new LNBTypeSetting(lnb);
     parent->addChild(m_type);
-    m_lofSwitch = new LNBLOFSwitchSetting(lnb);
     parent->addChild(m_lofSwitch);
-    m_lofLo = new LNBLOFLowSetting(lnb);
     parent->addChild(m_lofLo);
-    m_lofHi = new LNBLOFHighSetting(lnb);
     parent->addChild(m_lofHi);
-    m_polInv = new LNBPolarityInvertedSetting(lnb);
     parent->addChild(m_polInv);
     connect(m_type,      qOverload<const QString&>(&StandardSetting::valueChanged),
             this,        qOverload<const QString&>(&LNBConfig::UpdateType));

commit 59455856c680c6b86430f65a17b31bb3871ddf68

Fix member initialization order in the MythVDPAUHelper constructors.
Commit 728caf2 introduced a bug where the m_vdpGetProcAddress member
variable was being referenced before being assigned.  Fix the order of
initialization to prevent this problem.  Default member initialization
requires the members to be initialized in the same order as they
appear in the class declaration, so the two booleans need to be moved
later in the declaration.

Fixes #954.

mythtv/libs/libmythtv/decoders/mythvdpauhelper.cpp
@@ -234,9 +234,9 @@ static void vdpau_preemption_callback(VdpDevice /*unused*/, void* Opaque)
  * \brief A simple wrapper around VDPAU functionality.
 */
 MythVDPAUHelper::MythVDPAUHelper(AVVDPAUDeviceContext* Context)
-  : m_valid(InitProcs()),
-    m_device(Context->device),
-    m_vdpGetProcAddress(Context->get_proc_address)
+  : m_device(Context->device),
+    m_vdpGetProcAddress(Context->get_proc_address),
+    m_valid(InitProcs())
 {
     if (m_valid)
     {
@@ -254,8 +254,8 @@ static const char* DummyGetError(VdpStatus /*status*/)
 }
 
 MythVDPAUHelper::MythVDPAUHelper(void)
-  : m_createdDevice(true),
-    m_display(MythXDisplay::OpenMythXDisplay(false))
+  : m_display(MythXDisplay::OpenMythXDisplay(false)),
+    m_createdDevice(true)
 {
     if (!m_display)
         return;

mythtv/libs/libmythtv/decoders/mythvdpauhelper.h
@@ -100,8 +100,6 @@ class MythVDPAUHelper : public QObject
     bool   InitProcs(void);
 
   private:
-    bool                              m_valid                            { false   };
-    bool                              m_createdDevice                    { false   };
     VdpDevice                         m_device                           { 0       };
     MythXDisplay                     *m_display                          { nullptr };
 
@@ -123,6 +121,9 @@ class MythVDPAUHelper : public QObject
     VdpOutputSurfaceDestroy          *m_vdpOutputSurfaceDestroy          { nullptr };
     VdpVideoSurfaceGetParameters     *m_vdpVideoSurfaceGetParameters     { nullptr };
     VdpPreemptionCallbackRegister    *m_vdpPreemptionCallbackRegister    { nullptr };
+
+    bool                              m_createdDevice                    { false   };
+    bool                              m_valid                            { false   };
 };
 
 #endif // MYTHVDPAUHELPER_H