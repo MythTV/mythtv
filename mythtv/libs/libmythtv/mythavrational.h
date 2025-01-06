e8a14fa171c42f0b7484eb409f3d7ad5595972de
David Hampton <mythtv@love2code.net>
2025-01-06 16:23:29 -0500

    clazy: Convert avrational functions to pass-by-value.
    These functions all accept an AVrational as an argument, which is an
    object with 16 bytes of storage.  The analysis tools believe that any
    object that is <= 16 bytes, and is trivially copyable and trivially
    destructible, is more efficiently passed by value than by reference.
    
    https://github.com/KDE/clazy/blob/master/docs/checks/README-function-args-by-value.md

--- mythtv/libs/libmythtv/mythavrational.h
+++ mythtv/libs/libmythtv/mythavrational.h
@@ -56,45 +56,45 @@ class MythAVRational
     }
     [[nodiscard]] MythAVRational invert() const { return MythAVRational(av_inv_q(m_q)); }
 
-    static int cmp(const MythAVRational& a, const MythAVRational& b) { return av_cmp_q(a.m_q, b.m_q); }
+    static int cmp(MythAVRational a, MythAVRational b) { return av_cmp_q(a.m_q, b.m_q); }
 
-    MythAVRational& operator+=(const MythAVRational& rhs);
-    MythAVRational& operator-=(const MythAVRational& rhs);
-    MythAVRational& operator*=(const MythAVRational& rhs);
-    MythAVRational& operator/=(const MythAVRational& rhs);
+    MythAVRational& operator+=(MythAVRational rhs);
+    MythAVRational& operator-=(MythAVRational rhs);
+    MythAVRational& operator*=(MythAVRational rhs);
+    MythAVRational& operator/=(MythAVRational rhs);
   private:
     AVRational m_q {.num = 0, .den = 1};
 };
 
-inline bool operator==(const MythAVRational& lhs, const MythAVRational& rhs) { return MythAVRational::cmp(lhs, rhs) == 0; }
-inline bool operator!=(const MythAVRational& lhs, const MythAVRational& rhs) { return MythAVRational::cmp(lhs, rhs) != 0; }
-inline bool operator< (const MythAVRational& lhs, const MythAVRational& rhs) { return MythAVRational::cmp(lhs, rhs) <  0; }
-inline bool operator> (const MythAVRational& lhs, const MythAVRational& rhs) { return MythAVRational::cmp(lhs, rhs) >  0; }
-inline bool operator<=(const MythAVRational& lhs, const MythAVRational& rhs) { return MythAVRational::cmp(lhs, rhs) <= 0; }
-inline bool operator>=(const MythAVRational& lhs, const MythAVRational& rhs) { return MythAVRational::cmp(lhs, rhs) >= 0; }
+inline bool operator==(MythAVRational lhs, MythAVRational rhs) { return MythAVRational::cmp(lhs, rhs) == 0; }
+inline bool operator!=(MythAVRational lhs, MythAVRational rhs) { return MythAVRational::cmp(lhs, rhs) != 0; }
+inline bool operator< (MythAVRational lhs, MythAVRational rhs) { return MythAVRational::cmp(lhs, rhs) <  0; }
+inline bool operator> (MythAVRational lhs, MythAVRational rhs) { return MythAVRational::cmp(lhs, rhs) >  0; }
+inline bool operator<=(MythAVRational lhs, MythAVRational rhs) { return MythAVRational::cmp(lhs, rhs) <= 0; }
+inline bool operator>=(MythAVRational lhs, MythAVRational rhs) { return MythAVRational::cmp(lhs, rhs) >= 0; }
 
-inline MythAVRational operator+(const MythAVRational& lhs, const MythAVRational& rhs)
+inline MythAVRational operator+(MythAVRational lhs, MythAVRational rhs)
 {
     return MythAVRational(av_add_q(lhs.get_q(), rhs.get_q()));
 }
 
-inline MythAVRational operator-(const MythAVRational& lhs, const MythAVRational& rhs)
+inline MythAVRational operator-(MythAVRational lhs, MythAVRational rhs)
 {
     return MythAVRational(av_sub_q(lhs.get_q(), rhs.get_q()));
 }
 
-inline MythAVRational operator*(const MythAVRational& lhs, const MythAVRational& rhs)
+inline MythAVRational operator*(MythAVRational lhs, MythAVRational rhs)
 {
     return MythAVRational(av_mul_q(lhs.get_q(), rhs.get_q()));
 }
 
-inline MythAVRational operator/(const MythAVRational& lhs, const MythAVRational& rhs)
+inline MythAVRational operator/(MythAVRational lhs, MythAVRational rhs)
 {
     return MythAVRational(av_div_q(lhs.get_q(), rhs.get_q()));
 }
 
-inline MythAVRational& MythAVRational::operator+=(const MythAVRational& rhs) { return *this = operator+(*this, rhs); }
-inline MythAVRational& MythAVRational::operator-=(const MythAVRational& rhs) { return *this = operator-(*this, rhs); }
-inline MythAVRational& MythAVRational::operator*=(const MythAVRational& rhs) { return *this = operator*(*this, rhs); }
-inline MythAVRational& MythAVRational::operator/=(const MythAVRational& rhs) { return *this = operator/(*this, rhs); }
+inline MythAVRational& MythAVRational::operator+=(MythAVRational rhs) { return *this = operator+(*this, rhs); }
+inline MythAVRational& MythAVRational::operator-=(MythAVRational rhs) { return *this = operator-(*this, rhs); }
+inline MythAVRational& MythAVRational::operator*=(MythAVRational rhs) { return *this = operator*(*this, rhs); }
+inline MythAVRational& MythAVRational::operator/=(MythAVRational rhs) { return *this = operator/(*this, rhs); }
 #endif // MYTH_AV_RATIONAL_H
