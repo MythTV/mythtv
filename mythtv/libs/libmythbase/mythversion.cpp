5a07b2fa04d29694c99e811f8d5b2f9b3f8dce0b
David Hampton <mythtv@love2code.net>
2025-02-20 12:59:32 -0500

    Use common version parsing for front/backend theme checks.
    The backend and frontend need to share the parsing logic that
    determines whether the running code should pull the "trunk" version of
    the themes, or a version like "34.1".

--- mythtv/libs/libmythbase/mythversion.cpp
+++ mythtv/libs/libmythbase/mythversion.cpp
@@ -2,6 +2,8 @@
 
 #include "version.h"
 
+#include <QRegularExpression>
+
 const char *GetMythSourceVersion()
 {
     return MYTH_SOURCE_VERSION;
@@ -10,6 +12,33 @@ const char *GetMythSourceVersion()
 const char *GetMythSourcePath()
 {
     return MYTH_SOURCE_PATH;
+}
+
+// Require major version. Optional minor and point versions. Optional
+// '-pre' tag.  If present, must be in this order.
+//
+// Match 1: major
+// Match 2: .minor
+// Match 3: minor
+// Match 4: .point
+// Match 5: point
+// Match 6: -pre
+static const QRegularExpression themeparts
+    { "v([0-9]+)(\\.([0-9]+))?(\\.([0-9]+))?(-pre)?-*", QRegularExpression::CaseInsensitiveOption };
+
+bool ParseMythSourceVersion(const QString version, bool& devel, uint8_t& major, uint8_t &minor)
+{
+    auto match = themeparts.match(version);
+    if (!match.isValid() || !match.hasMatch())
+        return false;
+    devel = !match.captured(6).isEmpty();
+    major = match.captured(1).toUInt();
+    minor = match.captured(3).toUInt();
+    return true;
+}
+bool ParseMythSourceVersion(bool& devel, uint8_t& major, uint8_t &minor)
+{
+    return ParseMythSourceVersion(MYTH_SOURCE_VERSION, devel, major, minor);
 }
 
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
