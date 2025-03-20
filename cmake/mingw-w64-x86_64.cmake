b2b89b74506911a1c43f214113c3acfcaafc9142
David Hampton <mythtv@love2code.net>
2025-03-20 23:07:15 +0000

    cmake/win: Add support for debian posix version of the mingw compiler.

--- cmake/mingw-w64-x86_64.cmake
+++ cmake/mingw-w64-x86_64.cmake
@@ -10,10 +10,32 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
+file(
+  GLOB _dirs
+  LIST_DIRECTORIES true
+  RELATIVE /usr/lib/gcc/${TOOLCHAIN_PREFIX}
+  /usr/lib/gcc/${TOOLCHAIN_PREFIX}/[1-9][0-9]*)
+
 # cross compilers to use for C, C++ and Fortran
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
+if(_dirs MATCHES "[0-9][0-9]-posix")
+  # Debian/Ubuntu has directories named 14-win32 and 14-posix.
+  set(TOOLCHAIN_LIBDIR ${CMAKE_MATCH_0})
+  set(CMAKE_AR ${TOOLCHAIN_PREFIX}-gcc-ar-posix)
+  set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc-${CMAKE_MATCH_0})
+  set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-posix)
+  set(CMAKE_NM ${TOOLCHAIN_PREFIX}-gcc-nm-posix)
+  set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}-gcc-ranlib-posix)
+elseif(_dirs MATCHES "[0-9][0-9]-win32")
+  message(
+    FATAL_ERROR "You need the install the posix version of the mingw compiler.")
+else()
+  # Fedora has directories named 14.x.x
+  set(CMAKE_AR ${TOOLCHAIN_PREFIX}-ar)
+  set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+  set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+  set(CMAKE_NM ${TOOLCHAIN_PREFIX}-nm)
+  set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}-ranlib)
+endif()
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # target environment on the build host system
--- cmake/platform/WindowsCrossSearchPaths.cmake
+++ cmake/platform/WindowsCrossSearchPaths.cmake
@@ -6,11 +6,17 @@
 
 # Look for precompiled libraries. Debian doesn't have many of these, but Fedora
 # has a bunch.
-set(MINGW_SYSROOT "/usr/${TOOLCHAIN_PREFIX}/sys-root/mingw")
-list(PREPEND PKG_CONFIG_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
-list(APPEND CMAKE_FIND_ROOT_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
-if(EXISTS ${MINGW_SYSROOT}/lib/libz.dll.a)
-  set(ZLIB_ROOT ${MINGW_SYSROOT})
-else()
-  set(ZLIB_ROOT ${LIBS_INSTALL_PREFIX})
+if(EXISTS "/usr/${TOOLCHAIN_PREFIX}/sys-root/mingw")
+  set(MINGW_SYSROOT "/usr/${TOOLCHAIN_PREFIX}/sys-root/mingw")
+  list(PREPEND PKG_CONFIG_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
+  list(APPEND CMAKE_FIND_ROOT_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
+  if(EXISTS ${MINGW_SYSROOT}/lib/libz.dll.a)
+    set(ZLIB_ROOT ${MINGW_SYSROOT})
+  else()
+    set(ZLIB_ROOT ${LIBS_INSTALL_PREFIX})
+  endif()
+endif()
+if(EXISTS /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_LIBDIR})
+  # Needed on Debian for fontconfig and libbluray to find -lssp
+  list(APPEND CMAKE_SHARED_LINKER_FLAGS -L/usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_LIBDIR})
 endif()
