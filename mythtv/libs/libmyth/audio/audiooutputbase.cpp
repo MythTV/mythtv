fccc38654cd31005b2d8e12c36918a45ab43b9ea
Scott Theisen <scott.the.elm@gmail.com>
2025-05-22 18:44:40 -0400

    move DownmixFrames() to its only user
    The mixing coefficient matrices should be transposed so the coefficients for
    each output channel are contiguous in memory and line up with the input data.
    The matrix multiplication would probably be faster and easier to vectorize.
    
    This probably could be implemented with FFmpeg’s pan filter.
    https://trac.ffmpeg.org/wiki/AudioChannelManipulation
    https://ffmpeg.org/ffmpeg-filters.html#pan
    
    I have no idea where the mixing coefficients come from and throwing away the
    LFE information is not optimal.  The coefficients don’t match ATSC A/52:2012
    section 7.8.2 Downmixing into Two Channels nor Ogg’s
    (https://www.rfc-editor.org/rfc/rfc7845#section-5.1.1.5).
    Additionally, the coefficients are not normalized, so this could cause clipping.
    
    Other coefficients: https://superuser.com/questions/852400/properly-downmix-5-1-to-stereo-using-ffmpeg#1410620

--- mythtv/libs/libmyth/audio/audiooutputbase.cpp
+++ mythtv/libs/libmyth/audio/audiooutputbase.cpp
@@ -1,5 +1,6 @@
 // C++ headers
 #include <algorithm>
+#include <array>
 #include <cmath>
 #include <limits>
 
@@ -26,7 +27,6 @@
 
 #include "audiooutputbase.h"
 #include "audiooutputdigitalencoder.h"
-#include "audiooutputdownmix.h"
 #include "audiooutpututil.h"
 #include "freesurround.h"
 #include "spdifencoder.h"
@@ -50,6 +50,195 @@
 static constexpr int UPMIX_CHANNEL_MASK { (1<<1)|(1<<2)|(1<<5)|(1<<7) };
 static constexpr bool IS_VALID_UPMIX_CHANNEL(int ch)
 { return ((1 << ch) & UPMIX_CHANNEL_MASK) != 0; }
+
+/*
+ SMPTE channel layout
+ DUAL-MONO      L   R
+ DUAL-MONO-LFE  L   R   LFE
+ MONO           M
+ MONO-LFE       M   LFE
+ STEREO         L   R
+ STEREO-LFE     L   R   LFE
+ 3F             L   R   C
+ 3F-LFE         L   R   C    LFE
+ 2F1            L   R   S
+ 2F1-LFE        L   R   LFE  S
+ 3F1            L   R   C    S
+ 3F1-LFE        L   R   C    LFE S
+ 2F2            L   R   LS   RS
+ 2F2-LFE        L   R   LFE  LS   RS
+ 3F2            L   R   C    LS   RS
+ 3F2-LFE        L   R   C    LFE  LS   RS
+ 3F3R-LFE       L   R   C    LFE  BC   LS   RS
+ 3F4-LFE        L   R   C    LFE  Rls  Rrs  LS   RS
+ */
+
+static const float m6db = 0.5;
+static const float m3db = 0.7071067811865476F;           // 3dB  = SQRT(2)
+static const float mm3db = -0.7071067811865476F;         // -3dB = SQRT(1/2)
+static const float msqrt_1_3 = -0.577350269189626F;      // -SQRT(1/3)
+static const float sqrt_2_3 = 0.816496580927726F;        // SQRT(2/3)
+static const float sqrt_2_3by3db = 0.577350269189626F;   // SQRT(2/3)*-3dB = SQRT(2/3)*SQRT(1/2)=SQRT(1/3)
+static const float msqrt_1_3bym3db = 0.408248290463863F; // -SQRT(1/3)*-3dB = -SQRT(1/3)*SQRT(1/2) = -SQRT(1/6)
+
+using two_speaker_ratio = std::array<float,2>;
+using two_speaker_set   = std::array<two_speaker_ratio,8>;
+static const std::array<two_speaker_set,8> stereo_matrix
+{{
+//1F      L                R
+    {{
+        { 1,               1 },                 // M
+    }},
+
+//2F      L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+    }},
+
+//3F      L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+        { 1,               1 },                 // C
+    }},
+
+//3F1R    L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+        { m3db,            m3db },              // C
+        { mm3db,           m3db },              // S
+    }},
+
+//3F2R    L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+        { m3db,            m3db },              // C
+        { sqrt_2_3,        msqrt_1_3 },         // LS
+        { msqrt_1_3,       sqrt_2_3 },          // RS
+    }},
+
+//3F2R.1  L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+        { m3db,            m3db },              // C
+        { 0,               0 },                 // LFE
+        { sqrt_2_3,        msqrt_1_3 },         // LS
+        { msqrt_1_3,       sqrt_2_3 },          // RS
+    }},
+
+// 3F3R.1 L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+        { m3db,            m3db },              // C
+        { 0,               0 },                 // LFE
+        { m6db,            m6db },              // Cs
+        { sqrt_2_3,        msqrt_1_3 },         // LS
+        { msqrt_1_3,       sqrt_2_3 },          // RS
+    }},
+
+// 3F4R.1 L                R
+    {{
+        { 1,               0 },                 // L
+        { 0,               1 },                 // R
+        { m3db,            m3db },              // C
+        { 0,               0 },                 // LFE
+        { sqrt_2_3by3db,   msqrt_1_3bym3db },   // Rls
+        { msqrt_1_3bym3db, sqrt_2_3by3db },     // Rrs
+        { sqrt_2_3by3db,   msqrt_1_3bym3db },   // LS
+        { msqrt_1_3bym3db, sqrt_2_3by3db },     // RS
+    }}
+}};
+
+using six_speaker_ratio = std::array<float,6>;
+using six_speaker_set   = std::array<six_speaker_ratio,8>;
+static const std::array<six_speaker_set,3> s51_matrix
+{{
+    // 3F2R.1 in -> 3F2R.1 out
+    // L  R  C  LFE         LS       RS
+    {{
+        { 1, 0, 0, 0,       0,       0 },     // L
+        { 0, 1, 0, 0,       0,       0 },     // R
+        { 0, 0, 1, 0,       0,       0 },     // C
+        { 0, 0, 0, 1,       0,       0 },     // LFE
+        { 0, 0, 0, 0,       1,       0 },     // LS
+        { 0, 0, 0, 0,       0,       1 },     // RS
+    }},
+    // 3F3R.1 in -> 3F2R.1 out
+    // Used coefficient found at http://www.yamahaproaudio.com/training/self_training/data/smqr_en.pdf
+    // L  R  C  LFE         LS       RS
+    {{
+        { 1, 0, 0, 0,       0,       0 },     // L
+        { 0, 1, 0, 0,       0,       0 },     // R
+        { 0, 0, 1, 0,       0,       0 },     // C
+        { 0, 0, 0, 1,       0,       0 },     // LFE
+        { 0, 0, 0, 0,       m3db,    m3db },  // Cs
+        { 0, 0, 0, 0,       1,       0 },     // LS
+        { 0, 0, 0, 0,       0,       1 },     // RS
+    }},
+    // 3F4R.1 -> 3F2R.1 out
+    // L  R  C  LFE         LS       RS
+    {{
+        { 1, 0, 0, 0,       0,       0 },     // L
+        { 0, 1, 0, 0,       0,       0 },     // R
+        { 0, 0, 1, 0,       0,       0 },     // C
+        { 0, 0, 0, 1,       0,       0 },     // LFE
+        { 0, 0, 0, 0,       m3db,    0 },     // Rls
+        { 0, 0, 0, 0,       0,       m3db },  // Rrs
+        { 0, 0, 0, 0,       m3db,    0 },     // LS
+        { 0, 0, 0, 0,       0,       m3db },  // RS
+    }}
+}};
+
+static int DownmixFrames(int channels_in, int  channels_out,
+                                      float *dst, const float *src, int frames)
+{
+    if (channels_in < channels_out)
+        return -1;
+
+    //LOG(VB_AUDIO, LOG_INFO, QString("Downmixing %1 frames (in:%2 out:%3)")
+    //    .arg(frames).arg(channels_in).arg(channels_out));
+    if (channels_out == 2)
+    {
+        int index = channels_in - 1;
+        for (int n=0; n < frames; n++)
+        {
+            for (int i=0; i < channels_out; i++)
+            {
+                float tmp = 0.0F;
+                for (int j=0; j < channels_in; j++)
+                    tmp += src[j] * stereo_matrix[index][j][i];
+                *dst++ = tmp;
+            }
+            src += channels_in;
+        }
+    }
+    else if (channels_out == 6)
+    {
+        int index = channels_in - 6;
+        for (int n=0; n < frames; n++)
+        {
+            for (int i=0; i < channels_out; i++)
+            {
+                float tmp = 0.0F;
+                for (int j=0; j < channels_in; j++)
+                    tmp += src[j] * s51_matrix[index][j][i];
+                *dst++ = tmp;
+            }
+            src += channels_in;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+
+    return frames;
+}
 
 const char *AudioOutputBase::quality_string(int q)
 {
@@ -1477,7 +1666,7 @@ bool AudioOutputBase::AddData(void *in_buffer, int in_len,
         // Perform downmix if necessary
         if (m_needsDownmix)
         {
-            if(AudioOutputDownmix::DownmixFrames(m_sourceChannels,
+            if(DownmixFrames(m_sourceChannels,
                                                  m_configuredChannels,
                                                  m_srcIn, m_srcIn, frames) < 0)
                 LOG(VB_GENERAL, LOG_ERR, LOC + "Error occurred while downmixing");
