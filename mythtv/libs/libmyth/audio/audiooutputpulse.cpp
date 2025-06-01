3d3a2cc5fa7c0bb715ab41ab785a76341373b08d
Steve Erlenborn <1751095+SteveErl@users.noreply.github.com>
2025-06-01 11:02:29 -0500

    Close memory leaks in PulseAudio output handler (#1119)
    * Close memory leaks in PulseAudio output handler
    
    The original code allocated memory with calls
    to pa_proplist_new(), and stored those addresses
    in a local variable, 'proplist'. The corresponding
    memory was never released. The same local
    variable was used in two different methods,
    ContextConnect() and ConnectPlaybackStream().
    
    We need to keep track of these memory pointers
    separately, so 2 new members have been added to
    the AudioOutputPulseAudio class:
            pa_proplist *m_ctproplist
            pa_proplist *m_stproplist
    
    When memory is allocated for Context or Stream,
    the updated code stores the pa_proplist * value
    in m_ctproplist or m_stproplist. Later on, when
    it's time to release memory, these class members
    are used in calls to pa_proplist_free().
    
    A separate memory leak was identified in the call
    to pa_context_drain(). This routine allocates
    memory which it may need when the drain operation
    finishes asynchronously. It is designed to invoke
    a callback function when the operation completes.
    The original code passed 'nullptr' as the callback
    function pointer, but this causes a leak. The
    code has been updated to point to a new callback
    function, ContextDrainCallback(). If there is
    nothing to be drained, we'll disconnect the
    Context and drop a reference to it immediately.
    If there was stuff to be drained, the new callback
    function will disconnect the Context and drop the
    reference.
    
    After applying these modifications, all of the
    memory leaks associated with audiooutputpulse.cpp
    by 'valgrind' have been eliminated.
    
    Resolves: #1100
    
    * Close memory leaks in PulseAudio output handler
    
    Fixing the handling of the pa_context_drain() memory leak.
    This update provides a loop to wait for the
    ContextDrainCallback() to be invoked before destroying
    the reference to the pa_context_drain() operation.
    This fixes the memory leak without failing to
    disconnect the context.
    
    Resolves #1100
    
    * Close memory leaks in PulseAudio output handler
    
    Adding a check that m_mainloop is non-null before
    invoking pa_context_drain(). It is extremely
    unlikely that such a circumstance will ever happen.
    The lock is only aquired if it is non-null,
    and the code for handling pa_context_drain()
    requires that the lock be held before calling
    pa_threaded_mainloop_wait(), so this small
    modification makes the code safer.
    
    Resolves #1100

--- mythtv/libs/libmyth/audio/audiooutputpulse.cpp
+++ mythtv/libs/libmyth/audio/audiooutputpulse.cpp
@@ -52,6 +52,16 @@ AudioOutputPulseAudio::~AudioOutputPulseAudio()
         pa_context_unref(m_pcontext);
         m_pcontext = nullptr;
     }
+    if (m_ctproplist)
+    {
+        pa_proplist_free(m_ctproplist);
+        m_ctproplist = nullptr;
+    }
+    if (m_stproplist)
+    {
+        pa_proplist_free(m_stproplist);
+        m_stproplist = nullptr;
+    }
 }
 
 AudioOutputSettings* AudioOutputPulseAudio::GetOutputSettings(bool /*digital*/)
@@ -78,6 +88,16 @@ AudioOutputSettings* AudioOutputPulseAudio::GetOutputSettings(bool /*digital*/)
         pa_threaded_mainloop_stop(m_mainloop);
         pa_threaded_mainloop_free(m_mainloop);
         m_mainloop = nullptr;
+        if (m_ctproplist)
+        {
+            pa_proplist_free(m_ctproplist);
+            m_ctproplist = nullptr;
+        }
+        if (m_stproplist)
+        {
+            pa_proplist_free(m_stproplist);
+            m_stproplist = nullptr;
+        }
         delete m_aoSettings;
         return nullptr;
     }
@@ -122,6 +142,16 @@ AudioOutputSettings* AudioOutputPulseAudio::GetOutputSettings(bool /*digital*/)
     pa_threaded_mainloop_stop(m_mainloop);
     pa_threaded_mainloop_free(m_mainloop);
     m_mainloop = nullptr;
+    if (m_ctproplist)
+    {
+        pa_proplist_free(m_ctproplist);
+        m_ctproplist = nullptr;
+    }
+    if (m_stproplist)
+    {
+        pa_proplist_free(m_stproplist);
+        m_stproplist = nullptr;
+    }
 
     return m_aoSettings;
 }
@@ -185,6 +215,16 @@ bool AudioOutputPulseAudio::OpenDevice()
         pa_threaded_mainloop_stop(m_mainloop);
         pa_threaded_mainloop_free(m_mainloop);
         m_mainloop = nullptr;
+        if (m_ctproplist)
+        {
+            pa_proplist_free(m_ctproplist);
+            m_ctproplist = nullptr;
+        }
+        if (m_stproplist)
+        {
+            pa_proplist_free(m_stproplist);
+            m_stproplist = nullptr;
+        }
         return false;
     }
 
@@ -194,6 +234,16 @@ bool AudioOutputPulseAudio::OpenDevice()
         pa_threaded_mainloop_stop(m_mainloop);
         pa_threaded_mainloop_free(m_mainloop);
         m_mainloop = nullptr;
+        if (m_ctproplist)
+        {
+            pa_proplist_free(m_ctproplist);
+            m_ctproplist = nullptr;
+        }
+        if (m_stproplist)
+        {
+            pa_proplist_free(m_stproplist);
+            m_stproplist = nullptr;
+        }
         return false;
     }
 
@@ -216,7 +266,19 @@ void AudioOutputPulseAudio::CloseDevice()
 
     if (m_pcontext)
     {
-        pa_context_drain(m_pcontext, nullptr, nullptr);
+        if (m_mainloop)
+        {
+            pa_operation *op = pa_context_drain(m_pcontext, ContextDrainCallback, m_mainloop);
+            if (op)
+            {
+                // Wait for the asynchronous draining to complete
+                while (pa_operation_get_state(op) == PA_OPERATION_RUNNING)
+                {
+                    pa_threaded_mainloop_wait(m_mainloop);
+                }
+                pa_operation_unref(op);
+            }
+        }
         pa_context_disconnect(m_pcontext);
         pa_context_unref(m_pcontext);
         m_pcontext = nullptr;
@@ -228,6 +290,17 @@ void AudioOutputPulseAudio::CloseDevice()
         pa_threaded_mainloop_stop(m_mainloop);
         pa_threaded_mainloop_free(m_mainloop);
         m_mainloop = nullptr;
+    }
+
+    if (m_ctproplist)
+    {
+        pa_proplist_free(m_ctproplist);
+        m_ctproplist = nullptr;
+    }
+    if (m_stproplist)
+    {
+        pa_proplist_free(m_stproplist);
+        m_stproplist = nullptr;
     }
 }
 
@@ -415,21 +488,23 @@ bool AudioOutputPulseAudio::ContextConnect(void)
         m_pcontext = nullptr;
         return false;
     }
-    pa_proplist *proplist = pa_proplist_new();
-    if (!proplist)
+    m_ctproplist = pa_proplist_new();
+    if (!m_ctproplist)
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + fn_log_tag + QString("failed to create new proplist"));
         return false;
     }
-    pa_proplist_sets(proplist, PA_PROP_APPLICATION_NAME, "MythTV");
-    pa_proplist_sets(proplist, PA_PROP_APPLICATION_ICON_NAME, "mythtv");
-    pa_proplist_sets(proplist, PA_PROP_MEDIA_ROLE, "video");
+    pa_proplist_sets(m_ctproplist, PA_PROP_APPLICATION_NAME, "MythTV");
+    pa_proplist_sets(m_ctproplist, PA_PROP_APPLICATION_ICON_NAME, "mythtv");
+    pa_proplist_sets(m_ctproplist, PA_PROP_MEDIA_ROLE, "video");
     m_pcontext =
         pa_context_new_with_proplist(pa_threaded_mainloop_get_api(m_mainloop),
-                                     "MythTV", proplist);
+                                     "MythTV", m_ctproplist);
     if (!m_pcontext)
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + fn_log_tag + "failed to acquire new context");
+        pa_proplist_free(m_ctproplist);
+        m_ctproplist = nullptr;
         return false;
     }
     pa_context_set_state_callback(m_pcontext, ContextStateCallback, this);
@@ -443,6 +518,8 @@ bool AudioOutputPulseAudio::ContextConnect(void)
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + fn_log_tag + QString("context connect failed: %1")
                              .arg(pa_strerror(pa_context_errno(m_pcontext))));
+        pa_proplist_free(m_ctproplist);
+        m_ctproplist = nullptr;
         return false;
     }
     bool connected = false;
@@ -461,6 +538,8 @@ bool AudioOutputPulseAudio::ContextConnect(void)
                 LOG(VB_GENERAL, LOG_ERR, LOC + fn_log_tag +
                         QString("context connection failed or terminated: %1")
                         .arg(pa_strerror(pa_context_errno(m_pcontext))));
+                pa_proplist_free(m_ctproplist);
+                m_ctproplist = nullptr;
                 return false;
 
             default:
@@ -507,19 +586,21 @@ QString AudioOutputPulseAudio::ChooseHost(void)
 bool AudioOutputPulseAudio::ConnectPlaybackStream(void)
 {
     QString fn_log_tag = "ConnectPlaybackStream, ";
-    pa_proplist *proplist = pa_proplist_new();
-    if (!proplist)
+    m_stproplist = pa_proplist_new();
+    if (!m_stproplist)
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + fn_log_tag + QString("failed to create new proplist"));
         return false;
     }
-    pa_proplist_sets(proplist, PA_PROP_MEDIA_ROLE, "video");
+    pa_proplist_sets(m_stproplist, PA_PROP_MEDIA_ROLE, "video");
     m_pstream =
         pa_stream_new_with_proplist(m_pcontext, "MythTV playback", &m_sampleSpec,
-                                    &m_channelMap, proplist);
+                                    &m_channelMap, m_stproplist);
     if (!m_pstream)
     {
         LOG(VB_GENERAL, LOG_ERR, LOC + "failed to create new playback stream");
+        pa_proplist_free(m_stproplist);
+        m_stproplist = nullptr;
         return false;
     }
     pa_stream_set_state_callback(m_pstream, StreamStateCallback, this);
@@ -567,6 +648,8 @@ bool AudioOutputPulseAudio::ConnectPlaybackStream(void)
             case PA_CONTEXT_TERMINATED:
                 LOG(VB_GENERAL, LOG_ERR, LOC + QString("context is stuffed, %1")
                             .arg(pa_strerror(pa_context_errno(m_pcontext))));
+                pa_proplist_free(m_stproplist);
+                m_stproplist = nullptr;
                 failed = true;
                 break;
             default:
@@ -580,6 +663,8 @@ bool AudioOutputPulseAudio::ConnectPlaybackStream(void)
                         LOG(VB_GENERAL, LOG_ERR, LOC + QString("stream failed or was terminated, "
                                         "context state %1, stream state %2")
                                     .arg(cstate).arg(sstate));
+                        pa_proplist_free(m_stproplist);
+                        m_stproplist = nullptr;
                         failed = true;
                         break;
                     default:
@@ -609,6 +694,12 @@ void AudioOutputPulseAudio::FlushStream(const char *caller)
         pa_operation_unref(op);
     else
         LOG(VB_GENERAL, LOG_ERR, LOC + fn_log_tag + "stream flush operation failed ");
+}
+
+void AudioOutputPulseAudio::ContextDrainCallback(pa_context */*c*/, void *arg)
+{
+    pa_threaded_mainloop *mloop = (pa_threaded_mainloop *)arg;
+    pa_threaded_mainloop_signal(mloop, 0);
 }
 
 void AudioOutputPulseAudio::ContextStateCallback(pa_context *c, void *arg)
