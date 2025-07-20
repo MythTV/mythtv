2f9de8042b7b94ed62972875a898b97688d2b25b
Klaas de Waal <klaas@kldo.nl>
2025-07-20 23:33:59 +0200

    Show recordedtime also for recordings in progress
    Show the recordedtime in the slider info not only for recordings
    that have been completed but also for recordings in progress.

--- mythtv/libs/libmythtv/mythplayeroverlayui.cpp
+++ mythtv/libs/libmythtv/mythplayeroverlayui.cpp
@@ -239,7 +239,8 @@ void MythPlayerOverlayUI::UpdateSliderInfo(osdInfo &Info, bool PaddedFields)
         QString dtformat = gCoreContext->GetSetting("DateFormat", "ddd MMMM d yyyy")
             + ", " + gCoreContext->GetSetting("TimeFormat", "hh:mm");
 
-        if (m_playerCtx->GetState() == kState_WatchingPreRecorded )
+        if ((m_playerCtx->GetState() == kState_WatchingPreRecorded) ||
+            (m_playerCtx->GetState() == kState_WatchingRecording  ))
         {
             QDateTime recordedtime =
                 m_playerCtx->m_playingRecStart.addSecs(static_cast<qint64>(secsplayed.count()));
