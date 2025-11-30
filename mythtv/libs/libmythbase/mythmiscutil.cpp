7d23b430b979d263cdafa18d392d22df057871c6
Scott Theisen <scott.the.elm@gmail.com>
2025-11-29 22:25:22 -0500

    libmythbase/mythmiscutil.cpp: Remove unnecessary includes
    All targets already include <sys/stat.h> so make it unconditional.
    
    statfs is not used in this file so neither <sys/vfs.h> nor
    <sys/mount.h> are needed.
    
    sysctl() is also available on at least OpenBSD, so use
    Q_OS_BSD4 to enable that code for all BSD 4.4 OSes.

--- mythtv/libs/libmythbase/mythmiscutil.cpp
+++ mythtv/libs/libmythbase/mythmiscutil.cpp
@@ -1,7 +1,3 @@
-#ifdef _WIN32
-    #include <sys/stat.h>
-#endif
-
 #include "mythmiscutil.h"
 
 // C++ headers
@@ -14,15 +10,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sched.h>
+#include <sys/stat.h> // for umask, chmod
 
 // System specific C headers
 #include "compat.h"
 #include <QtGlobal>
 
 #ifdef __linux__
-#include <sys/vfs.h>
 #include <sys/sysinfo.h>
-#include <sys/stat.h> // for umask, chmod
 #endif
 
 #ifdef Q_OS_DARWIN
@@ -30,9 +25,7 @@
 #endif
 
 #ifdef Q_OS_BSD4
-#include <sys/mount.h>  // for struct statfs
 #include <sys/sysctl.h>
-#include <sys/stat.h> // for umask, chmod
 #endif
 
 // Qt headers
@@ -73,8 +66,7 @@ bool getUptime(std::chrono::seconds &uptime)
     }
     uptime = std::chrono::seconds(sinfo.uptime);
 
-#elif defined(__FreeBSD__) || defined(Q_OS_DARWIN)
-
+#elif defined(Q_OS_BSD4)
     std::array<int,2> mib { CTL_KERN, KERN_BOOTTIME };
     struct timeval bootTime;
     size_t         len;
