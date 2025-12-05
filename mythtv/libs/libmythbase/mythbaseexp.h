5a0a38f74ac67b681985317a9caf0702fd2aa1b1
Scott Theisen <scott.the.elm@gmail.com>
2025-12-05 17:01:17 -0500

    Simplify symbol export macro headers
    Only set MBASE_PUBLIC to export with MBASE_API since
    MPLUGIN_API has its own MPLUGIN_PUBLIC export/import define.
    
    Only libmythtv/io/mythiowrapper.h is included by C code in
    libdvdnav and libdvdread.

--- mythtv/libs/libmythbase/mythbaseexp.h
+++ mythtv/libs/libmythbase/mythbaseexp.h
@@ -1,18 +1,11 @@
 #ifndef MYTHBASEEXP_H_
 #define MYTHBASEEXP_H_
 
-// This header is called from some non-QT projects,
-// and if non C++ then Q_DECL_XXX never defined
-
-#if defined( QT_CORE_LIB ) && defined( __cplusplus )
-# include <QtCore/qglobal.h>
-# if defined(MBASE_API) || defined(MPLUGIN_API)
-#  define MBASE_PUBLIC Q_DECL_EXPORT
-# else
-#  define MBASE_PUBLIC Q_DECL_IMPORT
-# endif
+#include <QtGlobal>
+#if defined(MBASE_API)
+# define MBASE_PUBLIC Q_DECL_EXPORT
 #else
-# define MBASE_PUBLIC
+# define MBASE_PUBLIC Q_DECL_IMPORT
 #endif
 
 #endif // MYTHBASEEXP_H_
--- mythtv/libs/libmythtv/mythtvexp.h
+++ mythtv/libs/libmythtv/mythtvexp.h
@@ -1,11 +1,10 @@
 #ifndef MYTHTVEXP_H_
 #define MYTHTVEXP_H_
 
-// This header is called from some non-QT projects, 
-// and if non C++ then Q_DECL_XXX never defined
+// Included by libdvdnav and libdvdread C code
 
-#if defined( QT_CORE_LIB ) && defined( __cplusplus )
-# include <QtCore/qglobal.h>
+#ifdef __cplusplus
+# include <QtGlobal>
 # ifdef MTV_API
 #  define MTV_PUBLIC Q_DECL_EXPORT
 # else
