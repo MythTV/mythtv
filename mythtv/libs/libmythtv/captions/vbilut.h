aef1e21b626fbf2ccb897672051fe9703062ec04
Scott Theisen <scott.the.elm@gmail.com>
2025-12-24 01:41:04 -0500

    vbilut.*: Remove unused constants
    hamm24* is now unused.  chartab_original and unham84tab were never used.

--- mythtv/libs/libmythtv/captions/vbilut.h
+++ mythtv/libs/libmythtv/captions/vbilut.h
@@ -1,18 +1,7 @@
 #ifndef VBILUT_H
 #define VBILUT_H
 
-#include <array>
 #include <cstdint>
-#include <string>
-
-extern const std::array<const uint8_t,     13> chartab_original;
-extern const std::array<const uint16_t,   256> hammtab;
-extern const std::array<const uint8_t,    256> hamm84tab;
-extern const std::array<const uint8_t,    256> unham84tab;
-extern const std::array<const std::array<const uint8_t,256>,3> hamm24par;
-extern const std::array<const uint8_t,    256> hamm24val;
-extern const std::array<const uint16_t,    64> hamm24err;
-extern const std::array<const int32_t,     64> hamm24cor;
 
 enum vbimode : std::uint8_t
 {
@@ -24,6 +13,5 @@ enum vbimode : std::uint8_t
 int hamm8(const uint8_t *p, int *err);
 int hamm84(const uint8_t *p, int *err);
 int hamm16(const uint8_t *p, int *err);
-int hamm24(const uint8_t *p, int *err);
 
 #endif // VBILUT_H
