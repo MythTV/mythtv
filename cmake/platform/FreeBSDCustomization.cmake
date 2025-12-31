a7343ed7497d478c8a25d633d01ed9b720b5b16a
David Hampton <mythtv@love2code.net>
2025-12-31 15:53:42 -0500

    cmake: Convert extra x11 libs from 'if' statement to a variable.

--- cmake/platform/FreeBSDCustomization.cmake
+++ cmake/platform/FreeBSDCustomization.cmake
@@ -14,3 +14,6 @@ list(APPEND FF_PLATFORM_ARGS "--extra-ldflags=-L/usr/local/lib")
 # Fix problem with iconv_xxx vs libiconv_xxx function names.
 list(APPEND FF_PLATFORM_ARGS "--extra-cflags=-DLIBICONV_PLUG")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DLIBICONV_PLUG")
+
+# Add extra X11 libs for linking
+set(X11_EXTRA_LIBS X11::Xext X11::Xxf86vm)
