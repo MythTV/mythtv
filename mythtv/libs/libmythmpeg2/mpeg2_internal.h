5cb50e83cb8a2cb71539a39bff2d315719cc7117
Scott Theisen <scott.the.elm@gmail.com>
2026-01-24 00:41:18 -0500

    Use -Werror=undef

--- mythtv/libs/libmythmpeg2/mpeg2_internal.h
+++ mythtv/libs/libmythmpeg2/mpeg2_internal.h
@@ -20,6 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  */
+#include "mpeg2config.h"
 
 #define STATE_INTERNAL_NORETURN ((mpeg2_state_t)-1)
 
