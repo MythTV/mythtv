MythTV/mythtv

commit f36c44f2d634b6def563605a4e09ca1ff30eaf88

MythMusic: delete any unused directory_ids from music_directories after a scan

mythtv/libs/libmythmetadata/musicfilescanner.cpp
@@ -378,6 +378,7 @@ void MusicFileScanner::cleanDB()
     MSqlQuery query(MSqlQuery::InitCon());
     MSqlQuery deletequery(MSqlQuery::InitCon());
 
+    // delete unused genre_ids from music_genres
     if (!query.exec("SELECT g.genre_id FROM music_genres g "
                     "LEFT JOIN music_songs s ON g.genre_id=s.genre_id "
                     "WHERE s.genre_id IS NULL;"))
@@ -393,6 +394,7 @@ void MusicFileScanner::cleanDB()
                             deletequery);
     }
 
+    // delete unused album_ids from music_albums
     if (!query.exec("SELECT a.album_id FROM music_albums a "
                     "LEFT JOIN music_songs s ON a.album_id=s.album_id "
                     "WHERE s.album_id IS NULL;"))
@@ -408,6 +410,7 @@ void MusicFileScanner::cleanDB()
                             deletequery);
     }
 
+    // delete unused artist_ids from music_artists
     if (!query.exec("SELECT a.artist_id FROM music_artists a "
                     "LEFT JOIN music_songs s ON a.artist_id=s.artist_id "
                     "LEFT JOIN music_albums l ON a.artist_id=l.artist_id "
@@ -425,6 +428,60 @@ void MusicFileScanner::cleanDB()
                             deletequery);
     }
 
+    // delete unused directory_ids from music_directories
+    // get a list of directory_ids not referenced in music_songs
+    if (!query.exec("SELECT d.directory_id, d.parent_id FROM music_directories d "
+                    "LEFT JOIN music_songs s ON d.directory_id=s.directory_id "
+                    "WHERE s.directory_id IS NULL ORDER BY directory_id DESC;"))
+        MythDB::DBError("MusicFileScanner::cleanDB - select music_directories", query);
+
+    deletequery.prepare("DELETE FROM music_directories WHERE directory_id=:DIRECTORYID");
+
+    MSqlQuery parentquery(MSqlQuery::InitCon());
+    parentquery.prepare("SELECT COUNT(*) FROM music_directories "
+                        "WHERE parent_id=:DIRECTORYID ");
+
+    int deletedCount;
+
+    do
+    {
+        deletedCount = 0;
+
+        if (!query.first())
+            break;
+
+        // loop through the list of unused directory_ids deleting any which
+        // aren't referenced by any other directories parent_id
+        do
+        {
+            int directoryid = query.value(0).toInt();
+
+            // have we still got references to this directory_id from other directories
+            parentquery.bindValue(":DIRECTORYID", directoryid);
+            if (!parentquery.exec())
+                MythDB::DBError("MusicFileScanner::cleanDB - get parent directory count",
+                                parentquery);
+
+            if (parentquery.next())
+            {
+                int parentCount = parentquery.value(0).toInt();
+
+                if (parentCount == 0)
+                {
+                    deletequery.bindValue(":DIRECTORYID", directoryid);
+                    if (!deletequery.exec())
+                        MythDB::DBError("MusicFileScanner::cleanDB - delete music_directories",
+                                        deletequery);
+
+                    deletedCount += deletequery.numRowsAffected();
+                }
+            }
+
+        } while (query.next());
+
+    } while (deletedCount);
+
+    // delete unused albumart_ids from music_albumart (embedded images)
     if (!query.exec("SELECT a.albumart_id FROM music_albumart a LEFT JOIN "
                     "music_songs s ON a.song_id=s.song_id WHERE "
                     "embedded='1' AND s.song_id IS NULL;"))

commit 5a5f07f5f2fa0ed5b24cea36e1c38d817618ac98

MusicFileScanner: Don't delete directory_ids referenced in music_albumart
cleanDB() currently deletes directory_ids that are not referenced in
music_songs, which means that directories that do not contain any music
files but do contain artwork files are deleted from music_directories.
The affected artwork files are still present in music_albumart but their
directory_ids are invalid.

With this change, the next scan will remove any invalid rows from
music_albumart, and all rows that are added will stay valid.

Fixes: f36c44f2d634 ("MythMusic: delete any unused directory_ids from music_directories after a scan")

mythtv/libs/libmythmetadata/musicfilescanner.cpp
@@ -422,13 +422,15 @@ void MusicFileScanner::cleanDB()
 
     // delete unused directory_ids from music_directories
     //
-    // Get a list of directory_ids not referenced in music_songs.
+    // Get a list of directory_ids not referenced in music_songs or music_albumart.
     // This list will contain any directory that is only used for
     // organization.  I.E. If your songs are organized by artist and
     // then by album, this will contain all of the artist directories.
     if (!query.exec("SELECT d.directory_id, d.parent_id FROM music_directories d "
                     "LEFT JOIN music_songs s ON d.directory_id=s.directory_id "
-                    "WHERE s.directory_id IS NULL ORDER BY directory_id DESC;"))
+                    "LEFT JOIN music_albumart a ON d.directory_id=a.directory_id "
+                    "WHERE s.directory_id IS NULL AND a.directory_id IS NULL "
+                    "ORDER BY directory_id DESC;"))
         MythDB::DBError("MusicFileScanner::cleanDB - select music_directories", query);
 
     deletequery.prepare("DELETE FROM music_directories WHERE directory_id=:DIRECTORYID");
@@ -848,7 +850,8 @@ void MusicFileScanner::ScanArtwork(MusicLoadedMap &art_files, QList<int> &albuma
     albumartidsToDelete.clear();
 
     MSqlQuery query(MSqlQuery::InitCon());
-    query.prepare("SELECT CONCAT_WS('/', path, filename), albumart_id "
+    query.prepare("SELECT CONCAT_WS('/', path, filename), albumart_id, "
+                  "music_albumart.directory_id, music_directories.directory_id "
                   "FROM music_albumart LEFT JOIN music_directories ON "
                   "music_albumart.directory_id=music_directories.directory_id "
                   "WHERE music_albumart.embedded = 0 "
@@ -867,6 +870,14 @@ void MusicFileScanner::ScanArtwork(MusicLoadedMap &art_files, QList<int> &albuma
     {
         while (query.next())
         {
+            // cleanDB() used to have a bug where it could delete entries from
+            // music_directories that were still referenced in music_albumart,
+            // so we check for that here and delete any affected rows.
+            if (query.value(2).toInt() != 0 && query.isNull(3)) {
+                albumartidsToDelete.append(query.value(1).toInt());
+                continue;
+            }
+
             for (int x = 0; x < m_startDirs.count(); x++)
             {
                 name = m_startDirs[x] + query.value(0).toString();