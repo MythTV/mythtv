7972fc892353a49544e3ea2a6b74e6087044af1a
Steve Erlenborn <1751095+SteveErl@users.noreply.github.com>
2026-09-03 14:13:42 -0500

    Remove raise of StopIteration in generator
    Tom Peterson's suggested change to replace the
    "raise StopIteration" lines with "return" inside the
    generator routine has been implemented in ttvdbv4_api.py.
    This is the PEP 479-compliant way to end a generator.
    
    In myth4ttvdbv4.py, the buildSingle() routine has
    been updated to accommodate a possible StopIteration
    exception when the generator is called. If that
    exception happens, we will exit with a "No matching
    episode found" error message. Similarly, buildCollection()
    has been updated to exit with a "No matching collection
    found" error message when the provided reference
    identifier does not correspond to a known TV series.
    
    The tvmaze metadata grabber had similar vague failures.
    
    For a non-existent episode:
    tvmaze.py -l en -a US -D 32938 2 35
    ERROR: <class 'AttributeError'> : 'NoneType' object has no attribute 'name'
    
    For a non-existent series:
    tvmaze.py -l en -a US -C 99999
    ERROR: <class 'AttributeError'> : 'NoneType' object has no attribute 'name'
    
    The tvmaze.py script has been updated to switch these to:
    
    tvmaze.py -l en -a US -D 32938 2 35
    ERROR: <class 'SystemExit'> : No matching episode found
    
    tvmaze.py -l en -a US -C 99999
    ERROR: <class 'SystemExit'> : No matching collection found
    
    This provides consistent behavior between ttvdb4.py and
    tvmaze.py grabbers for these types of failures.
    
    Resolves #1433

--- mythtv/programs/scripts/metadata/Television/tvmaze.py
+++ mythtv/programs/scripts/metadata/Television/tvmaze.py
@@ -444,6 +444,8 @@ def buildSingleItem(inetref, season, episode_id):
 
     # get info for season episodes:
     ep_info = tvmaze.get_episode_information(episode_id)
+    if not ep_info:
+        sys.exit("No matching episode found")
     m = VideoMetadata()
     if show_info.genres is not None and len(show_info.genres) > 0:
         for g in show_info.genres:
@@ -554,6 +556,8 @@ def buildCollection(tvinetref, opts):
         print("Function 'buildCollection' called with argument '%s'" % tvinetref)
 
     show_info = tvmaze.get_show(tvinetref)
+    if not show_info:
+        sys.exit("No matching collection found")
     if opts.debug:
         for k, v in show_info.__dict__.items():
             print(k, " : ", v)
